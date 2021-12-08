339ad92993df2454e5743f4da9e5aecb6370796c
Eduardo Armendariz <eduardo@ethyca.com>
2021-12-08 13:57:04 -0800

    Fix formatting and lint

--- fidesctl/src/fidesapi/view.py
+++ fidesctl/src/fidesapi/view.py
@@ -2,22 +2,22 @@
 Contains api endpoints for fides web pages
 """
 from fastapi import APIRouter
-
+from fastapi.responses import HTMLResponse
 from fidesapi.crud import list_resource
 from fidesapi.sql_models import Evaluation
-from fastapi.responses import HTMLResponse
 
 router = APIRouter(
     tags=["View"],
-    prefix=f"/view",
+    prefix="/view",
 )
+
 
 @router.get("/evaluations")
 async def evaluation_view() -> HTMLResponse:
     "Returns an html document with a list of evaluations"
 
     html = '<html lang="en">'
-    html += '<head>'
+    html += "<head>"
     html += '<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">'
     html += '<body>'
     html += '<h2>Evaluations</h2>'
@@ -25,15 +25,15 @@ async def evaluation_view() -> HTMLResponse:
     html += '<tr><th>Id</th><th>Status</th><th>Details</th><th>Message</th></tr>'
     for evaluation in list_resource(Evaluation):
         html += '<tr>'
-        html += f'<td><div">{evaluation.fides_key}</div></td>';
-        html += f'<td><div">{evaluation.status}</div></td>';
+        html += f'<td>{evaluation.fides_key}</td>'
+        html += f'<td>{evaluation.status}</td>'
 
-        html += '<td><div">';
+        html += '<td>'
         for detail in evaluation.details:
-            html += f'{detail}<br/>';
-        html += '</div></td>';
-        
-        html += f'<td><div">{evaluation.message}</div></td>';
+            html += f"{detail}<br/>"
+        html += '</td>'
+
+        html += f'<td>{evaluation.message}</td>'
         html += '</tr>'
     html += '</table>'
     html += '</head>'
