0f5f8d25123d5d23c5589724b3fc2a270f9a6887
Phil Salant <PSalant@gmail.com>
2021-12-10 18:19:00 -0500

    Change `print` calls to use `log.info`

--- fidesctl/src/fidesapi/database.py
+++ fidesctl/src/fidesapi/database.py
@@ -3,10 +3,11 @@ Contains all of the logic for spinning up/tearing down the database.
 """
 import os
 
-from sqlalchemy_utils.functions import create_database, database_exists
 from alembic import command
 from alembic.config import Config
 from alembic.migration import MigrationContext
+from loguru import logger as log
+from sqlalchemy_utils.functions import create_database, database_exists
 
 from fidesapi.sql_models import sql_model_map, SqlAlchemyBase
 from fidesapi.crud import upsert_resources
@@ -29,7 +30,7 @@ def get_alembic_config(database_url: str) -> Config:
 
 def upgrade_db(alembic_config: Config, revision: str = "head") -> None:
     "Upgrade the database to the specified migration revision."
-
+    log.info("Running database migrations")
     command.upgrade(alembic_config, revision)
 
 
@@ -37,6 +38,7 @@ def init_db(database_url: str) -> None:
     """
     Runs the migrations and creates all of the database objects.
     """
+    log.info("Initializing database")
     alembic_config = get_alembic_config(database_url)
     upgrade_db(alembic_config)
     load_default_taxonomy()
@@ -52,20 +54,19 @@ def create_db_if_not_exists(database_url: str) -> None:
 
 def load_default_taxonomy() -> None:
     "Upserts the default taxonomy into the database."
-    print("UPSERTING the default fideslang taxonomy")
+    log.info("UPSERTING the default fideslang taxonomy")
     for resource_type in list(DEFAULT_TAXONOMY.__fields_set__):
-        print("-" * 10)
-        print(f"Processing {resource_type} resources...")
+        log.info(f"Processing {resource_type} resources...")
         resources = list(map(dict, dict(DEFAULT_TAXONOMY)[resource_type]))
         upsert_resources(sql_model_map[resource_type], resources)
-        print(f"UPSERTED {len(resources)} {resource_type} resources.")
-    print("-" * 10)
+        log.info(f"Successfully UPSERTED {len(resources)} {resource_type} resources")
 
 
 def reset_db(database_url: str) -> None:
     """
     Drops all tables/metadata from the database.
     """
+    log.info("Resetting database")
     engine = get_db_engine(database_url)
     connection = engine.connect()
     SqlAlchemyBase.metadata.drop_all(connection)
