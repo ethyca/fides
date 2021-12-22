4eb5c79b91e8e1b019a322f990ee2734d11b73d2
Steven Benjamin <steven.e.benjamin@gmail.com>
2021-12-22 13:47:38 -0500

    Data generator (#137)
    * sketch for data generator
    
    * generating values
    
    * working with types
    
    * cleanup
    
    * sample data generator
    
    Co-authored-by: Steven Benjamin <steven@ethyca.com>

--- tests/tasks/test_scheduled.py
+++ tests/tasks/test_scheduled.py
@@ -5,7 +5,9 @@ from fidesops.models.privacy_request import PrivacyRequestStatus
 from fidesops.schemas.storage.storage import (
     StorageDetails,
 )
-from fidesops.service.privacy_request.request_runner_service import initiate_paused_privacy_request_followup
+from fidesops.service.privacy_request.request_runner_service import (
+    initiate_paused_privacy_request_followup,
+)
 from fidesops.tasks.scheduled.scheduler import scheduler
 from fidesops.tasks.scheduled.tasks import (
     initiate_scheduled_request_intake,
@@ -34,7 +36,9 @@ def test_initiate_scheduled_request_intake(storage_config_onetrust) -> None:
     )
 
 
-def test_initiate_scheduled_paused_privacy_request_followup(privacy_request, db) -> None:
+def test_initiate_scheduled_paused_privacy_request_followup(
+    privacy_request, db
+) -> None:
     privacy_request.status = PrivacyRequestStatus.paused
     privacy_request.save(db=db)
     initiate_paused_privacy_request_followup(privacy_request)
