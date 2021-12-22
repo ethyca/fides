49d5cdd5c6f2e9a3f0d6c32fff29ef8959098ed5
Thomas La Piana <tal103020@icloud.com>
2021-12-22 13:11:27 -0600

    remove mssql from the "all" extra, annotate the integration test markers

--- fidesctl/setup.py
+++ fidesctl/setup.py
@@ -25,7 +25,7 @@ extras = {
     "webserver": [fastapi, uvicorn, postgres_connector],
 }
 dangerous_extras = ["mssql"]  # These extras break on certain platforms
-extras["all"] = sum(extras.values(), [])
+extras["all"] = sum([value for value in extras.values() if value not in dangerous_extras], [])
 
 setup(
     name="fidesctl",
