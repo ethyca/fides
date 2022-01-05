840e1de86473cc1c76319bd6ae9b82e0528732f6
earmenda <eduardo.armendariz13@gmail.com>
2022-01-05 10:03:06 -0800

    Fix evaluations page after evaluation model refactor (#301)
    Co-authored-by: Eduardo Armendariz <eduardo@ethyca.com>

--- fidesctl/src/fidesapi/view.py
+++ fidesctl/src/fidesapi/view.py
@@ -22,15 +22,27 @@ async def evaluation_view() -> HTMLResponse:
     html += "<body>"
     html += "<h2>Evaluations</h2>"
     html += '<table class="table table-striped" style="text-align: left;">'
-    html += "<tr><th>Id</th><th>Status</th><th>Details</th><th>Message</th></tr>"
+    html += "<tr><th>Id</th><th>Status</th><th>Violations</th><th>Message</th></tr>"
     for evaluation in list_resource(Evaluation):
         html += "<tr>"
         html += f"<td>{evaluation.fides_key}</td>"
         html += f"<td>{evaluation.status}</td>"
 
         html += "<td>"
-        for detail in evaluation.details:
-            html += f"{detail}<br/>"
+        for index, violation in enumerate(evaluation.violations):
+            violating_attributes = violation.get("violating_attributes")
+            html += f"<b>Violation {index + 1}</b><br/>"
+            html += "<ul>"
+            html += f"<li>Data Categories: {violating_attributes.get('data_categories')}</li>"
+            html += (
+                f"<li>Data Subjects: {violating_attributes.get('data_subjects')}</li>"
+            )
+            html += f"<li>Data Uses: {violating_attributes.get('data_uses')}</li>"
+            html += (
+                f"<li>Data Qualifier: {violating_attributes.get('data_qualifier')}</li>"
+            )
+            html += f"<li>Detail: {violation.get('detail')}</li>"
+            html += "</ul>"
         html += "</td>"
 
         html += f"<td>{evaluation.message}</td>"
