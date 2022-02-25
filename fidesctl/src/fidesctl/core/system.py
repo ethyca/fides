797fd5c01485c2e7dd68187efef0f89a92497a62
earmenda <eduardo.armendariz13@gmail.com>
2022-02-25 11:23:37 -0800

    Implement scan system aws command (#377)
    * Implement scan system aws command
    
    * Use fixture for creating system
    
    * Fix integration test fixtures
    
    * Fix test expected transformed output
    
    Co-authored-by: Eduardo Armendariz <eduardo@ethyca.com>

--- fidesctl/src/fidesctl/core/system.py
+++ fidesctl/src/fidesctl/core/system.py
@@ -1,11 +1,18 @@
 """Module that adds functionality for generating or scanning systems."""
-from typing import Dict, List
+from typing import Dict, List, Tuple, Callable
 
 import boto3
 
+from pydantic import AnyHttpUrl
+
+from fidesctl.cli.utils import handle_cli_response
+from fidesctl.core import api
+from fidesctl.core.parse import parse
+from fidesctl.core.api_helpers import get_server_resources
 from fideslang import manifests
 from fideslang.models import System, SystemMetadata
-from .utils import echo_green
+
+from .utils import echo_green, echo_red
 
 
 def describe_redshift_clusters() -> Dict[str, List[Dict]]:
@@ -76,3 +83,145 @@ def generate_system_aws(file_name: str, include_null: bool) -> str:
     )
     echo_green(f"Generated system manifest written to {file_name}")
     return file_name
+
+
+def get_redshift_arns(describe_clusters: Dict[str, List[Dict]]) -> List[str]:
+    """
+    Given a describe clusters response, build a list of cluster arns
+    """
+    redshift_arns = [
+        cluster["ClusterNamespaceArn"] for cluster in describe_clusters["Clusters"]
+    ]
+    return redshift_arns
+
+
+def scan_redshift_systems() -> Tuple[str, List[str]]:
+    """
+    Fetches Redshift clusters from AWS and returns a scan label and
+    list of cluster arns.
+    """
+    redshift_clusters = describe_redshift_clusters()
+    redshift_arns = get_redshift_arns(describe_clusters=redshift_clusters)
+    return "Redshift Clusters", redshift_arns
+
+
+def get_all_server_systems(
+    url: AnyHttpUrl, headers: Dict[str, str], exclude_systems: List[System]
+) -> List[System]:
+    """
+    Get a list of all of the Systems that exist on the server. Excludes any systems
+    provided in exclude_systems
+    """
+    ls_response = handle_cli_response(
+        api.ls(url=url, resource_type="system", headers=headers), verbose=False
+    )
+    exclude_system_keys = [system.fides_key for system in exclude_systems]
+    system_keys = [
+        resource["fides_key"]
+        for resource in ls_response.json()
+        if resource["fides_key"] not in exclude_system_keys
+    ]
+    system_list = get_server_resources(
+        url=url, resource_type="system", headers=headers, existing_keys=system_keys
+    )
+    return system_list
+
+
+def scan_aws_systems(
+    scan_system_functions: List[Callable[[], Tuple[str, List[str]]]],
+    existing_system_arns: List[str],
+) -> Tuple[str, int, int]:
+    """
+    Given a list of scan system functions, compares function result to
+    existing system arns. Returns missing resource text output, scanned
+    resource count, and missing resource count.
+    """
+    scan_text_output = ""
+    scanned_resource_count = 0
+    missing_resource_count = 0
+    for scan_system_function in scan_system_functions:
+        resource_label, resource_arns = scan_system_function()
+        missing_resources = set(resource_arns) - set(existing_system_arns)
+        scanned_resource_count += len(resource_arns)
+        missing_resource_count += len(missing_resources)
+        if missing_resource_count > 0:
+            scan_text_output += f"Missing {resource_label}:\n"
+            scan_text_output += "\t{}\n".format("\n\t".join(missing_resources))
+
+    return scan_text_output, scanned_resource_count, missing_resource_count
+
+
+def print_scan_system_aws_result(
+    scan_text_output: str,
+    scanned_resource_count: int,
+    missing_resource_count: int,
+    coverage_threshold: int,
+) -> None:
+    """
+    Prints uncategorized fields and raises an exception if coverage
+    is lower than provided threshold.
+    """
+    if missing_resource_count < 1:
+        echo_green(
+            f"Scanned {scanned_resource_count} resources and all were found in taxonomy."
+        )
+    else:
+        echo_red(
+            f"Scanned {scanned_resource_count} resource(s) and found {missing_resource_count} missing resource(s).\n"
+            + scan_text_output
+        )
+
+    coverage_percent = int(
+        ((scanned_resource_count - missing_resource_count) / scanned_resource_count)
+        * 100
+    )
+    annotation_output = "Resource coverage: {}%".format(coverage_percent)
+    if coverage_percent < coverage_threshold:
+        echo_red(annotation_output)
+        raise SystemExit(1)
+    echo_green(annotation_output)
+
+
+def scan_system_aws(
+    manifest_dir: str,
+    coverage_threshold: int,
+    url: AnyHttpUrl,
+    headers: Dict[str, str],
+) -> None:
+    """
+    Connect to an aws account by leveraging a valid boto3 environment varible
+    configuration and compares tracked resources to existing systems.
+    Tracked resources: [Redshift]
+    """
+    scan_system_functions = [scan_redshift_systems]
+
+    manifest_taxonomy = parse(manifest_dir) if manifest_dir else None
+    manifest_systems = manifest_taxonomy.system if manifest_taxonomy else []
+    server_systems = get_all_server_systems(
+        url=url, headers=headers, exclude_systems=manifest_systems
+    )
+    existing_system_arns = [
+        system.fidesctl_meta.resource_id
+        for system in manifest_systems + server_systems
+        if system.fidesctl_meta and system.fidesctl_meta.resource_id
+    ]
+
+    (
+        scan_text_output,
+        scanned_resource_count,
+        missing_resource_count,
+    ) = scan_aws_systems(
+        scan_system_functions=scan_system_functions,
+        existing_system_arns=existing_system_arns,
+    )
+
+    if scanned_resource_count < 1:
+        echo_red("Did not find any resources to scan in AWS account")
+        raise SystemExit(1)
+
+    print_scan_system_aws_result(
+        scan_text_output=scan_text_output,
+        scanned_resource_count=scanned_resource_count,
+        missing_resource_count=missing_resource_count,
+        coverage_threshold=coverage_threshold,
+    )
--- fidesctl/tests/core/test_system.py
+++ fidesctl/tests/core/test_system.py
@@ -1,11 +1,47 @@
 import pytest
 
-from fidesctl.core import system as _system
+from typing import List
+
+from fidesctl.core import system as _system, api
 from fideslang.models import System, SystemMetadata
 
 
-@pytest.mark.unit
-def test_transform_redshift_systems():
+def create_server_systems(test_config, systems):
+    for system in systems:
+        api.create(
+            url=test_config.cli.server_url,
+            resource_type="system",
+            json_resource=system.json(exclude_none=True),
+            headers=test_config.user.request_headers,
+        )
+ 
+def delete_server_systems(test_config, systems):
+    for system in systems:
+        api.delete(
+            url=test_config.cli.server_url,
+            resource_type="system",
+            resource_id=system.fides_key,
+            headers=test_config.user.request_headers,
+        )
+
+@pytest.fixture(scope="function")
+def create_test_server_systems(test_config, redshift_systems):
+    systems = redshift_systems
+    delete_server_systems(test_config, systems)
+    create_server_systems(test_config, systems)
+    yield
+    delete_server_systems(test_config, systems)
+
+@pytest.fixture(scope="function")
+def create_external_server_systems(test_config):
+    systems = _system.generate_redshift_systems()
+    delete_server_systems(test_config, systems)
+    create_server_systems(test_config, systems)
+    yield
+    delete_server_systems(test_config, systems)
+
+@pytest.fixture()
+def redshift_describe_clusters():
     describe_clusters = {
         "Clusters": [
             {
@@ -15,10 +51,23 @@ def test_transform_redshift_systems():
                     "Port": 5439,
                 },
                 "ClusterNamespaceArn": "arn:aws:redshift:us-east-1:910934740016:namespace:057d5b0e-7eaa-4012-909c-3957c7149176",
-            }
+            },
+            {
+                "ClusterIdentifier": "redshift-cluster-2",
+                "Endpoint": {
+                    "Address": "redshift-cluster-2.c2angfh5kpo4.us-east-1.redshift.amazonaws.com",
+                    "Port": 5439,
+                },
+                "ClusterNamespaceArn": "arn:aws:redshift:us-east-1:910934740016:namespace:057d5b0e-7eaa-4012-909c-3957c7149177",
+            },
         ]
     }
-    expected_result = [
+    yield describe_clusters
+
+
+@pytest.fixture()
+def redshift_systems():
+    redshift_systems = [
         System(
             fides_key="redshift-cluster-1",
             organization_fides_key="default_organization",
@@ -31,11 +80,77 @@ def test_transform_redshift_systems():
             ),
             system_type="redshift_cluster",
             privacy_declarations=[],
-        )
+        ),
+        System(
+            fides_key="redshift-cluster-2",
+            organization_fides_key="default_organization",
+            name="redshift-cluster-2",
+            description="Fides Generated Description for Cluster: redshift-cluster-2",
+            fidesctl_meta=SystemMetadata(
+                endpoint_address="redshift-cluster-2.c2angfh5kpo4.us-east-1.redshift.amazonaws.com",
+                endpoint_port="5439",
+                resource_id="arn:aws:redshift:us-east-1:910934740016:namespace:057d5b0e-7eaa-4012-909c-3957c7149177",
+            ),
+            system_type="redshift_cluster",
+            privacy_declarations=[],
+        ),
     ]
-    actual_result = _system.transform_redshift_systems(describe_clusters)
+    yield redshift_systems
+
+
+@pytest.mark.unit
+def test_transform_redshift_systems(redshift_describe_clusters, redshift_systems):
+    actual_result = _system.transform_redshift_systems(redshift_describe_clusters)
+    assert actual_result == redshift_systems
+
+
+@pytest.mark.unit
+def test_get_redshift_arns(redshift_describe_clusters):
+    expected_result = [
+        "arn:aws:redshift:us-east-1:910934740016:namespace:057d5b0e-7eaa-4012-909c-3957c7149176",
+        "arn:aws:redshift:us-east-1:910934740016:namespace:057d5b0e-7eaa-4012-909c-3957c7149177",
+    ]
+    actual_result = _system.get_redshift_arns(redshift_describe_clusters)
     assert actual_result == expected_result
 
+
+@pytest.mark.unit
+def test_scan_aws_systems():
+    (
+        scan_text_output,
+        scanned_resource_count,
+        missing_resource_count,
+    ) = _system.scan_aws_systems(
+        scan_system_functions=[
+            lambda: ("Scanned Resource Type 1", ["arn1", "arn2", "arn3"]),
+            lambda: ("Scanned Resource Type 2", ["arn4"]),
+            lambda: ("Scanned Resource Type 3", ["arn5"]),
+        ],
+        existing_system_arns=["arn4", "arn3"],
+    )
+    assert scan_text_output
+    assert scanned_resource_count == 5
+    assert missing_resource_count == 3
+
+
+@pytest.mark.integration
+def test_get_all_server_systems(test_config, create_test_server_systems):
+    actual_result = _system.get_all_server_systems(
+        url=test_config.cli.server_url,
+        headers=test_config.user.request_headers,
+        exclude_systems=[],
+    )
+    assert actual_result
+
+
+@pytest.mark.external
+def test_scan_system_aws_passes(test_config, create_external_server_systems):
+    _system.scan_system_aws(
+        coverage_threshold=100,
+        manifest_dir="",
+        url=test_config.cli.server_url,
+        headers=test_config.user.request_headers,
+    )
 
 @pytest.mark.external
 def test_generate_system_aws(tmpdir):
