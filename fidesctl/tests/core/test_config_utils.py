7d531af931d8185e68b8d09b4c8daf6584ab2804
SteveDMurphy <steven.d.murphy@gmail.com>
2022-03-15 16:35:50 -0400

    testing for update_config_file

---
+++ fidesctl/tests/core/test_config_utils.py
@@ -0,0 +1,44 @@
+import os
+
+import pytest
+from toml import dump, load
+
+from fidesctl.core.config.utils import update_config_file
+
+
+@pytest.fixture
+def test_change_config():
+    """Create a dictionary to be used as an example config file"""
+
+    yield {"cli": {"analytics_id": "initial_id"}}
+
+
+@pytest.mark.unit
+def test_update_config_file_new_value(test_change_config, tmpdir):
+    """
+    Create an example config.toml and validate both updating an
+    existing config setting and adding a new section and setting.
+    """
+
+    config_path = os.path.join(tmpdir, "test_writing_config.toml")
+
+    with open(config_path, "w") as config_file:
+        dump(test_change_config, config_file)
+
+    config_updates = {
+        "cli": {"analytics_id": "updated_id"},
+        "user": {"analytics_opt_out": True},
+    }
+
+    update_config_file(config_updates, config_path)
+
+    updated_config = load(config_path)
+
+    assert updated_config["cli"] is not None, "updated_config.cli should exist"
+    assert (
+        updated_config["cli"]["analytics_id"] == "updated_id"
+    ), "updated_config.cli.analytics_id should be 'updated_id'"
+    assert updated_config["user"] is not None, "updated_config.user should exist"
+    assert (
+        updated_config["user"]["analytics_opt_out"] == True
+    ), "updated_config.user.analytics_opt_out should be True"
