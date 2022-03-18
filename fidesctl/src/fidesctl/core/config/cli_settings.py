79b00810d036bf0f4096bba5133c34e989219940
Phil Salant <PSalant@gmail.com>
2022-03-18 15:42:10 -0400

    Use `FIDESCTL__CLI__ANALYTICS_ID` directly

--- fidesctl/src/fidesctl/core/config/cli_settings.py
+++ fidesctl/src/fidesctl/core/config/cli_settings.py
@@ -1,6 +1,4 @@
 """This module defines the settings for everything related to the CLI."""
-from os import getenv
-from typing import Optional
 
 from fideslog.sdk.python.utils import generate_client_id, FIDESCTL_CLI
 from pydantic import validator
@@ -15,21 +13,13 @@ class CLISettings(FidesSettings):
 
     local_mode: bool = False
     server_url: str = "http://localhost:8080"
-    analytics_id: Optional[str]
+    analytics_id: str = generate_client_id(FIDESCTL_CLI)
 
     @validator("analytics_id", always=True)
-    def ensure_analytics_id_override(cls, value: str) -> str:
+    def ensure_not_empty(cls, value: str) -> str:
         """
-        Resolves the `analytics_id` value in the following order of priority:
-        1. The value of the `ANALYTICS_ID_OVERRIDE` environment variable
-        2. The value of the `FIDESCTL__CLI__ANALYTICS_ID` environment variable
-        3. The value included in the config file
-        4. A newly generated value
+        Validate that the `analytics_id` is not `""`.
         """
-
-        override_id = getenv("ANALYTICS_ID_OVERRIDE")
-        if override_id is not None and override_id != "":
-            return override_id
 
         return value if value != "" else generate_client_id(FIDESCTL_CLI)
 
