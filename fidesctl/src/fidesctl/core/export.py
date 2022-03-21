5db4d84ee65b9da689f9ac2b06d5abbab0522194
SteveDMurphy <steven.d.murphy@gmail.com>
2022-03-21 11:14:50 -0400

    new type hinting around a list of tuples

--- fidesctl/src/fidesctl/core/export.py
+++ fidesctl/src/fidesctl/core/export.py
@@ -126,13 +126,13 @@ def generate_system_records(
     server_system_list: List,
     url: str,
     headers: Dict[str, str],
-) -> List[Tuple[str, str, str, str, str, str, str, str, str, str, str, str, str]]:
+) -> List[Tuple[str, ...]]:
     """
     Takes a list of systems from the server, creating a list of tuples
     to be used as records to be exported. The headers of the csv are
     currently added here as well.
     """
-    output_list = [
+    output_list: List[Tuple[str, ...]] = [
         (
             "system.name",
             "system.description",
