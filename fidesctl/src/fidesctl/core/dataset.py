8e1e25e1928cf78285e0cc7e836a0bcd1369b83a
ThomasLaPiana <thomas.lapiana@pm.me>
2022-03-31 14:27:11 -0500

    fix a bug in dataset.py that fails if the manifest param is empty

--- fidesctl/src/fidesctl/core/dataset.py
+++ fidesctl/src/fidesctl/core/dataset.py
@@ -212,12 +212,13 @@ def scan_dataset_db(
     local manifest (if one is provided).
     """
     manifest_taxonomy = parse(manifest_dir) if manifest_dir else None
-    dataset_keys = [dataset.fides_key for dataset in manifest_taxonomy.dataset]
-    echo_green(
-        "Loaded the following dataset manifests:\n\t{}".format(
-            "\t\n".join(dataset_keys)
+    if manifest_taxonomy:
+        dataset_keys = [dataset.fides_key for dataset in manifest_taxonomy.dataset]
+        echo_green(
+            "Loaded the following dataset manifests:\n\t{}".format(
+                "\t\n".join(dataset_keys)
+            )
         )
-    )
 
     # Generate the collections and fields for the target database
     db_engine = get_db_engine(connection_string)
