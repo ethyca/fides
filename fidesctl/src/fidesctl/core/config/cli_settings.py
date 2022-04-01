125161662dc6b47f16f08575d4eb1b392a010f73
ThomasLaPiana <thomas.lapiana@pm.me>
2022-04-01 16:18:48 -0500

    fix the mypy error

--- fidesctl/src/fidesctl/core/config/cli_settings.py
+++ fidesctl/src/fidesctl/core/config/cli_settings.py
@@ -13,8 +13,9 @@ class CLISettings(FidesSettings):
     """Class used to store values from the 'cli' section of the config."""
 
     local_mode: bool = False
-    server_host: AnyHttpUrl = "http://localhost"
-    server_port: Optional[int] = 8080
+    # Ignored due to this bug: https://github.com/samuelcolvin/pydantic/issues/3143
+    server_host: AnyHttpUrl = "http://localhost"  # type: ignore
+    server_port: Optional[int]
     server_url: Optional[AnyHttpUrl]
     analytics_id: str = generate_client_id(FIDESCTL_CLI)
 
@@ -26,9 +27,11 @@ class CLISettings(FidesSettings):
         This needs to be set as a root validator,
         otherwise the type errors gets suppressed.
         """
-        values["server_url"] = "{}:{}".format(
-            values["server_host"],
-            values["server_port"],
+        server_host = values["server_url"]
+        server_port = values["server_port"]
+        values["server_url"] = "{}{}".format(
+            server_host,
+            ":" + str(server_port) if server_port else "",
         )
         return values
 
