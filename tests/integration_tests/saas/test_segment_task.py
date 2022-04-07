875d04debf23111b7090323bf18bb1d63785fb12
Dawn Pattison <pattisdr@users.noreply.github.com>
2022-04-07 12:49:08 -0500

    [Saas Connectors] Segment Masking [#354] (#368)
    * Fix segment data_protection_request for making GDPR delete requests.
    
    - Add end-to-end test for segment masking that creates a user and then issues a GDPR delete request for that user.  Request is confirmed to have been made but we can't verify deletion here as there is a delay on the segment side.
    
    * Remove two fields from segment yaml that are only used in testing to populate the initial data.

--- tests/integration_tests/saas/test_segment_task.py
+++ tests/integration_tests/saas/test_segment_task.py
@@ -1,12 +1,20 @@
-from fidesops.task.filter_results import filter_data_categories
-import pytest
+from typing import Dict
+import time
 import random
+import requests
+import pytest
+from faker import Faker
+
+from fidesops.core.config import config
+from fidesops.task.filter_results import filter_data_categories
+
 
 from fidesops.graph.graph import DatasetGraph
 from fidesops.models.privacy_request import PrivacyRequest
 from fidesops.schemas.redis_cache import PrivacyRequestIdentity
 
 from fidesops.task import graph_task
+from fidesops.task.graph_task import get_cached_data_for_erasures
 from tests.graph.graph_test_util import assert_rows_match
 
 
@@ -102,19 +110,17 @@ def test_segment_saas_access_request_task(
     }
 
     assert set(filtered_results[f"{dataset_name}:traits"][0].keys()) == {
-
-            "address",
-            "title",
-            "description",
-            "username",
-            "phone",
-            "id",
-            "website",
-            "email",
-            "name",
-            "age",
-            "firstName",
-
+        "address",
+        "title",
+        "description",
+        "username",
+        "phone",
+        "id",
+        "website",
+        "email",
+        "name",
+        "age",
+        "firstName",
     }
     assert (
         filtered_results[f"{dataset_name}:traits"][0]["email"] == segment_identity_email
@@ -131,3 +137,165 @@ def test_segment_saas_access_request_task(
     )
 
     assert filtered_results[f"{dataset_name}:segment_user"][0]["segment_id"]
+
+
+def _create_test_segment_email(base_email: str, timestamp: int) -> str:
+    at_index: int = base_email.find("@")
+    email = f"{base_email[0:at_index]}{timestamp}{base_email[at_index:]}"
+    return email
+
+
+def create_segment_test_data(
+    segment_secrets: Dict[str, str], segment_identity_email: str
+):
+    """Seeds a segment user and event"""
+    if not segment_identity_email:  # Don't run unnecessarily locally
+        return
+
+    faker = Faker()
+
+    ts = int(time.time())
+    email = _create_test_segment_email(segment_identity_email, ts)
+    first_name = faker.first_name()
+    last_name = faker.last_name()
+
+    # Create user
+    headers = {
+        "Content-Type": "application/json",
+        "Authorization": f"Basic {segment_secrets['user_token']}",
+    }
+    body = {
+        "userId": email,
+        "traits": {
+            "subscriptionStatus": "active",
+            "address": {
+                "city": faker.city(),
+                "country": faker.country(),
+                "postalCode": faker.postcode(),
+                "state": "NY",
+            },
+            "age": random.randrange(18, 99),
+            "avatar": "",
+            "industry": "data",
+            "description": faker.job(),
+            "email": email,
+            "firstName": first_name,
+            "id": ts,
+            "lastName": last_name,
+            "name": f"{first_name} {last_name}",
+            "phone": faker.phone_number(),
+            "title": faker.prefix(),
+            "username": f"test_fidesops_user_{ts}",
+            "website": "www.example.com",
+        },
+    }
+    resp = requests.post(
+        f"https://{segment_secrets['api_domain']}identify", headers=headers, json=body
+    )
+    assert resp.status_code == 200
+
+    # Create event
+    body = {
+        "userId": email,
+        "type": "track",
+        "event": "User Registered",
+        "properties": {"plan": "Free", "accountType": faker.company()},
+        "context": {"ip": faker.ipv4()},
+    }
+
+    resp = requests.post(
+        f"https://{segment_secrets['api_domain']}track", headers=headers, json=body
+    )
+    assert resp.status_code == 200
+    return email
+
+
+@pytest.mark.integration_saas
+@pytest.mark.integration_segment
+def test_segment_saas_erasure_request_task(
+    db,
+    policy,
+    segment_connection_config,
+    segment_dataset_config,
+    segment_identity_email,
+    segment_secrets,
+) -> None:
+    """Full erasure request based on the Segment SaaS config"""
+    config.execution.MASKING_STRICT = False  # Allow GDPR Delete
+
+    # Create user for GDPR delete
+    erasure_email = create_segment_test_data(segment_secrets, segment_identity_email)
+    privacy_request = PrivacyRequest(
+        id=f"test_saas_access_request_task_{random.randint(0, 1000)}"
+    )
+    identity = PrivacyRequestIdentity(**{"email": erasure_email})
+    privacy_request.cache_identity(identity)
+
+    dataset_name = segment_connection_config.get_saas_config().fides_key
+    merged_graph = segment_dataset_config.get_graph()
+    graph = DatasetGraph(merged_graph)
+
+    v = graph_task.run_access_request(
+        privacy_request,
+        policy,
+        graph,
+        [segment_connection_config],
+        {"email": erasure_email},
+    )
+
+    assert_rows_match(
+        v[f"{dataset_name}:track_events"],
+        min_size=1,
+        keys=[
+            "external_ids",
+            "context",
+            "type",
+            "source_id",
+            "message_id",
+            "timestamp",
+            "properties",
+            "event",
+            "related",
+        ],
+    )
+    assert_rows_match(
+        v[f"{dataset_name}:traits"],
+        min_size=1,
+        keys=[
+            "address",
+            "age",
+            "avatar",
+            "description",
+            "email",
+            "firstName",
+            "id",
+            "industry",
+            "lastName",
+            "name",
+            "phone",
+            "subscriptionStatus",
+            "title",
+            "username",
+            "website",
+        ],
+    )
+
+    x = graph_task.run_erasure(
+        privacy_request,
+        policy,
+        graph,
+        [segment_connection_config],
+        {"email": erasure_email},
+        get_cached_data_for_erasures(privacy_request.id),
+    )
+
+    # Assert erasure request made to segment_user - cannot verify success immediately as this can take
+    # days, weeks to process
+    assert x == {
+        "segment_connector_example:segment_user": 1,
+        "segment_connector_example:traits": 0,
+        "segment_connector_example:external_ids": 0,
+        "segment_connector_example:track_events": 0,
+    }
+
+    config.execution.MASKING_STRICT = True  # Reset
