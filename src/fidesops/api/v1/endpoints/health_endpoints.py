ade810562b8def62e57d0241f58b20bc0fb4c803
Paul Sanders <psanders1@gmail.com>
2022-04-18 09:55:54 -0400

    Remove unused import (#347)
    * Remove unused import
    
    * Add health url to urn_registry.py

--- src/fidesops/api/v1/endpoints/health_endpoints.py
+++ src/fidesops/api/v1/endpoints/health_endpoints.py
@@ -1,12 +1,13 @@
-from typing import Any, Dict
+from typing import Dict
 
 from fastapi import APIRouter
 
+from fidesops.api.v1.urn_registry import HEALTH
 
 router = APIRouter(tags=["Public"])
 
 
-@router.get("/health", response_model=Dict[str, bool])
+@router.get(HEALTH, response_model=Dict[str, bool])
 def health_check() -> Dict[str, bool]:
     return {
         "healthy": True,
