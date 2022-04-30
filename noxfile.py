b20095ed0a8f5da59098d3c98c15e1d36a8d7904
ThomasLaPiana <thomas.lapiana@pm.me>
2022-04-30 02:38:12 -0500

    all of the dev commands are working, further cleanup

--- noxfile.py
+++ noxfile.py
@@ -1,7 +1,8 @@
-from git.repo import Repo
+from os import getenv
 
 import nox
 
+# Sets the default session to `--list`
 nox.options.sessions = []
 
 ###############
@@ -9,6 +10,8 @@ nox.options.sessions = []
 ###############
 def get_current_tag() -> str:
     """Get the current git tag."""
+    from git.repo import Repo
+
     repo = Repo()
     git_session = repo.git()
     git_session.fetch("--force", "--tags")
@@ -23,33 +26,26 @@ WITH_TEST_CONFIG = "-f tests/test_config.toml"
 
 # Image Names & Tags
 REGISTRY = "ethyca"
-IMAGE_TAG = get_current_tag()
 IMAGE_NAME = "fidesctl"
-IMAGE = f"{REGISTRY}/{IMAGE_NAME}:{IMAGE_TAG}"
-IMAGE_LOCAL = f"{REGISTRY}/{IMAGE_NAME}:local"
-IMAGE_LATEST = f"{REGISTRY}/{IMAGE_NAME}:latest"
-
-# Requirements
-DOCKER = "docker>=5"
-DOCKER_COMPOSE = "docker-compose>=1.29.2"
+IMAGE = f"{REGISTRY}/{IMAGE_NAME}"
+IMAGE_LOCAL = f"{IMAGE}:local"
+IMAGE_LATEST = f"{IMAGE}:latest"
 
 # Disable TTY to perserve output within Github Actions logs
 # CI env variable is always set to true in Github Actions
-CI_ARGS = "--no-TTY"
-# ifeq "$(CI)" "true"
-#     CI_ARGS:=--no-TTY
-# endif
+# The else statement is required due to the way commmands are structured and is arbitrary.
+CI_ARGS = "--no-TTY" if getenv("CI") else "--user=root"
 
 # If FIDESCTL__CLI__ANALYTICS_ID is set in the local environment, use its value as the analytics_id
 ANALYTICS_ID_OVERRIDE = ("-e", "FIDESCTL__CLI__ANALYTICS_ID")
 # Reusable Commands
-RUN = ("docker-compose", "run", "--rm", ANALYTICS_ID_OVERRIDE, CI_ARGS, IMAGE_NAME)
+RUN = ("docker-compose", "run", "--rm", *ANALYTICS_ID_OVERRIDE, CI_ARGS, IMAGE_NAME)
 RUN_NO_DEPS = (
     "docker-compose",
     "run",
     "--no-deps",
     "--rm",
-    ANALYTICS_ID_OVERRIDE,
+    *ANALYTICS_ID_OVERRIDE,
     CI_ARGS,
     IMAGE_NAME,
 )
@@ -62,32 +58,36 @@ START_APP = ("docker-compose", "up", "-d", IMAGE_NAME)
 def reset_db(session: nox.Session) -> None:
     """Reset the database."""
     build_local(session)
-    session.run(START_APP)
-    session.run(RUN, "fidesctl", "db", "reset", "-y")
     session.notify("teardown")
+    session.run(*START_APP, external=True)
+    reset_db_command = (*RUN, "fidesctl", "db", "reset", "-y")
+    session.run(*reset_db_command, external=True)
 
 
 @nox.session()
 def api(session: nox.Session) -> None:
     """Spin up the webserver."""
     build_local(session)
-    session.run("docker-compose", "up", IMAGE_NAME, external=True)
-    teardown(session)
+    session.notify("teardown")
+    run_in_background = ("docker-compose", "up", IMAGE_NAME)
+    session.run(*run_in_background, external=True)
 
 
 @nox.session()
 def cli(session: nox.Session) -> None:
     """Spin up a local development shell."""
     build_local(session)
-    session.run("docker-compose", "up", IMAGE_NAME, external=True)
-    session.run(RUN, "/bin/bash", external=True)
-    teardown(session)
+    session.notify("teardown")
+    session.run(*START_APP, external=True)
+    run_shell = (*RUN, "/bin/bash")
+    session.run(*run_shell, external=True, success_codes=[0, 130])
 
 
 @nox.session()
 def cli_integration(session: nox.Session) -> None:
     """Spin up a local development shell with integration images spun up."""
-    session.notify("build_local")
+    build_local(session)
+    session.notify("teardown")
     session.run(
         "docker-compose",
         "-f",
@@ -97,15 +97,17 @@ def cli_integration(session: nox.Session) -> None:
         "up",
         "-d",
         IMAGE_NAME,
+        external=True,
     )
-    session.run(RUN, "/bin/bash")
-    session.notify("teardown")
+    run_shell = (*RUN, "/bin/bash")
+    session.run(*run_shell, external=True)
 
 
 @nox.session()
 def db(session: nox.Session) -> None:
     """Spin up the application database in the background."""
-    session.run("docker", "compose", "up", "-d", "fidesctl-db")
+    db_up = ("docker-compose", "up", "-d", "fidesctl-db")
+    session.run(*db_up, external=True)
 
 
 ############
@@ -114,7 +116,15 @@ def db(session: nox.Session) -> None:
 @nox.session()
 def build(session: nox.Session) -> None:
     """Build the Docker container for fidesctl."""
-    session.run("docker", "build", "--target=prod", "--tag", IMAGE, ".", external=True)
+    session.run(
+        "docker",
+        "build",
+        "--target=prod",
+        "--tag",
+        f"{IMAGE}:{get_current_tag()}",
+        ".",
+        external=True,
+    )
 
 
 @nox.session()
@@ -144,25 +154,25 @@ def push(session: nox.Session) -> None:
 ###########
 ## Utils ##
 ###########
+COMPOSE_DOWN = (
+    "docker-compose",
+    "-f",
+    COMPOSE_FILE,
+    "-f",
+    INTEGRATION_COMPOSE_FILE,
+    "down",
+    "--remove-orphans",
+)
+
+
 @nox.session()
 def clean(session: nox.Session) -> None:
     """
     Clean up docker containers, remove orphans, remove volumes
     and prune images related to this project.
     """
-    session.run(
-        "docker-compose",
-        "-f",
-        COMPOSE_FILE,
-        "-f",
-        INTEGRATION_COMPOSE_FILE,
-        "down",
-        "--remove-orphans",
-        "--volumes",
-        "--rmi",
-        "all",
-        external=True,
-    )
+    clean_command = (*COMPOSE_DOWN, "--volumes", "--rmi", "all")
+    session.run(*clean_command, external=True)
     session.run("docker", "system", "prune", "--force", external=True)
     print("Clean Complete!")
 
@@ -170,14 +180,5 @@ def clean(session: nox.Session) -> None:
 @nox.session()
 def teardown(session: nox.Session) -> None:
     """Tear down the docker dev environment."""
-    session.run(
-        "docker-compose",
-        "-f",
-        COMPOSE_FILE,
-        "-f",
-        INTEGRATION_COMPOSE_FILE,
-        "down",
-        "--remove-orphans",
-        external=True,
-    )
+    session.run(*COMPOSE_DOWN, external=True)
     print("Teardown complete")
