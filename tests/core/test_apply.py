781835e121210ba7a770d7f2f8fb5f0f9a161b4c
SteveDMurphy <steven.d.murphy@gmail.com>
2022-05-02 22:52:03 -0400

    tests and documentation

--- tests/core/test_apply.py
+++ tests/core/test_apply.py
@@ -2,7 +2,7 @@
 import pytest
 
 import fideslang as models
-from fidesctl.core.apply import sort_create_update
+from fidesctl.core.apply import sort_create_update, validate_dataset_usage
 
 
 # Helpers
@@ -17,6 +17,26 @@ def server_resource_list():
 @pytest.fixture()
 def server_resource_key_pairs():
     yield {"testKey": 1, "anotherTestKey": 2}
+
+
+@pytest.fixture()
+def system_with_dataset_reference():
+    yield [
+        models.System(
+            organization_fides_key=1,
+            fides_key="test_system",
+            system_type="test",
+            privacy_declarations=[
+                models.PrivacyDeclaration(
+                    name="test_privacy_declaration",
+                    data_categories=[],
+                    data_use="test_data_use",
+                    data_subjects=[],
+                    dataset_references=["test_dataset"],
+                ),
+            ],
+        ),
+    ]
 
 
 # Unit
@@ -71,3 +91,84 @@ def test_sort_create_update_update():
     ) = sort_create_update(manifest_resource_list, server_resource_list)
     assert [] == create_result
     assert expected_update_result == update_result
+
+
+@pytest.mark.parametrize(
+    "taxonomies, expected_length",
+    [
+        (
+            {
+                "dataset": [
+                    models.Dataset(
+                        organization_fides_key=1,
+                        fides_key="test_dataset",
+                        collections=[],
+                    ),
+                ],
+            },
+            0,
+        ),
+        (
+            {
+                "dataset": [
+                    models.Dataset(
+                        organization_fides_key=1,
+                        fides_key="test_dataset_unused",
+                        collections=[],
+                    ),
+                ],
+            },
+            1,
+        ),
+        (
+            {
+                "dataset": [
+                    models.Dataset(
+                        organization_fides_key=1,
+                        fides_key="test_dataset",
+                        collections=[],
+                    ),
+                    models.Dataset(
+                        organization_fides_key=1,
+                        fides_key="test_dataset_unused",
+                        collections=[],
+                    ),
+                ],
+            },
+            1,
+        ),
+        (
+            {
+                "dataset": [
+                    models.Dataset(
+                        organization_fides_key=1,
+                        fides_key="test_dataset",
+                        collections=[],
+                    ),
+                    models.Dataset(
+                        organization_fides_key=1,
+                        fides_key="test_dataset_unused",
+                        collections=[],
+                    ),
+                    models.Dataset(
+                        organization_fides_key=1,
+                        fides_key="test_dataset_also_unused",
+                        collections=[],
+                    ),
+                ],
+            },
+            2,
+        ),
+    ],
+)
+@pytest.mark.unit
+def test_validate_dataset_usage(
+    taxonomies: dict, expected_length: int, system_with_dataset_reference: list
+):
+    """
+    Tests different scenarios for referenced datasets
+    """
+    taxonomies["system"] = system_with_dataset_reference
+    taxonomy = models.Taxonomy.parse_obj(taxonomies)
+    missing_datasets = validate_dataset_usage(taxonomy)
+    assert len(missing_datasets) == expected_length
