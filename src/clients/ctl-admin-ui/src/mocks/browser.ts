8816d08d4d4b8b7938534344060ed7845e4a4ca1
Elliot Bonneville <elliot.bonneville@gmail.com>
2022-05-10 10:51:47 -0400

    Copy over Admin UI base

---
+++ src/clients/ctl-admin-ui/src/mocks/browser.ts
@@ -0,0 +1,5 @@
+import { setupWorker } from 'msw';
+import { handlers } from './handlers';
+
+// eslint-disable-next-line import/prefer-default-export
+export const worker = setupWorker(...handlers);
