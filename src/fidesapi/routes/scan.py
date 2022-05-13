00cbff75e4b9830d88ff9ecc04aa1aa88a359a1e
SteveDMurphy <steven.d.murphy@gmail.com>
2022-05-13 14:20:13 -0400

    [skip ci] iterative updates tested with #640

--- src/fidesapi/routes/scan.py
+++ src/fidesapi/routes/scan.py
@@ -7,7 +7,10 @@ from typing import Dict
 from fastapi import APIRouter, Response, status
 from pydantic import BaseModel
 
+from fidesapi.routes.crud import get_resource
+from fidesapi.sql_models import sql_model_map
 from fidesapi.utils.helpers import unobscure
+from fidesctl.core.system import filter_aws_systems, generate_aws_systems
 
 
 class ScanTypeEnum(str, Enum):
@@ -53,9 +56,34 @@ async def generate_scan(scan_resource: Scan, response: Response) -> Dict:
     Kicks off a generate command for fidesctl.
 
     Starting off, this will only take 'generate system aws'
+
+    Debate is still up for if one endpoint makes sense or many, might be more
+    difficult to document etc. with all of the config options.
+
+    Currently follows the same logic as `generate_system_aws` in `system.py`
     """
     response.status_code = status.HTTP_201_CREATED
-    scan_resource.scan_config["aws_secret_key"] = unobscure(
-        scan_resource.scan_config["aws_secret_key"]
-    ).decode()
-    return scan_resource.dict()
+    aws_config = {
+        "region_name": scan_resource.scan_config["region_name"],
+        "aws_access_key_id": unobscure(
+            scan_resource.scan_config["aws_access_key_id"]
+        ).decode(),
+        "aws_secret_access_key": unobscure(
+            scan_resource.scan_config["aws_secret_access_key"]
+        ).decode(),
+    }
+
+    organization_key = scan_resource.organization_key
+
+    aws_systems = generate_aws_systems(
+        organization_key=organization_key, aws_config=aws_config
+    )
+
+    organization = await get_resource(sql_model_map["organization"], organization_key)
+
+    filtered_aws_systems = filter_aws_systems(
+        systems=aws_systems, organization=organization
+    )
+
+    output_list_of_dicts = [i.dict(exclude_none=True) for i in filtered_aws_systems]
+    return {"systems": output_list_of_dicts}
