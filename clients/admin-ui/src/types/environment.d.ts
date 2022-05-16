ebc270d8442b4d9cc32e84c83ba062234b17d86e
Elliot Bonneville <elliot.bonneville@gmail.com>
2022-05-16 16:01:18 -0400

    Copy over Admin UI base for ctl-admin-ui (#624)
    * Copy over Admin UI base
    
    * Update from fidesops -> fidesctl
    
    Co-authored-by: Thomas <thomas.lapiana+github@pm.me>
    
    * Rename to admin-ui
    
    * Update references to fidesops -> fidesctl
    
    * Update the logo to fidesctl
    
    * Update changelog
    
    * Move from src/clients -> clients
    
    Co-authored-by: Allison King <allisonjuliaking@gmail.com>
    Co-authored-by: Thomas <thomas.lapiana+github@pm.me>

---
+++ clients/admin-ui/src/types/environment.d.ts
@@ -0,0 +1,10 @@
+declare global {
+  namespace NodeJS {
+    interface ProcessEnv {
+      NEXT_PUBLIC_AUTH_ENDPOINT: string;
+      NODE_ENV: 'development' | 'production';
+      PORT?: string;
+      PWD: string;
+    }
+  }
+}
