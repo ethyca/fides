41e40c4d7038a5a974c818b54efb35345360df05
Allison King <allisonjuliaking@gmail.com>
2022-05-18 14:30:30 -0400

    Remove nextjs dynamic features

--- clients/admin-ui/src/pages/index.tsx
+++ clients/admin-ui/src/pages/index.tsx
@@ -1,12 +1,9 @@
 import { Box, Button, Flex, Heading } from '@fidesui/react';
 import type { NextPage } from 'next';
-import { getSession } from 'next-auth/react';
 import Head from 'next/head';
 import React from 'react';
-import { wrapper } from '../app/store';
 import Header from '../features/common/Header';
 import { ArrowDownLineIcon } from '../features/common/Icon';
-import { assignToken } from '../features/user/user.slice';
 
 
 
@@ -30,7 +27,7 @@ const Home: NextPage<{ session: { username: string } }> = ({ session }) => (
         borderColor="gray.100"
       >
         <Button variant="ghost" mr={4} colorScheme="complimentary">
-          Subject Requests
+          Subject Requests!!
         </Button>
         <Button variant="ghost" disabled mr={4}>
           Datastore Connections
@@ -51,24 +48,24 @@ const Home: NextPage<{ session: { username: string } }> = ({ session }) => (
   </div>
 );
 
-export const getServerSideProps = wrapper.getServerSideProps(
-  (store) => async (context) => {
-    const session = await getSession(context);
-    if (session && typeof session.accessToken !== 'undefined') {
-      await store.dispatch(assignToken(session.accessToken));
-      return { props: { session } };
-    }
+// TODO: replace auth https://github.com/ethyca/fidesui/issues/2
+// export const getServerSideProps = wrapper.getServerSideProps(
+//   (store) => async (context) => {
+//     const session = await getSession(context);
+//     if (session && typeof session.accessToken !== 'undefined') {
+//       await store.dispatch(assignToken(session.accessToken));
+//       return { props: { session } };
+//     }
 
-    // TODO: once user login is implemented in fides-ctl, re-enable this to make
-    // sure the home page is login-protected
-    return {
-      props: { session }
-      //   redirect: {
-      //     destination: '/login',
-      //     permanent: false,
-      //   },
-    };
-  }
-);
+//     // TODO: once user login is implemented in fides-ctl, re-enable this to make
+//     // sure the home page is login-protected
+//     return {
+//       //   redirect: {
+//       //     destination: '/login',
+//       //     permanent: false,
+//       //   },
+//     };
+//   }
+// );
 
 export default Home;
