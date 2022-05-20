daf3e067c5adef1055ecdfba3669c84523090944
Allison King <allisonjuliaking@gmail.com>
2022-05-20 13:56:58 -0400

    Rename folder to match pluralization

---
+++ clients/admin-ui/src/pages/dataset/index.tsx
@@ -0,0 +1,31 @@
+import { Heading, Spinner } from "@fidesui/react";
+import type { NextPage } from "next";
+import React from "react";
+
+import Layout from "@/features/common/Layout";
+import { useGetAllDatasetsQuery } from "@/features/dataset/dataset.slice";
+import DatasetsTable from "@/features/dataset/DatasetTable";
+
+const useDatasetsTable = () => {
+  const { data, isLoading } = useGetAllDatasetsQuery();
+
+  return {
+    isLoading,
+    datasets: data,
+  };
+};
+
+const DataSets: NextPage = () => {
+  const { isLoading, datasets } = useDatasetsTable();
+
+  return (
+    <Layout title="Datasets">
+      <Heading mb={8} fontSize="2xl" fontWeight="semibold">
+        Datasets
+      </Heading>
+      {isLoading ? <Spinner /> : <DatasetsTable datasets={datasets} />}
+    </Layout>
+  );
+};
+
+export default DataSets;
