4dc32f679aa23bf7719604b08f5de8c7a7fb8954
Thomas <thomas.lapiana+github@pm.me>
2022-05-26 15:43:39 -0500

    update testing organization

--- tests/cli/test_cli_utils.py
+++ tests/cli/test_cli_utils.py
@@ -57,77 +57,80 @@ def test_check_server_version_comparisons(
         assert expected_output in captured.out
 
 
-def test_handle_database_credentials_options_neither_raises(
-    test_config: FidesctlConfig,
-) -> None:
-    "Check for an exception if neither option is supplied."
-    with pytest.raises(click.UsageError):
-        input_connection_string = ""
-        input_credentials_id = ""
-        utils.handle_database_credentials_options(
-            fides_config=test_config,
-            connection_string=input_connection_string,
-            credentials_id=input_credentials_id,
-        )
+@pytest.mark.unit
+class TestHandleDatabaseCredentialsOptions:
+    def test_neither_option_supplied_raises(
+        self,
+        test_config: FidesctlConfig,
+    ) -> None:
+        "Check for an exception if neither option is supplied."
+        with pytest.raises(click.UsageError):
+            input_connection_string = ""
+            input_credentials_id = ""
+            utils.handle_database_credentials_options(
+                fides_config=test_config,
+                connection_string=input_connection_string,
+                credentials_id=input_credentials_id,
+            )
 
+    def test_both_options_supplied_raises(
+        self,
+        test_config: FidesctlConfig,
+    ) -> None:
+        "Check for an exception if both options are supplied."
+        with pytest.raises(click.UsageError):
+            input_connection_string = "my_connection_string"
+            input_credentials_id = "postgres_1"
+            utils.handle_database_credentials_options(
+                fides_config=test_config,
+                connection_string=input_connection_string,
+                credentials_id=input_credentials_id,
+            )
 
-def test_handle_database_credentials_options_both_raises(
-    test_config: FidesctlConfig,
-) -> None:
-    "Check for an exception if both options are supplied."
-    with pytest.raises(click.UsageError):
+    def test_config_does_not_exist_raises(
+        self,
+        test_config: FidesctlConfig,
+    ) -> None:
+        "Check for an exception if credentials dont exist"
+        with pytest.raises(click.UsageError):
+            input_connection_string = ""
+            input_credentials_id = "UNKNOWN"
+            utils.handle_database_credentials_options(
+                fides_config=test_config,
+                connection_string=input_connection_string,
+                credentials_id=input_credentials_id,
+            )
+
+    def test_returns_input_connection_string(
+        self,
+        test_config: FidesctlConfig,
+    ) -> None:
+        "Checks if expected connection string is returned from input"
         input_connection_string = "my_connection_string"
-        input_credentials_id = "postgres_1"
-        utils.handle_database_credentials_options(
+        input_credentials_id = ""
+        connection_string = utils.handle_database_credentials_options(
             fides_config=test_config,
             connection_string=input_connection_string,
             credentials_id=input_credentials_id,
         )
+        assert connection_string == input_connection_string
 
-
-def test_handle_database_credentials_options_config_dne_raises(
-    test_config: FidesctlConfig,
-) -> None:
-    "Check for an exception if credentials dont exist"
-    with pytest.raises(click.UsageError):
+    def test_returns_config_connection_string(
+        self,
+        test_config: FidesctlConfig,
+    ) -> None:
+        "Checks if expected connection string is returned from config"
         input_connection_string = ""
-        input_credentials_id = "UNKNOWN"
-        utils.handle_database_credentials_options(
+        input_credentials_id = "postgres_1"
+        connection_string = utils.handle_database_credentials_options(
             fides_config=test_config,
             connection_string=input_connection_string,
             credentials_id=input_credentials_id,
         )
-
-
-def test_handle_database_credentials_options_returns_input_connection_string(
-    test_config: FidesctlConfig,
-) -> None:
-    "Checks if expected connection string is returned from input"
-    input_connection_string = "my_connection_string"
-    input_credentials_id = ""
-    connection_string = utils.handle_database_credentials_options(
-        fides_config=test_config,
-        connection_string=input_connection_string,
-        credentials_id=input_credentials_id,
-    )
-    assert connection_string == input_connection_string
-
-
-def test_handle_database_credentials_options_returns_config_connection_string(
-    test_config: FidesctlConfig,
-) -> None:
-    "Checks if expected connection string is returned from config"
-    input_connection_string = ""
-    input_credentials_id = "postgres_1"
-    connection_string = utils.handle_database_credentials_options(
-        fides_config=test_config,
-        connection_string=input_connection_string,
-        credentials_id=input_credentials_id,
-    )
-    assert (
-        connection_string
-        == "postgresql+psycopg2://postgres:fidesctl@fidesctl-db:5432/fidesctl_test"
-    )
+        assert (
+            connection_string
+            == "postgresql+psycopg2://postgres:fidesctl@fidesctl-db:5432/fidesctl_test"
+        )
 
 
 def test_handle_okta_credentials_options_both_raises(
@@ -146,85 +149,108 @@ def test_handle_okta_credentials_options_both_raises(
         )
 
 
-def test_handle_okta_credentials_options_config_dne_raises(
-    test_config: FidesctlConfig,
-) -> None:
-    "Check for an exception if credentials dont exist"
-    with pytest.raises(click.UsageError):
+@pytest.mark.unit
+class TestHandleOktaCredentialsOptions:
+    def test_config_dne_raises(
+        self,
+        test_config: FidesctlConfig,
+    ) -> None:
+        "Check for an exception if credentials dont exist"
+        with pytest.raises(click.UsageError):
+            input_org_url = ""
+            input_token = ""
+            input_credentials_id = "UNKNOWN"
+            utils.handle_okta_credentials_options(
+                fides_config=test_config,
+                token=input_token,
+                org_url=input_org_url,
+                credentials_id=input_credentials_id,
+            )
+
+    def test_returns_config_dict(
+        self,
+        test_config: FidesctlConfig,
+    ) -> None:
+        "Check for an exception if credentials dont exist"
         input_org_url = ""
         input_token = ""
-        input_credentials_id = "UNKNOWN"
-        utils.handle_okta_credentials_options(
+        input_credentials_id = "okta_1"
+        okta_config = utils.handle_okta_credentials_options(
             fides_config=test_config,
             token=input_token,
             org_url=input_org_url,
             credentials_id=input_credentials_id,
         )
+        assert okta_config == {
+            "orgUrl": "https://dev-78908748.okta.com",
+            "token": "redacted_override_in_tests",
+        }
 
-
-def test_handle_okta_credentials_options_returns_config_dict(
-    test_config: FidesctlConfig,
-) -> None:
-    "Check for an exception if credentials dont exist"
-    input_org_url = ""
-    input_token = ""
-    input_credentials_id = "okta_1"
-    okta_config = utils.handle_okta_credentials_options(
-        fides_config=test_config,
-        token=input_token,
-        org_url=input_org_url,
-        credentials_id=input_credentials_id,
-    )
-    assert okta_config == {
-        "orgUrl": "https://dev-78908748.okta.com",
-        "token": "redacted_override_in_tests",
-    }
-
-
-def test_handle_okta_credentials_options_returns_input_dict(
-    test_config: FidesctlConfig,
-) -> None:
-    "Check for an exception if credentials dont exist"
-    input_org_url = "hello.com"
-    input_token = "abcd12345"
-    input_credentials_id = ""
-    okta_config = utils.handle_okta_credentials_options(
-        fides_config=test_config,
-        token=input_token,
-        org_url=input_org_url,
-        credentials_id=input_credentials_id,
-    )
-    assert okta_config == {"orgUrl": input_org_url, "token": input_token}
-
-
-def test_handle_aws_credentials_options_both_raises(
-    test_config: FidesctlConfig,
-) -> None:
-    "Check for an exception if both credentials options are supplied."
-    with pytest.raises(click.UsageError):
-        input_access_key = "access_key"
-        input_access_key_id = "access_key_id"
-        input_region = "us-east-1"
-        input_credentials_id = "aws_1"
-        utils.handle_aws_credentials_options(
+    def test_returns_input_dict(
+        self,
+        test_config: FidesctlConfig,
+    ) -> None:
+        "Check for an exception if credentials dont exist"
+        input_org_url = "hello.com"
+        input_token = "abcd12345"
+        input_credentials_id = ""
+        okta_config = utils.handle_okta_credentials_options(
             fides_config=test_config,
-            access_key_id=input_access_key_id,
-            secret_access_key=input_access_key,
-            region=input_region,
+            token=input_token,
+            org_url=input_org_url,
             credentials_id=input_credentials_id,
         )
+        assert okta_config == {"orgUrl": input_org_url, "token": input_token}
 
 
-def test_handle_aws_credentials_options_config_dne_raises(
-    test_config: FidesctlConfig,
-) -> None:
-    "Check for an exception if credentials dont exist"
-    with pytest.raises(click.UsageError):
+@pytest.mark.unit
+class TestHandleAWSCredentialsOptions:
+    def test_both_raises(
+        self,
+        test_config: FidesctlConfig,
+    ) -> None:
+        "Check for an exception if both credentials options are supplied."
+        with pytest.raises(click.UsageError):
+            input_access_key = "access_key"
+            input_access_key_id = "access_key_id"
+            input_region = "us-east-1"
+            input_credentials_id = "aws_1"
+            utils.handle_aws_credentials_options(
+                fides_config=test_config,
+                access_key_id=input_access_key_id,
+                secret_access_key=input_access_key,
+                region=input_region,
+                credentials_id=input_credentials_id,
+            )
+
+    def test_config_dne_raises(
+        self,
+        test_config: FidesctlConfig,
+    ) -> None:
+        "Check for an exception if credentials dont exist"
+        with pytest.raises(click.UsageError):
+            input_access_key = ""
+            input_access_key_id = ""
+            input_region = ""
+            input_credentials_id = "UNKNOWN"
+            utils.handle_aws_credentials_options(
+                fides_config=test_config,
+                access_key_id=input_access_key_id,
+                secret_access_key=input_access_key,
+                region=input_region,
+                credentials_id=input_credentials_id,
+            )
+
+    def test_returns_config_dict(
+        self,
+        test_config: FidesctlConfig,
+    ) -> None:
+        "Check for an exception if credentials dont exist"
         input_access_key = ""
         input_access_key_id = ""
         input_region = ""
-        input_credentials_id = "UNKNOWN"
-        utils.handle_aws_credentials_options(
+        input_credentials_id = "aws_1"
+        aws_config = utils.handle_aws_credentials_options(
             fides_config=test_config,
             access_key_id=input_access_key_id,
             secret_access_key=input_access_key,
@@ -232,47 +258,30 @@ def test_handle_aws_credentials_options_config_dne_raises(
             credentials_id=input_credentials_id,
         )
 
+        assert aws_config == {
+            "aws_access_key_id": "redacted_id_override_in_tests",
+            "aws_secret_access_key": "redacted_override_in_tests",
+            "region_name": "us-east-1",
+        }
 
-def test_handle_aws_credentials_options_returns_config_dict(
-    test_config: FidesctlConfig,
-) -> None:
-    "Check for an exception if credentials dont exist"
-    input_access_key = ""
-    input_access_key_id = ""
-    input_region = ""
-    input_credentials_id = "aws_1"
-    aws_config = utils.handle_aws_credentials_options(
-        fides_config=test_config,
-        access_key_id=input_access_key_id,
-        secret_access_key=input_access_key,
-        region=input_region,
-        credentials_id=input_credentials_id,
-    )
-
-    assert aws_config == {
-        "aws_access_key_id": "redacted_id_override_in_tests",
-        "aws_secret_access_key": "redacted_override_in_tests",
-        "region_name": "us-east-1",
-    }
-
-
-def test_handle_aws_credentials_options_returns_input_dict(
-    test_config: FidesctlConfig,
-) -> None:
-    "Check for an exception if credentials dont exist"
-    input_access_key = "access_key"
-    input_access_key_id = "access_key_id"
-    input_region = "us-east-1"
-    input_credentials_id = ""
-    aws_config = utils.handle_aws_credentials_options(
-        fides_config=test_config,
-        access_key_id=input_access_key_id,
-        secret_access_key=input_access_key,
-        region=input_region,
-        credentials_id=input_credentials_id,
-    )
-    assert aws_config == {
-        "aws_access_key_id": input_access_key_id,
-        "aws_secret_access_key": input_access_key,
-        "region_name": input_region,
-    }
+    def test_returns_input_dict(
+        self,
+        test_config: FidesctlConfig,
+    ) -> None:
+        "Check for an exception if credentials dont exist"
+        input_access_key = "access_key"
+        input_access_key_id = "access_key_id"
+        input_region = "us-east-1"
+        input_credentials_id = ""
+        aws_config = utils.handle_aws_credentials_options(
+            fides_config=test_config,
+            access_key_id=input_access_key_id,
+            secret_access_key=input_access_key,
+            region=input_region,
+            credentials_id=input_credentials_id,
+        )
+        assert aws_config == {
+            "aws_access_key_id": input_access_key_id,
+            "aws_secret_access_key": input_access_key,
+            "region_name": input_region,
+        }
--- tests/core/config/test_credentials_settings.py
+++ tests/core/config/test_credentials_settings.py
@@ -8,85 +8,82 @@ import pytest
 from fidesctl.core.config.credentials_settings import merge_credentials_environment
 
 
-@patch.dict(
-    os.environ,
-    {
-        "FIDESCTL__CREDENTIALS__POSTGRES_1__CONNECTION_STRING": "postgresql+psycopg2://fidesctl:env_variable.com:5439/fidesctl_test",
-        "FIDESCTL__CREDENTIALS__AWS_ACCOUNT_1__REGION": "us-east-1",
-        "FIDESCTL__CREDENTIALS__AWS_ACCOUNT_1__ACCESS_KEY_ID": "ACCESS_KEY_ID_1",
-        "FIDESCTL__CREDENTIALS__AWS_ACCOUNT_1__ACCESS_KEY": "ACCESS_KEY_1",
-    },
-    clear=True,
-)
 @pytest.mark.unit
-def test_merge_credentials_environment() -> None:
-    "Test merging environment variables into a settings dict."
-    credentials_dict: Dict = dict()
-    merge_credentials_environment(credentials_dict)
-
-    assert credentials_dict == {
-        "postgres_1": {
-            "connection_string": "postgresql+psycopg2://fidesctl:env_variable.com:5439/fidesctl_test"
+class TestMergeCredentialsEnvironment:
+    @patch.dict(
+        os.environ,
+        {
+            "FIDESCTL__CREDENTIALS__POSTGRES_1__CONNECTION_STRING": "postgresql+psycopg2://fidesctl:env_variable.com:5439/fidesctl_test",
+            "FIDESCTL__CREDENTIALS__AWS_ACCOUNT_1__REGION": "us-east-1",
+            "FIDESCTL__CREDENTIALS__AWS_ACCOUNT_1__ACCESS_KEY_ID": "ACCESS_KEY_ID_1",
+            "FIDESCTL__CREDENTIALS__AWS_ACCOUNT_1__ACCESS_KEY": "ACCESS_KEY_1",
         },
-        "aws_account_1": {
-            "region": "us-east-1",
-            "access_key_id": "ACCESS_KEY_ID_1",
-            "access_key": "ACCESS_KEY_1",
-        },
-    }
+        clear=True,
+    )
+    def test_merge_credentials(self) -> None:
+        "Test merging environment variables into a settings dict."
+        credentials_dict: Dict = dict()
+        merge_credentials_environment(credentials_dict)
 
-
-@patch.dict(
-    os.environ,
-    {
-        "FIDESCTL__CREDENTIALS__POSTGRES_1__CONNECTION_STRING": "postgresql+psycopg2://fidesctl:env_variable.com:5439/fidesctl_test",
-        "FIDESCTL__CREDENTIALS__AWS_ACCOUNT_1__ACCESS_KEY_ID": "ACCESS_KEY_ID_1",
-        "FIDESCTL__CREDENTIALS__AWS_ACCOUNT_1__ACCESS_KEY": "ACCESS_KEY_1",
-    },
-    clear=True,
-)
-@pytest.mark.unit
-def test_merge_credentials_environment_mixed_configs() -> None:
-    "Test that merging environment variables works with mixed configs"
-    credentials_dict = {"aws_account_1": {"region": "us-east-1"}}
-    merge_credentials_environment(credentials_dict)
-
-    assert credentials_dict == {
-        "postgres_1": {
-            "connection_string": "postgresql+psycopg2://fidesctl:env_variable.com:5439/fidesctl_test"
-        },
-        "aws_account_1": {
-            "region": "us-east-1",
-            "access_key_id": "ACCESS_KEY_ID_1",
-            "access_key": "ACCESS_KEY_1",
-        },
-    }
-
-
-@patch.dict(
-    os.environ,
-    {
-        "FIDESCTL__CREDENTIALS__AWS_ACCOUNT_1__ACCESS_KEY_ID": "ACCESS_KEY_ID_OVERRIDE",
-        "FIDESCTL__CREDENTIALS__AWS_ACCOUNT_1__ACCESS_KEY": "ACCESS_KEY_OVERRIDE",
-    },
-    clear=True,
-)
-@pytest.mark.unit
-def test_merge_credentials_environment_environment_override() -> None:
-    "Test merging environment variable works as an override."
-    credentials_dict = {
-        "aws_account_1": {
-            "region": "us-east-1",
-            "access_key_id": "ACCESS_KEY_ID_1",
-            "access_key": "ACCESS_KEY_1",
+        assert credentials_dict == {
+            "postgres_1": {
+                "connection_string": "postgresql+psycopg2://fidesctl:env_variable.com:5439/fidesctl_test"
+            },
+            "aws_account_1": {
+                "region": "us-east-1",
+                "access_key_id": "ACCESS_KEY_ID_1",
+                "access_key": "ACCESS_KEY_1",
+            },
         }
-    }
-    merge_credentials_environment(credentials_dict)
 
-    assert credentials_dict == {
-        "aws_account_1": {
-            "region": "us-east-1",
-            "access_key_id": "ACCESS_KEY_ID_OVERRIDE",
-            "access_key": "ACCESS_KEY_OVERRIDE",
+    @patch.dict(
+        os.environ,
+        {
+            "FIDESCTL__CREDENTIALS__POSTGRES_1__CONNECTION_STRING": "postgresql+psycopg2://fidesctl:env_variable.com:5439/fidesctl_test",
+            "FIDESCTL__CREDENTIALS__AWS_ACCOUNT_1__ACCESS_KEY_ID": "ACCESS_KEY_ID_1",
+            "FIDESCTL__CREDENTIALS__AWS_ACCOUNT_1__ACCESS_KEY": "ACCESS_KEY_1",
+        },
+        clear=True,
+    )
+    def test_mixed_configs(self) -> None:
+        "Test that merging environment variables works with mixed configs"
+        credentials_dict = {"aws_account_1": {"region": "us-east-1"}}
+        merge_credentials_environment(credentials_dict)
+
+        assert credentials_dict == {
+            "postgres_1": {
+                "connection_string": "postgresql+psycopg2://fidesctl:env_variable.com:5439/fidesctl_test"
+            },
+            "aws_account_1": {
+                "region": "us-east-1",
+                "access_key_id": "ACCESS_KEY_ID_1",
+                "access_key": "ACCESS_KEY_1",
+            },
         }
-    }
+
+    @patch.dict(
+        os.environ,
+        {
+            "FIDESCTL__CREDENTIALS__AWS_ACCOUNT_1__ACCESS_KEY_ID": "ACCESS_KEY_ID_OVERRIDE",
+            "FIDESCTL__CREDENTIALS__AWS_ACCOUNT_1__ACCESS_KEY": "ACCESS_KEY_OVERRIDE",
+        },
+        clear=True,
+    )
+    def test_environment_override(self) -> None:
+        "Test merging environment variable works as an override."
+        credentials_dict = {
+            "aws_account_1": {
+                "region": "us-east-1",
+                "access_key_id": "ACCESS_KEY_ID_1",
+                "access_key": "ACCESS_KEY_1",
+            }
+        }
+        merge_credentials_environment(credentials_dict)
+
+        assert credentials_dict == {
+            "aws_account_1": {
+                "region": "us-east-1",
+                "access_key_id": "ACCESS_KEY_ID_OVERRIDE",
+                "access_key": "ACCESS_KEY_OVERRIDE",
+            }
+        }
