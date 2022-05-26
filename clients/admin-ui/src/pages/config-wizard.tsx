7ea95b502cd0236838a313ced8badb35d3360003
Laura Smith <laurasmith@Lauras-MacBook-Pro.local>
2022-05-26 10:44:54 -0500

    Initial

--- clients/admin-ui/src/pages/config-wizard.tsx
+++ clients/admin-ui/src/pages/config-wizard.tsx
@@ -1,7 +1,15 @@
 import type { NextPage } from "next";
-
 import Setup from "./setup";
+// import ConfigWizardModal from "./config-wizard-modal"
 
-const ConfigWizard: NextPage = () => <Setup />;
+const ConfigWizard: NextPage = () => {
+  return (
+    // step 0
+    <Setup />
+
+    // wizard initiated
+    // <ConfigWizardModal />
+  );
+};
 
 export default ConfigWizard;
