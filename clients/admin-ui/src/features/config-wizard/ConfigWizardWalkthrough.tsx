d960a36003c067b644f0c46dc557a59ccabdec94
Laura Smith <laurasmith@Lauras-MacBook-Pro.local>
2022-05-30 12:55:34 -0500

    Adjust

--- clients/admin-ui/src/features/config-wizard/ConfigWizardWalkthrough.tsx
+++ clients/admin-ui/src/features/config-wizard/ConfigWizardWalkthrough.tsx
@@ -26,12 +26,18 @@ const ConfigWizardWalkthrough = () => {
           x Cancel setup
         </Button>
       </Box>
-      <Divider orientation="horizontal" />
-      <Stack direction={"row"} spacing="24px">
-        <Box>
-          <Stepper activeStep={1} />
-        </Box>
-        <OrganizationInfoForm handleChangeStep={handleChangeStep} />
+      <Stack>
+        <Divider orientation="horizontal" />
+        <Stack direction={"row"} spacing="24px">
+          <Box>
+            <Stepper activeStep={1} />
+          </Box>
+          <OrganizationInfoForm handleChangeStep={handleChangeStep} />
+        </Stack>
+      </Stack>
+      <Stack>
+        Right hand side
+        {/* give this stack 50% width to split page, this stack holds tooltips */}
       </Stack>
     </>
   );
