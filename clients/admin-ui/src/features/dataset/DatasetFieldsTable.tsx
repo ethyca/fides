5093dc878ae692e5b103f1e8ff77b72c13ec2957
Allison King <allisonjuliaking@gmail.com>
2022-05-31 16:21:38 -0400

    Make table columns dynamic

--- clients/admin-ui/src/features/dataset/DatasetFieldsTable.tsx
+++ clients/admin-ui/src/features/dataset/DatasetFieldsTable.tsx
@@ -1,48 +1,35 @@
 import { Table, Tbody, Td, Th, Thead, Tr } from "@fidesui/react";
-import { useState } from "react";
 
-import { DatasetField } from "./types";
+import { ColumnMetadata, DatasetField } from "./types";
 
-interface ColumnMetadata {
-  name: string;
-  attribute: keyof DatasetField;
-}
-const ALL_COLUMNS: ColumnMetadata[] = [
-  { name: "Field Name", attribute: "name" },
-  { name: "Description", attribute: "description" },
-  { name: "Personal Data Categories", attribute: "data_categories" },
-  { name: "Identifiability", attribute: "data_qualifier" },
-];
 interface Props {
   fields: DatasetField[];
+  columns: ColumnMetadata[];
 }
 
-const DatasetFieldsTable = ({ fields }: Props) => {
-  const [columns, setColumns] = useState<ColumnMetadata[]>(ALL_COLUMNS);
-  return (
-    <Table size="sm">
-      <Thead>
-        <Tr>
+const DatasetFieldsTable = ({ fields, columns }: Props) => (
+  <Table size="sm">
+    <Thead>
+      <Tr>
+        {columns.map((c) => (
+          <Th key={c.name} pl={0}>
+            {c.name}
+          </Th>
+        ))}
+      </Tr>
+    </Thead>
+    <Tbody>
+      {fields.map((field) => (
+        <Tr key={field.name}>
           {columns.map((c) => (
-            <Th key={c.name} pl={0}>
-              {c.name}
-            </Th>
+            <Td key={`${field.name}-${field[c.attribute]}`} pl={0}>
+              {field[c.attribute]}
+            </Td>
           ))}
         </Tr>
-      </Thead>
-      <Tbody>
-        {fields.map((field) => (
-          <Tr key={field.name}>
-            {columns.map((c) => (
-              <Td key={`${field.name}-${field[c.attribute]}`} pl={0}>
-                {field[c.attribute]}
-              </Td>
-            ))}
-          </Tr>
-        ))}
-      </Tbody>
-    </Table>
-  );
-};
+      ))}
+    </Tbody>
+  </Table>
+);
 
 export default DatasetFieldsTable;
