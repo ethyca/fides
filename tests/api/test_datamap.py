e502c0bf61b884b75365d49eec19fc8ba323ba57
SteveDMurphy <steven.d.murphy@gmail.com>
2022-06-02 23:29:58 -0400

    test endpoint

---
+++ tests/api/test_datamap.py
@@ -0,0 +1,39 @@
+# pylint: disable=missing-docstring, redefined-outer-name
+from typing import Generator
+
+import pytest
+from starlette.testclient import TestClient
+
+from fidesapi import main
+from fidesapi.routes.util import API_PREFIX
+from fidesctl.core.config import FidesctlConfig
+
+
+@pytest.fixture()
+def test_client() -> Generator:
+    """Starlette test client fixture. Easier to use mocks with when testing out API calls"""
+    with TestClient(main.app) as test_client:
+        yield test_client
+
+
+@pytest.mark.integration
+@pytest.mark.parametrize(
+    "organization_fides_key, expected_status_code",
+    [
+        ("fake_organization", 404),
+        ("default_organization", 200),
+    ],
+)
+def test_datamap(
+    test_config: FidesctlConfig,
+    organization_fides_key: str,
+    expected_status_code: int,
+    test_client: TestClient,
+) -> None:
+    # test_client.wait_startup()
+    response = test_client.get(
+        test_config.cli.server_url + API_PREFIX + "/datamap/" + organization_fides_key,
+        headers=test_config.user.request_headers,
+    )
+    # test_client.wait_shutdown()
+    assert response.status_code == expected_status_code
