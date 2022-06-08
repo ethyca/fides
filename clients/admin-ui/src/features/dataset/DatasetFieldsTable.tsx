05b40f72e9dcda7b85b0bae0a4a0436a0cd47529
Allison King <allisonjuliaking@gmail.com>
2022-06-08 14:26:06 -0400

    Create form for editing dataset fields (#718)
    * Scaffold EditFieldDrawer
    
    * WIP add formik form
    
    * Add close button
    
    * Add formik and chakra form components
    
    * WIP call PUT via redux
    
    * wip massage data to send to backend
    
    * Update dataset fields properly
    
    * Call backend
    
    * Close drawer on save
    
    * Fix bug that reset active collection
    
    * Fix identifiability
    
    * Clean up and comment
    
    * Handle null fields better
    
    * Prefer ternary

--- clients/admin-ui/src/features/dataset/DatasetFieldsTable.tsx
+++ clients/admin-ui/src/features/dataset/DatasetFieldsTable.tsx
@@ -1,5 +1,9 @@
-import { Table, Tbody, Td, Th, Thead, Tr } from "@fidesui/react";
+import { Box, Table, Tbody, Td, Th, Thead, Tr } from "@fidesui/react";
+import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
 
+import { selectActiveFieldIndex, setActiveFieldIndex } from "./dataset.slice";
+import EditFieldDrawer from "./EditFieldDrawer";
 import { ColumnMetadata, DatasetField } from "./types";
 
 interface Props {
@@ -7,29 +11,68 @@ interface Props {
   columns: ColumnMetadata[];
 }
 
-const DatasetFieldsTable = ({ fields, columns }: Props) => (
-  <Table size="sm">
-    <Thead>
-      <Tr>
-        {columns.map((c) => (
-          <Th key={c.name} pl={0}>
-            {c.name}
-          </Th>
-        ))}
-      </Tr>
-    </Thead>
-    <Tbody>
-      {fields.map((field) => (
-        <Tr key={field.name}>
-          {columns.map((c) => (
-            <Td key={`${field.name}-${field[c.attribute]}`} pl={0}>
-              {field[c.attribute]}
-            </Td>
+const DatasetFieldsTable = ({ fields, columns }: Props) => {
+  const dispatch = useDispatch();
+  const [editDrawerIsOpen, setEditDrawerIsOpen] = useState(false);
+  const activeFieldIndex = useSelector(selectActiveFieldIndex);
+
+  const handleClose = () => {
+    setEditDrawerIsOpen(false);
+    dispatch(setActiveFieldIndex(null));
+  };
+
+  const handleClick = (index: number) => {
+    dispatch(setActiveFieldIndex(index));
+    setEditDrawerIsOpen(true);
+  };
+
+  const activeField =
+    activeFieldIndex != null ? fields[activeFieldIndex] : null;
+
+  return (
+    <Box>
+      <Table size="sm">
+        <Thead>
+          <Tr>
+            {columns.map((c) => (
+              <Th key={c.name} pl={0}>
+                {c.name}
+              </Th>
+            ))}
+          </Tr>
+        </Thead>
+        <Tbody>
+          {fields.map((field, idx) => (
+            <Tr
+              key={field.name}
+              _hover={{ bg: "gray.50" }}
+              cursor="pointer"
+              onClick={() => handleClick(idx)}
+              tabIndex={0}
+              onKeyPress={(e) => {
+                if (e.key === "Enter") {
+                  handleClick(idx);
+                }
+              }}
+            >
+              {columns.map((c) => (
+                <Td key={`${c.name}-${field.name}`} pl={0}>
+                  {field[c.attribute]}
+                </Td>
+              ))}
+            </Tr>
           ))}
-        </Tr>
-      ))}
-    </Tbody>
-  </Table>
-);
+        </Tbody>
+      </Table>
+      {activeField ? (
+        <EditFieldDrawer
+          isOpen={editDrawerIsOpen}
+          onClose={handleClose}
+          field={activeField}
+        />
+      ) : null}
+    </Box>
+  );
+};
 
 export default DatasetFieldsTable;
