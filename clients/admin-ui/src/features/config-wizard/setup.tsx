ae1f2a7add0112e243c6b9680a33010245c40a32
LKCSmith <99051851+LKCSmith@users.noreply.github.com>
2022-06-08 13:09:33 -0500

    [#569, #570] Organization setup (#719)

--- clients/admin-ui/src/features/config-wizard/setup.tsx
+++ clients/admin-ui/src/features/config-wizard/setup.tsx
@@ -1,55 +1,68 @@
 import { Button, Container, Heading, Image, Stack } from "@fidesui/react";
-import type { NextPage } from "next";
+import { useRouter } from "next/router";
 import React from "react";
 
-const Setup: NextPage = () => (
-  <Stack>
-    <Container
-      height="35vh"
-      maxW="100vw"
-      padding="0px"
-      bg="white"
-      color="white"
-    >
-      <Image
-        boxSize="100%"
-        // fallback={}
-        objectFit="fill"
-        // src=""
-        alt="Data Map"
-      />
-    </Container>
+interface Props {
+  wizardStep: Function;
+}
 
-    <main>
-      <Stack px={9} py={10} spacing={6}>
-        <Heading as="h3" size="lg">
-          Get Started
-        </Heading>
-        <div>
-          Privacy engineering can seem like an endlessly complex confluence of
-          legal and data engineering terminology - fear not; Fides is here to
-          simplify this. This wizard will help you to very quickly configure
-          Fidesctl and in doing so build your first data map. Along the way
-          you&apos;ll learn the basic resources and concepts of privacy
-          engineering with Fides so you can quickly apply in all your work.
-        </div>
-        <div>Let&apos;s get started!</div>
-        <Stack direction={["column", "row"]} spacing="24px">
-          <Button
-            bg="primary.800"
-            _hover={{ bg: "primary.400" }}
-            _active={{ bg: "primary.500" }}
-            colorScheme="primary"
-          >
-            Guided Setup (Recommended)
-          </Button>
-          <Button variant="ghost" mr={4} colorScheme="complimentary">
-            Skip (Power User)
-          </Button>
+const Setup = ({ wizardStep }: Props) => {
+  const router = useRouter();
+  return (
+    <Stack>
+      <Container
+        height="35vh"
+        maxW="100vw"
+        padding="0px"
+        bg="white"
+        color="white"
+      >
+        <Image
+          boxSize="100%"
+          // fallback={}
+          objectFit="fill"
+          // src=""
+          alt="Data Map"
+        />
+      </Container>
+
+      <main>
+        <Stack px={9} py={10} spacing={6}>
+          <Heading as="h3" size="lg">
+            Get Started
+          </Heading>
+          <div>
+            Privacy engineering can seem like an endlessly complex confluence of
+            legal and data engineering terminology - fear not; Fides is here to
+            simplify this. This wizard will help you to very quickly configure
+            Fidesctl and in doing so build your first data map. Along the way
+            you&apos;ll learn the basic resources and concepts of privacy
+            engineering with Fides so you can quickly apply in all your work.
+          </div>
+          <div>Let&apos;s get started!</div>
+          <Stack direction={["column", "row"]} spacing="24px">
+            <Button
+              bg="primary.800"
+              _hover={{ bg: "primary.400" }}
+              _active={{ bg: "primary.500" }}
+              colorScheme="primary"
+              onClick={() => wizardStep(true)}
+            >
+              Guided Setup (Recommended)
+            </Button>
+            <Button
+              variant="ghost"
+              mr={4}
+              colorScheme="complimentary"
+              onClick={() => router.push("/")}
+            >
+              Skip (Power User)
+            </Button>
+          </Stack>
         </Stack>
-      </Stack>
-    </main>
-  </Stack>
-);
+      </main>
+    </Stack>
+  );
+};
 
 export default Setup;
