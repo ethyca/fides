a042f863f9b6f1e52c52f4cbfe30aeff4831acca
Allison King <allisonjuliaking@gmail.com>
2022-06-09 15:57:06 -0400

    Add data category tag

--- clients/admin-ui/src/features/dataset/DataCategoryDropdown.tsx
+++ clients/admin-ui/src/features/dataset/DataCategoryDropdown.tsx
@@ -4,10 +4,12 @@ import {
   Menu,
   MenuButton,
   MenuDivider,
+  MenuItem,
   MenuList,
+  MenuOptionGroup,
   Text,
 } from "@fidesui/react";
-import { useMemo, useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 
 import CheckboxTree from "../common/CheckboxTree";
 import { ArrowDownLineIcon } from "../common/Icon";
@@ -32,12 +34,18 @@ const DataCategoryDropdown = ({
 
   const [internalChecked, setInternalChecked] = useState(checked);
 
+  useEffect(() => {
+    // make sure this stays up to date with the parent `checked` since this is not the only
+    // component that can control which items are checked
+    setInternalChecked(checked);
+  }, [checked]);
+
   const handleClear = () => {
     setInternalChecked([]);
   };
 
   return (
-    <Menu>
+    <Menu closeOnSelect>
       <MenuButton
         as={Button}
         variant="outline"
@@ -53,20 +61,40 @@ const DataCategoryDropdown = ({
       </MenuButton>
       <MenuList>
         <Box maxHeight="500px" minWidth="300px" overflowY="scroll">
-          <Box position="sticky" top={0} zIndex={1} backgroundColor="white">
-            <Box display="flex" justifyContent="space-between" px={2} mb={2}>
-              <Button variant="outline" size="xs" onClick={handleClear} mr={2}>
-                Clear
-              </Button>
-              <Text mr={2}>Data Categories</Text>
-              <Button
-                size="xs"
-                colorScheme="primary"
-                onClick={() => onChecked(internalChecked)}
-              >
-                Done
-              </Button>
-            </Box>
+          <Box
+            position="sticky"
+            top={0}
+            zIndex={1}
+            backgroundColor="white"
+            pt={1}
+          >
+            <MenuOptionGroup>
+              <Box display="flex" justifyContent="space-between" px={2} mb={2}>
+                <MenuItem
+                  as={Button}
+                  variant="outline"
+                  size="xs"
+                  onClick={handleClear}
+                  mr={2}
+                  width="auto"
+                  closeOnSelect={false}
+                >
+                  Clear
+                </MenuItem>
+                <Text mr={2}>Data Categories</Text>
+                <MenuItem
+                  as={Button}
+                  size="xs"
+                  colorScheme="primary"
+                  color="white"
+                  _hover={{ backgroundColor: "primary.600" }}
+                  onClick={() => onChecked(internalChecked)}
+                  width="auto"
+                >
+                  Done
+                </MenuItem>
+              </Box>
+            </MenuOptionGroup>
             <MenuDivider />
           </Box>
           <Box px={2}>
--- clients/admin-ui/src/features/dataset/EditCollectionOrFieldForm.tsx
+++ clients/admin-ui/src/features/dataset/EditCollectionOrFieldForm.tsx
@@ -9,6 +9,7 @@ import { CustomSelect, CustomTextInput } from "../common/form/inputs";
 import { DATA_QUALIFIERS } from "./constants";
 import DataCategoryDropdown from "./DataCategoryDropdown";
 import { DatasetCollection, DatasetField } from "./types";
+import DataCategoryTag from "~/features/taxonomy/DataCategoryTag";
 
 type FormValues =
   | Pick<DatasetField, "description" | "data_qualifier" | "data_categories">
@@ -29,14 +30,33 @@ const EditCollectionOrFieldForm = ({ values, onClose, onSubmit }: Props) => {
     data_qualifier: values.data_qualifier,
     data_categories: values.data_categories,
   };
-  const dataCategories = useSelector(selectDataCategories);
+  const allDataCategories = useSelector(selectDataCategories);
 
   const [checkedDataCategories, setCheckedDataCategories] = useState<string[]>(
-    []
+    initialValues.data_categories ?? []
   );
 
+  const sortedCheckedDataCategories = checkedDataCategories
+    .slice()
+    .sort((a, b) => a.localeCompare(b));
+
+  const handleSubmit = (values: FormValues) => {
+    // data categories need to be handled separately since they are not a typical form element
+    const newValues = {
+      ...values,
+      ...{ data_categories: checkedDataCategories },
+    };
+    onSubmit(newValues);
+  };
+
+  const handleRemoveDataCategory = (dataCategoryName: string) => {
+    setCheckedDataCategories(
+      checkedDataCategories.filter((dc) => dc !== dataCategoryName)
+    );
+  };
+
   return (
-    <Formik initialValues={initialValues} onSubmit={onSubmit}>
+    <Formik initialValues={initialValues} onSubmit={handleSubmit}>
       <Form>
         <Box
           display="flex"
@@ -60,11 +80,24 @@ const EditCollectionOrFieldForm = ({ values, onClose, onSubmit }: Props) => {
             <Box>
               <SimpleGrid columns={[1, 2]}>
                 <FormLabel>Data Categories</FormLabel>
-                <DataCategoryDropdown
-                  dataCategories={dataCategories}
-                  checked={checkedDataCategories}
-                  onChecked={setCheckedDataCategories}
-                />
+                <Stack>
+                  <DataCategoryDropdown
+                    dataCategories={allDataCategories}
+                    checked={checkedDataCategories}
+                    onChecked={setCheckedDataCategories}
+                  />
+                  <Stack>
+                    {sortedCheckedDataCategories.map((dc) => (
+                      <DataCategoryTag
+                        key={dc}
+                        name={dc}
+                        onClose={() => {
+                          handleRemoveDataCategory(dc);
+                        }}
+                      />
+                    ))}
+                  </Stack>
+                </Stack>
               </SimpleGrid>
             </Box>
           </Stack>
