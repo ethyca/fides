ac09288b35c3a3ddb4ae2cd917b57fbc8d79a610
Andrew Jackson <andrew.c.j1995@gmail.com>
2022-06-14 15:22:13 -0400

    Add `AdminUiSettings` to helper method (#647)
    * Add `AdminUiSettings` to helper method
    
    * format code
    
    * Update changelog

--- src/fidesops/core/config.py
+++ src/fidesops/core/config.py
@@ -156,7 +156,13 @@ class FidesopsConfig(FidesSettings):
 
     def log_all_config_values(self) -> None:
         """Output DEBUG logs of all the config values."""
-        for settings in [self.database, self.redis, self.security, self.execution]:
+        for settings in [
+            self.database,
+            self.redis,
+            self.security,
+            self.execution,
+            self.admin_ui,
+        ]:
             for key, value in settings.dict().items():  # type: ignore
                 logger.debug(
                     "Using config: %s%s = %s",
