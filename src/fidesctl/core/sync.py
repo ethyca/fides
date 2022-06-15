aeb74a005f5abea0dfb9c0e865642b40d8422ac9
Thomas <thomas.lapiana+github@pm.me>
2022-06-15 14:14:24 +0700

    get basic version of sync working, update the formatting of the resource files

--- src/fidesctl/core/sync.py
+++ src/fidesctl/core/sync.py
@@ -1,9 +1,8 @@
 """This module handles the logic for syncing remote resource versions into their local file."""
 import glob
-from http import server
 from typing import Dict, List
 
-
+import yaml
 from fideslang.manifests import load_yaml_into_dict
 
 from fidesctl.core.api_helpers import get_server_resource
@@ -27,13 +26,21 @@ def sync(manifests_dir: str, url: str, headers: Dict[str, str]) -> None:
 
     for manifest_path in manifest_path_list:
         manifest = load_yaml_into_dict(manifest_path)
+        updated_manifest = {}
+
         for resource_type in manifest.keys():
             resource_list = manifest[resource_type]
+            updated_resource_list = []
+
             for resource in resource_list:
                 fides_key = resource["fides_key"]
                 server_resource = get_server_resource(
                     url, resource_type, fides_key, headers
                 )
-                manifest[resource_type][resource] = server_resource
-
-                print(server_resource)
+                if server_resource:
+                    updated_resource_list.append(server_resource)
+                else:
+                    updated_resource_list.append(resource)
+            updated_manifest[resource_type] = updated_resource_list
+        with open(manifest_path, "w") as manifest_file:
+            yaml.dump(manifest, manifest_file, sort_keys=False, indent=2)
