bc4924c005d87eab990b2f99a1d862a7359f83cc
Allison King <allisonjuliaking@gmail.com>
2022-06-15 17:54:34 -0400

    Add a multiselect input

--- clients/admin-ui/src/features/common/form/inputs.tsx
+++ clients/admin-ui/src/features/common/form/inputs.tsx
@@ -6,7 +6,7 @@ import {
   SimpleGrid,
 } from "@fidesui/react";
 import { Select } from "chakra-react-select";
-import { FieldHookConfig, useField } from "formik";
+import { FieldHookConfig, useField, useFormikContext } from "formik";
 
 interface InputProps {
   label: string;
@@ -40,16 +40,20 @@ interface SelectProps {
   label: string;
   options: Option[];
   isSearchable?: boolean;
+  isClearable?: boolean;
 }
 export const CustomSelect = ({
   label,
   options,
   isSearchable,
+  isClearable,
   ...props
 }: SelectProps & FieldHookConfig<string>) => {
   const [field, meta] = useField(props);
-
   const isInvalid = !!(meta.touched && meta.error);
+
+  const selected = options.find((o) => o.value === field.value);
+
   return (
     <FormControl isInvalid={isInvalid}>
       <SimpleGrid columns={[1, 2]}>
@@ -63,13 +67,13 @@ export const CustomSelect = ({
               field.onBlur(props.name);
             }
           }}
-          onChange={(option) => {
-            if (option) {
-              field.onChange(props.name)(option.value);
+          onChange={(newValue) => {
+            if (newValue) {
+              field.onChange(props.name)(newValue.value);
             }
           }}
           name={props.name}
-          value={options.find((o) => o.value === field.value)}
+          value={selected}
           size="sm"
           chakraStyles={{
             dropdownIndicator: (provided) => ({
@@ -84,6 +88,69 @@ export const CustomSelect = ({
             }),
           }}
           isSearchable={isSearchable ?? false}
+          isClearable={isClearable}
+        />
+      </SimpleGrid>
+      {isInvalid ? <FormErrorMessage>{meta.error}</FormErrorMessage> : null}
+    </FormControl>
+  );
+};
+
+// mostly the same as CustomSelect except can handle multiple values
+// the types are easier when this is a separate component as opposed to
+// extending CustomSelect
+export const CustomMultiSelect = ({
+  label,
+  options,
+  isSearchable,
+  isClearable,
+  ...props
+}: SelectProps & FieldHookConfig<string[]>) => {
+  const [field, meta] = useField(props);
+  const isInvalid = !!(meta.touched && meta.error);
+  const selected = options.filter((o) => field.value.indexOf(o.value) >= 0);
+  // note: for Multiselect we have to do setFieldValue instead of field.onChange
+  // because field.onChange only accepts strings or events right now, not string[]
+  // https://github.com/jaredpalmer/formik/issues/1667
+  const { setFieldValue } = useFormikContext();
+
+  return (
+    <FormControl isInvalid={isInvalid}>
+      <SimpleGrid columns={[1, 2]}>
+        <FormLabel htmlFor={props.id || props.name} size="sm">
+          {label}
+        </FormLabel>
+        <Select
+          options={options}
+          onBlur={(option) => {
+            if (option) {
+              field.onBlur(props.name);
+            }
+          }}
+          onChange={(newValue) => {
+            setFieldValue(
+              field.name,
+              newValue.map((v) => v.value)
+            );
+          }}
+          name={props.name}
+          value={selected}
+          size="sm"
+          chakraStyles={{
+            dropdownIndicator: (provided) => ({
+              ...provided,
+              bg: "transparent",
+              px: 2,
+              cursor: "inherit",
+            }),
+            indicatorSeparator: (provided) => ({
+              ...provided,
+              display: "none",
+            }),
+          }}
+          isSearchable={isSearchable ?? false}
+          isClearable={isClearable}
+          isMulti
         />
       </SimpleGrid>
       {isInvalid ? <FormErrorMessage>{meta.error}</FormErrorMessage> : null}
