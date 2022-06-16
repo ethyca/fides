7d4e689babfd7f1fd8fd79fb974f3e1c42253e9d
Allison King <allisonjuliaking@gmail.com>
2022-06-16 15:43:03 -0400

    Multiselect component (#771)
    * Rework CustomSelect to use chakra-react-select
    
    * Add a multiselect input

--- clients/admin-ui/src/features/dataset/EditCollectionOrFieldForm.tsx
+++ clients/admin-ui/src/features/dataset/EditCollectionOrFieldForm.tsx
@@ -10,6 +10,11 @@ import { DATA_QUALIFIERS } from "./constants";
 import DataCategoryInput from "./DataCategoryInput";
 import { DatasetCollection, DatasetField } from "./types";
 
+const IDENTIFIER_OPTIONS = DATA_QUALIFIERS.map((dq) => ({
+  value: dq.key,
+  label: dq.label,
+}));
+
 type FormValues =
   | Pick<DatasetField, "description" | "data_qualifier" | "data_categories">
   | Pick<
@@ -58,13 +63,12 @@ const EditCollectionOrFieldForm = ({ values, onClose, onSubmit }: Props) => {
               <CustomTextInput name="description" label="Description" />
             </Box>
             <Box>
-              <CustomSelect name="data_qualifier" label="Identifiability">
-                {DATA_QUALIFIERS.map((qualifier) => (
-                  <option key={qualifier.key} value={qualifier.key}>
-                    {qualifier.label}
-                  </option>
-                ))}
-              </CustomSelect>
+              <CustomSelect
+                name="data_qualifier"
+                label="Identifiability"
+                options={IDENTIFIER_OPTIONS}
+                isSearchable={false}
+              />
             </Box>
             <Box>
               <DataCategoryInput
