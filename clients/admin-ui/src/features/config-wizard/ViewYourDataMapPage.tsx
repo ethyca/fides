fa0a477c13588b898c7f4d650dae1e132096ec38
LKCSmith <99051851+LKCSmith@users.noreply.github.com>
2022-06-21 15:44:01 -0500

    Finish page (#791)

---
+++ clients/admin-ui/src/features/config-wizard/ViewYourDataMapPage.tsx
@@ -0,0 +1,18 @@
+import { chakra, Heading, Stack } from "@fidesui/react";
+import type { NextPage } from "next";
+import React from "react";
+
+const ViewYourDataMapPage: NextPage<{}> = () => (
+  <chakra.form w="100%">
+    <Stack ml="100px" spacing={10}>
+      <Heading as="h3" size="lg">
+        View your Data Map!
+      </Heading>
+      <div>
+        Congratulations, you have built your first data map on Fides! From here
+        you can easily extend this by adding additional systems.
+      </div>
+    </Stack>
+  </chakra.form>
+);
+export default ViewYourDataMapPage;
