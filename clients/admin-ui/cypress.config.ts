f78190aaa4b58cc5924827bcabe9de3fdf78eb28
Allison King <allisonjuliaking@gmail.com>
2022-06-29 14:49:30 -0400

    Initial cypress setup

---
+++ clients/admin-ui/cypress.config.ts
@@ -0,0 +1,11 @@
+/* eslint-disable import/no-extraneous-dependencies */
+import { defineConfig } from "cypress";
+
+export default defineConfig({
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+    baseUrl: "http://localhost:3000",
+  },
+});
---
+++ clients/admin-ui/cypress/support/commands.ts
@@ -0,0 +1,50 @@
+/// <reference types="cypress" />
+// ***********************************************
+// This example commands.ts shows you how to
+// create various custom commands and overwrite
+// existing commands.
+//
+// For more comprehensive examples of custom
+// commands please read more here:
+// https://on.cypress.io/custom-commands
+// ***********************************************
+//
+//
+// -- This is a parent command --
+// Cypress.Commands.add('login', (email, password) => { ... })
+//
+//
+// -- This is a child command --
+// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
+//
+//
+// -- This is a dual command --
+// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
+//
+//
+// -- This will overwrite an existing command --
+// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+//
+
+Cypress.Commands.add("getByTestId", (selector, ...args) =>
+  cy.get(`[data-testid=${selector}]`, ...args)
+);
+
+declare global {
+  namespace Cypress {
+    interface Chainable {
+      /**
+       * Custom command to select DOM element by data-testid attribute
+       * @example cy.getByTestId('clear-btn')
+       */
+      getByTestId(selector: string): Chainable<JQuery<HTMLElement>>;
+      //   login(email: string, password: string): Chainable<void>
+      //   drag(subject: string, options?: Partial<TypeOptions>): Chainable<Element>
+      //   dismiss(subject: string, options?: Partial<TypeOptions>): Chainable<Element>
+      //   visit(originalFn: CommandOriginalFn, url: string, options: Partial<VisitOptions>): Chainable<Element>
+    }
+  }
+}
+
+// Convert this to a module instead of script (allows import/export)
+export {};
