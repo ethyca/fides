025ab2f9fa0adcf966776cc0f412dcad26ace022
Paul Sanders <psanders1@gmail.com>
2022-07-07 09:45:46 -0400

    Add fixture to clear tables between test (#680)
    * Add fixture to clear tables between test runs
    
    * Update CHANGELOG
    
    * Add missing ordering to customer_details logs query in test.
    
    * update import path
    
    Co-authored-by: Paul Sanders <pau@ethyca.com>
    Co-authored-by: Dawn Pattison <pattisdr@users.noreply.github.com>
    Co-authored-by: Sean Preston <sean@ethyca.com>

--- tests/conftest.py
+++ tests/conftest.py
@@ -15,10 +15,12 @@ from fideslib.cryptography.schemas.jwt import (
 from fideslib.db.session import Session, get_db_engine, get_db_session
 from fideslib.models.client import ClientDetail
 from fideslib.oauth.jwt import generate_jwe
+from sqlalchemy.exc import IntegrityError
 from sqlalchemy_utils.functions import create_database, database_exists, drop_database
 
 from fidesops.api.v1.scope_registry import SCOPE_REGISTRY
 from fidesops.core.config import config
+from fidesops.db.base import Base
 from fidesops.db.database import init_db
 from fidesops.main import app
 from fidesops.models.privacy_request import generate_request_callback_jwe
@@ -91,6 +93,34 @@ def db() -> Generator:
     logger.debug(f"Database at: {engine.url} successfully dropped")
 
 
+@pytest.fixture(autouse=True)
+def clear_db_tables(db):
+    """Clear data from tables between tests.
+
+    If relationships are not set to cascade on delete they will fail with an
+    IntegrityError if there are relationsips present. This function stores tables
+    that fail with this error then recursively deletes until no more IntegrityErrors
+    are present.
+    """
+    yield
+
+    def delete_data(tables):
+        redo = []
+        for table in tables:
+            try:
+                db.execute(table.delete())
+            except IntegrityError:
+                redo.append(table)
+            finally:
+                db.commit()
+
+        if redo:
+            delete_data(redo)
+
+    db.commit()  # make sure all transactions are closed before starting deletes
+    delete_data(Base.metadata.sorted_tables)
+
+
 @pytest.fixture(scope="session")
 def cache() -> Generator:
     yield get_cache()
@@ -115,7 +145,6 @@ def oauth_client(db: Session) -> Generator:
     db.commit()
     db.refresh(client)
     yield client
-    client.delete(db)
 
 
 def generate_auth_header_for_user(user, scopes) -> Dict[str, str]:
