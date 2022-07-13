461e71ee481b706e63d0a4426bfe5aa5841464a8
Allison King <allisonjuliaking@gmail.com>
2022-07-13 12:29:39 -0400

    Update menu functionality

--- clients/admin-ui/src/features/dataset/ColumnDropdown.tsx
+++ clients/admin-ui/src/features/dataset/ColumnDropdown.tsx
@@ -8,7 +8,6 @@ import {
   MenuList,
   Stack,
 } from "@fidesui/react";
-import { useState } from "react";
 
 import { ArrowDownLineIcon } from "~/features/common/Icon";
 
@@ -20,19 +19,23 @@ interface Props {
   onChange: (columns: ColumnMetadata[]) => void;
 }
 const ColumnDropdown = ({ allColumns, selectedColumns, onChange }: Props) => {
-  // this component keeps internal column state as well, since the user can select fields
-  // and then click the "Done" button to propagate to the rest of the UI
-  const [columns, setColumns] = useState(selectedColumns);
-
   const handleClear = () => {
-    setColumns([]);
+    onChange([]);
   };
 
-  const handleSubmit = () => {
+  const handleChange = (column: ColumnMetadata) => {
+    let newColumns: ColumnMetadata[] = [];
+    if (selectedColumns.filter((col) => col.name === column.name).length > 0) {
+      // remove from selected columns
+      newColumns = selectedColumns.filter((col) => col.name !== column.name);
+    } else {
+      newColumns = [...selectedColumns, column];
+    }
+
     // we want to keep the sort order the same based off of all columns, so that the
     // table columns don't go jumping around
     const allColumnNames = allColumns.map((c) => c.name);
-    const sortedSelected = columns
+    const sortedSelected = newColumns
       .slice()
       .sort(
         (a, b) =>
@@ -41,57 +44,53 @@ const ColumnDropdown = ({ allColumns, selectedColumns, onChange }: Props) => {
     onChange(sortedSelected);
   };
 
-  const handleChange = (column: ColumnMetadata) => {
-    if (columns.filter((col) => col.name === column.name).length > 0) {
-      // remove from selected columns
-      setColumns(columns.filter((col) => col.name !== column.name));
-    } else {
-      setColumns([...columns, column]);
-    }
-  };
-
   return (
     <Menu>
-      <MenuButton
-        as={Button}
-        rightIcon={<ArrowDownLineIcon />}
-        variant="outline"
-        fontWeight="normal"
-      >
-        Columns
-      </MenuButton>
-      <MenuList>
-        <Box px={2}>
-          <Box display="flex" justifyContent="space-between" mb={2}>
-            <Button variant="outline" size="xs" onClick={handleClear}>
-              Clear
-            </Button>
-            <Button size="xs" colorScheme="primary" onClick={handleSubmit}>
-              Done
-            </Button>
-          </Box>
-          <CheckboxGroup colorScheme="complimentary">
-            <Stack>
-              {allColumns.map((column) => {
-                const isChecked =
-                  columns.filter((selected) => selected.name === column.name)
-                    .length > 0;
-                return (
-                  <Checkbox
-                    id={column.name}
-                    key={column.name}
-                    _hover={{ bg: "gray.100" }}
-                    isChecked={isChecked}
-                    onChange={() => handleChange(column)}
-                  >
-                    {column.name}
-                  </Checkbox>
-                );
-              })}
-            </Stack>
-          </CheckboxGroup>
-        </Box>
-      </MenuList>
+      {({ onClose }) => (
+        <>
+          <MenuButton
+            as={Button}
+            rightIcon={<ArrowDownLineIcon />}
+            variant="outline"
+            fontWeight="normal"
+          >
+            Columns
+          </MenuButton>
+          <MenuList>
+            <Box px={2}>
+              <Box display="flex" justifyContent="space-between" mb={2}>
+                <Button variant="outline" size="xs" onClick={handleClear}>
+                  Clear
+                </Button>
+                <Button size="xs" colorScheme="primary" onClick={onClose}>
+                  Done
+                </Button>
+              </Box>
+              <CheckboxGroup colorScheme="complimentary">
+                <Stack>
+                  {allColumns.map((column) => {
+                    const isChecked =
+                      selectedColumns.filter(
+                        (selected) => selected.name === column.name
+                      ).length > 0;
+                    return (
+                      <Checkbox
+                        id={column.name}
+                        key={column.name}
+                        _hover={{ bg: "gray.100" }}
+                        isChecked={isChecked}
+                        onChange={() => handleChange(column)}
+                      >
+                        {column.name}
+                      </Checkbox>
+                    );
+                  })}
+                </Stack>
+              </CheckboxGroup>
+            </Box>
+          </MenuList>
+        </>
+      )}
     </Menu>
   );
 };
