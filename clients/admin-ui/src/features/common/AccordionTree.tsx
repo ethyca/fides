500da5952bbf96e2d293c6b9a1310e75e39e7657
Allison King <allisonjuliaking@gmail.com>
2022-07-15 15:45:38 -0400

    Implement hover, kind of

--- clients/admin-ui/src/features/common/AccordionTree.tsx
+++ clients/admin-ui/src/features/common/AccordionTree.tsx
@@ -9,7 +9,7 @@ import {
   ButtonGroup,
   Text,
 } from "@fidesui/react";
-import { Fragment } from "react";
+import { Fragment, useState } from "react";
 
 import { TreeNode } from "./types";
 
@@ -24,44 +24,48 @@ interface Props {
   nodes: TreeNode[];
 }
 const AccordionTree = ({ nodes }: Props) => {
+  const [hoverNode, setHoverNode] = useState<TreeNode | undefined>(undefined);
   /**
    * Recursive function to generate the accordion tree
    */
   const createTree = (node: TreeNode) => {
-    // TODO: these borders are cut a little short, need to figure out CSS
-    const borderProps = {
+    // some nodes render as AccordionItems and some as just Boxes, but
+    // we want to keep their styling similar, so pass them the same props
+    const itemProps = {
+      // TODO: these borders are cut a little short, need to figure out CSS
       borderBottom: "1px solid",
       borderColor: "gray.200",
+      display: "flex",
+      justifyContent: "space-between",
+      alignItems: "center",
+      _hover: { bg: "gray.50" },
+      onMouseEnter: () => {
+        setHoverNode(node);
+      },
     };
+
     if (node.children.length === 0) {
       return (
         <Box
           pl={9}
           py={2}
-          _hover={{ bg: "gray.50" }}
-          {...borderProps}
+          {...itemProps}
           display="flex"
           justifyContent="space-between"
         >
           <Text>{node.label}</Text>
-          <ActionButtons />
+          {hoverNode?.value === node.value ? <ActionButtons /> : null}
         </Box>
       );
     }
     return (
       <AccordionItem py={0} border="none">
-        <Box
-          display="flex"
-          justifyContent="space-between"
-          alignItems="center"
-          {...borderProps}
-        >
+        <Box {...itemProps}>
           <AccordionButton _expanded={{ color: "complimentary.500" }}>
             <AccordionIcon />
             {node.label}
           </AccordionButton>
-          {/* TODO: only show action buttons on hover */}
-          <ActionButtons />
+          {hoverNode?.value === node.value ? <ActionButtons /> : null}
         </Box>
 
         <AccordionPanel py={0} pr={0}>
