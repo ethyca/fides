8e106c28f520e241f2a0e91eee9f371cc5c0ae25
Sean Preston <sean@ethyca.com>
2022-07-15 11:44:19 +0200

    Adds `celery.toml` for loading custom Celery config [#821] (#865)
    * adds option to configure EVENT_QUEUE_PREFIX for celery
    
    * provide the option to specify a default queue name too
    
    * update celery config to load in from its own config toml file
    
    * updates changelog
    
    * update value for event_queue_prefix
    
    * test celery config overrides
    
    * include config_path arg
    
    * add type def
    
    * add config path to execution settings
    
    * correct values

--- tests/tasks/test_celery.py
+++ tests/tasks/test_celery.py
@@ -1,3 +1,7 @@
+from fidesops.core.config import config
+from fidesops.tasks import _create_celery
+
+
 def test_create_task(celery_session_app, celery_session_worker):
     @celery_session_app.task
     def multiply(x, y):
@@ -8,3 +12,17 @@ def test_create_task(celery_session_app, celery_session_worker):
     celery_session_worker.reload()
     assert multiply.run(4, 4) == 16
     assert multiply.delay(4, 4).get(timeout=10) == 16
+
+
+def test_celery_default_config() -> None:
+    celery_app = _create_celery()
+    assert celery_app.conf["broker_url"] == config.redis.CONNECTION_URL
+    assert celery_app.conf["result_backend"] == config.redis.CONNECTION_URL
+    assert celery_app.conf["event_queue_prefix"] == "fidesops_worker"
+    assert celery_app.conf["default_queue_name"] == "fidesops"
+
+
+def test_celery_config_override() -> None:
+    celery_app = _create_celery(config_path="data/config/celery.toml")
+    assert celery_app.conf["event_queue_prefix"] == "overridden_fidesops_worker"
+    assert celery_app.conf["default_queue_name"] == "overridden_fidesops"
