859f7ed01f19eeeb0dde97059d08eb9a587473e7
Eduardo Armendariz <eduardo.armendariz13@gmail.com>
2022-07-18 22:04:12 -0700

    Merge fidesctl config with fideslib config (#913)
    * Merge fidesctl config with fideslib config
    
    * Fix weirdness with pydantic n custom constructor
    
    * Fix pylint issues
    
    * remove api settings from code base
    
    * Refactor configs to be consistent
    
    * Remove uneccessary configs
    
    * Add defaults again to fields
    
    * Remove usage of default methods in favor of overriding class fields
    
    * Merge config path loading with fideslib
    
    * Fix typo
    
    * Update docs and small fixes
    
    * Fix config override to allow full override
    
    * Update src/fidesctl/core/config/logging_settings.py
    
    Co-authored-by: Paul Sanders <psanders1@gmail.com>
    
    * Update src/fidesctl/core/config/logging_settings.py
    
    Co-authored-by: Paul Sanders <psanders1@gmail.com>
    
    * Add default value for FIDESCTL_TEST_MODE in log settings
    
    Co-authored-by: Eduardo Armendariz <eduardo@ethyca.com>
    Co-authored-by: Paul Sanders <psanders1@gmail.com>
    Co-authored-by: Paul Sanders <pau@ethyca.com>

--- src/fidesctl/core/config/cli_settings.py
+++ src/fidesctl/core/config/cli_settings.py
@@ -9,7 +9,7 @@ from .fides_settings import FidesSettings
 # pylint: disable=C0115,C0116, E0213
 
 
-class CLISettings(FidesSettings):
+class FidesctlCLISettings(FidesSettings):
     """Class used to store values from the 'cli' section of the config."""
 
     local_mode: bool = False
