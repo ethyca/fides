0ad5a176ed3087afc65df049e37ae29c428ed489
chriscalhoun1974 <68459950+chriscalhoun1974@users.noreply.github.com>
2022-07-26 10:01:07 -0400

    863 - Retry a DSR (FE) (#938)

--- clients/admin-ui/src/features/subject-request/RequestDetails.tsx
+++ clients/admin-ui/src/features/subject-request/RequestDetails.tsx
@@ -1,10 +1,19 @@
-import { Box, Divider, Flex, Heading, Text } from "@fidesui/react";
-import React from "react";
-
-import ClipboardButton from "../common/ClipboardButton";
-import RequestStatusBadge from "../common/RequestStatusBadge";
-import RequestType from "../common/RequestType";
-import { PrivacyRequest } from "../privacy-requests/types";
+import {
+  Button,
+  Divider,
+  Flex,
+  Heading,
+  HStack,
+  Text,
+  useToast,
+} from "@fidesui/react";
+import ClipboardButton from "common/ClipboardButton";
+import { isErrorWithDetail, isErrorWithDetailArray } from "common/helpers";
+import RequestStatusBadge from "common/RequestStatusBadge";
+import RequestType from "common/RequestType";
+import { useRetryMutation } from "privacy-requests/privacy-requests.slice";
+import { PrivacyRequest } from "privacy-requests/types";
+import { useState } from "react";
 
 type RequestDetailsProps = {
   subjectRequest: PrivacyRequest;
@@ -12,6 +21,30 @@ type RequestDetailsProps = {
 
 const RequestDetails = ({ subjectRequest }: RequestDetailsProps) => {
   const { id, status, policy } = subjectRequest;
+  const [retry] = useRetryMutation();
+  const toast = useToast();
+  const [isRetrying, setRetrying] = useState(false);
+
+  const handleRetry = async () => {
+    setRetrying(true);
+    retry(subjectRequest)
+      .unwrap()
+      .catch((error) => {
+        let errorMsg = "An unexpected error occurred. Please try again.";
+        if (isErrorWithDetail(error)) {
+          errorMsg = error.data.detail;
+        } else if (isErrorWithDetailArray(error)) {
+          errorMsg = error.data.detail[0].msg;
+        }
+        toast({
+          status: "error",
+          description: errorMsg,
+        });
+      })
+      .finally(() => {
+        setRetrying(false);
+      });
+  };
 
   return (
     <>
@@ -35,7 +68,6 @@ const RequestDetails = ({ subjectRequest }: RequestDetailsProps) => {
         </Text>
         <ClipboardButton requestId={id} />
       </Flex>
-
       <Flex alignItems="center">
         <Text mb={4} mr={2} fontSize="sm" color="gray.900" fontWeight="500">
           Request Type:
@@ -46,9 +78,21 @@ const RequestDetails = ({ subjectRequest }: RequestDetailsProps) => {
         <Text mb={4} mr={2} fontSize="sm" color="gray.900" fontWeight="500">
           Status:
         </Text>
-        <Box>
+        <HStack spacing="16px">
           <RequestStatusBadge status={status} />
-        </Box>
+          {status === "error" && (
+            <Button
+              isLoading={isRetrying}
+              loadingText="Retrying"
+              onClick={handleRetry}
+              size="xs"
+              spinnerPlacement="end"
+              variant="outline"
+            >
+              Retry
+            </Button>
+          )}
+        </HStack>
       </Flex>
     </>
   );
