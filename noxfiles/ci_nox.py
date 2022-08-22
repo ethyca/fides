1c819aa802b79a221608cde74b3e1571b0777434
Thomas <thomas.lapiana+github@pm.me>
2022-08-22 17:42:10 +0800

    isort things

--- noxfiles/ci_nox.py
+++ noxfiles/ci_nox.py
@@ -5,10 +5,10 @@ import nox
 
 from constants_nox import (
     CI_ARGS,
-    COMPOSE_SERVICE_NAME,
     COMPOSE_FILE,
-    INTEGRATION_COMPOSE_FILE,
+    COMPOSE_SERVICE_NAME,
     IMAGE_NAME,
+    INTEGRATION_COMPOSE_FILE,
     RUN,
     RUN_NO_DEPS,
     RUN_OPS_NO_DEPS,
