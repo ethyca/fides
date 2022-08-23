b0eee6d0c11136c31788b2b2dff48061782def25
Andrew Jackson <andrew.c.j1995@gmail.com>
2022-08-23 11:56:03 -0400

    Add email_templates module (#1123)
    * Add email_templates module
    
    * run isort
    
    * Add unit tests
    
    * Update ttl calculation
    
    * Add ttl minutes test
    
    * fix lint issues
    
    * fix pylint issue
    
    * fix pylint issue
    
    * fix isort
    
    * Update template constant
    
    * Update changelog
    
    * fix lints
    
    * Add jinja to requirements.txt
    
    * update templates directory
    
    * update unit test
    
    * Update imports
    
    * fix issue template path
    
    * Add templates to manifest
    
    Co-authored-by: Paul Sanders <pau@ethyca.com>

--- src/fidesops/ops/common_exceptions.py
+++ src/fidesops/ops/common_exceptions.py
@@ -117,6 +117,10 @@ class EmailDispatchException(FidesopsException):
     """Custom Exception - Email Dispatch Error"""
 
 
+class EmailTemplateUnhandledActionType(FidesopsException):
+    """Custom Exception - Email Template Unhandled ActionType Error"""
+
+
 class OAuth2TokenException(FidesopsException):
     """Custom Exception - Unable to access or refresh OAuth2 tokens for SaaS connector"""
 
