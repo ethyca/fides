c4b0d9089b8d0866634b827a6144b2f2fdbc3d80
Sebastian Sangervasi <sebastian@ethyca.com>
2022-08-31 13:48:20 -0700

    fctl/nav: Replace Jest nav tests with Cypress tests
    This change switches moves our nav routing tests into Cypress instead of Jest.
    The React test renderer was having trouble integrating with Next's dynamic import,
    which was going to require a complex solution. Instead, by moving the nav tests into
    Cypress we can test the real router state without relying on mocks.
    
    This will prove even more useful in a later commit when we update what links are available
    based on whether the API says we are in fidesctl-plus.

---
+++ clients/ctl/admin-ui/cypress/e2e/nav-bar.cy.ts
@@ -0,0 +1,31 @@
+describe("Nav Bar", () => {
+  it("Renders all page links", () => {
+    cy.visit("/");
+
+    // Enabled links
+    cy.getByTestId("nav-link-Datasets");
+    cy.getByTestId("nav-link-Taxonomy");
+
+    // Disabled links
+    cy.getByTestId("nav-link-Systems").should("have.attr", "disabled");
+    cy.getByTestId("nav-link-Policies").should("have.attr", "disabled");
+    cy.getByTestId("nav-link-User Management").should("have.attr", "disabled");
+    cy.getByTestId("nav-link-More").should("have.attr", "disabled");
+  });
+
+  it("Renders the active page based on the current route", () => {
+    // Start on the dataset page.
+    cy.visit("/dataset");
+
+    // The nav should reflect the active page.
+    cy.getByTestId("nav-link-Datasets").should("have.attr", "data-active");
+    cy.getByTestId("nav-link-Taxonomy").should("not.have.attr", "data-active");
+
+    // Navigate by clicking a nav link.
+    cy.getByTestId("nav-link-Taxonomy").click();
+
+    // The nav should update which page is active.
+    cy.getByTestId("nav-link-Taxonomy").should("have.attr", "data-active");
+    cy.getByTestId("nav-link-Datasets").should("not.have.attr", "data-active");
+  });
+});
