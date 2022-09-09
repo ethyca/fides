c04bd289e7ca41ead7d02cb3dee303a26c9d6ffd
Allison King <allisonjuliaking@gmail.com>
2022-09-09 14:49:35 -0400

    Add cypress test for system

--- clients/ctl/admin-ui/cypress/e2e/systems.cy.ts
+++ clients/ctl/admin-ui/cypress/e2e/systems.cy.ts
@@ -40,4 +40,64 @@ describe("System management page", () => {
       cy.getByTestId("system-demo_marketing_system");
     });
   });
+
+  describe("Can create a new system", () => {
+    it("Can create a system via yaml", () => {
+      cy.intercept("POST", "/api/v1/system", { fixture: "system.json" }).as(
+        "postSystem"
+      );
+      cy.visit("/system/new");
+      cy.getByTestId("upload-yaml-btn").click();
+      cy.fixture("system.json").then((system) => {
+        const systemAsString = JSON.stringify(system);
+        // Cypress doesn't have a native "paste" command, so instead do change the value directly
+        // (.type() is too slow, even with 0 delay)
+        cy.getByTestId("input-yaml")
+          .click()
+          .invoke("val", systemAsString)
+          .trigger("change");
+        // type just one space character to make sure the text field triggers Formik's handlers
+        cy.getByTestId("input-yaml").type(" ");
+
+        cy.getByTestId("submit-yaml-btn").click();
+        cy.wait("@postSystem").then((interception) => {
+          const { body } = interception.request;
+          expect(body).to.eql(system);
+        });
+
+        // should navigate to the created system
+        cy.getByTestId("toast-success-msg");
+        cy.url().should("contain", `system`);
+      });
+    });
+
+    it("Can render errors in yaml", () => {
+      cy.intercept("POST", "/api/v1/system", {
+        statusCode: 422,
+        body: {
+          detail: [
+            {
+              loc: ["body", "fides_key"],
+              msg: "field required",
+              type: "value_error.missing",
+            },
+            {
+              loc: ["body", "system_type"],
+              msg: "field required",
+              type: "value_error.missing",
+            },
+          ],
+        },
+      }).as("postSystem");
+      cy.visit("/system/new");
+      cy.getByTestId("upload-yaml-btn").click();
+
+      // invalid system with missing fields
+      cy.getByTestId("input-yaml")
+        .clear()
+        .type("valid yaml that is not a system");
+      cy.getByTestId("submit-yaml-btn").click();
+      cy.getByTestId("error-yaml").should("contain", "field required");
+    });
+  });
 });
