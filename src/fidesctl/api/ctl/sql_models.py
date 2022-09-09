59cc208bf0efc8f2eefde7b12d7a3f06a52a9914
SteveDMurphy <steven.d.murphy@gmail.com>
2022-09-08 23:37:37 -0400

    cls migration
    See [fidesctl-plus#104](https://github.com/ethyca/fidesctl-plus/issues/104) for further rationale, but to summarize the idea of consolidating migrations to a single location was much more popular.
    
    Originally 4 tables were projected, but feedback in the above referenced issue provided insight to the opportunity to consolidate the data from one of the tables. Further changes can be expected to the classification details as more feedback on user experience is understood

--- src/fidesctl/api/ctl/sql_models.py
+++ src/fidesctl/api/ctl/sql_models.py
@@ -13,6 +13,7 @@ from sqlalchemy import (
     BOOLEAN,
     JSON,
     Column,
+    Integer,
     String,
     Text,
     TypeDecorator,
@@ -83,6 +84,67 @@ class PGEncryptedString(TypeDecorator):
 
     def process_result_value(self, value, dialect):
         pass
+
+
+class ClassificationDetail(Base):
+    """
+    The SQL model for a classification instance
+    """
+
+    __tablename__ = "cls_classification_detail"
+
+    classification_detail_id = Column(Integer, primary_key=True, autoincrement=True)
+    classification_detail_status_id = Column(Integer)
+    classification_dataset = Column(Text)
+    classification_collection = Column(Text)
+    classification_field = Column(Text)
+    classification_labels = Column(JSON)
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
+    updated_at = Column(
+        DateTime(timezone=True),
+        server_default=func.now(),
+        onupdate=func.now(),
+    )
+    # get the details from a classification json output (likely aggregate and options etc.)
+
+
+class ClassificationInstance(Base):
+    """
+    The SQL model for a classification instance
+    """
+
+    __tablename__ = "cls_classification_instance"
+
+    classification_instance_id = Column(Integer, primary_key=True, autoincrement=True)
+    classification_status_id = Column(Integer)
+    organization_key = Column(Text)
+    classification_target = Column(Text)
+    classification_type = Column(Text)
+    # update with metadata here to remove the need for a classification table (can join on similar attrs maybe)
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
+    updated_at = Column(
+        DateTime(timezone=True),
+        server_default=func.now(),
+        onupdate=func.now(),
+    )
+
+
+class ClassificationState(Base):
+    """
+    The SQL model for a classification instance
+    """
+
+    __tablename__ = "cls_classification_status"
+
+    classification_status_id = Column(Integer, primary_key=True, autoincrement=True)
+    classification_status = Column(Text)
+    classification_status_is_active = Column(BOOLEAN)
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
+    updated_at = Column(
+        DateTime(timezone=True),
+        server_default=func.now(),
+        onupdate=func.now(),
+    )
 
 
 # Privacy Types
