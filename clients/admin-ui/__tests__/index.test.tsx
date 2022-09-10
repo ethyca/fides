90bdf313140acba94117ad718f161126d4b9e077
Andrew Jackson <andrew.c.j1995@gmail.com>
2022-09-10 17:28:05 -0400

    Move over __tests__ files

--- clients/admin-ui/__tests__/index.test.tsx
+++ clients/admin-ui/__tests__/index.test.tsx
@@ -18,4 +18,17 @@ describe("Home", () => {
     const message = screen.getAllByText("Subject Requests")[0];
     expect(message).toBeInTheDocument();
   });
+
+  // TODO: Either update or remove this test. We no longer use the SessionProvider from next/auth
+  // skip until this is fixed in fidesops
+  // it.skip("renders a logged out notification when no session is present", () => {
+  //   render(
+  //     <SessionProvider>
+  //       <Home session={null} />
+  //     </SessionProvider>
+  //   );
+  //
+  //   const message = screen.getByText("You are not logged in.");
+  //   expect(message).toBeInTheDocument();
+  // });
 });
