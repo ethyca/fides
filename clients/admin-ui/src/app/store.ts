f0693a51ba5617002dda48ae07d18ed25a7548c5
Andrew Jackson <andrew.c.j1995@gmail.com>
2022-09-10 20:54:23 -0400

    Add ctl slices to redux store

--- clients/admin-ui/src/app/store.ts
+++ clients/admin-ui/src/app/store.ts
@@ -1,6 +1,8 @@
 import { configureStore, StateFromReducersMapObject } from "@reduxjs/toolkit";
 import { setupListeners } from "@reduxjs/toolkit/query/react";
 
+// import { createWrapper } from "next-redux-wrapper";
+
 import { STORED_CREDENTIALS_KEY } from "../constants";
 import {
   authApi,
@@ -25,6 +27,28 @@ import {
   userApi,
 } from "user-management/index";
 import { dispatch } from "jest-circus/build/state";
+import { reducer as configWizardReducer } from "~/features/config-wizard/config-wizard.slice";
+import { scannerApi } from "~/features/config-wizard/scanner.slice";
+import {
+  dataQualifierApi,
+  reducer as dataQualifierReducer,
+} from "~/features/data-qualifier/data-qualifier.slice";
+import {
+  dataSubjectsApi,
+  reducer as dataSubjectsReducer,
+} from "~/features/data-subjects/data-subject.slice";
+import {
+  dataUseApi,
+  reducer as dataUseReducer,
+} from "~/features/data-use/data-use.slice";
+import { datasetApi, reducer as datasetReducer } from "~/features/dataset";
+import {
+  organizationApi,
+  reducer as organizationReducer,
+} from "~/features/organization";
+import { systemApi } from "~/features/system";
+import { reducer as taxonomyReducer, taxonomyApi } from "~/features/taxonomy";
+import { reducer as userReducer } from "~/features/user";
 
 const reducer = {
   [privacyRequestApi.reducerPath]: privacyRequestApi.reducer,
@@ -37,6 +61,22 @@ const reducer = {
   auth: authReducer,
   [connectionTypeApi.reducerPath]: connectionTypeApi.reducer,
   connectionType: connectionTypeReducer,
+  configWizard: configWizardReducer,
+  user: userReducer,
+  dataset: datasetReducer,
+  taxonomy: taxonomyReducer,
+  dataQualifier: dataQualifierReducer,
+  dataSubjects: dataSubjectsReducer,
+  dataUse: dataUseReducer,
+  organization: organizationReducer,
+  [datasetApi.reducerPath]: datasetApi.reducer,
+  [organizationApi.reducerPath]: organizationApi.reducer,
+  [scannerApi.reducerPath]: scannerApi.reducer,
+  [systemApi.reducerPath]: systemApi.reducer,
+  [taxonomyApi.reducerPath]: taxonomyApi.reducer,
+  [dataQualifierApi.reducerPath]: dataQualifierApi.reducer,
+  [dataSubjectsApi.reducerPath]: dataSubjectsApi.reducer,
+  [dataUseApi.reducerPath]: dataUseApi.reducer,
 };
 
 export type RootState = StateFromReducersMapObject<typeof reducer>;
@@ -51,7 +91,15 @@ export const makeStore = (preloadedState?: Partial<RootState>) =>
         userApi.middleware,
         authApi.middleware,
         datastoreConnectionApi.middleware,
-        connectionTypeApi.middleware
+        connectionTypeApi.middleware,
+        datasetApi.middleware,
+        organizationApi.middleware,
+        scannerApi.middleware,
+        systemApi.middleware,
+        taxonomyApi.middleware,
+        dataQualifierApi.middleware,
+        dataSubjectsApi.middleware,
+        dataUseApi.middleware
       ),
     devTools: true,
     preloadedState,
@@ -81,3 +129,4 @@ export type AppDispatch = AppStore["dispatch"];
 setupListeners(store.dispatch);
 
 export default store;
+// export const wrapper = createWrapper<AppStore>(makeStore);
