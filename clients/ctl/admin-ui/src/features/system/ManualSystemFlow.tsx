da47ca194077f3690d00058c4d0bffb19f96feef
Allison King <allisonjuliaking@gmail.com>
2022-09-12 15:32:20 -0400

    Add ConfigureSteps

--- clients/ctl/admin-ui/src/features/system/ManualSystemFlow.tsx
+++ clients/ctl/admin-ui/src/features/system/ManualSystemFlow.tsx
@@ -1,17 +1,54 @@
-import { Box, Grid, Text } from "@fidesui/react";
+import { Box, Button, Grid, Stack, Text } from "@fidesui/react";
+import { useState } from "react";
 
-const ManualSystemFlow = () => (
-  <Grid templateColumns="2fr 8fr">
-    <Box data-testid="settings">
-      <Text>Configuration Settings</Text>
-      <Box>
-        <Text>Describe</Text>
-        <Text>Declare</Text>
-        <Text>Review</Text>
-      </Box>
-    </Box>
-    <Box>just a regular form here</Box>
-  </Grid>
+const STEPS = ["Describe", "Declare", "Review"];
+
+interface ConfigureStepsProps {
+  steps: string[];
+  currentStepIndex: number;
+  onChange: (index: number) => void;
+}
+const ConfigureSteps = ({
+  steps,
+  currentStepIndex,
+  onChange,
+}: ConfigureStepsProps) => (
+  <Stack pl={5}>
+    {steps.map((step, idx) => {
+      const isActive = idx === currentStepIndex;
+      return (
+        <Button
+          key={step}
+          size="sm"
+          variant="ghost"
+          colorScheme={isActive ? "complimentary" : "ghost"}
+          width="fit-content"
+          disabled={idx > currentStepIndex}
+          onClick={() => onChange(idx)}
+        >
+          {step}
+        </Button>
+      );
+    })}
+  </Stack>
 );
+
+const ManualSystemFlow = () => {
+  const [currentStepIndex, setCurrentStepIndex] = useState(0);
+
+  return (
+    <Grid templateColumns="2fr 8fr">
+      <Stack spacing={3} fontWeight="semibold" data-testid="settings">
+        <Text>Configuration Settings</Text>
+        <ConfigureSteps
+          steps={STEPS}
+          currentStepIndex={currentStepIndex}
+          onChange={setCurrentStepIndex}
+        />
+      </Stack>
+      <Box>{STEPS[currentStepIndex]}</Box>
+    </Grid>
+  );
+};
 
 export default ManualSystemFlow;
