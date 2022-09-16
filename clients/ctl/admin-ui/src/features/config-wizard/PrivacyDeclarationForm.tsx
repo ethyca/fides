d78470aca7583e4af0aa609c89d5a83c38b3c6f6
Sebastian Sangervasi <2236777+ssangervasi@users.noreply.github.com>
2022-09-16 10:41:07 -0700

    ui/state: Deduplicate query results that were being stored in state slices (#1083)
    * ui/dedup-state: Systems unused
    
    * ui/dedup-state: Datasets unused
    
    * ui/dedup-state: Organization hydrate unused
    
    * ui/dedup-state: Taxonomy DataCategories query
    
    * ui/dedup-state: DataSubjects query
    
    * ui/dedup-state: DataQualifiers query
    
    * ui/dedup-state: DataUses query

--- clients/ctl/admin-ui/src/features/config-wizard/PrivacyDeclarationForm.tsx
+++ clients/ctl/admin-ui/src/features/config-wizard/PrivacyDeclarationForm.tsx
@@ -19,29 +19,25 @@ import {
 import { SerializedError } from "@reduxjs/toolkit";
 import { FetchBaseQueryError } from "@reduxjs/toolkit/dist/query/fetchBaseQuery";
 import { Form, Formik } from "formik";
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 
 import { useAppDispatch, useAppSelector } from "~/app/hooks";
 import { getErrorMessage, isErrorResult } from "~/features/common/helpers";
 import { AddIcon, QuestionIcon } from "~/features/common/Icon";
 import {
   selectDataQualifiers,
-  setDataQualifiers,
   useGetAllDataQualifiersQuery,
 } from "~/features/data-qualifier/data-qualifier.slice";
 import {
   selectDataSubjects,
-  setDataSubjects,
   useGetAllDataSubjectsQuery,
 } from "~/features/data-subjects/data-subject.slice";
 import {
   selectDataUses,
-  setDataUses,
   useGetAllDataUsesQuery,
 } from "~/features/data-use/data-use.slice";
 import {
   selectDataCategories,
-  setDataCategories,
   useGetAllDataCategoriesQuery,
 } from "~/features/taxonomy/taxonomy.slice";
 import { PrivacyDeclaration } from "~/types/api";
@@ -76,24 +72,16 @@ const PrivacyDeclarationForm = ({
   const [updateSystem] = useUpdateSystemMutation();
   const [isLoading, setIsLoading] = useState(false);
 
-  const { data: dataCategories } = useGetAllDataCategoriesQuery();
-  const { data: dataSubjects } = useGetAllDataSubjectsQuery();
-  const { data: dataQualifiers } = useGetAllDataQualifiersQuery();
-  const { data: dataUses } = useGetAllDataUsesQuery();
+  // Query subscriptions:
+  useGetAllDataCategoriesQuery();
+  useGetAllDataSubjectsQuery();
+  useGetAllDataQualifiersQuery();
+  useGetAllDataUsesQuery();
 
   const allDataCategories = useAppSelector(selectDataCategories);
   const allDataSubjects = useAppSelector(selectDataSubjects);
   const allDataUses = useAppSelector(selectDataUses);
   const allDataQualifiers = useAppSelector(selectDataQualifiers);
-
-  useEffect(() => {
-    dispatch(setDataCategories(dataCategories ?? []));
-    dispatch(setDataSubjects(dataSubjects ?? []));
-    dispatch(setDataUses(dataUses ?? []));
-    dispatch(setDataQualifiers(dataQualifiers ?? []));
-  }, [dispatch, dataCategories, dataSubjects, dataUses, dataQualifiers]);
-
-  useEffect(() => {}, [formDeclarations]);
 
   const initialValues = {
     data_categories: [],
