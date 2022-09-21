4319065750618a1cacb6865323bb963b173f8ee9
Thomas <thomas.lapiana+github@pm.me>
2022-09-21 13:27:17 -0700

    fix bug in ci check

--- noxfiles/ci_nox.py
+++ noxfiles/ci_nox.py
@@ -119,7 +119,7 @@ def check_install(session: nox.Session) -> None:
 def check_fides_annotations(session: nox.Session) -> None:
     """Run a fidesctl evaluation."""
     if session.posargs == ["docker"]:
-        run_command = (*RUN_STATIC_ANALYSIS, "fidesctl")
+        run_command = (*RUN_STATIC_ANALYSIS, "check_fides_annotations")
     else:
         run_command = ("fides", "--local", *(WITH_TEST_CONFIG), "evaluate")
     session.run(*run_command, external=True)
