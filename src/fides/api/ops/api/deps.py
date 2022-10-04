4c9791a2d78b80f8e93d42e1bba5a223fc1bd531
Adam Sachs <adam@Adams-MacBook-Pro.local>
2022-10-04 10:33:03 -0400

    Re-add SaaS template update script to startup hook
    Also update dependency getter that returns the shared db session directly
    to be importable from outside classes.

--- src/fides/api/ops/api/deps.py
+++ src/fides/api/ops/api/deps.py
@@ -20,7 +20,7 @@ def get_config() -> FidesConfig:
 def get_db() -> Generator:
     """Return our database session"""
     try:
-        db = _get_session()
+        db = get_api_session()
         yield db
     finally:
         db.close()
@@ -29,14 +29,14 @@ def get_db() -> Generator:
 def get_db_for_health_check() -> Generator:
     """Gets a database session regardless of whether the application db is disabled, for a health check."""
     try:
-        db = _get_session()
+        db = get_api_session()
         yield db
     finally:
         db.close()
 
 
-def _get_session() -> Session:
-    """Gets a database session"""
+def get_api_session() -> Session:
+    """Gets the shared database session to use for API functionality"""
     global _engine  # pylint: disable=W0603
     if not _engine:
         _engine = get_db_engine(config=CONFIG)
