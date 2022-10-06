f57280227a2a42774ad5c56fe6880003b76485ca
Thomas <thomas.lapiana+github@pm.me>
2022-10-06 23:03:47 +0800

    merge alembic heads

---
+++ src/fides/api/ctl/migrations/versions/fb6b0150d6e4_final_unified_ctl_merge.py
@@ -0,0 +1,24 @@
+"""final unified ctl merge
+
+Revision ID: fb6b0150d6e4
+Revises: 3d9c476d7cea, 6b287ff8cde4
+Create Date: 2022-10-06 14:56:13.267477
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'fb6b0150d6e4'
+down_revision = ('3d9c476d7cea', '6b287ff8cde4')
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
