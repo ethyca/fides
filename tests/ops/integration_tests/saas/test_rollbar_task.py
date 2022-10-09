e84620fb2fe56f2a65c4c3f97bbff3d908bee007
Adrian Galvan <adrian@ethyca.com>
2022-10-09 11:27:55 -0700

    Adding Rollbar erasure

--- tests/ops/integration_tests/saas/test_rollbar_task.py
+++ tests/ops/integration_tests/saas/test_rollbar_task.py
@@ -8,8 +8,11 @@ from fides.api.ops.models.privacy_request import PrivacyRequest
 from fides.api.ops.schemas.redis_cache import Identity
 from fides.api.ops.service.connectors import get_connector
 from fides.api.ops.task import graph_task
+from fides.api.ops.task.graph_task import get_cached_data_for_erasures
+from fides.ctl.core.config import get_config
 from tests.ops.graph.graph_test_util import assert_rows_match
 
+CONFIG = get_config()
 logger = logging.getLogger(__name__)
 
 
@@ -86,3 +89,94 @@ async def test_rollbar_access_request_task(
     # verify we only returned data for our identity email
     for instance in v[f"{dataset_name}:instances"]:
         assert instance["data"]["person"]["email"] == rollbar_identity_email
+
+
+@pytest.mark.integration_saas
+@pytest.mark.integration_rollbar
+@pytest.mark.asyncio
+async def test_rollbar_erasure_request_task(
+    db,
+    policy,
+    erasure_policy_string_rewrite,
+    rollbar_connection_config,
+    rollbar_dataset_config,
+    rollbar_erasure_identity_email,
+    rollbar_erasure_data,
+    rollbar_test_client,
+) -> None:
+    """Full erasure request based on the Rollbar SaaS config"""
+
+    privacy_request = PrivacyRequest(
+        id=f"test_rollbar_erasure_request_task_{random.randint(0, 1000)}"
+    )
+    identity_kwargs = {"email": rollbar_erasure_identity_email}
+
+    identity = Identity(**identity_kwargs)
+    privacy_request.cache_identity(identity)
+
+    dataset_name = rollbar_connection_config.get_saas_config().fides_key
+    merged_graph = rollbar_dataset_config.get_graph()
+    graph = DatasetGraph(merged_graph)
+    v = await graph_task.run_access_request(
+        privacy_request,
+        policy,
+        graph,
+        [rollbar_connection_config],
+        identity_kwargs,
+        db,
+    )
+
+    assert_rows_match(
+        v[f"{dataset_name}:projects"],
+        min_size=1,
+        keys=[
+            "id",
+            "account_id",
+            "name",
+        ],
+    )
+    assert_rows_match(
+        v[f"{dataset_name}:project_access_tokens"],
+        min_size=1,
+        keys=[
+            "project_id",
+            "name",
+            "access_token",
+        ],
+    )
+    assert_rows_match(
+        v[f"{dataset_name}:instances"],
+        min_size=1,
+        keys=[
+            "id",
+            "timestamp",
+            "data",
+        ],
+    )
+
+    # verify we only returned data for our identity email
+    for instance in v[f"{dataset_name}:instances"]:
+        assert instance["data"]["person"]["email"] == rollbar_erasure_identity_email
+
+    temp_masking = CONFIG.execution.masking_strict
+    CONFIG.execution.masking_strict = False
+
+    x = await graph_task.run_erasure(
+        privacy_request,
+        erasure_policy_string_rewrite,
+        graph,
+        [rollbar_connection_config],
+        identity_kwargs,
+        get_cached_data_for_erasures(privacy_request.id),
+        db,
+    )
+
+    # verify masking request was issued for endpoints with delete actions
+    assert x == {
+        f"{dataset_name}:projects": 0,
+        f"{dataset_name}:project_access_tokens": 0,
+        f"{dataset_name}:instances": 0,
+        f"{dataset_name}:people": 1,
+    }
+
+    CONFIG.execution.masking_strict = temp_masking
