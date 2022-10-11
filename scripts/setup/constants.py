a1537bbc210fa3f8517ba4576dcc965af10eaa5e
Sean Preston <sean@ethyca.com>
2022-10-10 20:36:45 -0400

    add keys for access and erasure policies

--- scripts/setup/constants.py
+++ scripts/setup/constants.py
@@ -3,6 +3,9 @@ from fides.api.ops.api.v1 import urn_registry as urls
 FIDESOPS_URL = "http://localhost:8080"
 BASE_URL = FIDESOPS_URL + urls.V1_URL_PREFIX
 
+ACCESS_POLICY_KEY = "access"
+ERASURE_POLICY_KEY = "erasure"
+
 # These are external datastores so don't read them from the config
 POSTGRES_SERVER = "host.docker.internal"
 # POSTGRES_SERVER = "postgres_example"
@@ -10,7 +13,6 @@ POSTGRES_USER = "postgres"
 POSTGRES_PASSWORD = "postgres"
 POSTGRES_PORT = 6432
 POSTGRES_DB_NAME = "postgres_example"
-
 
 ROOT_CLIENT_ID = "fidesopsadmin"
 ROOT_CLIENT_SECRET = "fidesopsadminsecret"
