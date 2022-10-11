467ffcf1ca7e68dbf69e4c83432f55637b5e35e2
Sean Preston <sean@ethyca.com>
2022-10-10 20:43:32 -0400

    method to verify subject identity

---
+++ scripts/setup/subject_identity_verification.py
@@ -0,0 +1,27 @@
+import logging
+import requests
+
+from fides.api.ops.api.v1 import urn_registry as urls
+
+import constants
+
+
+logger = logging.getLogger(__name__)
+logging.basicConfig(level=logging.INFO)
+
+
+def verify_subject_identity(
+    privacy_request_id: str,
+    code: str,
+):
+    verification_path = urls.PRIVACY_REQUEST_VERIFY_IDENTITY.format(
+        privacy_request_id=privacy_request_id
+    )
+    response = requests.post(
+        f"{constants.BASE_URL}{verification_path}",
+        json={"code": code},
+    )
+    if not response.ok:
+        raise RuntimeError(
+            f"fidesops privacy request verification failed! response.status_code={response.status_code}, response.json()={response.json()}"
+        )
