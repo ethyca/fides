cb908a4dcfeaa13bb4a459972d117fdf7a0dd494
Thomas <thomas.lapiana+github@pm.me>
2022-10-12 18:41:32 -0700

    Add docker version checks to Nox

--- noxfiles/utils_nox.py
+++ noxfiles/utils_nox.py
@@ -53,6 +53,35 @@ def teardown(session: nox.Session) -> None:
     else:
         session.run(*COMPOSE_DOWN, external=True)
     print("Teardown complete")
+    
+def docker_compose_version_is_valid() -> bool:
+    """Verify the Docker Compose version."""
+    required_docker_compose_version = "1.29.10"
+    raw = run("docker-compose --version", stdout=PIPE)
+    parsed = raw.stdout.decode("utf-8").rstrip("\n")
+    docker_compose_version = parsed.split("v")[-1]
+    click.echo(parsed)
+    version_is_valid = docker_compose_version > required_docker_compose_version
+    if not version_is_valid:
+        echo_red(
+            f"Docker Compose version is not compatible, please update to at least version {required_docker_compose_version}!"
+        )
+    return version_is_valid
+
+
+def docker_version_is_valid() -> bool:
+    """Verify the Docker version."""
+    required_docker_version = "20.10.8"
+    raw = run("docker --version", stdout=PIPE)
+    parsed = raw.stdout.decode("utf-8").rstrip("\n")
+    docker_version = parsed.split("v")[-1]
+    click.echo(parsed)
+    version_is_valid = docker_version > required_docker_version
+    if not version_is_valid:
+        echo_red(
+            f"Docker version is not compatible, please update to at least version {required_docker_version}!"
+        )
+    return version_is_valid
 
 
 def install_requirements(session: nox.Session) -> None:
