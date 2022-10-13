ab1981444eab838b0177c9858377ca05ce7509bd
Thomas <thomas.lapiana+github@pm.me>
2022-10-13 09:18:20 -0700

    Add docker version checks to Nox (#1429)
    * Add docker version checks to Nox
    
    * add sessions to check docker versions
    
    * add version checks to the dev session
    
    * run linters, changed required versions to global constants
    
    * change the version comparison to use int instead of str
    
    * Add shell=True to version commands
    
    Co-authored-by: Paul Sanders <pau@ethyca.com>

--- noxfiles/dev_nox.py
+++ noxfiles/dev_nox.py
@@ -4,11 +4,15 @@ import nox
 from constants_nox import COMPOSE_SERVICE_NAME, RUN, START_APP
 from docker_nox import build
 from run_infrastructure import ALL_DATASTORES, run_infrastructure
+from utils_nox import check_docker_compose_version, check_docker_version
 
 
 @nox.session()
 def dev(session: nox.Session) -> None:
     """Spin up the application. Uses positional arguments for additional features."""
+    check_docker_compose_version(session)
+    check_docker_version(session)
+
     build(session, "dev")
     session.notify("teardown")
 
