5eeaa16a90f9f438bf540b2fada9f92a4dd5bf91
Phil Salant <PSalant@gmail.com>
2022-10-14 20:21:00 -0600

    Wrap `px deploy` in `fides system_scanner deploy`
    Includes handling for arguments native to `px deploy`, including label
    validation and selective passthrough.

--- src/fides/cli/commands/system_scanner.py
+++ src/fides/cli/commands/system_scanner.py
@@ -1,9 +1,29 @@
 """Defines the CLI commands for the System Scanner"""
 
-from click import Context, group, pass_context
+from subprocess import CalledProcessError, run
+from typing import Tuple
 
-from ..options import dry_flag, verbose_flag
+from click import Context, echo, group, option, pass_context
+
+from ...ctl.core.utils import echo_red
+from ..options import dry_flag, verbose_flag, yes_flag
 from ..utils import with_analytics
+
+RESERVED_LABELS = [
+    "app",
+    "component",
+    "vizier-bootstrap",
+    "vizier-updater-dep",
+]
+
+
+def validate_labels(labels: Tuple) -> None:
+    for label in labels:
+        label_str = label.split("=")[0]
+        if label_str in RESERVED_LABELS:
+            echo(f"Supplied a reserved label string: {label_str}")
+            echo("See -h, --help output for a list of reserved label strings.")
+            raise ValueError
 
 
 @group(name="system_scanner")
@@ -16,14 +36,105 @@ def system_scanner(ctx: Context) -> None:
 
 @system_scanner.command(name="deploy")
 @pass_context
-@verbose_flag
+@option(
+    "--annotation",
+    "-a",
+    help=(
+        "Custom annotation to apply to the deployed resources, in key=value format. "
+        + "This option may be included multiple times; once for each desired annotation."
+    ),
+    multiple=True,
+)
+@option(
+    "--cluster-name",
+    "-c",
+    "cluster_name",
+    help="The name of your cluster. Otherwise, the name will be taken from the current kubeconfig.",
+    type=str,
+)
+@option(
+    "--deploy-olm",
+    "-o",
+    is_flag=True,
+    help=(
+        "Whether to deploy Operator Lifecycle Management (OLM). OLM is required. "
+        + "Omit this argument only if OLM has already been deployed on the cluster manually, or through another application. "
+        + "OLM is deployed by default on Openshift clusters."
+    ),
+)
 @dry_flag
+@option(
+    "--kubeconfig",
+    "-k",
+    "kubeconfig_path",
+    help="Absolute path to the kubeconfig file. [default: /root/.kube/config]",
+    show_default=True,
+    type=str,
+)
+@option(
+    "--label",
+    "-l",
+    help=(
+        "Custom label to apply to the deployed resources, in key=value format. "
+        + "This option may be included multiple times; once for each desired label. "
+        + f"The following labels are reserved for internal use, and may not be used: {', '.join(RESERVED_LABELS)}."
+    ),
+    multiple=True,
+)
+@option(
+    "--use-etcd-operator",
+    "-e",
+    is_flag=True,
+    help=(
+        "Whether to use the operator for etcd instead of the statefulset. "
+        + "Required for clusters that do not support persistent volumes."
+    ),
+)
+@verbose_flag
+@yes_flag
 @with_analytics
 def system_scanner_deploy(
     ctx: Context,
-    verbose: bool = False,
+    annotation: Tuple,
+    label: Tuple,
+    cluster_name: str = "",
+    deploy_olm: bool = False,
     dry: bool = False,
+    kubeconfig_path: str = "",
+    use_etcd_operator: bool = False,
+    verbose: bool = False,
+    yes: bool = False,
 ) -> None:
     """
-    Deploy the required pods to the configured Kubernetes cluster.
+    Deploy the required pods to a Kubernetes cluster.
     """
+
+    try:
+        annotations = ""
+        if len(annotation):
+            annotations = "--annotations=" + ",".join(annotation)
+
+        labels = ""
+        if len(label):
+            validate_labels(label)
+            labels = "--labels=" + ",".join(label)
+
+        args = [
+            annotations,
+            "--check_only" if dry else "",
+            f'--cluster_name="{cluster_name or "test-cluster"}"',  # TODO: Use kubecontext cluster name
+            "--deploy_olm" if deploy_olm else "",
+            f'--kubeconfig="{kubeconfig_path}"' if kubeconfig_path else "",
+            labels,
+            "--olm_namespace fides-olm",
+            "--olm_operator_namespace fides-operator",
+            "" if verbose else "--quiet",
+            "--use_etcd_operator" if use_etcd_operator else "",
+            "-y" if yes else "",
+        ]
+        deploy_cmd = ["px deploy"] + [arg for arg in args if arg]
+
+        run(deploy_cmd, check=True)
+    except (CalledProcessError, FileNotFoundError, ValueError):
+        echo_red("Pod deployment failed")
+        raise SystemExit(1)
