807f918c158706d7dfca60a411501407cf42f1c9
Kelsey Thomas <101993653+Kelsey-Ethyca@users.noreply.github.com>
2022-10-16 13:19:49 -0700

    add configWizardFlag to hide nav bar and send to 404 page

--- clients/admin-ui/src/pages/config-wizard/index.tsx
+++ clients/admin-ui/src/pages/config-wizard/index.tsx
@@ -1,5 +1,10 @@
 import type { NextPage } from "next";
 import { useState } from "react";
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+import { Flags } from "react-feature-flags";
+
+import Custom404 from "../404";
 
 import Header from "~/features/common/Header";
 import Layout from "~/features/common/Layout";
@@ -15,14 +20,26 @@ const ConfigWizard: NextPage = () => {
   };
 
   return !configWizardStep ? (
+    <Flags
+    authorizedFlags={["configWizardFlag"]}
+    renderOn={() => (
     <Layout title="Config Wizard" noPadding>
       <Setup wizardStep={handleWizardStep} />
     </Layout>
+    )}
+    renderOff={() => <Custom404 />}
+    />
   ) : (
-    <>
-      <Header />
-      <ConfigWizardWalkthrough />
-    </>
+    <Flags
+    authorizedFlags={["configWizardFlag"]}
+    renderOn={() => (
+      <>
+        <Header />
+        <ConfigWizardWalkthrough />
+      </>
+      )}
+    renderOff={() => <Custom404 />}
+    />
   );
 };
 
