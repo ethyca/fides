2aa2e801959c4f01e8bb78bc4b1d348c45aa012f
Thomas <thomas.lapiana+github@pm.me>
2022-10-17 17:50:16 +0800

    update check logic

--- noxfile.py
+++ noxfile.py
@@ -5,6 +5,7 @@ To learn more about nox, visit https://nox.thea.codes/en/stable/index.html
 """
 import sys
 from subprocess import PIPE, run
+from typing import List
 
 import nox
 
@@ -22,19 +23,66 @@ nox.options.sessions = []
 nox.options.reuse_existing_virtualenvs = True
 
 
+def convert_semver_to_list(semver: str) -> List[int]:
+    """
+    Convert a standard semver string to a list of ints
+
+    '2.10.7' -> [2,10,7]
+    """
+    return [int(x) for x in semver.split(".")]
+
+
+def compare_semvers(version_a: List[int], version_b: List[int]) -> bool:
+    """
+    Determine which semver-style list of integers is larger.
+
+    [2,10,3], [2,10,2] -> True
+    [3,1,3], [2,10,2] -> True
+    [2,10,2], [2,10,2] -> True
+    [1,1,3], [2,10,2] -> False
+    """
+
+    major, minor, patch = [0, 1, 2]
+    # Compare Major Versions
+    if version_a[major] > version_b[major]:
+        return True
+    if version_a[major] < version_b[major]:
+        return False
+
+    # If Major Versions Match, Compare Minor Versions
+    if version_a[minor] > version_b[minor]:
+        return True
+    if version_a[minor] < version_b[minor]:
+        return False
+
+    # If Both Major & Minor Versions Match, Compare Patch Versions
+    if version_a[patch] < version_b[patch]:
+        return False
+
+    return True
+
+
 def check_docker_version() -> bool:
     """Verify the Docker version."""
     raw = run("docker --version", stdout=PIPE, check=True, shell=True)
     parsed = raw.stdout.decode("utf-8").rstrip("\n")
+    docker_version = parsed.split("version ")[-1].split(",")[0]
     print(parsed)
 
-    docker_version = parsed.split("version ")[-1].split(",")[0]
-    version_is_valid = int(docker_version.replace(".", "")) >= int(
-        REQUIRED_DOCKER_VERSION.replace(".", "")
+    split_docker_version = convert_semver_to_list(docker_version)
+    split_required_docker_version = convert_semver_to_list(REQUIRED_DOCKER_VERSION)
+
+    if len(split_docker_version) != 3:
+        raise SystemExit(
+            "Error: Docker version format is invalid, expecting semver format. Please upgrade to a more recent version and try again"
+        )
+
+    version_is_valid = compare_semvers(
+        split_docker_version, split_required_docker_version
     )
     if not version_is_valid:
         raise SystemExit(
-            f"Your Docker version (v{docker_version}) is not compatible, please update to at least version {REQUIRED_DOCKER_VERSION}!"
+            f"Error: Your Docker version (v{docker_version}) is not compatible, please update to at least version {REQUIRED_DOCKER_VERSION}!"
         )
     return version_is_valid
 
