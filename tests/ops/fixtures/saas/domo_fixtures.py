9e9e31a7947682ba9c5b0bb15ffa2776efe16589
Zeeshan <zmuhammad@onbench.com>
2022-10-18 22:33:42 +0500

    Square Access APIs ref: 11099-domo-access
    # Conflicts:
    #	pyproject.toml
    #	tests/ops/conftest.py

---
+++ tests/ops/fixtures/saas/domo_fixtures.py
@@ -0,0 +1,118 @@
+from typing import Any, Dict, Generator
+
+import pydash
+import pytest
+import requests
+from faker import Faker
+from fideslib.cryptography import cryptographic_util
+from fideslib.db import session
+from requests.auth import HTTPBasicAuth
+from sqlalchemy.orm import Session
+
+from fides.api.ops.models.connectionconfig import (
+    AccessLevel,
+    ConnectionConfig,
+    ConnectionType,
+)
+from fides.api.ops.models.datasetconfig import DatasetConfig
+from fides.api.ops.util.saas_util import (
+    load_config_with_replacement,
+    load_dataset_with_replacement,
+)
+from tests.ops.test_helpers.vault_client import get_secrets
+
+secrets = get_secrets("domo")
+faker = Faker()
+
+
+@pytest.fixture(scope="session")
+def domo_secrets(saas_config):
+    return {
+        "domain": pydash.get(saas_config, "domo.domain") or secrets["domain"],
+        "client_id": pydash.get(saas_config, "domo.client_id") or secrets["client_id"],
+        "client_secret": pydash.get(saas_config, "domo.client_secret")
+        or secrets["client_secret"],
+    }
+
+
+@pytest.fixture(scope="session")
+def domo_identity_email(saas_config):
+    return pydash.get(saas_config, "domo.identity_email") or secrets["identity_email"]
+
+
+@pytest.fixture(scope="session")
+def domo_erasure_identity_email():
+    return f"{cryptographic_util.generate_secure_random_string(13)}@email.com"
+
+
+@pytest.fixture(scope="session")
+def domo_token(domo_secrets) -> str:
+    body = {"grant_type": "client_credentials"}
+    basic_auth = HTTPBasicAuth(domo_secrets["client_id"], domo_secrets["client_secret"])
+    url = f"https://{domo_secrets['domain']}/oauth/token"
+    response = requests.post(url, body, auth=basic_auth)
+    return response.json()["access_token"]
+
+
+@pytest.fixture
+def domo_config() -> Dict[str, Any]:
+    return load_config_with_replacement(
+        "data/saas/config/domo_config.yml",
+        "<instance_fides_key>",
+        "domo_instance",
+    )
+
+
+@pytest.fixture
+def domo_dataset() -> Dict[str, Any]:
+    return load_dataset_with_replacement(
+        "data/saas/dataset/domo_dataset.yml",
+        "<instance_fides_key>",
+        "domo_dataset",
+    )[0]
+
+
+@pytest.fixture(scope="function")
+def domo_connection_config(
+    db: session,
+    domo_config,
+    domo_secrets,
+    domo_token,
+) -> Generator:
+    fides_key = domo_config["fides_key"]
+    domo_secrets["access_token"] = domo_token
+    connection_config = ConnectionConfig.create(
+        db=db,
+        data={
+            "key": fides_key,
+            "name": fides_key,
+            "connection_type": ConnectionType.saas,
+            "access": AccessLevel.write,
+            "secrets": domo_secrets,
+            "saas_config": domo_config,
+        },
+    )
+    yield connection_config
+    connection_config.delete(db)
+
+
+@pytest.fixture
+def domo_dataset_config(
+    db: Session,
+    domo_connection_config: ConnectionConfig,
+    domo_dataset: Dict[str, Any],
+) -> Generator:
+    fides_key = domo_dataset["fides_key"]
+    domo_connection_config.name = fides_key
+    domo_connection_config.key = fides_key
+    domo_connection_config.save(db=db)
+    dataset = DatasetConfig.create(
+        db=db,
+        data={
+            "connection_config_id": domo_connection_config.id,
+            "fides_key": fides_key,
+            "dataset": domo_dataset,
+        },
+    )
+    yield dataset
+    dataset.delete(db=db)
---
+++ tests/ops/integration_tests/saas/test_domo_task.py
@@ -0,0 +1,74 @@
+import random
+
+import pytest
+
+from fides.api.ops.graph.graph import DatasetGraph
+from fides.api.ops.models.privacy_request import PrivacyRequest
+from fides.api.ops.schemas.redis_cache import Identity
+from fides.api.ops.service.connectors import get_connector
+from fides.api.ops.task import graph_task
+from fides.ctl.core.config import get_config
+from tests.ops.graph.graph_test_util import assert_rows_match
+
+CONFIG = get_config()
+
+
+@pytest.mark.skip(reason="Currently unable to test OAuth2 connectors")
+@pytest.mark.integration_saas
+@pytest.mark.integration_domo
+def test_domo_connection_test(domo_connection_config) -> None:
+    get_connector(domo_connection_config).test_connection()
+
+
+# @pytest.mark.skip(reason="Currently unable to test OAuth2 connectors")
+@pytest.mark.integration_saas
+@pytest.mark.integration_domo
+@pytest.mark.asyncio
+async def test_domo_access_request_task(
+    policy,
+    domo_identity_email,
+    domo_connection_config,
+    domo_dataset_config,
+    db,
+) -> None:
+    """Full access request based on the Domo SaaS config"""
+
+    privacy_request = PrivacyRequest(
+        id=f"test_domo_access_request_task_{random.randint(0, 1000)}"
+    )
+    identity_kwargs = {"email": domo_identity_email}
+    identity = Identity(**identity_kwargs)
+    privacy_request.cache_identity(identity)
+
+    dataset_name = domo_connection_config.get_saas_config().fides_key
+    merged_graph = domo_dataset_config.get_graph()
+    graph = DatasetGraph(merged_graph)
+
+    v = await graph_task.run_access_request(
+        privacy_request,
+        policy,
+        graph,
+        [domo_connection_config],
+        identity_kwargs,
+        db,
+    )
+
+    assert_rows_match(
+        v[f"{dataset_name}:users"],
+        min_size=1,
+        keys=[
+            "id",
+            "title",
+            "email",
+            "role",
+            "name",
+            "timezone",
+            "department",
+            "roleId",
+            "createdAt",
+            "updatedAt",
+        ],
+    )
+
+    # verify we only returned data for our identity
+    assert v[f"{dataset_name}:users"][0]["email"] == domo_identity_email
