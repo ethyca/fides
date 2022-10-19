0dd706ba7d0bf74d8a1a6e01d3f056127cc6bfaf
Thomas <thomas.lapiana+github@pm.me>
2022-10-19 14:32:06 +0800

    check for an empty manifest list when parsing

--- src/fides/ctl/core/parse.py
+++ src/fides/ctl/core/parse.py
@@ -3,7 +3,7 @@ from fideslang import Taxonomy
 from fideslang.manifests import ingest_manifests
 from fideslang.parse import load_manifests_into_taxonomy
 
-from fides.ctl.core.utils import echo_green
+from fides.ctl.core.utils import echo_green, get_manifest_list
 
 
 def parse(manifests_dir: str) -> Taxonomy:
@@ -13,6 +13,9 @@ def parse(manifests_dir: str) -> Taxonomy:
 
     # Check if any manifests exist before trying to parse them
     print(f"Loading resource manifests from: {manifests_dir}")
+    if not get_manifest_list(manifests_dir):
+        print("No manifests found to parse, skipping...")
+        return Taxonomy()
     ingested_manifests = ingest_manifests(manifests_dir)
     taxonomy = load_manifests_into_taxonomy(ingested_manifests)
     echo_green("Taxonomy successfully created.")
