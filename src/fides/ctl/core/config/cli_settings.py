3436b52f47bf5b8c47f51634b8496a057f9f4c49
Thomas <thomas.lapiana+github@pm.me>
2022-10-25 17:28:36 +0800

    set the default server port to 8080

--- src/fides/ctl/core/config/cli_settings.py
+++ src/fides/ctl/core/config/cli_settings.py
@@ -18,7 +18,7 @@ class CLISettings(FidesSettings):
 
     server_protocol: str = "http"
     server_host: str = "localhost"
-    server_port: Optional[int]
+    server_port: str = "8080"
     server_url: Optional[AnyHttpUrl]
 
     @validator("server_url", always=True)
@@ -26,7 +26,7 @@ class CLISettings(FidesSettings):
     def get_server_url(cls, value: str, values: Dict) -> str:
         "Create the server_url."
         host = values["server_host"]
-        port = values["server_port"]
+        port = int(values["server_port"])
         protocol = values["server_protocol"]
 
         server_url = "{}://{}{}".format(
