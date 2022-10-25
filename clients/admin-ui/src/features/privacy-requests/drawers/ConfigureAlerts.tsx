3fb26718e237827baff34e387a391a6e1cb31f98
chriscalhoun1974 <68459950+chriscalhoun1974@users.noreply.github.com>
2022-10-24 21:13:30 -0400

    Added More button and Configure Alerts drawer

---
+++ clients/admin-ui/src/features/privacy-requests/drawers/ConfigureAlerts.tsx
@@ -0,0 +1,139 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import {
+  Box,
+  Button,
+  ButtonGroup,
+  Drawer,
+  DrawerBody,
+  DrawerCloseButton,
+  DrawerContent,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerOverlay,
+  FormControl,
+  FormLabel,
+  HStack,
+  MenuItem,
+  Switch,
+  Text,
+  useDisclosure,
+  VStack,
+} from "@fidesui/react";
+import { Form, Formik, FormikProps } from "formik";
+import { ChangeEvent, useRef } from "react";
+import * as Yup from "yup";
+
+type ConfigureAlertsProps = {};
+
+// eslint-disable-next-line no-empty-pattern
+const ConfigureAlerts: React.FC<ConfigureAlertsProps> = ({}) => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const firstField = useRef();
+
+  const handleSubmit = async (values: any, _actions: any) => {
+    onClose();
+  };
+
+  const handleToggle = (event: ChangeEvent<HTMLInputElement>) => {
+
+  }
+    
+
+  return (
+    <>
+      <MenuItem
+        onClick={onOpen}
+        _focus={{ bg: "gray.100", color: "complimentary.500" }}
+      >
+        Configure alerts
+      </MenuItem>
+      <Formik
+        enableReinitialize
+        initialValues={{}}
+        onSubmit={handleSubmit}
+        validateOnBlur={false}
+        validateOnChange={false}
+        validationSchema={Yup.object().shape({})}
+      >
+        {/* @ts-ignore */}
+        {(_props: FormikProps<Values>) => (
+          <Drawer
+            isOpen={isOpen}
+            placement="right"
+            // @ts-ignore
+            initialFocusRef={firstField}
+            onClose={onClose}
+            size="lg"
+          >
+            <DrawerOverlay />
+            <DrawerContent color="gray.700">
+              <DrawerCloseButton />
+              <DrawerHeader color="gray.900">
+                <Text fontSize="2xl" fontWeight="normal" mb={4}>
+                  Configure alerts and notifications
+                </Text>
+                <Box mt="26px">
+                  <Text fontSize="md" fontWeight="normal">
+                    Setup your alerts to send you a notification when there are
+                    any processing failures. You can also setup a threshold for
+                    connector failures for Fides to notify you after X amount of
+                    failures have occurred.
+                  </Text>
+                </Box>
+              </DrawerHeader>
+              <DrawerBody mt="20px">
+                <Form id="configure-alerts-form" noValidate>
+                  <Text fontSize="md">Contact details</Text>
+                  <VStack align="stretch" gap="16px">
+                    <HStack>
+                      <FormControl display="flex" alignItems="center" mt="45px">
+                        <FormLabel fontSize="md" mb="0">
+                          Notify me immediately if there are any DSR processing
+                          errors
+                        </FormLabel>
+                        <Switch
+                          colorScheme="secondary"
+                          onChange={handleToggle}
+                        />
+                      </FormControl>
+                    </HStack>
+                  </VStack>
+                  <Text color="#757575" fontSize="sm" mt="11px">
+                    If selected, then Fides will notify you by your chosen
+                    method of communication every time the system encounters a
+                    data subject request processing error. You can turn this off
+                    anytime and setup a more suitable notification method below
+                    if you wish.
+                  </Text>
+                </Form>
+              </DrawerBody>
+              <DrawerFooter justifyContent="flex-start">
+                <ButtonGroup size="sm" spacing="8px" variant="outline">
+                  <Button onClick={onClose} variant="outline">
+                    Cancel
+                  </Button>
+                  <Button
+                    bg="primary.800"
+                    color="white"
+                    form="configure-alerts-form"
+                    // isLoading={isSubmitting}
+                    loadingText="Submitting"
+                    size="sm"
+                    variant="solid"
+                    type="submit"
+                    _active={{ bg: "primary.500" }}
+                    _hover={{ bg: "primary.400" }}
+                  >
+                    Save
+                  </Button>
+                </ButtonGroup>
+              </DrawerFooter>
+            </DrawerContent>
+          </Drawer>
+        )}
+      </Formik>
+    </>
+  );
+};
+
+export default ConfigureAlerts;
--- clients/admin-ui/src/pages/index.tsx
+++ clients/admin-ui/src/pages/index.tsx
@@ -1,12 +1,17 @@
 import { Flex, Heading, Spacer } from "@fidesui/react";
 import type { NextPage } from "next";
+import dynamic from "next/dynamic";
 import RequestFilters from "privacy-requests/RequestFilters";
 import RequestTable from "privacy-requests/RequestTable";
 
 import { LOGIN_ROUTE } from "~/constants";
 import ProtectedRoute from "~/features/auth/ProtectedRoute";
 import Layout from "~/features/common/Layout";
-import ActionButtons from "~/features/privacy-requests/buttons/ActionButtons";
+
+const ActionButtons = dynamic(
+  () => import("~/features/privacy-requests/buttons/ActionButtons"),
+  { loading: () => <div>Loading...</div> }
+);
 
 const Home: NextPage = () => (
   <ProtectedRoute redirectUrl={LOGIN_ROUTE}>
