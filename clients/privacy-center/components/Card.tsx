716a6cea4385e4ce583330c0e31dd203ca7ac8d0
Neville Samuell <neville@ethyca.com>
2022-10-28 16:06:50 -0400

    Fix formatting

--- clients/privacy-center/components/Card.tsx
+++ clients/privacy-center/components/Card.tsx
@@ -30,11 +30,13 @@ const Card: React.FC<CardProps> = ({
     userSelect="none"
     m={2}
     _hover={{
-      boxShadow: "0px 20px 25px -5px rgba(0, 0, 0, 0.1), 0px 10px 10px -5px rgba(0, 0, 0, 0.04)",
+      boxShadow:
+        "0px 20px 25px -5px rgba(0, 0, 0, 0.1), 0px 10px 10px -5px rgba(0, 0, 0, 0.04)",
     }}
     _focus={{
       outline: "none",
-      boxShadow: "0px 20px 25px -5px rgba(0, 0, 0, 0.1), 0px 10px 10px -5px rgba(0, 0, 0, 0.04)",
+      boxShadow:
+        "0px 20px 25px -5px rgba(0, 0, 0, 0.1), 0px 10px 10px -5px rgba(0, 0, 0, 0.04)",
     }}
     onClick={() => {
       onClick();
