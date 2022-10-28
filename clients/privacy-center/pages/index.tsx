30c8d8cc8fcfdd2094ce56e36ff5239ad3a7580e
Neville Samuell <neville@ethyca.com>
2022-10-28 15:33:57 -0400

    Minor styling, use neutral borders & shadows

--- clients/privacy-center/pages/index.tsx
+++ clients/privacy-center/pages/index.tsx
@@ -116,6 +116,7 @@ const Home: NextPage = () => {
         >
           <Image
             src={config.logo_path}
+            margin="8px"
             height="56px"
             width="304px"
             alt="Logo"
