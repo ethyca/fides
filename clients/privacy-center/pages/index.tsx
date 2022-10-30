19331a647dffbdd217789ce73c7bd496ee119e3e
Neville Samuell <neville@ethyca.com>
2022-10-30 11:31:17 -0400

    Fix formatting on privacy center

--- clients/privacy-center/pages/index.tsx
+++ clients/privacy-center/pages/index.tsx
@@ -114,12 +114,7 @@ const Home: NextPage = () => {
           justifyContent="center"
           alignItems="center"
         >
-          <Image
-            src={config.logo_path}
-            margin="8px"
-            height="68px"
-            alt="Logo"
-          />
+          <Image src={config.logo_path} margin="8px" height="68px" alt="Logo" />
         </Flex>
       </header>
 
