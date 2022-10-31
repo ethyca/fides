0e0972534efcb603eb148ddafa5f6631933bdef0
chriscalhoun1974 <68459950+chriscalhoun1974@users.noreply.github.com>
2022-10-31 19:11:56 -0400

    Added configureAlertsFlag feature flag

--- clients/admin-ui/src/pages/index.tsx
+++ clients/admin-ui/src/pages/index.tsx
@@ -3,6 +3,9 @@ import type { NextPage } from "next";
 import dynamic from "next/dynamic";
 import RequestFilters from "privacy-requests/RequestFilters";
 import RequestTable from "privacy-requests/RequestTable";
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+import { Flags } from "react-feature-flags";
 
 import { LOGIN_ROUTE } from "~/constants";
 import ProtectedRoute from "~/features/auth/ProtectedRoute";
@@ -21,7 +24,10 @@ const Home: NextPage = () => (
           Privacy Requests
         </Heading>
         <Spacer />
-        <ActionButtons />
+        <Flags
+          authorizedFlags={["configureAlertsFlag"]}
+          renderOn={() => <ActionButtons />}
+        />
       </Flex>
       <RequestFilters />
       <RequestTable />
