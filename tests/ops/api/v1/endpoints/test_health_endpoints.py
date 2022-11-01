4055cda380c340ea63792f5379cea6d7e1cf498e
Sean Preston <seanmpreston@gmail.com>
2022-11-01 14:50:03 -0400

    [#1587] Adds connected worker info. to the healthcheck (#1588)
    * adds connected worker info. to the healthcheck
    
    * adds type hint
    
    * move the worker back into the main compose file and add a dev command to spin it up
    
    * updates CHANGELOG
    
    * makes healthcheck response consistent when workers are connected or not
    
    * update typecheck to be as healthcheck expects
    
    * update the health endpoint test
    
    * static checks
    
    * update the health response to include worker enablement and list of UUIDs
    
    * add exception logging
    
    * switch celery eager default back to true
    
    Co-authored-by: Thomas <thomas.lapiana+github@pm.me>

--- tests/ops/api/v1/endpoints/test_health_endpoints.py
+++ tests/ops/api/v1/endpoints/test_health_endpoints.py
@@ -7,7 +7,12 @@ from fides.api.ops.api.v1.urn_registry import HEALTH
 def test_health(api_client: TestClient) -> None:
     response = api_client.get(HEALTH)
     json = response.json()
-    assert json["webserver"] == "healthy"
-    assert json["database"] == "healthy"
-    assert json["cache"] == "healthy"
-    assert json["version"] == str(fides.__version__)
+    expected_response = {
+        "cache": "healthy",
+        "webserver": "healthy",
+        "database": "healthy",
+        "version": str(fides.__version__),
+        "workers_enabled": "False",
+        "workers": [],
+    }
+    assert json == expected_response
