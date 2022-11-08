1709d3c84ae0c2ef39673f670d00e84993931547
Allison King <allisonjuliaking@gmail.com>
2022-11-08 14:54:36 -0500

    Store the method of adding systems to redux

--- clients/admin-ui/src/features/config-wizard/AuthenticateScanner.tsx
+++ clients/admin-ui/src/features/config-wizard/AuthenticateScanner.tsx
@@ -1,26 +1,27 @@
 import React from "react";
 
+import { useAppSelector } from "~/app/hooks";
 import { ValidTargets } from "~/types/api";
 
 import AuthenticateAwsForm from "./AuthenticateAwsForm";
 import AuthenticateRuntimeForm from "./AuthenticateRuntimeForm";
-import { AddSystemMethods, SystemMethods } from "./types";
+import { selectAddSystemsMethod } from "./config-wizard.slice";
+import { SystemMethods } from "./types";
 
 // TODO(#577)
 const AuthenticateOktaForm = () => null;
 
-interface Props {
-  infrastructure?: AddSystemMethods;
-}
-
-const AuthenticateScanner = ({ infrastructure = ValidTargets.AWS }: Props) => (
-  <>
-    {infrastructure === ValidTargets.AWS ? <AuthenticateAwsForm /> : null}
-    {infrastructure === ValidTargets.OKTA ? <AuthenticateOktaForm /> : null}
-    {infrastructure === SystemMethods.RUNTIME ? (
-      <AuthenticateRuntimeForm />
-    ) : null}
-  </>
-);
+const AuthenticateScanner = () => {
+  const infrastructure = useAppSelector(selectAddSystemsMethod);
+  return (
+    <>
+      {infrastructure === ValidTargets.AWS ? <AuthenticateAwsForm /> : null}
+      {infrastructure === ValidTargets.OKTA ? <AuthenticateOktaForm /> : null}
+      {infrastructure === SystemMethods.RUNTIME ? (
+        <AuthenticateRuntimeForm />
+      ) : null}
+    </>
+  );
+};
 
 export default AuthenticateScanner;
