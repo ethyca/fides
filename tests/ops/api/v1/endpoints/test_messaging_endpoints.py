7db4d838d506d60922a96e530c5cc1b17c30733d
eastandwestwind <eastandwestwind@gmail.com>
2022-11-09 15:49:59 -0600

    uppercase err text in test

--- tests/ops/api/v1/endpoints/test_messaging_endpoints.py
+++ tests/ops/api/v1/endpoints/test_messaging_endpoints.py
@@ -101,7 +101,7 @@ class TestPostMessagingConfig:
         assert 422 == response.status_code
         assert (
             json.loads(response.text)["detail"][0]["msg"]
-            == "value is not a valid enumeration member; permitted: 'mailgun', 'twilio_text', 'twilio_email'"
+            == "value is not a valid enumeration member; permitted: 'MAILGUN', 'TWILIO_TEXT', 'TWILIO_EMAIL'"
         )
 
     def test_post_email_config_with_no_key(
