5e0c8dc61a62f057944d04fedb4d22c120585cec
Paul Sanders <pau@ethyca.com>
2022-11-11 10:30:55 -0500

    Add minimial config test to CI

--- noxfiles/ci_nox.py
+++ noxfiles/ci_nox.py
@@ -1,5 +1,9 @@
 """Contains the nox sessions used during CI checks."""
+import sys
+from time import sleep
+
 import nox
+import requests
 
 from constants_nox import (
     CI_ARGS,
@@ -149,8 +153,28 @@ def minimal_config_startup(session: nox.Session) -> None:
         "-d",
         IMAGE_NAME,
     )
-    print(f"{start_command=}")
     session.run(*start_command, external=True)
+    timeout = 120
+    wait_time = 2
+    elapsed_time = 0
+    status_code = 0
+
+    while status_code != 200 and elapsed_time < timeout:
+        response = None
+        try:
+            response = requests.get("http://localhost:8080/health")
+        except Exception:  # requests.exceptions.ConnectionError:
+            pass
+
+        if response:
+            status_code = response.status_code
+
+        if status_code != 200:
+            sleep(wait_time)
+            elapsed_time += wait_time
+
+    if status_code != 200:
+        raise ValueError("Startup failed")
 
 
 # Pytest
