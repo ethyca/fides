a2c52809a2c150df4a643968b07842234873c839
Paul Sanders <paul@ethyca.com>
2022-11-15 08:54:58 -0500

    Fix security config race condition (#1718)
    * Fix security config race condition
    
    * Fix pylint error
    
    * Refactor to allow loading security settings earlier
    
    * Use local database settings
    
    * Pre-load database settings
    
    * Desperate attempt at debugging CI
    
    * Remove unsuccessful debugging attempt
    
    * Bump pydantic to see if it will fix CI issue
    
    * More CI debugging
    
    * Fix python version in debugging
    
    * Fix typo
    
    * Isolate docker build
    
    * Remove debugging
    
    * Pull more fideslib code directly in
    
    * Fix install error
    
    * Add missing argument
    
    * Clear pydantic validator cache
    
    * Move cache clearing
    
    * Add required security settings to test file
    
    * Add environment vairables to docs
    
    * Fix docs environment vairables
    
    * Update CHANGELOG
    
    * init the security settings if they don't exist in the toml
    
    * Add minimal config test
    
    * Remove commented code
    
    * Add minimial config test to CI
    
    * Add requests to pip install
    
    * Add required dependency
    
    * Fix path
    
    * Change test to use docker compose wait
    
    * Remove unused import
    
    * Rename test step
    
    * code comment cleanup
    
    * refactor the new minimal config check to use the existing matrix pattern
    
    * remove the default security settings from the toml
    
    * get the config loading with partial env definition
    
    * fix the docs check
    
    * fix some of the config tests
    
    * fix the remaining config test failures
    
    * fix mypy errors
    
    * remove the explicit security setting field from the config file
    
    * fix "check_install", skip validation for the initial security settings init
    
    * re-add the required vars to the toml, since it is being docker ignored
    
    Co-authored-by: Paul Sanders <pau@ethyca.com>
    Co-authored-by: Thomas <thomas.lapiana+github@pm.me>

--- src/fides/ctl/core/config/security_settings.py
+++ src/fides/ctl/core/config/security_settings.py
@@ -1,21 +1,97 @@
 """This module handles finding and parsing fides configuration files."""
 
 # pylint: disable=C0115,C0116, E0213
-from typing import List, Optional
+from typing import Dict, List, Optional, Tuple, Union
 
-from fideslib.core.config import SecuritySettings as FideslibSecuritySettings
+import validators
+from fideslib.core.config import FidesSettings
+from fideslib.cryptography.cryptographic_util import generate_salt, hash_with_salt
+from fideslib.exceptions import MissingConfig
+from pydantic import validator
 
 from fides.api.ops.api.v1.scope_registry import SCOPE_REGISTRY
 
 ENV_PREFIX = "FIDES__SECURITY__"
 
 
-class SecuritySettings(FideslibSecuritySettings):
+class SecuritySettings(FidesSettings):
     """Configuration settings for Security variables."""
 
     root_user_scopes: Optional[List[str]] = SCOPE_REGISTRY
     subject_request_download_link_ttl_seconds: int = 432000  # 5 days
+    aes_encryption_key_length: int = 16
+    aes_gcm_nonce_length: int = 12
+    app_encryption_key: str
+    drp_jwt_secret: Optional[str] = None
+    root_username: Optional[str] = None
+    root_password: Optional[str] = None
     identity_verification_attempt_limit: int = 3  # 3 attempts
+    encoding: str = "UTF-8"
+
+    cors_origins: List[str] = []
+    oauth_root_client_id: str
+    oauth_root_client_secret: str
+    oauth_root_client_secret_hash: Optional[Tuple]
+    oauth_access_token_expire_minutes: int = 60 * 24 * 8
+    oauth_client_id_length_bytes = 16
+    oauth_client_secret_length_bytes = 16
+
+    @validator("app_encryption_key")
+    @classmethod
+    def validate_encryption_key_length(
+        cls, v: Optional[str], values: Dict[str, str]
+    ) -> Optional[str]:
+        """Validate the encryption key is exactly 32 characters"""
+        if v is None or len(v.encode(values.get("encoding", "UTF-8"))) != 32:
+            raise ValueError(
+                "APP_ENCRYPTION_KEY value must be exactly 32 characters long"
+            )
+        return v
+
+    @validator("cors_origins", pre=True)
+    @classmethod
+    def assemble_cors_origins(cls, v: Union[str, List[str]]) -> Union[List[str], str]:
+        """Return a list of valid origins for CORS requests"""
+
+        def validate(values: List[str]) -> None:
+            for value in values:
+                if value != "*":
+                    if not validators.url(value):
+                        raise ValueError(f"{value} is not a valid url")
+
+        if isinstance(v, str) and not v.startswith("["):
+            values = [i.strip() for i in v.split(",")]
+            validate(values)
+
+            return values
+        if isinstance(v, (list, str)):
+            validate(v)  # type: ignore
+
+            return v
+        raise ValueError(v)
+
+    @validator("oauth_root_client_secret_hash")
+    @classmethod
+    def assemble_root_access_token(
+        cls, v: Optional[str], values: Dict[str, str]
+    ) -> Tuple:
+        """
+        Sets a hashed value of the root access key.
+        This is hashed as it is not wise to return a plaintext for of the
+        root credential anywhere in the system.
+        """
+        value = values.get("oauth_root_client_secret")
+        if not value:
+            raise MissingConfig(
+                "oauth_root_client_secret is required", SecuritySettings
+            )
+
+        encoding = values.get("encoding", "UTF-8")
+
+        salt = generate_salt()
+        hashed_client_id = hash_with_salt(value.encode(encoding), salt.encode(encoding))
+        oauth_root_client_secret_hash = (hashed_client_id, salt.encode(encoding))  # type: ignore
+        return oauth_root_client_secret_hash
 
     class Config:
         env_prefix = ENV_PREFIX
