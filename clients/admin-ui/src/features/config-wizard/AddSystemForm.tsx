67bf33d85577dd54ef3beb7736efd70f2d4dd63b
Sean Preston <sean@ethyca.com>
2022-11-21 11:44:20 -0500

    run prettier

--- clients/admin-ui/src/features/config-wizard/AddSystemForm.tsx
+++ clients/admin-ui/src/features/config-wizard/AddSystemForm.tsx
@@ -44,10 +44,10 @@ const AddSystemForm = () => {
               <>
                 <h2>
                   Systems describe any services that store or process data for
-                  your organization, including third-party APIs, web applications,
-                  databases, and data warehouses. System discovery allows Fides to
-                  identify and build a data map from the list of systems that exist
-                  within your organization.
+                  your organization, including third-party APIs, web
+                  applications, databases, and data warehouses. System discovery
+                  allows Fides to identify and build a data map from the list of
+                  systems that exist within your organization.
                   <AccordionButton
                     display="inline"
                     padding="0px"
