a64f4f936a938b921ddf82802a70d0505192cb48
Allison King <allisonjuliaking@gmail.com>
2022-12-01 17:19:41 -0500

    Pull out ConnectedCircle into its own component

---
+++ clients/admin-ui/src/features/common/ConnectedCircle.tsx
@@ -0,0 +1,25 @@
+import { Box, BoxProps } from "@fidesui/react";
+
+const ConnectedCircle = ({
+  connected,
+  ...other
+}: { connected?: boolean } & BoxProps) => {
+  let color = "red.500";
+  if (connected == null) {
+    color = "gray.300";
+  } else if (connected) {
+    color = "green.500";
+  }
+
+  return (
+    <Box
+      width="12px"
+      height="12px"
+      borderRadius="6px"
+      backgroundColor={color}
+      {...other}
+    />
+  );
+};
+
+export default ConnectedCircle;
