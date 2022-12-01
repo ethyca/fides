a4ac0d5c9550b4c4c8f851712be0220d594e1747
Paul Sanders <pau@ethyca.com>
2022-12-01 14:09:03 -0500

    Merge alembic heads

---
+++ src/fides/api/ctl/migrations/versions/4b5267153477_merge_heads.py
@@ -0,0 +1,24 @@
+"""Merge heads
+
+Revision ID: 4b5267153477
+Revises: 8f84fad4e00b, b72541d79f10
+Create Date: 2022-12-01 12:46:57.564219
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '4b5267153477'
+down_revision = ('8f84fad4e00b', 'b72541d79f10')
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
