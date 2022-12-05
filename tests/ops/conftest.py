2698c0d9998c2cce83168fbdf826be2c1a6a7d1e
Thomas <thomas.lapiana+github@pm.me>
2022-12-05 10:46:56 +0800

    try adding the config cache clear to the ops tests

--- tests/ops/conftest.py
+++ tests/ops/conftest.py
@@ -105,6 +105,11 @@ def db() -> Generator:
 
 
 @pytest.fixture(autouse=True)
+def clear_get_config_cache() -> None:
+    get_config.cache_clear()
+
+
+@pytest.fixture(autouse=True)
 def clear_db_tables(db):
     """Clear data from tables between tests.
 
