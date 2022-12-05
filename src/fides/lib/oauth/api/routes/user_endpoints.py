2ee775d44179a51a82f1368d0e71d9f10762bf86
Thomas <thomas.lapiana+github@pm.me>
2022-12-05 11:40:32 +0800

    another mypy fix

--- src/fides/lib/oauth/api/routes/user_endpoints.py
+++ src/fides/lib/oauth/api/routes/user_endpoints.py
@@ -22,7 +22,8 @@ from fides.lib.models.client import ADMIN_UI_ROOT, ClientDetail
 from fides.lib.models.fides_user import FidesUser
 from fides.lib.models.fides_user_permissions import FidesUserPermissions
 from fides.lib.oauth.api import urn_registry as urls
-from fides.lib.oauth.api.deps import get_config, get_db, verify_oauth_client
+from fides.lib.oauth.api.deps import get_db, verify_oauth_client
+from fides.ctl.core.config import get_config
 from fides.lib.oauth.schemas.oauth import AccessToken
 from fides.lib.oauth.schemas.user import (
     UserCreate,
