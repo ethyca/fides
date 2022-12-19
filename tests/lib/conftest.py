2f3b0c04665adcc6c4d7c3714d5f5f6418006fc4
Thomas <thomas.lapiana+github@pm.me>
2022-12-19 10:43:15 -0800

    Fix user creation as root (#2082)
    Co-authored-by: Sean Preston <sean@ethyca.com>

--- tests/lib/conftest.py
+++ tests/lib/conftest.py
@@ -42,6 +42,12 @@ def config():
 @pytest.fixture
 def db(config):
     """Yield a connection to the test DB."""
+    # Included so that `AccessManualWebhook` can be located when
+    # `ConnectionConfig` is instantiated.
+    from fides.api.ops.models.manual_webhook import (  # pylint: disable=unused-import
+        AccessManualWebhook,
+    )
+
     # Create the test DB engine
     assert config.is_test_mode
     engine = get_db_engine(
