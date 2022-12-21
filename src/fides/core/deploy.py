963c5a483206efc1e99ecd0c8c1d5c5d8b8e36c1
Thomas <thomas.lapiana+github@pm.me>
2022-12-20 20:15:20 -0800

    fix the deploy sample project dir path

--- src/fides/core/deploy.py
+++ src/fides/core/deploy.py
@@ -15,7 +15,7 @@ FIDES_UPLOADS_DIR = getcwd() + "/fides_uploads/"
 REQUIRED_DOCKER_VERSION = "20.10.17"
 SAMPLE_PROJECT_DIR = join(
     dirname(__file__),
-    "../../data/sample_project",
+    "../data/sample_project",
 )
 DOCKER_COMPOSE_FILE = join(SAMPLE_PROJECT_DIR, "docker-compose.yml")
 DOCKER_COMPOSE_COMMAND = (
