4e99422e22429b24c8cd4bccf1a0c5a6929cbce2
Thomas <thomas.lapiana+github@pm.me>
2023-01-05 11:56:56 +0800

    fix: remove noxfile import of docs_nox

--- noxfile.py
+++ noxfile.py
@@ -15,7 +15,6 @@ sys.path.append("noxfiles")
 from ci_nox import *
 from dev_nox import *
 from docker_nox import *
-from docs_nox import *
 from utils_nox import *
 
 # pylint: enable=unused-wildcard-import, wildcard-import, wrong-import-position
