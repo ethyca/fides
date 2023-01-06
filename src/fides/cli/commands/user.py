ceb6c37f04cef7d54ebebadf50d3f2bf5cb83cc6
Thomas <thomas.lapiana+github@pm.me>
2023-01-06 14:14:28 +0800

    feat: add username/password options to the login command

--- src/fides/cli/commands/user.py
+++ src/fides/cli/commands/user.py
@@ -5,6 +5,8 @@ import click
 import requests
 import toml
 
+from fides.core.utils import echo_red
+
 
 @click.group(name="user")
 @click.pass_context
@@ -16,24 +18,42 @@ def user(ctx: click.Context) -> None:
 
 @user.command()
 @click.pass_context
-def login(ctx: click.Context) -> None:
-    """Use credentials to verify a user."""
+@click.option(
+    "-u",
+    "--username",
+    default="",
+    help="Username to authenticate with.",
+)
+@click.option(
+    "-p",
+    "--password",
+    default="",
+    help="Password to authenticate with.",
+)
+def login(ctx: click.Context, username: str, password: str) -> None:
+    """Use credentials to get a user access token."""
 
     # If no username/password was provided, attempt to use the root
     config = ctx.obj["CONFIG"]
-    username = config.security.oauth_root_client_id
-    password = config.security.oauth_root_client_secret
+    username = username or config.security.oauth_root_client_id
+    password = password or config.security.oauth_root_client_secret
     payload = {
         "client_id": username,
         "client_secret": password,
         "grant_type": "client_credentials",
     }
 
-    # Hit the auth endpoint
+    # Get the access token from the auth endpoint
     response = requests.post("http://localhost:8080/api/v1/oauth/token", data=payload)
+    if response.status_code != 200:
+        echo_red("Authentication failed!")
+        raise SystemExit(1)
     access_token = response.json()["access_token"]
 
     # Store the token in a .credentials file
     credentials_data = {"access_token": access_token}
-    with open(".credentials", "w", encoding="utf-8") as credentials_file:
+    credentials_path = ".credentials"
+    print(f"Writing credentials to: '{credentials_path}'...")
+    with open(credentials_path, "w", encoding="utf-8") as credentials_file:
         credentials_file.write(toml.dumps(credentials_data))
+    print("Credentials successfully created.")
