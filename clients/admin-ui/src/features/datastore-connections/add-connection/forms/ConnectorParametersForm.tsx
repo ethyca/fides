f5485db10a2f40df754a212d6959bd10bd1f04af
LKCSmith <99051851+LKCSmith@users.noreply.github.com>
2023-01-10 12:52:05 -0600

    Lint

--- clients/admin-ui/src/features/datastore-connections/add-connection/forms/ConnectorParametersForm.tsx
+++ clients/admin-ui/src/features/datastore-connections/add-connection/forms/ConnectorParametersForm.tsx
@@ -19,7 +19,6 @@ import {
   VStack,
 } from "@fidesui/react";
 // import { useAPIHelper } from "common/hooks";
-import { CircleHelpIcon } from "common/Icon";
 import { selectConnectionTypeState } from "connection-type/connection-type.slice";
 import {
   ConnectionTypeSecretSchemaProperty,
