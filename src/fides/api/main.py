70119f3097a99f4a5b2f4545ee043848dd25b272
Adrian Galvan <adrian@ethyca.com>
2023-01-09 22:58:21 -0800

    Importing custom request overrides in main.py

--- src/fides/api/main.py
+++ src/fides/api/main.py
@@ -50,6 +50,7 @@ from fides.api.ops.service.connectors.saas.connector_registry_service import (
     registry_file,
     update_saas_configs,
 )
+from fides.api.ops.service.saas_request.override_implementations import *
 from fides.api.ops.tasks.scheduled.scheduler import scheduler
 from fides.api.ops.util.cache import get_cache
 from fides.api.ops.util.logger import _log_exception
