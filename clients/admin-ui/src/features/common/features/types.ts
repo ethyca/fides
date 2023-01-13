5c0d5bc4a32a42ff1f48d0bef2352c880b94643f
Sebastian Sangervasi <sebastian@ethyca.com>
2023-01-13 11:48:36 -0800

    ui/features: Descriptive comments for type utils

--- clients/admin-ui/src/features/common/features/types.ts
+++ clients/admin-ui/src/features/common/features/types.ts
@@ -14,18 +14,76 @@ export type FlagEnvs<Value> = {
   production: Value;
 };
 
+/**
+ * The format of the `flags.json` file: an object mapping flag names to the values per environment,
+ * or a single value that applies everywhere.
+ *
+ * @example
+ * {
+ *   falseEverywhere: false,
+ *   envDependent: {
+ *     development: true,
+ *     test: true,
+ *     production: false,
+ *   }
+ * }
+ */
 export type FlagConfigDefaults<Flags> = {
   [Name in keyof Flags]: Flags[Name] | FlagEnvs<Flags[Name]>;
 };
 
+/**
+ * The type of a resolved configuration, which ensures a value is set for each environment.
+ *
+ * @example
+ * {
+ *   falseEverywhere: {
+ *     development: false,
+ *     test: false,
+ *     production: false,
+ *   },
+ *   envDependent: {
+ *     development: true,
+ *     test: true,
+ *     production: false,
+ *   }
+ * }
+ */
 export type FlagConfig<Flags> = {
   [Name in keyof Flags]: FlagEnvs<Flags[Name]>;
 };
 
+/**
+ * Utility type for extracting the mapping of flag names to flag values for a single environment.
+ *
+ * @example
+ * const flagConfig = {
+ *   falseEverywhere: {
+ *     development: false,
+ *     test: false,
+ *     production: false,
+ *   },
+ *   envDependent: {
+ *     development: true,
+ *     test: true,
+ *     production: false,
+ *   },
+ * };
+ * const flags: FlagsFor<typeof flagConfig> = {
+ *   falseEverywhere: false,
+ *   envDependent: true,
+ * };
+ */
 export type FlagsFor<FC> = FC extends FlagConfig<infer F> ? F : never;
 
+/**
+ * Utility type for getting the names of all flags.
+ */
 export type NamesFor<FC> = FC extends FlagConfig<infer F> ? keyof F : never;
 
+/**
+ * Utility type for getting the value type for a flag.
+ */
 export type ValueFor<FC, FN> = FN extends keyof FlagsFor<FC>
   ? FlagsFor<FC>[FN]
   : never;
