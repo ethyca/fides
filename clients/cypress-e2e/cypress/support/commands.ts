6c0d695240981fa0bfa0cfd4112e48b6206cac35
Allison King <allisonjuliaking@gmail.com>
2023-01-13 13:17:34 -0500

    Pull login into its own custom command

--- clients/cypress-e2e/cypress/support/commands.ts
+++ clients/cypress-e2e/cypress/support/commands.ts
@@ -1,8 +1,15 @@
 /// <reference types="cypress" />
+import { CREDENTIALS } from "./constants";
 
 Cypress.Commands.add("getByTestId", (selector, ...args) =>
   cy.get(`[data-testid='${selector}']`, ...args)
 );
+
+Cypress.Commands.add("login", () => {
+  cy.getByTestId("input-username").type(CREDENTIALS.username);
+  cy.getByTestId("input-password").type(CREDENTIALS.password);
+  cy.getByTestId("sign-in-btn").click();
+});
 
 declare global {
   namespace Cypress {
@@ -20,6 +27,10 @@ declare global {
             Cypress.Shadow
         >
       ): Chainable<JQuery<HTMLElement>>;
+      /**
+       * Log in. Must navigate to the login page before this command.
+       */
+      login(): void;
     }
   }
 }
