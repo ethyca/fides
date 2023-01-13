ca2ace673090b73c6c9d59290ca450a82906c4b1
Allison King <allisonjuliaking@gmail.com>
2023-01-13 13:17:34 -0500

    Pull login into its own custom command

--- clients/cypress-e2e/cypress/e2e/login.cy.ts
+++ clients/cypress-e2e/cypress/e2e/login.cy.ts
@@ -1,11 +1,7 @@
-import { CREDENTIALS } from "../support/constants";
-
 describe("Log in", () => {
   it("can log in and be redirected to the home page", () => {
     cy.visit("localhost:3000");
-    cy.getByTestId("input-username").type(CREDENTIALS.username);
-    cy.getByTestId("input-password").type(CREDENTIALS.password);
-    cy.getByTestId("sign-in-btn").click();
+    cy.login();
     cy.getByTestId("Home");
   });
 });
