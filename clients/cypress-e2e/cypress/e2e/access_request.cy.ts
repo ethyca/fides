ff1e189f1031b587ac1cd750bb408df7a1912f6b
Allison King <allisonjuliaking@gmail.com>
2023-01-13 14:11:30 -0500

    Full test for access request flow

--- clients/cypress-e2e/cypress/e2e/access_request.cy.ts
+++ clients/cypress-e2e/cypress/e2e/access_request.cy.ts
@@ -1,7 +1,14 @@
-import { CREDENTIALS } from "../support/constants";
-
 describe("Access request flow", () => {
   it("can submit an access request from the privacy center", () => {
+    // Watch these routes without changing or stubbing its response
+    cy.intercept(
+      "PATCH",
+      "http://0.0.0.0:8080/api/v1/privacy-request/administrate/approve"
+    ).as("patchRequest");
+    cy.intercept("GET", "http://0.0.0.0:8080/api/v1/privacy-request*").as(
+      "getRequests"
+    );
+
     // Submit the access request from the privacy center
     cy.visit("localhost:3001");
     cy.getByTestId("card").contains("Access your data").click();
@@ -17,6 +24,29 @@ describe("Access request flow", () => {
     cy.visit("localhost:3000");
     cy.origin("http://localhost:3000", () => {
       cy.login();
+      cy.get("div").contains("Review privacy requests").click();
+      let numCompletedRequests = 0;
+      cy.wait("@getRequests").then((interception) => {
+        const { items } = interception.response.body;
+        numCompletedRequests = items.filter(
+          (i) => i.status === "complete"
+        ).length;
+      });
+
+      cy.getByTestId("privacy-request-row-pending")
+        .first()
+        .trigger("mouseover")
+        .get("button")
+        .contains("Approve")
+        .click();
+
+      cy.wait("@patchRequest");
+      cy.wait("@getRequests");
+
+      // Make sure there is one more completed request than originally
+      cy.getByTestId("privacy-request-row-complete").then((rows) => {
+        expect(rows.length).to.eql(numCompletedRequests + 1);
+      });
     });
   });
 });
