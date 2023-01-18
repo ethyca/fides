775cea468b6f7e5d9bc8d3c76ae8d701f56218db
Thomas <thomas.lapiana+github@pm.me>
2023-01-18 19:57:34 +0800

    feat: coverage uploads expect and only run when the external tests are run

--- noxfiles/ci_nox.py
+++ noxfiles/ci_nox.py
@@ -275,9 +275,13 @@ def format_coverage(session: nox.Session, test_group: str) -> None:
 def sum_coverage(session: nox.Session) -> None:
     """
     Combine multiple Code Climate coverage files.
+
+    7 files are expected for summation, one from each of the test groups run in CI.
     """
     download_cc_reporter = f"curl -L {TEST_REPORTER_URL} > ./cc-test-reporter && chmod +x ./cc-test-reporter && "
-    sum_test_coverage = "./cc-test-reporter sum-coverage coverage/**/codeclimate.json ;"
+    sum_test_coverage = (
+        "./cc-test-reporter sum-coverage --parts 7 coverage/**/codeclimate.json ;"
+    )
 
     session.run(
         *RUN_NO_DEPS,
