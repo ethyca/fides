a1e71c7d9970645c405834caa9506ab078b9d902
Sebastian Sangervasi <sebastian@ethyca.com>
2023-01-20 13:15:22 -0800

    pc/modals: Validate phone and email even when they're optional

---
+++ clients/privacy-center/components/modals/validation.ts
@@ -0,0 +1,35 @@
+import * as Yup from "yup";
+
+export const nameValidation = (option?: string) => {
+  let validation = Yup.string();
+  if (option === "required") {
+    validation = validation.required("Name is required");
+  } else {
+    validation = validation.optional();
+  }
+  return validation;
+};
+
+export const emailValidation = (option?: string) => {
+  let validation = Yup.string().email("Email is invalid");
+  if (option === "required") {
+    validation = validation.required("Email is required");
+  } else {
+    validation = validation.optional();
+  }
+  return validation;
+};
+
+export const phoneValidation = (option?: string) => {
+  // E.164 international standard format
+  let validation = Yup.string().matches(
+    /^\+[1-9]\d{1,14}$/,
+    "Phone is invalid"
+  );
+  if (option === "required") {
+    validation = validation.required("Phone is required");
+  } else {
+    validation = validation.optional();
+  }
+  return validation;
+};
