9d4a8071e176e622897d53b06cc8bf95b3a31c9c
Thomas <thomas.lapiana+github@pm.me>
2023-01-20 23:30:58 +0800

    refactor: alphabetize the scopes_registry.py

--- src/fides/api/ops/api/v1/scope_registry.py
+++ src/fides/api/ops/api/v1/scope_registry.py
@@ -9,16 +9,19 @@ The format for defining a scope is:
 
 There is a redundant `fides/lib/oauth/scopes.py` file that should not be used.
 """
+
+# These permissions all endpoints used by the CLI's `push` & `evaluate` commands
+CLI_OBJECTS_CREATE = "cli-objects:create"
+CLI_OBJECTS_READ = "cli-objects:read"
+CLI_OBJECTS_UPDATE = "cli-objects:update"
+CLI_OBJECTS_DELETE = "cli-objects:delete"
+
 CLIENT_CREATE = "client:create"
 CLIENT_UPDATE = "client:update"
 CLIENT_READ = "client:read"
 CLIENT_DELETE = "client:delete"
 
 CONFIG_READ = "config:read"
-
-POLICY_CREATE_OR_UPDATE = "policy:create_or_update"
-POLICY_READ = "policy:read"
-POLICY_DELETE = "policy:delete"
 
 CONNECTION_TYPE_READ = "connection_type:read"
 
@@ -29,6 +32,22 @@ CONNECTION_AUTHORIZE = "connection:authorize"
 SAAS_CONNECTION_INSTANTIATE = "connection:instantiate"
 
 CONSENT_READ = "consent:read"
+
+DATABASE_RESET = "database:reset"
+
+DATASET_CREATE_OR_UPDATE = "dataset:create_or_update"
+DATASET_READ = "dataset:read"
+DATASET_DELETE = "dataset:delete"
+
+ENCRYPTION_EXEC = "encryption:exec"
+
+MESSAGING_CREATE_OR_UPDATE = "messaging:create_or_update"
+MESSAGING_READ = "messaging:read"
+MESSAGING_DELETE = "messaging:delete"
+
+POLICY_CREATE_OR_UPDATE = "policy:create_or_update"
+POLICY_READ = "policy:read"
+POLICY_DELETE = "policy:delete"
 
 PRIVACY_REQUEST_CREATE = "privacy-request:create"
 PRIVACY_REQUEST_READ = "privacy-request:read"
@@ -45,33 +64,19 @@ PRIVACY_REQUEST_TRANSFER = "privacy-request:transfer"
 PRIVACY_REQUEST_UPLOAD_DATA = "privacy-request:upload_data"
 PRIVACY_REQUEST_VIEW_DATA = "privacy-request:view_data"
 
-WEBHOOK_CREATE_OR_UPDATE = "webhook:create_or_update"
-WEBHOOK_READ = "webhook:read"
-WEBHOOK_DELETE = "webhook:delete"
-
 RULE_CREATE_OR_UPDATE = "rule:create_or_update"
 RULE_READ = "rule:read"
 RULE_DELETE = "rule:delete"
 
-STORAGE_CREATE_OR_UPDATE = "storage:create_or_update"
-STORAGE_READ = "storage:read"
-STORAGE_DELETE = "storage:delete"
-
-MESSAGING_CREATE_OR_UPDATE = "messaging:create_or_update"
-MESSAGING_READ = "messaging:read"
-MESSAGING_DELETE = "messaging:delete"
-
-SCOPE_READ = "scope:read"
-
-ENCRYPTION_EXEC = "encryption:exec"
-
-DATASET_CREATE_OR_UPDATE = "dataset:create_or_update"
-DATASET_READ = "dataset:read"
-DATASET_DELETE = "dataset:delete"
-
 SAAS_CONFIG_CREATE_OR_UPDATE = "saas_config:create_or_update"
 SAAS_CONFIG_READ = "saas_config:read"
 SAAS_CONFIG_DELETE = "saas_config:delete"
+
+SCOPE_READ = "scope:read"
+
+STORAGE_CREATE_OR_UPDATE = "storage:create_or_update"
+STORAGE_READ = "storage:read"
+STORAGE_DELETE = "storage:delete"
 
 USER_CREATE = "user:create"
 USER_UPDATE = "user:update"
@@ -83,13 +88,16 @@ USER_PERMISSION_CREATE = "user-permission:create"
 USER_PERMISSION_UPDATE = "user-permission:update"
 USER_PERMISSION_READ = "user-permission:read"
 
-# These permissions all endpoints used by the CLI's `push` & `evaluate` commands
-CLI_OBJECTS_CREATE = "cli-objects:create"
-CLI_OBJECTS_READ = "cli-objects:read"
-CLI_OBJECTS_UPDATE = "cli-objects:update"
-CLI_OBJECTS_DELETE = "cli-objects:delete"
+WEBHOOK_CREATE_OR_UPDATE = "webhook:create_or_update"
+WEBHOOK_READ = "webhook:read"
+WEBHOOK_DELETE = "webhook:delete"
 
+# Should contain all scopes listed above.
 SCOPE_REGISTRY = [
+    CLI_OBJECTS_CREATE,
+    CLI_OBJECTS_READ,
+    CLI_OBJECTS_UPDATE,
+    CLI_OBJECTS_DELETE,
     CLIENT_CREATE,
     CLIENT_UPDATE,
     CLIENT_READ,
@@ -102,6 +110,7 @@ SCOPE_REGISTRY = [
     SAAS_CONNECTION_INSTANTIATE,
     CONSENT_READ,
     CONNECTION_TYPE_READ,
+    DATABASE_RESET,
     DATASET_CREATE_OR_UPDATE,
     DATASET_DELETE,
     DATASET_READ,
