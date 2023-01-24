8c9e22cbac4e9e36500f68921c9fb396e884fde5
Allison King <allisonjuliaking@gmail.com>
2023-01-24 12:14:26 -0500

    Pull URLs into constants

--- clients/cypress-e2e/cypress/support/constants.ts
+++ clients/cypress-e2e/cypress/support/constants.ts
@@ -1,5 +1,9 @@
 /**
  * Based off of fides.toml
+ *
+ * TODO: if this becomes too cumbersome to keep in sync, we may also want to look
+ * into other ways of sharing constants
+ * https://docs.cypress.io/guides/guides/environment-variables#Option-3-CYPRESS_
  */
 export const CREDENTIALS = {
   username: "root_user",
@@ -7,3 +11,5 @@ export const CREDENTIALS = {
 };
 
 export const API_URL = "http://0.0.0.0:8080/api/v1";
+export const ADMIN_UI_URL = "http://localhost:3000";
+export const PRIVACY_CENTER_URL = "http://localhost:3001";
