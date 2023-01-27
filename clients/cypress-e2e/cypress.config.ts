bb239ae47f10a5c43e1eca826b1100bdae7fe982
Allison King <allisonjuliaking@gmail.com>
2023-01-27 14:48:45 -0500

    Configure cypress cloud

--- clients/cypress-e2e/cypress.config.ts
+++ clients/cypress-e2e/cypress.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "cypress";
 
 export default defineConfig({
+  projectId: "bauzeh",
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
