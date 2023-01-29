85da389c847d2ab3d00d2f523dd2148efafb16e0
Kelsey Thomas <101993653+Kelsey-Ethyca@users.noreply.github.com>
2023-01-29 15:49:21 -0800

    init kustomer config, dataset, passing access test, broken eraure test

---
+++ tests/ops/integration_tests/saas/test_kustomer_task.py
@@ -0,0 +1,141 @@
+import random
+import time
+
+import pytest
+import requests
+
+from fides.api.ops.graph.graph import DatasetGraph
+from fides.api.ops.models.privacy_request import PrivacyRequest
+from fides.api.ops.schemas.redis_cache import Identity
+from fides.api.ops.service.connectors import get_connector
+from fides.api.ops.task import graph_task
+from fides.api.ops.task.graph_task import get_cached_data_for_erasures
+from fides.core.config import get_config
+from tests.ops.graph.graph_test_util import assert_rows_match
+
+CONFIG = get_config()
+
+
+@pytest.mark.integration_saas
+@pytest.mark.integration_kustomer
+def test_kustomer_connection_test(kustomer_connection_config) -> None:
+    get_connector(kustomer_connection_config).test_connection()
+
+
+@pytest.mark.integration_saas
+@pytest.mark.integration_kustomer
+@pytest.mark.asyncio
+async def test_kustomer_access_request_task(
+    db,
+    policy,
+    kustomer_connection_config,
+    kustomer_dataset_config,
+    kustomer_identity_email,
+) -> None:
+    """Full access request based on the kustomer SaaS config"""
+
+    privacy_request = PrivacyRequest(
+        id=f"test_kustomer_access_request_task_{random.randint(0, 1000)}"
+    )
+    identity = Identity(**{"email": kustomer_identity_email})
+    privacy_request.cache_identity(identity)
+
+    dataset_name = kustomer_connection_config.get_saas_config().fides_key
+    merged_graph = kustomer_dataset_config.get_graph()
+    graph = DatasetGraph(merged_graph)
+
+    v = await graph_task.run_access_request(
+        privacy_request,
+        policy,
+        graph,
+        [kustomer_connection_config],
+        {"email": kustomer_identity_email},
+        db,
+    )
+
+    assert_rows_match(
+        v[f"{dataset_name}:customer"],
+        min_size=1,
+        keys=[
+            "type",
+            "id",
+            "attributes",
+            "relationships",
+            "links"
+        ],
+    )
+
+    # verify we only returned data for our identity email
+    assert v[f"{dataset_name}:customer"][0]["attributes"]["emails"][0]["email"] == kustomer_identity_email    
+
+@pytest.mark.integration_saas
+@pytest.mark.integration_kustomer
+@pytest.mark.asyncio
+async def test_kustomer_erasure_request_task(
+    db,
+    policy,
+    erasure_policy_string_rewrite,
+    kustomer_connection_config,
+    kustomer_dataset_config,
+    kustomer_erasure_identity_email,
+    kustomer_create_erasure_data,
+) -> None:
+    """Full erasure request based on the kustomer SaaS config"""
+
+    privacy_request = PrivacyRequest(
+        id=f"test_kustomer_erasure_request_task_{random.randint(0, 1000)}"
+    )
+    identity = Identity(**{"email": kustomer_erasure_identity_email})
+    privacy_request.cache_identity(identity)
+
+    dataset_name = kustomer_connection_config.get_saas_config().fides_key
+    merged_graph = kustomer_dataset_config.get_graph()
+    graph = DatasetGraph(merged_graph)
+
+    v = await graph_task.run_access_request(
+        privacy_request,
+        policy,
+        graph,
+        [kustomer_connection_config],
+        {"email": kustomer_erasure_identity_email},
+        db,
+    )
+
+    assert_rows_match(
+        v[f"{dataset_name}:customer"],
+        min_size=1,
+        keys=[
+            "type",
+            "id",
+            "attributes",
+            "relationships",
+            "links"
+        ],
+    )
+
+    x = await graph_task.run_erasure(
+        privacy_request,
+        erasure_policy_string_rewrite,
+        graph,
+        [kustomer_connection_config],
+        {"email": kustomer_erasure_identity_email},
+        get_cached_data_for_erasures(privacy_request.id),
+        db,
+    )
+
+    assert x == {
+        f"{dataset_name}:customer": 1
+    }
+
+    kustomer_secrets = kustomer_connection_config.secrets
+    headers = {
+        "Authorization": f"Bearer {kustomer_secrets['api_key']}",
+    }    
+    base_url = f"https://{kustomer_secrets['domain']}"
+
+    response = requests.get(
+        url=f"{base_url}/v1/customers/email={kustomer_erasure_identity_email}",
+        headers=headers,
+    )
+    customer = response.json()
+    assert customer[0]["attributes"]["emails"][0]["email"] == "MASKED"
