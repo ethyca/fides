02ef1a17b08b5fe1e9824e537af4260cf5fbb622
vivek <vivek@myambergroup.com>
2023-01-30 22:45:30 +0530

    yotpo access request and erasure test

---
+++ tests/ops/fixtures/saas/yotpo_fixtures.py
@@ -0,0 +1,187 @@
+from time import sleep
+from typing import Any, Dict, Generator
+
+import pydash
+import pytest
+import requests
+from sqlalchemy.orm import Session
+
+from fides.api.ops.models.connectionconfig import (
+    AccessLevel,
+    ConnectionConfig,
+    ConnectionType,
+)
+from fides.api.ops.models.datasetconfig import DatasetConfig
+from fides.api.ops.util.saas_util import (
+    load_config_with_replacement,
+    load_dataset_with_replacement,
+)
+from fides.lib.cryptography import cryptographic_util
+from tests.ops.test_helpers.saas_test_utils import poll_for_existence
+from tests.ops.test_helpers.vault_client import get_secrets
+
+secrets = get_secrets("yotpo")
+
+
+@pytest.fixture(scope="session")
+def yotpo_secrets(saas_config):
+    return {
+        "yopto_domain": pydash.get(saas_config, "yotpo.yopto_domain") or secrets["yopto_domain"],
+        "loyalty_api_domain": pydash.get(saas_config, "yotpo.loyalty_api_domain") or secrets["loyalty_api_domain"],
+        "app_key": pydash.get(saas_config, "yotpo.app_key") or secrets["app_key"],
+        "secret_key": pydash.get(saas_config, "yotpo.secret_key") or secrets["secret_key"],
+        "x_api_key": pydash.get(saas_config, "yotpo.x_api_key") or secrets["x_api_key"],
+        "x_guid": pydash.get(saas_config, "yotpo.x_guid") or secrets["x_guid"],
+        "external_id": pydash.get(saas_config, "yotpo.external_id") or secrets["external_id"]
+    }
+
+
+@pytest.fixture(scope="session")
+def yotpo_identity_email(saas_config):
+    return (
+        pydash.get(saas_config, "yotpo.identity_email") or secrets["identity_email"]
+    )
+
+
+@pytest.fixture(scope="function")
+def yotpo_erasure_identity_email() -> str:
+    return f"{cryptographic_util.generate_secure_random_string(13)}@email.com"
+
+@pytest.fixture(scope="function")
+def yotpo_erasure_external_id() -> str:
+    return f"{cryptographic_util.generate_secure_random_string(13)}"
+
+
+@pytest.fixture
+def yotpo_config() -> Dict[str, Any]:
+    return load_config_with_replacement(
+        "data/saas/config/yotpo_config.yml",
+        "<instance_fides_key>",
+        "yotpo_instance",
+    )
+
+
+@pytest.fixture
+def yotpo_dataset() -> Dict[str, Any]:
+    return load_dataset_with_replacement(
+        "data/saas/dataset/yotpo_dataset.yml",
+        "<instance_fides_key>",
+        "yotpo_instance",
+    )[0]
+
+
+@pytest.fixture(scope="function")
+def yotpo_connection_config(
+    db: Session, yotpo_config, yotpo_secrets
+) -> Generator:
+    fides_key = yotpo_config["fides_key"]
+    connection_config = ConnectionConfig.create(
+        db=db,
+        data={
+            "key": fides_key,
+            "name": fides_key,
+            "connection_type": ConnectionType.saas,
+            "access": AccessLevel.write,
+            "secrets": yotpo_secrets,
+            "saas_config": yotpo_config,
+        },
+    )
+    yield connection_config
+    connection_config.delete(db)
+
+
+@pytest.fixture
+def yotpo_dataset_config(
+    db: Session,
+    yotpo_connection_config: ConnectionConfig,
+    yotpo_dataset: Dict[str, Any],
+) -> Generator:
+    fides_key = yotpo_dataset["fides_key"]
+    yotpo_connection_config.name = fides_key
+    yotpo_connection_config.key = fides_key
+    yotpo_connection_config.save(db=db)
+    dataset = DatasetConfig.create(
+        db=db,
+        data={
+            "connection_config_id": yotpo_connection_config.id,
+            "fides_key": fides_key,
+            "dataset": yotpo_dataset,
+        },
+    )
+    yield dataset
+    dataset.delete(db=db)
+
+
+@pytest.fixture(scope="function")
+def yotpo_create_erasure_data(
+    yotpo_connection_config: ConnectionConfig, yotpo_erasure_identity_email: str, yotpo_erasure_external_id: str, yotpo_create_access_token : str
+) -> None:
+
+    # sleep(60)
+
+    yotpo_secrets = yotpo_connection_config.secrets
+    base_url = f"https://{yotpo_secrets['yopto_domain']}"
+    appkey = yotpo_secrets['app_key']
+    headers = {
+        "X-Yotpo-Token": yotpo_create_access_token,
+    }
+
+    # user
+    body = {
+        "customer": {
+            "external_id": yotpo_erasure_external_id,
+            "email": yotpo_erasure_identity_email
+        }
+    }
+
+    users_response = requests.patch(url=f"{base_url}/core/v3/stores/{appkey}/customers", headers=headers, json=body)
+    user = users_response.ok
+
+    sleep(30)
+
+    yield user
+
+@pytest.fixture(scope="function")
+def yotpo_create_loyalty_user(
+    yotpo_connection_config: ConnectionConfig, yotpo_erasure_identity_email: str
+) -> None:
+
+    yotpo_secrets = yotpo_connection_config.secrets
+    base_url = f"https://{yotpo_secrets['loyalty_api_domain']}"
+    headers = {
+        "x-api-key": yotpo_secrets['x_api_key'],
+        "x-guid": yotpo_secrets['x_guid'],
+    }
+
+    # user
+    body = {
+            "email": yotpo_erasure_identity_email,
+            "first_name": "vivek",
+            "last_name": "ct"
+    }
+
+    users_response = requests.post(url=f"{base_url}/api/v2/customers", headers=headers, json=body)
+    loyalty_user = users_response.ok
+        
+    sleep(30)
+
+    yield loyalty_user
+
+@pytest.fixture(scope="function")
+def yotpo_create_access_token(
+    yotpo_connection_config: ConnectionConfig
+) -> None:
+
+    yotpo_secrets = yotpo_connection_config.secrets
+    base_url = f"https://{yotpo_secrets['yopto_domain']}"
+    appkey = yotpo_secrets['app_key']
+
+    # user
+    body = {
+            "secret": yotpo_secrets['secret_key']
+    }
+
+    access_response = requests.post(url=f"{base_url}/core/v3/stores/{appkey}/access_tokens", json=body)
+    token = access_response.json()["access_token"]
+
+    yield token
---
+++ tests/ops/integration_tests/saas/test_yotpo_task.py
@@ -0,0 +1,314 @@
+import random
+import time
+
+import pytest
+import requests
+
+from fides.api.ops.graph.graph import DatasetGraph
+from fides.api.ops.models.privacy_request import PrivacyRequest
+from fides.api.ops.schemas.redis_cache import Identity
+from fides.api.ops.service.connectors import get_connector
+from fides.api.ops.task import graph_task
+from fides.api.ops.task.graph_task import get_cached_data_for_erasures
+from fides.core.config import get_config
+from tests.ops.graph.graph_test_util import assert_rows_match
+
+CONFIG = get_config()
+
+
+@pytest.mark.integration_saas
+@pytest.mark.integration_yotpo
+def test_yotpo_connection_test(yotpo_connection_config) -> None:
+    get_connector(yotpo_connection_config).test_connection()
+
+@pytest.mark.integration_saas
+@pytest.mark.integration_yotpo
+@pytest.mark.asyncio
+async def test_yotpo_access_request_task(
+    db,
+    policy,
+    yotpo_connection_config,
+    yotpo_dataset_config,
+    yotpo_identity_email,
+) -> None:
+    """Full access request based on the yotpo SaaS config"""
+
+    privacy_request = PrivacyRequest(
+        id=f"test_yotpo_access_request_task_{random.randint(0, 1000)}"
+    )
+    identity = Identity(**{"email": yotpo_identity_email})
+    privacy_request.cache_identity(identity)
+
+    dataset_name = yotpo_connection_config.get_saas_config().fides_key
+    merged_graph = yotpo_dataset_config.get_graph()
+    graph = DatasetGraph(merged_graph)
+
+    v = await graph_task.run_access_request(
+        privacy_request,
+        policy,
+        graph,
+        [yotpo_connection_config],
+        {"email": yotpo_identity_email},
+        db,
+    )
+
+    assert_rows_match(
+        v[f"{dataset_name}:get_utoken_(core)"],
+        min_size=1,
+        keys=[
+            "access_token"
+        ],
+    )
+
+    assert_rows_match(
+        v[f"{dataset_name}:customer_(core)"],
+        min_size=1,
+        keys=[
+            "external_id",
+            "email",
+            "phone_number",
+            "first_name",
+            "last_name",
+            "gender",
+            "account_created_at",
+            "account_status",
+            "default_language",
+            "default_currency",
+            "tags",
+            "address",
+            "custom_properties",
+            "accepts_email_marketing",
+            "accepts_sms_marketing"
+        ],
+    )
+
+    assert_rows_match(
+        v[f"{dataset_name}:reviews_-_merchant_(ugc)"],
+        min_size=1,
+        keys=[
+            "status",
+            "response"
+        ],
+    )
+
+    assert_rows_match(
+        v[f"{dataset_name}:email_analytics_(ugc)"],
+        min_size=1,
+        keys=[
+            "records"
+        ],
+    )
+
+    assert_rows_match(
+        v[f"{dataset_name}:visual_ugc_(ugc)"],
+        min_size=1,
+        keys=[
+            "pagination",
+            "images"
+        ],
+    )
+
+    assert_rows_match(
+        v[f"{dataset_name}:customers_(loyalty)"],
+        min_size=1,
+        keys=[
+            "total_spend_cents",
+            "total_purchases",
+            "perks_redeemed",
+            "last_purchase_at",
+            "first_name",
+            "last_name",
+            "phone_number",
+            "email",
+            "points_balance",
+            "points_earned",
+            "last_seen_at",
+            "thirty_party_id",
+            "third_party_id",
+            "pos_account_id",
+            "has_store_account",
+            "credit_balance",
+            "credit_balance_in_customer_currency",
+            "opt_in",
+            "opted_in_at",
+            "points_expire_at",
+            "vip_tier_actions_completed",
+            "vip_tier_upgrade_requirements"
+        ],
+    )
+
+        # verify we only returned data for our identity email
+    assert v[f"{dataset_name}:customer_(core)"][0]["email"] == yotpo_identity_email
+    user_id = v[f"{dataset_name}:customer_(core)"][0]["external_id"]
+
+
+@pytest.mark.integration_saas
+@pytest.mark.integration_yotpo
+@pytest.mark.asyncio
+async def test_yotpo_erasure_request_task(
+    db,
+    policy,
+    erasure_policy_string_rewrite,
+    yotpo_connection_config,
+    yotpo_dataset_config,
+    yotpo_erasure_identity_email,
+    yotpo_erasure_external_id,
+    yotpo_create_erasure_data,
+    yotpo_create_access_token,
+    yotpo_create_loyalty_user
+) -> None:
+    """Full erasure request based on the yotpo SaaS config"""
+
+    masking_strict = CONFIG.execution.masking_strict
+    CONFIG.execution.masking_strict = True  # Allow Delete
+
+    privacy_request = PrivacyRequest(
+        id=f"test_yotpo_erasure_request_task_{random.randint(0, 1000)}"
+    )
+    identity = Identity(**{"email": yotpo_erasure_identity_email})
+    privacy_request.cache_identity(identity)
+
+    dataset_name = yotpo_connection_config.get_saas_config().fides_key
+    merged_graph = yotpo_dataset_config.get_graph()
+    graph = DatasetGraph(merged_graph)
+
+    v = await graph_task.run_access_request(
+        privacy_request,
+        policy,
+        graph,
+        [yotpo_connection_config],
+        {"email": yotpo_erasure_identity_email},
+        db,
+    )
+
+    assert_rows_match(
+        v[f"{dataset_name}:get_utoken_(core)"],
+        min_size=1,
+        keys=[
+            "access_token"
+        ],
+    )
+
+    assert_rows_match(
+        v[f"{dataset_name}:customer_(core)"],
+        min_size=1,
+        keys=[
+            "external_id",
+            "email",
+            "phone_number",
+            "first_name",
+            "last_name",
+            "gender",
+            "account_created_at",
+            "account_status",
+            "default_language",
+            "default_currency",
+            "tags",
+            "address",
+            "custom_properties",
+            "accepts_email_marketing",
+            "accepts_sms_marketing"
+        ],
+    )
+
+    assert_rows_match(
+        v[f"{dataset_name}:reviews_-_merchant_(ugc)"],
+        min_size=1,
+        keys=[
+            "status",
+            "response"
+        ],
+    )
+
+    assert_rows_match(
+        v[f"{dataset_name}:email_analytics_(ugc)"],
+        min_size=1,
+        keys=[
+            "records"
+        ],
+    )
+
+    assert_rows_match(
+        v[f"{dataset_name}:visual_ugc_(ugc)"],
+        min_size=1,
+        keys=[
+            "pagination",
+            "images"
+        ],
+    )
+
+    assert_rows_match(
+        v[f"{dataset_name}:customers_(loyalty)"],
+        min_size=1,
+        keys=[
+            "total_spend_cents",
+            "total_purchases",
+            "perks_redeemed",
+            "last_purchase_at",
+            "first_name",
+            "last_name",
+            "phone_number",
+            "email",
+            "points_balance",
+            "points_earned",
+            "last_seen_at",
+            "thirty_party_id",
+            "third_party_id",
+            "pos_account_id",
+            "has_store_account",
+            "credit_balance",
+            "credit_balance_in_customer_currency",
+            "opt_in",
+            "opted_in_at",
+            "points_expire_at",
+            "vip_tier_actions_completed",
+            "vip_tier_upgrade_requirements"
+        ],
+    )
+
+    x = await graph_task.run_erasure(
+        privacy_request,
+        erasure_policy_string_rewrite,
+        graph,
+        [yotpo_connection_config],
+        {"email": yotpo_erasure_identity_email},
+        get_cached_data_for_erasures(privacy_request.id),
+        db,
+    )
+
+    # verify masking request was issued for endpoints with both update/delete actions
+    assert x == {
+        f"{dataset_name}:get_utoken_(core)": 0,
+        f"{dataset_name}:customer_(core)": 1,
+        f"{dataset_name}:reviews_-_merchant_(ugc)": 0,
+        f"{dataset_name}:email_analytics_(ugc)": 0,
+        f"{dataset_name}:visual_ugc_(ugc)": 0,
+        f"{dataset_name}:customers_(loyalty)": 1,
+    }
+
+    yotpo_secrets = yotpo_connection_config.secrets
+    base_url = f"https://{yotpo_secrets['yopto_domain']}"
+    loyalty_base_url = f"https://{yotpo_secrets['loyalty_api_domain']}"
+    headers = {
+        "X-Yotpo-Token": yotpo_create_access_token,
+    }
+    loyalty_headers = {
+        "x-api-key": yotpo_secrets['x_api_key'],
+        "x-guid": yotpo_secrets['x_guid'],
+    }
+
+    # user
+    response = requests.get(
+        url=f"{base_url}/core/v3/stores/{yotpo_secrets['app_key']}/customers",
+        headers=headers,
+        params={"external_ids": yotpo_erasure_external_id},
+    )
+    
+    # loyalty user
+    loyalty_response = requests.get(
+        url=f"{loyalty_base_url}/api/v2/customers",
+        headers=loyalty_headers,
+        params={"customer_email": yotpo_erasure_identity_email},
+    )
+
+    CONFIG.execution.masking_strict = masking_strict
