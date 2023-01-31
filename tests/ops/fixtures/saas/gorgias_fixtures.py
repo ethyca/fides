07054da185da4e6c9ef5883c412894759bd3bf70
vivek <vivek@myambergroup.com>
2023-02-01 00:05:54 +0530

    gorgias connection test

---
+++ tests/ops/fixtures/saas/gorgias_fixtures.py
@@ -0,0 +1,157 @@
+from time import sleep
+from typing import Any, Dict, Generator
+
+import pydash
+import pytest
+import requests
+from sqlalchemy.orm import Session
+
+from fides.api.ops.models.connectionconfig import (
+    AccessLevel,
+    ConnectionConfig,
+    ConnectionType,
+)
+from fides.api.ops.models.datasetconfig import DatasetConfig
+from fides.api.ops.util.saas_util import (
+    load_config_with_replacement,
+    load_dataset_with_replacement,
+)
+from fides.lib.cryptography import cryptographic_util
+from tests.ops.test_helpers.saas_test_utils import poll_for_existence
+from tests.ops.test_helpers.vault_client import get_secrets
+
+secrets = get_secrets("gorgias")
+
+
+@pytest.fixture(scope="session")
+def gorgias_secrets(saas_config):
+    return {
+        "domain": pydash.get(saas_config, "gorgias.domain") or secrets["domain"],
+        "api_key": pydash.get(saas_config, "gorgias.api_key") or secrets["api_key"]
+    }
+
+
+@pytest.fixture(scope="session")
+def gorgias_identity_email(saas_config):
+    return (
+        pydash.get(saas_config, "gorgias.identity_email") or secrets["identity_email"]
+    )
+
+
+@pytest.fixture(scope="function")
+def gorgias_erasure_identity_email() -> str:
+    return f"{cryptographic_util.generate_secure_random_string(13)}@email.com"
+
+
+@pytest.fixture
+def gorgias_config() -> Dict[str, Any]:
+    return load_config_with_replacement(
+        "data/saas/config/gorgias_config.yml",
+        "<instance_fides_key>",
+        "gorgias_instance",
+    )
+
+
+@pytest.fixture
+def gorgias_dataset() -> Dict[str, Any]:
+    return load_dataset_with_replacement(
+        "data/saas/dataset/gorgias_dataset.yml",
+        "<instance_fides_key>",
+        "gorgias_instance",
+    )[0]
+
+
+@pytest.fixture(scope="function")
+def gorgias_connection_config(
+    db: Session, gorgias_config, gorgias_secrets
+) -> Generator:
+    fides_key = gorgias_config["fides_key"]
+    connection_config = ConnectionConfig.create(
+        db=db,
+        data={
+            "key": fides_key,
+            "name": fides_key,
+            "connection_type": ConnectionType.saas,
+            "access": AccessLevel.write,
+            "secrets": gorgias_secrets,
+            "saas_config": gorgias_config,
+        },
+    )
+    yield connection_config
+    connection_config.delete(db)
+
+
+@pytest.fixture
+def gorgias_dataset_config(
+    db: Session,
+    gorgias_connection_config: ConnectionConfig,
+    gorgias_dataset: Dict[str, Any],
+) -> Generator:
+    fides_key = gorgias_dataset["fides_key"]
+    gorgias_connection_config.name = fides_key
+    gorgias_connection_config.key = fides_key
+    gorgias_connection_config.save(db=db)
+    dataset = DatasetConfig.create(
+        db=db,
+        data={
+            "connection_config_id": gorgias_connection_config.id,
+            "fides_key": fides_key,
+            "dataset": gorgias_dataset,
+        },
+    )
+    yield dataset
+    dataset.delete(db=db)
+
+def customer_exists(gorgias_erasure_identity_email: str, gorgias_secrets):
+    """
+    Confirm whether customer exists by calling customer search by email api and comparing resulting firstname str.
+    Returns customer ID if it exists, returns None if it does not.
+    """
+    base_url = f"https://{gorgias_secrets['domain']}"
+    headers = {
+        "Authorization": f"Basic {gorgias_secrets['api_key']}",
+    }
+
+    customer_response = requests.get(
+        url=f"{base_url}/api/customers?email={gorgias_erasure_identity_email}",
+        headers=headers,
+    )
+
+    # we expect 404 if customer doesn't exist
+    if 404 == customer_response.status_code:
+        return None
+
+    return customer_response.json()
+
+@pytest.fixture(scope="function")
+def gorgias_create_erasure_data(
+    gorgias_connection_config: ConnectionConfig, gorgias_erasure_identity_email: str
+) -> None:
+
+    # sleep(60)
+
+    gorgias_secrets = gorgias_connection_config.secrets
+    base_url = f"https://{gorgias_secrets['domain']}"
+    headers = {
+        "Authorization": f"Basic {gorgias_secrets['api_key']}",
+    }
+
+    # user
+    body = {
+            "name": "Ethyca Test Erasure",
+            "emailAddress": gorgias_erasure_identity_email,
+    }
+
+    users_response = requests.post(url=f"{base_url}/api/customers", headers=headers, json=body)
+    user = users_response.json()
+
+    sleep(30)
+
+    error_message = f"customer with email {gorgias_erasure_identity_email} could not be added to gorgias"
+    poll_for_existence(
+        customer_exists,
+        (gorgias_erasure_identity_email, gorgias_secrets),
+        error_message=error_message,
+    )
+
+    yield user
---
+++ tests/ops/integration_tests/saas/test_gorgias_task.py
@@ -0,0 +1,22 @@
+import random
+import time
+
+import pytest
+import requests
+
+from fides.api.ops.graph.graph import DatasetGraph
+from fides.api.ops.models.privacy_request import PrivacyRequest
+from fides.api.ops.schemas.redis_cache import Identity
+from fides.api.ops.service.connectors import get_connector
+from fides.api.ops.task import graph_task
+from fides.api.ops.task.graph_task import get_cached_data_for_erasures
+from fides.core.config import get_config
+from tests.ops.graph.graph_test_util import assert_rows_match
+
+CONFIG = get_config()
+
+
+@pytest.mark.integration_saas
+@pytest.mark.integration_gorgias
+def test_gorgias_connection_test(gorgias_connection_config) -> None:
+    get_connector(gorgias_connection_config).test_connection()
