4da4f612e1c352aba248542147ebc722e2780ea1
Sebastian Sangervasi <sebastian@ethyca.com>
2023-02-01 12:52:26 -0800

    ui/privacy-requests: ApproveButton and DenyButton components

---
+++ clients/admin-ui/src/features/privacy-requests/buttons/ApproveButton.tsx
@@ -0,0 +1,53 @@
+import { ButtonProps, forwardRef, useDisclosure } from "@fidesui/react";
+import { ReactNode } from "react";
+
+import ApprovePrivacyRequestModal from "~/features/privacy-requests/ApprovePrivacyRequestModal";
+import { useMutations } from "~/features/privacy-requests/hooks/useMutations";
+import { PrivacyRequestEntity } from "~/features/privacy-requests/types";
+
+import { StyledButton } from "./StyledButton";
+
+type ApproveButtonProps = {
+  buttonProps?: ButtonProps;
+  children?: ReactNode;
+  onClose?: () => void;
+  subjectRequest: PrivacyRequestEntity;
+};
+
+const ApproveButton = forwardRef<ApproveButtonProps, "button">(
+  ({ buttonProps, children, onClose, subjectRequest }, ref) => {
+    const { handleApproveRequest, isLoading } = useMutations({
+      subjectRequest,
+    });
+
+    const modal = useDisclosure();
+    const handleClose = () => {
+      modal.onClose();
+      onClose?.();
+    };
+
+    return (
+      <>
+        <StyledButton
+          ref={ref}
+          onClick={modal.onOpen}
+          isLoading={isLoading}
+          isDisabled={isLoading}
+          {...buttonProps}
+          data-testid="privacy-request-approve-btn"
+        >
+          {children}
+        </StyledButton>
+
+        <ApprovePrivacyRequestModal
+          isOpen={modal.isOpen}
+          isLoading={isLoading}
+          onClose={handleClose}
+          onApproveRequest={handleApproveRequest}
+        />
+      </>
+    );
+  }
+);
+
+export default ApproveButton;
