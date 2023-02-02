00c6f5d6acd6c150f9066b21be11aec44e9b69eb
Andrew Jackson <andrew.c.j1995@gmail.com>
2023-02-02 14:15:10 -0500

    Update migration to delete duplicate users

--- src/fides/api/ctl/migrations/versions/643249f65453_case_insensitive_usernames_delete_.py
+++ src/fides/api/ctl/migrations/versions/643249f65453_case_insensitive_usernames_delete_.py
@@ -6,13 +6,15 @@ Create Date: 2023-02-01 22:22:38.055862
 
 """
 from alembic import op
-import sqlalchemy as sa
-import sqlalchemy.types as sa_types
+from sqlalchemy.orm.session import Session
+from sqlalchemy.sql import text
 
+from fides.lib.models.fides_user import FidesUser
+from fides.lib.models.fides_user_permissions import FidesUserPermissions
 
 # revision identifiers, used by Alembic.
-revision = '643249f65453'
-down_revision = '392992c7733a'
+revision = "643249f65453"
+down_revision = "392992c7733a"
 branch_labels = None
 depends_on = None
 
@@ -21,13 +23,26 @@ def upgrade():
 
     # Found here in the 24.2.2.4. Nondeterministic Collations section:
     # https://www.postgresql.org/docs/current/collation.html
-    op.execute("CREATE COLLATION case_insensitive (provider = icu, locale = 'und-u-ks-level2', deterministic = false);")
-    op.alter_column('fidesuser','username', type_=sa_types.String(collation='case_insensitive'), nullable=True)
+    op.execute(
+        "CREATE COLLATION IF NOT EXISTS case_insensitive (provider = icu, locale = 'und-u-ks-level2', deterministic = false);"
+    )
+    session = Session(bind=op.get_bind())
+    conn = session.connection()
+    statement = text(
+        """SELECT DISTINCT ON (username) username collate case_insensitive, created_at, id
+FROM fidesuser
+ORDER BY username, created_at ASC"""
+    )
 
+    result = conn.execute(statement)
+    original_user_ids = [row[2] for row in result]
+    session.query(FidesUserPermissions).filter(
+        FidesUserPermissions.user_id.not_in(original_user_ids)
+    ).delete()
+    session.query(FidesUser).filter(FidesUser.id.not_in(original_user_ids)).delete()
 
-
+    op.execute("DROP COLLATION IF EXISTS case_insensitive")
 
 
 def downgrade():
-    op.alter_column('fidesuser','username', type_=sa_types.String(), nullable=True)
-    op.execute("DROP COLLATION IF EXISTS case_insensitive")
+    pass
