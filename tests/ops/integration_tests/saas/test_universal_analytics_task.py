cdb68422a64e0938fb56f862e967a1f961e13eac
Dawn Pattison <pattisdr@users.noreply.github.com>
2023-02-02 08:53:43 -0600

    Add a Universal Analytics Consent Connector (#2472)
    - Similar to the GA4 connector except it works with UA Tracking Ids.
    - Adds Friendbuy, Recharge, GA, and Mailchimp Transactional logos

--- tests/ops/integration_tests/saas/test_universal_analytics_task.py
+++ tests/ops/integration_tests/saas/test_universal_analytics_task.py
@@ -19,24 +19,24 @@ from fides.api.ops.task import graph_task
 
 
 @pytest.mark.integration_saas
-@pytest.mark.integration_google_analytics
+@pytest.mark.integration_universal_analytics
 @pytest.mark.skip(reason="Currently unable to test OAuth2 connectors")
-def test_google_analytics_connection_test(
-    google_analytics_connection_config,
+def test_universal_analytics_connection_test(
+    universal_analytics_connection_config,
 ) -> None:
-    get_connector(google_analytics_connection_config).test_connection()
+    get_connector(universal_analytics_connection_config).test_connection()
 
 
 @pytest.mark.integration_saas
-@pytest.mark.integration_google_analytics
+@pytest.mark.integration_universal_analytics
 @pytest.mark.asyncio
 @pytest.mark.skip(reason="Currently unable to test OAuth2 connectors")
-async def test_google_analytics_consent_request_task(
+async def test_universal_analytics_consent_request_task(
     db,
     consent_policy,
-    google_analytics_connection_config,
-    google_analytics_dataset_config,
-    google_analytics_client_id,
+    universal_analytics_connection_config,
+    universal_analytics_dataset_config,
+    universal_analytics_client_id,
 ) -> None:
     """Full consent request based on the Google Analytics SaaS config"""
 
@@ -45,17 +45,17 @@ async def test_google_analytics_consent_request_task(
         consent_preferences=[{"data_use": "advertising", "opt_in": False}],
     )
 
-    identity = Identity(**{"ga_client_id": google_analytics_client_id})
+    identity = Identity(**{"ga_client_id": universal_analytics_client_id})
     privacy_request.cache_identity(identity)
 
-    dataset_name = "google_analytics_instance"
+    dataset_name = "universal_analytics_instance"
 
     v = await graph_task.run_consent_request(
         privacy_request,
         consent_policy,
-        build_consent_dataset_graph([google_analytics_dataset_config]),
-        [google_analytics_connection_config],
-        {"ga_client_id": google_analytics_client_id},
+        build_consent_dataset_graph([universal_analytics_dataset_config]),
+        [universal_analytics_connection_config],
+        {"ga_client_id": universal_analytics_client_id},
         db,
     )
 
@@ -67,17 +67,17 @@ async def test_google_analytics_consent_request_task(
         privacy_request_id=privacy_request.id
     )
 
-    google_analytics_logs = execution_logs.filter_by(
+    universal_analytics_logs = execution_logs.filter_by(
         collection_name=dataset_name
     ).order_by("created_at")
-    assert google_analytics_logs.count() == 2
+    assert universal_analytics_logs.count() == 2
 
-    assert [log.status for log in google_analytics_logs] == [
+    assert [log.status for log in universal_analytics_logs] == [
         ExecutionLogStatus.in_processing,
         ExecutionLogStatus.complete,
     ]
 
-    for log in google_analytics_logs:
+    for log in universal_analytics_logs:
         assert log.dataset_name == dataset_name
         assert (
             log.collection_name == dataset_name
@@ -86,17 +86,17 @@ async def test_google_analytics_consent_request_task(
 
 
 @pytest.mark.integration_saas
-@pytest.mark.integration_google_analytics
+@pytest.mark.integration_universal_analytics
 @pytest.mark.asyncio
 @pytest.mark.skip(reason="Currently unable to test OAuth2 connectors")
 @mock.patch("fides.api.ops.service.connectors.saas_connector.AuthenticatedClient.send")
-async def test_google_analytics_consent_prepared_requests(
+async def test_universal_analytics_consent_prepared_requests(
     mocked_client_send,
     db,
     consent_policy,
-    google_analytics_connection_config,
-    google_analytics_dataset_config,
-    google_analytics_client_id,
+    universal_analytics_connection_config,
+    universal_analytics_dataset_config,
+    universal_analytics_client_id,
 ) -> None:
     """Assert attributes of the PreparedRequest created by the client for running the consent request"""
 
@@ -105,15 +105,15 @@ async def test_google_analytics_consent_prepared_requests(
         consent_preferences=[{"data_use": "advertising", "opt_in": False}],
     )
 
-    identity = Identity(**{"ga_client_id": google_analytics_client_id})
+    identity = Identity(**{"ga_client_id": universal_analytics_client_id})
     privacy_request.cache_identity(identity)
 
     await graph_task.run_consent_request(
         privacy_request,
         consent_policy,
-        build_consent_dataset_graph([google_analytics_dataset_config]),
-        [google_analytics_connection_config],
-        {"ga_client_id": google_analytics_client_id},
+        build_consent_dataset_graph([universal_analytics_dataset_config]),
+        [universal_analytics_connection_config],
+        {"ga_client_id": universal_analytics_client_id},
         db,
     )
 
@@ -125,23 +125,23 @@ async def test_google_analytics_consent_prepared_requests(
     )
     body = mocked_client_send.call_args[0][0].body
 
-    assert google_analytics_client_id in body
-    assert google_analytics_connection_config.secrets["property_id"] in body
+    assert universal_analytics_client_id in body
+    assert universal_analytics_connection_config.secrets["web_property_id"] in body
 
 
 @pytest.mark.integration_saas
-@pytest.mark.integration_google_analytics
+@pytest.mark.integration_universal_analytics
 @pytest.mark.asyncio
 @pytest.mark.skip(reason="Currently unable to test OAuth2 connectors")
 @mock.patch("fides.api.ops.service.connectors.saas_connector.AuthenticatedClient.send")
-async def test_google_analytics_no_ga_client_id(
+async def test_universal_analytics_no_ga_client_id(
     mocked_client_send,
     db,
     consent_policy,
-    google_analytics_connection_config,
-    google_analytics_dataset_config,
+    universal_analytics_connection_config,
+    universal_analytics_dataset_config,
 ) -> None:
-    """Test that the google analytics connector does not fail if there is no ga_client_id
+    """Test that the universal analytics connector does not fail if there is no ga_client_id
     We skip the request because it is marked as skip_missing_param_values=True.
 
     We won't always have this piece of identity data.
@@ -151,13 +151,13 @@ async def test_google_analytics_no_ga_client_id(
         id=str(uuid4()),
         consent_preferences=[{"data_use": "advertising", "opt_in": False}],
     )
-    dataset_name = "google_analytics_instance"
+    dataset_name = "universal_analytics_instance"
 
     v = await graph_task.run_consent_request(
         privacy_request,
         consent_policy,
-        build_consent_dataset_graph([google_analytics_dataset_config]),
-        [google_analytics_connection_config],
+        build_consent_dataset_graph([universal_analytics_dataset_config]),
+        [universal_analytics_connection_config],
         {},
         db,
     )
