b26eeeaffddd10a262f7eb4d88ae5878c25e63ab
Sebastian Sangervasi <sebastian@ethyca.com>
2023-02-02 16:39:25 -0800

    pc/types: API models for consent preferences

---
+++ clients/privacy-center/types/api/models/Consent.ts
@@ -0,0 +1,12 @@
+/* istanbul ignore file */
+/* tslint:disable */
+/* eslint-disable */
+
+/**
+ * Schema for consent.
+ */
+export type Consent = {
+  data_use: string;
+  data_use_description?: string;
+  opt_in: boolean;
+};
