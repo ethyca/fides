439ab852985b62ef2a55a85e00dbf8a0f8a50ecf
Andrew Jackson <andrew.c.j1995@gmail.com>
2023-02-06 14:48:01 -0500

    Fix migrations
    The issue was postgres didn't know what to cast the array to when it was null

--- src/fides/api/ctl/migrations/versions/643249f65453_case_insensitive_usernames_delete_.py
+++ src/fides/api/ctl/migrations/versions/643249f65453_case_insensitive_usernames_delete_.py
@@ -33,12 +33,12 @@ ORDER BY LOWER(username), created_at ASC"""
     original_user_ids = [row[2] for row in result]
     op.execute(
         f"""DELETE FROM fidesuserpermissions
-    WHERE NOT(user_id=ANY(ARRAY[{original_user_ids}]))"""
+    WHERE NOT(user_id=ANY(ARRAY[{original_user_ids}]::varchar[]))"""
     )
 
     op.execute(
         f"""DELETE FROM fidesuser 
-    WHERE NOT(id=ANY(ARRAY[{original_user_ids}]))"""
+    WHERE NOT(id=ANY(ARRAY[{original_user_ids}]::varchar[]))"""
     )
 
     op.execute("CREATE EXTENSION IF NOT EXISTS citext;")
