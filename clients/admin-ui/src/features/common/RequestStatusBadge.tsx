6d6e2d91d597a49f100225a8de2c4c2d86758a3c
Dawn Pattison <pattisdr@users.noreply.github.com>
2023-02-07 06:20:57 -0600

    Add new request status to the UI.

--- clients/admin-ui/src/features/common/RequestStatusBadge.tsx
+++ clients/admin-ui/src/features/common/RequestStatusBadge.tsx
@@ -13,6 +13,10 @@ export const statusPropMap: {
     bg: "green.500",
     label: "Completed",
   },
+  awaiting_consent_email_send: {
+    bg: "gray.400",
+    label: "Awaiting Email Send",
+  },
   denied: {
     bg: "red.500",
     label: "Denied",
