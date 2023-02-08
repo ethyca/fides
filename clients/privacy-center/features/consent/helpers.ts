5474983b70c234b70fb41e49cfdec88e7faa4cd8
Sebastian Sangervasi <sebastian@ethyca.com>
2023-02-07 16:33:14 -0800

    pc/consent: helpers now only needs makeCookieKeyConsent

--- clients/privacy-center/features/consent/helpers.ts
+++ clients/privacy-center/features/consent/helpers.ts
@@ -1,85 +1,32 @@
 import {
+  ConsentContext,
   CookieKeyConsent,
-  getConsentContext,
   resolveConsentValue,
 } from "fides-consent";
+
 import { ConfigConsentOption } from "~/types/config";
+import { FidesKeyToConsent } from "./types";
 
-import { ConsentItem, ApiUserConsents, ApiUserConsent } from "./types";
-
-export const makeConsentItems = (
-  data: ApiUserConsents,
-  consentOptions: ConfigConsentOption[]
-): ConsentItem[] => {
-  const consentContext = getConsentContext();
-
-  if (data.consent) {
-    const newConsentItems: ConsentItem[] = [];
-    const userConsentMap: { [key: string]: ApiUserConsent } = {};
-    data.consent.forEach((option) => {
-      const key = option.data_use;
-      userConsentMap[key] = option;
-    });
-    consentOptions.forEach((d) => {
-      const defaultValue = resolveConsentValue(d.default, consentContext);
-
-      if (d.fidesDataUseKey in userConsentMap) {
-        const currentConsent = userConsentMap[d.fidesDataUseKey];
-
-        newConsentItems.push({
-          defaultValue,
-          consentValue: currentConsent.opt_in,
-          description: currentConsent.data_use_description
-            ? currentConsent.data_use_description
-            : d.description,
-          fidesDataUseKey: currentConsent.data_use,
-          highlight: d.highlight ?? false,
-          name: d.name,
-          url: d.url,
-          cookieKeys: d.cookieKeys ?? [],
-          executable: d.executable ?? false,
-        });
-      } else {
-        newConsentItems.push({
-          defaultValue,
-          fidesDataUseKey: d.fidesDataUseKey,
-          name: d.name,
-          description: d.description,
-          highlight: d.highlight ?? false,
-          url: d.url,
-          cookieKeys: d.cookieKeys ?? [],
-          executable: d.executable ?? false,
-        });
-      }
-    });
-
-    return newConsentItems;
-  }
-
-  return consentOptions.map((option) => ({
-    fidesDataUseKey: option.fidesDataUseKey,
-    name: option.name,
-    description: option.description,
-    highlight: option.highlight ?? false,
-    url: option.url,
-    defaultValue: resolveConsentValue(option.default, consentContext),
-    cookieKeys: option.cookieKeys ?? [],
-    executable: option.executable ?? false,
-  }));
-};
-
-export const makeCookieKeyConsent = (
-  consentItems: ConsentItem[]
-): CookieKeyConsent => {
+export const makeCookieKeyConsent = ({
+  consentOptions,
+  fidesKeyToConsent,
+  consentContext,
+}: {
+  consentOptions: ConfigConsentOption[];
+  fidesKeyToConsent: FidesKeyToConsent;
+  consentContext: ConsentContext;
+}): CookieKeyConsent => {
   const cookieKeyConsent: CookieKeyConsent = {};
-  consentItems.forEach((item) => {
-    const consent =
-      item.consentValue === undefined ? item.defaultValue : item.consentValue;
+  consentOptions.forEach((option) => {
+    const defaultValue = resolveConsentValue(option.default, consentContext);
+    const value = fidesKeyToConsent[option.fidesDataUseKey] ?? defaultValue;
 
-    item.cookieKeys?.forEach((cookieKey) => {
+    option.cookieKeys?.forEach((cookieKey) => {
       const previousConsent = cookieKeyConsent[cookieKey];
+      // For a cookie key to have consent, _all_ data uses that target that cookie key
+      // must have consent.
       cookieKeyConsent[cookieKey] =
-        previousConsent === undefined ? consent : previousConsent && consent;
+        previousConsent === undefined ? value : previousConsent && value;
     });
   });
   return cookieKeyConsent;
