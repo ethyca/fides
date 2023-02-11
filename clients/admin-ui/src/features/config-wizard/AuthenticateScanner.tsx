db8e00451cc70c244ef2ba4cf0f8f7ac47629604
Andrew Jackson <andrew.c.j1995@gmail.com>
2023-02-11 16:19:05 -0500

    Format again

--- clients/admin-ui/src/features/config-wizard/AuthenticateScanner.tsx
+++ clients/admin-ui/src/features/config-wizard/AuthenticateScanner.tsx
@@ -13,10 +13,12 @@ import { SystemMethods } from "./types";
 const AuthenticateScanner = () => {
   const infrastructure = useAppSelector(selectAddSystemsMethod);
 
-
-
-
-  const width = [SystemMethods.DATA_FLOW, ValidTargets.WEB_SCANNER].indexOf(infrastructure) > -1 ? "100%" : "40%"
+  const width =
+    [SystemMethods.DATA_FLOW, ValidTargets.WEB_SCANNER].indexOf(
+      infrastructure
+    ) > -1
+      ? "100%"
+      : "40%";
 
   return (
     <Box w={width}>
@@ -24,13 +26,15 @@ const AuthenticateScanner = () => {
       {infrastructure === ValidTargets.OKTA ? <AuthenticateOktaForm /> : null}
       {infrastructure === ValidTargets.WEB_SCANNER ? <LoadWebScanner /> : null}
       {/*
-      * Data flow scanner currently authenticates via fidesctl.toml, so there is not
-      * an authentication step. However, to fit into the onboarding flow, it makes sense to
-      * load this at the same time as authentication since the other authenticate forms also
-      * show a loading screen. At least until each path has its own custom steps it goes through
-      * (fides#1514)
-      */}
-      {infrastructure === SystemMethods.DATA_FLOW ? <LoadDataFlowScanner /> : null}
+       * Data flow scanner currently authenticates via fidesctl.toml, so there is not
+       * an authentication step. However, to fit into the onboarding flow, it makes sense to
+       * load this at the same time as authentication since the other authenticate forms also
+       * show a loading screen. At least until each path has its own custom steps it goes through
+       * (fides#1514)
+       */}
+      {infrastructure === SystemMethods.DATA_FLOW ? (
+        <LoadDataFlowScanner />
+      ) : null}
     </Box>
   );
 };
