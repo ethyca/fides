fd2f4bcb7c1e02d8c80dd93fd51dab971e2a5b45
LKCSmith <99051851+LKCSmith@users.noreply.github.com>
2023-02-13 23:48:27 -0600

    Config onClick

--- clients/admin-ui/src/features/privacy-requests/configuration/ConfigurationNotificationBanner.tsx
+++ clients/admin-ui/src/features/privacy-requests/configuration/ConfigurationNotificationBanner.tsx
@@ -1,39 +1,43 @@
 import { Box, Button, Stack, Text } from "@fidesui/react";
+import { useRouter } from "next/router";
 
-const ConfigurationNotificationBanner = () => (
-  <Box
-    bg="gray.50"
-    border="1px solid"
-    borderColor="blue.400"
-    borderRadius="md"
-    justifyContent="space-between"
-    p={5}
-    mb={5}
-    mt={5}
-  >
-    <Box>
-      <Stack
-        direction={{ base: "column", sm: "row" }}
-        justifyContent="space-between"
-      >
-        <Text fontWeight="semibold">
-          {/* ICON */}
-          Configure your storage and messaging provider
-        </Text>
-        <Button
-          size="sm"
-          variant="outline"
-          // onClick route to cofiguration page
+const ConfigurationNotificationBanner = () => {
+  const router = useRouter();
+
+  const handleClick = () => {
+    router.push("/privacy-requests/configure");
+  };
+
+  return (
+    <Box
+      bg="gray.50"
+      border="1px solid"
+      borderColor="blue.400"
+      borderRadius="md"
+      justifyContent="space-between"
+      p={5}
+      mb={5}
+      mt={5}
+    >
+      <Box>
+        <Stack
+          direction={{ base: "column", sm: "row" }}
+          justifyContent="space-between"
         >
-          Configure
-        </Button>
-      </Stack>
+          <Text fontWeight="semibold">
+            Configure your storage and messaging provider
+          </Text>
+          <Button size="sm" variant="outline" onClick={handleClick}>
+            Configure
+          </Button>
+        </Stack>
 
-      <Text color="muted">
-        Before Fides can process your privacy requests we need two simple steps
-        to configure your storage and email client.{" "}
-      </Text>
+        <Text>
+          Before Fides can process your privacy requests we need two simple
+          steps to configure your storage and email client.{" "}
+        </Text>
+      </Box>
     </Box>
-  </Box>
-);
+  );
+};
 export default ConfigurationNotificationBanner;
