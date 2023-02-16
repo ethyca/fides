98241b0b0e7fbe3b6dd5b95c4130359afa76cc15
Thomas <thomas.lapiana+github@pm.me>
2023-02-16 15:18:06 +0800

    feat: add fields to notification and admin ui settings

--- src/fides/core/config/admin_ui_settings.py
+++ src/fides/core/config/admin_ui_settings.py
@@ -1,10 +1,13 @@
 from .fides_settings import FidesSettings
+from pydantic import Field
 
 
 class AdminUISettings(FidesSettings):
     """Configuration settings for Analytics variables."""
 
-    enabled: bool = True
+    enabled: bool = Field(
+        default=True, description="Toggle whether the Admin UI is served."
+    )
 
     class Config:
         env_prefix = "FIDES__ADMIN_UI__"
--- src/fides/core/config/notification_settings.py
+++ src/fides/core/config/notification_settings.py
@@ -1,6 +1,6 @@
 from typing import Optional
 
-from pydantic import validator
+from pydantic import validator, Field
 
 from .fides_settings import FidesSettings
 
@@ -10,10 +10,16 @@ ENV_PREFIX = "FIDES__NOTIFICATIONS__"
 class NotificationSettings(FidesSettings):
     """Configuration settings for data subject and/or data processor notifications"""
 
-    send_request_completion_notification: bool = False
-    send_request_receipt_notification: bool = False
-    send_request_review_notification: bool = False
-    notification_service_type: Optional[str] = None
+    notification_service_type: Optional[str] = Field(default=None, description="Sets the notification service type used to send notifications. Accepts mailgun, twilio_sms, or twilio_email.")
+    send_request_completion_notification: bool = Field(
+        default=False,
+        description="When set to True, enables subject notifications upon privacy request completion.",
+    )
+    send_request_receipt_notification: bool = Field(
+        default=False,
+        description="When set to True, enables subject notifications upon privacy request receipt.",
+    )
+    send_request_review_notification: bool = Field(default=False, description="When set to True, enables subject notifications upon privacy request review.")
 
     @validator("notification_service_type", pre=True)
     @classmethod
