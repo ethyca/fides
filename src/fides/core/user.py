3c12275af9d64c304f9dcaba9b35140c46a6c9a3
Thomas <thomas.lapiana+github@pm.me>
2023-02-16 02:07:40 -0800

    Convert `get_config()` to direct imports of `CONFIG`

--- src/fides/core/user.py
+++ src/fides/core/user.py
@@ -10,12 +10,11 @@ import toml
 from pydantic import BaseModel
 
 from fides.cli.utils import handle_cli_response
-from fides.core.config import get_config
+from fides.core.config import CONFIG
 from fides.core.utils import echo_green, echo_red
 from fides.lib.cryptography.cryptographic_util import str_to_b64_str
 from fides.lib.oauth.scopes import SCOPES
 
-config = get_config()
 CREATE_USER_PATH = "/api/v1/user"
 LOGIN_PATH = "/api/v1/login"
 USER_PERMISSIONS_PATH = "/api/v1/user/{}/permission"
