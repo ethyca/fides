415bbd1f7adbe1c25e360719b6f53e7cabbed8c3
Thomas <ThomasLaPiana@users.noreply.github.com>
2023-02-21 22:31:24 +0800

    fix: make sure default verbose path gets checked

--- tests/ctl/core/test_config.py
+++ tests/ctl/core/test_config.py
@@ -20,7 +20,7 @@ REQUIRED_ENV_VARS = {
 @pytest.mark.unit
 def test_get_config_verbose() -> None:
     """Simple test to check the 'verbose' code path."""
-    config = get_config(verbose=True)
+    config = get_config(verbose=True, config_path_override="fakefiledoesntexist.toml")
     assert config
 
 
