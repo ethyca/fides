9c84d7c6440ad45f5f61bc326208a4ce8e69e001
Thomas <ThomasLaPiana@users.noreply.github.com>
2023-02-21 14:47:22 +0800

    fix: placement of no cover comment

--- src/fides/core/config/__init__.py
+++ src/fides/core/config/__init__.py
@@ -183,8 +183,8 @@ def get_config(config_path_override: str = "", verbose: bool = False) -> FidesCo
     except IOError:
         echo_red(f"Error reading config file: {config_path}")
 
-    if verbose:
-        print("Using default configuration values.") # pragma: no cover
+    if verbose:  # pragma: no cover
+        print("Using default configuration values.")
     config = build_config(config_dict={})
 
     return config
