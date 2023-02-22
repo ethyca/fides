6e2129b02492f0f5bd8311165cbaa548e71b77f1
Adrian Galvan <adrian@ethyca.com>
2023-02-22 11:33:37 -0800

    Defaulting oauth_instance config to None

--- src/fides/core/config/__init__.py
+++ src/fides/core/config/__init__.py
@@ -58,7 +58,7 @@ class FidesConfig(FidesSettings):
         exclude=True,
     )
     oauth_instance: Optional[str] = Field(
-        default=getenv("FIDES__OAUTH_INSTANCE"),
+        default=None,
         description="A value that is prepended to the generated 'state' param in outbound OAuth2 authorization requests. Used during OAuth2 testing to associate callback responses back to this specific Fides instance.",
         exclude=True,
     )
