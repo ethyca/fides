3c587aa8f62b15db4ceb1e9764d81fa2232e4496
eastandwestwind <eastandwestwind@gmail.com>
2023-03-27 19:20:17 +0200

    removing e2e tests for phone input

--- clients/privacy-center/cypress/e2e/privacy-request.cy.ts
+++ clients/privacy-center/cypress/e2e/privacy-request.cy.ts
@@ -13,35 +13,6 @@ describe("Privacy request", () => {
       ).as("postPrivacyRequestVerify");
     });
 
-    it("can verify phone and navigate to form", () => {
-      cy.visit("/");
-      cy.getByTestId("card").contains("Access your data").click();
-
-      cy.getByTestId("privacy-request-form").within(() => {
-
-        cy.get("input#phone").type("555 867 5309");
-        cy.get("select[name=phoneCountry]").should("have.value", "US");
-        cy.get("input#phone").clear().type("+44 55 8675 3090");
-        cy.get("select[name=phoneCountry]").should("have.value", "GB");
-
-        cy.get("button").contains("Continue").click();
-      });
-      cy.wait("@postPrivacyRequest").then((interception) => {
-        expect(interception.request.body[0].identity).to.eql({
-          email: "",
-          phone_number: "+445586753090",
-        });
-      });
-
-      cy.getByTestId("verification-form").within(() => {
-        cy.get("input").type("112358");
-        cy.get("button").contains("Submit code").click();
-      });
-      cy.wait("@postPrivacyRequestVerify");
-
-      cy.getByTestId("request-submitted");
-    });
-
     it("requires valid inputs", () => {
       cy.visit("/");
       cy.getByTestId("card").contains("Access your data").click();
@@ -50,31 +21,16 @@ describe("Privacy request", () => {
         // This block uses `.root()` to keep queries within the form. This is necessary because of
         // `.blur()` which triggers input validation.
 
-        // test email being typed, phone becoming disabled, continue becoming disabled due to invalid email
+        // test email being typed, continue becoming disabled due to invalid email
         cy.root().get("input#email").type("invalid email").blur();
         cy.root().should("contain", "Email is invalid");
-        cy.root().get("input#phone").should("be.disabled");
         cy.root().get("button").contains("Continue").should("be.disabled");
         cy.root().get("input#email").clear().blur();
 
-        // test phone being typed, email becoming disabled, continue becoming disabled due to invalid phone
-        cy.root().get("input#phone").type("123 456 7890 1234567").blur();
-        cy.root().should("contain", "Phone is invalid");
-        cy.root().get("input#email").should("be.disabled");
-        cy.root().get("button").contains("Continue").should("be.disabled");
-        cy.root().get("input#phone").clear().blur();
-
-        // test valid email, phone becoming disabled, continue becoming enabled due to valid email
+        // test valid email, continue becoming enabled due to valid email
         cy.root().get("input#email").type("valid@example.com").blur();
         cy.root().get("button").contains("Continue").should("be.enabled");
-        cy.root().get("input#phone").should("be.disabled");
         cy.root().get("input#email").clear().blur();
-
-        // test valid phone, email becoming disabled, continue becoming enabled due to valid phone
-        cy.root().get("input#phone").type("123 456 7890").blur();
-        cy.root().get("button").contains("Continue").should("be.enabled");
-        cy.root().get("input#email").should("be.disabled");
-        cy.root().get("input#phone").clear().blur();
       });
     });
   });
