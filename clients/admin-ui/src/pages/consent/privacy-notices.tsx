0e279a5272f0ed830fdf4a8e5c6cb1828dd9393e
Allison King <allisonjuliaking@gmail.com>
2023-04-03 15:02:36 -0400

    Add preliminary privacy notice page and flag

---
+++ clients/admin-ui/src/pages/consent/privacy-notices.tsx
@@ -0,0 +1,11 @@
+import { Box } from "@fidesui/react";
+
+import Layout from "~/features/common/Layout";
+
+const PrivacyNoticesPage = () => (
+  <Layout title="Privacy notices">
+    <Box>Work in progress, check back later!</Box>
+  </Layout>
+);
+
+export default PrivacyNoticesPage;
