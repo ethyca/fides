548c4ae7c1c21c4906a93517038cc62a9df94efe
Allison King <allisonjuliaking@gmail.com>
2023-04-04 15:17:29 -0400

    Restore nav-config

---
+++ clients/admin-ui/src/features/common/nav/v2/routes.ts
@@ -0,0 +1,19 @@
+// Data map group
+export const ADD_SYSTEMS_ROUTE = "/add-systems";
+export const ADD_SYSTEMS_MANUAL_ROUTE = "/add-systems/manual";
+export const DATAMAP_ROUTE = "/plus/datamap";
+export const SYSTEM_ROUTE = "/systems";
+export const CLASSIFY_SYSTEMS_ROUTE = "/classify-systems";
+export const DATASET_ROUTE = "/dataset";
+
+// Privacy requests group
+export const DATASTORE_CONNECTION_ROUTE = "/datastore-connection";
+export const PRIVACY_NOTICES_ROUTE = "/consent/privacy-notices";
+export const PRIVACY_REQUESTS_ROUTE = "/privacy-requests";
+export const PRIVACY_REQUESTS_CONFIGURATION_ROUTE = `${PRIVACY_REQUESTS_ROUTE}/configure`;
+
+// Management group
+export const USER_MANAGEMENT_ROUTE = "/user-management";
+export const ORGANIZATION_MANAGEMENT_ROUTE = "/management/organization";
+export const TAXONOMY_ROUTE = "/taxonomy";
+export const ABOUT_ROUTE = "/management/about";
