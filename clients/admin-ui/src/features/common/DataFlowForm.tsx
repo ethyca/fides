3a78ea56cb5f44adcfda61bdb67d5ae72a94cf11
Andrew Jackson <andrew.c.j1995@gmail.com>
2023-04-05 20:18:49 -0400

    Improve accordion

--- clients/admin-ui/src/features/common/DataFlowForm.tsx
+++ clients/admin-ui/src/features/common/DataFlowForm.tsx
@@ -4,17 +4,34 @@ import {
   AccordionIcon,
   AccordionItem,
   AccordionPanel,
-  Box,
   Flex,
-  Heading,
   Tag,
   Text,
   Button,
+  Spacer,
+  Divider,
+  Box,
 } from "@fidesui/react";
 import { System } from "~/types/api/models/System";
 import { DataFlow } from "~/types/api";
 import React from "react";
 import { GearLightIcon } from "common/Icon";
+import QuestionTooltip from "common/QuestionTooltip";
+import { useGetAllSystemsQuery } from "~/features/system";
+
+type DataFlowItemProps = {
+  dataFlow: DataFlow;
+};
+
+const DataFlowItem = ({ dataFlow }: DataFlowItemProps) => {
+  const { data: system } = useGetAllSystemsQuery(undefined, {
+    selectFromResult: ({ data }) => ({
+      data: data?.find((s) => s.fides_key === dataFlow.fides_key),
+    }),
+  });
+
+  return <Box>{system ? system.name : dataFlow.fides_key}</Box>;
+};
 
 type DataFlowAccordionItemProps = {
   isIngress?: boolean;
@@ -25,43 +42,49 @@ const DataFlowAccordionItem = ({
   systems,
   isIngress,
 }: DataFlowAccordionItemProps) => {
-  const flowType = isIngress ? "Source" : "Destination";
+  const flowType = isIngress ? "Sources" : "Destinations";
 
   return (
     <AccordionItem>
-      <AccordionButton>
-        <Box
-          display="flex"
+      <AccordionButton height="44px">
+        <Flex
           alignItems="center"
-          justifyContent="space-between"
+          justifyContent="start"
           flex={1}
           textAlign="left"
         >
-          <Flex>
-            <Heading as="h4" size="xs" fontWeight="medium" mr={4}>
-              {flowType}
-            </Heading>
-            <Tag
-              ml={2}
-              backgroundColor="primary.400"
-              borderRadius="6px"
-              color="white"
-            >
-              {systems.length}
-            </Tag>
-          </Flex>
+          <Text size="sm" lineHeight={5} fontWeight="semibold" mr={4}>
+            {flowType}
+          </Text>
+          <QuestionTooltip label="helpful tip" />
+
+          <Tag
+            ml={2}
+            backgroundColor="primary.400"
+            borderRadius="6px"
+            color="white"
+          >
+            {systems.length}
+          </Tag>
+          <Spacer />
           <AccordionIcon />
-        </Box>
+        </Flex>
       </AccordionButton>
-      <AccordionPanel backgroundColor="gray.50">
-        <Button colorScheme="primary" size="xs" data-testid="add-btn">
-          Configure {flowType}s <GearLightIcon marginLeft={2} />
+      <AccordionPanel backgroundColor="gray.50" padding={6}>
+        <Button
+          colorScheme="primary"
+          size="xs"
+          data-testid="add-btn"
+          marginBottom={4}
+        >
+          Configure {flowType} <GearLightIcon marginLeft={2} />
         </Button>
-        <Text size="xs" lineHeight={4} fontWeight="medium">
+        <Text size="xs" lineHeight={4} fontWeight="medium" marginBottom={3}>
           {flowType} Systems
         </Text>
+        <Divider />
         {systems.map((dataFlow) => (
-          <div key={dataFlow.fides_key}>{dataFlow.fides_key}</div>
+          <DataFlowItem key={dataFlow.fides_key} dataFlow={dataFlow} />
         ))}
       </AccordionPanel>
     </AccordionItem>
