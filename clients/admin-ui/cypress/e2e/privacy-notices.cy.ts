241f6e031f942ab45010bcf5ae63d9da44c15cbd
Allison King <allison@ethyca.com>
2023-04-07 12:09:22 -0400

    Privacy notice table (#3001)

--- clients/admin-ui/cypress/e2e/privacy-notices.cy.ts
+++ clients/admin-ui/cypress/e2e/privacy-notices.cy.ts
@@ -6,6 +6,9 @@ import { RoleRegistryEnum } from "~/types/api";
 describe("Privacy notices", () => {
   beforeEach(() => {
     cy.login();
+    cy.intercept("GET", "/api/v1/privacy-notice/*", {
+      fixture: "privacy-notices/list.json",
+    }).as("getNotices");
     stubPlus(true);
   });
 
@@ -27,6 +30,41 @@ describe("Privacy notices", () => {
         cy.visit(PRIVACY_NOTICES_ROUTE);
         cy.getByTestId("privacy-notices-page");
       });
+    });
+  });
+
+  describe("table", () => {
+    beforeEach(() => {
+      cy.visit(PRIVACY_NOTICES_ROUTE);
+    });
+
+    it("should render a row for each privacy notice", () => {
+      [
+        "Essential",
+        "Functional",
+        "Analytics",
+        "Advertising",
+        "Data Sales",
+      ].forEach((name) => {
+        cy.getByTestId(`row-${name}`);
+      });
+    });
+
+    it("can sort", () => {
+      cy.get("tbody > tr").first().should("contain", "Data Sales");
+      // sort alphabetical
+      cy.getByTestId("column-Title").click();
+      cy.get("tbody > tr").first().should("contain", "Advertising");
+
+      // sort reverse
+      cy.getByTestId("column-Title").click();
+      cy.get("tbody > tr").first().should("contain", "Functional");
+    });
+
+    it("can click a row to go to the notice page", () => {
+      cy.getByTestId("row-Essential").click();
+      cy.getByTestId("privacy-notice-detail-page");
+      cy.url().should("contain", "pri_e76cbe20-6ffa-46b4-9a91-b1ae3412dd49");
     });
   });
 });
---
+++ clients/admin-ui/src/features/privacy-notices/cells.tsx
@@ -0,0 +1,79 @@
+import { Box, Switch, Tag, Text } from "@fidesui/react";
+import { CellProps } from "react-table";
+
+import { PrivacyNoticeResponse } from "~/types/api";
+
+import { MECHANISM_MAP } from "./constants";
+
+export const TitleCell = ({
+  value,
+}: CellProps<PrivacyNoticeResponse, string>) => (
+  <Text fontWeight="semibold" color="gray.600">
+    {value}
+  </Text>
+);
+
+export const WrappedCell = ({
+  value,
+}: CellProps<PrivacyNoticeResponse, string>) => (
+  <Text whiteSpace="normal">{value}</Text>
+);
+
+export const DateCell = ({ value }: CellProps<PrivacyNoticeResponse, string>) =>
+  new Date(value).toDateString();
+
+export const MechanismCell = ({
+  value,
+}: CellProps<PrivacyNoticeResponse, string>) => (
+  <Tag size="sm" backgroundColor="primary.400" color="white">
+    {MECHANISM_MAP.get(value) ?? value}
+  </Tag>
+);
+
+export const MultiTagCell = ({
+  value,
+}: CellProps<PrivacyNoticeResponse, string[]>) => {
+  // If we are over a certain number, render an "..." instead of all of the tags
+  const maxNum = 8;
+  const tags =
+    value.length > maxNum ? [...value.slice(0, maxNum), "..."] : value;
+  return (
+    <Box whiteSpace="normal">
+      {tags.map((v, idx) => (
+        <Tag
+          key={v}
+          size="sm"
+          backgroundColor="primary.400"
+          color="white"
+          mr={idx === value.length - 1 ? 0 : 3}
+          textTransform="uppercase"
+          mb={2}
+        >
+          {v.replace(/_/g, "-")}
+        </Tag>
+      ))}
+    </Box>
+  );
+};
+
+export const ToggleCell = ({
+  value,
+  column,
+  row,
+}: CellProps<PrivacyNoticeResponse, boolean>) => (
+  <Switch
+    colorScheme="complimentary"
+    isChecked={value}
+    onChange={() => {
+      /**
+       * It's difficult to use a custom column in react-table 7 since we'd have to modify
+       * the declaration file. However, that modifies the type globally, so our datamap table
+       * would also have issues. Ignoring the type for now, but should potentially revisit
+       * if we update to react-table 8
+       * https://github.com/DefinitelyTyped/DefinitelyTyped/discussions/59837
+       */
+      // @ts-ignore
+      column.onToggle(row.original.id);
+    }}
+  />
+);
