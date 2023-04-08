2302f040416311d63cc69e590adfa2cadd79ef6c
Adrian Galvan <adrian@ethyca.com>
2023-04-07 17:40:06 -0700

    Updating tests to use AircallTestClient

--- tests/fixtures/saas/aircall_fixtures.py
+++ tests/fixtures/saas/aircall_fixtures.py
@@ -1,10 +1,9 @@
-from time import sleep
-from typing import Any, Dict, Generator
+from typing import Any, Dict, Generator, Optional, cast
 
 import pydash
 import pytest
 import requests
-
+from requests import Response
 from sqlalchemy.orm import Session
 
 from fides.api.ctl.sql_models import Dataset as CtlDataset
@@ -19,6 +18,7 @@ from fides.api.ops.util.saas_util import (
     load_dataset_with_replacement,
 )
 from fides.lib.cryptography import cryptographic_util
+from tests.ops.test_helpers.saas_test_utils import poll_for_existence
 from tests.ops.test_helpers.vault_client import get_secrets
 
 secrets = get_secrets("aircall")
@@ -27,23 +27,19 @@ secrets = get_secrets("aircall")
 @pytest.fixture(scope="session")
 def aircall_secrets(saas_config):
     return {
-        "domain": pydash.get(saas_config, "aircall.domain") or secrets["domain"], 
+        "domain": pydash.get(saas_config, "aircall.domain") or secrets["domain"],
         "api_id": pydash.get(saas_config, "aircall.api_id") or secrets["api_id"],
-        "api_token": pydash.get(saas_config, "aircall.api_token") or secrets["api_token"],
-
+        "api_token": pydash.get(saas_config, "aircall.api_token")
+        or secrets["api_token"],
     }
 
-@pytest.fixture(scope="session")
-def formatted_phone_number(saas_config):  
-    n =  pydash.get(saas_config, "aircall.identity_phone_number") or secrets["identity_phone_number"]  
-    n = n[2:]                                                                                                                         
-    return "+1 "+format(int(n[:-1]), ",").replace(",", "-") + n[-1] 
 
 @pytest.fixture(scope="session")
 def aircall_identity_email(saas_config):
     return (
         pydash.get(saas_config, "aircall.identity_email") or secrets["identity_email"]
     )
+
 
 @pytest.fixture(scope="session")
 def aircall_identity_phone_number(saas_config):
@@ -56,6 +52,7 @@ def aircall_identity_phone_number(saas_config):
 @pytest.fixture(scope="function")
 def aircall_erasure_identity_email() -> str:
     return f"{cryptographic_util.generate_secure_random_string(13)}@email.com"
+
 
 @pytest.fixture(scope="function")
 def aircall_erasure_identity_phone_number() -> str:
@@ -125,34 +122,54 @@ def aircall_dataset_config(
     dataset.delete(db=db)
     ctl_dataset.delete(db=db)
 
+
+class AircallTestClient:
+    def __init__(self, aircall_connection_config: ConnectionConfig):
+        self.aircall_secrets = cast(Dict, aircall_connection_config.secrets)
+        self.auth = self.aircall_secrets["api_id"], self.aircall_secrets["api_token"]
+        self.base_url = f"https://{self.aircall_secrets['domain']}"
+
+    def create_contact(self, phone_number: str, email: str) -> Response:
+        body = {
+            "first_name": "john",
+            "phone_numbers": [{"label": "Office", "value": phone_number}],
+            "emails": [{"label": "Office", "value": email}],
+        }
+
+        return requests.post(
+            url=f"{self.base_url}/v1/contacts", auth=self.auth, json=body
+        )
+
+    def get_contact(self, phone_number: str) -> Optional[Response]:
+        response = requests.get(
+            url=f"{self.base_url}/v1/contacts/search",
+            auth=self.auth,
+            params={"phone_number": phone_number},
+        )
+        if response.json()["contacts"]:
+            return response
+        return None
+
+
+@pytest.fixture(scope="function")
+def aircall_test_client(
+    aircall_connection_config: ConnectionConfig,
+) -> Generator:
+    test_client = AircallTestClient(aircall_connection_config)
+    yield test_client
+
+
 @pytest.fixture(scope="function")
 def aircall_create_erasure_data(
-    aircall_connection_config: ConnectionConfig, aircall_erasure_identity_email: str, aircall_erasure_identity_phone_number: str
-) -> None:
+    aircall_test_client,
+    aircall_erasure_identity_email: str,
+    aircall_erasure_identity_phone_number: str,
+) -> Generator:
+    # create contact
+    aircall_test_client.create_contact(
+        aircall_erasure_identity_phone_number, aircall_erasure_identity_email
+    )
 
-
-    aircall_secrets = aircall_connection_config.secrets
-    base_url = f"https://{aircall_secrets['domain']}"
-    auth = aircall_secrets["api_id"], aircall_secrets["api_token"]
-    
-    # contact
-
-    body = {
-        "first_name":"john",
-        "phone_numbers": [
-            {
-            "label": "test",
-            "value": aircall_erasure_identity_phone_number
-            }
-        ],
-        "emails": [
-            {
-            "label": "Office",
-            "value": aircall_erasure_identity_email
-            }
-        ]
-    }
-
-    contact_response = requests.post(url=f"{base_url}/v1/contacts", auth=auth, json=body)
-    sleep(30)
-    yield contact_response.ok
+    poll_for_existence(
+        aircall_test_client.get_contact, (aircall_erasure_identity_phone_number,)
+    )
--- tests/ops/integration_tests/saas/test_aircall_task.py
+++ tests/ops/integration_tests/saas/test_aircall_task.py
@@ -1,6 +1,6 @@
 import random
+
 import pytest
-import requests
 
 from fides.api.ops.graph.graph import DatasetGraph
 from fides.api.ops.models.privacy_request import PrivacyRequest
@@ -10,6 +10,7 @@ from fides.api.ops.task import graph_task
 from fides.api.ops.task.graph_task import get_cached_data_for_erasures
 from fides.core.config import get_config
 from tests.ops.graph.graph_test_util import assert_rows_match
+from tests.ops.test_helpers.saas_test_utils import poll_for_existence
 
 CONFIG = get_config()
 
@@ -18,6 +19,7 @@ CONFIG = get_config()
 @pytest.mark.integration_aircall
 def test_aircall_connection_test(aircall_connection_config) -> None:
     get_connector(aircall_connection_config).test_connection()
+
 
 @pytest.mark.integration_saas
 @pytest.mark.integration_aircall
@@ -28,7 +30,6 @@ async def test_aircall_access_request_task_with_phone_number(
     aircall_connection_config,
     aircall_dataset_config,
     aircall_identity_phone_number,
-    formatted_phone_number
 ) -> None:
     """Full access request based on the Aircall SaaS config"""
 
@@ -69,42 +70,11 @@ async def test_aircall_access_request_task_with_phone_number(
         ],
     )
 
-    assert_rows_match(
-        v[f"{dataset_name}:calls"],
-        min_size=1,
-        keys=[
-            "id",
-            "direct_link",
-            "direction",
-            "status",
-            "missed_call_reason",
-            "started_at",
-            "answered_at",
-            "ended_at",
-            "duration",
-            "voicemail",
-            "recording",
-            "asset",
-            "raw_digits",
-            "user",
-            "contact",
-            "archived",
-            "assigned_to",
-            "tags",
-            "transferred_by",
-            "transferred_to",
-            "teams",
-            "number",
-            "cost",
-            "country_code_a2",
-            "pricing_type",
-            "comments"
-        ],
-    )
     # verify we only returned data for our identity email
-    assert v[f"{dataset_name}:contact"][0]["phone_numbers"][0]["value"] == aircall_identity_phone_number
-    assert v[f"{dataset_name}:calls"][0]["contact"]['phone_numbers'][0]["value"] == formatted_phone_number
-    
+    assert (
+        v[f"{dataset_name}:contact"][0]["phone_numbers"][0]["value"]
+        == aircall_identity_phone_number
+    )
 
 
 @pytest.mark.integration_saas
@@ -119,6 +89,7 @@ async def test_aircall_erasure_request_task(
     aircall_erasure_identity_phone_number,
     aircall_erasure_identity_email,
     aircall_create_erasure_data,
+    aircall_test_client,
 ) -> None:
     """Full erasure request based on the Aircall SaaS config"""
 
@@ -161,7 +132,7 @@ async def test_aircall_erasure_request_task(
             "emails",
         ],
     )
-    
+
     x = await graph_task.run_erasure(
         privacy_request,
         erasure_policy_string_rewrite,
@@ -177,19 +148,10 @@ async def test_aircall_erasure_request_task(
         f"{dataset_name}:calls": 0,
     }
 
-    aircall_secrets = aircall_connection_config.secrets
-    base_url = f"https://{aircall_secrets['domain']}"
-    auth = aircall_secrets["api_id"], aircall_secrets["api_token"]
-
-    # contact search
-    response = requests.get(
-        url=f"{base_url}/v1/contacts/search",
-        auth=auth,
-        params={"phone_number": aircall_erasure_identity_phone_number},
+    poll_for_existence(
+        aircall_test_client.get_contact,
+        (aircall_erasure_identity_phone_number,),
+        existence_desired=False,
     )
-
-    # Since contact is deleted
-    contact_res = response.json()['contacts']
-    assert len(contact_res) == 0
 
     CONFIG.execution.masking_strict = masking_strict
