6534e74d0d3cb0223ab2f46194a336c92e2964d5
Kelsey Thomas <101993653+Kelsey-Ethyca@users.noreply.github.com>
2023-04-11 19:09:56 -0700

    add consent script to cookie house

--- src/fides/data/sample_project/cookie_house/src/components/Home/index.tsx
+++ src/fides/data/sample_project/cookie_house/src/components/Home/index.tsx
@@ -11,59 +11,65 @@ interface Props {
 }
 
 const Home = ({ products }: Props) => {
-  const [productInPurchase, setProductInPurchase] = useState<Product | null>(null);
+  const [productInPurchase, setProductInPurchase] = useState<Product | null>(
+    null,
+  );
   const [popupTrigger, setPopupTrigger] = useState(0);
   const onCloseModal = useCallback(() => {
     setProductInPurchase(null);
   }, []);
 
   const onSubmit = useCallback(async (data: UserData) => {
-    await (await fetch('/api/purchase', {
-      method: 'POST',
-      body: JSON.stringify(data),
-      headers: new Headers({
-        'Content-Type': 'application/json',
-        Accept: 'application/json',
-      }),
-    })).json();
+    await (
+      await fetch('/api/purchase', {
+        method: 'POST',
+        body: JSON.stringify(data),
+        headers: new Headers({
+          'Content-Type': 'application/json',
+          Accept: 'application/json',
+        }),
+      })
+    ).json();
 
-    setPopupTrigger(v => v + 1);
+    setPopupTrigger((v) => v + 1);
 
     setProductInPurchase(null);
   }, []);
 
   return (
     <div className={css.main}>
-        <Header />
-        <main>
-            <div className={css.products}>
-            {products.map((product) => (
-                <div key={product.id} className={css.product}>
-                    {/* eslint-disable-next-line @next/next/no-img-element */}
-                    <img src={product.url} alt={product.name ?? 'Cookie'} />
-                    <div className={css.name}>{product.name}</div>
-                    <div className={css.description}>{product.description}</div>
-                    <div className={css.price}>{product.price}</div>
-                    <Button color="primary" 
-                      className={css.purchase} 
-                      onClick={() => setProductInPurchase(product)}
-                    >
-                      Purchase
-                    </Button>
-                </div>
-            ))}
+      <script src="http://localhost:3001/fides-consent.js" async></script>
+      <Header />
+      <main>
+        <div className={css.products}>
+          {products.map((product) => (
+            <div key={product.id} className={css.product}>
+              {/* eslint-disable-next-line @next/next/no-img-element */}
+              <img src={product.url} alt={product.name ?? 'Cookie'} />
+              <div className={css.name}>{product.name}</div>
+              <div className={css.description}>{product.description}</div>
+              <div className={css.price}>{product.price}</div>
+              <Button
+                color="primary"
+                className={css.purchase}
+                onClick={() => setProductInPurchase(product)}
+              >
+                Purchase
+              </Button>
             </div>
-        </main>
-        <footer className={css.footer}>
-          <a href="http://localhost:3001">Do not sell my personal Information</a>
-        </footer>
-        <PurchaseModal 
-          isOpen={!!productInPurchase} 
-          onRequestClose={onCloseModal} 
-          onSubmit={onSubmit}
-          product={productInPurchase} 
-        />
-        <Popup trigger={popupTrigger} />
+          ))}
+        </div>
+      </main>
+      <footer className={css.footer}>
+        <a href="http://localhost:3001">Do not sell my personal Information</a>
+      </footer>
+      <PurchaseModal
+        isOpen={!!productInPurchase}
+        onRequestClose={onCloseModal}
+        onSubmit={onSubmit}
+        product={productInPurchase}
+      />
+      <Popup trigger={popupTrigger} />
     </div>
   );
 };
