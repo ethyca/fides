cbb3bbd8bd783191640522459f13179661971fca
Allison King <allisonjuliaking@gmail.com>
2023-04-12 15:59:22 -0400

    Update autogenerated openapi types

---
+++ clients/admin-ui/src/types/api/models/Page_CurrentPrivacyPreferenceSchema_.ts
@@ -0,0 +1,12 @@
+/* istanbul ignore file */
+/* tslint:disable */
+/* eslint-disable */
+
+import type { CurrentPrivacyPreferenceSchema } from "./CurrentPrivacyPreferenceSchema";
+
+export type Page_CurrentPrivacyPreferenceSchema_ = {
+  items: Array<CurrentPrivacyPreferenceSchema>;
+  total: number;
+  page: number;
+  size: number;
+};
