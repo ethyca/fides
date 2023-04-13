2ed3a36e2550eaa543ac420b16b586bb842b7b93
Adam Sachs <adam@ethyca.com>
2023-04-13 15:11:07 -0400

    publish a constant :rc tagged docker image for rc tags

--- noxfiles/constants_nox.py
+++ noxfiles/constants_nox.py
@@ -32,7 +32,6 @@ IMAGE_NAME = "fides"
 IMAGE = f"{REGISTRY}/{IMAGE_NAME}"
 IMAGE_LOCAL = f"{IMAGE}:local"
 IMAGE_LOCAL_UI = f"{IMAGE}:local-ui"
-IMAGE_DEV = f"{IMAGE}:dev"
 IMAGE_LATEST = f"{IMAGE}:latest"
 
 # Image names for the secondary apps
