f4abaa17c95cecfeba963f2074dacf05376e7cfb
Neville Samuell <neville@ethyca.com>
2023-04-16 18:55:30 -0400

    Move cookie_house to clients folder

--- noxfiles/docker_nox.py
+++ noxfiles/docker_nox.py
@@ -105,7 +105,7 @@ def build(session: nox.Session, image: str, machine_type: str = "") -> None:
         session.run(
             "docker",
             "build",
-            "src/fides/data/sample_project/cookie_house",
+            "clients/cookie_house",
             "--tag",
             sample_app_image_tag,
             external=True,
