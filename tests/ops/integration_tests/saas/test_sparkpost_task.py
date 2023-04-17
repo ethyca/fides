4e4e29be073eac3113894235e257f60c35c2af4e
vivek <vivek@myambergroup.com>
2023-04-17 16:41:36 +0530

    sparkpost test changes

---
+++ tests/ops/integration_tests/saas/test_sparkpost_task.py
@@ -0,0 +1,47 @@
+import pytest
+
+from fides.api.ops.models.policy import Policy
+from tests.ops.integration_tests.saas.connector_runner import ConnectorRunner
+
+
+@pytest.mark.integration_saas
+class TestSparkpostConnector:
+    def test_connection(self, sparkpost_runner: ConnectorRunner):
+        sparkpost_runner.test_connection()
+
+    async def test_access_request(
+        self, sparkpost_runner: ConnectorRunner, policy, sparkpost_identity_email: str
+    ):
+        access_results = await sparkpost_runner.access_request(
+            access_policy=policy, identities={"email": sparkpost_identity_email}
+        )
+
+        # verify we only returned data for our identity email
+
+        for recipient in access_results["sparkpost_instance:recipient"]:
+            assert recipient["address"]["email"] == sparkpost_identity_email
+
+    async def test_non_strict_erasure_request(
+        self,
+        sparkpost_runner: ConnectorRunner,
+        policy: Policy,
+        erasure_policy_string_rewrite: Policy,
+        sparkpost_erasure_identity_email: str,
+        sparkpost_erasure_data,
+        sparkpost_client,
+    ):
+        (
+            access_results,
+            erasure_results,
+        ) = await sparkpost_runner.non_strict_erasure_request(
+            access_policy=policy,
+            erasure_policy=erasure_policy_string_rewrite,
+            identities={"email": sparkpost_erasure_identity_email},
+        )
+
+        assert erasure_results == {
+            "sparkpost_instance:all_recipients": 0,
+            "sparkpost_instance:recipient": 1,
+        }
+
+        # deletion check is not done here because deletion request takes minimum 30 days to delete the recipient data.
