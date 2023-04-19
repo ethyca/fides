18b9dde6be4a21eb03bc1a52e4972c409993598d
SteveDMurphy <steven.d.murphy@gmail.com>
2023-04-19 12:31:31 +0100

    resolve migration conflicts

--- src/fides/api/ctl/migrations/versions/fc04e3e637c0_add_dynamodb_to_connector_list.py
+++ src/fides/api/ctl/migrations/versions/fc04e3e637c0_add_dynamodb_to_connector_list.py
@@ -1,16 +1,15 @@
 """add dynamodb to connector list
 
 Revision ID: fc04e3e637c0
-Revises: ff782b0dc07e
+Revises: 144d9b85c712
 Create Date: 2023-04-14 10:19:50.681752
 
 """
-import sqlalchemy as sa
 from alembic import op
 
 # revision identifiers, used by Alembic.
 revision = "fc04e3e637c0"
-down_revision = "ff782b0dc07e"
+down_revision = "144d9b85c712"
 branch_labels = None
 depends_on = None
 
@@ -31,18 +30,6 @@ def upgrade():
     )
     op.execute("drop type connectiontype_old")
 
-    # Rename field to something more generic
-    # op.alter_column(
-    #     "privacyrequest",
-    #     "awaiting_consent_email_send_at",
-    #     new_column_name="awaiting_email_send_at",
-    # )
-
-    # Rename awaiting_consent_email_send enum value to awaiting_email_send
-    # op.execute(
-    #     "ALTER TYPE privacyrequeststatus RENAME VALUE 'awaiting_consent_email_send' TO 'awaiting_email_send'"
-    # )
-
 
 def downgrade():
     # Remove dynamodb from the connectiontype enum
@@ -60,15 +47,3 @@ def downgrade():
         )
     )
     op.execute("drop type connectiontype_old")
-
-    # Revert field name
-    # op.alter_column(
-    #     "privacyrequest",
-    #     "awaiting_email_send_at",
-    #     new_column_name="awaiting_consent_email_send_at",
-    # )
-
-    # # Revert awaiting_email_send enum value to awaiting_consent_email_send
-    # op.execute(
-    #     "ALTER TYPE privacyrequeststatus RENAME VALUE 'awaiting_email_send' TO 'awaiting_consent_email_send'"
-    # )
