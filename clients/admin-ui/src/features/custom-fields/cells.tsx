f43f9b4dedaee23e6f5879712332808d6d3ed11d
Allison King <allisonjuliaking@gmail.com>
2023-04-24 15:31:09 -0400

    Add warning for deleting a custom field

--- clients/admin-ui/src/features/custom-fields/cells.tsx
+++ clients/admin-ui/src/features/custom-fields/cells.tsx
@@ -5,10 +5,14 @@ import {
   MenuItem,
   MenuList,
   MoreIcon,
+  Text,
+  useDisclosure,
+  WarningIcon,
 } from "@fidesui/react";
 import React from "react";
 import { CellProps } from "react-table";
 
+import ConfirmationModal from "~/features/common/ConfirmationModal";
 import Restrict from "~/features/common/Restrict";
 import { EnableCell, MapCell } from "~/features/common/table/";
 import { FIELD_TYPE_MAP } from "~/features/custom-fields/constants";
@@ -46,13 +50,13 @@ export const EnableCustomFieldCell = (
 const MENU_ITEM_PROPS = { _hover: { color: "complimentary.500" } };
 
 export const MoreActionsCell = ({ row, column }: CellProps<any, any>) => {
+  const modal = useDisclosure();
   // Need to stopPropagation on all button calls here since the table row itself
   // also has a click handler. We want the buttons in this cell to take precedence
   // over the row handler
   const handleDelete = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
     e.stopPropagation();
-    // @ts-ignore revisit with react-table v8
-    column.onDelete(row.original);
+    modal.onOpen();
   };
 
   const handleEdit = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
@@ -62,30 +66,52 @@ export const MoreActionsCell = ({ row, column }: CellProps<any, any>) => {
   };
 
   return (
-    <Menu size="sm">
-      <MenuButton
-        as={Button}
-        variant="outline"
-        data-testid="more-actions-btn"
-        size="xs"
-        onClick={(e) => {
-          e.stopPropagation();
+    <>
+      <Menu size="sm">
+        <MenuButton
+          as={Button}
+          variant="outline"
+          data-testid="more-actions-btn"
+          size="xs"
+          onClick={(e) => {
+            e.stopPropagation();
+          }}
+        >
+          <MoreIcon />
+        </MenuButton>
+        <MenuList>
+          <Restrict scopes={[ScopeRegistryEnum.CUSTOM_FIELD_DELETE]}>
+            <MenuItem {...MENU_ITEM_PROPS} onClick={handleDelete}>
+              Delete
+            </MenuItem>
+          </Restrict>
+          <Restrict scopes={[ScopeRegistryEnum.CUSTOM_FIELD_UPDATE]}>
+            <MenuItem {...MENU_ITEM_PROPS} onClick={handleEdit}>
+              Edit
+            </MenuItem>
+          </Restrict>
+        </MenuList>
+      </Menu>
+      <ConfirmationModal
+        isOpen={modal.isOpen}
+        onClose={modal.onClose}
+        onConfirm={() => {
+          // @ts-ignore revisit with react-table v8
+          column.onDelete(row.original);
+          modal.onClose();
         }}
-      >
-        <MoreIcon />
-      </MenuButton>
-      <MenuList>
-        <Restrict scopes={[ScopeRegistryEnum.CUSTOM_FIELD_DELETE]}>
-          <MenuItem {...MENU_ITEM_PROPS} onClick={handleDelete}>
-            Delete
-          </MenuItem>
-        </Restrict>
-        <Restrict scopes={[ScopeRegistryEnum.CUSTOM_FIELD_UPDATE]}>
-          <MenuItem {...MENU_ITEM_PROPS} onClick={handleEdit}>
-            Edit
-          </MenuItem>
-        </Restrict>
-      </MenuList>
-    </Menu>
+        title="Delete custom field"
+        message={
+          <Text color="gray.500">
+            Are you sure you want to delete this custom field? This will remove
+            the custom field and all stored values and this action can&apos;t be
+            undone.
+          </Text>
+        }
+        continueButtonText="Confirm"
+        isCentered
+        icon={<WarningIcon />}
+      />
+    </>
   );
 };
