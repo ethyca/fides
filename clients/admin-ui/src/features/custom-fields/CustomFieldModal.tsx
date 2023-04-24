60f5173504ee6fa46d96152f9d4e8c8d8d4851c7
Andrew Jackson <andrew.c.j1995@gmail.com>
2023-04-24 14:20:47 -0400

    Add initial creation form

--- clients/admin-ui/src/features/custom-fields/CustomFieldModal.tsx
+++ clients/admin-ui/src/features/custom-fields/CustomFieldModal.tsx
@@ -1,29 +1,23 @@
 import {
   Box,
   Button,
+  Flex,
+  IconButton,
   Modal,
   ModalBody,
   ModalContent,
-  ModalFooter,
   ModalHeader,
   ModalOverlay,
   SimpleGrid,
   Text,
 } from "@fidesui/react";
-import { ReactNode, useRef } from "react";
 import FormSection from "common/form/FormSection";
-import { CustomSelect, CustomTextInput } from "common/form/inputs";
-import {
-  Field,
-  FieldInputProps,
-  Form,
-  Formik,
-  FormikHelpers,
-  FormikProps,
-} from "formik";
+import { CustomSelect } from "common/form/inputs";
+import { FieldArray, Form, Formik, FormikHelpers } from "formik";
 import * as Yup from "yup";
 import {
   AllowedTypes,
+  AllowListUpdate,
   CustomFieldDefinition,
   ResourceTypes,
 } from "~/types/api";
@@ -32,31 +26,23 @@ import {
   RESOURCE_TYPE_OPTIONS,
 } from "common/custom-fields";
 
-type HeaderProps = {
-  children: ReactNode;
-};
 import { CreateCustomLists } from "~/features/common/custom-fields/CreateCustomLists";
+import CustomInput, {
+  CUSTOM_LABEL_STYLES,
+} from "common/custom-fields/form/CustomInput";
+import {
+  useAddCustomFieldDefinitionMutation,
+  useUpsertAllowListMutation,
+} from "~/features/plus/plus.slice";
+import { useAlert } from "common/hooks";
+import { getErrorMessage } from "common/helpers";
+import { TrashCanSolidIcon } from "common/Icon/TrashCanSolidIcon";
+import { AddIcon } from "common/custom-fields/icons/AddIcon";
 
-const CustomFieldHeader = ({ children }: HeaderProps) => (
-  <ModalHeader
-    id="modal-header"
-    fontWeight="semibold"
-    lineHeight={5}
-    fontSize="sm"
-    textAlign="left"
-    py="18px"
-    px={6}
-    height="56px"
-    backgroundColor="gray.50"
-    borderColor="gray.200"
-    borderWidth="0px 0px 1px 1p"
-    borderTopRightRadius="8px"
-    borderTopLeftRadius="8px"
-    boxSizing="border-box"
-  >
-    {children}
-  </ModalHeader>
-);
+const CustomFieldLabelStyles = {
+  ...CUSTOM_LABEL_STYLES,
+  minWidth: "unset",
+};
 
 type ModalProps = {
   isOpen: boolean;
@@ -64,19 +50,92 @@ type ModalProps = {
   isLoading: boolean;
 };
 
-const initialValuesTemplate: CustomFieldDefinition = {
+type FormValues = CustomFieldDefinition & {
+  allow_list: AllowListUpdate;
+};
+
+const initialValuesTemplate: FormValues = {
   description: undefined,
   field_type: AllowedTypes.STRING,
   name: "",
-  resource_type: ResourceTypes.DATA_CATEGORY,
+  resource_type: ResourceTypes.SYSTEM,
+  allow_list: {
+    name: "",
+    allowed_values: [],
+  },
 };
+
+const validationSchema = Yup.object().shape({
+  name: Yup.string().required("Name is required").trim(),
+  allow_list: Yup.object().shape({
+    name: Yup.string().required("Name is required").trim(),
+    allowed_values: Yup.array(Yup.string().required("List item is required"))
+      .min(1, "Must add at least one list value")
+      .label("allowed_values"),
+  }),
+});
 
 export const CustomFieldModal = ({
   isOpen,
   onClose,
   isLoading,
 }: ModalProps) => {
-  const createCustomListsRef = useRef(null);
+  const { errorAlert, successAlert } = useAlert();
+  const [addCustomFieldDefinition] = useAddCustomFieldDefinitionMutation();
+  const [upsertAllowList] = useUpsertAllowListMutation();
+
+  // QUERY ALLOW LIST if the edited field has a allow list id.
+  // populate initial values with allow list data
+
+  const handleSubmit = async (
+    values: FormValues,
+    helpers: FormikHelpers<FormValues>
+  ) => {
+    console.log("values", values);
+
+    if (
+      [AllowedTypes.STRING_, AllowedTypes.STRING].includes(values.field_type)
+    ) {
+      const uniqueValues = new Set(
+        values.allow_list?.allowed_values
+          .map((v) => v.toLowerCase().trim())
+          .map((v) => v)
+      );
+      if (uniqueValues.size < values.allow_list!.allowed_values.length) {
+        errorAlert("List item value must be unique");
+        return;
+      }
+
+      const allowListPayload: AllowListUpdate = {
+        ...values.allow_list!,
+      };
+
+      if (values.allow_list_id) {
+        allowListPayload.id = values.allow_list_id;
+      }
+
+      const result = await upsertAllowList(allowListPayload);
+      if (!("error" in result) && !values.allow_list_id) {
+        // eslint-disable-next-line no-param-reassign
+        values.allow_list_id = result.data?.id;
+      }
+      //Add error handling here
+    }
+
+    const result = await addCustomFieldDefinition(values);
+
+    // Add custom field edit here. Check if there is an upsert
+
+    if ("error" in result) {
+      errorAlert(
+        getErrorMessage(result.error),
+        `Custom field has failed to save due to the following:`
+      );
+    } else {
+      helpers.resetForm();
+      successAlert(`Custom field successfully saved`);
+    }
+  };
 
   return (
     <Modal
@@ -95,13 +154,27 @@ export const CustomFieldModal = ({
         maxHeight="80%"
         overflowY="auto"
       >
-        <CustomFieldHeader>Edit Custom Field</CustomFieldHeader>
+        <ModalHeader
+          id="modal-header"
+          fontWeight="semibold"
+          lineHeight={5}
+          fontSize="sm"
+          textAlign="left"
+          py="18px"
+          px={6}
+          height="56px"
+          backgroundColor="gray.50"
+          borderColor="gray.200"
+          borderWidth="0px 0px 1px 1p"
+          borderTopRightRadius="8px"
+          borderTopLeftRadius="8px"
+          boxSizing="border-box"
+        >
+          Edit Custom Field
+        </ModalHeader>
         <ModalBody px={6} py={0}>
-          <Formik
-            initialValues={initialValuesTemplate}
-            onSubmit={async () => {}}
-          >
-            {({ dirty, isValid, isSubmitting }) => (
+          <Formik initialValues={initialValuesTemplate} onSubmit={handleSubmit}>
+            {({ dirty, isValid, isSubmitting, values, errors }) => (
               <Form
                 style={{
                   paddingTop: "12px",
@@ -110,12 +183,24 @@ export const CustomFieldModal = ({
               >
                 <Box py={3}>
                   <FormSection title="Field Information">
-                    <CustomTextInput label="Name" name="name" />
-                    <CustomTextInput label="Description" name="description" />
+                    <CustomInput
+                      displayHelpIcon
+                      isRequired
+                      label="Name"
+                      name="name"
+                      customLabelProps={CustomFieldLabelStyles}
+                    />
+                    <CustomInput
+                      displayHelpIcon
+                      label="Description"
+                      name="description"
+                      customLabelProps={CustomFieldLabelStyles}
+                    />
                     <CustomSelect
                       label="Location"
                       name="resource_type"
                       options={RESOURCE_TYPE_OPTIONS}
+                      labelProps={CustomFieldLabelStyles}
                     />
                   </FormSection>
                 </Box>
@@ -126,10 +211,107 @@ export const CustomFieldModal = ({
                       name="field_type"
                       options={FIELD_TYPE_OPTIONS}
                     />
-                    <CreateCustomLists
-                      onSubmitComplete={() => {}}
-                      ref={createCustomListsRef}
-                    />
+                    <Flex
+                      flexDirection="column"
+                      gap="12px"
+                      paddingTop="6px"
+                      paddingBottom="24px"
+                    >
+                      <CustomInput
+                        displayHelpIcon={false}
+                        isRequired
+                        label="Name"
+                        name="allow_list.name"
+                        placeholder=""
+                      />
+                      <CustomInput
+                        displayHelpIcon={false}
+                        height="100px"
+                        label="Description"
+                        name="allow_list.description"
+                        placeholder=""
+                        type="textarea"
+                      />
+                      <FieldArray
+                        name="allow_list.allowed_values"
+                        render={(fieldArrayProps) => {
+                          // eslint-disable-next-line @typescript-eslint/naming-convention
+                          const { allowed_values } = values.allow_list!;
+                          return (
+                            <Flex flexDirection="column" gap="24px" pl="24px">
+                              <Flex flexDirection="column">
+                                {allowed_values.map((_value, index) => (
+                                  <Flex
+                                    flexGrow={1}
+                                    gap="12px"
+                                    // eslint-disable-next-line react/no-array-index-key
+                                    key={index}
+                                    mt={index > 0 ? "12px" : undefined}
+                                    // ref={fieldRef}
+                                  >
+                                    <CustomInput
+                                      customLabelProps={{
+                                        color: "gray.600",
+                                        fontSize: "sm",
+                                        fontWeight: "500",
+                                        lineHeight: "20px",
+                                        minW: "126px",
+                                        pr: "8px",
+                                      }}
+                                      displayHelpIcon={false}
+                                      isRequired
+                                      label={`List item ${index + 1}`}
+                                      name={`allow_list.allowed_values[${index}]`}
+                                    />
+                                    <IconButton
+                                      aria-label="Remove this list value"
+                                      data-testid={`remove-list-value-btn-${index}`}
+                                      icon={<TrashCanSolidIcon />}
+                                      isDisabled={allowed_values.length <= 1}
+                                      onClick={() =>
+                                        fieldArrayProps.remove(index)
+                                      }
+                                      size="sm"
+                                      variant="ghost"
+                                    />
+                                  </Flex>
+                                ))}
+                              </Flex>
+                              <Flex alignItems="center">
+                                <Text
+                                  color="gray.600"
+                                  fontSize="xs"
+                                  fontWeight="500"
+                                  lineHeight="16px"
+                                  pr="8px"
+                                >
+                                  Add a list value
+                                </Text>
+                                <IconButton
+                                  aria-label="Add a list value"
+                                  data-testid="add-list-value-btn"
+                                  icon={<AddIcon h="7px" w="7px" />}
+                                  onClick={() => {
+                                    fieldArrayProps.push("");
+                                  }}
+                                  size="xs"
+                                  variant="outline"
+                                />
+                                {allowed_values.length === 0 && (
+                                  <Text color="red.500" pl="18px" size="sm">
+                                    {errors.allow_list}
+                                  </Text>
+                                )}
+                              </Flex>
+                            </Flex>
+                          );
+                        }}
+                      />
+                    </Flex>
+                    {/* <CreateCustomLists */}
+                    {/*   onSubmitComplete={() => {}} */}
+                    {/*   ref={createCustomListsRef} */}
+                    {/* /> */}
                   </FormSection>
                 </Box>
 
@@ -144,6 +326,7 @@ export const CustomFieldModal = ({
                     Cancel
                   </Button>
                   <Button
+                    type="submit"
                     colorScheme="primary"
                     data-testid="save-btn"
                     isLoading={isLoading}
