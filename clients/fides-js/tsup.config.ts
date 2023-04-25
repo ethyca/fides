80ec6695f1f636a7e8a32935a411b8f9b69769b8
Neville Samuell <neville@ethyca.com>
2023-04-25 08:25:52 -0400

    Add tsup.config.ts for clarity

---
+++ clients/fides-js/tsup.config.ts
@@ -0,0 +1,20 @@
+import { defineConfig } from "tsup";
+
+// Define the configuration used to bundle the fides-js package using `tsup`
+export default defineConfig({
+    entry: ["src/fides.ts"],
+    dts: true,
+    format: ["esm", "iife"],
+    // NOTE: tsup behaves differently from other bundlers like rollup in that it
+    // *doesn't* bundle dependencies in the ESM module (dist/fides.mjs).
+    //
+    // This creates some complexity when importing this module into other local
+    // packages since they need to be able to resolve all dependencies
+    // themselves - that tends to work for Typescript / Turborepo tooling, but
+    // we ran into issues with Jest trying to resolve everything.
+    //
+    // Therefore, we bundle *everything* into the tsup output, including what
+    // would otherwise be "external" dependencies.
+    // See discussion here: https://github.com/egoist/tsup/issues/619
+    noExternal: [/(.*)/],
+})
