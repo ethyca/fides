9059f81c1d3a01c3b22c7c1c24a021f556c08099
Adrian Galvan <adrian@ethyca.com>
2023-04-25 13:24:19 -0700

    Adding erasure support and OAuth2 authentication

--- tests/fixtures/saas/unbounce_fixtures.py
+++ tests/fixtures/saas/unbounce_fixtures.py
@@ -1,9 +1,9 @@
-from time import sleep
-from typing import Any, Dict, Generator
+from typing import Any, Dict, Generator, cast
 
 import pydash
 import pytest
 import requests
+from requests import Response
 from sqlalchemy.orm import Session
 
 from fides.api.ctl.sql_models import Dataset as CtlDataset
@@ -27,7 +27,15 @@ secrets = get_secrets("unbounce")
 def unbounce_secrets(saas_config):
     return {
         "domain": pydash.get(saas_config, "unbounce.domain") or secrets["domain"],
-        "api_key": pydash.get(saas_config, "unbounce.api_key") or secrets["api_key"],
+        "client_id": pydash.get(saas_config, "unbounce.client_id")
+        or secrets["client_id"],
+        "client_secret": pydash.get(saas_config, "unbounce.client_secret")
+        or secrets["client_secret"],
+        "redirect_uri": pydash.get(saas_config, "unbounce.redirect_uri")
+        or secrets["redirect_uri"],
+        "access_token": pydash.get(saas_config, "unbounce.access_token")
+        or secrets["access_token"],
+        "page_id": pydash.get(saas_config, "unbounce.page_id") or secrets["page_id"],
     }
 
 
@@ -107,3 +115,47 @@ def unbounce_dataset_config(
     ctl_dataset.delete(db=db)
 
 
+class UnbounceTestClient:
+    def __init__(self, unbounce_connection_config: ConnectionConfig):
+        self.unbounce_secrets = cast(Dict, unbounce_connection_config.secrets)
+        self.headers = {
+            "Authorization": f"Bearer {self.unbounce_secrets['access_token']}"
+        }
+        self.page_id = self.unbounce_secrets["page_id"]
+        self.base_url = f"https://{self.unbounce_secrets['domain']}"
+
+    def create_lead(self, email: str) -> Response:
+        body = {
+            "conversion": "True",
+            "form_submission": {
+                "variant_id": "a",
+                "form_data": {
+                    "first_name": "Test",
+                    "last_name": "User",
+                    "email": email,
+                },
+            },
+        }
+
+        return requests.post(
+            url=f"{self.base_url}/pages/{self.page_id}/leads",
+            headers=self.headers,
+            json=body,
+        )
+
+
+@pytest.fixture(scope="function")
+def unbounce_test_client(
+    unbounce_connection_config: ConnectionConfig,
+) -> Generator:
+    test_client = UnbounceTestClient(unbounce_connection_config)
+    yield test_client
+
+
+@pytest.fixture(scope="function")
+def unbounce_create_erasure_data(
+    unbounce_test_client,
+    unbounce_erasure_identity_email: str,
+) -> None:
+    # create lead
+    unbounce_test_client.create_lead(unbounce_erasure_identity_email)
--- tests/ops/integration_tests/saas/test_unbounce_task.py
+++ tests/ops/integration_tests/saas/test_unbounce_task.py
@@ -7,6 +7,7 @@ from fides.api.ops.models.privacy_request import PrivacyRequest
 from fides.api.ops.schemas.redis_cache import Identity
 from fides.api.ops.service.connectors import get_connector
 from fides.api.ops.task import graph_task
+from fides.api.ops.task.graph_task import get_cached_data_for_erasures
 from fides.core.config import get_config
 from tests.ops.graph.graph_test_util import assert_rows_match
 
@@ -17,6 +18,7 @@ CONFIG = get_config()
 @pytest.mark.integration_unbounce
 def test_unbounce_connection_test(unbounce_connection_config) -> None:
     get_connector(unbounce_connection_config).test_connection()
+
 
 @pytest.mark.integration_saas
 @pytest.mark.integration_unbounce
@@ -65,12 +67,12 @@ async def test_unbounce_access_request_task(
             "state",
             "lastPublishedAt",
             "variantsCount",
-            "domain"
+            "domain",
         ],
     )
 
     assert_rows_match(
-        v[f"{dataset_name}:lead"],
+        v[f"{dataset_name}:leads"],
         min_size=1,
         keys=[
             "created_at",
@@ -79,10 +81,98 @@ async def test_unbounce_access_request_task(
             "form_data",
             "page_id",
             "variant_id",
-            "metadata"
+            "metadata",
         ],
     )
 
     # verify we only returned data for our identity email
-    for leads in v[f"{dataset_name}:lead"]:
-        assert leads["form_data"]['email'] == unbounce_identity_email
+    for leads in v[f"{dataset_name}:leads"]:
+        assert unbounce_identity_email in leads["form_data"]["email"]
+
+
+@pytest.mark.integration_saas
+@pytest.mark.integration_unbounce
+@pytest.mark.asyncio
+async def test_unbounce_erasure_request_task(
+    db,
+    policy,
+    erasure_policy_string_rewrite,
+    unbounce_connection_config,
+    unbounce_dataset_config,
+    unbounce_erasure_identity_email,
+    unbounce_create_erasure_data,
+) -> None:
+    """Full erasure request based on the Unbounce SaaS config"""
+
+    masking_strict = CONFIG.execution.masking_strict
+    CONFIG.execution.masking_strict = False  # Allow Delete
+
+    privacy_request = PrivacyRequest(
+        id=f"test_unbounce_erasure_request_task_{random.randint(0, 1000)}"
+    )
+    identity = Identity(**{"email": unbounce_erasure_identity_email})
+    privacy_request.cache_identity(identity)
+
+    dataset_name = unbounce_connection_config.get_saas_config().fides_key
+    merged_graph = unbounce_dataset_config.get_graph()
+    graph = DatasetGraph(merged_graph)
+
+    v = await graph_task.run_access_request(
+        privacy_request,
+        policy,
+        graph,
+        [unbounce_connection_config],
+        {"email": unbounce_erasure_identity_email},
+        db,
+    )
+
+    assert_rows_match(
+        v[f"{dataset_name}:pages"],
+        min_size=1,
+        keys=[
+            "subAccountId",
+            "integrations",
+            "integrationsCount",
+            "integrationsErrorsCount",
+            "id",
+            "url",
+            "metadata",
+            "createdAt",
+            "name",
+            "state",
+            "lastPublishedAt",
+            "variantsCount",
+            "domain",
+        ],
+    )
+
+    assert_rows_match(
+        v[f"{dataset_name}:leads"],
+        min_size=1,
+        keys=[
+            "created_at",
+            "id",
+            "extra_data",
+            "form_data",
+            "page_id",
+            "variant_id",
+            "metadata",
+        ],
+    )
+
+    x = await graph_task.run_erasure(
+        privacy_request,
+        erasure_policy_string_rewrite,
+        graph,
+        [unbounce_connection_config],
+        {"email": unbounce_erasure_identity_email},
+        get_cached_data_for_erasures(privacy_request.id),
+        db,
+    )
+
+    assert x == {
+        f"{dataset_name}:pages": 0,
+        f"{dataset_name}:leads": 1,
+    }
+
+    CONFIG.execution.masking_strict = masking_strict
