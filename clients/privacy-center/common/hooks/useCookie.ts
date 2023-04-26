2172804f6a742d0ad357f89816546afd4ce21fc1
Sean Preston <sean@ethyca.com>
2023-04-25 21:27:26 -0400

    use typescript cookie dep to manage cookies

--- clients/privacy-center/common/hooks/useCookie.ts
+++ clients/privacy-center/common/hooks/useCookie.ts
@@ -1,29 +1,43 @@
-const fidesUserDeviceIdCookieKey = "fides_user_device_id";
+import { getCookie, setCookie, Types } from "typescript-cookie";
+
+const FIDES_USER_DEVICE_ID_COOKIE_NAME = "fides_user_device_id";
+const MAX_AGE_DAYS = 1;
+
+const CODEC: Types.CookieCodecConfig<string, string> = {
+  decodeName: decodeURIComponent,
+  decodeValue: decodeURIComponent,
+  encodeName: encodeURIComponent,
+  encodeValue: encodeURIComponent,
+};
 
 export const setFidesUserDeviceIdCookie = () => {
-  if (typeof window === "undefined") {
+  // Generates a new `fides_user_device_id` and stores it to the cookie
+  if (typeof document === "undefined") {
     return undefined;
   }
-  let cookies = window.document.cookie.trim();
+
+  const rootDomain = window.location.hostname.split(".").slice(-2).join(".");
   const uuid = crypto.randomUUID();
-  if (cookies.slice(-1) !== ';') cookies += ";";
-  cookies += ` ${fidesUserDeviceIdCookieKey}=${uuid}`;
-  document.cookie = cookies;
+
+  setCookie(
+    FIDES_USER_DEVICE_ID_COOKIE_NAME,
+    uuid,
+    {
+      domain: rootDomain,
+      expires: MAX_AGE_DAYS,
+    },
+    CODEC
+  );
   return uuid;
 }
 
 export const getFidesUserDeviceIdCookie = () => {
-  if (typeof window === "undefined") {
+  // Returns the `fides_user_device_id` from the cookie
+  if (typeof document === "undefined") {
     return undefined;
   }
-  const cookies = window.document.cookie.split(';');
-  for (let i = 0; i < cookies.length; i++) {
-    const c = cookies[i].trim();
-    if (c.startsWith(`${fidesUserDeviceIdCookieKey}=`)) {
-      const [_, uuid] = c.split('=');
-      return uuid;
-    }
-  }
+
+  return getCookie(FIDES_USER_DEVICE_ID_COOKIE_NAME, CODEC);
 }
 
 export const useFidesUserDeviceIdCookie = () => {
