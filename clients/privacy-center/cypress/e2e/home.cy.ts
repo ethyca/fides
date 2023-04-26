31f0f7a325bcd9a485c34648d063b6fe65a7b70f
Neville Samuell <neville@ethyca.com>
2023-04-26 11:26:05 -0400

    Get privacy center cypress tests building (but failing)

--- clients/privacy-center/cypress/e2e/home.cy.ts
+++ clients/privacy-center/cypress/e2e/home.cy.ts
@@ -1,16 +1,16 @@
-import { config } from "~/constants";
-
 describe("Home", () => {
   it("renders the configured page info", () => {
-    cy.visit("/");
-    cy.getByTestId("home");
+    cy.fixture("config/config_test.json").then((config) => {
+      cy.visit("/");
+      cy.getByTestId("home");
 
-    cy.getByTestId("heading").contains(config.title);
-    cy.getByTestId("description").contains(config.description);
-    cy.getByTestId("logo").should("have.attr", "src", config.logo_path);
+      cy.getByTestId("heading").contains(config.title);
+      cy.getByTestId("description").contains(config.description);
+      cy.getByTestId("logo").should("have.attr", "src", config.logo_path);
 
-    config.actions.forEach((action) => {
-      cy.contains(action.title);
+      config.actions.forEach((action) => {
+        cy.contains(action.title);
+      });
     });
-  });
+  })
 });
