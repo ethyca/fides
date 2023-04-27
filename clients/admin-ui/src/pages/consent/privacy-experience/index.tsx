f35bd2b07a2c9118b8acc1a2c83daaaaa2dfafaa
Allison King <allisonjuliaking@gmail.com>
2023-04-27 15:47:25 -0400

    Configure nav for new page

---
+++ clients/admin-ui/src/pages/consent/privacy-experience/index.tsx
@@ -0,0 +1,47 @@
+import { PRIVACY_REQUESTS_ROUTE } from "@fidesui/components";
+import { Box, Breadcrumb, BreadcrumbItem, Heading, Text } from "@fidesui/react";
+import NextLink from "next/link";
+import React from "react";
+
+import Layout from "~/features/common/Layout";
+import { PRIVACY_EXPERIENCE_ROUTE } from "~/features/common/nav/v2/routes";
+
+const PrivacyExperiencePage = () => (
+  <Layout title="Privacy notices">
+    <Box mb={4}>
+      <Heading fontSize="2xl" fontWeight="semibold" mb={2} data-testid="header">
+        Privacy experience
+      </Heading>
+      <Box>
+        <Breadcrumb
+          fontWeight="medium"
+          fontSize="sm"
+          color="gray.600"
+          data-testid="breadcrumbs"
+        >
+          <BreadcrumbItem>
+            <NextLink href={PRIVACY_REQUESTS_ROUTE}>Privacy requests</NextLink>
+          </BreadcrumbItem>
+          {/* TODO: Add Consent breadcrumb once the page exists */}
+          <BreadcrumbItem color="complimentary.500">
+            <NextLink href={PRIVACY_EXPERIENCE_ROUTE}>
+              Privacy experience
+            </NextLink>
+          </BreadcrumbItem>
+        </Breadcrumb>
+      </Box>
+    </Box>
+    <Text fontSize="sm" mb={8} width={{ base: "100%", lg: "50%" }}>
+      Based on your privacy notices, Fides has created the overlay and privacy
+      experience configuration below. Your privacy notices will be presented by
+      region in these components. Edit each component to adjust the text that
+      displays in the privacy center, overlay, and banners that show your
+      notices. When you’re ready to include these privacy notices on your
+      website, copy the javascript using the button on this page and place it on
+      your website.
+    </Text>
+    <Box data-testid="privacy-experience-page">Work in progress!</Box>
+  </Layout>
+);
+
+export default PrivacyExperiencePage;
