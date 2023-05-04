30e179d2581ebd522aff102f92d8dab87f36d3ce
Adrian Galvan <adrian@ethyca.com>
2023-05-04 12:54:36 -0700

    Implementing fixtures to test external dataset references

--- tests/fixtures/saas/onesignal_fixtures.py
+++ tests/fixtures/saas/onesignal_fixtures.py
@@ -2,6 +2,7 @@ from typing import Any, Dict, Generator
 
 import pydash
 import pytest
+import requests
 
 from tests.ops.integration_tests.saas.connector_runner import (
     ConnectorRunner,
@@ -15,12 +16,9 @@ secrets = get_secrets("onesignal")
 @pytest.fixture(scope="session")
 def onesignal_secrets(saas_config) -> Dict[str, Any]:
     return {
-        "domain": pydash.get(saas_config, "onesignal.domain")
-        or secrets["domain"],
-        "api_key": pydash.get(saas_config, "onesignal.api_key") or secrets["api_key"],
+        "domain": pydash.get(saas_config, "onesignal.domain") or secrets["domain"],
         "app_id": pydash.get(saas_config, "onesignal.app_id") or secrets["app_id"],
-        "player_id": pydash.get(saas_config, "onesignal.player_id")
-        or secrets["player_id"],
+        "api_key": pydash.get(saas_config, "onesignal.api_key") or secrets["api_key"],
     }
 
 
@@ -31,27 +29,67 @@ def onesignal_identity_email(saas_config) -> str:
     )
 
 
-@pytest.fixture
+@pytest.fixture(scope="session")
 def onesignal_erasure_identity_email() -> str:
     return generate_random_email()
 
 
 @pytest.fixture
-def onesignal_external_references() -> Dict[str, Any]:
-    return {}
+def onesignal_external_references(saas_config) -> Dict[str, Any]:
+    return {
+        "player_id": pydash.get(saas_config, "onesignal.player_id")
+        or secrets["player_id"]
+    }
 
 
 @pytest.fixture
-def onesignal_erasure_external_references() -> Dict[str, Any]:
-    return {}
+def onesignal_erasure_external_references(onesignal_erasure_data) -> Dict[str, Any]:
+    player_id = onesignal_erasure_data
+    return {"player_id": player_id}
 
 
-@pytest.fixture
+@pytest.fixture(scope="session")
 def onesignal_erasure_data(
+    onesignal_secrets,
     onesignal_erasure_identity_email: str,
 ) -> Generator:
     # create the data needed for erasure tests here
-    yield {}
+    response = requests.post(
+        url="https://onesignal.com/api/v1/players",
+        headers={
+            "Authorization": f"Basic {onesignal_secrets['api_key']}",
+        },
+        json={
+            "app_id": onesignal_secrets["app_id"],
+            "device_type": 11,
+            "identifier": onesignal_erasure_identity_email,
+            "test_type": "1",
+            "language": "en",
+            "external_user_id": "5088124543035",
+            "timezone": "-28800",
+            "game_version": "1.1.1",
+            "device_model": "Mi Note 11",
+            "device_os": "15.1.1",
+            "session_count": 600,
+            "tags": {
+                "first_name": "Vivek",
+                "last_name": "Thiyagarajan",
+                "level": "99",
+                "amount_spent": "6000",
+                "account_type": "VIP",
+                "key_to_delete": '""',
+            },
+            "amount_spent": "100.99",
+            "created_at": 0,
+            "playtime": 600,
+            "last_active": 0,
+            "notification_types": 1,
+            "lat": 37.563,
+            "long": 122.3255,
+            "country": "US",
+        },
+    )
+    yield response.json()["id"]
 
 
 @pytest.fixture
--- tests/ops/integration_tests/saas/test_onesignal_task.py
+++ tests/ops/integration_tests/saas/test_onesignal_task.py
@@ -16,36 +16,24 @@ class TestOneSignalConnector:
             access_policy=policy, identities={"email": onesignal_identity_email}
         )
 
-    # async def test_strict_erasure_request(
-    #     self,
-    #     onesignal_runner: ConnectorRunner,
-    #     policy: Policy,
-    #     erasure_policy_string_rewrite: Policy,
-    #     onesignal_erasure_identity_email: str,
-    #     onesignal_erasure_data,
-    # ):
-    #     (
-    #         access_results,
-    #         erasure_results,
-    #     ) = await onesignal_runner.strict_erasure_request(
-    #         access_policy=policy,
-    #         erasure_policy=erasure_policy_string_rewrite,
-    #         identities={"email": onesignal_erasure_identity_email},
-    #     )
+    async def test_non_strict_erasure_request(
+        self,
+        onesignal_runner: ConnectorRunner,
+        policy: Policy,
+        erasure_policy_string_rewrite: Policy,
+        onesignal_erasure_identity_email: str,
+        onesignal_erasure_data,
+    ):
+        (
+            access_results,
+            erasure_results,
+        ) = await onesignal_runner.non_strict_erasure_request(
+            access_policy=policy,
+            erasure_policy=erasure_policy_string_rewrite,
+            identities={"email": onesignal_erasure_identity_email},
+        )
 
-    # async def test_non_strict_erasure_request(
-    #     self,
-    #     onesignal_runner: ConnectorRunner,
-    #     policy: Policy,
-    #     erasure_policy_string_rewrite: Policy,
-    #     onesignal_erasure_identity_email: str,
-    #     onesignal_erasure_data,
-    # ):
-    #     (
-    #         access_results,
-    #         erasure_results,
-    #     ) = await onesignal_runner.non_strict_erasure_request(
-    #         access_policy=policy,
-    #         erasure_policy=erasure_policy_string_rewrite,
-    #         identities={"email": onesignal_erasure_identity_email},
-    #     )
+        assert erasure_results == {
+            "onesignal_instance:devices": 1,
+            "onesignal_external_dataset:onesignal_external_collection": 0,
+        }
