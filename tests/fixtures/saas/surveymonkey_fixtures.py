5b5b2e408fb9898fe421fceae0fd89e094a0d76c
vivek <vivek@myambergroup.com>
2023-05-05 21:31:51 +0530

    survey monkey test request

---
+++ tests/fixtures/saas/surveymonkey_fixtures.py
@@ -0,0 +1,147 @@
+from typing import Any, Dict, Generator
+
+import pydash
+import pytest
+import requests
+
+from tests.ops.integration_tests.saas.connector_runner import (
+    ConnectorRunner,
+    generate_random_email,
+)
+from tests.ops.test_helpers.vault_client import get_secrets
+
+secrets = get_secrets("surveymonkey")
+
+
+@pytest.fixture(scope="session")
+def surveymonkey_secrets(saas_config) -> Dict[str, Any]:
+    return {
+        "domain": pydash.get(saas_config, "surveymonkey.domain") or secrets["domain"],
+        "api_token": pydash.get(saas_config, "surveymonkey.api_token") or secrets["api_token"],
+        # add the rest of your secrets here
+    }
+
+
+@pytest.fixture(scope="session")
+def surveymonkey_identity_email(saas_config) -> str:
+    return (
+        pydash.get(saas_config, "surveymonkey.identity_email") or secrets["identity_email"]
+    )
+
+
+@pytest.fixture
+def surveymonkey_erasure_identity_email() -> str:
+    return generate_random_email()
+
+
+@pytest.fixture
+def surveymonkey_external_references() -> Dict[str, Any]:
+    return {}
+
+
+@pytest.fixture
+def surveymonkey_erasure_external_references() -> Dict[str, Any]:
+    return {}
+
+class surveymonkeyClient:
+    headers: object = {}
+    base_url: str = ""
+
+    def __init__(self, secrets: Dict[str, Any]):
+        self.base_url = f"https://{secrets['domain']}"
+        self.headers = {
+            "Authorization": f"Bearer {secrets['api_token']}",
+        }
+
+    def get_recipient(self, recipient_id: str, email: str) -> requests.Response:
+        body = {
+            "filterGroups": [
+                {
+                    "filters": [
+                        {
+                            "value": email,
+                            "propertyName": "address.email",
+                            "operator": "EQ",
+                        }
+                    ]
+                }
+            ]
+        }
+        recipient_response: requests.Response = requests.post(
+            url=f"{self.base_url}/api/v1/recipient-lists/{recipient_id}?show_recipients=true",
+            json=body,
+            headers=self.headers,
+        )
+        assert recipient_response.ok
+        return recipient_response.json()
+
+    def create_survey_response(self, email):
+        return requests.post(
+            url=f"{self.base_url}/api/v1/recipient-lists?num_rcpt_errors=1",
+            headers=self.headers,
+            json={
+                "name": "Ethyca test",
+                "description": "An email list of employees at UMBC",
+                "attributes": {
+                    "internal_id": 113,
+                    "list_group_id": 12322
+                },
+                "recipients": [
+                    {
+                        "address": {
+                            "email": email,
+                            "name": "test"
+                        },
+                        "tags": [
+                            "reading"
+                        ],
+                        "metadata": {
+                            "age": "31",
+                            "place": "Test location"
+                        },
+                        "substitution_data": {
+                            "favorite_color": "surveymonkey Orange",
+                            "job": "Software Engineer"
+                        }
+                    }
+                ]
+            },
+        )
+
+@pytest.fixture
+def surveymonkey_client(surveymonkey_secrets) -> Generator:
+    yield surveymonkeyClient(surveymonkey_secrets)
+
+@pytest.fixture
+def surveymonkey_erasure_data(
+    surveymonkey_client: surveymonkeyClient,
+    surveymonkey_erasure_identity_email: str,
+) -> Generator:
+    
+    response = surveymonkey_client.create_survey_response(surveymonkey_erasure_identity_email)
+    recipient = response.json()["results"]
+    recipient_id = recipient["id"]
+
+    # error_message = f"customer with email {surveymonkey_erasure_identity_email} could not be created in Recharge"
+    # poll_for_existence(
+    #     surveymonkey_client.get_recipient,
+    #     (surveymonkey_erasure_identity_email,recipient_id),
+    #     error_message=error_message,
+    # )
+
+    yield recipient_id
+
+@pytest.fixture
+def surveymonkey_runner(
+    db,
+    cache,
+    surveymonkey_secrets,
+    surveymonkey_external_references,
+    surveymonkey_erasure_external_references,
+) -> ConnectorRunner:
+    return ConnectorRunner(
+        db,
+        cache,
+        "surveymonkey",
+        surveymonkey_secrets,
+    )
---
+++ tests/ops/integration_tests/saas/test_surveymonkey_task.py
@@ -0,0 +1,56 @@
+import pytest
+
+from fides.api.ops.models.policy import Policy
+from tests.ops.integration_tests.saas.connector_runner import ConnectorRunner
+
+
+@pytest.mark.integration_saas
+class TestSurveyMonkeyConnector:
+    def test_connection(self, surveymonkey_runner: ConnectorRunner):
+        surveymonkey_runner.test_connection()
+
+    # async def test_access_request(
+    #     self, surveymonkey_runner: ConnectorRunner, policy, surveymonkey_identity_email: str
+    # ):
+    #     access_results = await surveymonkey_runner.access_request(
+    #         access_policy=policy, identities={"email": surveymonkey_identity_email}
+    #     )
+        
+        # verify we only returned data for our identity email
+
+        # for contacts in access_results["surveymonkey_instance:contacts"]:
+        #     assert contacts["data"]["email"] == surveymonkey_identity_email
+
+    # async def test_strict_erasure_request(
+    #     self,
+    #     surveymonkey_runner: ConnectorRunner,
+    #     policy: Policy,
+    #     erasure_policy_string_rewrite: Policy,
+    #     surveymonkey_erasure_identity_email: str,
+    #     surveymonkey_erasure_data,
+    # ):
+    #     (
+    #         access_results,
+    #         erasure_results,
+    #     ) = await surveymonkey_runner.strict_erasure_request(
+    #         access_policy=policy,
+    #         erasure_policy=erasure_policy_string_rewrite,
+    #         identities={"email": surveymonkey_erasure_identity_email},
+    #     )
+
+    # async def test_non_strict_erasure_request(
+    #     self,
+    #     surveymonkey_runner: ConnectorRunner,
+    #     policy: Policy,
+    #     erasure_policy_string_rewrite: Policy,
+    #     surveymonkey_erasure_identity_email: str,
+    #     surveymonkey_erasure_data,
+    # ):
+    #     (
+    #         access_results,
+    #         erasure_results,
+    #     ) = await surveymonkey_runner.non_strict_erasure_request(
+    #         access_policy=policy,
+    #         erasure_policy=erasure_policy_string_rewrite,
+    #         identities={"email": surveymonkey_erasure_identity_email},
+    #     )
