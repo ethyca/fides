ef9a3b6158db8e1d726c06375db8febc3698e7dc
Neville Samuell <neville@ethyca.com>
2023-05-08 10:14:27 -0400

    Add (failing) tests for fides.js cache behaviour

---
+++ clients/privacy-center/cypress/e2e/fides-js.cy.ts
@@ -0,0 +1,98 @@
+describe("fides.js", () => {
+  it("returns the fides.js packaged bundled with the global config", () => {
+    cy.request("/fides.js").then(response => {
+      expect(response.status).to.eq(200);
+      expect(response)
+        .to.have.property("headers")
+        .to.have.property("content-type")
+        .to.eql("application/javascript");
+      // TODO: ensure bundle has config
+      // TODO: ensure JS is valid
+    });
+  });
+
+  it.only("caches in the browser", () => {
+    cy.intercept("/fides.js").as("fidesJS");
+
+    // Load the demo page 3 times, and check /fides.js is called *at most* once
+    // NOTE: Depending on browser cache, it might not be called at all - so zero
+    // times is a valid number of calls
+    cy.visit("/fides-js-demo.html");
+    cy.visit("/fides-js-demo.html");
+    cy.visit("/fides-js-demo.html");
+    cy.get("@fidesJS.all").its("length").should("be.within", 0, 1);
+  });
+
+  describe("when generating cache-control headers", () => {
+    beforeEach(() => {
+      cy.request("/fides.js").then(response => {
+        expect(response.status).to.eq(200);
+        cy.wrap(response.headers).as("headers");
+        cy.get("@headers").should("have.property", "etag").as("etag");
+        cy.get("@headers").should("have.property", "cache-control").as("cacheHeaders");
+      });
+    });
+
+    it("stores publicly for at least one hour, at most one day", () => {
+      cy.get("@cacheHeaders").should("match", /public/);
+      cy.get("@cacheHeaders")
+        .invoke("match", /max-age=(?<expiry>\d+)/)
+        .its("groups.expiry")
+        .then(parseInt)
+        .should("be.within", 3600, 86400);
+    });
+
+    it("generates 'etag' that is consistent when re-requested", () => {
+      cy.request("/fides.js").should("have.nested.property", "headers.etag").then((etag) => {
+        cy.get("@etag").should("eq", etag);
+      });
+    });
+
+    it("generates 'etag' that varies based on location query params", () => {
+      cy.request("/fides.js?location=US-CA").should("have.nested.property", "headers.etag").as("USCATag").then((etag) => {
+        cy.get("@etag").should("not.eq", etag);
+      });
+
+      // Fetch a second time with a different location param
+      cy.request("/fides.js?location=FR").should("have.nested.property", "headers.etag").then((etag) => {
+        cy.get("@etag").should("not.eq", etag);
+        cy.get("@USCATag").should("not.eq", etag);
+      });
+    });
+
+    it("generates 'etag' that varies based on Cloudfront location headers", () => {
+      cy.request({
+        url: "/fides.js",
+        headers: {
+          "Cloudfront-Viewer-Country": "US",
+          "Cloudfront-Viewer-Country-Region": "CA",
+        }
+      }).should("have.nested.property", "headers.etag").as("USCATag").then((etag) => {
+        cy.get("@etag").should("not.eq", etag);
+      });
+
+      // Fetch a second time with different location headers
+      cy.request({
+        url: "/fides.js",
+        headers: {
+          "Cloudfront-Viewer-Country": "FR",
+        }
+      }).should("have.nested.property", "headers.etag").as("headersTag").then((etag) => {
+        cy.get("@etag").should("not.eq", etag);
+        cy.get("@USCATag").should("not.eq", etag);
+      });
+    });
+
+    it("returns 'vary' header for supported Cloudfront location headers", () => {
+      const expected = [
+        "Cloudfront-Viewer-Country",
+        "Cloudfront-Viewer-Country-Region",
+      ];
+      cy.get("@headers").should("have.property", "vary").then((vary) => {
+        expected.forEach((header) => {
+          expect(vary).includes(header);
+        });
+      });
+    });
+  });
+});
