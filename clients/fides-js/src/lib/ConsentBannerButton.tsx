d4bef327a7bc86e9dda841fc30d9c960aa7c5dbe
Neville Samuell <neville@ethyca.com>
2023-05-08 18:57:51 -0400

    Refactor ConsentBannerButton into a Preact FunctionComponent function

--- clients/fides-js/src/lib/ConsentBannerButton.tsx
+++ clients/fides-js/src/lib/ConsentBannerButton.tsx
@@ -1,48 +1,20 @@
-import {h, Component, VNode} from "preact";
+import {h, FunctionComponent} from "preact";
 import {ButtonType} from "./consent-types";
-
 
 interface ButtonProps {
     buttonType: ButtonType;
     label?: string,
-    onClick: () => void
+    onClick?: () => void
 }
 
-class ConsentBannerButton extends Component<ButtonProps> {
-
-    private readonly buttonType: ButtonType;
-
-    private readonly onClick: () => void;
-
-    private readonly label?: string;
-
-    constructor(props: ButtonProps) {
-        super(props);
-        this.buttonType = props.buttonType;
-        this.onClick = props.onClick;
-        this.label = props.label;
-    }
-
-    /**
-     * Builds a button DOM element with the given id, class name, and text label
-     */
-    buildButton = (
-        buttonType: ButtonType,
-        onClick: () => void,
-        label?: string
-    ) => (
-        <button
-            id={`fides-consent-banner-button-${buttonType.valueOf()}`}
-            class={`fides-consent-banner-button fides-consent-banner-button-${buttonType.valueOf()}`}
-            onClick={onClick || undefined}
-        >
-            {label || ""}
-        </button>
-    );
-
-    render(): VNode {
-        return this.buildButton(this.buttonType, this.onClick, this.label)
-    }
-}
+const ConsentBannerButton: FunctionComponent<ButtonProps> = ({ buttonType, label, onClick }) => (
+    <button
+        id={`fides-consent-banner-button-${buttonType.valueOf()}`}
+        class={`fides-consent-banner-button fides-consent-banner-button-${buttonType.valueOf()}`}
+        onClick={onClick}
+    >
+        {label || ""}
+    </button>
+);
 
 export default ConsentBannerButton;
