1d973a3d2cf78cd949ffb713af3a326dfe1e5fef
Thomas <ThomasLaPiana@users.noreply.github.com>
2023-05-10 12:29:56 +0800

    feat: cleanup main.py by moving startup logic into app_setup.py

--- src/fides/api/main.py
+++ src/fides/api/main.py
@@ -50,6 +50,12 @@ from fides.api.ops.schemas.analytics import Event, ExtraData
 from fides.api.ops.service.connectors.saas.connector_registry_service import (
     update_saas_configs,
 )
+from fides.api.app_setup import (
+    create_fides_app,
+    log_startup,
+    run_database_startup,
+    check_redis,
+)
 
 # pylint: disable=wildcard-import, unused-wildcard-import
 from fides.api.ops.service.privacy_request.email_batch_service import (
@@ -81,70 +87,6 @@ ROUTERS = crud.routers + [  # type: ignore[attr-defined]
     system.system_connections_router,
     system.system_router,
 ]
-
-
-def create_fides_app(
-    cors_origins: Union[str, List[str]] = CONFIG.security.cors_origins,
-    cors_origin_regex: Optional[Pattern] = CONFIG.security.cors_origin_regex,
-    routers: List = ROUTERS,
-    app_version: str = VERSION,
-    api_prefix: str = API_PREFIX,
-    request_rate_limit: str = CONFIG.security.request_rate_limit,
-    rate_limit_prefix: str = CONFIG.security.rate_limit_prefix,
-    security_env: str = CONFIG.security.env,
-) -> FastAPI:
-    """Return a properly configured application."""
-    setup_logging(
-        CONFIG.logging.level,
-        serialize=CONFIG.logging.serialization,
-        desination=CONFIG.logging.destination,
-    )
-    logger.bind(api_config=CONFIG.logging.json()).debug(
-        "Logger configuration options in use"
-    )
-
-    fastapi_app = FastAPI(title="fides", version=app_version)
-    fastapi_app.state.limiter = Limiter(
-        default_limits=[request_rate_limit],
-        headers_enabled=True,
-        key_prefix=rate_limit_prefix,
-        key_func=get_remote_address,
-        retry_after="http-date",
-    )
-    fastapi_app.add_exception_handler(RateLimitExceeded, _rate_limit_exceeded_handler)
-    for handler in ExceptionHandlers.get_handlers():
-        fastapi_app.add_exception_handler(FunctionalityNotConfigured, handler)
-    fastapi_app.add_middleware(SlowAPIMiddleware)
-
-    if cors_origins or cors_origin_regex:
-        fastapi_app.add_middleware(
-            CORSMiddleware,
-            allow_origins=[str(origin) for origin in cors_origins],
-            allow_origin_regex=cors_origin_regex,
-            allow_credentials=True,
-            allow_methods=["*"],
-            allow_headers=["*"],
-        )
-
-    for router in routers:
-        fastapi_app.include_router(router)
-    fastapi_app.include_router(user_router, tags=["Users"], prefix=f"{api_prefix}")
-    fastapi_app.include_router(api_router)
-
-    if security_env == "dev":
-        # This removes auth requirements for specific endpoints
-        fastapi_app.dependency_overrides[verify_oauth_client_prod] = get_root_client
-        fastapi_app.dependency_overrides[
-            verify_oauth_client_for_system_from_request_body_cli
-        ] = get_system_schema
-        fastapi_app.dependency_overrides[
-            verify_oauth_client_for_system_from_fides_key_cli
-        ] = get_system_fides_key
-    elif security_env == "prod":
-        # This is the most secure, so all security deps are maintained
-        pass
-
-    return fastapi_app
 
 
 app = create_fides_app()
@@ -229,73 +171,10 @@ async def prepare_and_log_request(
 @app.on_event("startup")
 async def setup_server() -> None:
     "Run all of the required setup steps for the webserver."
-    logger.info(f"Starting Fides - v{VERSION}")
-    logger.info(
-        "Startup configuration: reloading = {}, dev_mode = {}",
-        CONFIG.hot_reloading,
-        CONFIG.dev_mode,
-    )
-    logger.info("Startup configuration: pii logging = {}", CONFIG.logging.log_pii)
 
-    if CONFIG.logging.level == DEBUG:
-        logger.warning(
-            "WARNING: log level is DEBUG, so sensitive or personal data may be logged. "
-            "Set FIDES__LOGGING__LEVEL to INFO or higher in production."
-        )
-        CONFIG.log_all_config_values()
-
-    if not CONFIG.database.sync_database_uri:
-        raise FidesError("No database uri provided")
-
-    await configure_db(
-        CONFIG.database.sync_database_uri, samples=CONFIG.database.load_samples
-    )
-
-    try:
-        create_or_update_parent_user()
-    except Exception as e:
-        logger.error("Error creating parent user: {}", str(e))
-        raise FidesError(f"Error creating parent user: {str(e)}")
-
-    logger.info("Loading config settings into database...")
-    try:
-        db = get_api_session()
-        ApplicationConfig.update_config_set(db, CONFIG)
-    except Exception as e:
-        logger.error("Error occurred writing config settings to database: {}", str(e))
-        raise FidesError(
-            f"Error occurred writing config settings to database: {str(e)}"
-        )
-    finally:
-        db.close()
-
-    logger.info("Validating SaaS connector templates...")
-    try:
-        db = get_api_session()
-        update_saas_configs(db)
-        logger.info("Finished loading SaaS templates")
-    except Exception as e:
-        logger.error(
-            "Error occurred during SaaS connector template validation: {}",
-            str(e),
-        )
-        return
-    finally:
-        db.close()
-
-    logger.info("Running Cache connection test...")
-
-    try:
-        get_cache()
-    except (RedisConnectionError, RedisError, ResponseError) as e:
-        logger.error("Connection to cache failed: {}", str(e))
-        return
-    else:
-        logger.debug("Connection to cache succeeded")
-
-    if not scheduler.running:
-        scheduler.start()
-
+    log_startup()
+    await run_database_startup()
+    check_redis()
     initiate_scheduled_batch_email_send()
 
     logger.debug("Sending startup analytics events...")
