04273d8235af6951b0869fc1ac54f60074828713
Adam Sachs <adam@ethyca.com>
2023-05-15 11:22:15 -0400

    remove unused import (again)

--- src/fides/api/ops/api/v1/endpoints/connection_type_endpoints.py
+++ src/fides/api/ops/api/v1/endpoints/connection_type_endpoints.py
@@ -1,6 +1,6 @@
 from typing import Any, Dict, Optional
 
-from fastapi import Depends, HTTPException, Query
+from fastapi import HTTPException, Query
 from fastapi.params import Security
 from fastapi_pagination import Page, Params, paginate
 from fastapi_pagination.bases import AbstractPage
