85afcfe71d24c1a114eb4032315df8ae42336ac5
Allison King <allisonjuliaking@gmail.com>
2023-05-16 11:15:04 -0400

    Update TS types

--- clients/admin-ui/src/types/api/models/ExperienceConfigCreate.ts
+++ clients/admin-ui/src/types/api/models/ExperienceConfigCreate.ts
@@ -7,21 +7,22 @@ import type { DeliveryMechanism } from "./DeliveryMechanism";
 import type { PrivacyNoticeRegion } from "./PrivacyNoticeRegion";
 
 /**
- * An API representation of a PrivacyNotice.
+ * An API representation to create ExperienceConfig.
  * This model doesn't include an `id` so that it can be used for creation.
  * It also establishes some fields _required_ for creation
  */
-export type PrivacyExperienceCreate = {
-  disabled?: boolean;
-  component: ComponentType;
-  delivery_mechanism: DeliveryMechanism;
-  regions: Array<PrivacyNoticeRegion>;
-  component_title?: string;
-  component_description?: string;
+export type ExperienceConfigCreate = {
+  acknowledgement_button_label?: string;
   banner_title?: string;
   banner_description?: string;
-  link_label?: string;
+  component: ComponentType;
+  component_title: string;
+  component_description?: string;
   confirmation_button_label?: string;
+  delivery_mechanism: DeliveryMechanism;
+  disabled?: boolean;
+  is_default?: boolean;
+  link_label?: string;
   reject_button_label?: string;
-  acknowledgement_button_label?: string;
+  regions: Array<PrivacyNoticeRegion>;
 };
