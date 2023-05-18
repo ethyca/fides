b2437583c947f883e06eb55af0dcc7ecc6f22bbf
Allison King <allisonjuliaking@gmail.com>
2023-05-18 17:31:30 -0400

    Add privacy notice related types

---
+++ clients/privacy-center/types/api/models/PrivacyExperienceResponse.ts
@@ -0,0 +1,26 @@
+/* istanbul ignore file */
+/* tslint:disable */
+/* eslint-disable */
+
+import type { ComponentType } from "./ComponentType";
+import type { DeliveryMechanism } from "./DeliveryMechanism";
+import type { ExperienceConfigResponse } from "./ExperienceConfigResponse";
+import type { PrivacyNoticeRegion } from "./PrivacyNoticeRegion";
+import type { PrivacyNoticeResponse } from "./PrivacyNoticeResponse";
+
+/**
+ * An API representation of a PrivacyExperience used for response payloads
+ */
+export type PrivacyExperienceResponse = {
+  disabled?: boolean;
+  component?: ComponentType;
+  delivery_mechanism?: DeliveryMechanism;
+  region: PrivacyNoticeRegion;
+  experience_config?: ExperienceConfigResponse;
+  id: string;
+  created_at: string;
+  updated_at: string;
+  version: number;
+  privacy_experience_history_id: string;
+  privacy_notices?: Array<PrivacyNoticeResponse>;
+};
