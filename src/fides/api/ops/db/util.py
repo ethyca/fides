ba28bf20a05bb49a27ce8471c04a69fe933f2dec
Sean Preston <sean@ethyca.com>
2023-05-19 15:51:50 -0400

    handle extra edge case with kwargs, add explanation

--- src/fides/api/ops/db/util.py
+++ src/fides/api/ops/db/util.py
@@ -1,12 +1,14 @@
-from typing import TypeVar, Type, TYPE_CHECKING
+from typing import Any, TypeVar, Type, TYPE_CHECKING
 
 if TYPE_CHECKING:
+    # Override the Enum constructor not to accept a list of strings and confuse mypy:
+    # https://github.com/dropbox/sqlalchemy-stubs/issues/114#issuecomment-541770319
     from sqlalchemy.sql.type_api import TypeEngine
 
     T = TypeVar("T")
 
     class EnumColumn(TypeEngine[T]):
-        def __init__(self, enum: Type[T]) -> None:
+        def __init__(self, enum: Type[T], **kwargs: Any) -> None:
             ...
 
 else:
