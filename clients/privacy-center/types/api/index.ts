92ce196c54ebcf39cb3f2fef9ed81f8bccaa2d3f
Allison King <allisonjuliaking@gmail.com>
2023-05-19 10:25:50 -0400

    fix enums

--- clients/privacy-center/types/api/index.ts
+++ clients/privacy-center/types/api/index.ts
@@ -2,17 +2,17 @@
 /* tslint:disable */
 /* eslint-disable */
 
-export type { ComponentType } from "./models/ComponentType";
+export { ComponentType } from "./models/ComponentType";
 export type { Consent } from "./models/Consent";
 export type { ConsentMechanism } from "./models/ConsentMechanism";
 export type { ConsentPreferences } from "./models/ConsentPreferences";
 export type { ConsentPreferencesWithVerificationCode } from "./models/ConsentPreferencesWithVerificationCode";
 export type { ConsentWithExecutableStatus } from "./models/ConsentWithExecutableStatus";
-export type { DeliveryMechanism } from "./models/DeliveryMechanism";
-export type { EnforcementLevel } from "./models/EnforcementLevel";
+export { DeliveryMechanism } from "./models/DeliveryMechanism";
+export { EnforcementLevel } from "./models/EnforcementLevel";
 export type { ExperienceConfigResponse } from "./models/ExperienceConfigResponse";
 export type { Identity } from "./models/Identity";
 export type { IdentityVerificationConfigResponse } from "./models/IdentityVerificationConfigResponse";
 export type { PrivacyExperienceResponse } from "./models/PrivacyExperienceResponse";
-export type { PrivacyNoticeRegion } from "./models/PrivacyNoticeRegion";
+export { PrivacyNoticeRegion } from "./models/PrivacyNoticeRegion";
 export type { PrivacyNoticeResponse } from "./models/PrivacyNoticeResponse";
