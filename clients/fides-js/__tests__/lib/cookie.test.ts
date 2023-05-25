cdc7747d4e897c36e30aa8cea500c35de5ce0734
eastandwestwind <eastandwestwind@gmail.com>
2023-05-25 17:44:33 -0400

    fix cookie test types

--- clients/fides-js/__tests__/lib/cookie.test.ts
+++ clients/fides-js/__tests__/lib/cookie.test.ts
@@ -7,8 +7,8 @@ import {
   makeFidesCookie,
   saveFidesCookie,
 } from "../../src/lib/cookie";
-import type { ConsentConfig } from "../../src/lib/consent-config";
 import type { ConsentContext } from "../../src/lib/consent-context";
+import {LegacyConsentConfig} from "~/lib/consent-types";
 
 // Setup mock date
 const MOCK_DATE = "2023-01-01T12:00:00.000Z";
@@ -172,7 +172,7 @@ describe("saveFidesCookie", () => {
 });
 
 describe("makeConsentDefaults", () => {
-  const config: ConsentConfig = {
+  const config: LegacyConsentConfig = {
     options: [
       {
         cookieKeys: ["default_undefined"],
@@ -205,7 +205,7 @@ describe("makeConsentDefaults", () => {
     const context: ConsentContext = {};
 
     it("returns the default consent values by key", () => {
-      expect(makeConsentDefaults({ config, context })).toEqual({
+      expect(makeConsentDefaults({ config, context, debug:false })).toEqual({
         default_true: true,
         default_false: false,
         default_true_with_gpc_false: true,
@@ -220,7 +220,7 @@ describe("makeConsentDefaults", () => {
     };
 
     it("returns the default consent values by key", () => {
-      expect(makeConsentDefaults({ config, context })).toEqual({
+      expect(makeConsentDefaults({ config, context, debug:false })).toEqual({
         default_true: true,
         default_false: false,
         default_true_with_gpc_false: false,
