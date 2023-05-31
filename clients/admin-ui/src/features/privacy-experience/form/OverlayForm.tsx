7f16a48509a00bc520dc107d47dae3ed457fa148
Allison King <allisonjuliaking@gmail.com>
2023-05-30 17:40:49 -0700

    Refactor experience config forms with updated fields

---
+++ clients/admin-ui/src/features/privacy-experience/form/OverlayForm.tsx
@@ -0,0 +1,83 @@
+import { Stack } from "@fidesui/react";
+
+import FormSection from "~/features/common/form/FormSection";
+import {
+  CustomSelect,
+  CustomTextArea,
+  CustomTextInput,
+} from "~/features/common/form/inputs";
+import { enumToOptions } from "~/features/common/helpers";
+import { BANNER_ENABLED_MAP } from "~/features/privacy-experience/constants";
+import { BannerEnabled } from "~/types/api";
+
+import PrivacyPolicy from "./PrivacyPolicy";
+
+const BANNER_ENABLED_OPTIONS = enumToOptions(BannerEnabled).map((opt) => ({
+  ...opt,
+  label: BANNER_ENABLED_MAP.get(opt.value) ?? opt.label,
+}));
+
+const OverlayForm = () => (
+  <Stack spacing={6}>
+    <FormSection
+      title="Cookie banner and privacy preferences labeling"
+      data-testid="banner-and-preferences-labeling"
+    >
+      <CustomTextInput
+        name="title"
+        label="Banner title"
+        variant="stacked"
+        isRequired
+      />
+      <CustomTextArea
+        label="Banner description"
+        name="description"
+        variant="stacked"
+      />
+      <CustomTextInput
+        label="Accept button displayed on the Banner and “Privacy preferences”"
+        name="accept_button_label"
+        variant="stacked"
+        isRequired
+      />
+      <CustomTextInput
+        label="Reject button displayed on the Banner and “Privacy preferences”"
+        name="reject_button_label"
+        variant="stacked"
+        isRequired
+      />
+      <CustomTextInput
+        label="“Privacy preferences” link label"
+        name="privacy_preferences_link_label"
+        variant="stacked"
+      />
+      <CustomTextInput
+        label="Privacy preferences “Save” button label"
+        name="save_button_label"
+        variant="stacked"
+        isRequired
+      />
+      <CustomTextInput
+        label="Acknowledge button label for notice only banner"
+        name="acknowledge_button_label"
+        variant="stacked"
+        isRequired
+      />
+    </FormSection>
+    <PrivacyPolicy />
+    <FormSection
+      title="Display configuration"
+      data-testid="display-configuration"
+    >
+      <CustomSelect
+        name="banner_enabled"
+        label="Banner"
+        options={BANNER_ENABLED_OPTIONS}
+        variant="stacked"
+        isRequired
+      />
+    </FormSection>
+  </Stack>
+);
+
+export default OverlayForm;
--- clients/admin-ui/src/features/privacy-experience/form/helpers.tsx
+++ clients/admin-ui/src/features/privacy-experience/form/helpers.tsx
@@ -3,17 +3,19 @@ import * as Yup from "yup";
 
 import {
   ComponentType,
-  DeliveryMechanism,
   ExperienceConfigCreate,
   ExperienceConfigResponse,
   PrivacyNoticeResponse,
 } from "~/types/api";
 
 export const defaultInitialValues: ExperienceConfigCreate = {
-  component_title: "",
+  title: "",
+  description: "",
+  accept_button_label: "",
+  reject_button_label: "",
+  save_button_label: "",
   regions: [],
   component: ComponentType.OVERLAY,
-  delivery_mechanism: DeliveryMechanism.LINK,
 };
 
 export const transformExperienceConfigResponseToCreation = (
@@ -23,39 +25,44 @@ export const transformExperienceConfigResponseToCreation = (
   const {
     created_at: createdAt,
     updated_at: updatedAt,
+    version,
+    experience_config_history_id: experienceConfigHistoryId,
     ...rest
   } = experienceConfig;
   return {
     ...rest,
-    component_title: experienceConfig.component_title ?? "",
+    title: experienceConfig.title ?? "",
+    description: experienceConfig.description ?? "",
+    accept_button_label: experienceConfig.accept_button_label ?? "",
+    reject_button_label: experienceConfig.reject_button_label ?? "",
+    save_button_label: experienceConfig.save_button_label ?? "",
     regions: experienceConfig.regions ?? defaultInitialValues.regions,
     component: experienceConfig.component ?? defaultInitialValues.component,
-    delivery_mechanism:
-      experienceConfig.delivery_mechanism ??
-      defaultInitialValues.delivery_mechanism,
   };
 };
 
-const privacyCenterValidationSchema = Yup.object().shape({
-  link_label: Yup.string().required().label("Link label"),
-  component_description: Yup.string().nullable(),
-});
-
-const bannerValidationSchema = Yup.object().shape({
-  banner_title: Yup.string().required().label("Banner title"),
-  banner_description: Yup.string().nullable(),
-  link_label: Yup.string().label("Link label").nullable(),
-});
-
 const buttonGroupValidationSchema = Yup.object().shape({
-  acknowledgement_button_label: Yup.string()
-    .required()
-    .label("Acknowledgment button label"),
-  confirmation_button_label: Yup.string()
-    .required()
-    .label("Confirmation button label"),
+  accept_button_label: Yup.string().label("Accept button label").required(),
   reject_button_label: Yup.string().required().label("Reject button label"),
+  save_button_label: Yup.string().required().label("Save button label"),
 });
+
+const privacyCenterValidationSchema = Yup.object()
+  .shape({
+    title: Yup.string().required().label("Title"),
+    description: Yup.string().required().label("Description"),
+  })
+  .concat(buttonGroupValidationSchema);
+
+const bannerValidationSchema = Yup.object()
+  .shape({
+    title: Yup.string().required().label("Banner title"),
+    description: Yup.string().required().label("Banner description"),
+    acknowledgement_button_label: Yup.string()
+      .nullable()
+      .label("Acknowledge button label"),
+  })
+  .concat(buttonGroupValidationSchema);
 
 export interface ExperienceFormRules {
   isOverlay: boolean;
@@ -64,7 +71,7 @@ export interface ExperienceFormRules {
 /**
  * Use the various rules/conditions of a privacy experience form
  */
-export const useExperienceFormRules = ({
+export const useExperienceForm = ({
   privacyExperience,
 }: {
   privacyExperience: ExperienceConfigCreate;
@@ -80,7 +87,7 @@ export const useExperienceFormRules = ({
     if (!isOverlay) {
       return privacyCenterValidationSchema;
     }
-    return bannerValidationSchema.concat(buttonGroupValidationSchema);
+    return bannerValidationSchema;
   }, [isOverlay]);
 
   return {
