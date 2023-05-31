38bb94625cdc59168a95389a5c651e04e1bd9bd3
Thomas <ThomasLaPiana@users.noreply.github.com>
2023-05-31 17:56:23 +0800

    feat: add downgrades

--- src/fides/api/ctl/migrations/versions/63b482f5b49b_upgrade_to_fideslang_1_4.py
+++ src/fides/api/ctl/migrations/versions/63b482f5b49b_upgrade_to_fideslang_1_4.py
@@ -8,13 +8,9 @@ Create Date: 2023-05-26 07:51:25.947974
 import asyncio
 from typing import Dict
 
-from fideslang import DEFAULT_TAXONOMY
-
 from fides.api.ctl.database.crud import list_resource, upsert_resources
 from fides.api.ctl.database.session import async_session
 from fides.api.ctl.sql_models import PolicyCtl, PrivacyDeclaration
-from fides.api.oauth.roles import OWNER
-from fides.core.config import CONFIG
 
 # revision identifiers, used by Alembic.
 revision = "63b482f5b49b"
@@ -23,7 +19,7 @@ branch_labels = None
 depends_on = None
 
 # The `key` is the old value, the `value` is the new value
-data_use_updates: Dict[str, str] = {
+data_use_upgrades: Dict[str, str] = {
     "third_party_sharing.payment_processing": "essential.service.payment_processing"
     "third_party_sharing.fraud_detection",
     "essential.service.fraud_detection"
@@ -36,17 +32,20 @@ data_use_updates: Dict[str, str] = {
     "provide": "essential",
     "provide.service": "essential.service",
 }
+data_use_downgrades: Dict[str, str] = {
+    value: key for key, value in data_use_upgrades.items()
+}
 
 
-async def update_privacy_declarations() -> None:
+async def upgrade_privacy_declarations() -> None:
     async with async_session() as session:
         resources = await list_resource(PrivacyDeclaration, session)
 
         for resource in resources:
             current_data_use = resource.data_use
 
-            if current_data_use in data_use_updates.keys():
-                resource.data_use = data_use_updates[resource.data_use]
+            if current_data_use in data_use_upgrades.keys():
+                resource.data_use = data_use_upgrades[resource.data_use]
 
         await upsert_resources(PrivacyDeclaration, resources, session)
 
@@ -54,40 +53,86 @@ async def update_privacy_declarations() -> None:
         existing_resources = await list_resource(PrivacyDeclaration, session)
         assert not any(
             [
-                resource.data_use in data_use_updates.keys()
+                resource.data_use in data_use_upgrades.keys()
                 for resource in existing_resources
             ]
-        ), "ERROR: Data Use Migration for Fideslang v1.4 failed for Privacy Declarations!"
+        ), "ERROR: Data Use Upgrade for Fideslang v1.4 failed for Privacy Declarations!"
 
 
-async def update_policy_rules() -> None:
+async def upgrade_policy_rules() -> None:
     async with async_session() as session:
         resources = await list_resource(PolicyCtl, session)
         for resource in resources:
             for rule in resource.rules:
                 current_data_use = rule.data_use
-                if current_data_use in data_use_updates.keys():
-                    rule.data_use = data_use_updates[resource.data_use]
+                if current_data_use in data_use_upgrades.keys():
+                    rule.data_use = data_use_upgrades[resource.data_use]
         await upsert_resources(PolicyCtl, resources, session)
 
         # Since we can't write tests for data migrations, we do it live
         existing_resources = await list_resource(PolicyCtl, session)
         assert not any(
             [
-                rule.data_use in data_use_updates.keys()
+                rule.data_use in data_use_upgrades.keys()
                 for resource in existing_resources
                 for rule in resource
             ]
-        ), "ERROR: Data Use Migration for Fideslang v1.4 failed for Policy Rules!"
+        ), "ERROR: Data Use Upgrade for Fideslang v1.4 failed for Policy Rules!"
 
 
-def upgrade():
-    # Update every place where an old data use could be
-
+def upgrade() -> None:
+    "Update every place that refers to data uses."
     loop = asyncio.get_running_loop()
-    asyncio.ensure_future(update_privacy_declarations(), loop=loop)
-    asyncio.ensure_future(update_policy_rules(), loop=loop)
+    asyncio.ensure_future(upgrade_privacy_declarations(), loop=loop)
+    asyncio.ensure_future(upgrade_policy_rules(), loop=loop)
 
 
-def downgrade():
+async def downgrade_privacy_declarations() -> None:
+    async with async_session() as session:
+        resources = await list_resource(PrivacyDeclaration, session)
+
+        for resource in resources:
+            current_data_use = resource.data_use
+
+            if current_data_use in data_use_downgrades.values():
+                resource.data_use = data_use_downgrades[resource.data_use]
+
+        await upsert_resources(PrivacyDeclaration, resources, session)
+
+        # Since we can't write tests for data migrations, we do it live
+        existing_resources = await list_resource(PrivacyDeclaration, session)
+        assert not any(
+            [
+                resource.data_use in data_use_downgrades.keys()
+                for resource in existing_resources
+            ]
+        ), "ERROR: Data Use Downgrade for Fideslang v1.4 failed for Privacy Declarations!"
+
+
+async def downgrade_policy_rules() -> None:
+    async with async_session() as session:
+        resources = await list_resource(PolicyCtl, session)
+        for resource in resources:
+            for rule in resource.rules:
+                current_data_use = rule.data_use
+                if current_data_use in data_use_downgrades.keys():
+                    rule.data_use = data_use_downgrades[resource.data_use]
+        await upsert_resources(PolicyCtl, resources, session)
+
+        # Since we can't write tests for data migrations, we do it live
+        existing_resources = await list_resource(PolicyCtl, session)
+        assert not any(
+            [
+                rule.data_use in data_use_downgrades.keys()
+                for resource in existing_resources
+                for rule in resource
+            ]
+        ), "ERROR: Data Use Downgrade from Fideslang v1.4 failed for Policy Rules!"
+
+
+def downgrade() -> None:
+    "Revert every place that refers to data uses."
+    loop = asyncio.get_running_loop()
+    asyncio.ensure_future(downgrade_privacy_declarations(), loop=loop)
+    asyncio.ensure_future(downgrade_policy_rules(), loop=loop)
     pass
