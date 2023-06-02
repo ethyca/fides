2ec32791e73280f181eabf9da2f8a18b230eca8c
Dawn Pattison <pattisdr@users.noreply.github.com>
2023-06-02 00:34:44 -0500

    Fideslang 1.4 Migration Updates (#3428)

--- src/fides/api/ctl/migrations/versions/63b482f5b49b_upgrade_to_fideslang_1_4.py
+++ src/fides/api/ctl/migrations/versions/63b482f5b49b_upgrade_to_fideslang_1_4.py
@@ -1,22 +1,22 @@
 """Upgrade to fideslang 1.4
 
 Revision ID: 63b482f5b49b
-Revises: 2661f31daffb
+Revises: 587c53fe3e99
 Create Date: 2023-05-26 07:51:25.947974
 
 """
-import asyncio
-from typing import Dict
+import json
+from typing import Dict, List, Optional
 
-from fides.api.ctl.database.crud import list_resource, upsert_resources
-from fides.api.ctl.database.session import async_session
-from fides.api.ctl.sql_models import PolicyCtl, PrivacyDeclaration
-from fides.api.models.privacy_notice import PrivacyNotice, PrivacyNoticeHistory
-from fides.api.models.privacy_request import Consent
+from alembic import op
+from loguru import logger
+from sqlalchemy import text
+from sqlalchemy.engine import Connection, ResultProxy
+from sqlalchemy.sql.elements import TextClause
 
 # revision identifiers, used by Alembic.
 revision = "63b482f5b49b"
-down_revision = "2661f31daffb"
+down_revision = "587c53fe3e99"
 branch_labels = None
 depends_on = None
 
@@ -39,102 +39,66 @@ data_use_downgrades: Dict[str, str] = {
 }
 
 
-async def upgrade_privacy_declarations() -> None:
-    async with async_session() as session:
-        resources = await list_resource(PrivacyDeclaration, session)
+def update_privacy_declaration_data_uses(bind, data_use_map: Dict[str, str]):
+    """Upgrade or downgrade data uses from fideslang 1.4 for privacy declarations"""
+    existing_ctl_policies: ResultProxy = bind.execute(
+        text("SELECT id, data_use FROM privacydeclaration;")
+    )
+    for row in existing_ctl_policies:
+        data_use: str = row["data_use"]
+        updated_data_use: Optional[str] = data_use_map.get(data_use, None)
 
-        for resource in resources:
-            current_data_use = resource.data_use
-
-            if current_data_use in data_use_upgrades.keys():
-                resource.data_use = data_use_upgrades[resource.data_use]
-
-        await upsert_resources(PrivacyDeclaration, resources, session)
-
-        # Since we can't write tests for data migrations, we do it live
-        existing_resources = await list_resource(PrivacyDeclaration, session)
-        assert not any(
-            [
-                resource.data_use in data_use_upgrades.keys()
-                for resource in existing_resources
-            ]
-        ), "ERROR: Data Use Upgrade for Fideslang v1.4 failed for Privacy Declarations!"
+        if updated_data_use:
+            update_data_use_query: TextClause = text(
+                "UPDATE privacydeclaration SET data_use = :updated_use WHERE id= :declaration_id"
+            )
+            bind.execute(
+                update_data_use_query,
+                {"declaration_id": row["id"], "updated_use": updated_data_use},
+            )
 
 
-async def upgrade_policy_rules() -> None:
-    async with async_session() as session:
-        resources = await list_resource(PolicyCtl, session)
-        for resource in resources:
-            for rule in resource.rules:
-                current_data_use = rule.data_use
-                if current_data_use in data_use_upgrades.keys():
-                    rule.data_use = data_use_upgrades[resource.data_use]
-        await upsert_resources(PolicyCtl, resources, session)
+def update_ctl_policy_data_uses(bind: Connection, data_use_map: Dict[str, str]):
+    """Upgrade or downgrade data uses from fideslang 1.4 for ctl policies"""
+    existing_ctl_policies: ResultProxy = bind.execute(
+        text("SELECT id, rules FROM ctl_policies;")
+    )
+    for row in existing_ctl_policies:
+        needs_update: bool = False
+        rules: List[Dict] = row["rules"]
+        for i, rule in enumerate(rules or []):
+            data_uses: Dict = rule.get("data_uses", {})
+            for j, val in enumerate(data_uses.get("values", [])):
+                new_data_use: Optional[str] = data_use_map.get(val, None)
+                if new_data_use:
+                    rules[i]["data_uses"]["values"][j] = new_data_use
+                    needs_update = True
 
-        # Since we can't write tests for data migrations, we do it live
-        existing_resources = await list_resource(PolicyCtl, session)
-        assert not any(
-            [
-                rule.data_use in data_use_upgrades.keys()
-                for resource in existing_resources
-                for rule in resource
-            ]
-        ), "ERROR: Data Use Upgrade for Fideslang v1.4 failed for Policy Rules!"
+        if needs_update:
+            update_data_use_query: TextClause = text(
+                "UPDATE ctl_policies SET rules = :updated_rules WHERE id= :policy_id"
+            )
+            bind.execute(
+                update_data_use_query,
+                {"policy_id": row["id"], "updated_rules": json.dumps(rules)},
+            )
 
 
 def upgrade() -> None:
-    "Update every place that refers to data uses."
-    loop = asyncio.get_running_loop()
-    asyncio.ensure_future(upgrade_privacy_declarations(), loop=loop)
-    asyncio.ensure_future(upgrade_policy_rules(), loop=loop)
+    bind: Connection = op.get_bind()
+
+    logger.info("Upgrading data use on privacy declaration")
+    update_privacy_declaration_data_uses(bind, data_use_upgrades)
+
+    logger.info("Upgrading ctl policy rule data uses")
+    update_ctl_policy_data_uses(bind, data_use_upgrades)
 
 
-async def downgrade_privacy_declarations() -> None:
-    async with async_session() as session:
-        resources = await list_resource(PrivacyDeclaration, session)
+def downgrade():
+    bind: Connection = op.get_bind()
 
-        for resource in resources:
-            current_data_use = resource.data_use
+    logger.info("Downgrading data use on privacy declaration")
+    update_privacy_declaration_data_uses(bind, data_use_downgrades)
 
-            if current_data_use in data_use_downgrades.values():
-                resource.data_use = data_use_downgrades[resource.data_use]
-
-        await upsert_resources(PrivacyDeclaration, resources, session)
-
-        # Since we can't write tests for data migrations, we do it live
-        existing_resources = await list_resource(PrivacyDeclaration, session)
-        assert not any(
-            [
-                resource.data_use in data_use_downgrades.keys()
-                for resource in existing_resources
-            ]
-        ), "ERROR: Data Use Downgrade for Fideslang v1.4 failed for Privacy Declarations!"
-
-
-async def downgrade_policy_rules() -> None:
-    async with async_session() as session:
-        resources = await list_resource(PolicyCtl, session)
-        for resource in resources:
-            for rule in resource.rules:
-                current_data_use = rule.data_use
-                if current_data_use in data_use_downgrades.keys():
-                    rule.data_use = data_use_downgrades[resource.data_use]
-        await upsert_resources(PolicyCtl, resources, session)
-
-        # Since we can't write tests for data migrations, we do it live
-        existing_resources = await list_resource(PolicyCtl, session)
-        assert not any(
-            [
-                rule.data_use in data_use_downgrades.keys()
-                for resource in existing_resources
-                for rule in resource
-            ]
-        ), "ERROR: Data Use Downgrade from Fideslang v1.4 failed for Policy Rules!"
-
-
-def downgrade() -> None:
-    "Revert every place that refers to data uses."
-    loop = asyncio.get_running_loop()
-    asyncio.ensure_future(downgrade_privacy_declarations(), loop=loop)
-    asyncio.ensure_future(downgrade_policy_rules(), loop=loop)
-    pass
+    logger.info("Downgrading ctl policy rule data uses")
+    update_ctl_policy_data_uses(bind, data_use_downgrades)
