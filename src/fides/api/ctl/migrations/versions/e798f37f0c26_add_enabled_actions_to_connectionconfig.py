2c584cbbdf8ac3b36e1169ba00e53a5fd0051319
Adrian Galvan <adrian@ethyca.com>
2023-06-06 09:58:47 -0700

    Fixing downrev

--- src/fides/api/ctl/migrations/versions/e798f37f0c26_add_enabled_actions_to_connectionconfig.py
+++ src/fides/api/ctl/migrations/versions/e798f37f0c26_add_enabled_actions_to_connectionconfig.py
@@ -1,7 +1,7 @@
 """add enabled_actions to connectionconfig
 
 Revision ID: e798f37f0c26
-Revises: 63b482f5b49b
+Revises: 317e6197c76a
 Create Date: 2023-05-11 17:54:05.476225
 
 """
@@ -11,7 +11,7 @@ from sqlalchemy.dialects import postgresql
 
 # revision identifiers, used by Alembic.
 revision = "e798f37f0c26"
-down_revision = "63b482f5b49b"
+down_revision = "317e6197c76a"
 branch_labels = None
 depends_on = None
 
