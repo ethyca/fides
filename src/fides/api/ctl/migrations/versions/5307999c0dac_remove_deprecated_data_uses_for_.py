2a3d243a32c1a7292b3102325e6b1530c0d93f8d
Thomas <ThomasLaPiana@users.noreply.github.com>
2023-06-11 12:40:34 +0800

    feat: first pass at a migration

--- src/fides/api/ctl/migrations/versions/5307999c0dac_remove_deprecated_data_uses_for_.py
+++ src/fides/api/ctl/migrations/versions/5307999c0dac_remove_deprecated_data_uses_for_.py
@@ -6,7 +6,10 @@ Create Date: 2023-06-11 11:15:53.386526
 
 """
 from alembic import op
-import sqlalchemy as sa
+
+from loguru import logger
+from sqlalchemy import text
+from sqlalchemy.engine import Connection
 
 
 # revision identifiers, used by Alembic.
@@ -16,12 +19,12 @@ branch_labels = None
 depends_on = None
 
 
-def upgrade():
-    # Remove all "default" Data uses
+def upgrade() -> None:
+    logger.info("Removing default data uses.")
+    bind: Connection = op.get_bind()
+    bind.execute(text("DELETE FROM ctl_data_uses WHERE is_default = TRUE;"))
 
-    # Insert the new default data uses
+
+def downgrade() -> None:
     pass
-
-
-def downgrade():
-    pass
+    # This is not reversible, because it relies on an older version of Fideslang
