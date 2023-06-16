21d3bab7d3d3893c0bac11d63949775d3b9ad302
Adrian Galvan <adrian@ethyca.com>
2023-06-16 16:02:44 -0700

    Adding custom auth for Heap

--- tests/fixtures/saas/heap_fixtures.py
+++ tests/fixtures/saas/heap_fixtures.py
@@ -1,8 +1,7 @@
-from typing import Any, Dict, Generator
+from typing import Any, Dict
 
 import pydash
 import pytest
-import requests
 
 from tests.ops.integration_tests.saas.connector_runner import (
     ConnectorRunner,
@@ -23,71 +22,8 @@ def heap_secrets(saas_config) -> Dict[str, Any]:
 
 
 @pytest.fixture(scope="session")
-def heap_identity_email(saas_config) -> str:
-    return (
-        pydash.get(saas_config, "heap.identity_email") or secrets["identity_email"]
-    )
-
-
-@pytest.fixture(scope="session")
 def heap_erasure_identity_email() -> str:
     return generate_random_email()
-
-
-class heapClient:
-    def __init__(self, secrets: Dict[str, Any]):
-        self.base_url = f"https://{secrets['domain']}"
-        # self.headers = (
-        #     {
-        #         "Authorization": f"Basic {secrets['api_key']}",
-        #     },
-        # )
-        self.auth = secrets["app_id"], secrets["api_key"]
-
-        self.api_key = secrets['api_key']
-        self.app_id = secrets["app_id"]
-
-    def create_device(self, email) -> str:
-        response = requests.post(
-            url=f"{self.base_url}/api/add_user_properties",
-            #auth={self.auth},
-            json={
-                "app_id": self.app_id,
-                "identity": email,
-                "properties": {
-                    "age": "25",
-                    "language": "English",
-                    "profession": "Scientist",
-                    "email": email
-                }
-            },
-        )
-        
-        return response.ok
-
-    def get_device(self, player_id: str):
-        response = requests.get(
-            url=f"{self.base_url}/api/v1/players/{player_id}",
-            headers={"Authorization": self.api_key},
-            params={"app_id": self.app_id},
-        )
-        if response.ok:
-            return response.json()["id"]
-
-
-@pytest.fixture
-def heap_client(heap_secrets) -> Generator:
-    yield heapClient(heap_secrets)
-
-
-@pytest.fixture
-def heap_erasure_data(
-    heap_client: heapClient,
-    heap_erasure_identity_email: str,
-) -> Generator:
-    # create user
-    user = heap_client.create_device(heap_erasure_identity_email)
-    yield user
 
 
 @pytest.fixture
@@ -95,14 +31,5 @@ def heap_runner(
     db,
     cache,
     heap_secrets,
-   # heap_external_references,
-    #heap_erasure_external_references,
 ) -> ConnectorRunner:
-    return ConnectorRunner(
-        db,
-        cache,
-        "heap",
-        heap_secrets,
-        #external_references=heap_external_references,
-       # erasure_external_references=heap_erasure_external_references,
-    )
+    return ConnectorRunner(db, cache, "heap", heap_secrets)
--- tests/ops/integration_tests/saas/test_heap_task.py
+++ tests/ops/integration_tests/saas/test_heap_task.py
@@ -2,24 +2,20 @@ import pytest
 
 from fides.api.models.policy import Policy
 from tests.ops.integration_tests.saas.connector_runner import ConnectorRunner
-from tests.ops.test_helpers.saas_test_utils import poll_for_existence
 
 
 @pytest.mark.integration_saas
 class TestHeapConnector:
     def test_connection(self, heap_runner: ConnectorRunner):
         heap_runner.test_connection()
-    
+
     async def test_non_strict_erasure_request(
         self,
         heap_runner: ConnectorRunner,
         policy: Policy,
         erasure_policy_string_rewrite: Policy,
         heap_erasure_identity_email: str,
-        heap_erasure_data,
-        heap_client,
     ):
-        
         (
             access_results,
             erasure_results,
@@ -30,8 +26,5 @@ class TestHeapConnector:
         )
 
         assert erasure_results == {
-            "heap_instance:auth_token": 0,
-            "heap_instance:user": 1
+            "heap_instance:user": 1,
         }
-
-       
