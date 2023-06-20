47010a90266486e2040a49ef0d05271c9813aa1f
Dawn Pattison <pattisdr@users.noreply.github.com>
2023-06-20 11:24:15 -0500

    Bump fideslang commit

--- tests/ops/api/v1/endpoints/test_dataset_endpoints.py
+++ tests/ops/api/v1/endpoints/test_dataset_endpoints.py
@@ -19,7 +19,6 @@ from fides.api.api.v1.scope_registry import (
     DATASET_CREATE_OR_UPDATE,
     DATASET_DELETE,
     DATASET_READ,
-    CTL_DATASET_READ,
 )
 from fides.api.api.v1.urn_registry import (
     CONNECTION_DATASETS,
@@ -27,10 +26,9 @@ from fides.api.api.v1.urn_registry import (
     DATASET_CONFIGS,
     DATASET_VALIDATE,
     DATASETCONFIG_BY_KEY,
-    CONNECTION_DATASETS,
+    DATASETS,
     V1_URL_PREFIX,
     YAML_DATASETS,
-    DATASETS,
 )
 from fides.api.models.connectionconfig import ConnectionConfig
 from fides.api.models.datasetconfig import DatasetConfig
