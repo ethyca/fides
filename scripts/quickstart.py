5904f058546f8c7bf0e4333f0e430757d73b588b
Thomas <ThomasLaPiana@users.noreply.github.com>
2023-06-21 12:38:49 +0800

    fix: docs config imports

--- scripts/quickstart.py
+++ scripts/quickstart.py
@@ -17,7 +17,7 @@ from fides.api.api.v1 import urn_registry as ops_urls
 from fides.api.models.connectionconfig import ConnectionType
 from fides.api.models.policy import ActionType
 from fides.common.api.scope_registry import SCOPE_REGISTRY
-from fides.core.config import get_config
+from fides.config import get_config
 
 CONFIG = get_config()
 
