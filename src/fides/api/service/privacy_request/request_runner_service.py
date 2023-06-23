0f36eb20d1cc9eb0bb99f7eeb53d945591a6526e
Adam Sachs <adam@ethyca.com>
2023-06-22 21:14:33 -0400

    pass data category field mapping through to access result upload functions

--- src/fides/api/service/privacy_request/request_runner_service.py
+++ src/fides/api/service/privacy_request/request_runner_service.py
@@ -231,6 +231,7 @@ def upload_access_results(  # pylint: disable=R0912
                 privacy_request=privacy_request,
                 data=filtered_results,
                 storage_key=storage_destination.key,  # type: ignore
+                data_category_field_mapping=dataset_graph.data_category_field_mapping,
             )
             if download_url:
                 download_urls.append(download_url)
--- src/fides/api/service/storage/storage_uploader_service.py
+++ src/fides/api/service/storage/storage_uploader_service.py
@@ -5,6 +5,7 @@ from loguru import logger
 from sqlalchemy.orm import Session
 
 from fides.api.common_exceptions import StorageUploadError
+from fides.api.graph.graph import DataCategoryFieldMapping
 from fides.api.models.privacy_request import PrivacyRequest
 from fides.api.models.storage import StorageConfig
 from fides.api.schemas.storage.storage import (
@@ -17,7 +18,11 @@ from fides.api.tasks.storage import upload_to_local, upload_to_s3
 
 
 def upload(
-    db: Session, *, privacy_request: PrivacyRequest, data: Dict, storage_key: FidesKey
+    db: Session,
+    privacy_request: PrivacyRequest,
+    data: Dict,
+    storage_key: FidesKey,
+    data_category_field_mapping: Optional[DataCategoryFieldMapping] = None,
 ) -> str:
     """
     Retrieves storage configs and calls appropriate upload method
@@ -35,7 +40,7 @@ def upload(
         logger.warning("Storage type not found: {}", storage_key)
         raise StorageUploadError(f"Storage type not found: {storage_key}")
     uploader: Any = _get_uploader_from_config_type(config.type)  # type: ignore
-    return uploader(db, config, data, privacy_request)
+    return uploader(db, config, data, privacy_request, data_category_field_mapping)
 
 
 def get_extension(resp_format: ResponseFormat) -> str:
@@ -76,7 +81,11 @@ def _get_uploader_from_config_type(storage_type: StorageType) -> Any:
 
 
 def _s3_uploader(
-    _: Session, config: StorageConfig, data: Dict, privacy_request: PrivacyRequest
+    _: Session,
+    config: StorageConfig,
+    data: Dict,
+    privacy_request: PrivacyRequest,
+    data_category_field_mapping: Optional[DataCategoryFieldMapping],
 ) -> str:
     """Constructs necessary info needed for s3 before calling upload"""
     file_key: str = _construct_file_key(privacy_request.id, config)
@@ -85,13 +94,17 @@ def _s3_uploader(
     auth_method = config.details[StorageDetails.AUTH_METHOD.value]
 
     return upload_to_s3(
-        config.secrets, data, bucket_name, file_key, config.format.value, privacy_request, auth_method  # type: ignore
+        config.secrets, data, bucket_name, file_key, config.format.value, privacy_request, auth_method, data_category_field_mapping  # type: ignore
     )
 
 
 def _local_uploader(
-    _: Session, config: StorageConfig, data: Dict, privacy_request: PrivacyRequest
+    _: Session,
+    config: StorageConfig,
+    data: Dict,
+    privacy_request: PrivacyRequest,
+    data_category_field_mapping: Optional[DataCategoryFieldMapping],
 ) -> str:
     """Uploads data to local storage, used for quick-start/demo purposes"""
     file_key: str = _construct_file_key(privacy_request.id, config)
-    return upload_to_local(data, file_key, privacy_request, config.format.value)  # type: ignore
+    return upload_to_local(data, file_key, privacy_request, config.format.value, data_category_field_mapping)  # type: ignore
