bdb8f369d5d2032999c713128f41766b0c930d53
Dawn Pattison <pattisdr@users.noreply.github.com>
2023-06-28 17:32:02 -0500

    Fixture was renamed.

--- tests/ops/api/v1/endpoints/test_privacy_experience_endpoints.py
+++ tests/ops/api/v1/endpoints/test_privacy_experience_endpoints.py
@@ -382,7 +382,7 @@ class TestGetPrivacyExperiences:
         "privacy_notice_us_co_provide_service_operations",  # not displayed in overlay or privacy center
         "privacy_notice_eu_cy_provide_service_frontend_only",  # doesn't overlap with any regions,
         "privacy_experience_overlay",  # us_ca
-        "privacy_notice_eu_fr_provide_service_frontend_only",  # eu_fr
+        "privacy_notice_fr_provide_service_frontend_only",  # eu_fr
         "privacy_notice_us_ca_provide",  # us_ca
         "privacy_experience_privacy_center",
     )
