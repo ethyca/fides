035a1b6fcd337ead4a3b111b24559b56fa85f544
Adam Sachs <adam@ethyca.com>
2023-06-28 08:29:42 -0400

    remove unused import

--- src/fides/api/util/saas_util.py
+++ src/fides/api/util/saas_util.py
@@ -3,7 +3,6 @@ from __future__ import annotations
 import json
 import re
 from collections import defaultdict
-from functools import reduce
 from typing import Any, Dict, List, Optional, Set, Tuple
 
 import yaml
