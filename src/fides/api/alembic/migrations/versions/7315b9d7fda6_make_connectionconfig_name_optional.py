cb9c8ab831729c39ac14d2777794083bcaa17b7b
Adrian Galvan <adrian@ethyca.com>
2023-06-29 11:35:28 -0700

    Fixing downrev

--- src/fides/api/alembic/migrations/versions/7315b9d7fda6_make_connectionconfig_name_optional.py
+++ src/fides/api/alembic/migrations/versions/7315b9d7fda6_make_connectionconfig_name_optional.py
@@ -1,23 +1,21 @@
 """make ConnectionConfig.name optional
 
 Revision ID: 7315b9d7fda6
-Revises: 2be84e68df32
+Revises: d2996381c4dd
 Create Date: 2023-06-26 20:39:29.953904
 
 """
-from alembic import op
 import sqlalchemy as sa
-
+from alembic import op
 
 # revision identifiers, used by Alembic.
 revision = "7315b9d7fda6"
-down_revision = "2be84e68df32"
+down_revision = "d2996381c4dd"
 branch_labels = None
 depends_on = None
 
 
 def upgrade():
-    pass
     op.alter_column("connectionconfig", "name", nullable=True)
 
     op.drop_index(op.f("ix_connectionconfig_name"), table_name="connectionconfig")
