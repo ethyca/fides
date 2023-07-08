3340e5dc4753f94070f9f3bb5fa81b7f17723c03
Rachel Silver <raichlea@gmail.com>
2023-07-07 22:26:02 -0400

    Updated MySQL required fields

--- src/fides/api/schemas/connection_configuration/connection_secrets_mysql.py
+++ src/fides/api/schemas/connection_configuration/connection_secrets_mysql.py
@@ -11,15 +11,14 @@ from fides.api.schemas.connection_configuration.connection_secrets import (
 class MySQLSchema(ConnectionConfigSecretsSchema):
     """Schema to validate the secrets needed to connect to a MySQL Database"""
 
-    username: Optional[str] = None
-    password: Optional[str] = Field(None, sensitive=True)
+    host: str = Field(None, title="Host")  
+    port: int = Field(None, title="Port")  
+    username: str = Field(None, title="Username") 
+    password: str = Field(None, title="Password", sensitive=True)
     dbname: Optional[str] = Field(None, title="DB Name")
-    host: Optional[
-        str
-    ] = None  # Either the entire "url" *OR* the "host" should be supplied.
-    port: Optional[int] = None
 
-    _required_components: List[str] = ["host"]
+
+    _required_components: List[str] = ["host","port","username","password"]
 
 
 class MySQLDocsSchema(MySQLSchema, NoValidationSchema):
