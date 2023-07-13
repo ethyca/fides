e7ad2aac9c8ac4aa835d7e0d8ae7e311e0d1077d
Allison King <allisonjuliaking@gmail.com>
2023-07-13 11:34:04 -0400

    Make tabs data driven

--- clients/fides-js/src/components/Tabs.tsx
+++ clients/fides-js/src/components/Tabs.tsx
@@ -1,64 +1,49 @@
-import { h, JSX } from "preact";
+import { h } from "preact";
 
-type ButtonAttrs = JSX.IntrinsicElements["button"];
+const TCF_TABS = [
+  { name: "Purposes", content: "one" },
+  { name: "Features", content: "two" },
+  { name: "Vendors", content: "three" },
+];
 
-const TabButton = ({ children, ...props }: ButtonAttrs) => (
-  <button role="tab" type="button" className="fides-tab-button" {...props}>
-    {children}
-  </button>
-);
-
-const Tabs = () => (
-  <div className="fides-tabs">
-    <ul role="tablist" className="fides-tab-list">
-      <li role="presentation">
-        <TabButton id="tab1" aria-selected="true">
-          Tab one
-        </TabButton>
-      </li>
-      <li role="presentation">
-        <TabButton id="tab2">Tab two</TabButton>
-      </li>
-      <li role="presentation">
-        <TabButton id="tab3">Tab three</TabButton>
-      </li>
-      <li role="presentation">
-        <TabButton id="tab4">Tab four</TabButton>
-      </li>
-    </ul>
-    <div className="tabpanel-container">
-      <section role="tabpanel" id="panel1" aria-labelledby="tab1" tabIndex={0}>
-        one
-      </section>
-      <section
-        role="tabpanel"
-        id="panel2"
-        aria-labelledby="tab2"
-        tabIndex={0}
-        hidden
-      >
-        two
-      </section>
-      <section
-        role="tabpanel"
-        id="panel3"
-        aria-labelledby="tab3"
-        tabIndex={0}
-        hidden
-      >
-        three
-      </section>
-      <section
-        role="tabpanel"
-        id="panel4"
-        aria-labelledby="tab4"
-        tabIndex={0}
-        hidden
-      >
-        four
-      </section>
+const Tabs = () => {
+  // TODO useState, but that doesn't work with cypress component tests yet...
+  const activeTabIndex = 1;
+  return (
+    <div className="fides-tabs">
+      <ul role="tablist" className="fides-tab-list">
+        {TCF_TABS.map(({ name }, idx) => (
+          <li role="presentation" key={name}>
+            <button
+              id={`fides-tab-${name}`}
+              aria-selected={idx === activeTabIndex}
+              //   onClick={() => {
+              //     setActiveTabIndex(idx);
+              //   }}
+              role="tab"
+              type="button"
+              className="fides-tab-button"
+            >
+              {name}
+            </button>
+          </li>
+        ))}
+      </ul>
+      <div className="tabpanel-container">
+        {TCF_TABS.map(({ name, content }, idx) => (
+          <section
+            role="tabpanel"
+            id={`fides-panel-${name}`}
+            aria-labelledby={`fides-tab-${name}`}
+            tabIndex={0}
+            hidden={idx !== activeTabIndex}
+            key={name}
+          >
+            {content}
+          </section>
+        ))}
+      </div>
     </div>
-  </div>
-);
-
+  );
+};
 export default Tabs;
