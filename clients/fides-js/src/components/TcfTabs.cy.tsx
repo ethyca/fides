cee40f9d4f014c5ccb39054ba9229ab6ada888e2
Allison King <allisonjuliaking@gmail.com>
2023-07-13 17:16:00 -0400

    Disable eslint for cypress test

--- clients/fides-js/src/components/TcfTabs.cy.tsx
+++ clients/fides-js/src/components/TcfTabs.cy.tsx
@@ -2,6 +2,7 @@ import h from "preact";
 import TcfTabs from "./TcfTabs";
 
 describe("<TcfTabs />", () => {
+  // eslint-disable-next-line jest/expect-expect
   it("renders", () => {
     cy.mount(<TcfTabs />);
   });
