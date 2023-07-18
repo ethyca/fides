4182bde690c19ee8c6270a211501cfca7da9e659
Thomas <ThomasLaPiana@users.noreply.github.com>
2023-07-18 18:04:05 +0800

    feat: match the logging format with Fides

--- clients/privacy-center/middleware.ts
+++ clients/privacy-center/middleware.ts
@@ -1,8 +1,12 @@
 import { NextResponse } from 'next/server'
 
 export default function middleware(request) {
-  const response = NextResponse.next()
-  console.log(request);
-  console.log(response);
+  const start = Date.now()
+  const response = NextResponse.next();
+  const stop = Date.now()
+  const handler_time = stop - start
+
+  const log_dict = { method: request.method, status_code: response.status, handler_time: handler_time.getMilliseconds, path: request.destination }
+  console.info(log_dict);
   return response;
 }
