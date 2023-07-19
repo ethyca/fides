1ffd6f6f3b295284f9dc06140fccab05c475d840
Thomas <ThomasLaPiana@users.noreply.github.com>
2023-07-19 12:22:41 +0800

    feat: further tweaks

--- clients/privacy-center/middleware.ts
+++ clients/privacy-center/middleware.ts
@@ -1,12 +1,17 @@
-import { NextResponse } from 'next/server'
+import { NextResponse } from "next/server";
 
 export default function middleware(request) {
-  const start = Date.now()
+  const start = Date.now();
   const response = NextResponse.next();
-  const stop = Date.now()
-  const handler_time = stop - start
+  const stop = Date.now();
+  const handler_time = stop - start;
 
-  const log_dict = { method: request.method, status_code: response.status, handler_time: handler_time.getMilliseconds, path: request.destination }
+  const log_dict = {
+    method: request.method,
+    status_code: response.status,
+    handler_time: `${handler_time}ms`,
+    path: request.destination,
+  };
   console.info(log_dict);
   return response;
 }
