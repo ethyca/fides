c87ac0f2d43c05bc43a91ffdcbb9b53465681b38
Thomas <ThomasLaPiana@users.noreply.github.com>
2023-07-20 14:55:05 +0800

    fix: prettier

--- clients/privacy-center/middleware.ts
+++ clients/privacy-center/middleware.ts
@@ -1,5 +1,5 @@
 import { NextResponse } from "next/server";
-import type { NextRequest} from "next/server";
+import type { NextRequest } from "next/server";
 
 export default function middleware(request: NextRequest) {
   const start = Date.now();
