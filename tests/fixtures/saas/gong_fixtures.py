ee3efe6d151403621a11271324a30370f6a3515a
Soundarya <Soundarya>
2023-07-21 17:33:00 +0000

    Gong access and erasure tests

--- tests/fixtures/saas/gong_fixtures.py
+++ tests/fixtures/saas/gong_fixtures.py
@@ -2,6 +2,8 @@ from typing import Any, Dict, Generator
 
 import pydash
 import pytest
+import requests
+
 
 from tests.ops.integration_tests.saas.connector_runner import (
     ConnectorRunner,
@@ -19,7 +21,11 @@ def gong_secrets(saas_config) -> Dict[str, Any]:
         or secrets["domain"],
         "token": pydash.get(saas_config, "gong.token")
         or secrets["token"],
-        
+        "salesforce_domain": pydash.get(saas_config, "gong.salesforce_domain")
+        or secrets["salesforce_domain"],
+        "salesforce_token": pydash.get(saas_config, "gong.salesforce_token")
+        or secrets["salesforce_token"],
+
         # add the rest of your secrets here
     }
 
@@ -27,13 +33,16 @@ def gong_secrets(saas_config) -> Dict[str, Any]:
 @pytest.fixture(scope="session")
 def gong_identity_email(saas_config) -> str:
     return (
-        pydash.get(saas_config, "gong.identity_email") or secrets["identity_email"]
+        pydash.get(
+            saas_config, "gong.identity_email") or secrets["identity_email"]
     )
-    
+
+
 @pytest.fixture(scope="session")
 def gong_identity_phone_number(saas_config) -> str:
     return (
-        pydash.get(saas_config, "gong.identity_phone_number") or secrets["identity_phone_number"]
+        pydash.get(
+            saas_config, "gong.identity_phone_number") or secrets["identity_phone_number"]
     )
 
 
@@ -52,12 +61,62 @@ def gong_erasure_external_references() -> Dict[str, Any]:
     return {}
 
 
+class GongClient:
+    def __init__(self, secrets: Dict[str, Any]):
+        self.base_url = f"https://{secrets['domain']}"
+        self.salesforce_url = f"https://{secrets['salesforce_domain']}"
+        self.headers = (
+            {
+                "Authorization": f"Basic {secrets['token']}",
+            },
+        )
+        self.salesforce_token = f"Bearer {secrets['salesforce_token']}"
+
+    def create_data(self, email) -> str:
+        response = requests.post(
+            url=f"{self.salesforce_url}/services/data/v54.0/sobjects/Contact",
+            headers={"Authorization": self.salesforce_token},
+            json={
+                "firstName": "Gong",
+                "lastName": "Test",
+                "email": email,
+                "phone": "%2B19518938593"
+            }
+        )
+        return response.json()
+
+    def get_email_data(self, player_id: str):
+        response = requests.get(
+            url=f"{self.base_url}/v2/data-privacy/data-for-email-address",
+            headers=self.headers,
+            params={"emailAddress": gong_erasure_identity_email},
+        )
+        if response.ok:
+            return response.json()["requestId"]
+        
+    def get_phone_data(self, player_id: str):
+        response = requests.get(
+            url=f"{self.base_url}/v2/data-privacy/data-for-phone-number",
+            headers=self.headers,
+            params={"phoneNumber": "%2B19518938593"},
+        )
+        if response.ok:
+            return response.json()["requestId"]
+
+
+@pytest.fixture
+def gong_client(gong_secrets) -> Generator:
+    yield GongClient(gong_secrets)
+
+
 @pytest.fixture
 def gong_erasure_data(
+    gong_client: GongClient,
     gong_erasure_identity_email: str,
 ) -> Generator:
-    # create the data needed for erasure tests here
-    yield {}
+    # create data
+    data = gong_client.create_data(gong_erasure_identity_email)
+    yield data
 
 
 @pytest.fixture
--- tests/ops/integration_tests/saas/test_gong_task.py
+++ tests/ops/integration_tests/saas/test_gong_task.py
@@ -2,6 +2,7 @@ import pytest
 
 from fides.api.models.policy import Policy
 from tests.ops.integration_tests.saas.connector_runner import ConnectorRunner
+from tests.ops.test_helpers.saas_test_utils import poll_for_existence
 
 
 @pytest.mark.integration_saas
@@ -12,30 +13,40 @@ class TestGongConnector:
     async def test_access_request_email(
         self, gong_runner: ConnectorRunner, policy, gong_identity_email: str
     ):
+        
         access_results = await gong_runner.access_request(
             access_policy=policy, identities={"email": gong_identity_email}
         )
         
-    async def test_access_request_phone_number(
-        self, gong_runner: ConnectorRunner, policy, gong_identity_phone_number: str
+    # async def test_access_request_phone_number(
+    #     self, gong_runner: ConnectorRunner, policy, gong_identity_phone_number: str
+    # ):
+    #     access_results = await gong_runner.access_request(
+    #         access_policy=policy, identities={"phoneNumber": gong_identity_phone_number}
+    #     )
+
+    async def test_non_strict_erasure_request(
+        self,
+        gong_runner: ConnectorRunner,
+        policy: Policy,
+        erasure_policy_string_rewrite: Policy,
+        gong_erasure_identity_email: str,
+        gong_erasure_data,
+        gong_client,
     ):
-        access_results = await gong_runner.access_request(
-            access_policy=policy, identities={"phoneNumber": gong_identity_phone_number}
+        (
+            access_results,
+            erasure_results,
+        ) = await gong_runner.non_strict_erasure_request(
+            access_policy=policy,
+            erasure_policy=erasure_policy_string_rewrite,
+            identities={"email": gong_erasure_identity_email},
         )
 
-    # async def test_non_strict_erasure_request(
-    #     self,
-    #     gong_runner: ConnectorRunner,
-    #     policy: Policy,
-    #     erasure_policy_string_rewrite: Policy,
-    #     gong_erasure_identity_email: str,
-    #     gong_erasure_data,
-    # ):
-    #     (
-    #         access_results,
-    #         erasure_results,
-    #     ) = await gong_runner.non_strict_erasure_request(
-    #         access_policy=policy,
-    #         erasure_policy=erasure_policy_string_rewrite,
-    #         identities={"email": gong_erasure_identity_email},
-    #     )
+        assert erasure_results == {
+            "gong_instance:data_privacy": 1,
+        }
+
+        # poll_for_existence(
+        #     gong_client.get_email_data, (player_id,), existence_desired=False
+        # )
