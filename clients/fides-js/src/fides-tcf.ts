4ce9f07d84d32e08a6a869b95c2c44217140a4f1
Allison King <allisonjuliaking@gmail.com>
2023-07-24 18:04:43 -0400

    Update comment

--- clients/fides-js/src/fides-tcf.ts
+++ clients/fides-js/src/fides-tcf.ts
@@ -1,5 +1,6 @@
 /**
- * Fides.js: Javascript library for Fides (https://github.com/ethyca/fides)
+ * Fides-tcf.js: Javascript library for Fides (https://github.com/ethyca/fides), including
+ * features for supporting the Transparency Consent Framework
  *
  * This JS module provides easy access to interact with Fides from a webpage, including the ability to:
  * - initialize the page with default consent options (e.g. opt-out of advertising cookies, opt-in to analytics, etc.)
