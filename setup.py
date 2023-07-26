d1a881edf83990afd1b4a116b51f311f0abcf8ff
Adam Sachs <adam@ethyca.com>
2023-07-26 08:27:43 -0400

    support pymssql as an extra dep in setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
 import pathlib
+from typing import List
 
 from setuptools import find_packages, setup
 
@@ -13,6 +14,43 @@ long_description = open("README.md", encoding="utf-8").read()
 
 install_requires = open("requirements.txt", encoding="utf-8").read().strip().split("\n")
 dev_requires = open("dev-requirements.txt", encoding="utf-8").read().strip().split("\n")
+dangerous_requires = (
+    open("dangerous-requirements.txt", encoding="utf-8").read().strip().split("\n")
+)
+
+
+def optional_requirements(
+    dependency_names: List[str], requires: List[str] = dangerous_requires
+) -> List[str]:
+    """
+    Matches the provided dependency names to lines in `optional-requirements.txt`,
+    and returns the full dependency string for each one.
+    Prevents the need to store version numbers in two places.
+    """
+
+    requirements: List[str] = []
+
+    for dependency in dependency_names:
+        for optional_dependency in requires:
+            if optional_dependency.startswith(dependency):
+                requirements.append(optional_dependency)
+                break
+
+    if len(requirements) == len(dependency_names):
+        return requirements
+
+    raise ModuleNotFoundError
+
+
+# Human-Readable Extras
+# Versions are read from corresponding lines in `optional-requirements.txt`
+extras = {
+    "mssql": optional_requirements(["pymssql"], dangerous_requires),
+}
+dangerous_extras = ["mssql"]  # These extras break on certain platforms
+extras["all"] = sum(
+    [value for key, value in extras.items() if key not in dangerous_extras], []
+)
 
 ###################
 ## Package Setup ##
@@ -35,6 +73,7 @@ setup(
     license="Apache License 2.0",
     install_requires=install_requires,
     dev_requires=dev_requires,
+    extras_require=extras,
     classifiers=[
         "License :: OSI Approved :: Apache Software License",
         "Programming Language :: Python :: 3 :: Only",
