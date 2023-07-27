98ee20058a5c5ed60d2ac7621822c56f31fcbf74
Andrew Jackson <andrew.c.j1995@gmail.com>
2023-07-27 11:25:19 -0400

    Initial removal of choose system screen

--- clients/admin-ui/cypress/e2e/systems-plus.cy.ts
+++ clients/admin-ui/cypress/e2e/systems-plus.cy.ts
@@ -2,6 +2,7 @@ import {
   stubPlus,
   stubSystemCrud,
   stubTaxonomyEntities,
+  stubVendorList
 } from "cypress/support/stubs";
 
 import { SYSTEM_ROUTE } from "~/features/common/nav/v2/routes";
@@ -16,6 +17,30 @@ describe("System management with Plus features", () => {
       fixture: "systems/systems.json",
     }).as("getSystems");
   });
+
+  describe("vendor list", ()=>{
+    beforeEach(()=>{
+      cy.visit(`${SYSTEM_ROUTE}/configure/demo_analytics_system`);
+      stubVendorList();
+    })
+
+    it("can display the vendor list dropdown",()=>{
+      cy.getSelectValueContainer("input-meta.vendor.id")
+    })
+
+    it("contains dictionary entries", ()=>{
+      cy.selectOption("input-meta.vendor.id", "Aniview LTD")
+    })
+    
+    it("can switch entries", ()=>{
+      cy.selectOption("input-meta.vendor.id", "Aniview LTD")
+      cy.getSelectValueContainer("input-meta.vendor.id").contains("Aniview LTD")
+
+      cy.selectOption("input-meta.vendor.id", "Jaduda GmbH")
+      cy.getSelectValueContainer("input-meta.vendor.id").contains("Jaduda GmbH")
+    })
+    
+  })
 
   describe("custom metadata", () => {
     beforeEach(() => {
