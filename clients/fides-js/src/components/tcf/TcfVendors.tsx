b46b2f895362c6c0279701eea0eb4ffd08ec73b7
Allison King <allisonjuliaking@gmail.com>
2023-07-31 12:56:00 -0400

    Add vendor details

--- clients/fides-js/src/components/tcf/TcfVendors.tsx
+++ clients/fides-js/src/components/tcf/TcfVendors.tsx
@@ -1,8 +1,33 @@
 import { h } from "preact";
-import { TCFVendorRecord } from "../../lib/tcf/types";
+import { useState } from "preact/hooks";
+import { EmbeddedLineItem, TCFVendorRecord } from "../../lib/tcf/types";
 import { PrivacyExperience } from "../../lib/consent-types";
 import { UpdateEnabledIds } from "./TcfOverlay";
 import DataUseToggle from "../DataUseToggle";
+import FilterButtons from "./FilterButtons";
+
+const FILTERS = [{ name: "All vendors" }, { name: "GVL vendors" }];
+
+const VendorDetails = ({
+  lineItems,
+}: {
+  lineItems: EmbeddedLineItem[] | undefined;
+}) => {
+  if (!lineItems || lineItems.length === 0) {
+    return null;
+  }
+
+  return (
+    <p className="fides-tcf-purpose-vendor fides-tcf-toggle-content">
+      <span className="fides-tcf-purpose-vendor-title">Purposes</span>
+      <ul className="fides-tcf-purpose-vendor-list">
+        {lineItems.map((item) => (
+          <li key={item.id}>{item.name}</li>
+        ))}
+      </ul>
+    </p>
+  );
+};
 
 const TcfVendors = ({
   allVendors,
@@ -13,6 +38,8 @@ const TcfVendors = ({
   enabledIds: string[];
   onChange: (payload: UpdateEnabledIds) => void;
 }) => {
+  const [isFiltered, setIsFiltered] = useState(false);
+
   if (!allVendors || allVendors.length === 0) {
     // TODO: empty state?
     return null;
@@ -32,16 +59,38 @@ const TcfVendors = ({
     }
   };
 
+  const handleFilter = (index: number) => {
+    if (index === 0) {
+      setIsFiltered(false);
+    } else {
+      setIsFiltered(true);
+    }
+  };
+
+  const vendorsToDisplay = isFiltered
+    ? allVendors.filter((v) => v.is_gvl)
+    : allVendors;
+
   return (
     <div>
-      {allVendors.map((vendor) => (
+      <FilterButtons filters={FILTERS} onChange={handleFilter} />
+      {vendorsToDisplay.map((vendor) => (
         <DataUseToggle
           dataUse={{ key: vendor.id, name: vendor.name }}
           onToggle={() => {
             handleToggle(vendor);
           }}
           checked={enabledIds.indexOf(vendor.id) !== -1}
-        />
+          badge={vendor.is_gvl ? "GVL" : undefined}
+        >
+          <div className="fides-background-dark ">
+            <p>{vendor.description}</p>
+            <VendorDetails lineItems={vendor.purposes} />
+            <VendorDetails lineItems={vendor.special_purposes} />
+            <VendorDetails lineItems={vendor.features} />
+            <VendorDetails lineItems={vendor.special_features} />
+          </div>
+        </DataUseToggle>
       ))}
     </div>
   );
