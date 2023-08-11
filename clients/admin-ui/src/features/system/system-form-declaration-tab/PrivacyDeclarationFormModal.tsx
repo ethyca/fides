7d020b28130b5c9d0799cd9dcc70e7ca3610c9d5
Jeremy Pople <jeremy@ethyca.com>
2023-08-11 15:40:20 -0400

    finish styling changes to data use form

---
+++ clients/admin-ui/src/features/system/system-form-declaration-tab/PrivacyDeclarationFormModal.tsx
@@ -0,0 +1,53 @@
+import {
+  Box,
+  Button,
+  Flex,
+  Heading,
+  HStack,
+  Modal,
+  ModalBody,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  ModalOverlay,
+  Spacer,
+  Text,
+  useToast,
+} from "@fidesui/react";
+
+type DataUseFormModalProps = {
+  isOpen: boolean;
+  onClose: () => void;
+  testId?: String;
+  children: React.ReactNode;
+};
+
+export const PrivacyDeclarationFormModal: React.FC<DataUseFormModalProps> = ({
+  isOpen,
+  onClose,
+  testId = "privacy-declaration-modal",
+  children,
+}) => {
+  return (
+    <Modal isOpen={isOpen} onClose={onClose} scrollBehavior="inside" size="3xl">
+      <ModalOverlay />
+      <ModalContent textAlign="left" p={0} data-testid={testId}>
+        <ModalHeader p={0}>
+          <Box
+            backgroundColor="gray.50"
+            px={6}
+            py={4}
+            border="1px"
+            borderColor="gray.200"
+            borderTopRadius={6}
+          >
+            <Heading as="h3" size="sm">
+              Configure data use
+            </Heading>
+          </Box>
+        </ModalHeader>
+        <ModalBody pb={4}>{children}</ModalBody>
+      </ModalContent>
+    </Modal>
+  );
+};
