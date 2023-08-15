412551bf283ccf26ff25485b6691eb81dba847d6
Jeremy Pople <jeremy@ethyca.com>
2023-08-15 01:14:13 -0400

    integrate new backend and generate openAPI

---
+++ clients/admin-ui/src/types/api/models/LegalBasisForProfilingEnum.ts
@@ -0,0 +1,12 @@
+/* istanbul ignore file */
+/* tslint:disable */
+/* eslint-disable */
+
+/**
+ * The model for describing the legal basis under which profiling is performed
+ */
+export enum LegalBasisForProfilingEnum {
+  EXPLICIT_CONSENT = "Explicit consent",
+  CONTRACT = "Contract",
+  AUTHORISED_BY_LAW = "Authorised by law",
+}
