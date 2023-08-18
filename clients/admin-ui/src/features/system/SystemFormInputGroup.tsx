6b5294141567c3a88033baac8efd2881f25b49eb
TheAndrewJackson <andrew.c.j1995@gmail.com>
2023-08-17 22:34:33 -0400

    format

--- clients/admin-ui/src/features/system/SystemFormInputGroup.tsx
+++ clients/admin-ui/src/features/system/SystemFormInputGroup.tsx
@@ -9,43 +9,43 @@ const SystemFormInputGroup = ({
   HeadingButton?: React.ReactNode;
   children?: React.ReactNode;
 }) => (
-    <Stack spacing={4}>
+  <Stack spacing={4}>
+    <Box
+      maxWidth="720px"
+      border="1px"
+      borderColor="gray.200"
+      borderRadius={6}
+      overflow="visible"
+      mt={6}
+    >
       <Box
-        maxWidth="720px"
-        border="1px"
+        backgroundColor="gray.50"
+        px={6}
+        py={4}
+        display="flex"
+        flexDirection="row"
+        alignItems="center"
+        borderBottom="1px"
         borderColor="gray.200"
-        borderRadius={6}
-        overflow="visible"
-        mt={6}
+        borderTopRadius={6}
       >
-        <Box
-          backgroundColor="gray.50"
-          px={6}
-          py={4}
-          display="flex"
-          flexDirection="row"
-          alignItems="center"
-          borderBottom="1px"
-          borderColor="gray.200"
-          borderTopRadius={6}
-        >
-          <Heading as="h3" size="xs">
-            {heading}
-          </Heading>
-          {HeadingButton ? (
-            <>
-              <Spacer />
-              {/* @ts-ignore */}
-              <HeadingButton />
-            </>
-          ) : null}
-        </Box>
-
-        <Stack spacing={4} px={6} py={6}>
-          {children}
-        </Stack>
+        <Heading as="h3" size="xs">
+          {heading}
+        </Heading>
+        {HeadingButton ? (
+          <>
+            <Spacer />
+            {/* @ts-ignore */}
+            <HeadingButton />
+          </>
+        ) : null}
       </Box>
-    </Stack>
-  );
+
+      <Stack spacing={4} px={6} py={6}>
+        {children}
+      </Stack>
+    </Box>
+  </Stack>
+);
 
 export default SystemFormInputGroup;
