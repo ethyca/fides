70b1901dc65a3636e44c0b32e275a97478b04677
Thomas <ThomasLaPiana@users.noreply.github.com>
2023-08-22 03:35:10 +0800

    feat: add discrete endpoints for worker/database healthchecks with more verbosity

--- src/fides/api/api/v1/endpoints/health.py
+++ src/fides/api/api/v1/endpoints/health.py
@@ -1,31 +1,46 @@
 from typing import Dict, List, Literal, Optional
 
-from fastapi import HTTPException, status
+from fastapi import HTTPException, status, Depends
 from loguru import logger
 from pydantic import BaseModel
 from redis.exceptions import ResponseError
+from sqlalchemy.orm import Session
+
 
 import fides
+from fides.api.api.deps import get_db
 from fides.api.common_exceptions import RedisConnectionError
 from fides.api.db.database import DatabaseHealth, get_db_health
+from fides.api.tasks import celery_app, get_worker_ids
 from fides.api.util.api_router import APIRouter
 from fides.api.util.cache import get_cache
 from fides.api.util.logger import Pii
 from fides.config import CONFIG
+
 
 CacheHealth = Literal["healthy", "unhealthy", "no cache configured"]
 HEALTH_ROUTER = APIRouter(tags=["Health"])
 
 
 class CoreHealthCheck(BaseModel):
-    """Healthcheck schema"""
+    """Server Healthcheck schema"""
 
     webserver: str
     version: str
-    database: DatabaseHealth
     cache: CacheHealth
+
+
+class DatabaseHealthCheck(BaseModel):
+    """Database Healthcheck Schema"""
+
+    database: str
+
+
+class WorkerHealthCheck(BaseModel):
+    """Worker Healthcheck Schema"""
+
     workers_enabled: bool
-    workers: List[Optional[str]]
+    workers: List[str]
 
 
 def get_cache_health() -> str:
@@ -39,21 +54,57 @@ def get_cache_health() -> str:
     except (RedisConnectionError, ResponseError) as e:
         logger.error("Unable to reach cache: {}", Pii(str(e)))
         return "unhealthy"
-        
-# add descrete endpoints for Database health and worker health
+
 
 @HEALTH_ROUTER.get(
-    "/health",
-    response_model=CoreHealthCheck,
+    "/health/database",
+    response_model=DatabaseHealthCheck,
     responses={
         status.HTTP_200_OK: {
             "content": {
                 "application/json": {
                     "example": {
-                        "webserver": "healthy",
-                        "version": "1.0.0",
                         "database": "healthy",
-                        "cache": "healthy",
+                    }
+                }
+            }
+        },
+        status.HTTP_503_SERVICE_UNAVAILABLE: {
+            "content": {
+                "application/json": {
+                    "example": {
+                        "detail": {
+                            "database": "unhealthy",
+                        }
+                    }
+                }
+            }
+        },
+    },
+)
+async def database_health(db: Session = Depends(get_db)) -> Dict:
+    """Confirm that the API is running and healthy."""
+    health = get_db_health(CONFIG.database.sync_database_uri, db=db)
+    response = DatabaseHealthCheck(
+        database=health,
+    ).dict()
+
+    if database_health != "healthy":
+        raise HTTPException(
+            status_code=status.HTTP_503_SERVICE_UNAVAILABLE, detail=response
+        )
+
+    return response
+
+
+@HEALTH_ROUTER.get(
+    "/health/workers",
+    response_model=WorkerHealthCheck,
+    responses={
+        status.HTTP_200_OK: {
+            "content": {
+                "application/json": {
+                    "example": {
                         "workers_enabled": "True",
                         "workers": ["celery@c606808353b5"],
                     }
@@ -65,10 +116,6 @@ def get_cache_health() -> str:
                 "application/json": {
                     "example": {
                         "detail": {
-                            "webserver": "healthy",
-                            "version": "1.0.0",
-                            "database": "unhealthy",
-                            "cache": "healthy",
                             "workers_enabled": "True",
                             "workers": ["celery@c606808353b5"],
                         }
@@ -78,17 +125,59 @@ def get_cache_health() -> str:
         },
     },
 )
+async def workers_health() -> Dict:
+    """Confirm that the API is running and healthy."""
+    response = WorkerHealthCheck(
+        workers_enabled=False,
+        workers=[],
+    ).dict()
+
+    fides_is_using_workers = not celery_app.conf["task_always_eager"]
+    if fides_is_using_workers:
+        response["workers_enabled"] = True
+        # Figure out a way to make this faster
+        response["workers"] = get_worker_ids()
+
+    return response
+
+
+@HEALTH_ROUTER.get(
+    "/health",
+    response_model=CoreHealthCheck,
+    responses={
+        status.HTTP_200_OK: {
+            "content": {
+                "application/json": {
+                    "example": {
+                        "webserver": "healthy",
+                        "version": "1.0.0",
+                        "cache": "healthy",
+                    }
+                }
+            }
+        },
+        status.HTTP_503_SERVICE_UNAVAILABLE: {
+            "content": {
+                "application/json": {
+                    "example": {
+                        "detail": {
+                            "webserver": "healthy",
+                            "version": "1.0.0",
+                            "cache": "unhealthy",
+                        }
+                    }
+                }
+            }
+        },
+    },
+)
 async def health() -> Dict:
     """Confirm that the API is running and healthy."""
-    database_health = get_db_health(CONFIG.database.sync_database_uri)
     cache_health = get_cache_health()
     response = CoreHealthCheck(
         webserver="healthy",
         version=str(fides.__version__),
-        database=database_health,
         cache=cache_health,
-        workers_enabled=False,
-        workers=[],
     ).dict()
 
     for _, value in response.items():
