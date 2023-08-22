f6749599a4cef77b2958893ffc2a7cd7e2fd2179
Adrian Galvan <adrian@ethyca.com>
2023-08-22 14:44:36 -0700

    Removing invalid characters from system name

--- clients/admin-ui/src/features/datastore-connections/system_portal_config/helpers.ts
+++ clients/admin-ui/src/features/datastore-connections/system_portal_config/helpers.ts
@@ -4,7 +4,10 @@
  * @returns
  */
 export const formatKey = (key: string): string =>
-  key.toLowerCase().replace(/ /g, "_");
+  key
+    .toLowerCase()
+    .replace(/ /g, "_")
+    .replace(/[^a-zA-Z0-9._<>-]/g, "");
 
 /**
  * Modify the current history entry, replacing it with the new URL
