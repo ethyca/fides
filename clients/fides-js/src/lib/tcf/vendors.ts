dc11477049a437a7aecc28b188787b37c07863f4
Allison King <allisonjuliaking@gmail.com>
2023-08-23 09:04:52 -0700

    Add has_vendor_id check

--- clients/fides-js/src/lib/tcf/vendors.ts
+++ clients/fides-js/src/lib/tcf/vendors.ts
@@ -4,4 +4,4 @@ import GVL_JSON from "./gvl.json";
 const GVL_VENDOR_IDS = Object.keys(GVL_JSON.vendors);
 
 export const vendorIsGvl = (vendor: TCFVendorRecord) =>
-  GVL_VENDOR_IDS.indexOf(vendor.id) !== -1;
+  vendor.has_vendor_id && GVL_VENDOR_IDS.indexOf(vendor.id) !== -1;
