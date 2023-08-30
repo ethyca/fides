d98f8e0288bbd84b6f89700be959391426bf04f8
Thomas <thomas.lapiana+github@pm.me>
2023-08-30 15:05:59 +0800

    Add profiling/benchmarks to CI suite

--- noxfiles/ci_nox.py
+++ noxfiles/ci_nox.py
@@ -16,6 +16,7 @@ from constants_nox import (
 from setup_tests_nox import pytest_ctl, pytest_lib, pytest_nox, pytest_ops
 from utils_nox import install_requirements
 
+# Add a Nox command for resource usage
 
 ###################
 ## Static Checks ##
