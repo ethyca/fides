795d16a593fea588ffbb3d27cc91567dcb862a3a
TheAndrewJackson <andrew.c.j1995@gmail.com>
2023-09-05 11:11:11 -0400

    update to mutable jsonb

--- src/fides/api/models/fides_cloud.py
+++ src/fides/api/models/fides_cloud.py
@@ -1,5 +1,6 @@
 from sqlalchemy import Column
 from sqlalchemy.dialects.postgresql import JSONB
+from sqlalchemy.ext.mutable import MutableDict
 
 from fides.api.db.base_class import Base
 
@@ -7,4 +8,4 @@ from fides.api.db.base_class import Base
 class FidesCloud(Base):
     """Stores all Fides Cloud related config variables"""
 
-    config = Column(JSONB, nullable=True)
+    config = Column(MutableDict.as_mutable(JSONB), nullable=True)
