e6a84d75ff11e6af02f4a8c135c89ef39edd1460
Dawn Pattison <pattisdr@users.noreply.github.com>
2023-09-08 16:21:03 -0500

    PROD-1060 - add a commit to link the branch while we add some type hinting.

--- src/fides/api/api/v1/endpoints/privacy_experience_endpoints.py
+++ src/fides/api/api/v1/endpoints/privacy_experience_endpoints.py
@@ -70,12 +70,16 @@ def _filter_experiences_by_region_or_country(
     region: str = escape(region).replace("-", "_").lower()
     country: str = region.split("_")[0]
 
-    overlay = PrivacyExperience.get_experience_by_region_and_component(
+    overlay: Optional[
+        PrivacyExperience
+    ] = PrivacyExperience.get_experience_by_region_and_component(
         db, region, ComponentType.overlay
     ) or PrivacyExperience.get_experience_by_region_and_component(
         db, country, ComponentType.overlay
     )
-    privacy_center = PrivacyExperience.get_experience_by_region_and_component(
+    privacy_center: Optional[
+        PrivacyExperience
+    ] = PrivacyExperience.get_experience_by_region_and_component(
         db, region, ComponentType.privacy_center
     ) or PrivacyExperience.get_experience_by_region_and_component(
         db, country, ComponentType.privacy_center
