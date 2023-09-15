e401afcd8e6dffe18a0b08be369786ba91c64b89
Adrian Galvan <adrian@ethyca.com>
2023-09-15 14:23:13 -0700

    Updating import

--- clients/admin-ui/src/pages/dataset/index.tsx
+++ clients/admin-ui/src/pages/dataset/index.tsx
@@ -11,7 +11,7 @@ import NextLink from "next/link";
 
 import Layout from "~/features/common/Layout";
 import { useGetAllFilteredDatasetsQuery } from "~/features/dataset/dataset.slice";
-import DatasetsTable from "~/features/dataset/DatasetTable";
+import DatasetTable from "~/features/dataset/DatasetTable";
 
 const DataSets: NextPage = () => {
   const { isLoading } = useGetAllFilteredDatasetsQuery({
@@ -33,7 +33,7 @@ const DataSets: NextPage = () => {
           </BreadcrumbItem>
         </Breadcrumb>
       </Box>
-      <Box mb={4}>{isLoading ? <Spinner /> : <DatasetsTable />}</Box>
+      <Box mb={4}>{isLoading ? <Spinner /> : <DatasetTable />}</Box>
       <Box>
         <Button
           size="sm"
