9974c8bbb57927d4b063d00ac3db226521988c52
Kelsey Thomas <101993653+Kelsey-Ethyca@users.noreply.github.com>
2023-09-21 17:43:48 -0700

    change null to undefined

--- clients/admin-ui/src/features/taxonomy/hooks.tsx
+++ clients/admin-ui/src/features/taxonomy/hooks.tsx
@@ -78,9 +78,9 @@ const transformTaxonomyBaseToInitialValues = (
   description: t.description ?? "",
   parent_key: t.parent_key ?? "",
   is_default: t.is_default ?? false,
-  version_added: t.version_added ?? null,
-  version_deprecated: t.version_deprecated ?? null,
-  replaced_by: t.replaced_by ?? null,
+  version_added: t.version_added ?? undefined,
+  version_deprecated: t.version_deprecated ?? undefined,
+  replaced_by: t.replaced_by ?? undefined,
   customFieldValues,
 });
 
