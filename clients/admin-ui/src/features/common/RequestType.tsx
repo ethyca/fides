92e0580d26079d33b0ee9c5ee1b6f21f3d050389
Adrian Galvan <adrian@ethyca.com>
2023-09-26 13:33:35 -0700

    Pause manual erasure requests (#4115)

--- clients/admin-ui/src/features/common/RequestType.tsx
+++ clients/admin-ui/src/features/common/RequestType.tsx
@@ -8,25 +8,33 @@ type RequestTypeProps = {
   rules: Rule[];
 };
 
-const RequestType = ({ rules }: RequestTypeProps) => {
-  const actions = Array.from(
+/**
+ * Extracts and returns the unique action types from the rules.
+ * @param rules Array of Rule objects.
+ */
+export const getActionTypes = (rules: Rule[]): ActionType[] =>
+  Array.from(
     new Set(
       rules
         .filter((d) => Object.values(ActionType).includes(d.action_type))
-        .map((d) => capitalize(d.action_type))
+        .map((d) => d.action_type)
     )
   );
-  const tags = actions.map((action_type) => (
-    <Tag
-      key={action_type}
-      color="white"
-      bg="primary.400"
-      fontWeight="medium"
-      fontSize="sm"
-    >
-      {action_type}
-    </Tag>
-  ));
+
+const RequestType = ({ rules }: RequestTypeProps) => {
+  const tags = getActionTypes(rules)
+    .map((action) => capitalize(action))
+    .map((action_type) => (
+      <Tag
+        key={action_type}
+        color="white"
+        bg="primary.400"
+        fontWeight="medium"
+        fontSize="sm"
+      >
+        {action_type}
+      </Tag>
+    ));
 
   return <Box>{tags}</Box>;
 };
