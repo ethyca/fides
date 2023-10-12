23167474dcdb8618d28b2471f66ed1505bf8fbbc
jpople <jeremy@ethyca.com>
2023-10-12 10:50:16 -0400

    Consent use dropdown (#4251)

--- clients/admin-ui/src/features/configure-consent/constants.ts
+++ clients/admin-ui/src/features/configure-consent/constants.ts
@@ -2,6 +2,7 @@ import { Cookies, PrivacyDeclaration } from "~/types/api";
 
 export interface MinimalPrivacyDeclaration {
   name: string;
+  consent_use?: string;
   data_use: PrivacyDeclaration["data_use"];
   data_categories: PrivacyDeclaration["data_categories"];
   /**
@@ -22,9 +23,36 @@ export interface FormValues {
 
 export const EMPTY_DECLARATION: MinimalPrivacyDeclaration = {
   name: "",
+  consent_use: "",
   data_use: "",
   // TODO(fides#4059): data categories will eventually be optional
   data_categories: ["user"],
   cookieNames: [],
   cookies: [],
 };
+
+export const CONSENT_USE_OPTIONS = [
+  {
+    label: "Analytics",
+    value: "analytics",
+    description:
+      "Provides analytics for activities such as system and advertising performance reporting, insights and fraud detection.",
+  },
+  {
+    label: "Essential",
+    value: "essential",
+    description:
+      "Operates the service or product, including legal obligations, support and basic system operations.",
+  },
+  {
+    label: "Functional",
+    value: "functional",
+    description: "Used for specific, necessary, and legitimate purposes.",
+  },
+  {
+    label: "Marketing",
+    value: "marketing",
+    description:
+      "Enables marketing, promotion, advertising and sales activities for the product, service, application or system.",
+  },
+];
