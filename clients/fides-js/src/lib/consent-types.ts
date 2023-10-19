84712885ba2887d392d415536ade7c571a78015c
Catherine Smith <eastandwestwind@gmail.com>
2023-10-19 17:40:13 +0200

    PROD-1125 1118 config overrides query cookie window (#4297)

--- clients/fides-js/src/lib/consent-types.ts
+++ clients/fides-js/src/lib/consent-types.ts
@@ -201,11 +201,16 @@ export type UserGeolocation = {
   region?: string; // "NY"
 };
 
-// Regex to validate a location string, which must:
-// 1) Start with a 2-3 character country code (e.g. "US")
-// 2) Optionally end with a 2-3 character region code (e.g. "CA")
-// 3) Separated by a dash (e.g. "US-CA")
-export const VALID_ISO_3166_LOCATION_REGEX = /^\w{2,3}(-\w{2,3})?$/;
+export type OverrideOptions = {
+  fides_string: string;
+  fides_disable_save_api: boolean;
+  fides_embed: boolean;
+};
+
+export type FidesOptionOverrides = Pick<
+  FidesOptions,
+  "fidesString" | "fidesDisableSaveApi" | "fidesEmbed"
+>;
 
 export enum ButtonType {
   PRIMARY = "primary",
