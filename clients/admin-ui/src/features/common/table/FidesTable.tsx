492910f487fb8880dfe7c3803114d04627851c87
TheAndrewJackson <andrew.c.j1995@gmail.com>
2023-10-20 09:34:54 -0400

    Revert fides table v1 to original state

--- clients/admin-ui/src/features/common/table/FidesTable.tsx
+++ clients/admin-ui/src/features/common/table/FidesTable.tsx
@@ -1,7 +1,6 @@
 import {
   ArrowDownIcon,
   ArrowUpIcon,
-  Box,
   Flex,
   Table,
   TableContainer,
@@ -12,12 +11,10 @@ import {
   Thead,
   Tr,
 } from "@fidesui/react";
-import React, { MutableRefObject, ReactNode, useEffect, useMemo } from "react";
+import React, { ReactNode, useMemo } from "react";
 import {
   Column,
   Hooks,
-  TableInstance,
-  TableState,
   useGlobalFilter,
   useSortBy,
   useTable,
@@ -35,12 +32,9 @@ type Props<T extends FidesObject> = {
   data: T[];
   showSearchBar?: boolean;
   searchBarPlaceHolder?: string;
-  searchBarRightButton?: ReactNode;
   footer?: ReactNode;
   onRowClick?: (row: T) => void;
   customHooks?: Array<(hooks: Hooks<T>) => void>;
-  tableInstanceRef?: MutableRefObject<TableInstance<T> | undefined>;
-  initialState?: Partial<TableState<T>>;
 };
 
 export const FidesTable = <T extends FidesObject>({
@@ -48,12 +42,9 @@ export const FidesTable = <T extends FidesObject>({
   data,
   showSearchBar,
   searchBarPlaceHolder,
-  searchBarRightButton,
   footer,
   onRowClick,
   customHooks,
-  tableInstanceRef,
-  initialState,
 }: Props<T>) => {
   const plugins = useMemo(() => {
     if (customHooks) {
@@ -63,133 +54,118 @@ export const FidesTable = <T extends FidesObject>({
   }, [customHooks]);
 
   const tableInstance = useTable(
-    {
-      columns,
-      data,
-      autoResetSortBy: false,
-      initialState: initialState !== undefined ? initialState : {},
-    },
+    { columns, data, autoResetSortBy: false },
     ...plugins
   );
-  useEffect(() => {
-    if (tableInstanceRef) {
-      /* eslint-disable no-param-reassign */
-      tableInstanceRef.current = tableInstance;
-    }
-  }, [tableInstance, tableInstanceRef]);
 
   const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } =
     tableInstance;
 
   return (
-    <Box height="inherit">
+    <TableContainer>
       {showSearchBar ? (
-        <Flex flexGrow={1} marginBottom={3} justifyContent="space-between">
+        <Flex flexGrow={1} marginBottom={3}>
           <GlobalFilter
             globalFilter={tableInstance.state.globalFilter}
             setGlobalFilter={tableInstance.setGlobalFilter}
             placeholder={searchBarPlaceHolder}
           />
-          {searchBarRightButton || null}
         </Flex>
       ) : null}
-      <TableContainer
-        height="inherit"
-        overflowY="auto"
-        border="1px solid"
-        boxSizing="border-box"
-        borderColor="gray.200"
+      <Table
+        {...getTableProps()}
         borderRadius="6px 6px 0px 0px"
+        border="1px solid"
+        borderColor="gray.200"
+        fontSize="sm"
       >
-        <Table {...getTableProps()} fontSize="sm">
-          <Thead position="sticky" top="0" backgroundColor="gray.50">
-            {headerGroups.map((headerGroup) => {
-              const { key: headerRowKey, ...headerGroupProps } =
-                headerGroup.getHeaderGroupProps();
-              return (
-                <Tr key={headerRowKey} {...headerGroupProps}>
-                  {headerGroup.headers.map((column) => {
-                    const { key: columnKey, ...headerProps } =
-                      column.getHeaderProps(column.getSortByToggleProps());
-                    let sortIcon: ReactNode = null;
-                    if (column.isSorted) {
-                      sortIcon = column.isSortedDesc ? (
-                        <ArrowDownIcon color="gray.500" />
-                      ) : (
-                        <ArrowUpIcon color="gray.500" />
-                      );
-                    }
-
-                    return (
-                      <Th
-                        key={columnKey}
-                        {...headerProps}
-                        textTransform="none"
-                        fontSize="sm"
-                        p={4}
-                        data-testid={`column-${column.Header}`}
-                      >
-                        <Text
-                          _hover={{ backgroundColor: "gray.100" }}
-                          p={1}
-                          borderRadius="4px"
-                          pr={sortIcon ? 0 : 3.5}
-                        >
-                          {column.render("Header")}
-                          {sortIcon}
-                        </Text>
-                      </Th>
+        <Thead backgroundColor="gray.50">
+          {headerGroups.map((headerGroup) => {
+            const { key: headerRowKey, ...headerGroupProps } =
+              headerGroup.getHeaderGroupProps();
+            return (
+              <Tr key={headerRowKey} {...headerGroupProps}>
+                {headerGroup.headers.map((column) => {
+                  const { key: columnKey, ...headerProps } =
+                    column.getHeaderProps(column.getSortByToggleProps());
+                  let sortIcon: ReactNode = null;
+                  if (column.isSorted) {
+                    sortIcon = column.isSortedDesc ? (
+                      <ArrowDownIcon color="gray.500" />
+                    ) : (
+                      <ArrowUpIcon color="gray.500" />
                     );
-                  })}
-                </Tr>
-              );
-            })}
-          </Thead>
-          <Tbody {...getTableBodyProps()}>
-            {rows.map((row) => {
-              prepareRow(row);
-              const { key: rowKey, ...rowProps } = row.getRowProps();
-              const rowName = row.original.name;
-              const rowId = row.original.id;
-              return (
-                <Tr
-                  key={rowKey}
-                  {...rowProps}
-                  _hover={
-                    onRowClick
-                      ? { backgroundColor: "gray.50", cursor: "pointer" }
-                      : undefined
                   }
-                  data-testid={`row-${rowName ?? rowId}`}
-                >
-                  {row.cells.map((cell) => {
-                    const { key: cellKey, ...cellProps } = cell.getCellProps();
-                    return (
-                      <Td
-                        key={cellKey}
-                        {...cellProps}
-                        p={5}
-                        verticalAlign="baseline"
-                        width={cell.column.width}
-                        onClick={
-                          cell.column.Header !== "Enable" && onRowClick
-                            ? () => {
-                                onRowClick(row.original);
-                              }
-                            : undefined
-                        }
+
+                  return (
+                    <Th
+                      key={columnKey}
+                      {...headerProps}
+                      textTransform="none"
+                      fontSize="sm"
+                      p={4}
+                      data-testid={`column-${column.Header}`}
+                    >
+                      <Text
+                        _hover={{ backgroundColor: "gray.100" }}
+                        p={1}
+                        borderRadius="4px"
+                        pr={sortIcon ? 0 : 3.5}
                       >
-                        {cell.render("Cell")}
-                      </Td>
-                    );
-                  })}
-                </Tr>
-              );
-            })}
-          </Tbody>
-          {footer}
-        </Table>
-      </TableContainer>
-    </Box>
+                        {column.render("Header")}
+                        {sortIcon}
+                      </Text>
+                    </Th>
+                  );
+                })}
+              </Tr>
+            );
+          })}
+        </Thead>
+        <Tbody {...getTableBodyProps()}>
+          {rows.map((row) => {
+            prepareRow(row);
+            const { key: rowKey, ...rowProps } = row.getRowProps();
+            const rowName = row.original.name;
+            const rowId = row.original.id;
+            return (
+              <Tr
+                key={rowKey}
+                {...rowProps}
+                _hover={
+                  onRowClick
+                    ? { backgroundColor: "gray.50", cursor: "pointer" }
+                    : undefined
+                }
+                data-testid={`row-${rowName ?? rowId}`}
+              >
+                {row.cells.map((cell) => {
+                  const { key: cellKey, ...cellProps } = cell.getCellProps();
+                  return (
+                    <Td
+                      key={cellKey}
+                      {...cellProps}
+                      p={5}
+                      verticalAlign="baseline"
+                      width={cell.column.width}
+                      onClick={
+                        cell.column.Header !== "Enable" && onRowClick
+                          ? () => {
+                              onRowClick(row.original);
+                            }
+                          : undefined
+                      }
+                    >
+                      {cell.render("Cell")}
+                    </Td>
+                  );
+                })}
+              </Tr>
+            );
+          })}
+        </Tbody>
+        {footer}
+      </Table>
+    </TableContainer>
   );
 };
