2579adbf28879cf0003c5f0d3ab2971df25034a6
Jeremy Pople <jeremy@ethyca.com>
2023-10-23 20:34:51 -0400

    run prettier

--- clients/admin-ui/src/pages/GVLNotice.tsx
+++ clients/admin-ui/src/pages/GVLNotice.tsx
@@ -3,24 +3,24 @@ import { Box, Link, Text } from "@fidesui/react";
 import EmptyTableState from "~/features/common/table/EmptyTableState";
 
 const GVLNotice = () => (
-    <Box mb="6" maxW="720px">
-      <EmptyTableState
-        title="This system is part of the TCF Global Vendor Listing"
-        description={
-          <Text>
-            Some form elements below will be disabled as they cannot be edited
-            if they are populated directly from the Global Vendor List.{" "}
-            <Link
-              href="https://fid.es/tcf_gvl"
-              isExternal
-              color="complimentary.500"
-            >
-              For more information on the Global Vendor List, click here.
-            </Link>
-          </Text>
-        }
-      />
-    </Box>
-  );
+  <Box mb="6" maxW="720px">
+    <EmptyTableState
+      title="This system is part of the TCF Global Vendor Listing"
+      description={
+        <Text>
+          Some form elements below will be disabled as they cannot be edited if
+          they are populated directly from the Global Vendor List.{" "}
+          <Link
+            href="https://fid.es/tcf_gvl"
+            isExternal
+            color="complimentary.500"
+          >
+            For more information on the Global Vendor List, click here.
+          </Link>
+        </Text>
+      }
+    />
+  </Box>
+);
 
 export default GVLNotice;
