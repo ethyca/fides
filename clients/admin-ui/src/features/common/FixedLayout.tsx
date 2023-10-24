5486feed12393f59cfbcf7ca469d565aa2582bb2
TheAndrewJackson <andrew.c.j1995@gmail.com>
2023-10-24 08:25:33 -0400

    format file

--- clients/admin-ui/src/features/common/FixedLayout.tsx
+++ clients/admin-ui/src/features/common/FixedLayout.tsx
@@ -15,31 +15,31 @@ const FixedLayout = ({
   title: string;
   mainProps?: FlexProps;
 }) => (
-    // NOTE: unlike the main Layout, this layout specifies a fixed height
-    // of 100vh for the page, and overflow="auto" for the main content area. This
-    // allows the content area to fitting the *viewport* height, which is a
-    // special case layout needed by the datamap spatial & table views so that we
-    // scroll within those elements instead of scrolling the overall page itself
-    //
-    // Ideally we could have one common layout used by *all* pages, but we couldn't
-    // come up with a common method here without introducing other gotchas, so having
-    // a slightly different layout was decided as the most maintainable option
-    <Flex data-testid={title} direction="column" height="100vh">
-      <Head>
-        <title>Fides Admin UI - {title}</title>
-        <meta name="description" content="Privacy Engineering Platform" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <Header />
-      <NavTopBar />
-      <Flex as="main" overflow="auto" flexGrow={1} gap={10} {...mainProps}>
-        <Box flex={0} flexShrink={0}>
-          <NavSideBar />
-        </Box>
-        <Flex direction="column" flex={1} minWidth={0}>
-          {children}
-        </Flex>
+  // NOTE: unlike the main Layout, this layout specifies a fixed height
+  // of 100vh for the page, and overflow="auto" for the main content area. This
+  // allows the content area to fitting the *viewport* height, which is a
+  // special case layout needed by the datamap spatial & table views so that we
+  // scroll within those elements instead of scrolling the overall page itself
+  //
+  // Ideally we could have one common layout used by *all* pages, but we couldn't
+  // come up with a common method here without introducing other gotchas, so having
+  // a slightly different layout was decided as the most maintainable option
+  <Flex data-testid={title} direction="column" height="100vh">
+    <Head>
+      <title>Fides Admin UI - {title}</title>
+      <meta name="description" content="Privacy Engineering Platform" />
+      <link rel="icon" href="/favicon.ico" />
+    </Head>
+    <Header />
+    <NavTopBar />
+    <Flex as="main" overflow="auto" flexGrow={1} gap={10} {...mainProps}>
+      <Box flex={0} flexShrink={0}>
+        <NavSideBar />
+      </Box>
+      <Flex direction="column" flex={1} minWidth={0}>
+        {children}
       </Flex>
     </Flex>
-  );
+  </Flex>
+);
 export default FixedLayout;
