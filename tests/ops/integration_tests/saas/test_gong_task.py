29da11625c0b1821bbfb91123243a6e33981e6f4
Marc Guimond <MarcGEthyca@users.noreply.github.com>
2023-11-06 12:20:43 -0500

    Tetsing

--- tests/ops/integration_tests/saas/test_gong_task.py
+++ tests/ops/integration_tests/saas/test_gong_task.py
@@ -15,7 +15,7 @@ class TestGongConnector:
         access_results = await gong_runner.access_request(
             access_policy=policy, identities={"email": gong_identity_email}
         )
-        assert access_results["gong_instance:user"][0] == gong_identity_email
+        assert access_results == {"gong_instance:user": 1}
 
     async def test_strict_erasure_request(
         self,
