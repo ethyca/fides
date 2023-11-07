eef19099c4dab15a81e3d4d69c6ca12ead31ba60
Dawn Pattison <pattisdr@users.noreply.github.com>
2023-11-07 12:12:05 -0600

    Split cookie name system id constraint migration into a separate file.

--- src/fides/api/alembic/migrations/versions/842790fa918a_add_compass_system_sync.py
+++ src/fides/api/alembic/migrations/versions/842790fa918a_add_compass_system_sync.py
@@ -7,7 +7,6 @@ Create Date: 2023-11-07 16:22:01.746436
 """
 import sqlalchemy as sa
 from alembic import op
-from sqlalchemy import text
 from sqlalchemy.dialects import postgresql
 
 # revision identifiers, used by Alembic.
@@ -18,8 +17,6 @@ depends_on = None
 
 
 def upgrade():
-    bind = op.get_bind()
-
     op.create_table(
         "system_compass_sync",
         sa.Column("id", sa.String(length=255), nullable=False),
@@ -44,22 +41,7 @@ def upgrade():
         op.f("ix_system_compass_sync_id"), "system_compass_sync", ["id"], unique=False
     )
 
-    # Remove duplicate cookies on system id and system name.
-    bind.execute(
-        text(
-            "DELETE FROM cookies A USING cookies B "
-            "WHERE A.id < B.id AND A.name = B.name "
-            "AND A.system_id = B.system_id "
-            "AND A.system_id IS NOT NULL"
-        )
-    )
-
-    op.create_unique_constraint(
-        "_cookie_name_system_uc", "cookies", ["name", "system_id"]
-    )
-
 
 def downgrade():
-    op.drop_constraint("_cookie_name_system_uc", "cookies", type_="unique")
     op.drop_index(op.f("ix_system_compass_sync_id"), table_name="system_compass_sync")
     op.drop_table("system_compass_sync")
