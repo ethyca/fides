c92520b76a67e7c79ebc73141174f3d672e2ae13
Allison King <allisonjuliaking@gmail.com>
2023-11-14 13:48:03 -0500

    Move files around

--- clients/fides-js/src/lib/gpp.ts
+++
@@ -1,7 +0,0 @@
-import { makeStub } from "./gpp/stub";
-
-export const initializeGppCmpApi = () => {
-  makeStub();
-
-  // TODO: instantiate a real (non-stubbed) GPP CMP API and set up listeners
-};
