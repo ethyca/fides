c1463dcf31dfb3128899b02c0e50d264680e6555
Allison King <allisonjuliaking@gmail.com>
2023-11-15 17:38:33 -0500

    Set up GPP listeners from FidesUI events instead of TCF

--- clients/fides-js/src/extensions/gpp.ts
+++ clients/fides-js/src/extensions/gpp.ts
@@ -13,10 +13,10 @@ import {
   SignalStatus,
 } from "@iabgpp/cmpapi";
 import { makeStub } from "../lib/gpp/stub";
+import { fidesEventToTcString } from "../lib/tcf/events";
 
 const CMP_ID = 407; // TODO: is this supposed to be the same as TCF, or is this separate?
 const CMP_VERSION = 1;
-const TCF_VERSION = 2;
 
 const TCF_SECTION_ID = 2;
 
@@ -26,26 +26,31 @@ export const initializeGppCmpApi = () => {
   const cmpApi = new CmpApi(CMP_ID, CMP_VERSION);
   cmpApi.setApplicableSections([TCF_SECTION_ID]);
   cmpApi.setCmpStatus(CmpStatus.LOADED);
-  cmpApi.setSignalStatus(SignalStatus.READY);
 
-  if (window.__tcfapi) {
-    window.__tcfapi("addEventListener", TCF_VERSION, (tcData, success) => {
-      if (!success) {
-        return;
-      }
-      if (tcData.eventStatus === "cmpuishown") {
-        cmpApi.setCmpDisplayStatus(CmpDisplayStatus.VISIBLE);
-      }
-      if (tcData.eventStatus === "useractioncomplete") {
-        cmpApi.setCmpDisplayStatus(CmpDisplayStatus.HIDDEN);
-      }
-      // Workaround for bug in base library https://github.com/IABTechLab/iabgpp-es/issues/35
-      cmpApi.setFieldValueBySectionId(TCF_SECTION_ID, "CmpId", CMP_ID);
-      const { tcString } = tcData;
-      cmpApi.setSectionStringById(TCF_SECTION_ID, tcString);
-      cmpApi.fireSectionChange("tcfeuv2");
-    });
-  }
+  window.addEventListener("FidesUIInitialized", () => {
+    // TODO: if we don't need to resurface consent, we can set
+    // cmpApi.setSignalStatus(SignalStatus.READY);
+    // But we don't have easy access to tell if we should resurface consent here yet
+    // (we need the experience too)
+  });
+
+  window.addEventListener("FidesUIShown", () => {
+    cmpApi.setSignalStatus(SignalStatus.NOT_READY);
+    cmpApi.setCmpDisplayStatus(CmpDisplayStatus.VISIBLE);
+  });
+
+  window.addEventListener("FidesModalClosed", () => {
+    cmpApi.setCmpDisplayStatus(CmpDisplayStatus.HIDDEN);
+  });
+
+  window.addEventListener("FidesUpdated", (event) => {
+    const tcString = fidesEventToTcString(event);
+    // Workaround for bug in base library https://github.com/IABTechLab/iabgpp-es/issues/35
+    cmpApi.setFieldValueBySectionId(TCF_SECTION_ID, "CmpId", CMP_ID);
+    cmpApi.setSectionStringById(TCF_SECTION_ID, tcString ?? "");
+    cmpApi.fireSectionChange("tcfeuv2");
+    cmpApi.setSignalStatus(SignalStatus.READY);
+  });
 };
 
 initializeGppCmpApi();
--- clients/fides-js/src/lib/tcf.ts
+++ clients/fides-js/src/lib/tcf.ts
@@ -18,7 +18,7 @@ import {
 } from "./tcf/vendors";
 import { PrivacyExperience } from "./consent-types";
 import { FIDES_SEPARATOR } from "./tcf/constants";
-import { FidesEvent } from "./events";
+import { fidesEventToTcString } from "./tcf/events";
 
 // TCF
 const CMP_ID = 407;
@@ -147,25 +147,6 @@ export const generateFidesString = async ({
     return Promise.resolve("");
   }
   return Promise.resolve(encodedString);
-};
-
-/**
- * Extract just the TC string from a FidesEvent. This will also remove parts of the
- * TC string that we do not want to surface with our CMP API events, such as
- * `vendors_disclosed` and our own AC string addition.
- */
-const fidesEventToTcString = (event: FidesEvent) => {
-  const { fides_string: cookieString } = event.detail;
-  if (cookieString) {
-    // Remove the AC portion which is separated by FIDES_SEPARATOR
-    const [tcString] = cookieString.split(FIDES_SEPARATOR);
-    // We only want to return the first part of the tcString, which is separated by '.'
-    // This means Publisher TC is not sent either, which is okay for now since we do not set it.
-    // However, if we do one day set it, we would have to decode the string and encode it again
-    // without vendorsDisclosed
-    return tcString.split(".")[0];
-  }
-  return cookieString;
 };
 
 /**
