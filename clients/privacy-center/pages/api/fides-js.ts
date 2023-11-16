81b66d983b7f5c1b99a8a211ab894dedd84501f2
Allison King <allisonjuliaking@gmail.com>
2023-11-16 17:26:29 -0500

    Remove debug call from privacy center

--- clients/privacy-center/pages/api/fides-js.ts
+++ clients/privacy-center/pages/api/fides-js.ts
@@ -112,7 +112,6 @@ export default async function handler(
         fidesRegionString,
         environment.settings.SERVER_SIDE_FIDES_API_URL ||
           environment.settings.FIDES_API_URL,
-        environment.settings.DEBUG,
         null,
         null
       );
