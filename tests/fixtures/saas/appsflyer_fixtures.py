3ffa7e75a4aee96bf1d1300eaf1ead88927ed299
Marc Guimond <MarcGEthyca@users.noreply.github.com>
2023-11-27 14:11:29 -0500

    started

---
+++ tests/fixtures/saas/appsflyer_fixtures.py
@@ -0,0 +1,69 @@
+from typing import Any, Dict, Generator
+
+import pydash
+import pytest
+
+from tests.ops.integration_tests.saas.connector_runner import (
+    ConnectorRunner,
+    generate_random_email,
+)
+from tests.ops.test_helpers.vault_client import get_secrets
+
+secrets = get_secrets("appsflyer")
+
+
+@pytest.fixture(scope="session")
+def appsflyer_secrets(saas_config) -> Dict[str, Any]:
+    return {
+        "domain": pydash.get(saas_config, "appsflyer.domain")
+        or secrets["domain"]
+        # add the rest of your secrets here
+    }
+
+
+@pytest.fixture(scope="session")
+def appsflyer_identity_email(saas_config) -> str:
+    return (
+        pydash.get(saas_config, "appsflyer.identity_email") or secrets["identity_email"]
+    )
+
+
+@pytest.fixture
+def appsflyer_erasure_identity_email() -> str:
+    return generate_random_email()
+
+
+@pytest.fixture
+def appsflyer_external_references() -> Dict[str, Any]:
+    return {}
+
+
+@pytest.fixture
+def appsflyer_erasure_external_references() -> Dict[str, Any]:
+    return {}
+
+
+@pytest.fixture
+def appsflyer_erasure_data(
+    appsflyer_erasure_identity_email: str,
+) -> Generator:
+    # create the data needed for erasure tests here
+    yield {}
+
+
+@pytest.fixture
+def appsflyer_runner(
+    db,
+    cache,
+    appsflyer_secrets,
+    appsflyer_external_references,
+    appsflyer_erasure_external_references,
+) -> ConnectorRunner:
+    return ConnectorRunner(
+        db,
+        cache,
+        "appsflyer",
+        appsflyer_secrets,
+        external_references=appsflyer_external_references,
+        erasure_external_references=appsflyer_erasure_external_references,
+    )
