04521f426c93fb08d23dc683fa32dc403e75d403
Thomas <ThomasLaPiana@users.noreply.github.com>
2023-11-28 14:55:07 +0800

    fix json schema generation

--- src/fides/api/custom_types.py
+++ src/fides/api/custom_types.py
@@ -1,58 +1,43 @@
 """Logic related to sanitizing and validating user application input."""
 from html import escape
 from re import compile as regex
-from typing import Generator
+from typing import Annotated
+
+from pydantic import BeforeValidator
 
 
-class SafeStr(str):
-    """
-    This class is designed to be used in place of the `str` type
-    any place where user input is expected.
+def validate_safe_str(value: str) -> str:
+    # HTML Escapes
+    value = escape(value)
 
-    The validation applied here does the typical sanitization/cleanup
-    that is required when dealing with user-supplied input.
-    """
+    if len(value) > 32000:
+        raise ValueError("Value must be 32000 characters or less.")
 
-    @classmethod
-    def __get_validators__(cls) -> Generator:  # pragma: no cover
-        yield cls.validate
-
-    @classmethod
-    def validate(cls, value: str) -> str:
-        # HTML Escapes
-        value = escape(value)
-
-        if len(value) > 32000:
-            raise ValueError("Value must be 32000 characters or less.")
-
-        return value
+    return value
 
 
-class PhoneNumber(str):
-    """
-    Format validated type for phone numbers.
+"""
+This class is designed to be used in place of the `str` type
+any place where user input is expected.
 
-    Standard format can be found here: https://en.wikipedia.org/wiki/E.164
-    """
+The validation applied here does the typical sanitization/cleanup
+that is required when dealing with user-supplied input.
+"""
+SafeStr = Annotated[str, BeforeValidator(validate_safe_str)]
 
-    @classmethod
-    def __get_validators__(cls) -> Generator:
-        yield cls.validate
 
-    @classmethod
-    def validate(cls, value: str) -> str:
-        # The front-end sends an empty string if the user doesn't input anything
-        if value == "":
-            return ""
-        max_length = 16  # Includes the +
-        min_length = 9
-        pattern = regex(r"^\+[1-9]\d{1,14}$")
-        if (
-            len(value) > max_length
-            or len(value) < min_length
-            or not pattern.search(value)
-        ):
-            raise ValueError(
-                "Phone number must be formatted in E.164 format, i.e. '+15558675309'."
-            )
-        return value
+def validate_phone_number(value: str) -> str:
+    # The front-end sends an empty string if the user doesn't input anything
+    if value == "":
+        return ""
+    max_length = 16  # Includes the +
+    min_length = 9
+    pattern = regex(r"^\+[1-9]\d{1,14}$")
+    if len(value) > max_length or len(value) < min_length or not pattern.search(value):
+        raise ValueError(
+            "Phone number must be formatted in E.164 format, i.e. '+15558675309'."
+        )
+    return value
+
+
+PhoneNumber = Annotated[str, BeforeValidator(validate_phone_number)]
