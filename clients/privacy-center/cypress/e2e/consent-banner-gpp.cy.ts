c56e67ec5a418d52201e3211a3d5e0acf21dd8a8
Allison King <allisonjuliaking@gmail.com>
2023-11-30 17:07:17 -0500

    Add initial tests

--- clients/privacy-center/cypress/e2e/consent-banner-gpp.cy.ts
+++ clients/privacy-center/cypress/e2e/consent-banner-gpp.cy.ts
@@ -1,6 +1,7 @@
 /* eslint-disable no-underscore-dangle */
-import { FidesEndpointPaths } from "fides-js";
-import { API_URL } from "../support/constants";
+import { CONSENT_COOKIE_NAME, FidesEndpointPaths } from "fides-js";
+import { API_URL, TCF_VERSION_HASH } from "../support/constants";
+import { mockCookie } from "../support/mocks";
 import { stubConfig } from "../support/stubs";
 
 describe("Fides-js GPP extension", () => {
@@ -58,6 +59,119 @@ describe("Fides-js GPP extension", () => {
               expect(data.signalStatus).to.eql("not ready");
             });
         });
+      });
+    });
+
+    /**
+     * Follows the flow documented here for Event Order Example 1:
+     * https://github.com/InteractiveAdvertisingBureau/Global-Privacy-Platform/blob/main/Core/CMP%20API%20Specification.md#eventlistener-
+     * 1. initial data from stub
+     * 2. listenerRegistered
+     * 3. cmpStatus = loaded
+     * 4. cmpDisplayStatus = visible
+     * 5. User makes a choice
+     * 6. cmpDisplayStatus = hidden
+     * 7. sectionChange = tcfeuv2
+     * 8. signalStatus = ready
+     */
+    it("fires appropriate gpp events for first time user", () => {
+      cy.waitUntilFidesInitialized().then(() => {
+        cy.get("@FidesUIShown").should("have.been.calledOnce");
+        // TODO(PROD#1439): Because the stub is too late right now, we can't listen for events
+        // 3 and 4 yet.
+        cy.window().then((win) => {
+          win.__gpp("addEventListener", cy.stub().as("gppListener"));
+        });
+
+        cy.get("@gppListener")
+          .should("have.been.calledOnce")
+          .its("lastCall.args")
+          .then(([data, success]) => {
+            expect(success).to.eql(true);
+            expect(data.eventName).to.eql("listenerRegistered");
+            const { cmpDisplayStatus, signalStatus, gppString } = data.pingData;
+            expect(cmpDisplayStatus).to.eql("visible");
+            expect(signalStatus).to.eql("not ready");
+            expect(gppString).to.eql("DBAA"); // empty string, header only
+          });
+
+        cy.get("button").contains("Opt in to all").click();
+
+        const expected = [
+          { eventName: "cmpDisplayStatus", data: "hidden" },
+          { eventName: "sectionChange", data: "tcfeuv2" },
+          { eventName: "signalStatus", data: "ready" },
+        ];
+
+        cy.get("@gppListener")
+          .its("args")
+          .then((args) => {
+            expect(args.length).to.eql(4);
+            // we already checked the first arg, so inspect the other three
+            [args[1], args[2], args[3]].forEach(([data, success], idx) => {
+              expect(success).to.eql(true);
+              expect(data.eventName).to.eql(expected[idx].eventName);
+              expect(data.data).to.eql(expected[idx].data);
+            });
+            // The gpp string should also have an extra section now and the header should
+            // indicate TCF
+            expect(args[3][0].pingData.gppString).to.contain("DBABMA~");
+          });
+      });
+    });
+
+    /**
+     * Follows the flow documented here for Event Order Example 2:
+     * https://github.com/InteractiveAdvertisingBureau/Global-Privacy-Platform/blob/main/Core/CMP%20API%20Specification.md#eventlistener-
+     * 1. initial data from stub
+     * 2. listenerRegistered
+     * 3. cmpStatus = loaded
+     * 4. signalStatus = ready
+     * 5. User opens the consent layer to change their voice
+     * 6. signalStatus = not ready
+     * 7. cmpDisplayStatus = visible
+     * 8. User makes their choice
+     * 9. cmpDisplayStatus = hidden
+     * 10. sectionChange = tcfeuv2
+     * 11. signalStatus = ready
+     */
+    it.only("fires appropriate gpp events for returning user", () => {
+      // Set a cookie to mimic a returning user
+      const cookie = mockCookie({
+        tcf_version_hash: TCF_VERSION_HASH,
+      });
+      cy.setCookie(CONSENT_COOKIE_NAME, JSON.stringify(cookie));
+      cy.fixture("consent/experience_tcf.json").then((experience) => {
+        stubConfig({
+          options: {
+            isOverlayEnabled: true,
+            tcfEnabled: true,
+            gppEnabled: true,
+          },
+          experience: experience.items[0],
+        });
+      });
+
+      cy.waitUntilFidesInitialized().then(() => {
+        cy.get("@FidesUIShown").should("not.have.been.called");
+        // TODO(PROD#1439): Because the stub is too late right now, we can't listen for events
+        // 3 and 4 yet.
+        cy.window().then((win) => {
+          win.__gpp("addEventListener", cy.stub().as("gppListener"));
+        });
+
+        cy.get("@gppListener")
+          .should("have.been.calledOnce")
+          .its("lastCall.args")
+          .then(([data, success]) => {
+            expect(success).to.eql(true);
+            expect(data.eventName).to.eql("listenerRegistered");
+            const { cmpDisplayStatus, signalStatus, gppString } = data.pingData;
+            console.log({ data });
+            expect(cmpDisplayStatus).to.eql("hidden");
+            expect(signalStatus).to.eql("ready");
+            expect(gppString).to.eql("DBAA"); // empty string, header only
+          });
       });
     });
   });
