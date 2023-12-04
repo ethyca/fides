dfcdd540b058c769af824ea5cb7f99c08761055f
Allison King <allisonjuliaking@gmail.com>
2023-12-04 11:18:56 -0500

    Initial commit for cookie refactor

--- clients/privacy-center/cypress/e2e/consent-banner-tcf.cy.ts
+++ clients/privacy-center/cypress/e2e/consent-banner-tcf.cy.ts
@@ -2698,25 +2698,9 @@ describe("Fides-js TCF", () => {
     });
 
     it("can initialize from an AC string", () => {
-      const uuid = "4fbb6edf-34f6-4717-a6f1-541fd1e5d585";
-      const CREATED_DATE = "2022-12-24T12:00:00.000Z";
-      const UPDATED_DATE = "2022-12-25T12:00:00.000Z";
-      const cookie = {
-        identity: { fides_user_device_id: uuid },
-        fides_meta: {
-          version: "0.9.0",
-          createdAt: CREATED_DATE,
-          updatedAt: UPDATED_DATE,
-        },
-        consent: {},
-        tcf_consent: {
-          purpose_consent_preferences: { 2: false, [PURPOSE_4.id]: true },
-          special_feature_preferences: { [SPECIAL_FEATURE_1.id]: true },
-          system_legitimate_interests_preferences: { [SYSTEM_1.id]: false },
-          vendor_consent_preferences: { [VENDOR_1.id]: false },
-        },
-        tc_string: "CPzbcgAPzbcgAGXABBENATEIAACAAAAAAAAAABEAAAAA.IABE",
-      };
+      const cookie = mockCookie({
+        fides_string: "CPzbcgAPzbcgAGXABBENATEIAACAAAAAAAAAABEAAAAA.IABE",
+      });
       cy.setCookie(CONSENT_COOKIE_NAME, JSON.stringify(cookie));
       cy.fixture("consent/experience_tcf.json").then((experience) => {
         stubConfig({
