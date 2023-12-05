80deda601609cf9e3b041ff4bae23a412d7a946d
Adam Sachs <adam@ethyca.com>
2023-12-05 17:36:08 -0500

    refactor to include generic resource query execution function

--- src/fides/api/util/tcf/__init__.py
+++ src/fides/api/util/tcf/__init__.py
@@ -29,28 +29,16 @@ def exclude_ac_systems(query: Select) -> Select:
 
 
 async def list_non_tcf_systems(
-    async_session: AsyncSession, exclude_gvl: bool = True, exclude_ac: bool = True
-) -> List[System]:
+    exclude_gvl: bool = True, exclude_ac: bool = True
+) -> Select:
     """
     Utility to retrieve all Systems that are not GVL systems using
     the provided (async) DB session.
     """
-    with logger.contextualize(sql_model="System"):
-        async with async_session.begin():
-            try:
-                logger.debug("Fetching non TCF systems")
-                query = select(System)
-                if exclude_gvl:
-                    query = exclude_gvl_systems(query)
-                if exclude_ac:
-                    query = exclude_ac_systems(query)
-                result = await async_session.execute(query)
-                sql_resources = result.scalars().all()
-            except SQLAlchemyError:
-                error = errors.QueryError()
-                logger.bind(error=error.detail["error"]).info(  # type: ignore[index]
-                    "Failed to fetch non TCF systems"
-                )
-                raise error
 
-            return sql_resources
+    query = select(System)
+    if exclude_gvl:
+        query = exclude_gvl_systems(query)
+    if exclude_ac:
+        query = exclude_ac_systems(query)
+    return query
