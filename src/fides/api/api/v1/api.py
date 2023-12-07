659eea3671f9def47a2a9cfc4707c2ce2a4ef23c
Dawn Pattison <pattisdr@users.noreply.github.com>
2023-12-06 20:08:01 -0600

    Start moving served notice endpoints and privacy preference endpoints to plus.

--- src/fides/api/api/v1/api.py
+++ src/fides/api/api/v1/api.py
@@ -15,12 +15,10 @@ from fides.api.api.v1.endpoints import (
     policy_webhook_endpoints,
     privacy_experience_config_endpoints,
     privacy_notice_endpoints,
-    privacy_preference_endpoints,
     privacy_request_endpoints,
     purpose_endpoints,
     registration_endpoints,
     saas_config_endpoints,
-    served_notice_endpoints_v2,
     storage_endpoints,
     user_endpoints,
     user_permission_endpoints,
@@ -41,7 +39,6 @@ api_router.include_router(policy_endpoints.router)
 api_router.include_router(policy_webhook_endpoints.router)
 api_router.include_router(privacy_experience_config_endpoints.router)
 api_router.include_router(privacy_notice_endpoints.router)
-api_router.include_router(privacy_preference_endpoints.router)
 api_router.include_router(privacy_request_endpoints.router)
 api_router.include_router(identity_verification_endpoints.router)
 api_router.include_router(storage_endpoints.router)
@@ -52,4 +49,3 @@ api_router.include_router(user_permission_endpoints.router)
 api_router.include_router(manual_webhook_endpoints.router)
 api_router.include_router(registration_endpoints.router)
 api_router.include_router(purpose_endpoints.router)
-api_router.include_router(served_notice_endpoints_v2.router)
