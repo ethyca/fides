3535d62d37e3113c2c50449ba1f9feff92ea66eb
Kelsey Thomas <101993653+Kelsey-Ethyca@users.noreply.github.com>
2023-12-06 18:40:34 -0800

    fix testid

--- clients/admin-ui/src/features/datamap/GVLDatamapNotice.tsx
+++ clients/admin-ui/src/features/datamap/GVLDatamapNotice.tsx
@@ -3,7 +3,7 @@ import { Box, Link } from "@fidesui/react";
 import EmptyTableState from "~/features/common/table/EmptyTableState";
 
 const GVLDatamapNotice = () => (
-  <Box mb="6" maxW="720px" data-testid="locked-for-GVL-notice">
+  <Box mb="6" maxW="720px" data-testid="GVL-datamap-notice">
     <EmptyTableState
       title="GVL and AC vendors are not displayed"
       description="Global Vendor List and Google Additional Consent vendors are not displayed on the data map for performance reasons. An enhancement is in the works!"
