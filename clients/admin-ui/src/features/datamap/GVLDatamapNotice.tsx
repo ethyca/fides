39753a4b76825f407489a79a36c1bba0ee166eba
Kelsey Thomas <101993653+Kelsey-Ethyca@users.noreply.github.com>
2023-12-07 09:14:35 -0800

    add margin right and remove max width to gvl datamap notice

--- clients/admin-ui/src/features/datamap/GVLDatamapNotice.tsx
+++ clients/admin-ui/src/features/datamap/GVLDatamapNotice.tsx
@@ -3,7 +3,7 @@ import { Box } from "@fidesui/react";
 import EmptyTableState from "~/features/common/table/EmptyTableState";
 
 const GVLDatamapNotice = () => (
-  <Box mb="6" maxW="720px" data-testid="GVL-datamap-notice">
+  <Box mb="6" mr="40px" data-testid="GVL-datamap-notice">
     <EmptyTableState
       title="GVL and AC vendors are not displayed"
       description="Global Vendor List and Google Additional Consent vendors are not displayed on the data map for performance reasons. An enhancement is in the works!"
