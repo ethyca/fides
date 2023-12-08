f020c10ab506f96fd135911ece7eb1f36dc1a743
Allison King <allisonjuliaking@gmail.com>
2023-12-08 14:53:39 -0500

    Add notice key map

--- clients/fides-js/src/lib/gpp/constants.ts
+++ clients/fides-js/src/lib/gpp/constants.ts
@@ -1,1 +1,160 @@
+import {
+  UsCaV1Field,
+  UsCoV1Field,
+  UsCtV1Field,
+  UsNatV1Field,
+  UsUtV1Field,
+  UsVaV1Field,
+} from "@iabgpp/cmpapi";
+
 export const SUPPORTED_APIS = ["2:tcfeuv2"];
+
+interface GppFields {
+  gpp_notice_fields: string[];
+  gpp_mechanism_fields: string[];
+}
+type FidesRegionToGppFields = Record<string, GppFields>;
+type NoticeKeyToFidesRegionGppFields = Record<string, FidesRegionToGppFields>;
+
+/**
+ * Eventually this will be encoded on the privacy notice itself
+ *
+ * Currently based off of this table
+ * https://ethyca.atlassian.net/wiki/spaces/EN/pages/2852945989/2023-11-30+Fides.js+GPP+implementation?focusedCommentId=2861989902
+ */
+export const NOTICE_KEY_TO_GPP_FIELDS: NoticeKeyToFidesRegionGppFields = {
+  sales_sharing_targeted_advertising: {
+    us: {
+      gpp_notice_fields: [
+        UsNatV1Field.SALE_OPT_OUT_NOTICE,
+        UsNatV1Field.SHARING_OPT_OUT_NOTICE,
+        UsNatV1Field.TARGETED_ADVERTISING_OPT_OUT_NOTICE,
+        UsNatV1Field.SHARING_NOTICE,
+      ],
+      gpp_mechanism_fields: [
+        UsNatV1Field.SALE_OPT_OUT,
+        UsNatV1Field.SHARING_OPT_OUT,
+        UsNatV1Field.TARGETED_ADVERTISING_OPT_OUT,
+      ],
+    },
+  },
+  data_sales_and_sharing: {
+    us_ca: {
+      gpp_notice_fields: [
+        UsCaV1Field.SALE_OPT_OUT_NOTICE,
+        UsCaV1Field.SHARING_OPT_OUT_NOTICE,
+      ],
+      gpp_mechanism_fields: [
+        UsCaV1Field.SALE_OPT_OUT,
+        UsCaV1Field.SHARING_OPT_OUT,
+      ],
+    },
+  },
+  targeted_advertising: {
+    us_co: {
+      gpp_notice_fields: [
+        UsCoV1Field.SALE_OPT_OUT_NOTICE,
+        UsCoV1Field.TARGETED_ADVERTISING_OPT_OUT_NOTICE,
+      ],
+      gpp_mechanism_fields: [
+        UsCoV1Field.SALE_OPT_OUT,
+        UsCoV1Field.TARGETED_ADVERTISING_OPT_OUT,
+      ],
+    },
+    us_ct: {
+      gpp_notice_fields: [UsCtV1Field.TARGETED_ADVERTISING_OPT_OUT_NOTICE],
+      gpp_mechanism_fields: [UsCtV1Field.TARGETED_ADVERTISING_OPT_OUT],
+    },
+    us_ut: {
+      gpp_notice_fields: [UsUtV1Field.TARGETED_ADVERTISING_OPT_OUT_NOTICE],
+      gpp_mechanism_fields: [UsUtV1Field.TARGETED_ADVERTISING_OPT_OUT],
+    },
+    us_va: {
+      gpp_notice_fields: [],
+      gpp_mechanism_fields: [],
+    },
+    us_ia: {
+      // DEFER: Iowa isn't part of the GPP spec yet
+      gpp_notice_fields: [],
+      gpp_mechanism_fields: [],
+    },
+  },
+  data_sharing: {
+    us_ut: {
+      gpp_notice_fields: [UsUtV1Field.SHARING_NOTICE],
+      gpp_mechanism_fields: [],
+    },
+    us_va: {
+      gpp_notice_fields: [UsVaV1Field.SHARING_NOTICE],
+      gpp_mechanism_fields: [],
+    },
+    us_co: {
+      gpp_notice_fields: [],
+      gpp_mechanism_fields: [],
+    },
+    us_ct: {
+      gpp_notice_fields: [UsCtV1Field.SHARING_NOTICE],
+      gpp_mechanism_fields: [],
+    },
+  },
+  data_sales: {
+    us_co: {
+      gpp_notice_fields: [],
+      gpp_mechanism_fields: [],
+    },
+    us_ct: {
+      gpp_notice_fields: [UsCtV1Field.SALE_OPT_OUT_NOTICE],
+      gpp_mechanism_fields: [UsCtV1Field.SALE_OPT_OUT],
+    },
+    us_ut: {
+      gpp_notice_fields: [UsUtV1Field.SALE_OPT_OUT_NOTICE],
+      gpp_mechanism_fields: [UsUtV1Field.SALE_OPT_OUT],
+    },
+    us_va: {
+      gpp_notice_fields: [UsVaV1Field.SALE_OPT_OUT_NOTICE],
+      gpp_mechanism_fields: [UsVaV1Field.SALE_OPT_OUT],
+    },
+    us_ia: {
+      // DEFER: Iowa isn't part of the GPP spec yet
+      gpp_notice_fields: [],
+      gpp_mechanism_fields: [],
+    },
+  },
+  sensitive_personal_data_sharing: {
+    us_ca: {
+      gpp_notice_fields: [UsCaV1Field.SENSITIVE_DATA_LIMIT_USE_NOTICE],
+      gpp_mechanism_fields: [
+        UsCaV1Field.KNOWN_CHILD_SENSITIVE_DATA_CONSENTS,
+        UsCaV1Field.PERSONAL_DATA_CONSENTS,
+      ],
+    },
+    us_ut: {
+      gpp_notice_fields: [UsUtV1Field.SENSITIVE_DATA_PROCESSING_OPT_OUT_NOTICE],
+      gpp_mechanism_fields: [
+        UsUtV1Field.SENSITIVE_DATA_PROCESSING,
+        UsUtV1Field.KNOWN_CHILD_SENSITIVE_DATA_CONSENTS,
+      ],
+    },
+    us_va: {
+      gpp_notice_fields: [],
+      gpp_mechanism_fields: [
+        UsVaV1Field.SENSITIVE_DATA_PROCESSING,
+        UsVaV1Field.KNOWN_CHILD_SENSITIVE_DATA_CONSENTS,
+      ],
+    },
+    us_co: {
+      gpp_notice_fields: [],
+      gpp_mechanism_fields: [
+        UsCoV1Field.SENSITIVE_DATA_PROCESSING,
+        UsCoV1Field.KNOWN_CHILD_SENSITIVE_DATA_CONSENTS,
+      ],
+    },
+    us_ct: {
+      gpp_notice_fields: [],
+      gpp_mechanism_fields: [
+        UsCtV1Field.KNOWN_CHILD_SENSITIVE_DATA_CONSENTS,
+        UsCtV1Field.SENSITIVE_DATA_PROCESSING,
+      ],
+    },
+  },
+};
