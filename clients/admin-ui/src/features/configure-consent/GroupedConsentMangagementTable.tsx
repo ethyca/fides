1142baea812d3124a41da6a000892329734e9f56
TheAndrewJackson <andrew.c.j1995@gmail.com>
2023-12-11 16:26:21 -0500

    initial grouped prototype table

---
+++ clients/admin-ui/src/features/configure-consent/GroupedConsentMangagementTable.tsx
@@ -0,0 +1,318 @@
+/* eslint-disable react/no-unstable-nested-components */
+import { Button, Flex } from "@fidesui/react";
+import {
+  createColumnHelper,
+  getCoreRowModel,
+  getPaginationRowModel,
+  getGroupedRowModel,
+  getExpandedRowModel,
+  useReactTable,
+} from "@tanstack/react-table";
+import { useFeatures } from "common/features";
+import {
+  BadgeCell,
+  DefaultCell,
+  DefaultHeaderCell,
+  FidesTableV2,
+  GlobalFilterV2,
+  PaginationBar,
+  TableActionBar,
+  TableSkeletonLoader,
+  useClientSidePagination,
+} from "common/table/v2";
+import { useEffect, useMemo, useState } from "react";
+
+import {
+  ConsentManagementFilterModal,
+  Option,
+  useConsentManagementFilters,
+} from "~/features/configure-consent/ConsentManagementFilterModal";
+import {
+  ConsentManagementModal,
+  useConsentManagementModal,
+} from "~/features/configure-consent/ConsentManagementModal";
+import {
+  useGetHealthQuery,
+  useGetVendorReportQuery,
+} from "~/features/plus/plus.slice";
+import { Page_SystemSummary_, SystemSummary } from "~/types/api";
+
+type PDGroupedBySystem = {
+  systemName: string;
+  dataUse: string;
+  dataCategories: string[];
+  dataSubject: string[];
+};
+
+const tableData: PDGroupedBySystem[] = [
+  {
+    systemName: "ACME 1",
+    dataUse: "data use 1",
+    dataCategories: ["cat1", " cat 2", "cat 3"],
+    dataSubject: ["subject 1", "subject 2", "subject 3"],
+  },
+  {
+    systemName: "ACME 1",
+    dataUse: "data use 2",
+    dataCategories: ["cat1", " cat 2", "cat 3"],
+    dataSubject: ["subject 1", "subject 2", "subject 3"],
+  },
+  {
+    systemName: "ACME 1",
+    dataUse: "data use 3",
+    dataCategories: ["cat1", " cat 2", "cat 3"],
+    dataSubject: ["subject 1", "subject 2", "subject 3"],
+  },
+  {
+    systemName: "ACME 1",
+    dataUse: "data use 4",
+    dataCategories: ["cat1", " cat 2", "cat 3"],
+    dataSubject: ["subject 1", "subject 2", "subject 3"],
+  },
+  {
+    systemName: "ACME 1",
+    dataUse: "data use 5",
+    dataCategories: ["cat1", " cat 2", "cat 3"],
+    dataSubject: ["subject 1", "subject 2", "subject 3"],
+  },
+  {
+    systemName: "ACME 1",
+    dataUse: "data use 6",
+    dataCategories: ["cat1", " cat 2", "cat 3"],
+    dataSubject: ["subject 1", "subject 2", "subject 3"],
+  },
+
+  {
+    systemName: "ACME 2",
+    dataUse: "data use 1",
+    dataCategories: ["cat1", " cat 2", "cat 3"],
+    dataSubject: ["subject 1", "subject 2", "subject 3"],
+  },
+  {
+    systemName: "ACME 2",
+    dataUse: "data use 2",
+    dataCategories: ["cat1", " cat 2", "cat 3"],
+    dataSubject: ["subject 1", "subject 2", "subject 3"],
+  },
+  {
+    systemName: "ACME 2",
+    dataUse: "data use 3",
+    dataCategories: ["cat1", " cat 2", "cat 3"],
+    dataSubject: ["subject 1", "subject 2", "subject 3"],
+  },
+  {
+    systemName: "ACME 2",
+    dataUse: "data use 4",
+    dataCategories: ["cat1", " cat 2", "cat 3"],
+    dataSubject: ["subject 1", "subject 2", "subject 3"],
+  },
+  {
+    systemName: "ACME 2",
+    dataUse: "data use 5",
+    dataCategories: ["cat1", " cat 2", "cat 3"],
+    dataSubject: ["subject 1", "subject 2", "subject 3"],
+  },
+  {
+    systemName: "ACME 2",
+    dataUse: "data use 6",
+    dataCategories: ["cat1", " cat 2", "cat 3"],
+    dataSubject: ["subject 1", "subject 2", "subject 3"],
+  },
+
+  {
+    systemName: "ACME 3",
+    dataUse: "data use 1",
+    dataCategories: ["cat1", " cat 2", "cat 3"],
+    dataSubject: ["subject 1", "subject 2", "subject 3"],
+  },
+  {
+    systemName: "ACME 3",
+    dataUse: "data use 2",
+    dataCategories: ["cat1", " cat 2", "cat 3"],
+    dataSubject: ["subject 1", "subject 2", "subject 3"],
+  },
+  {
+    systemName: "ACME 3",
+    dataUse: "data use 3",
+    dataCategories: ["cat1", " cat 2", "cat 3"],
+    dataSubject: ["subject 1", "subject 2", "subject 3"],
+  },
+  {
+    systemName: "ACME 3",
+    dataUse: "data use 4",
+    dataCategories: ["cat1", " cat 2", "cat 3"],
+    dataSubject: ["subject 1", "subject 2", "subject 3"],
+  },
+  {
+    systemName: "ACME 3",
+    dataUse: "data use 5",
+    dataCategories: ["cat1", " cat 2", "cat 3"],
+    dataSubject: ["subject 1", "subject 2", "subject 3"],
+  },
+  {
+    systemName: "ACME 3",
+    dataUse: "data use 6",
+    dataCategories: ["cat1", " cat 2", "cat 3"],
+    dataSubject: ["subject 1", "subject 2", "subject 3"],
+  },
+];
+
+const columnHelper = createColumnHelper<PDGroupedBySystem>();
+
+const emptyVendorReportResponse: Page_SystemSummary_ = {
+  items: [],
+  total: 0,
+  page: 1,
+  size: 25,
+  pages: 1,
+};
+export const GroupedConsentManagementTable = () => {
+  const { tcf: isTcfEnabled } = useFeatures();
+  const { isLoading: isLoadingHealthCheck } = useGetHealthQuery();
+  const {
+    isOpen: isRowModalOpen,
+    onOpen: onRowModalOpen,
+    onClose: onRowModalClose,
+  } = useConsentManagementModal();
+  const [systemFidesKey, setSystemFidesKey] = useState<string>();
+
+  const {
+    isOpen: isFilterOpen,
+    onOpen: onOpenFilter,
+    onClose: onCloseFilter,
+    resetFilters,
+    purposeOptions,
+    onPurposeChange,
+    dataUseOptions,
+    onDataUseChange,
+    legalBasisOptions,
+    onLegalBasisChange,
+    consentCategoryOptions,
+    onConsentCategoryChange,
+  } = useConsentManagementFilters();
+
+  const [globalFilter, setGlobalFilter] = useState<string>();
+
+  const updateGlobalFilter = (searchTerm: string) => {
+    resetPageIndexToDefault();
+    setGlobalFilter(searchTerm);
+  };
+
+  const tcfColumns = useMemo(
+    () => [
+      columnHelper.accessor((row) => row.systemName, {
+        enableGrouping: true,
+        id: "systemName",
+        cell: (props) => <DefaultCell value={props.getValue()} />,
+        header: (props) => <DefaultHeaderCell value="Vendor" {...props} />,
+        meta: {
+          maxWidth: "350px",
+        },
+      }),
+      columnHelper.accessor((row) => row.dataUse, {
+        id: "dataUse",
+        cell: (props) => <DefaultCell value={props.getValue()} />,
+        header: (props) => <DefaultHeaderCell value="Data use" {...props} />,
+        meta: {
+          width: "175px",
+        },
+      }),
+      columnHelper.accessor((row) => row.dataCategories, {
+        id: "dataCategories",
+        cell: (props) => <DefaultCell value={"world"} />,
+        header: (props) => (
+          <DefaultHeaderCell value="Data categories" {...props} />
+        ),
+        meta: {
+          width: "175px",
+        },
+      }),
+      columnHelper.accessor((row) => row.dataSubject, {
+        id: "dataSubject",
+        cell: (props) => <DefaultCell value={"hello"} />,
+        header: (props) => <DefaultHeaderCell value="Legal basis" {...props} />,
+        meta: {
+          width: "175px",
+        },
+      }),
+    ],
+    []
+  );
+  const [grouping, setGrouping] = useState(["systemName"]);
+
+  const tableInstance = useReactTable<PDGroupedBySystem>({
+    columns: tcfColumns,
+    getPaginationRowModel: getPaginationRowModel(),
+    getGroupedRowModel: getGroupedRowModel(),
+    getExpandedRowModel: getExpandedRowModel(),
+    data: tableData,
+    debugTable: true,
+    state: {
+      expanded: true,
+      grouping,
+    },
+    onGroupingChange: setGrouping,
+    getCoreRowModel: getCoreRowModel(),
+  });
+
+  const pageProps = useClientSidePagination(tableInstance);
+
+  const onRowClick = (system: SystemSummary) => {
+    setSystemFidesKey(system.fides_key);
+    onRowModalOpen();
+  };
+
+  return (
+    <Flex flex={1} direction="column" overflow="auto">
+      {grouping}
+      {isRowModalOpen && systemFidesKey ? (
+        <ConsentManagementModal
+          isOpen={isRowModalOpen}
+          fidesKey={systemFidesKey}
+          onClose={onRowModalClose}
+        />
+      ) : null}
+      <TableActionBar>
+        <GlobalFilterV2
+          globalFilter={globalFilter}
+          setGlobalFilter={updateGlobalFilter}
+          placeholder="Search"
+        />
+        <ConsentManagementFilterModal
+          isOpen={isFilterOpen}
+          isTcfEnabled={isTcfEnabled}
+          onClose={onCloseFilter}
+          resetFilters={resetFilters}
+          purposeOptions={purposeOptions}
+          onPurposeChange={onPurposeChange}
+          dataUseOptions={dataUseOptions}
+          onDataUseChange={onDataUseChange}
+          legalBasisOptions={legalBasisOptions}
+          onLegalBasisChange={onLegalBasisChange}
+          consentCategoryOptions={consentCategoryOptions}
+          onConsentCategoryChange={onConsentCategoryChange}
+        />
+        <Flex alignItems="center">
+          <Button
+            onClick={onOpenFilter}
+            data-testid="filter-multiple-systems-btn"
+            size="xs"
+            variant="outline"
+          >
+            Filter
+          </Button>
+          <Button
+            onClick={() => {
+              tableInstance.setGrouping(["dataUse"]);
+            }}
+          >
+            Group
+          </Button>
+        </Flex>
+      </TableActionBar>
+
+      <FidesTableV2 tableInstance={tableInstance} onRowClick={onRowClick} />
+      <PaginationBar {...pageProps} pageSizes={[5, 10, 15]} />
+    </Flex>
+  );
+};
---
+++ clients/admin-ui/src/pages/consent/configure/grouping-test.tsx
@@ -0,0 +1,104 @@
+import {
+  Box,
+  Breadcrumb,
+  BreadcrumbItem,
+  Flex,
+  Heading,
+  Spacer,
+  Text,
+} from "@fidesui/react";
+import { useFeatures } from "common/features";
+import NextLink from "next/link";
+import React from "react";
+
+import FixedLayout from "~/features/common/FixedLayout";
+import Layout from "~/features/common/Layout";
+import { CONFIGURE_CONSENT_ROUTE } from "~/features/common/nav/v2/routes";
+import AddVendor from "~/features/configure-consent/AddVendor";
+import ConfigureConsent from "~/features/configure-consent/ConfigureConsent";
+import { GroupedConsentManagementTable } from "~/features/configure-consent/GroupedConsentMangagementTable";
+
+type Props = {
+  includeAddVendors?: boolean;
+  title: string;
+  breadCrumbText: string;
+  description: string;
+};
+
+const ConsentMetadata: React.FC<Props> = ({
+  includeAddVendors,
+  title,
+  breadCrumbText,
+  description,
+}) => (
+  <>
+    <Box mb={4}>
+      <Heading fontSize="2xl" fontWeight="semibold" mb={2} data-testid="header">
+        {title}
+      </Heading>
+      <Box>
+        <Breadcrumb
+          fontWeight="medium"
+          fontSize="sm"
+          color="gray.600"
+          data-testid="breadcrumbs"
+        >
+          <BreadcrumbItem>
+            <NextLink href={CONFIGURE_CONSENT_ROUTE}>Consent</NextLink>
+          </BreadcrumbItem>
+          <BreadcrumbItem color="complimentary.500">
+            <NextLink href="#">{breadCrumbText}</NextLink>
+          </BreadcrumbItem>
+        </Breadcrumb>
+      </Box>
+    </Box>
+    <Flex>
+      <Text fontSize="sm" mb={8} width={{ base: "100%", lg: "50%" }}>
+        {description}
+      </Text>
+      {includeAddVendors ? (
+        <>
+          <Spacer />
+          <AddVendor />
+        </>
+      ) : null}
+    </Flex>
+  </>
+);
+
+const ConfigureConsentPage = () => {
+  const { tcf: isTcfEnabled } = useFeatures();
+
+  if (isTcfEnabled) {
+    return (
+      <FixedLayout
+        title="Configure consent"
+        mainProps={{
+          padding: "40px",
+          paddingRight: "48px",
+        }}
+      >
+        <ConsentMetadata
+          includeAddVendors
+          title="Manage your vendors"
+          breadCrumbText="Vendors"
+          description="Use the table below to manage your vendors. Modify the legal basis for a vendor if permitted and view and group your views by applying different filters"
+        />
+        <GroupedConsentManagementTable />
+      </FixedLayout>
+    );
+  }
+
+  return (
+    <Layout title="Configure consent">
+      <ConsentMetadata
+        title="Configure consent"
+        breadCrumbText="Configure consent"
+        description="Your current cookies and tracking information."
+      />
+      <ConfigureConsent />
+    </Layout>
+  );
+};
+
+export default ConfigureConsentPage;
