558b19e789a42a64f76eb64e247217cf3bfded6a
Dawn Pattison <pattisdr@users.noreply.github.com>
2023-12-13 13:41:46 -0600

    Actually remove PurposesResponse -

--- src/fides/api/schemas/tcf.py
+++
@@ -1,9 +0,0 @@
-from typing import Dict
-
-from fideslang.gvl.models import MappedPurpose
-from pydantic import BaseModel
-
-
-class PurposesResponse(BaseModel):
-    purposes: Dict[str, MappedPurpose]
-    special_purposes: Dict[str, MappedPurpose]
