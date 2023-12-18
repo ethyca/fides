20f32bc253277a467f601938ebbbb6f71fc65eff
Neville Samuell <neville@ethyca.com>
2023-12-18 18:17:28 -0500

    Fix ConsentMethod type definitions

--- clients/fides-js/src/lib/consent-types.ts
+++ clients/fides-js/src/lib/consent-types.ts
@@ -420,14 +420,15 @@ export enum ButtonType {
 }
 
 export enum ConsentMethod {
-  button = "button", // deprecated- keeping for backwards-compatibility
-  reject = "reject",
-  accept = "accept",
-  save = "save",
-  dismiss = "dismiss",
-  gpc = "gpc",
-  individual_notice = "api",
+  BUTTON = "button", // deprecated- keeping for backwards-compatibility
+  REJECT = "reject",
+  ACCEPT = "accept",
+  SAVE = "save",
+  DISMISS = "dismiss",
+  GPC = "gpc",
+  INDIVIDUAL_NOTICE = "individual_notice",
 }
+
 
 export type PrivacyPreferencesRequest = {
   browser_identity: Identity;
