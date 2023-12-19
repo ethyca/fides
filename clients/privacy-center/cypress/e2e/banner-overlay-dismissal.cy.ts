5a7e150c6740531bc71502115d5d115c45b1512c
Neville Samuell <neville@ethyca.com>
2023-12-19 09:03:46 -0500

    Refactor banner dismissal tests and check for consentMethod too

--- clients/privacy-center/cypress/e2e/banner-overlay-dismissal.cy.ts
+++ clients/privacy-center/cypress/e2e/banner-overlay-dismissal.cy.ts
@@ -1,4 +1,4 @@
-import { ConsentMethod } from "fides-js";
+import { ConsentMethod, CONSENT_COOKIE_NAME, FidesCookie } from "fides-js";
 import { stubConfig } from "../support/stubs";
 
 function assertDismissCalled() {
@@ -9,303 +9,184 @@ function assertDismissCalled() {
       expect(consentMethod).to.eql(ConsentMethod.DISMISS);
     });
 
+  cy.waitUntilCookieExists(CONSENT_COOKIE_NAME).then(() => {
+    cy.getCookie(CONSENT_COOKIE_NAME).then((cookie) => {
+      const fidesCookie: FidesCookie = JSON.parse(
+        decodeURIComponent(cookie!.value)
+      );
+      expect(fidesCookie.fides_meta)
+        .property("consentMethod")
+        .is.eql(ConsentMethod.DISMISS);
+    });
+  });
+
   cy.wait("@patchPrivacyPreference");
 }
 
-describe("Banner and overlay dismissal", () => {
-  describe("Consent with dismissal allowed", () => {
-    beforeEach(() => {
-      stubConfig({
-        options: {
-          tcfEnabled: false,
-          preventDismissal: false,
-        },
-      });
-    });
+describe("Banner and modal dismissal", () => {
+  interface TestCaseOptions {
+    tcfEnabled: boolean,
+    preventDismissal: boolean,
+  };
 
-    describe("Banner dismissal", () => {
-      it("Should dismiss the banner by clicking the x", () => {
-        cy.get("#fides-banner").should("be.visible");
-        cy.get("#fides-banner").within(() => {
-          cy.get(".fides-close-button").click();
-        });
-        cy.get("#fides-banner").should("not.be.visible");
-
-        assertDismissCalled();
-        // TODO: assert cookie updated
-        // TODO: reload and ensure banner is displayed again
-      });
-
-      it("Should dismiss the banner by clicking outside", () => {
-        cy.get("#fides-banner").should("be.visible");
-        cy.get("h1").click(); // click outside
-        cy.get("#fides-banner").should("not.be.visible");
-
-        assertDismissCalled();
-      });
-
-      it("Should dismiss the banner by hitting ESC", () => {
-        cy.get("#fides-banner").should("be.visible");
-        cy.get("body").type("{esc}");
-        cy.get("#fides-banner").should("not.be.visible");
-
-        assertDismissCalled();
-      });
-    });
-
-    describe("Overlay dismissal", () => {
-      it("Should dismiss the banner by clicking the x", () => {
-        cy.get("#fides-banner").should("be.visible");
-        cy.getByTestId("Manage preferences-btn").click();
-        cy.get(".fides-modal-content").should("be.visible");
-        cy.get(".fides-modal-content").within(() => {
-          cy.get(".fides-close-button").click();
-        });
-        cy.get(".fides-modal-content").should("not.be.visible");
-
-        assertDismissCalled();
+  // Test all combinations of TCF enabled/disabled and prevent dismissal enabled/disabled
+  [
+    { tcfEnabled: false, preventDismissal: false },
+    { tcfEnabled: false, preventDismissal: true },
+    { tcfEnabled: true, preventDismissal: false },
+    { tcfEnabled: true, preventDismissal: true },
+  ].forEach(({ tcfEnabled, preventDismissal }) => {
+    describe(`when tcfEnabled is ${tcfEnabled} and preventDismissal is ${preventDismissal}`, () => {
+      beforeEach(() => {
+        if (!tcfEnabled) {
+          stubConfig({
+            options: {
+              tcfEnabled: false,
+              preventDismissal: preventDismissal,
+            },
+          });
+        } else {
+          cy.fixture("consent/experience_tcf.json").then((experience) => {
+            stubConfig({
+              options: {
+                tcfEnabled: true,
+                preventDismissal: preventDismissal,
+              },
+              experience: experience.items[0],
+            });
+          });
+        }
       });
 
-      it("Should dismiss the banner by clicking outside", () => {
-        cy.get("#fides-banner").should("be.visible");
-        cy.getByTestId("Manage preferences-btn").click();
-        cy.get(".fides-modal-content").should("be.visible");
-        cy.get(".fides-modal-overlay").click({ force: true });
-        cy.get(".fides-modal-content").should("not.be.visible");
+      if (!preventDismissal) {
+        /**
+         * Tests for the default case, when dismissal is allowed
+         */
+        describe("when using the banner", () => {
+          it("should dismiss the banner by clicking the x", () => {
+            cy.get("#fides-banner").should("be.visible");
+            cy.get("#fides-banner").within(() => {
+              cy.get(".fides-close-button").click();
+            });
+            cy.get("#fides-banner").should("not.be.visible");
 
-        assertDismissCalled();
-      });
+            assertDismissCalled();
+          });
 
-      it("Should dismiss the banner by hitting ESC", () => {
-        cy.get("#fides-banner").should("be.visible");
-        cy.getByTestId("Manage preferences-btn").click();
-        cy.get(".fides-modal-content").should("be.visible");
-        cy.get("body").type("{esc}");
-        cy.get(".fides-modal-content").should("not.be.visible");
+          it("should dismiss the banner by clicking outside", () => {
+            cy.get("#fides-banner").should("be.visible");
+            cy.get("h1").click(); // click outside
+            cy.get("#fides-banner").should("not.be.visible");
 
-        assertDismissCalled();
-      });
-    });
-  });
+            assertDismissCalled();
+          });
 
-  describe("Consent with dismissal not allowed", () => {
-    beforeEach(() => {
-      stubConfig({
-        options: {
-          tcfEnabled: false,
-          preventDismissal: true,
-        },
-      });
-    });
+          it("should dismiss the banner by hitting ESC", () => {
+            cy.get("#fides-banner").should("be.visible");
+            cy.get("body").type("{esc}");
+            cy.get("#fides-banner").should("not.be.visible");
 
-    describe("Banner dismissal", () => {
-      it("Should not show the x button", () => {
-        cy.get("#fides-banner").should("be.visible");
-        cy.get("#fides-banner").within(() => {
-          cy.get(".fides-close-button").should("not.be.visible");
+            assertDismissCalled();
+          });
         });
 
-        cy.get("@FidesUpdated").should("not.have.been.called");
-      });
+        describe("when using the modal", () => {
+          it("should dismiss the modal by clicking the x", () => {
+            cy.get("#fides-banner").should("be.visible");
+            cy.getByTestId("Manage preferences-btn").click();
+            cy.get(".fides-modal-content").should("be.visible");
+            cy.get(".fides-modal-content").within(() => {
+              cy.get(".fides-close-button").click();
+            });
+            cy.get(".fides-modal-content").should("not.be.visible");
 
-      it("Should not dismiss the banner by clicking outside", () => {
-        cy.get("#fides-banner").should("be.visible");
-        cy.get("h1").click({ force: true }); // click outside
-        cy.get("#fides-banner").should("be.visible");
+            assertDismissCalled();
+          });
 
-        cy.get("@FidesUpdated").should("not.have.been.called");
-      });
+          it("should dismiss the modal by clicking outside", () => {
+            cy.get("#fides-banner").should("be.visible");
+            cy.getByTestId("Manage preferences-btn").click();
+            cy.get(".fides-modal-content").should("be.visible");
+            cy.get(".fides-modal-overlay").click({ force: true });
+            cy.get(".fides-modal-content").should("not.be.visible");
 
-      it("Should not dismiss the banner by hitting ESC", () => {
-        cy.get("#fides-banner").should("be.visible");
-        cy.get("body").type("{esc}");
-        cy.get("#fides-banner").should("be.visible");
+            assertDismissCalled();
+          });
 
-        cy.get("@FidesUpdated").should("not.have.been.called");
-      });
-    });
+          it("should dismiss the modal by hitting ESC", () => {
+            cy.get("#fides-banner").should("be.visible");
+            cy.getByTestId("Manage preferences-btn").click();
+            cy.get(".fides-modal-content").should("be.visible");
+            cy.get("body").type("{esc}");
+            cy.get(".fides-modal-content").should("not.be.visible");
 
-    describe("Overlay dismissal", () => {
-      it("Should not show the x button", () => {
-        cy.get("#fides-banner").should("be.visible");
-        cy.getByTestId("Manage preferences-btn").click();
-        cy.get(".fides-modal-content").should("be.visible");
-        cy.get(".fides-modal-content").within(() => {
-          cy.get(".fides-close-button").should("not.be.visible");
+            assertDismissCalled();
+          });
+        });
+      } else {
+        /**
+         * Tests for the special case, when dismissal is prevented
+         */
+        describe("when using the banner", () => {
+          it("should not show the x button", () => {
+            cy.get("#fides-banner").should("be.visible");
+            cy.get("#fides-banner").within(() => {
+              cy.get(".fides-close-button").should("not.be.visible");
+            });
+
+            cy.get("@FidesUpdated").should("not.have.been.called");
+          });
+
+          it("should not dismiss the banner by clicking outside", () => {
+            cy.get("#fides-banner").should("be.visible");
+            cy.get("h1").click({ force: true }); // click outside
+            cy.get("#fides-banner").should("be.visible");
+
+            cy.get("@FidesUpdated").should("not.have.been.called");
+          });
+
+          it("should not dismiss the banner by hitting ESC", () => {
+            cy.get("#fides-banner").should("be.visible");
+            cy.get("body").type("{esc}");
+            cy.get("#fides-banner").should("be.visible");
+
+            cy.get("@FidesUpdated").should("not.have.been.called");
+          });
         });
 
-        cy.get("@FidesUpdated").should("not.have.been.called");
-      });
+        describe("when using the modal", () => {
+          it("should not show the x button", () => {
+            cy.get("#fides-banner").should("be.visible");
+            cy.getByTestId("Manage preferences-btn").click();
+            cy.get(".fides-modal-content").should("be.visible");
+            cy.get(".fides-modal-content").within(() => {
+              cy.get(".fides-close-button").should("not.be.visible");
+            });
 
-      it("Should not dismiss the banner by clicking outside", () => {
-        cy.get("#fides-banner").should("be.visible");
-        cy.getByTestId("Manage preferences-btn").click();
-        cy.get(".fides-modal-content").should("be.visible");
-        cy.get(".fides-modal-overlay").click({ force: true });
-        cy.get(".fides-modal-content").should("be.visible");
+            cy.get("@FidesUpdated").should("not.have.been.called");
+          });
 
-        cy.get("@FidesUpdated").should("not.have.been.called");
-      });
+          it("should not dismiss the modal by clicking outside", () => {
+            cy.get("#fides-banner").should("be.visible");
+            cy.getByTestId("Manage preferences-btn").click();
+            cy.get(".fides-modal-content").should("be.visible");
+            cy.get(".fides-modal-overlay").click({ force: true });
+            cy.get(".fides-modal-content").should("be.visible");
 
-      it("Should not dismiss the banner by hitting ESC", () => {
-        cy.get("#fides-banner").should("be.visible");
-        cy.getByTestId("Manage preferences-btn").click();
-        cy.get(".fides-modal-content").should("be.visible");
-        cy.get("body").type("{esc}");
-        cy.get(".fides-modal-content").should("be.visible");
+            cy.get("@FidesUpdated").should("not.have.been.called");
+          });
 
-        cy.get("@FidesUpdated").should("not.have.been.called");
-      });
-    });
-  });
+          it("should not dismiss the modal by hitting ESC", () => {
+            cy.get("#fides-banner").should("be.visible");
+            cy.getByTestId("Manage preferences-btn").click();
+            cy.get(".fides-modal-content").should("be.visible");
+            cy.get("body").type("{esc}");
+            cy.get(".fides-modal-content").should("be.visible");
 
-  describe("TCF with dismissal allowed", () => {
-    beforeEach(() => {
-      cy.fixture("consent/experience_tcf.json").then((experience) => {
-        stubConfig({
-          options: {
-            tcfEnabled: true,
-            preventDismissal: false,
-          },
-          experience: experience.items[0],
+            cy.get("@FidesUpdated").should("not.have.been.called");
+          });
         });
-      });
-    });
-
-    describe("Banner dismissal", () => {
-      it("Should dismiss the banner by clicking the x", () => {
-        cy.get("#fides-banner").should("be.visible");
-        cy.get("#fides-banner").within(() => {
-          cy.get(".fides-close-button").click();
-        });
-        cy.get("#fides-banner").should("not.be.visible");
-
-        assertDismissCalled();
-      });
-
-      it("Should dismiss the banner by clicking outside", () => {
-        cy.get("#fides-banner").should("be.visible");
-        cy.get("h1").click(); // click outside
-        cy.get("#fides-banner").should("not.be.visible");
-
-        assertDismissCalled();
-      });
-
-      it("Should dismiss the banner by hitting ESC", () => {
-        cy.get("#fides-banner").should("be.visible");
-        cy.get("body").type("{esc}");
-        cy.get("#fides-banner").should("not.be.visible");
-
-        assertDismissCalled();
-      });
-    });
-
-    describe("Overlay dismissal", () => {
-      it("Should dismiss the overlay by clicking the x", () => {
-        cy.get("#fides-banner").should("be.visible");
-        cy.getByTestId("Manage preferences-btn").click();
-        cy.get(".fides-modal-content").should("be.visible");
-        cy.get(".fides-modal-content").within(() => {
-          cy.get(".fides-close-button").click();
-        });
-        cy.get(".fides-modal-content").should("not.be.visible");
-
-        assertDismissCalled();
-      });
-
-      it("Should dismiss the overlay by clicking outside", () => {
-        cy.get("#fides-banner").should("be.visible");
-        cy.getByTestId("Manage preferences-btn").click();
-        cy.get(".fides-modal-content").should("be.visible");
-        cy.get(".fides-modal-overlay").click({ force: true });
-        cy.get(".fides-modal-content").should("not.be.visible");
-
-        assertDismissCalled();
-      });
-
-      it("Should dismiss the overlay by hitting ESC", () => {
-        cy.get("#fides-banner").should("be.visible");
-        cy.getByTestId("Manage preferences-btn").click();
-        cy.get(".fides-modal-content").should("be.visible");
-        cy.get("body").type("{esc}");
-        cy.get(".fides-modal-content").should("not.be.visible");
-
-        assertDismissCalled();
-      });
-    });
-  });
-
-  describe("TCF with dismissal not allowed", () => {
-    beforeEach(() => {
-      cy.fixture("consent/experience_tcf.json").then((experience) => {
-        stubConfig({
-          options: {
-            tcfEnabled: true,
-            preventDismissal: true,
-          },
-          experience: experience.items[0],
-        });
-      });
-    });
-
-    describe("Banner dismissal", () => {
-      it("Should not show the x button", () => {
-        cy.get("#fides-banner").should("be.visible");
-        cy.get("#fides-banner").within(() => {
-          cy.get(".fides-close-button").should("not.be.visible");
-        });
-
-        cy.get("@FidesUpdated").should("not.have.been.called");
-      });
-
-      it("Should not dismiss the banner by clicking outside", () => {
-        cy.get("#fides-banner").should("be.visible");
-        cy.get("h1").click({ force: true }); // click outside
-        cy.get("#fides-banner").should("be.visible");
-
-        cy.get("@FidesUpdated").should("not.have.been.called");
-      });
-
-      it("Should not dismiss the banner by hitting ESC", () => {
-        cy.get("#fides-banner").should("be.visible");
-        cy.get("body").type("{esc}");
-        cy.get("#fides-banner").should("be.visible");
-
-        cy.get("@FidesUpdated").should("not.have.been.called");
-      });
-    });
-
-    describe("Overlay dismissal", () => {
-      it("Should not show the x button", () => {
-        cy.get("#fides-banner").should("be.visible");
-        cy.getByTestId("Manage preferences-btn").click();
-        cy.get(".fides-modal-content").should("be.visible");
-        cy.get(".fides-modal-content").within(() => {
-          cy.get(".fides-close-button").should("not.be.visible");
-        });
-
-        cy.get("@FidesUpdated").should("not.have.been.called");
-      });
-
-      it("Should not dismiss the overlay by clicking outside", () => {
-        cy.get("#fides-banner").should("be.visible");
-        cy.getByTestId("Manage preferences-btn").click();
-        cy.get(".fides-modal-content").should("be.visible");
-        cy.get(".fides-modal-overlay").click({ force: true });
-        cy.get(".fides-modal-content").should("be.visible");
-
-        cy.get("@FidesUpdated").should("not.have.been.called");
-      });
-
-      it("Should not dismiss the overlay by hitting ESC", () => {
-        cy.get("#fides-banner").should("be.visible");
-        cy.getByTestId("Manage preferences-btn").click();
-        cy.get(".fides-modal-content").should("be.visible");
-        cy.get("body").type("{esc}");
-        cy.get(".fides-modal-content").should("be.visible");
-
-        cy.get("@FidesUpdated").should("not.have.been.called");
-      });
+      }
     });
   });
 });
