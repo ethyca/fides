2a36f51dbf932ab91b5e3c8753b73e51a9818683
Neville Samuell <neville@ethyca.com>
2023-12-22 15:49:19 -0500

    Use dynamic interval

--- clients/admin-ui/src/pages/insights/index.tsx
+++ clients/admin-ui/src/pages/insights/index.tsx
@@ -95,20 +95,21 @@ const LABEL_REQUESTS_SECTION = "Privacy Requests";
 const LABEL_REQUESTS_TOTAL = "Total Privacy Requests";
 const LABEL_REQUESTS_BY_POLICY = "Privacy Requests by Policy";
 const LABEL_REQUESTS_BY_STATUS = "Privacy Requests by Status";
-const LABEL_REQUESTS_TIMESERIES = `${LABEL_INTERVAL} Privacy Requests`;
-const LABEL_REQUESTS_TIMESERIES_BY_POLICY = `${LABEL_INTERVAL} Privacy Requests by Policy`;
+const LABEL_REQUESTS_TIMESERIES = "Privacy Requests";
+const LABEL_REQUESTS_TIMESERIES_BY_POLICY = "Privacy Requests by Policy";
 
 const LABEL_PREFS_SECTION = "Consent Preferences";
 const LABEL_PREFS_TOTAL = "Total Preferences";
 const LABEL_PREFS_BY_NOTICE = "Preferences by Notice";
-const LABEL_PREFS_TIMESERIES = `${LABEL_INTERVAL} Preferences`;
+const LABEL_PREFS_TIMESERIES = "Preferences";
 const LABEL_PREFS_BY_PREFERENCE = "Preferences by Value";
-const LABEL_PREFS_TIMESERIES_BY_PREFERENCE = `${LABEL_INTERVAL} Preferences by Value`;
+const LABEL_PREFS_TIMESERIES_BY_PREFERENCE = "Preferences by Value";
 
 const InsightsPage: NextPage = () => {
 
     type DateRange = {
         label: string;
+        value: string;
         startDate: string;
         endDate: string;
     }
@@ -121,10 +122,10 @@ const InsightsPage: NextPage = () => {
     const prior90DaysDate = new Date(new Date().setDate(today.getDate() - 90)).toISOString()
     const prior180DaysDate = new Date(new Date().setDate(today.getDate() - 180)).toISOString()
     const prior365DaysDate = new Date(new Date().setDate(today.getDate() - 365)).toISOString()
-    dateRangeMap.set("30", {label: "Last 30 days", endDate: todayFormatted, startDate: prior30DaysDate})
-    dateRangeMap.set("90", {label: "Last 90 days", endDate: todayFormatted, startDate: prior90DaysDate})
-    dateRangeMap.set("180", {label: "Last 180 days", endDate: todayFormatted, startDate: prior180DaysDate})
-    dateRangeMap.set("365", {label: "Last year", endDate: todayFormatted, startDate: prior365DaysDate})
+    dateRangeMap.set("30", {label: "Last 30 days", value: "30", endDate: todayFormatted, startDate: prior30DaysDate})
+    dateRangeMap.set("90", {label: "Last 90 days", value: "90", endDate: todayFormatted, startDate: prior90DaysDate})
+    dateRangeMap.set("180", {label: "Last 180 days", value: "180", endDate: todayFormatted, startDate: prior180DaysDate})
+    dateRangeMap.set("365", {label: "Last year", value: "365", endDate: todayFormatted, startDate: prior365DaysDate})
 
     // options for the date selector
     const dateRangeOptions: Map<string, ItemOption> = new Map<string, ItemOption>();
@@ -137,6 +138,18 @@ const InsightsPage: NextPage = () => {
         dateRangeMap.get("180")
     );
 
+    const intervalOptions: Map<string, ItemOption> = new Map<string, ItemOption>([
+        ["Daily", { value: TimeInterval.days }],
+        ["Weekly", { value: TimeInterval.weeks }],
+        ["Monthly", { value: TimeInterval.months }],
+    ]);
+    const [interval, setInterval] = useState(TimeInterval.weeks);
+    const handleIntervalChange = (value?: string) => {
+        console.log("handleIntervalChange", value);
+        if (value) {
+            setInterval(value);
+        }
+    };
 
     const { data: privacyRequestByPolicy, isLoading: isPrivacyRequestByPolicyLoading } =
         useGetInsightsAggregateQuery({
@@ -155,7 +168,7 @@ const InsightsPage: NextPage = () => {
     const { data: privacyRequestByDay, isLoading: isPrivacyRequestByDayLoading } =
         useGetInsightsTimeSeriesQuery({
             record_type: RecordType.dsr,
-            time_interval: INTERVAL,
+            time_interval: interval,
             created_gt: dateRange.startDate,
             created_lt: dateRange.endDate,
         });
@@ -163,7 +176,7 @@ const InsightsPage: NextPage = () => {
         useGetInsightsTimeSeriesQuery({
             record_type: RecordType.dsr,
             group_by: GroupByOptions.dsr_policy,
-            time_interval: INTERVAL,
+            time_interval: interval,
             created_gt: dateRange.startDate,
             created_lt: dateRange.endDate,
         });
@@ -177,7 +190,7 @@ const InsightsPage: NextPage = () => {
     const { data: consentByDay, isLoading: isConsentByDayLoading } =
         useGetInsightsTimeSeriesQuery({
             record_type: RecordType.consent,
-            time_interval: INTERVAL,
+            time_interval: interval,
             created_gt: dateRange.startDate,
             created_lt: dateRange.endDate,
         });
@@ -191,7 +204,7 @@ const InsightsPage: NextPage = () => {
     const { data: consentByDayAndPreference, isLoading: isConsentByDaysAndPreferenceLoading } =
         useGetInsightsTimeSeriesQuery({
             record_type: RecordType.consent,
-            time_interval: INTERVAL,
+            time_interval: interval,
             group_by: GroupByOptions.preference,
             created_gt: dateRange.startDate,
             created_lt: dateRange.endDate,
@@ -353,12 +366,6 @@ const InsightsPage: NextPage = () => {
                 sizeref: 100,
                 cmin: 0,
                 cmax: 10000,
-                colorscale: 'Greens',
-                colorbar: {
-                    title: 'Consent Preferences',
-                    ticksuffix: '',
-                    showticksuffix: 'last'
-                },
                 line: {
                     color: 'black'
                 }
@@ -390,7 +397,14 @@ const InsightsPage: NextPage = () => {
         geo: {
             scope: 'usa',
             resolution: 100
-        }
+        },
+        title: {
+            text: "Preferences by US State",
+            font: {
+                family: "Inter",
+                size: 16,
+            }
+        },
     };
 
     const getBarChartPlotlyLayout = (title?: string): Partial<Plotly.Layout> => {
@@ -460,14 +474,26 @@ const InsightsPage: NextPage = () => {
                         <Box fontSize="sm" mr={1}>
                             Date Range:
                         </Box>
-                        <Box>
+                        <Box mr={2}>
                             <SelectDropdown
                                 label={dateRange.label}
                                 list={dateRangeOptions}
                                 hasClear={false}
                                 menuButtonProps={{  }}
                                 onChange={handleDateChange}
-                                selectedValue="Last 180 days"
+                                selectedValue={dateRange.value}
+                            />
+                        </Box>
+                        <Box fontSize="sm" mr={1}>
+                            Interval:
+                        </Box>
+                        <Box>
+                            <SelectDropdown
+                                list={intervalOptions}
+                                hasClear={false}
+                                menuButtonProps={{  }}
+                                onChange={handleIntervalChange}
+                                selectedValue={interval}
                             />
                         </Box>
                     </Flex>
