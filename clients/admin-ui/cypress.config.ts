e4917deb42c7f839077daddf3e2c52e5d93d7bc0
Allison King <allisonjuliaking@gmail.com>
2024-01-09 17:16:43 -0500

    Configure cypress to run with reduced motion preference

--- clients/admin-ui/cypress.config.ts
+++ clients/admin-ui/cypress.config.ts
@@ -4,6 +4,15 @@ import { defineConfig } from "cypress";
 export default defineConfig({
   e2e: {
     baseUrl: "http://localhost:3000",
+    setupNodeEvents(on) {
+      on("before:browser:launch", (browser, launchOptions) => {
+        if (browser.family === "chromium") {
+          // No need for tests to be slowed down by animations!
+          launchOptions.args.push("--force-prefers-reduced-motion");
+        }
+        return launchOptions;
+      });
+    },
   },
 
   defaultCommandTimeout: 5000,
