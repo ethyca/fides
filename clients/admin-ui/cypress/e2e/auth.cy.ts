c9ac6a2ba801d2d29cce9360cb58d2f20650895a
Allison King <allisonjuliaking@gmail.com>
2024-01-09 17:09:56 -0500

    Add login page tests

--- clients/admin-ui/cypress/e2e/auth.cy.ts
+++ clients/admin-ui/cypress/e2e/auth.cy.ts
@@ -1,6 +1,17 @@
 import { SYSTEM_ROUTE } from "~/features/common/nav/v2/routes";
 
 describe("User Authentication", () => {
+  const login = () => {
+    cy.fixture("login.json").then((body) => {
+      cy.intercept("POST", "/api/v1/login", body).as("postLogin");
+      cy.intercept("/api/v1/user/*/permission", {
+        fixture: "user-management/permissions.json",
+      }).as("getUserPermission");
+    });
+
+    cy.getByTestId("input-username").type("cypress-user@ethyca.com");
+    cy.getByTestId("input-password").type("FakePassword123!{Enter}");
+  };
   describe("when the user not logged in", () => {
     it("redirects them to the login page", () => {
       cy.visit("/");
@@ -20,17 +31,19 @@ describe("User Authentication", () => {
       cy.getByTestId("Login");
 
       cy.intercept("GET", "/api/v1/system", { body: [] });
-      cy.fixture("login.json").then((body) => {
-        cy.intercept("POST", "/api/v1/login", body).as("postLogin");
-        cy.intercept("/api/v1/user/*/permission", {
-          fixture: "user-management/permissions.json",
-        }).as("getUserPermission");
-      });
-
-      cy.getByTestId("input-username").type("cypress-user@ethyca.com");
-      cy.getByTestId("input-password").type("FakePassword123!{Enter}");
+      login();
 
       cy.getByTestId("Home");
+    });
+
+    it("can persist URL after logging in", () => {
+      cy.visit("/user-management");
+      cy.location("pathname").should("eq", "/login");
+      cy.location("search").should("eq", "?redirect=%2Fuser-management");
+
+      // Now log in
+      login();
+      cy.location("pathname").should("eq", "/user-management");
     });
   });
 
@@ -68,6 +81,38 @@ describe("User Authentication", () => {
     it("/login redirects to the Home page", () => {
       cy.visit("/login");
       cy.location("pathname").should("eq", "/");
+    });
+  });
+
+  describe("invited user", () => {
+    beforeEach(() => {
+      cy.intercept("/api/v1/user/*/permission", {
+        fixture: "user-management/permissions.json",
+      }).as("getUserPermission");
+      cy.fixture("login.json").then((body) => {
+        cy.intercept("POST", "/api/v1/user/accept-invite*", body).as(
+          "postAcceptInvite"
+        );
+      });
+    });
+    it("can prefill email and render different copy for an invited user", () => {
+      const data = { username: "testuser", invite_code: "123" };
+      const newPassword = "FakePassword123!";
+      cy.visit("/login", {
+        qs: data,
+      });
+      cy.getByTestId("input-username").should("be.disabled");
+      cy.getByTestId("input-username").should("have.value", data.username);
+      cy.get("label").contains("Set new password");
+      cy.getByTestId("input-password").type(newPassword);
+      cy.get("button").contains("Setup user").click();
+      cy.wait("@postAcceptInvite").then((interception) => {
+        const { body, url } = interception.request;
+        expect(url).to.contain(data.invite_code);
+        expect(url).to.contain(data.username);
+        expect(body).to.eql({ new_password: newPassword });
+      });
+      cy.getByTestId("Home");
     });
   });
 });
