3deb7a12ca9c0c103b8ca09b6bfacdc00c776d98
TheAndrewJackson <andrew.c.j1995@gmail.com>
2024-01-11 15:11:54 -0500

    update test to account for feature flag

--- clients/admin-ui/src/features/common/nav/v2/nav-config.test.ts
+++ clients/admin-ui/src/features/common/nav/v2/nav-config.test.ts
@@ -65,6 +65,9 @@ describe("configureNavGroups", () => {
     const navGroups = configureNavGroups({
       config: NAV_CONFIG,
       hasPlus: true,
+      flags: {
+        datamapReportingPage: true,
+      },
       userScopes: ALL_SCOPES,
     });
 
