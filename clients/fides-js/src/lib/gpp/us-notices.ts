9f420af0cde1e0a6eab3b06236a13377ecfe64db
Allison King <allisonjuliaking@gmail.com>
2024-01-16 16:25:14 -0500

    Update GPP Notice fields to come from privacy notice

--- clients/fides-js/src/lib/gpp/us-notices.ts
+++ clients/fides-js/src/lib/gpp/us-notices.ts
@@ -2,7 +2,7 @@
  * Helper functions to set the GPP CMP API based on Fides values
  */
 
-import { CmpApi } from "@iabgpp/cmpapi";
+import { CmpApi, UsNatV1, UsNatV1Field } from "@iabgpp/cmpapi";
 
 import {
   FIDES_REGION_TO_GPP_SECTION,
@@ -23,26 +23,66 @@ export const setGppNoticesProvidedFromExperience = ({
   experience: PrivacyExperience;
 }) => {
   const sectionsChanged = new Set<{ name: string; id: number }>();
-  const { privacy_notices: notices, region } = experience;
+  const {
+    privacy_notices: notices = [],
+    region,
+    gpp_settings: gppSettings,
+  } = experience;
+  const gppSection = FIDES_REGION_TO_GPP_SECTION[region];
 
-  Object.entries(NOTICE_KEY_TO_FIDES_REGION_GPP_FIELDS).forEach(
-    ([noticeKey, regionGppFields]) => {
-      const gppFields = regionGppFields[region];
-      if (gppFields) {
-        const { gpp_notice_fields: fields } = gppFields;
-        const experienceHasNotice = !!notices?.find(
-          (n) => n.notice_key === noticeKey
-        );
-        // 1 = Yes notice was provided, 2 = No, notice was not provided.
-        const value = experienceHasNotice ? 1 : 2;
-        const gppSection = FIDES_REGION_TO_GPP_SECTION[region];
-        sectionsChanged.add(gppSection);
-        fields.forEach((field) => {
-          cmpApi.setFieldValue(gppSection.name, field, value);
-        });
-      }
+  if (!gppSection) {
+    return [];
+  }
+
+  sectionsChanged.add(gppSection);
+
+  notices.forEach((notice) => {
+    const { gpp_field_mapping: fieldMapping } = notice;
+    const gppNotices = fieldMapping?.find((fm) => fm.region === region)?.notice;
+    if (gppNotices) {
+      gppNotices.forEach((gppNotice) => {
+        // 1 means notice was provided
+        cmpApi.setFieldValue(gppSection.name, gppNotice, 1);
+      });
     }
+  });
+
+  // Set MSPA
+  if (gppSettings) {
+    const mspaFields = [
+      {
+        gppSettingField: gppSettings.mspa_covered_transactions,
+        cmpApiField: UsNatV1Field.MSPA_COVERED_TRANSACTION,
+      },
+      {
+        gppSettingField: gppSettings.mspa_opt_out_option_mode,
+        cmpApiField: UsNatV1Field.MSPA_OPT_OUT_OPTION_MODE,
+      },
+      {
+        gppSettingField: gppSettings.mspa_service_provider_mode,
+        cmpApiField: UsNatV1Field.MSPA_SERVICE_PROVIDER_MODE,
+      },
+    ];
+    mspaFields.forEach(({ gppSettingField, cmpApiField }) => {
+      cmpApi.setFieldValue(
+        gppSection.name,
+        cmpApiField,
+        gppSettingField ? 1 : 2
+      );
+    });
+  }
+
+  // Set all other *Notice fields to 2
+  const section = cmpApi.getSection(gppSection.name);
+  const sectionNoticeKeys = Object.keys(section).filter((key) =>
+    key.endsWith("Notice")
   );
+  sectionNoticeKeys.forEach((sectionKey) => {
+    const curValue = cmpApi.getFieldValue(gppSection.name, sectionKey);
+    if (curValue !== 1) {
+      cmpApi.setFieldValue(gppSection.name, sectionKey, 2);
+    }
+  });
 
   return Array.from(sectionsChanged);
 };
