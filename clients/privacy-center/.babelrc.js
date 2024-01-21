6d7e2aee04efab44bda3739239dc52cf4a79c9d5
Neville Samuell <neville@ethyca.com>
2024-01-20 21:50:04 -0500

    Try only running admin-ui, does coverage work there in CI

--- clients/privacy-center/.babelrc.js
+++ clients/privacy-center/.babelrc.js
@@ -1,3 +1,4 @@
+console.log(`babelrc loading... process.env.NODE_ENV=${process.env.NODE_ENV}`);
 module.exports = {
   presets: ["next/babel"],
   plugins: process.env.NODE_ENV == "test" ? ["istanbul"] : [],
