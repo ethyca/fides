c9c619068e073b6e4fb42b5ad22d56863413df9f
Adam Sachs <adam@ethyca.com>
2024-01-24 13:00:03 -0500

    typing updates

--- src/fides/api/util/cors_middleware_utils.py
+++ src/fides/api/util/cors_middleware_utils.py
@@ -1,19 +1,23 @@
-from typing import Iterable, Optional
+from typing import Any, Iterable, Optional, Pattern
 
 from fastapi import FastAPI
+from fastapi.middleware import Middleware
 from fastapi.middleware.cors import CORSMiddleware
 
 
 def update_cors_middleware(
-    app: FastAPI, allow_origins: Iterable[str], allow_origin_regex: str
-) -> CORSMiddleware:
+    app: FastAPI,
+    allow_origins: Iterable[str],
+    allow_origin_regex: Optional[Pattern[Any]],
+) -> Optional[Middleware]:
     """
     Update the CORSMiddleware of the running app with the provided origin parameters.
 
     Returns the _old_ middleware instance that is no longer being used.
     """
     existing_middleware = find_cors_middleware(app)
-    app.user_middleware.remove(existing_middleware)
+    if existing_middleware:
+        app.user_middleware.remove(existing_middleware)
     app.add_middleware(
         CORSMiddleware,
         allow_origins=allow_origins,
@@ -25,7 +29,7 @@ def update_cors_middleware(
     return existing_middleware
 
 
-def find_cors_middleware(app: FastAPI) -> Optional[CORSMiddleware]:
+def find_cors_middleware(app: FastAPI) -> Optional[Middleware]:
     """Utility function to find the (first) cors middleware of the provided FastAPI app"""
     for mw in app.user_middleware:
         if mw.cls is CORSMiddleware:
