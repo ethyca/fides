3e16b6bf436605dc4597a3417c41bb3ea40630bd
Robert Keyser <robert@ethyca.com>
2024-02-02 13:13:51 -0600

    feat: add asserts to Talkable tests

--- tests/ops/integration_tests/saas/test_talkable_task.py
+++ tests/ops/integration_tests/saas/test_talkable_task.py
@@ -15,22 +15,11 @@ class TestTalkableConnector:
         access_results = await talkable_runner.access_request(
             access_policy=policy, identities={"email": talkable_identity_email}
         )
-
-    async def test_strict_erasure_request(
-        self,
-        talkable_runner: ConnectorRunner,
-        policy: Policy,
-        erasure_policy_string_rewrite: Policy,
-        talkable_erasure_identity_email: str,
-        talkable_erasure_data,
-    ):
-        (
-            access_results,
-            erasure_results,
-        ) = await talkable_runner.strict_erasure_request(
-            access_policy=policy,
-            erasure_policy=erasure_policy_string_rewrite,
-            identities={"email": talkable_erasure_identity_email},
+        
+        # verify we only returned data for our identity email
+        assert (
+            access_results["talkable_instance:person"][0]["email"]
+            == talkable_identity_email
         )
 
     async def test_non_strict_erasure_request(
@@ -49,3 +38,7 @@ class TestTalkableConnector:
             erasure_policy=erasure_policy_string_rewrite,
             identities={"email": talkable_erasure_identity_email},
         )
+
+        assert erasure_results == {
+            "talkable_instance:person": 1,
+        }
