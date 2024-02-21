76cc904651a6541290c8844456eec4d782fbd65b
eastandwestwind <eastandwestwind@gmail.com>
2024-02-21 15:10:18 -0500

    adds more types

---
+++ clients/privacy-center/types/api/models/GPPFieldMapping.ts
@@ -0,0 +1,16 @@
+/* istanbul ignore file */
+/* tslint:disable */
+/* eslint-disable */
+
+import type { GPPMechanismMapping } from './GPPMechanismMapping';
+import type { PrivacyNoticeRegion } from './PrivacyNoticeRegion';
+
+/**
+ * A base template for all other Fides Schemas to inherit from.
+ */
+export type GPPFieldMapping = {
+  region: PrivacyNoticeRegion;
+  notice?: Array<string>;
+  mechanism?: Array<GPPMechanismMapping>;
+};
+
