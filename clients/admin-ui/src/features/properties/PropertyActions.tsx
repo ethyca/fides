836637689868ad751c13554407d62c2a0f189e3b
Adrian Galvan <adrian@ethyca.com>
2024-03-07 14:38:58 -0800

    Updating UI permissions

--- clients/admin-ui/src/features/properties/PropertyActions.tsx
+++ clients/admin-ui/src/features/properties/PropertyActions.tsx
@@ -2,10 +2,10 @@ import { Box, EditIcon, IconButton } from "@fidesui/react";
 import { useRouter } from "next/router";
 import React from "react";
 
-import { Property } from "~/types/api";
-
-import { PROPERTIES_ROUTE } from "../common/nav/v2/routes";
-import NewJavaScriptTag from "../privacy-experience/NewJavaScriptTag";
+import { PROPERTIES_ROUTE } from "~/features/common/nav/v2/routes";
+import Restrict from "~/features/common/Restrict";
+import NewJavaScriptTag from "~/features/privacy-experience/NewJavaScriptTag";
+import { Property, ScopeRegistryEnum } from "~/types/api";
 
 interface Props {
   property: Property;
@@ -21,17 +21,19 @@ const PropertyActions = ({ property }: Props) => {
   return (
     <Box py={2}>
       <NewJavaScriptTag property={property} />
-      <IconButton
-        aria-label="Edit property"
-        variant="outline"
-        size="xs"
-        marginRight="10px"
-        icon={<EditIcon />}
-        onClick={(e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
-          e.stopPropagation();
-          handleEdit();
-        }}
-      />
+      <Restrict scopes={[ScopeRegistryEnum.PROPERTY_UPDATE]}>
+        <IconButton
+          aria-label="Edit property"
+          variant="outline"
+          size="xs"
+          marginRight="10px"
+          icon={<EditIcon />}
+          onClick={(e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+            e.stopPropagation();
+            handleEdit();
+          }}
+        />
+      </Restrict>
     </Box>
   );
 };
