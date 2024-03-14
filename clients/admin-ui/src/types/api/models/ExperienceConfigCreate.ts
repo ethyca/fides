4fcaea07fb4bd9258a95c48091f073af6af1ac61
Adrian Galvan <adrian@ethyca.com>
2024-03-14 11:34:09 -0700

    Misc fixes

--- clients/admin-ui/src/types/api/models/ExperienceConfigCreate.ts
+++ clients/admin-ui/src/types/api/models/ExperienceConfigCreate.ts
@@ -4,6 +4,7 @@
 
 import type { ComponentType } from "./ComponentType";
 import type { ExperienceTranslationCreate } from "./ExperienceTranslationCreate";
+import { MinimalProperty } from "./MinimalProperty";
 import type { PrivacyNoticeRegion } from "./PrivacyNoticeRegion";
 import type { Property } from "./Property";
 
@@ -22,5 +23,5 @@ export type ExperienceConfigCreate = {
   component: ComponentType;
   privacy_notice_ids?: Array<string>;
   translations?: Array<ExperienceTranslationCreate>;
-  properties?: Array<Property>;
+  properties?: Array<MinimalProperty>;
 };
