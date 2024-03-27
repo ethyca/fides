72062e55ec67e0583dcf3651e4f610c0ef9c91e5
Jeremy Pople <jeremy@ethyca.com>
2024-03-27 10:58:13 -0400

    fix build failure

--- clients/admin-ui/src/features/properties/PropertyForm.tsx
+++ clients/admin-ui/src/features/properties/PropertyForm.tsx
@@ -58,6 +58,7 @@ const ExperiencesFormSection = () => {
         values={values.experiences ?? []}
         setValues={(newValues) => setFieldValue("experiences", newValues)}
         draggable
+        baseTestId="experience"
       />
     </FormSection>
   );
