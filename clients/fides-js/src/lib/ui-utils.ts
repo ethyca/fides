1808214982bedb03b978ab69aeaf7f58245f5b7e
Lucano Vera <lucanovera@ethyca.com>
2024-03-27 12:00:04 -0300

    Formatting

--- clients/fides-js/src/lib/ui-utils.ts
+++ clients/fides-js/src/lib/ui-utils.ts
@@ -1,6 +1,6 @@
 export const blockPageScrolling = () => {
-    document.body.style.overflowY = "hidden";
-}
+  document.body.style.overflowY = "hidden";
+};
 export const unblockPageScrolling = () => {
-    document.body.style.overflowY = "auto";
-}
+  document.body.style.overflowY = "auto";
+};
