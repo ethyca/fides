2ae66d8ae2c4c9efc5c112a3a965f6b0f343c0fc
Marc Guimond <marc@ethyca.com>
2024-04-02 10:28:32 -0400

    in progress

--- tests/fixtures/saas/openweb_fixtures.py
+++ tests/fixtures/saas/openweb_fixtures.py
@@ -16,31 +16,33 @@ secrets = get_secrets("openweb")
 def openweb_secrets(saas_config) -> Dict[str, Any]:
     return {
         "domain": pydash.get(saas_config, "openweb.domain")
-        or secrets["domain"]
+        or secrets["domain"],
+        "api_key": pydash.get(saas_config, "openweb.api_key")
+        or secrets["api_key"]
         # add the rest of your secrets here
     }
 
 
-@pytest.fixture(scope="session")
-def openweb_identity_email(saas_config) -> str:
-    return (
-        pydash.get(saas_config, "openweb.identity_email") or secrets["identity_email"]
-    )
+# @pytest.fixture(scope="session")
+# def openweb_identity_email(saas_config) -> str:
+#     return (
+#         pydash.get(saas_config, "openweb.identity_email") or secrets["identity_email"]
+#     )
 
 
 @pytest.fixture
 def openweb_erasure_identity_email() -> str:
     return generate_random_email()
 
-
+# we do need a means of creating a random 'primay_key' and using that for the erasure request. THere is a difference in how the endpoint responds when sent an invalid (or already used) primary_key. A saved 
 @pytest.fixture
 def openweb_external_references() -> Dict[str, Any]:
-    return {}
+    return {"openweb_user_id": "deletemetest"}
 
 
 @pytest.fixture
 def openweb_erasure_external_references() -> Dict[str, Any]:
-    return {}
+    return {"openweb_user_id": "deletemetest"}
 
 
 @pytest.fixture
--- tests/ops/integration_tests/saas/test_openweb_task.py
+++ tests/ops/integration_tests/saas/test_openweb_task.py
@@ -16,23 +16,6 @@ class TestOpenWebConnector:
             access_policy=policy, identities={"email": openweb_identity_email}
         )
 
-    async def test_strict_erasure_request(
-        self,
-        openweb_runner: ConnectorRunner,
-        policy: Policy,
-        erasure_policy_string_rewrite: Policy,
-        openweb_erasure_identity_email: str,
-        openweb_erasure_data,
-    ):
-        (
-            access_results,
-            erasure_results,
-        ) = await openweb_runner.strict_erasure_request(
-            access_policy=policy,
-            erasure_policy=erasure_policy_string_rewrite,
-            identities={"email": openweb_erasure_identity_email},
-        )
-
     async def test_non_strict_erasure_request(
         self,
         openweb_runner: ConnectorRunner,
