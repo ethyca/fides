d7494e379abd3aa2db72e8ec3a5a752caf3a3ebd
Marc Guimond <marc@ethyca.com>
2024-04-04 10:50:44 -0400

    Marc's instantiy

--- tests/ops/integration_tests/saas/test_openweb_task.py
+++ tests/ops/integration_tests/saas/test_openweb_task.py
@@ -8,6 +8,7 @@ from tests.ops.integration_tests.saas.connector_runner import ConnectorRunner
 class TestOpenWebConnector:
     def test_connection(self, openweb_runner: ConnectorRunner):
         openweb_runner.test_connection()
+        
 
     # async def test_access_request(
     #     self, openweb_runner: ConnectorRunner, policy, openweb_identity_email: str
@@ -16,20 +17,21 @@ class TestOpenWebConnector:
     #         access_policy=policy, identities={"email": openweb_identity_email}
     #     )
 
-    async def test_non_strict_erasure_request(
-        self,
-        openweb_runner: ConnectorRunner,
-        policy: Policy,
-        erasure_policy_string_rewrite: Policy,
-        openweb_erasure_identity_email: str,
-    ):
-        (
-            # access_results,
-            _,
-            erasure_results,
-        ) = await openweb_runner.non_strict_erasure_request(
-            access_policy=policy,
-            erasure_policy=erasure_policy_string_rewrite,
-            # identities={"email": openweb_erasure_identity_email},
-        )
-        assert erasure_results == {"openweb_instance:user": 1}
+    # async def test_non_strict_erasure_request(
+    #     self,
+    #     openweb_runner: ConnectorRunner,
+    #     policy: Policy,
+
+    #     erasure_policy_string_rewrite: Policy,
+    #     openweb_erasure_identity_email: str,
+    # ):
+    #     (
+    #         # access_results,
+    #         _,
+    #         erasure_results,
+    #     ) = await openweb_runner.non_strict_erasure_request(
+    #         access_policy=policy,
+    #         erasure_policy=erasure_policy_string_rewrite,
+    #         # identities={"email": openweb_erasure_identity_email},
+    #     )
+    #     assert erasure_results == {"openweb_instance:user": 1}
