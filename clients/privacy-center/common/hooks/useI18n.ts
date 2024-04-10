898070a81aad2c64a877ab3e68606b823a77f43f
Lucano Vera <lucanovera@ethyca.com>
2024-04-10 19:35:54 -0300

    PROD-1748 fix bug

--- clients/privacy-center/common/hooks/useI18n.ts
+++ clients/privacy-center/common/hooks/useI18n.ts
@@ -1,4 +1,4 @@
-import { useContext } from "react";
+import { useCallback, useContext } from "react";
 import {
   ExperienceConfig,
   PrivacyNotice,
@@ -15,41 +15,43 @@ const useI18n = () => {
     throw new Error("useI18n must be used within a I18nProvider");
   }
 
-  const getPrivacyExperienceConfigHistoryId = (
-    experienceConfig: ExperienceConfig
-  ) => {
-    const experienceConfigTransalation = selectBestExperienceConfigTranslation(
-      i18n,
-      experienceConfig
-    );
-    if (!experienceConfigTransalation) {
-      throw new Error(
-        "Coudln't find correct privacy experience config history id to save consent"
+  const getPrivacyExperienceConfigHistoryId = useCallback(
+    (experienceConfig: ExperienceConfig) => {
+      const experienceConfigTransalation =
+        selectBestExperienceConfigTranslation(i18n, experienceConfig);
+      if (!experienceConfigTransalation) {
+        throw new Error(
+          "Coudln't find correct privacy experience config history id to save consent"
+        );
+      }
+
+      const privacyExperienceConfigHistoryId =
+        experienceConfigTransalation.privacy_experience_config_history_id;
+
+      return privacyExperienceConfigHistoryId;
+    },
+    []
+  );
+
+  const getPrivacyExperienceNoticeHistoryId = useCallback(
+    (notice: PrivacyNotice) => {
+      const selectedNotice = selectBestNoticeTranslation(
+        i18n,
+        notice as PrivacyNotice
       );
-    }
+      if (!selectedNotice) {
+        throw new Error(
+          "Coudln't find correct privacy experience notice history id to save consent"
+        );
+      }
 
-    const privacyExperienceConfigHistoryId =
-      experienceConfigTransalation.privacy_experience_config_history_id;
+      const privacyExperienceConfigNoticeId =
+        selectedNotice?.privacy_notice_history_id;
 
-    return privacyExperienceConfigHistoryId;
-  };
-
-  const getPrivacyExperienceNoticeHistoryId = (notice: PrivacyNotice) => {
-    const selectedNotice = selectBestNoticeTranslation(
-      i18n,
-      notice as PrivacyNotice
-    );
-    if (!selectedNotice) {
-      throw new Error(
-        "Coudln't find correct privacy experience notice history id to save consent"
-      );
-    }
-
-    const privacyExperienceConfigNoticeId =
-      selectedNotice?.privacy_notice_history_id;
-
-    return privacyExperienceConfigNoticeId;
-  };
+      return privacyExperienceConfigNoticeId;
+    },
+    []
+  );
 
   return {
     ...context,
