bb71443e3dbf23ea9367bf4b7e6b71e61b448a14
Lucano Vera <lucanovera@ethyca.com>
2024-04-11 12:49:52 -0300

    PROD-1952 Add new data detection and discovery beta flags and pages

---
+++ clients/admin-ui/src/pages/data-discovery/discovery.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import FixedLayout from "~/features/common/FixedLayout";
+
+const DataDiscoveryPage = () => (
+  <FixedLayout
+    title="Data discovery "
+    mainProps={{
+      padding: "40px",
+      paddingRight: "48px",
+    }}
+  >
+    <h1>test2</h1>
+  </FixedLayout>
+);
+
+export default DataDiscoveryPage;
