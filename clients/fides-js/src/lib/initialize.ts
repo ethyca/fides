526558bdfdac184b376c556a8b6b4e507c0f9b35
Jason Gill <jason.gill@ethyca.com>
2024-04-15 17:16:18 -0600

    address race condition using setTimeout
    also clean up the `updateExperience` function for easier to read code and consistency with fides-tcf.ts

--- clients/fides-js/src/lib/initialize.ts
+++ clients/fides-js/src/lib/initialize.ts
@@ -457,20 +457,20 @@ export const initialize = async ({
          * set any applicable notices to "opt-out" unless the user has previously
          * saved consent, etc.
          *
-         * NOTE: Do *not* await the results of this function, even though it's
-         * async and returns a Promise! Instead, let the GPC update run
-         * asynchronously but continue our initialization. If GPC applies, this
-         * will kick off an update to the user's consent preferences which will
-         * also call the Fides API, but we want to finish initialization
-         * immediately while those API updates happen in parallel.
+         * NOTE: We want to finish initialization immediately while GPC updates
+         * run in parallel. To ensure that any GPC API calls don't block the rest
+         * of the code from executing, we use setTimeout with no delay which
+         * simply moves it to the end of the JavaScript event queue.
          */
-        automaticallyApplyGPCPreferences({
-          savedConsent,
-          effectiveExperience,
-          cookie,
-          fidesRegionString,
-          fidesOptions: options,
-          i18n,
+        setTimeout(() => {
+          automaticallyApplyGPCPreferences({
+            savedConsent,
+            effectiveExperience: effectiveExperience as PrivacyExperience,
+            cookie,
+            fidesRegionString,
+            fidesOptions: options,
+            i18n,
+          });
         });
       }
     }
