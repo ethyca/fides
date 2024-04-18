ae37f1b9521dddd0c591f197c9278b917c77b4bd
Robert Keyser <robert@ethyca.com>
2024-04-18 08:58:32 -0500

    feat: remove bulky Celery banner and replace with a single line
    Derived from https://blog.sneawo.com/blog/2021/06/17/how-to-replace-celery-banner-with-one-log-line/

--- src/fides/api/worker/__init__.py
+++ src/fides/api/worker/__init__.py
@@ -1,3 +1,8 @@
+import json
+
+from celery import VERSION_BANNER
+from celery.apps.worker import Worker
+from celery.signals import celeryd_after_setup
 from loguru import logger
 
 from fides.api.service.saas_request.override_implementations import *
@@ -9,12 +14,31 @@ def start_worker() -> None:
     default_queue_name = celery_app.conf.get("task_default_queue", "celery")
     celery_app.worker_main(
         argv=[
+            "--quiet",  # Disable Celery startup banner
             "worker",
             "--loglevel=info",
             "--concurrency=2",
             f"--queues={default_queue_name},{MESSAGING_QUEUE_NAME}",
         ]
     )
+
+
+@celeryd_after_setup.connect
+def log_celery_setup(sender: str, instance: Worker, **kwargs) -> None:
+    """In lieu of the Celery banner, print the connection details"""
+    app = instance.app
+    concurrency = str(instance.concurrency)
+    celery_details = {
+        "hostname": instance.hostname,
+        "version": VERSION_BANNER,
+        "app": "{0}:{1:#x}".format(app.main or "__main__", id(app)),
+        "transport": app.connection().as_uri(),
+        "results": app.backend.as_uri(),
+        "concurrency": concurrency,
+        "queues": "|".join(str(queue) for queue in app.amqp.queues.keys()),
+    }
+
+    logger.info(f"Celery connection details | {json.dumps(celery_details)}")
 
 
 if __name__ == "__main__":  # pragma: no cover
