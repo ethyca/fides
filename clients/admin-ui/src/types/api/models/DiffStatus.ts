327614c43064d28b327b4995ee16ce9149cc4e94
Lucano Vera <lucanovera@ethyca.com>
2024-04-19 14:33:54 -0400

    PROD-1952 Update models with openapi generate

---
+++ clients/admin-ui/src/types/api/models/DiffStatus.ts
@@ -0,0 +1,12 @@
+/* istanbul ignore file */
+/* tslint:disable */
+/* eslint-disable */
+
+/**
+ * An enumeration.
+ */
+export enum DiffStatus {
+  ADDITION = "Addition",
+  REMOVAL = "Removal",
+  MODIFICATION = "Modification",
+}
