f59d954d0c52801ea712f971eb724fd097ecbafc
Jason Gill <jason.gill@ethyca.com>
2024-04-22 10:46:32 -0600

    add documentation

--- clients/fides-js/src/lib/consent-types.ts
+++ clients/fides-js/src/lib/consent-types.ts
@@ -121,7 +121,7 @@ export interface FidesInitOptions {
   // Shows fides.js overlay UI on load deleting the fides_consent cookie as if no preferences have been saved
   fidesClearCookie: boolean;
 
-  // Whether the developer forced the inclusion of the GPP extension
+  // Whether the developer forced the inclusion of the GPP extension via query param on the script tag
   forceGpp: boolean;
 }
 
