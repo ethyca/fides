c5646dfeae53cce5c4e0c39291f51d7eee5f1006
Adam Sachs <adam@ethyca.com>
2024-04-28 17:43:18 -0400

    try fixing lint error

--- clients/admin-ui/src/features/data-discovery-and-detection/TaxonomyDisplayAndEdit.tsx
+++ clients/admin-ui/src/features/data-discovery-and-detection/TaxonomyDisplayAndEdit.tsx
@@ -40,7 +40,7 @@ const TaxonomyDisplayAndEdit: React.FC<TaxonomyDisplayAndEditProps> = ({
       : fidesLangKey
   );
 
-  const handleCategoryChange = ({ value }) => {
+  const handleCategoryChange = ({ value: string }) => {
     updateResourceCategoryMutation({
       staged_resource_urn: resource.urn,
       monitor_config_id: resource.monitor_config_id,
