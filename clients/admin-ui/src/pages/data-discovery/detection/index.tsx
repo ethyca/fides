7ecc4350908b80d57d178ad4d689f89080b1edc7
Lucano Vera <lucanovera@ethyca.com>
2024-04-29 13:46:37 -0300

    Prod-1962 add actions to detection activity page

--- clients/admin-ui/src/pages/data-discovery/detection/index.tsx
+++ clients/admin-ui/src/pages/data-discovery/detection/index.tsx
@@ -3,22 +3,34 @@ import React from "react";
 import FixedLayout from "~/features/common/FixedLayout";
 import { DATA_DETECTION_ROUTE } from "~/features/common/nav/v2/routes";
 import DiscoveryMonitorBreadcrumbs from "~/features/data-discovery-and-detection/DiscoveryMonitorBreadcrumbs";
-import DetectionResultTable from "~/features/data-discovery-and-detection/tables/DetectionResultTable";
+import useDiscoveryRoutes from "~/features/data-discovery-and-detection/hooks/useDiscoveryRoutes";
+import ActivityTable from "~/features/data-discovery-and-detection/tables/ActivityTable";
+import { DiffStatus } from "~/types/api";
 
-const DataDetectionActivityPage = () => (
-  <FixedLayout
-    title="Data detection"
-    mainProps={{
-      padding: "40px",
-      paddingRight: "48px",
-    }}
-  >
-    <DiscoveryMonitorBreadcrumbs
-      parentTitle="Data detection"
-      parentLink={DATA_DETECTION_ROUTE}
-    />
-    <DetectionResultTable />
-  </FixedLayout>
-);
+const DataDetectionActivityPage = () => {
+  const { navigateToDetectionResults } = useDiscoveryRoutes();
+
+  return (
+    <FixedLayout
+      title="Data detection"
+      mainProps={{
+        padding: "40px",
+        paddingRight: "48px",
+      }}
+    >
+      <DiscoveryMonitorBreadcrumbs
+        parentTitle="Data detection"
+        parentLink={DATA_DETECTION_ROUTE}
+      />
+      <ActivityTable
+        onRowClick={(row) =>
+          navigateToDetectionResults({ resourceUrn: row.urn })
+        }
+        statusFilters={[DiffStatus.ADDITION, DiffStatus.REMOVAL]}
+        childsStatusFilters={[DiffStatus.ADDITION, DiffStatus.REMOVAL]}
+      />
+    </FixedLayout>
+  );
+};
 
 export default DataDetectionActivityPage;
