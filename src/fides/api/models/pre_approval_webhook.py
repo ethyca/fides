5b4983db3d3ecebe902af35a354d44e58f4c7a19
Dawn Pattison <pattisdr@users.noreply.github.com>
2024-04-29 09:14:33 -0500

    Sync up Database and PreApprovalWebhook/PreApprovalWebhookReply Models (#4838)

--- src/fides/api/models/pre_approval_webhook.py
+++ src/fides/api/models/pre_approval_webhook.py
@@ -1,14 +1,23 @@
-from sqlalchemy import Boolean, Column, ForeignKey, String
+from sqlalchemy import Boolean, Column, ForeignKey, String, UniqueConstraint
 from sqlalchemy.orm import Session, relationship  # type: ignore
 
 from fides.api.db.base_class import Base
 
 
 class PreApprovalWebhook(Base):
+    """
+    An HTTPS callback that calls an external REST API endpoint as soon as a privacy request
+    is received (or after user identity verification, if applicable).
+    """
+
     name = Column(String, unique=True, nullable=False)
     key = Column(String, index=True, unique=True, nullable=False)
-    connection_config_id = Column(
-        String, ForeignKey("connectionconfig.id"), nullable=False
+    connection_config_id = (
+        Column(  # Deleting a Connection Config also deletes the Pre Approval Webhook
+            String,
+            ForeignKey("connectionconfig.id", ondelete="CASCADE"),
+            nullable=False,
+        )
     )
     connection_config = relationship(
         "ConnectionConfig", back_populates="pre_approval_webhooks", uselist=False  # type: ignore
@@ -24,13 +33,26 @@ class PreApprovalWebhook(Base):
 
 
 class PreApprovalWebhookReply(Base):
+    """
+    Stores the callback response to the PreApprovalWebhook to determine if the Privacy
+    Request is eligible to be automatically approved
+    """
+
     webhook_id = Column(
-        String, ForeignKey(PreApprovalWebhook.id_field_path), nullable=False
+        String,
+        ForeignKey(PreApprovalWebhook.id_field_path, ondelete="SET NULL"), index=True
     )
     privacy_request_id = Column(
         String, ForeignKey("privacyrequest.id", ondelete="SET NULL"), index=True
     )  # Which privacy request this webhook response belongs to
     is_eligible = Column(Boolean, nullable=False)
+
     privacy_request = relationship(
         "PrivacyRequest", back_populates="pre_approval_webhook_replies", uselist=False  # type: ignore
+    )
+
+    __table_args__ = (
+        UniqueConstraint(
+            "webhook_id", "privacy_request_id", name="webhook_id_privacy_request_id"
+        ),
     )
