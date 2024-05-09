d5d4b3ebfaa1705bd36f66e65c353e983951735b
Lucano Vera <lucanovera@ethyca.com>
2024-05-09 17:12:54 -0300

    PROD-1952 Improve code based on CR

--- clients/admin-ui/src/features/data-discovery-and-detection/utils/findProjectFromUrn.ts
+++ clients/admin-ui/src/features/data-discovery-and-detection/utils/findProjectFromUrn.ts
@@ -1,5 +1,13 @@
+/**
+ * findProjectFromUrn returns the 'project' part of the resource urn
+ * @param urn urn string for a resource
+ * @returns project part of the urn
+ * urns structure for reference: ${monitorId}.${projectName}.${datasetName}[...]
+ */
 const findProjectFromUrn = (urn: string) => {
   const urnParts = urn.split(".");
+
+  // Get the [1] element of the array. The first part is the monitor id. The second part is the project.
   if (urnParts.length > 1) {
     return urnParts[1];
   }
