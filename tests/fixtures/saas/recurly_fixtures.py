0cfe8e0890964c52bc807247888da2041b8f6ccb
Marc Guimond <marc@ethyca.com>
2024-05-15 10:39:04 -0400

    starting testing configuration

--- tests/fixtures/saas/recurly_fixtures.py
+++ tests/fixtures/saas/recurly_fixtures.py
@@ -1,9 +1,12 @@
-from typing import Any, Dict
+from typing import Any, Dict, Generator
 
 import pydash
 import pytest
 
-from tests.ops.integration_tests.saas.connector_runner import ConnectorRunner
+from tests.ops.integration_tests.saas.connector_runner import (
+    ConnectorRunner,
+    generate_random_email,
+)
 from tests.ops.test_helpers.vault_client import get_secrets
 
 secrets = get_secrets("recurly")
@@ -23,6 +26,16 @@ def recurly_identity_email(saas_config) -> str:
         pydash.get(saas_config, "recurly.identity_email") or secrets["identity_email"]
     )
 
+@pytest.fixture
+def testing_erasure_identity_email() -> str:
+    return generate_random_email()
+
+@pytest.fixture
+def testing_erasure_data(
+    testing_erasure_identity_email: str,
+) -> Generator:
+    # create the data needed for erasure tests here
+    yield {}
 
 @pytest.fixture
 def recurly_runner(db, cache, recurly_secrets) -> ConnectorRunner:
