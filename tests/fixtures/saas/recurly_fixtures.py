fff1c44ce36af97d5ee54ac7ba3ebc93dfb6792f
Marc Guimond <marc@ethyca.com>
2024-05-16 15:04:16 -0400

    Internal testing passing and verified

--- tests/fixtures/saas/recurly_fixtures.py
+++ tests/fixtures/saas/recurly_fixtures.py
@@ -96,6 +96,7 @@ def recurly_erasure_data(
     # part of the response to the request to create a new account is the account_id
     account_id = response.json()["shipping_addresses"][0]["account_id"]
     shipping_id = response.json()["shipping_addresses"][0]["id"]
+    accounts = response.json()['username']
     # now we can form up a request to add billing details
     body = {
         "first_name": "first bill",
@@ -115,6 +116,7 @@ def recurly_erasure_data(
     response = requests.put(billing_url, headers=headers, json=body)
     assert response.ok
     # if we get this far we've got our new account with billing and shipping info
+    billing_info = response.json()['first_name']
     # I'll need 3 here, one for account, shipping addresses and billing info
 
     check_url = f"{accounts_url}/{account_id}"
@@ -144,7 +146,15 @@ def recurly_erasure_data(
     mask_account_url = f"{accounts_url}/{account_id}"
     response = requests.put(mask_account_url, headers=headers, json=body)
     assert response.ok
-    # import pdb; pdb.set_trace()
+
+    assert response.json()['first_name'] == 'MASKED', "Expected 'MASKED' for first_name, got {}".format(response.json()['first_name'])
+    assert response.json()['last_name'] == 'MASKED', "Expected 'MASKED' for last_name, got {}".format(response.json()['last_name'])
+    assert response.json()['email'] == 'MASKED@Ethyca.com', "Expected 'MASKED@Ethyca.com' for email, got {}".format(response.json()['email'])
+    assert response.json()['address']['street1'] == 'MASKED', "Expected 'MASKED' for street1, got {}".format(response.json()['address']['street1'])
+    assert response.json()['address']['street2'] == 'MASKED', "Expected 'MASKED' for street2, got {}".format(response.json()['address']['street2'])
+    assert response.json()['address']['city'] == 'MASKED', "Expected 'MASKED' for city, got {}".format(response.json()['address']['city'])
+    assert response.json()['address']['region'] == 'MASKED', "Expected 'MASKED' for region, got {}".format()(response.json()['address']['region'])
+    assert response.json()['address']['phone'] == 'MASKED', "Expected 'MASKED' for phone, got {}".format()(response.json()['address']['phone'])
     ## repeat for shipping and billing
     # billing
     body = {
@@ -158,29 +168,47 @@ def recurly_erasure_data(
             "region": "string",
             "postal_code": "3446",
             "country": "IN",
-            "phone": "string",
+            "phone": "MASKED",
         }
     }
     mask_billing_url = f"{accounts_url}/{account_id}/billing_info"
     response = requests.put(mask_billing_url, headers=headers, json=body)
     assert response.ok
 
+    assert response.json()['first_name'] == 'MASKED', "Expected 'MASKED' for first_name, got {}".format(response.json()['first_name'])
+    assert response.json()['last_name'] == 'MASKED', "Expected 'MASKED' for last_name, got {}".format(response.json()['last_name'])
+    assert response.json()['payment_method']['first_six'] == '411111', "Expected '411111' for first_six, got {}".format(response.json()['first_six'])
+    assert response.json()['payment_method']['last_four'] == '1111', "Expected '1111' for last_four, got {}".format(response.json()['last_four'])
+    assert response.json()['address']['phone'] == 'MASKED', "Expected 'MASKED' for phone, got {}".format(response.json()['address']['phone'])
+    assert response.json()['address']['street1'] == 'MASKED', "Expected 'MASKED' for street1, got {}".format(response.json()['address']['street1'])
+    assert response.json()['address']['street2'] == 'MASKED', "Expected 'MASKED' for street2, got {}".format(response.json()['address']['street2'])
+    assert response.json()['address']['city'] == 'MASKED', "Expected 'MASKED' for city, got {}".format(response.json()['address']['city'])
+## good to here
+
     # shipping
     body = {
         "first_name": "MASKED",
         "last_name": "MASKED",
-        "email": "MASKED",
+        "email": "MASKED@Ethyca.com",
         "phone": "MASKED",
         "street1": "MASKED",
         "street2": "MASKED",
     }
     mask_shipping_url = f"{accounts_url}/{account_id}/shipping_addresses/{shipping_id}"
     response = requests.put(mask_shipping_url, headers=headers, json=body)
+    shipping_address = response.json()['first_name']
+    # verification that the values we expected to be masked were masked.
+    assert response.json()['first_name'] == 'MASKED', "Expected 'MASKED' for first_name, got {}".format(response.json()['first_name'])
+    assert response.json()['last_name'] == 'MASKED', "Expected 'MASKED' for last_name, got {}".format(response.json()['last_name'])
+    assert response.json()['phone'] == 'MASKED', "Expected 'MASKED' for phone, got {}".format(response.json()['phone'])
+    assert response.json()['street1'] == 'MASKED', "Expected 'MASKED' for street1, got {}".format(response.json()['street1'])
+    assert response.json()['street2'] == 'MASKED', "Expected 'MASKED' for street2, got {}".format(response.json()['street2'])
+    assert response.json()['email'] == 'MASKED@Ethyca.com', "Expected 'MASKED' for email, got {}".format(response.json()['email'])
 
-    import pdb; pdb.set_trace()
 
+    # import pdb; pdb.set_trace()
 
-    # yield {}
+    yield accounts
 
 @pytest.fixture
 def recurly_runner(db, cache, recurly_secrets) -> ConnectorRunner:
