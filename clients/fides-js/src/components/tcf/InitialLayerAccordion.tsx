704dd550b248deda763960fa4be6efe3e13c8b46
Kelsey Thomas <101993653+Kelsey-Ethyca@users.noreply.github.com>
2024-05-21 17:39:15 -0700

    lint fix

--- clients/fides-js/src/components/tcf/InitialLayerAccordion.tsx
+++ clients/fides-js/src/components/tcf/InitialLayerAccordion.tsx
@@ -6,15 +6,6 @@ import {
   TCFPurposeLegitimateInterestsRecord,
 } from "../../lib/tcf/types";
 
-const ArrowDown = () => (
-  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none">
-    <path
-      fill="#2D3748"
-      d="m12 13.172 4.95-4.95 1.414 1.414L12 16 5.636 9.636 7.05 8.222l4.95 4.95Z"
-    />
-  </svg>
-);
-
 const InitialLayerAccordion = ({
   i18n,
   title,
