463ea2e43fee54cb31a540859206d544f50a82d2
Lucano Vera <lucanovera@live.com.ar>
2024-05-24 13:47:01 -0300

    PROD-1983 Privacy Center custom paths for properties (#4907)
    Co-authored-by: Lucano Vera <lucanovera@ethyca.com>

--- clients/privacy-center/__tests__/app/server-environment.test.ts
+++ clients/privacy-center/__tests__/app/server-environment.test.ts
@@ -1,6 +1,9 @@
 import { produce } from "immer";
 
-import { validateConfig } from "~/app/server-environment";
+import {
+  loadPrivacyCenterEnvironment,
+  validateConfig,
+} from "~/app/server-environment";
 import minimalJson from "~/config/examples/minimal.json";
 import fullJson from "~/config/examples/full.json";
 import v2ConsentJson from "~/config/examples/v2Consent.json";
@@ -68,5 +71,78 @@ describe("validateConfig", () => {
     test(tc.name, () => {
       expect(validateConfig(tc.config)).toMatchObject(tc.expected);
     });
+  });
+});
+
+const mockAsServerSide = () => {
+  jest
+    .spyOn(globalThis, "window", "get")
+    .mockImplementation(() => undefined as any);
+};
+
+const mockAsClientSide = () => {
+  jest.spyOn(globalThis, "window", "get").mockImplementation(() => ({} as any));
+};
+
+const getPropertyFromUrlMock = jest.fn();
+const loadEnvironmentVariablesMock = jest.fn();
+
+jest.mock(
+  "~/app/server-utils/getPropertyFromUrl",
+  () => (arg: any) => getPropertyFromUrlMock(arg)
+);
+
+jest.mock(
+  "~/app/server-utils/loadEnvironmentVariables",
+  () => (arg: any) => loadEnvironmentVariablesMock(arg)
+);
+
+describe("loadPrivacyCenterEnvironment", () => {
+  beforeEach(() => {
+    mockAsServerSide();
+    loadEnvironmentVariablesMock.mockReturnValue({
+      CUSTOM_PROPERTIES: true,
+    });
+  });
+
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it("throws when trying to run in window environment", () => {
+    mockAsClientSide();
+    expect(() => loadPrivacyCenterEnvironment({})).rejects.toThrow();
+  });
+
+  it("doesn't fetch properties when env variable CUSTOM_PROPERTIES is false", () => {
+    loadEnvironmentVariablesMock.mockReturnValue({
+      CUSTOM_PROPERTIES: false,
+    });
+
+    loadPrivacyCenterEnvironment({ customPropertyPath: "mycustompath" });
+    expect(getPropertyFromUrlMock).toHaveBeenCalledTimes(0);
+  });
+
+  it("calls getPropertyFromUrl with the correct path when called with a path", () => {
+    loadPrivacyCenterEnvironment({ customPropertyPath: "mycustompath" });
+    expect(getPropertyFromUrlMock).toHaveBeenCalledTimes(1);
+    expect(getPropertyFromUrlMock).toHaveBeenCalledWith({
+      customPropertyPath: "mycustompath",
+    });
+  });
+
+  it("returns property config and stylesheet if a property was found by it's path", async () => {
+    getPropertyFromUrlMock.mockReturnValue({
+      privacy_center_config: { some: "config" },
+      stylesheet: "some stylesheet",
+    });
+
+    const result = await loadPrivacyCenterEnvironment({
+      customPropertyPath: "mycustompath",
+    });
+    expect(getPropertyFromUrlMock).toHaveBeenCalledTimes(1);
+
+    expect(result.config).toEqual({ some: "config" });
+    expect(result.styles).toEqual("some stylesheet");
   });
 });
