7f5c71224db6459b1392efe106ff514b8db123e6
Jason Gill <jason.gill@ethyca.com>
2024-05-30 09:31:48 -0600

    fix loop logic to support localhost

--- clients/fides-js/src/lib/cookie.ts
+++ clients/fides-js/src/lib/cookie.ts
@@ -218,7 +218,7 @@ export const saveFidesCookie = (
 
   const hostname = window.location.hostname.split(".");
   let topViableDomain = "";
-  for (let i = 1; i < hostname.length; i += 1) {
+  for (let i = 1; i < hostname.length + 1; i += 1) {
     // This loop guarantees to get the top-level hostname because that's the smallest one browsers will let you set cookies in. We test a given suffix for whether we are able to set cookies, if not we try the next suffix until we find the one that works.
     topViableDomain = hostname.slice(-i).join(".");
     setCookie(
