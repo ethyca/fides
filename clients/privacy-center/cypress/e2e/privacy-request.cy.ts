1f244df59a52a40cbb53837660aaa96fc7d623ac
Lucano Vera <lucanovera@ethyca.com>
2024-06-04 10:24:05 -0300

    PROD-2068 update tests

--- clients/privacy-center/cypress/e2e/privacy-request.cy.ts
+++ clients/privacy-center/cypress/e2e/privacy-request.cy.ts
@@ -15,22 +15,24 @@ describe("Privacy request", () => {
 
     it("requires valid inputs", () => {
       cy.visit("/");
-      cy.getByTestId("card").contains("Access your data").click();
+      cy.loadConfigFixture("config/config_all.json").then(() => {
+        cy.getByTestId("card").contains("Access your data").click();
 
-      cy.getByTestId("privacy-request-form").within(() => {
-        // This block uses `.root()` to keep queries within the form. This is necessary because of
-        // `.blur()` which triggers input validation.
+        cy.getByTestId("privacy-request-form").within(() => {
+          // This block uses `.root()` to keep queries within the form. This is necessary because of
+          // `.blur()` which triggers input validation.
 
-        // test email being typed, continue becoming disabled due to invalid email
-        cy.root().get("input#email").type("invalid email").blur();
-        cy.root().should("contain", "Email is invalid");
-        cy.root().get("button").contains("Continue").should("be.disabled");
-        cy.root().get("input#email").clear().blur();
+          // test email being typed, continue becoming disabled due to invalid email
+          cy.root().get("input#email").type("invalid email").blur();
+          cy.root().should("contain", "Email is invalid");
+          cy.root().get("button").contains("Continue").should("be.disabled");
+          cy.root().get("input#email").clear().blur();
 
-        // test valid email, continue becoming enabled due to valid email
-        cy.root().get("input#email").type("valid@example.com").blur();
-        cy.root().get("button").contains("Continue").should("be.enabled");
-        cy.root().get("input#email").clear().blur();
+          // test valid email, continue becoming enabled due to valid email
+          cy.root().get("input#email").type("valid@example.com").blur();
+          cy.root().get("button").contains("Continue").should("be.enabled");
+          cy.root().get("input#email").clear().blur();
+        });
       });
     });
   });
