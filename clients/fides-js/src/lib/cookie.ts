12ffb63af71cb61a4b6947588a9dc980c37171ea
Jason Gill <jason.gill@ethyca.com>
2024-06-04 10:50:57 -0600

    fix unit tests

--- clients/fides-js/src/lib/cookie.ts
+++ clients/fides-js/src/lib/cookie.ts
@@ -78,7 +78,7 @@ export const makeFidesCookie = (consent?: NoticeConsent): FidesCookie => {
   return {
     consent: consent || {},
     identity: {
-      fides_user_device_id: userDeviceId,
+      fides_user_device_id: userDeviceId || generateFidesUserDeviceId(), // the fallback here is a bit overkill, but it is mostly to make the unit test work since it doesn't have a global context.
     },
     fides_meta: {
       version: "0.9.0",
