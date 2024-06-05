2782bf54287a7f61cc07f7ee96f61de175320ba1
eastandwestwind <eastandwestwind@gmail.com>
2024-06-05 13:11:43 -0400

    bump alembic revision

--- src/fides/api/alembic/migrations/versions/2736c942faa2_property_specific_messaging_db_models.py
+++ src/fides/api/alembic/migrations/versions/2736c942faa2_property_specific_messaging_db_models.py
@@ -12,7 +12,7 @@ import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
 revision = "2736c942faa2"
-down_revision = "efddde14da21"
+down_revision = "52a5f1a957bc"
 branch_labels = None
 depends_on = None
 
