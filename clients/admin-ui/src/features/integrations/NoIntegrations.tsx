31d450901f71a465377f528ba8d6b5543561bd6d
Jeremy Pople <jeremy@ethyca.com>
2024-06-06 10:23:58 -0500

    fix font sizes on integrations page

--- clients/admin-ui/src/features/integrations/NoIntegrations.tsx
+++ clients/admin-ui/src/features/integrations/NoIntegrations.tsx
@@ -2,10 +2,10 @@ import { Button, Flex, Text } from "fidesui";
 
 const NoIntegrations = ({ onOpenAddModal }: { onOpenAddModal: () => void }) => (
   <Flex direction="column" alignItems="center" data-testid="empty-state">
-    <Text color="gray.700" fontWeight="semibold" fontSize="xl">
+    <Text color="gray.700" fontWeight="semibold" fontSize="md">
       No integrations
     </Text>
-    <Text color="gray.700" marginTop="8px">
+    <Text color="gray.700" fontSize="sm" marginTop="8px">
       You have not configured any integrations
     </Text>
     <Text color="gray.700">
