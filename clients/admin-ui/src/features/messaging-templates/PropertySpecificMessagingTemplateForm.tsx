212e41e89747875efe9bb0d734c3423cf6e5812f
Lucano Vera <lucanovera@ethyca.com>
2024-06-18 23:34:08 -0300

    PROD-2039 Add delete confirmation modal

--- clients/admin-ui/src/features/messaging-templates/PropertySpecificMessagingTemplateForm.tsx
+++ clients/admin-ui/src/features/messaging-templates/PropertySpecificMessagingTemplateForm.tsx
@@ -1,164 +1,172 @@
-import {MESSAGING_ROUTE} from "common/nav/v2/routes";
-import {Box, Button, Flex} from "fidesui";
-import {Form, Formik} from "formik";
-import {useRouter} from "next/router";
-import {useMemo} from "react";
+import { MESSAGING_ROUTE } from "common/nav/v2/routes";
+import { Box, Button, Flex } from "fidesui";
+import { Form, Formik } from "formik";
+import { useRouter } from "next/router";
+import { useMemo } from "react";
 
 import { useAppSelector } from "~/app/hooks";
 import FormSection from "~/features/common/form/FormSection";
 import {
-    CustomSwitch, CustomTextArea,
-    CustomTextInput,
+  CustomSwitch,
+  CustomTextArea,
+  CustomTextInput,
 } from "~/features/common/form/inputs";
 import ScrollableList from "~/features/common/ScrollableList";
-import {MessagingTemplateResponse} from "~/features/messaging-templates/messaging-templates.slice";
+import { MessagingTemplateResponse } from "~/features/messaging-templates/messaging-templates.slice";
 import MessagingActionTypeLabelEnum from "~/features/messaging-templates/MessagingActionTypeLabelEnum";
 import {
-    selectAllProperties,
-    selectPage as selectPropertyPage,
-    selectPageSize as selectPropertyPageSize, useGetAllPropertiesQuery
+  selectAllProperties,
+  selectPage as selectPropertyPage,
+  selectPageSize as selectPropertyPageSize,
+  useGetAllPropertiesQuery,
 } from "~/features/properties";
-import {MessagingActionType, MinimalProperty} from "~/types/api";
-
+import { MessagingActionType, MinimalProperty } from "~/types/api";
 
 interface Props {
-    template: MessagingTemplateResponse;
-    handleSubmit: (values: FormValues) => Promise<void>;
-    handleDelete?: (id: string) => Promise<void>;
+  template: MessagingTemplateResponse;
+  handleSubmit: (values: FormValues) => Promise<void>;
+  handleDelete?: () => void;
 }
 
 export interface FormValues {
-    type: string;
-    id?: string;
-    is_enabled: boolean;
-    content: {
-        subject: string;
-        body: string;
-    };
-    properties?: MinimalProperty[]
+  type: string;
+  id?: string;
+  is_enabled: boolean;
+  content: {
+    subject: string;
+    body: string;
+  };
+  properties?: MinimalProperty[];
 }
 
+const PropertySpecificMessagingTemplateForm = ({
+  template,
+  handleSubmit,
+  handleDelete,
+}: Props) => {
+  const router = useRouter();
+  const propertyPage = useAppSelector(selectPropertyPage);
+  const propertyPageSize = useAppSelector(selectPropertyPageSize);
+  useGetAllPropertiesQuery({ page: propertyPage, size: propertyPageSize });
+  const allProperties = useAppSelector(selectAllProperties);
 
-const PropertySpecificMessagingTemplateForm = ({ template, handleSubmit, handleDelete }: Props) => {
-    const router = useRouter();
-    const propertyPage = useAppSelector(selectPropertyPage);
-    const propertyPageSize = useAppSelector(selectPropertyPageSize);
-    useGetAllPropertiesQuery({ page: propertyPage, size: propertyPageSize });
-    const allProperties = useAppSelector(selectAllProperties);
+  const handleCancel = () => {
+    router.push(MESSAGING_ROUTE);
+  };
 
-    const handleCancel = () => {
-        router.push(MESSAGING_ROUTE);
-    };
+  const initialValues = useMemo(
+    () => ({
+      type: template.type,
+      content: template.content,
+      properties: template.properties,
+      is_enabled: template.is_enabled,
+      id: template.id,
+    }),
+    [template]
+  );
 
-    const initialValues = useMemo(
-        () => ({
-                type: template.type,
-                content: template.content,
-                properties: template.properties,
-                is_enabled: template.is_enabled,
-                id: template.id
-            }),
-        [template]
-    );
-
-
-    return (
-        <Formik<FormValues>
-            enableReinitialize
-            initialValues={initialValues}
-            onSubmit={handleSubmit}
+  return (
+    <Formik<FormValues>
+      enableReinitialize
+      initialValues={initialValues}
+      onSubmit={handleSubmit}
+    >
+      {({ dirty, isValid, isSubmitting, setFieldValue }) => (
+        <Form
+          style={{
+            paddingTop: "12px",
+            paddingBottom: "12px",
+          }}
         >
-            {({ dirty, isValid, isSubmitting, setFieldValue }) => (
-                <Form
-                    style={{
-                        paddingTop: "12px",
-                        paddingBottom: "12px",
-                    }}
-                >
-                    <Box py={3}>
-                        <FormSection title={`${MessagingActionTypeLabelEnum[
-                            initialValues.type as MessagingActionType
-                            ]}`}>
-                            <CustomTextInput
-                                isRequired
-                                label="Message subject"
-                                name="content.subject"
-                                tooltip="Email subject to display"
-                                variant="stacked"
-                            />
-                            <CustomTextArea
-                                isRequired
-                                label="Message body"
-                                name="content.body"
-                                value="test"
-                                variant="stacked"
-                                resize
-                            />
-                            <Box py={3}>
-                                <ScrollableList
-                                    label="Associated properties"
-                                    addButtonLabel="Add property"
-                                    idField="id"
-                                    nameField="name"
-                                    allItems={allProperties.map((property) => ({
-                                        id: property.id,
-                                        name: property.name,
-                                    }))}
-                                    values={initialValues.properties ?? []}
-                                    // fixme- setFieldValue is not working properly
-                                    setValues={(newValues) => setFieldValue("properties", newValues)}
-                                    draggable
-                                    maxHeight={100}
-                                    baseTestId="property"
-                                />
-                            </Box>
-                            <CustomSwitch
-                                name="is_enabled"
-                                label="Message enabled"
-                                tooltip="Configure whether this messaging template is enabled"
-                                variant="switchFirst"
-                            />
-                        </FormSection>
-                    </Box>
-                    <Flex justifyContent="space-between" width="100%" paddingTop={2}>
-                        {initialValues.id && handleDelete && (
-                            <Button
-                                data-testid="delete-template-button"
-                                size="sm"
-                                variant="outline"
-                                isLoading={false}
-                                mr={3}
-                                // todo- use confirmation modal
-                                onClick={() => handleDelete(initialValues.id as string)}
-                            >
-                                Delete
-                            </Button>
-                        )}
-                        <Flex justifyContent="right" width="100%" paddingTop={2}>
-                            <Button
-                                size="sm"
-                                variant="outline"
-                                isLoading={false}
-                                mr={3}
-                                onClick={handleCancel}
-                            >
-                                Cancel
-                            </Button>
-                            <Button
-                                size="sm"
-                                type="submit"
-                                colorScheme="primary"
-                                isDisabled={isSubmitting || !dirty || !isValid}
-                                isLoading={isSubmitting}
-                            >
-                                Save
-                            </Button>
-                        </Flex>
-                    </Flex>
-                </Form>
+          <Box py={3}>
+            <FormSection
+              title={`${
+                MessagingActionTypeLabelEnum[
+                  initialValues.type as MessagingActionType
+                ]
+              }`}
+            >
+              <CustomTextInput
+                isRequired
+                label="Message subject"
+                name="content.subject"
+                tooltip="Email subject to display"
+                variant="stacked"
+              />
+              <CustomTextArea
+                isRequired
+                label="Message body"
+                name="content.body"
+                value="test"
+                variant="stacked"
+                resize
+              />
+              <Box py={3}>
+                <ScrollableList
+                  label="Associated properties"
+                  addButtonLabel="Add property"
+                  idField="id"
+                  nameField="name"
+                  allItems={allProperties.map((property) => ({
+                    id: property.id,
+                    name: property.name,
+                  }))}
+                  values={initialValues.properties ?? []}
+                  // fixme- setFieldValue is not working properly
+                  setValues={(newValues) =>
+                    setFieldValue("properties", newValues)
+                  }
+                  draggable
+                  maxHeight={100}
+                  baseTestId="property"
+                />
+              </Box>
+              <CustomSwitch
+                name="is_enabled"
+                label="Message enabled"
+                tooltip="Configure whether this messaging template is enabled"
+                variant="switchFirst"
+              />
+            </FormSection>
+          </Box>
+          <Flex justifyContent="space-between" width="100%" paddingTop={2}>
+            {initialValues.id && handleDelete && (
+              <Button
+                data-testid="delete-template-button"
+                size="sm"
+                variant="outline"
+                isLoading={false}
+                mr={3}
+                onClick={handleDelete}
+              >
+                Delete
+              </Button>
             )}
-        </Formik>
-    );
+            <Flex justifyContent="right" width="100%" paddingTop={2}>
+              <Button
+                size="sm"
+                variant="outline"
+                isLoading={false}
+                mr={3}
+                onClick={handleCancel}
+              >
+                Cancel
+              </Button>
+              <Button
+                size="sm"
+                type="submit"
+                colorScheme="primary"
+                isDisabled={isSubmitting || !dirty || !isValid}
+                isLoading={isSubmitting}
+              >
+                Save
+              </Button>
+            </Flex>
+          </Flex>
+        </Form>
+      )}
+    </Formik>
+  );
 };
 
 export default PropertySpecificMessagingTemplateForm;
--- clients/admin-ui/src/pages/messaging/[id].tsx
+++ clients/admin-ui/src/pages/messaging/[id].tsx
@@ -1,88 +1,126 @@
-import {Box, Heading, Spinner, Text, useToast} from "fidesui";
+import {
+  Box,
+  ConfirmationModal,
+  Heading,
+  Spinner,
+  Text,
+  useDisclosure,
+  useToast,
+} from "fidesui";
 import type { NextPage } from "next";
 import { useRouter } from "next/router";
 
 import { getErrorMessage } from "~/features/common/helpers";
 import Layout from "~/features/common/Layout";
+import { MESSAGING_ROUTE } from "~/features/common/nav/v2/routes";
 import { errorToastParams, successToastParams } from "~/features/common/toast";
 import {
-    MessagingTemplateCreateOrUpdate,
-    useDeleteMessagingTemplateByIdMutation,
-    useGetMessagingTemplateByIdQuery,
-    useUpdateMessagingTemplateByIdMutation
+  MessagingTemplateCreateOrUpdate,
+  useDeleteMessagingTemplateByIdMutation,
+  useGetMessagingTemplateByIdQuery,
+  useUpdateMessagingTemplateByIdMutation,
 } from "~/features/messaging-templates/messaging-templates.slice";
-import PropertySpecificMessagingTemplateForm
-    , {FormValues} from "~/features/messaging-templates/PropertySpecificMessagingTemplateForm";
+import PropertySpecificMessagingTemplateForm, {
+  FormValues,
+} from "~/features/messaging-templates/PropertySpecificMessagingTemplateForm";
 import { isErrorResult } from "~/types/errors";
 
 const EditPropertyPage: NextPage = () => {
-    const toast = useToast();
-    const router = useRouter();
-    // to test- http://localhost:3000/messaging/mes_0512f494-f62d-4d9a-9e73-c3abca6c6849
-    const templateId = router.query.id;
+  const toast = useToast();
+  const router = useRouter();
+  // to test- http://localhost:3000/messaging/mes_0512f494-f62d-4d9a-9e73-c3abca6c6849
+  const templateId = router.query.id;
 
-    const { data: messagingTemplate, isLoading } = useGetMessagingTemplateByIdQuery(templateId as string);
+  const { data: messagingTemplate, isLoading } =
+    useGetMessagingTemplateByIdQuery(templateId as string);
 
-    const [updateMessagingTemplate] = useUpdateMessagingTemplateByIdMutation();
-    const [deleteMessagingTemplate] = useDeleteMessagingTemplateByIdMutation();
+  const [updateMessagingTemplate] = useUpdateMessagingTemplateByIdMutation();
+  const [deleteMessagingTemplate] = useDeleteMessagingTemplateByIdMutation();
 
-    const handleSubmit = async (values: FormValues) => {
-        const templateData: MessagingTemplateCreateOrUpdate = {
-            is_enabled: values.is_enabled,
-            content: {
-                subject: values.content.subject,
-                body: values.content.body,
-            },
-            properties: []
-        }
-        values.properties?.forEach(property => templateData.properties?.push(property.id))
-
-        const result = await updateMessagingTemplate({templateId: templateId as string, template: templateData as MessagingTemplateCreateOrUpdate});
-
-        if (isErrorResult(result)) {
-            toast(errorToastParams(getErrorMessage(result.error)));
-            return;
-        }
-
-        toast(successToastParams(`Messaging template updated successfully`));
+  const handleSubmit = async (values: FormValues) => {
+    const templateData: MessagingTemplateCreateOrUpdate = {
+      is_enabled: values.is_enabled,
+      content: {
+        subject: values.content.subject,
+        body: values.content.body,
+      },
+      properties: [],
     };
-
-    const handleDelete = async (id: string) => {
-        const result = await deleteMessagingTemplate(id);
-
-        if (isErrorResult(result)) {
-            toast(errorToastParams(getErrorMessage(result.error)));
-            return;
-        }
-
-        toast(successToastParams(`Messaging template deleted successfully`));
-    };
-
-    if (!messagingTemplate) {
-        return null;
-    }
-
-    if (isLoading) {
-        return <Spinner />;
-    }
-
-    return (
-        <Layout title="Configure Message">
-            <Box data-testid="add-messaging-template">
-                <Heading marginBottom={2} fontSize="2xl">
-                    Configure message
-                </Heading>
-                <Box maxWidth="720px">
-                    <Text fontSize="sm">
-                        Configure this message
-                    </Text>
-                    <Box padding={2}>
-                        <PropertySpecificMessagingTemplateForm template={messagingTemplate} handleSubmit={handleSubmit} handleDelete={handleDelete} />
-                    </Box>
-                </Box>
-            </Box>
-        </Layout>
+    values.properties?.forEach((property) =>
+      templateData.properties?.push(property.id)
     );
+
+    const result = await updateMessagingTemplate({
+      templateId: templateId as string,
+      template: templateData as MessagingTemplateCreateOrUpdate,
+    });
+
+    if (isErrorResult(result)) {
+      toast(errorToastParams(getErrorMessage(result.error)));
+      return;
+    }
+
+    toast(successToastParams(`Messaging template updated successfully`));
+  };
+
+  const {
+    isOpen: deleteIsOpen,
+    onOpen: onDeleteOpen,
+    onClose: onDeleteClose,
+  } = useDisclosure();
+
+  const handleDelete = async () => {
+    const result = await deleteMessagingTemplate(templateId as string);
+
+    if (isErrorResult(result)) {
+      toast(errorToastParams(getErrorMessage(result.error)));
+      return;
+    }
+
+    toast(successToastParams(`Messaging template deleted successfully`));
+
+    router.push(MESSAGING_ROUTE);
+  };
+
+  if (!messagingTemplate) {
+    return null;
+  }
+
+  if (isLoading) {
+    return <Spinner />;
+  }
+
+  return (
+    <Layout title="Configure Message">
+      <Box data-testid="add-messaging-template">
+        <Heading marginBottom={2} fontSize="2xl">
+          Configure message
+        </Heading>
+        <Box maxWidth="720px">
+          <Text fontSize="sm">Configure this message</Text>
+          <Box padding={2}>
+            <PropertySpecificMessagingTemplateForm
+              template={messagingTemplate}
+              handleSubmit={handleSubmit}
+              handleDelete={onDeleteOpen}
+            />
+          </Box>
+        </Box>
+      </Box>
+      <ConfirmationModal
+        isOpen={deleteIsOpen}
+        onClose={onDeleteClose}
+        onConfirm={handleDelete}
+        title="Delete message template"
+        message={
+          <Text>
+            You are about to permanently delete this template message. Are you
+            sure you would like to continue?
+          </Text>
+        }
+      />
+    </Layout>
+  );
 };
 
 export default EditPropertyPage;
