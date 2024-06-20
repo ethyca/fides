f3ae8336e66d53b0bffd46aaf5378e89939a52b6
Dawn Pattison <pattisdr@users.noreply.github.com>
2024-06-20 11:42:45 -0500

    Fix tests by using  model_dump to compare the dictionary version of what's coming out of the database with the pydantic model. In Pydantic V2, models are no longer equal to the dicts containing their data.

--- tests/ops/models/test_property.py
+++ tests/ops/models/test_property.py
@@ -46,7 +46,7 @@ class TestProperty:
         assert prop.name == "New Property"
         assert prop.type == PropertyType.website
         assert prop.id.startswith("FDS")
-        assert prop.privacy_center_config == privacy_center_config
+        assert prop.privacy_center_config == privacy_center_config.model_dump()
         assert prop.stylesheet == ":root:root { --chakra-colors-gray-50: #fff9ea; }"
         assert prop.paths == ["test"]
         assert prop.is_default is True
@@ -73,7 +73,7 @@ class TestProperty:
         assert prop.name == "New Property 2"
         assert prop.type == PropertyType.website
         assert prop.id.startswith("FDS")
-        assert prop.privacy_center_config == privacy_center_config
+        assert prop.privacy_center_config == privacy_center_config.model_dump()
         assert prop.stylesheet == ":root:root { --chakra-colors-gray-50: #fff9ea; }"
         assert prop.paths == ["testing"]
         assert prop.is_default is False
