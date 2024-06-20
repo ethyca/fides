2ae70bbbdf207005cb8cccbbcc0147747634c73f
Andres Torres <andres.torres.marroquin@gmail.com>
2024-06-20 14:09:45 -0600

    Add early migrations for Google Cloud SQL for Postgres (#5015)

--- src/fides/api/alembic/migrations/versions/8976def679db_add_google_cloud_sql_postgres_to_.py
+++ src/fides/api/alembic/migrations/versions/8976def679db_add_google_cloud_sql_postgres_to_.py
@@ -1,23 +1,66 @@
-"""add google_cloud_sql_mysql to connectiontype
+"""add google_cloud_sql_postgres to connectiontype
 
-Revision ID: 7641ea685ea4
-Revises: cb344673f633
-Create Date: 2024-06-18 16:17:15.012408
+Revision ID: 8976def679db
+Revises: 7641ea685ea4
+Create Date: 2024-06-20 18:12:42.560977
 
 """
-
-import sqlalchemy as sa
 from alembic import op
 
+
 # revision identifiers, used by Alembic.
-revision = "7641ea685ea4"
-down_revision = "cb344673f633"
+revision = '8976def679db'
+down_revision = '7641ea685ea4'
 branch_labels = None
 depends_on = None
 
 
 def upgrade():
-    # Add 'google_cloud_sql_mysql' to ConnectionType enum
+    # Add 'google_cloud_sql_postgres' to ConnectionType enum
+    op.execute("ALTER TYPE connectiontype RENAME TO connectiontype_old")
+    op.execute(
+        """
+        CREATE TYPE connectiontype AS ENUM (
+            'mongodb',
+            'mysql',
+            'https',
+            'snowflake',
+            'redshift',
+            'mssql',
+            'mariadb',
+            'bigquery',
+            'saas',
+            'manual',
+            'manual_webhook',
+            'timescale',
+            'fides',
+            'sovrn',
+            'attentive',
+            'dynamodb',
+            'postgres',
+            'generic_consent_email',
+            'generic_erasure_email',
+            'scylla',
+            's3',
+            'google_cloud_sql_mysql',
+            'google_cloud_sql_postgres'
+        )
+    """
+    )
+    op.execute(
+        """
+        ALTER TABLE connectionconfig ALTER COLUMN connection_type TYPE connectiontype USING
+        connection_type::text::connectiontype
+    """
+    )
+    op.execute("DROP TYPE connectiontype_old")
+
+
+def downgrade():
+    # Remove 'google_cloud_sql_postgres' from ConnectionType enum
+    op.execute(
+        "DELETE FROM connectionconfig WHERE connection_type IN ('google_cloud_sql_postgres')"
+    )
     op.execute("ALTER TYPE connectiontype RENAME TO connectiontype_old")
     op.execute(
         """
@@ -44,48 +87,6 @@ def upgrade():
             'scylla',
             's3',
             'google_cloud_sql_mysql'
-        )
-    """
-    )
-    op.execute(
-        """
-        ALTER TABLE connectionconfig ALTER COLUMN connection_type TYPE connectiontype USING
-        connection_type::text::connectiontype
-    """
-    )
-    op.execute("DROP TYPE connectiontype_old")
-
-
-def downgrade():
-    # Remove 'google_cloud_sql_mysql' from ConnectionType enum
-    op.execute(
-        "DELETE FROM connectionconfig WHERE connection_type IN ('google_cloud_sql_mysql')"
-    )
-    op.execute("ALTER TYPE connectiontype RENAME TO connectiontype_old")
-    op.execute(
-        """
-        CREATE TYPE connectiontype AS ENUM (
-            'mongodb',
-            'mysql',
-            'https',
-            'snowflake',
-            'redshift',
-            'mssql',
-            'mariadb',
-            'bigquery',
-            'saas',
-            'manual',
-            'manual_webhook',
-            'timescale',
-            'fides',
-            'sovrn',
-            'attentive',
-            'dynamodb',
-            'postgres',
-            'generic_consent_email',
-            'generic_erasure_email',
-            'scylla',
-            's3'
         )
     """
     )
