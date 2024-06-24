702aa0f91edfb9a7cf43b130ade2d81a77886f64
Eliana Rosselli <eliana@ethyca.com>
2024-06-24 16:44:06 -0300

    Update pymongo to 4.7.3

--- src/fides/api/service/connectors/mongodb_connector.py
+++ src/fides/api/service/connectors/mongodb_connector.py
@@ -71,7 +71,7 @@ class MongoDBConnector(BaseConnector[MongoClient]):
             )
             if default_auth_db:
                 db = client[default_auth_db]
-                db.collection_names()
+                db.list_collection_names()
         except ServerSelectionTimeoutError:
             raise ConnectionException(
                 "Server Selection Timeout Error connecting to MongoDB."
