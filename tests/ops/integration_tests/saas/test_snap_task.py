b4b2913442aea6c9e7f2f8b2db0e8e0c7e2f5c79
Marc Guimond <marc@ethyca.com>
2024-06-28 14:12:03 -0400

    adding skip to tests due to OAuth2

--- tests/ops/integration_tests/saas/test_snap_task.py
+++ tests/ops/integration_tests/saas/test_snap_task.py
@@ -3,7 +3,7 @@ import pytest
 from fides.api.models.policy import Policy
 from tests.ops.integration_tests.saas.connector_runner import ConnectorRunner
 
-
+@pytest.mark.skip(reason="Currently unable to test OAuth2 connectors")
 @pytest.mark.integration_saas
 class TestsnapConnector:
     def test_connection(self, snap_runner: ConnectorRunner):
