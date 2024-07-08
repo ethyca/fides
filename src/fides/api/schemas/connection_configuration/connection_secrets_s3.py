87101142ea132dc2a7b67ea6ea2bff3cbff7c6ca
Dawn Pattison <pattisdr@users.noreply.github.com>
2024-07-08 09:05:10 -0500

    Add default values to S3 schema to keep optional fields optional and turn the validator into an after validator

--- src/fides/api/schemas/connection_configuration/connection_secrets_s3.py
+++ src/fides/api/schemas/connection_configuration/connection_secrets_s3.py
@@ -13,41 +13,44 @@ class S3Schema(ConnectionConfigSecretsSchema):
     """Schema to validate the secrets needed to connect to Amazon S3"""
 
     auth_method: AWSAuthMethod = Field(
+        default=None,
         title="Authentication Method",
         description="Determines which type of authentication method to use for connecting to Amazon S3",
     )
 
     aws_access_key_id: Optional[str] = Field(
+        default=None,
         title="Access Key ID",
         description="Part of the credentials that provide access to your AWS account. This is required if using secret key authentication.",
     )
     aws_secret_access_key: Optional[str] = Field(
+        default=None,
         title="Secret Access Key",
         description="Part of the credentials that provide access to your AWS account. This is required if using secret key authentication.",
         sensitive=True,
     )
 
     aws_assume_role_arn: Optional[str] = Field(
+        default=None,
         title="Assume Role ARN",
         description="If provided, the ARN of the role that should be assumed to connect to s3.",
     )
 
     _required_components: ClassVar[List[str]] = ["auth_method"]
 
-    @model_validator(mode="before")
-    @classmethod
-    def keys_provided_if_needed(cls, values: Dict) -> Dict:
+    @model_validator(mode="after")
+    def keys_provided_if_needed(self) -> "S3Schema":
         """
         Validates that both access and secret access keys are provided if using a `secret_keys` auth method.
         """
-        if values.get("auth_method") == AWSAuthMethod.SECRET_KEYS.value and not (
-            values.get("aws_access_key_id") and values.get("aws_secret_access_key")
+        if self.auth_method == AWSAuthMethod.SECRET_KEYS.value and not (
+           self.aws_access_key_id and self.aws_secret_access_key
         ):
             raise ValueError(
                 f"An Access Key ID and a Secret Access Key must be provided if using the `{AWSAuthMethod.SECRET_KEYS.value}` Authentication Method"
             )
 
-        return values
+        return self
 
 
 class S3DocsSchema(S3Schema, NoValidationSchema):
