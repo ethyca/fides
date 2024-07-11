b0285febb23aea98ed4ff384c2bdfb394c8bc9a4
Marc Guimond <marc@ethyca.com>
2024-07-11 15:46:20 -0400

    Fixes - currently all tests passing

--- src/fides/api/service/saas_request/override_implementations/alchemer_request_overrides.py
+++ src/fides/api/service/saas_request/override_implementations/alchemer_request_overrides.py
@@ -1,7 +1,6 @@
 from typing import Any, Dict, List
 import requests
 
-
 from fides.api.graph.execution import ExecutionNode
 from fides.api.models.policy import Policy
 from fides.api.models.privacy_request import PrivacyRequest
@@ -17,44 +16,42 @@ from fides.api.util.collection_util import Row
 @register("alchemer_user_delete", [SaaSRequestType.DELETE])
 def alchemer_user_delete(
     client: AuthenticatedClient,
-    node: ExecutionNode,
+    param_values_per_row: List[Dict[str, Any]],
     policy: Policy,
     privacy_request: PrivacyRequest,
-    input_data: Dict[str, List[Any]], # may not need for our case here
     secrets: Dict[str, Any],
 ) -> int:
     # The delete endpoint has a structure like this
     # https://api.alchemer.com/v5/contactlist/31/contactlistcontact/100012345?_method=DELETE
-    # where 31 is the contact list id
-    # and 100012345 is the contact id
-    # What we have to start with is just our identity email
+    # where 31 is the contact list id and 100012345 is the contact id
     # So we first get all the contact lists and extract their ids
     # Then we query for all contacts in each list, filtering on our identity email
     # Then we call a delete on the contact
-# think about paging
-    # was using this as hardcoded for testing will need to pull this from input_data I think
-    identity_email = "connectors@ethyca.com"
+    rows_deleted = 0
     contact_list_url = f"https://{secrets['domain']}/v5/contactlist"
     params = {
-        "api_token": secrets['api_token'],
-        "api_token_secret": secrets['api_token_secret'],
+        "api_token": secrets["api_token"],
+        "api_token_secret": secrets["api_token_secret"],
     }
-    response = requests.request("GET",contact_list_url, params=params)
+    response = requests.request("GET", contact_list_url, params=params)
     list_ids_data = response.json()
 
     list_results = []
-    for list_id in list_ids_data['data']:
-        list_results.append(list_id['id'])
+    for list_id in list_ids_data["data"]:
+        list_results.append(list_id["id"])
     for list in list_results:
-        contact_url = f"https://{secrets['domain']}/v5/contactlist/{list}/contactlistcontact"
+        contact_url = (
+            f"https://{secrets['domain']}/v5/contactlist/{list}/contactlistcontact"
+        )
         response = requests.request("GET", contact_url, params=params)
         contacts_data = response.json()
-        # contact_results = []
-        for contact in contacts_data['data']:
-            if contact['email_address'] == identity_email: # input_data.get["email"]:
-                #    contact_results.append(contact)
-                del_url = f"https://{secrets['domain']}/v5/contactlist/{list}/contactlistcontact/{contact['id']}"
-                response = requests.request("DELETE", del_url, params=params)
+        for contact in contacts_data["data"]:
+            for row_param_values in param_values_per_row:
+                email = row_param_values["email"]
+                if contact["email_address"] == email:
+                    del_url = f"https://{secrets['domain']}/v5/contactlist/{list}/contactlistcontact/{contact['id']}"
+                    response = requests.request("DELETE", del_url, params=params)
+                    rows_deleted += 1
 
 
-    return 1
+    return rows_deleted
--- tests/fixtures/saas/alchemer_fixtures.py
+++ tests/fixtures/saas/alchemer_fixtures.py
@@ -1,9 +1,9 @@
+import random
+import string
 from typing import Any, Dict, Generator
 
 import pydash
 import pytest
-import random
-import string
 import requests
 
 from tests.ops.integration_tests.saas.connector_runner import (
@@ -18,8 +18,7 @@ secrets = get_secrets("alchemer")
 @pytest.fixture(scope="session")
 def alchemer_secrets(saas_config) -> Dict[str, Any]:
     return {
-        "domain": pydash.get(saas_config, "alchemer.domain")
-        or secrets["domain"],
+        "domain": pydash.get(saas_config, "alchemer.domain") or secrets["domain"],
         "api_token": pydash.get(saas_config, "alchemer.api_token")
         or secrets["api_token"],
         "api_token_secret": pydash.get(saas_config, "alchemer.api_token_secret")
@@ -55,13 +54,13 @@ def alchemer_erasure_data(
     alchemer_secrets,
 ) -> Generator:
     gen_string = string.ascii_lowercase
-    test_contactlist = ''.join(random.choice(gen_string) for i in range(10))
+    test_contactlist = "".join(random.choice(gen_string) for i in range(10))
     x_contactlist_name = f"Ethyca Test {test_contactlist}"
 
     base_url = f"https://{alchemer_secrets['domain']}/v5"
     params = {
-        "api_token": alchemer_secrets['api_token'],
-        "api_token_secret": alchemer_secrets['api_token_secret'],
+        "api_token": alchemer_secrets["api_token"],
+        "api_token_secret": alchemer_secrets["api_token_secret"],
         "list_name": x_contactlist_name,
     }
     contactlist_url = f"{base_url}/contactlist/"
@@ -71,8 +70,8 @@ def alchemer_erasure_data(
     contactlist_id = response.json()["data"]["id"]
     contactlistcontact_url = f"{contactlist_url}{contactlist_id}/contactlistcontact"
     params = {
-        "api_token": alchemer_secrets['api_token'],
-        "api_token_secret": alchemer_secrets['api_token_secret'],
+        "api_token": alchemer_secrets["api_token"],
+        "api_token_secret": alchemer_secrets["api_token_secret"],
         "email_address": alchemer_erasure_identity_email,
     }
     response = requests.put(contactlistcontact_url, params=params)
