ba3013ebc547d0f4ab17f4c0234b7e4858b3a607
Jason Gill <jason.gill@ethyca.com>
2024-07-16 14:46:33 -0600

    remove rewrites from NextJS config (#5087)

--- clients/admin-ui/next.config.js
+++ clients/admin-ui/next.config.js
@@ -17,31 +17,6 @@ const nextConfig = {
      */
     proxyTimeout: 120000,
   },
-  async rewrites() {
-    // The tests run without a server. Rewrites cause Next to continually try to connect,
-    // which spams the logs with "ECONNREFUSED".
-    if (process.env.NODE_ENV === "test") {
-      return [];
-    }
-
-    // these paths are unnecessarily complicated due to our backend being
-    // picky about trailing slashes https://github.com/ethyca/fides/issues/690
-    return [
-      {
-        source: `/api/v1/:path`,
-        destination: `${process.env.NEXT_PUBLIC_FIDESCTL_API_SERVER}/api/v1/:path/`,
-      },
-      {
-        source: `/api/v1/:first/:second*`,
-        destination: `${process.env.NEXT_PUBLIC_FIDESCTL_API_SERVER}/api/v1/:first/:second*`,
-      },
-      // The /health path does not live under /api/v1
-      {
-        source: `/health`,
-        destination: `${process.env.NEXT_PUBLIC_FIDESCTL_API_SERVER}/health`,
-      },
-    ];
-  },
   images: {
     loader: "custom",
   },
