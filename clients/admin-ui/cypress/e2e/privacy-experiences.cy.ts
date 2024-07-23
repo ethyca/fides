9bdd3f2f977386f35e090b3e0d2e6137e34b9087
Jason Gill <jason.gill@ethyca.com>
2024-07-23 16:04:07 -0600

    fix Firefox test issues

--- clients/admin-ui/cypress/e2e/privacy-experiences.cy.ts
+++ clients/admin-ui/cypress/e2e/privacy-experiences.cy.ts
@@ -79,19 +79,21 @@ describe("Privacy experiences", () => {
     cy.getByTestId("empty-state");
   });
 
-  it("can copy a JS script tag", () => {
-    cy.visit(PRIVACY_EXPERIENCE_ROUTE);
-    cy.getByTestId("js-tag-btn").click();
-    cy.getByTestId("copy-js-tag-modal");
-    // Have to use a "real click" in order for Cypress to properly inspect
-    // the window's clipboard https://github.com/cypress-io/cypress/issues/18198
-    cy.getByTestId("clipboard-btn").realClick();
-    cy.window().then((win) => {
-      win.navigator.clipboard.readText().then((text) => {
-        expect(text).to.contain("<script src=");
+  if (Cypress.isBrowser({ family: "chromium" })) {
+    it("can copy a JS script tag", () => {
+      cy.visit(PRIVACY_EXPERIENCE_ROUTE);
+      cy.getByTestId("js-tag-btn").click();
+      cy.getByTestId("copy-js-tag-modal");
+      // Have to use a "real click" in order for Cypress to properly inspect
+      // the window's clipboard https://github.com/cypress-io/cypress/issues/18198
+      cy.getByTestId("clipboard-btn").first().realClick();
+      cy.window().then((win) => {
+        win.navigator.clipboard.readText().then((text) => {
+          expect(text).to.contain("<script src=");
+        });
       });
     });
-  });
+  }
 
   describe("table", () => {
     beforeEach(() => {
