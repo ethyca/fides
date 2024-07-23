ccc745e1fe3ccffca2c0b0a8137566cc26629c1f
Marc Guimond <marc@ethyca.com>
2024-07-23 15:07:35 -0400

    mods to allow for testing with staging

--- tests/fixtures/saas/checkr_fixtures.py
+++ tests/fixtures/saas/checkr_fixtures.py
@@ -1,3 +1,4 @@
+import time
 from typing import Any, Dict, Generator
 
 import pydash
@@ -17,19 +18,15 @@ secrets = get_secrets("checkr")
 @pytest.fixture(scope="session")
 def checkr_secrets(saas_config) -> Dict[str, Any]:
     return {
-        "domain": pydash.get(saas_config, "checkr.domain")
-        or secrets["domain"],
+        "domain": pydash.get(saas_config, "checkr.domain") or secrets["domain"],
         "api_key_secret": pydash.get(saas_config, "checkr.api_key_secret")
         or secrets["api_key_secret"],
-        # add the rest of your secrets here
     }
 
 
 @pytest.fixture(scope="session")
 def checkr_identity_email(saas_config) -> str:
-    return (
-        pydash.get(saas_config, "checkr.identity_email") or secrets["identity_email"]
-    )
+    return pydash.get(saas_config, "checkr.identity_email") or secrets["identity_email"]
 
 
 @pytest.fixture
@@ -38,54 +35,37 @@ def checkr_erasure_identity_email() -> str:
 
 
 @pytest.fixture
-def checkr_external_references() -> Dict[str, Any]:
-    return {}
-
-
-@pytest.fixture
-def checkr_erasure_external_references() -> Dict[str, Any]:
-    return {}
-
-
-@pytest.fixture
 def checkr_erasure_data(
     checkr_secrets,
     checkr_erasure_identity_email: str,
 ) -> Generator:
     candidate_url = "https://api.checkr-staging.com/v1/candidates"
-    auth = HTTPBasicAuth(checkr_secrets["api_key_secret"], '')
+    auth = HTTPBasicAuth(checkr_secrets["api_key_secret"], "")
     body = {
         "first_name": "ethycafirst",
         "middle_name": "at",
-        "no_middle_name": 'false',
+        "no_middle_name": "false",
         "last_name": "ethycalast",
         "mother_maiden_name": "Smith",
         "email": checkr_erasure_identity_email,
-        "phone": '5555555555',
-        "zipcode": '90402',
+        "phone": "5555555555",
+        "zipcode": "90402",
         "dob": "1972-01-20",
         "driver_license_number": "F2101655",
         "driver_license_state": "CA",
         "previous_driver_license_number": "F1500739",
         "previous_driver_license_state": "MD",
-        "copy_requested": 'false',
+        "copy_requested": "false",
         "custom_id": "HRIS-21",
         "metadata": {},
-        "work_locations": [
-        {
-            "country": "US",
-            "state": "CA",
-            "city": "San Francisco"
-        }
-        ]
+        "work_locations": [{"country": "US", "state": "CA", "city": "San Francisco"}],
     }
     response = requests.post(candidate_url, json=body, auth=auth)
-    # create the data needed for erasure tests here
     assert response.ok
     json_response = response.json()
-    user_id = json_response["id"]
     assert json_response["id"] is not None
-    yield {user_id}
+    time.sleep(5)  # required wait for the candidate to be created
+    return
 
 
 @pytest.fixture
@@ -93,14 +73,10 @@ def checkr_runner(
     db,
     cache,
     checkr_secrets,
-    checkr_external_references,
-    checkr_erasure_external_references,
 ) -> ConnectorRunner:
     return ConnectorRunner(
         db,
         cache,
         "checkr",
         checkr_secrets,
-        external_references=checkr_external_references,
-        erasure_external_references=checkr_erasure_external_references,
     )
