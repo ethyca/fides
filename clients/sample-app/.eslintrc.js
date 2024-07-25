19b9f307e765b1501f9e044b1fe089d124087ce7
Jason Gill <jason.gill@ethyca.com>
2024-07-25 14:23:08 -0600

    revert sample app's eslintrc to be standalone

--- clients/sample-app/.eslintrc.js
+++ clients/sample-app/.eslintrc.js
@@ -1,5 +1,25 @@
 module.exports = {
-  extends: ["next/core-web-vitals"],
+  extends: [
+    "airbnb",
+    "airbnb-typescript/base",
+    "prettier",
+    "next/core-web-vitals",
+  ],
+  root: true,
+  rules: {
+    // "curly": ["error", "all"],
+    // "nonblock-statement-body-position": ["error", "below"],
+    // "import/prefer-default-export": "off",
+    "react/jsx-filename-extension": [1, { extensions: [".tsx"] }],
+    "react/jsx-props-no-spreading": [0],
+    "react/function-component-definition": [
+      2,
+      {
+        namedComponents: "arrow-function",
+      },
+    ],
+    "react/require-default-props": "off",
+  },
   parserOptions: {
     ecmaVersion: 2022,
     sourceType: "module",
@@ -7,5 +27,4 @@ module.exports = {
     tsconfigRootDir: __dirname,
     createDefaultProgram: true,
   },
-  rules: {},
 };
