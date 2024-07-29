3dad8c47d331eef2ff5a8fdceeab9c397f82d85b
eastandwestwind <eastandwestwind@gmail.com>
2024-07-29 16:10:32 -0400

    format

--- src/fides/api/alembic/migrations/versions/fc82ab64bd5e_adds_additional_config_vals_on_.py
+++ src/fides/api/alembic/migrations/versions/fc82ab64bd5e_adds_additional_config_vals_on_.py
@@ -6,9 +6,8 @@ Create Date: 2024-07-29 14:55:01.609732
 
 """
 
-from alembic import op
 import sqlalchemy as sa
-
+from alembic import op
 
 # revision identifiers, used by Alembic.
 revision = "fc82ab64bd5e"
