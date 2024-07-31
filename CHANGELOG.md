681b39c7f105ab83e1ca5a78374ba02ef5c6302e
Adrian Galvan <adrian@ethyca.com>
2024-07-31 10:29:39 -0700

    Changes to support bidirectional consent (#5118)

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -26,6 +26,7 @@ The types of changes are:
 - Add "status" field to detection & discovery tables [#5141](https://github.com/ethyca/fides/pull/5141)
 - Added optional filters `exclude_saas_datasets` and `only_unlinked_datasets` to the list datasets endpoint [#5132](https://github.com/ethyca/fides/pull/5132)
 - Add new config options to support notice-only banner and modal [#5136](https://github.com/ethyca/fides/pull/5136)
+- Added models to support bidirectional consent (Fides Plus feature) [#5118](https://github.com/ethyca/fides/pull/5118)
 
 ### Changed
 - Moving Privacy Center endpoint logging behind debug flag [#5103](https://github.com/ethyca/fides/pull/5103)
---
+++ tests/ops/schemas/test_consentable_item_schema.py
@@ -0,0 +1,207 @@
+from fides.api.schemas.consentable_item import ConsentableItem, merge_consentable_items
+
+
+class TestConsentableItemUtils:
+    def test_merge_consentable_items_single_level(self):
+        api_values = [
+            ConsentableItem(
+                type="Channel",
+                id="1",
+                name="Email",
+                children=[
+                    ConsentableItem(type="MessageType", id="1", name="Welcome"),
+                    ConsentableItem(type="MessageType", id="2", name="Promotional"),
+                ],
+            )
+        ]
+        db_values = [
+            ConsentableItem(
+                type="Channel",
+                id="2",
+                name="SMS",
+                children=[
+                    ConsentableItem(type="MessageType", id="1", name="Welcome"),
+                    ConsentableItem(type="MessageType", id="2", name="Transactional"),
+                ],
+            )
+        ]
+        assert merge_consentable_items(api_values, db_values) == [
+            ConsentableItem(
+                id="1",
+                type="Channel",
+                name="Email",
+                notice_id=None,
+                children=[
+                    ConsentableItem(
+                        id="1",
+                        type="MessageType",
+                        name="Welcome",
+                        notice_id=None,
+                        children=[],
+                        unmapped=True,
+                    ),
+                    ConsentableItem(
+                        id="2",
+                        type="MessageType",
+                        name="Promotional",
+                        notice_id=None,
+                        children=[],
+                        unmapped=True,
+                    ),
+                ],
+                unmapped=True,
+            )
+        ]
+
+    def test_merge_consentable_items_nested(self):
+        api_values = [
+            ConsentableItem(
+                type="Channel",
+                id="1",
+                name="Marketing",
+                children=[
+                    ConsentableItem(type="MessageType", id="1", name="Newsletter"),
+                    ConsentableItem(type="MessageType", id="3", name="Promotional"),
+                ],
+            )
+        ]
+        db_values = [
+            ConsentableItem(
+                type="Channel",
+                id="1",
+                notice_id="notice_123",
+                name="Marketing",
+                children=[
+                    ConsentableItem(
+                        type="MessageType",
+                        id="1",
+                        name="Newsletter",
+                        notice_id="notice_456",
+                    ),
+                    ConsentableItem(type="MessageType", id="3", name="Transactional"),
+                ],
+            )
+        ]
+        assert merge_consentable_items(api_values, db_values) == [
+            ConsentableItem(
+                id="1",
+                type="Channel",
+                name="Marketing",
+                notice_id="notice_123",
+                children=[
+                    ConsentableItem(
+                        id="1",
+                        type="MessageType",
+                        name="Newsletter",
+                        notice_id="notice_456",
+                        children=[],
+                        unmapped=False,
+                    ),
+                    ConsentableItem(
+                        id="3",
+                        type="MessageType",
+                        name="Promotional",
+                        notice_id=None,
+                        children=[],
+                        unmapped=False,
+                    ),
+                ],
+                unmapped=False,
+            )
+        ]
+
+    def test_merge_consentable_items_with_empty_list(self):
+        api_values = [
+            ConsentableItem(
+                type="Channel",
+                id="1",
+                name="Email",
+                children=[ConsentableItem(type="MessageType", id="1", name="Welcome")],
+            )
+        ]
+        assert merge_consentable_items(api_values, []) == [
+            ConsentableItem(
+                id="1",
+                type="Channel",
+                name="Email",
+                notice_id=None,
+                children=[
+                    ConsentableItem(
+                        id="1",
+                        type="MessageType",
+                        name="Welcome",
+                        notice_id=None,
+                        children=[],
+                        unmapped=True,
+                    )
+                ],
+                unmapped=True,
+            )
+        ]
+
+    def test_merge_consentable_items_with_none_value(self):
+        api_values = [
+            ConsentableItem(
+                type="Channel",
+                id="1",
+                name="Email",
+                children=[ConsentableItem(type="MessageType", id="1", name="Welcome")],
+            )
+        ]
+        assert merge_consentable_items(api_values, None) == [
+            ConsentableItem(
+                id="1",
+                type="Channel",
+                name="Email",
+                notice_id=None,
+                children=[
+                    ConsentableItem(
+                        id="1",
+                        type="MessageType",
+                        name="Welcome",
+                        notice_id=None,
+                        children=[],
+                        unmapped=True,
+                    )
+                ],
+                unmapped=True,
+            )
+        ]
+
+    def test_merge_consentable_items_multiple_items(self):
+        api_values = [
+            ConsentableItem(type="Channel", id="1", name="Email"),
+            ConsentableItem(type="Channel", id="2", name="SMS"),
+            ConsentableItem(type="Channel", id="3", name="Push"),
+        ]
+        db_values = [
+            ConsentableItem(type="Channel", id="1", name="Email"),
+            ConsentableItem(type="Channel", id="3", name="Push"),
+            ConsentableItem(type="Channel", id="4", name="Web"),
+        ]
+        assert merge_consentable_items(api_values, db_values) == [
+            ConsentableItem(
+                id="1",
+                type="Channel",
+                name="Email",
+                notice_id=None,
+                children=[],
+                unmapped=False,
+            ),
+            ConsentableItem(
+                id="2",
+                type="Channel",
+                name="SMS",
+                notice_id=None,
+                children=[],
+                unmapped=True,
+            ),
+            ConsentableItem(
+                id="3",
+                type="Channel",
+                name="Push",
+                notice_id=None,
+                children=[],
+                unmapped=False,
+            ),
+        ]
