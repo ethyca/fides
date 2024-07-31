f1ff2689645b84d60daa2d2a63ade0be850ed82c
Neville Samuell <neville@ethyca.com>
2024-07-31 13:33:21 -0400

    Improve the error messages provided when attempting to edit the is_default field on taxonomy resources

--- tests/ctl/core/test_api.py
+++ tests/ctl/core/test_api.py
@@ -2938,7 +2938,10 @@ class TestDefaultTaxonomyCrud:
             resource_id=resource.fides_key,
             headers=test_config.user.auth_header,
         )
+        print(f"result object: {result}")
+        print(f"result json: {result.json()}")
         assert result.status_code == 403
+        assert "cannot modify a resource where 'is_default' is true" in result.json()["detail"]["error"]
 
     @pytest.mark.parametrize("endpoint", TAXONOMY_ENDPOINTS)
     def test_api_can_update_default(
@@ -2987,6 +2990,7 @@ class TestDefaultTaxonomyCrud:
             headers=test_config.user.auth_header,
         )
         assert result.status_code == 403
+        assert "cannot create a resource where 'is_default' is true" in result.json()["detail"]["error"]
 
         _api.delete(
             url=test_config.cli.server_url,
@@ -3012,6 +3016,7 @@ class TestDefaultTaxonomyCrud:
             resources=[manifest.dict()],
         )
         assert result.status_code == 403
+        assert "cannot create a resource where 'is_default' is true" in result.json()["detail"]["error"]
 
         _api.delete(
             url=test_config.cli.server_url,
@@ -3043,6 +3048,7 @@ class TestDefaultTaxonomyCrud:
             json_resource=manifest.json(exclude_none=True),
         )
         assert result.status_code == 403
+        assert "cannot modify 'is_default' field on an existing resource" in result.json()["detail"]["error"]
 
     @pytest.mark.parametrize("endpoint", TAXONOMY_ENDPOINTS)
     def test_api_cannot_upsert_is_default(
@@ -3071,6 +3077,7 @@ class TestDefaultTaxonomyCrud:
             resources=[manifest.dict(), second_item.dict()],
         )
         assert result.status_code == 403
+        assert "cannot modify 'is_default' field on an existing resource" in result.json()["detail"]["error"]
 
         _api.delete(
             url=test_config.cli.server_url,
