dc85cad78655ca01ad29c7a28b486c8264a26b8e
Eliana Rosselli <eliana@ethyca.com>
2024-08-02 10:54:54 -0300

    Add test

--- tests/fixtures/scylladb_fixtures.py
+++ tests/fixtures/scylladb_fixtures.py
@@ -1,4 +1,10 @@
 import pytest
+from typing import List, Dict
+from sqlalchemy.orm import Session
+
+from fides.api.models.connectionconfig import ConnectionConfig
+from fides.api.models.datasetconfig import DatasetConfig
+from fides.api.models.sql_models import Dataset as CtlDataset
 
 from fides.api.service.connectors.scylla_connector import ScyllaConnector
 
@@ -15,3 +21,29 @@ def scylladb_integration_no_keyspace(integration_scylladb_config):
     connector = ScyllaConnector(integration_scylladb_config)
     client = connector.create_client()
     yield client
+
+
+@pytest.fixture(scope="function")
+def scylladb_test_dataset_config(
+    integration_scylladb_config_with_keyspace: ConnectionConfig,
+    db: Session,
+    example_datasets: List[Dict],
+):
+    scylladb_dataset = example_datasets[15]
+    fides_key = scylladb_dataset["fides_key"]
+    integration_scylladb_config_with_keyspace.name = fides_key
+    integration_scylladb_config_with_keyspace.key = fides_key
+    integration_scylladb_config_with_keyspace.save(db=db)
+
+    ctl_dataset = CtlDataset.create_from_dataset_dict(db, scylladb_dataset)
+    dataset = DatasetConfig.create(
+        db=db,
+        data={
+            "connection_config_id": integration_scylladb_config_with_keyspace.id,
+            "fides_key": fides_key,
+            "ctl_dataset_id": ctl_dataset.id,
+        },
+    )
+    yield dataset
+    dataset.delete(db=db)
+    ctl_dataset.delete(db=db)
--- tests/ops/service/privacy_request/test_request_runner_service.py
+++ tests/ops/service/privacy_request/test_request_runner_service.py
@@ -875,6 +875,66 @@ def test_create_and_process_access_request_mysql(
     pr.delete(db=db)
 
 
+@pytest.mark.integration
+@pytest.mark.integration_scylladb
+@mock.patch("fides.api.models.privacy_request.PrivacyRequest.trigger_policy_webhook")
+@pytest.mark.parametrize(
+    "dsr_version",
+    ["use_dsr_3_0", "use_dsr_2_0"],
+)
+def test_create_and_process_access_request_scylladb(
+    trigger_webhook_mock,
+    scylladb_test_dataset_config,
+    db,
+    cache,
+    policy,
+    dsr_version,
+    request,
+    policy_pre_execution_webhooks,
+    policy_post_execution_webhooks,
+    run_privacy_request_task,
+):
+    request.getfixturevalue(dsr_version)  # REQUIRED to test both DSR 3.0 and 2.0
+
+    customer_email = "customer-1@example.com"
+    data = {
+        "requested_at": "2021-08-30T16:09:37.359Z",
+        "policy_key": policy.key,
+        "identity": {"email": customer_email},
+    }
+
+    pr = get_privacy_request_results(
+        db,
+        policy,
+        run_privacy_request_task,
+        data,
+    )
+
+    results = pr.get_raw_access_results()
+    assert len(results.keys()) == 2
+
+    assert "scylladb_example_test_dataset:users" in results
+    assert len(results["scylladb_example_test_dataset:users"]) == 1
+    assert results["scylladb_example_test_dataset:users"][0]["email"] == customer_email
+    assert results["scylladb_example_test_dataset:users"][0]["age"] == 41
+    assert results["scylladb_example_test_dataset:users"][0][
+        "alternative_contacts"
+    ] == {"phone": "+1 (531) 988-5905", "work_email": "customer-1@example.com"}
+
+    assert "scylladb_example_test_dataset:user_activity" in results
+    assert len(results["scylladb_example_test_dataset:user_activity"]) == 3
+
+    for activity in results["scylladb_example_test_dataset:user_activity"]:
+        assert activity["user_id"]
+        assert activity["timestamp"]
+        assert activity["activity_type"]
+        assert activity["user_agent"]
+
+    # Both pre-execution webhooks and both post-execution webhooks were called
+    assert trigger_webhook_mock.call_count == 4
+    pr.delete(db=db)
+
+
 @pytest.mark.integration_external
 @pytest.mark.integration_google_cloud_sql_mysql
 @mock.patch("fides.api.models.privacy_request.PrivacyRequest.trigger_policy_webhook")
