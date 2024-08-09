53d4d856120ef993d2889f86f2856f5f6b379727
Jeremy Pople <jeremy@ethyca.com>
2024-08-09 15:13:18 -0500

    add docstring

--- clients/admin-ui/src/features/data-discovery-and-detection/utils/isNestedField.ts
+++ clients/admin-ui/src/features/data-discovery-and-detection/utils/isNestedField.ts
@@ -1,5 +1,10 @@
 import { DiscoveryMonitorItem } from "~/features/data-discovery-and-detection/types/DiscoveryMonitorItem";
 
+/**
+ * Helper method to determine whether a resource is a "top-level" field.
+ * `true` if the resource is at top-level nested field; `false`if the
+ * resource is not a field, has no subfields, or is a subfield of another field
+ */
 const isNestedField = (resource: DiscoveryMonitorItem): boolean => {
   if (!resource.parent_table_urn) {
     // resource is not a field
