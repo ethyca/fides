b928a58fa90e7f303c45469528cc7655d525c753
Adrian Galvan <adrian@ethyca.com>
2024-08-09 16:34:25 -0700

    Adding customer lookup by phone number

--- tests/fixtures/saas/yotpo_reviews_fixtures.py
+++ tests/fixtures/saas/yotpo_reviews_fixtures.py
@@ -1,5 +1,4 @@
 from typing import Any, Dict, Generator
-from uuid import uuid4
 
 import pydash
 import pytest
@@ -36,26 +35,17 @@ def yotpo_reviews_identity_email(saas_config) -> str:
     )
 
 
+@pytest.fixture(scope="session")
+def yotpo_reviews_identity_phone_number(saas_config) -> str:
+    return (
+        pydash.get(saas_config, "yotpo_reviews.identity_phone_number")
+        or secrets["identity_phone_number"]
+    )
+
+
 @pytest.fixture
 def yotpo_reviews_erasure_identity_email() -> str:
     return generate_random_email()
-
-
-@pytest.fixture
-def yotpo_reviews_erasure_yotpo_external_id() -> str:
-    return f"ext-{uuid4()}"
-
-
-@pytest.fixture
-def yotpo_reviews_external_references() -> Dict[str, Any]:
-    return {"yotpo_external_id": "ak123798684365sdfkj"}
-
-
-@pytest.fixture
-def yotpo_reviews_erasure_external_references(
-    yotpo_reviews_erasure_yotpo_external_id,
-) -> Dict[str, Any]:
-    return {"yotpo_external_id": yotpo_reviews_erasure_yotpo_external_id}
 
 
 class YotpoReviewsTestClient:
@@ -71,14 +61,13 @@ class YotpoReviewsTestClient:
         assert response.ok
         self.access_token = response.json()["access_token"]
 
-    def create_customer(self, external_id: str, email: str) -> Response:
+    def create_customer(self, email: str) -> Response:
         faker = Faker()
         return requests.patch(
             url=f"https://{self.domain}/core/v3/stores/{self.store_id}/customers",
             headers={"X-Yotpo-Token": self.access_token},
             json={
                 "customer": {
-                    "external_id": external_id,
                     "email": email,
                     "first_name": faker.first_name(),
                     "last_name": faker.last_name(),
@@ -103,11 +92,11 @@ class YotpoReviewsTestClient:
             },
         )
 
-    def get_customer(self, external_id: str) -> Any:
+    def get_customer(self, email: str) -> Any:
         response = requests.get(
             url=f"https://{self.domain}/core/v3/stores/{self.store_id}/customers",
             headers={"X-Yotpo-Token": self.access_token},
-            params={"external_ids": external_id},
+            params={"email": email},
         )
         customers = response.json().get("customers")
         return (
@@ -126,23 +115,22 @@ def yotpo_reviews_test_client(
 @pytest.fixture
 def yotpo_reviews_erasure_data(
     yotpo_reviews_test_client: YotpoReviewsTestClient,
-    yotpo_reviews_erasure_yotpo_external_id,
     yotpo_reviews_erasure_identity_email,
 ) -> Generator:
     # create customer
     response = yotpo_reviews_test_client.create_customer(
-        yotpo_reviews_erasure_yotpo_external_id, yotpo_reviews_erasure_identity_email
+        yotpo_reviews_erasure_identity_email
     )
     assert response.ok
 
     poll_for_existence(
         yotpo_reviews_test_client.get_customer,
-        (yotpo_reviews_erasure_yotpo_external_id,),
+        (yotpo_reviews_erasure_identity_email,),
         interval=30,
         verification_count=3,
     )
 
-    yield yotpo_reviews_erasure_identity_email, yotpo_reviews_erasure_yotpo_external_id
+    yield yotpo_reviews_erasure_identity_email
 
 
 @pytest.fixture
@@ -150,14 +138,5 @@ def yotpo_reviews_runner(
     db,
     cache,
     yotpo_reviews_secrets,
-    yotpo_reviews_external_references,
-    yotpo_reviews_erasure_external_references,
 ) -> ConnectorRunner:
-    return ConnectorRunner(
-        db,
-        cache,
-        "yotpo_reviews",
-        yotpo_reviews_secrets,
-        external_references=yotpo_reviews_external_references,
-        erasure_external_references=yotpo_reviews_erasure_external_references,
-    )
+    return ConnectorRunner(db, cache, "yotpo_reviews", yotpo_reviews_secrets)
