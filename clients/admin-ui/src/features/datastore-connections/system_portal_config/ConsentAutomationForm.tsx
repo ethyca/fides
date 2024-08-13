364b71ecbfaa9437c1917c4c2b1a62047439a131
Jason Gill <jason.gill@ethyca.com>
2024-08-13 10:51:49 -0600

    clean up obsolete "default" language

--- clients/admin-ui/src/features/datastore-connections/system_portal_config/ConsentAutomationForm.tsx
+++ clients/admin-ui/src/features/datastore-connections/system_portal_config/ConsentAutomationForm.tsx
@@ -218,8 +218,7 @@ export const ConsentAutomationForm = ({
           >
             <Text mb={7}>
               Configure the notices that will trigger consent propagation for
-              this integration. A default value can be used to trigger every
-              consent preference or each item can be configured individually.
+              this integration.
             </Text>
             <Formik initialValues={initialValues || {}} onSubmit={handleSubmit}>
               <Form>
