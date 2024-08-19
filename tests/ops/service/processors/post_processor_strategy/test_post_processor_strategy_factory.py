85c9fe5d9f879d944c9370fa519805dd9073e8fe
bruno <bruno.gutierrez96@gmail.com>
2024-08-19 16:38:11 -0400

    Adding base test for Post processor building

--- tests/ops/service/processors/post_processor_strategy/test_post_processor_strategy_factory.py
+++ tests/ops/service/processors/post_processor_strategy/test_post_processor_strategy_factory.py
@@ -11,6 +11,11 @@ from fides.api.service.processors.post_processor_strategy.post_processor_strateg
     UnwrapPostProcessorStrategy,
 )
 
+from fides.api.service.processors.post_processor_strategy.post_processor_strategy_error_validation import (
+    ErrorValidationPostProcessorStrategy,
+)
+
+
 
 def test_get_strategy_filter():
     config = {"field": "email_contact", "value": "somebody@email.com"}
@@ -27,6 +32,12 @@ def test_get_strategy_unwrap():
     )
     assert isinstance(strategy, UnwrapPostProcessorStrategy)
 
+def test_strategy_error_validation():
+    config = { "http_code" : 400, "error_message_field": "error_msg", "expected_message": "This is an Example"}
+    strategy = PostProcessorStrategy.get_strategy(
+        strategy_name="error_validation", configuration=config
+    )
+    assert isinstance(strategy, ErrorValidationPostProcessorStrategy)
 
 def test_get_strategy_invalid_config():
     with pytest.raises(ValidationError):
