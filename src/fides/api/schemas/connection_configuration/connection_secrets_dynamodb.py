c5505cc4c5764674de1dfd15a512def7c9faaf4c
Andrés Torres Marroquín <andres.torres.marroquin@gmail.com>
2024-08-27 10:21:54 -0600

    Adding auth_method and auth_method to DynamoDBSchema

--- src/fides/api/schemas/connection_configuration/connection_secrets_dynamodb.py
+++ src/fides/api/schemas/connection_configuration/connection_secrets_dynamodb.py
@@ -1,4 +1,4 @@
-from typing import ClassVar, List
+from typing import ClassVar, List, Optional
 
 from pydantic import Field
 
@@ -6,6 +6,7 @@ from fides.api.schemas.base_class import NoValidationSchema
 from fides.api.schemas.connection_configuration.connection_secrets import (
     ConnectionConfigSecretsSchema,
 )
+from fides.api.schemas.storage.storage import AWSAuthMethod
 
 
 class DynamoDBSchema(ConnectionConfigSecretsSchema):
@@ -15,17 +16,29 @@ class DynamoDBSchema(ConnectionConfigSecretsSchema):
         title="Region",
         description="The AWS region where your DynamoDB table is located (ex. us-west-2).",
     )
+
+    auth_method: AWSAuthMethod = Field(
+        title="Authentication Method",
+        description="Determines which type of authentication method to use for connecting to Amazon S3",
+        default=AWSAuthMethod.SECRET_KEYS,
+    )
+
     aws_access_key_id: str = Field(
         title="Access Key ID",
         description="Part of the credentials that provide access to your AWS account.",
     )
+
     aws_secret_access_key: str = Field(
         title="Secret Access Key",
         description="Part of the credentials that provide access to your AWS account.",
         json_schema_extra={"sensitive": True},
     )
 
-    # TODO: include an aws_assume_role_arn and more closely follow the pattern in `connection_secrets_s3`
+    aws_assume_role_arn: Optional[str] = Field(
+        default=None,
+        title="Assume Role ARN",
+        description="If provided, the ARN of the role that should be assumed to connect to s3.",
+    )
 
     _required_components: ClassVar[List[str]] = [
         "region_name",
