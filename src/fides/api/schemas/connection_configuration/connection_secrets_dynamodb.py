fd0b03afef174090da42cd26764f0c1ad0856194
Andrés Torres Marroquín <andres.torres.marroquin@gmail.com>
2024-08-28 13:12:25 -0600

    QA

--- src/fides/api/schemas/connection_configuration/connection_secrets_dynamodb.py
+++ src/fides/api/schemas/connection_configuration/connection_secrets_dynamodb.py
@@ -1,45 +1,19 @@
-from typing import ClassVar, List, Optional
+from typing import ClassVar, List
 
 from pydantic import Field
 
 from fides.api.schemas.base_class import NoValidationSchema
-from fides.api.schemas.connection_configuration.connection_secrets import (
-    ConnectionConfigSecretsSchema,
+from fides.api.schemas.connection_configuration.connection_secrets_base_aws import (
+    BaseAWSSchema,
 )
-from fides.api.schemas.storage.storage import AWSAuthMethod
 
 
-class DynamoDBSchema(ConnectionConfigSecretsSchema):
+class DynamoDBSchema(BaseAWSSchema):
     """Schema to validate the secrets needed to connect to an Amazon DynamoDB cluster"""
 
     region_name: str = Field(
         title="Region",
         description="The AWS region where your DynamoDB table is located (ex. us-west-2).",
-    )
-
-    auth_method: AWSAuthMethod = Field(
-        title="Authentication Method",
-        description="Determines which type of authentication method to use for connecting to Amazon DynamoDB",
-        default=AWSAuthMethod.SECRET_KEYS,
-    )
-
-    aws_access_key_id: Optional[str] = Field(
-        default=None,
-        title="Access Key ID",
-        description="Part of the credentials that provide access to your AWS account. This is required if using secret key authentication.",
-    )
-
-    aws_secret_access_key: Optional[str] = Field(
-        default=None,
-        title="Secret Access Key",
-        description="Part of the credentials that provide access to your AWS account. This is required if using secret key authentication.",
-        json_schema_extra={"sensitive": True},
-    )
-
-    aws_assume_role_arn: Optional[str] = Field(
-        default=None,
-        title="Assume Role ARN",
-        description="If provided, the ARN of the role that should be assumed to connect to Amazon DynamoDB.",
     )
 
     _required_components: ClassVar[List[str]] = ["auth_method", "region_name"]
