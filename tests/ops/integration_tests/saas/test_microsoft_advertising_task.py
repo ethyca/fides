b63e0257b2723d5d8467e752180a1a803688e857
bruno <bruno.gutierrez96@gmail.com>
2024-08-28 12:15:09 -0400

    Removing unnecesary parameters

--- tests/ops/integration_tests/saas/test_microsoft_advertising_task.py
+++ tests/ops/integration_tests/saas/test_microsoft_advertising_task.py
@@ -4,14 +4,12 @@ from fides.api.models.policy import Policy
 from tests.ops.integration_tests.saas.connector_runner import ConnectorRunner
 
 
+@pytest.mark.skip(reason="Currently unable to test OAuth2 connectors")
 @pytest.mark.integration_saas
 class TestMicrosoftAdvertisingConnector:
     def test_connection(
         self,
         microsoft_advertising_runner: ConnectorRunner,
-        policy: Policy,
-        erasure_policy_string_rewrite: Policy,
-        microsoft_advertising_erasure_identity_email: str,
     ):
         microsoft_advertising_runner.test_connection()
 
