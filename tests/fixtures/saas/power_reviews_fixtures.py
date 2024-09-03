aba6f110d68e96f356b6c0b83296d1cd33ed3aba
bruno <bruno.gutierrez96@gmail.com>
2024-09-03 15:19:34 -0400

    Setting base test and Fixtures

---
+++ tests/fixtures/saas/power_reviews_fixtures.py
@@ -0,0 +1,45 @@
+from typing import Any, Dict
+
+import pydash
+import pytest
+
+from tests.ops.integration_tests.saas.connector_runner import (
+    ConnectorRunner,
+    generate_random_email,
+)
+from tests.ops.test_helpers.vault_client import get_secrets
+
+secrets = get_secrets("power_reviews")
+
+
+@pytest.fixture(scope="session")
+def power_reviews_secrets(saas_config) -> Dict[str, Any]:
+    return {
+        "domain": pydash.get(saas_config, "power_reviews.domain")
+        or secrets["domain"],
+        "client_id": pydash.get(saas_config, "power_reviews.client_id")
+        or secrets["client_id"],
+        "client_secret": pydash.get(saas_config, "power_reviews.client_secret")
+        or secrets["client_secret"],
+        "merchant_id": pydash.get(saas_config, "power_reviews.merchant_id")
+        or secrets["merchant_id"],
+        "merchant_group_id": pydash.get(saas_config, "power_reviews.merchant_group_id")
+        or secrets["merchant_group_id"],
+
+    }
+
+
+@pytest.fixture
+def power_reviews_erasure_identity_email() -> str:
+    return generate_random_email()
+
+
+@pytest.fixture
+def power_reviews_runner(
+    db,
+    cache,
+    power_reviews_secrets,
+) -> ConnectorRunner:
+    return ConnectorRunner(
+        db, cache, "power_reviews", power_reviews_secrets
+    )
---
+++ tests/ops/integration_tests/saas/test_power_reviews_task.py
@@ -0,0 +1,29 @@
+import pytest
+
+from fides.api.models.policy import Policy
+from tests.ops.integration_tests.saas.connector_runner import ConnectorRunner
+
+
+@pytest.mark.integration_saas
+class TestPowerReviewsConnector:
+    def test_connection(self, power_reviews_runner: ConnectorRunner):
+        power_reviews_runner.test_connection()
+
+
+    async def test_non_strict_erasure_request(
+        self,
+        power_reviews_runner: ConnectorRunner,
+        policy: Policy,
+        erasure_policy_string_rewrite: Policy,
+        power_reviews_erasure_identity_email: str,
+    ):
+        (
+            _,
+            erasure_results,
+        ) = await power_reviews_runner.non_strict_erasure_request(
+            access_policy=policy,
+            erasure_policy=erasure_policy_string_rewrite,
+            identities={"email": power_reviews_erasure_identity_email},
+        )
+        # We set the email to 1 since its 1 request only(?)
+        assert erasure_results == {"power_reviews_instance:email": 1}
