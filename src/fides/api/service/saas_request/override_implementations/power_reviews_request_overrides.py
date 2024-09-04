538f1c57dff36dff05d60e57d5d402cae030fdff
bruno <bruno.gutierrez96@gmail.com>
2024-09-04 12:33:54 -0400

    Base request override configuration

---
+++ src/fides/api/service/saas_request/override_implementations/power_reviews_request_overrides.py
@@ -0,0 +1,53 @@
+import json
+from typing import Any, Dict, List
+
+from fides.api.models.policy import Policy
+from fides.api.models.privacy_request import PrivacyRequest
+
+from fides.api.schemas.saas.shared_schemas import HTTPMethod, SaaSRequestParams
+
+from fides.api.service.connectors.saas.authenticated_client import AuthenticatedClient
+from fides.api.service.saas_request.saas_request_override_factory import (
+    SaaSRequestType,
+    register,
+)
+
+@register("power_reviews_user_delete", [SaaSRequestType.DELETE])
+def power_reviews_user_delete(
+    client: AuthenticatedClient,
+    param_values_per_row: List[Dict[str, Any]],
+    policy: Policy,
+    privacy_request: PrivacyRequest,
+    secrets: Dict[str, Any],
+) -> int:
+    rows_deleted = 0
+    """
+    Calls PowerReviews's `DELETE right-to-be-forgotten` endpoint with the correct base URL, and with the email in JSON format
+    """
+    client.uri = "https://enterprise-api.powerreviews.com"
+    path = "/v1/privacy/right-to-be-forgotten"
+
+    for row_param_values in param_values_per_row:
+        email = row_param_values["email"]
+
+        payload = json.dumps({
+            "requests": [
+                {
+                    "email": email
+                }
+            ]
+        })
+
+        client.send(
+            SaaSRequestParams(
+                method=HTTPMethod.POST,
+                path=path,
+                data=payload,
+            )
+        )
+
+
+
+        rows_deleted += 1
+
+    return rows_deleted
