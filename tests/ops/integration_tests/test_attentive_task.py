f800706c4eca0bfae2019265ac5e4f697044c2b7
bruno <bruno.gutierrez96@gmail.com>
2024-09-05 15:44:12 -0400

    Setting Up test and fixtures
    Base test passing

---
+++ tests/ops/integration_tests/test_attentive_task.py
@@ -0,0 +1,32 @@
+
+import pytest
+
+from fides.api.models.policy import Policy
+from tests.ops.integration_tests.saas.connector_runner import ConnectorRunner
+
+
+@pytest.mark.integration_saas
+class TestAttentiveConnector:
+    def test_connection(self, attentive_runner: ConnectorRunner):
+        attentive_runner.test_connection()
+
+    async def test_non_strict_erasure_request(
+        self,
+        attentive_runner: ConnectorRunner,
+        policy: Policy,
+        erasure_policy_string_rewrite: Policy,
+        attentive_erasure_identity_email: str,
+    ):
+        (
+            _,
+            erasure_results,
+        ) = await attentive_runner.non_strict_erasure_request(
+            access_policy=policy,
+            erasure_policy=erasure_policy_string_rewrite,
+            identities={
+                "email": attentive_erasure_identity_email,
+                "phoneNumber": "56991231"
+            },
+        )
+        # We set the email to 1 since its 1 request only(?)
+        assert erasure_results == {"attentive_instance:privacy": 1}
