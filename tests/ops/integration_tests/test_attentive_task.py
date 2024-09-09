921650c35095a20bc4d2facef69e8acf2535ab6f
Adrian Galvan <adrian@ethyca.com>
2024-09-09 13:11:15 -0700

    Attentive suggestions

--- tests/ops/integration_tests/test_attentive_task.py
+++ tests/ops/integration_tests/test_attentive_task.py
@@ -9,13 +9,12 @@ class TestAttentiveConnector:
     def test_connection(self, attentive_runner: ConnectorRunner):
         attentive_runner.test_connection()
 
-    async def test_non_strict_erasure_request(
+    async def test_non_strict_erasure_request_with_email(
         self,
         attentive_runner: ConnectorRunner,
         policy: Policy,
         erasure_policy_string_rewrite: Policy,
         attentive_erasure_identity_email: str,
-        attentive_erasure_identity_phone_number: str,
     ):
         (
             _,
@@ -25,8 +24,25 @@ class TestAttentiveConnector:
             erasure_policy=erasure_policy_string_rewrite,
             identities={
                 "email": attentive_erasure_identity_email,
+            },
+        )
+        assert erasure_results == {"attentive_instance:user": 1}
+
+    async def test_non_strict_erasure_request_with_phone_number(
+        self,
+        attentive_runner: ConnectorRunner,
+        policy: Policy,
+        erasure_policy_string_rewrite: Policy,
+        attentive_erasure_identity_phone_number: str,
+    ):
+        (
+            _,
+            erasure_results,
+        ) = await attentive_runner.non_strict_erasure_request(
+            access_policy=policy,
+            erasure_policy=erasure_policy_string_rewrite,
+            identities={
                 "phone_number": attentive_erasure_identity_phone_number,
             },
         )
-        # We set the email to 1 since its 1 request only(?)
-        assert erasure_results == {"attentive_instance:delete_request": 1}
+        assert erasure_results == {"attentive_instance:user": 1}
