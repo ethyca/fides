07dd79b306a44ec66de61ed50976bf3320f08696
Adrian Galvan <adrian@ethyca.com>
2024-09-28 14:58:21 -0700

    Updating migration

--- src/fides/api/alembic/migrations/versions/59d9c4beeb30_updating_attentive_connection_to_.py
+++ src/fides/api/alembic/migrations/versions/59d9c4beeb30_updating_attentive_connection_to_.py
@@ -1,7 +1,7 @@
 """Updating Attentive connection to Attentive email
 
 Revision ID: 59d9c4beeb30
-Revises: 9de4bb76307a
+Revises: 75bb9ee843f5
 Create Date: 2024-09-24 14:52:49.199323
 
 """
@@ -11,7 +11,7 @@ from alembic import op
 
 # revision identifiers, used by Alembic.
 revision = "59d9c4beeb30"
-down_revision = "9de4bb76307a"
+down_revision = "75bb9ee843f5"
 branch_labels = None
 depends_on = None
 
@@ -48,6 +48,7 @@ def upgrade():
             'google_cloud_sql_mysql',
             'google_cloud_sql_postgres',
             'dynamic_erasure_email'
+            'rds_mysql'
         )
     """
     )
@@ -97,7 +98,8 @@ def upgrade():
             's3',
             'google_cloud_sql_mysql',
             'google_cloud_sql_postgres',
-            'dynamic_erasure_email'
+            'dynamic_erasure_email',
+            'rds_mysql'
         )
     """
     )
@@ -111,6 +113,9 @@ def upgrade():
 
 
 def downgrade():
+    op.execute(
+        "delete from connectionconfig where connection_type in ('attentive_email')"
+    )
     # add 'attentive' to ConnectionType enum
     op.execute("ALTER TYPE connectiontype RENAME TO connectiontype_old")
     op.execute(
@@ -131,7 +136,6 @@ def downgrade():
             'fides',
             'sovrn',
             'attentive',
-            'attentive_email',
             'dynamodb',
             'postgres',
             'generic_consent_email',
@@ -140,7 +144,8 @@ def downgrade():
             's3',
             'google_cloud_sql_mysql',
             'google_cloud_sql_postgres',
-            'dynamic_erasure_email'
+            'dynamic_erasure_email',
+            'rds_mysql'
         )
     """
     )
@@ -151,54 +156,3 @@ def downgrade():
     """
     )
     op.execute("DROP TYPE connectiontype_old")
-
-    # update connectionconfig reference from 'attentive_email' to 'attentive'
-    op.execute(
-        """
-        UPDATE connectionconfig
-        SET connection_type = 'attentive'::connectiontype
-        WHERE connection_type = 'attentive_email'::connectiontype
-        """
-    )
-
-    # remove 'attentive_email' from ConnectionType enum
-    op.execute("ALTER TYPE connectiontype RENAME TO connectiontype_staging")
-
-    op.execute(
-        """
-        CREATE TYPE connectiontype AS ENUM (
-            'mongodb',
-            'mysql',
-            'https',
-            'snowflake',
-            'redshift',
-            'mssql',
-            'mariadb',
-            'bigquery',
-            'saas',
-            'manual',
-            'manual_webhook',
-            'timescale',
-            'fides',
-            'sovrn',
-            'attentive',
-            'dynamodb',
-            'postgres',
-            'generic_consent_email',
-            'generic_erasure_email',
-            'scylla',
-            's3',
-            'google_cloud_sql_mysql',
-            'google_cloud_sql_postgres',
-            'dynamic_erasure_email'
-        )
-    """
-    )
-
-    op.execute(
-        """
-        ALTER TABLE connectionconfig ALTER COLUMN connection_type TYPE connectiontype USING
-        connection_type::text::connectiontype
-    """
-    )
-    op.execute("DROP TYPE connectiontype_staging")
