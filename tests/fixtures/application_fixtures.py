0ea77a67b73cc3be1c25433b690f8bb69b4afb31
Andres Torres <andres.torres.marroquin@gmail.com>
2024-10-03 10:30:07 -0600

    PROD-2823 - Add RDSMySQLConnector (#5343)

--- tests/fixtures/application_fixtures.py
+++ tests/fixtures/application_fixtures.py
@@ -221,6 +221,19 @@ integration_secrets = {
         "username": pydash.get(integration_config, "scylladb_example.username"),
         "password": pydash.get(integration_config, "scylladb_example.password"),
     },
+    "rds_mysql_example": {
+        "aws_access_key_id": pydash.get(
+            integration_config, "rds_mysql_example.aws_access_key_id"
+        ),
+        "aws_secret_access_key": pydash.get(
+            integration_config,
+            "rds_mysql_example.aws_secret_access_key",
+        ),
+        "db_username": pydash.get(integration_config, "rds_mysql_example.db_username"),
+        "db_instance": pydash.get(integration_config, "rds_mysql_example.db_instance"),
+        "db_name": pydash.get(integration_config, "rds_mysql_example.db_name"),
+        "region": pydash.get(integration_config, "rds_mysql_example.region"),
+    },
 }
 
 
