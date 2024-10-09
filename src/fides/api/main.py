c1a27f1245214d3ba9a21c204bc2128d13518b54
Andrés Torres Marroquín <andres.torres.marroquin@gmail.com>
2024-10-09 16:24:46 -0700

    Testing server reload

--- src/fides/api/main.py
+++ src/fides/api/main.py
@@ -332,6 +332,7 @@ def start_webserver(port: int = 8080) -> None:
         server.config.port,
         server.config.log_level,
     )
+    logger.info(f"reload_dirs: {server.config.reload_dirs}, reload: {server.config.reload}")
 
     warn_root_user_enabled()
 
