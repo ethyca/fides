cec739a4ecaf16bcede3f201ecd27c6c40202f79
Robert Keyser <robert@ethyca.com>
2024-10-10 09:38:58 -0700

    feat: add front-end validation for common passwords

--- clients/admin-ui/src/features/common/form/validation.ts
+++ clients/admin-ui/src/features/common/form/validation.ts
@@ -1,3 +1,4 @@
+import { commonPasswords, PasswordStrength } from "tai-password-strength";
 import * as Yup from "yup";
 
 export const passwordValidation = Yup.string()
@@ -6,4 +7,17 @@ export const passwordValidation = Yup.string()
   .matches(/[0-9]/, "Password must have at least one number.")
   .matches(/[A-Z]/, "Password must have at least one capital letter.")
   .matches(/[a-z]/, "Password must have at least one lowercase letter.")
-  .matches(/[\W_]/, "Password must have at least one symbol.");
+  .matches(/[\W_]/, "Password must have at least one symbol.")
+  .test(
+    "is-common-password",
+    "Password cannot be a common password.",
+    passwordIsNotCommon,
+  );
+
+function passwordIsNotCommon(password: string) {
+  const strengthTester = new PasswordStrength();
+  const strength = strengthTester
+    .addCommonPasswords(commonPasswords)
+    .check(password);
+  return !strength.commonPassword;
+}
