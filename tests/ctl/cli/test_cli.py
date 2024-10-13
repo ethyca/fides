40f5715eb6e72e6424b759a348a805077dd43190
Neville Samuell <neville@ethyca.com>
2024-10-13 14:13:37 -0400

    Add a basic test for 'fides --version'

--- tests/ctl/cli/test_cli.py
+++ tests/ctl/cli/test_cli.py
@@ -85,6 +85,16 @@ def test_commands_print_help_text_even_on_invalid(
     assert result.exit_code == 0
     assert "Usage: fides user permissions [OPTIONS]" in result.output
 
+@pytest.mark.unit
+def test_cli_version(test_cli_runner: CliRunner) -> None:
+    result = test_cli_runner.invoke(
+        cli,
+        ["--version"]
+    )
+    import fides
+    assert f"fides, version {fides.__version__}" in result.output
+    assert result.exit_code == 0
+
 
 class TestView:
     @pytest.mark.unit
