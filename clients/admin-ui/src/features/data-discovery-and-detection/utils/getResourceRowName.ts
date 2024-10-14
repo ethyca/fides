47359686f5d60a2933c269a3f5bb40e2af4af10c
Jeremy Pople <jeremy@ethyca.com>
2024-10-14 11:19:16 -0500

    fix D&D table row keys

--- clients/admin-ui/src/features/data-discovery-and-detection/utils/getResourceRowName.ts
+++ clients/admin-ui/src/features/data-discovery-and-detection/utils/getResourceRowName.ts
@@ -1,6 +1,6 @@
 import { StagedResource } from "~/types/api";
 
 const getResourceRowName = (row: StagedResource) =>
-  `${row.monitor_config_id}-${row.name ?? row.urn}`;
+  `${row.monitor_config_id}-${row.urn}`;
 
 export default getResourceRowName;
