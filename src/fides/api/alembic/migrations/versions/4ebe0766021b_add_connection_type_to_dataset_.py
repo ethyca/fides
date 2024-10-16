ebc2fc231260c49cf0f6aa381184a87fc5dea786
Eliana Rosselli <eliana@ethyca.com>
2024-10-16 16:43:57 -0300

    Add migration for existing datasets

---
+++ src/fides/api/alembic/migrations/versions/4ebe0766021b_add_connection_type_to_dataset_.py
@@ -0,0 +1,43 @@
+"""Add connection_type to dataset namespace meta
+
+Revision ID: 4ebe0766021b
+Revises: 5dafbc1818ae
+Create Date: 2024-10-16 15:25:08.544946
+
+"""
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = "4ebe0766021b"
+down_revision = "5dafbc1818ae"
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    # Populate the connection_type field in the namespace meta for all datasets
+    # that have a namespace key in their fides_meta json field.
+    # We use bigquery as the initial value since only datasets generated from
+    # BigQuery monitors will have a namespace key in their fides_meta json field.
+    op.execute(
+        """
+        update ctl_datasets
+        set fides_meta = jsonb_set(fides_meta::jsonb, '{namespace, connection_type}', '"bigquery"', true)
+        where fides_meta::jsonb ? 'namespace';
+        """
+    )
+
+
+def downgrade():
+    # Remove the connection_type field from the namespace meta for all datasets
+    # that have a namespace key in their fides_meta json field.
+    op.execute(
+        """
+        update ctl_datasets
+        set fides_meta  = fides_meta::jsonb #- '{namespace, connection_type}'
+        where fides_meta::jsonb ? 'namespace';
+        """
+    )
