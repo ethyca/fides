e37bd3f8ec1b8b4693509389da7546a557c3a1a4
Andrés Torres Marroquín <andres.torres.marroquin@gmail.com>
2024-10-29 12:42:33 -0600

    QA

--- tests/ops/task/test_graph_task.py
+++ tests/ops/task/test_graph_task.py
@@ -431,22 +431,22 @@ def test_sql_dry_run_queries(db) -> None:
 
     assert (
         env[CollectionAddress("mysql", "Customer")]
-        == 'SELECT customer_id, name, email, contact_address_id FROM "Customer" WHERE email = ?'
+        == 'SELECT customer_id, name, email, contact_address_id FROM "Customer" WHERE (email = ?)'
     )
 
     assert (
         env[CollectionAddress("mysql", "User")]
-        == 'SELECT id, user_id, name FROM "User" WHERE user_id = ?'
+        == 'SELECT id, user_id, name FROM "User" WHERE (user_id = ?)'
     )
 
     assert (
         env[CollectionAddress("postgres", "Order")]
-        == 'SELECT order_id, customer_id, shipping_address_id, billing_address_id FROM "Order" WHERE customer_id IN (?, ?)'
+        == 'SELECT order_id, customer_id, shipping_address_id, billing_address_id FROM "Order" WHERE (customer_id IN (?, ?))'
     )
 
     assert (
         env[CollectionAddress("mysql", "Address")]
-        == 'SELECT id, street, city, state, zip FROM "Address" WHERE id IN (?, ?)'
+        == 'SELECT id, street, city, state, zip FROM "Address" WHERE (id IN (?, ?))'
     )
 
     assert (
