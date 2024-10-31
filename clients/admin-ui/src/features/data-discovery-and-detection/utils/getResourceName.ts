6489e64e46ff6c310ea9c9cd6b1868d6e426a07b
Jeremy Pople <jeremy@ethyca.com>
2024-10-31 02:31:24 -0500

    fix subfield naming

---
+++ clients/admin-ui/src/features/data-discovery-and-detection/utils/getResourceName.ts
@@ -0,0 +1,19 @@
+import { DiscoveryMonitorItem } from "~/features/data-discovery-and-detection/types/DiscoveryMonitorItem";
+
+const TOP_LEVEL_FIELD_URN_PARTS = 5;
+
+const getResourceName = (resource: DiscoveryMonitorItem) => {
+  const splitUrn = resource.urn.split(".");
+  if (
+    !resource.parent_table_urn ||
+    splitUrn.length === TOP_LEVEL_FIELD_URN_PARTS
+  ) {
+    // use name as-is if it's not a subfield
+    return resource.name;
+  }
+  // URN format is "monitor.project.dataset.field.[any number of subfields]"
+  // for a subfield, we want to show all subfield names separated by "."
+  return splitUrn.slice(5).join(".");
+};
+
+export default getResourceName;
