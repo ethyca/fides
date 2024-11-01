06f77121482e7fd5cb81543a3154c8528643b65b
bruno <bruno.gutierrez96@gmail.com>
2024-11-01 16:49:22 -0300

    Expanding request override
    Including Email Format Masking
    
    Including the rest of the update endpoints

--- src/fides/api/service/saas_request/override_implementations/salesforce_request_overrides.py
+++ src/fides/api/service/saas_request/override_implementations/salesforce_request_overrides.py
@@ -13,9 +13,29 @@ from fides.api.service.saas_request.saas_request_override_factory import (
 )
 from fides.api.util.saas_util import PRIVACY_REQUEST_ID
 
+# check if the masked field is over 40 characters long
+# if so, truncate it to 40 characters
+def truncateFieldsTo40Characters(masked_object_fields: Dict) -> Dict:
+    for key in masked_object_fields:
+        logger.info(key)
+        logger.info((masked_object_fields[key]))
+        if(type(masked_object_fields[key]) != str):
+            continue
+        if(len(masked_object_fields[key])>40):
+            masked_object_fields[key] = masked_object_fields[key][:40]
+    return masked_object_fields
 
-@register("salesforce_contact_update", [SaaSRequestType.UPDATE])
-def salesforce_contact_update(
+#Masking Email properly so it does not breaks validation rules
+#using the privacy request id to have an unique id
+def maskEmail(masked_object_fields: Dict, email_field:str) -> Dict:
+    if email_field in masked_object_fields:
+        masked_object_fields[
+            email_field
+        ] =  "Masked@company.com"
+    return masked_object_fields
+
+@register("salesforce_contacts_update", [SaaSRequestType.UPDATE])
+def salesforce_contacts_update(
     client: AuthenticatedClient,
     param_values_per_row: List[Dict[str, Any]],
     policy: Policy,
@@ -25,14 +45,15 @@ def salesforce_contact_update(
     rows_updated = 0
     # each update_params dict correspond to a record that needs to be updated
     for row_param_values in param_values_per_row:
-        # check if the masked field is over 40 characters long
-        # if so, truncate it
+
         masked_object_fields = row_param_values["masked_object_fields"]
-        for key in masked_object_fields:
-            logger.info("key: %s", key)
-            logger.info("size: %s", len(masked_object_fields[key]))
-            if(len(masked_object_fields[key])>40):
-                masked_object_fields[key] = masked_object_fields[key][:40]
+
+        masked_object_fields = maskEmail(
+            masked_object_fields,
+            "Email"
+        )
+
+        masked_object_fields = truncateFieldsTo40Characters(masked_object_fields)
 
         update_body = dumps(masked_object_fields)
         contact_id = row_param_values["contact_id"]
@@ -46,3 +67,98 @@ def salesforce_contact_update(
         )
         rows_updated += 1
     return rows_updated
+
+@register("salesforce_cases_update", [SaaSRequestType.UPDATE])
+def salesforce_cases_update(
+    client: AuthenticatedClient,
+    param_values_per_row: List[Dict[str, Any]],
+    policy: Policy,
+    privacy_request: PrivacyRequest,
+    secrets: Dict[str, Any],
+) -> int:
+    rows_updated = 0
+    # each update_params dict correspond to a record that needs to be updated
+    for row_param_values in param_values_per_row:
+        masked_object_fields = row_param_values["masked_object_fields"]
+
+        masked_object_fields = maskEmail(masked_object_fields,"SuppliedEmail")
+
+        masked_object_fields = truncateFieldsTo40Characters(masked_object_fields)
+
+        update_body = dumps(masked_object_fields)
+        case_id = row_param_values["case_id"]
+        client.send(
+            SaaSRequestParams(
+                method=HTTPMethod.PATCH,
+                headers={"Content-Type": "application/json"},
+                path=f"/services/data/v54.0/sobjects/Case/{case_id}",
+                body=update_body,
+            )
+        )
+        rows_updated += 1
+
+
+@register("salesforce_leads_update", [SaaSRequestType.UPDATE])
+def salesforce_leads_update(
+    client: AuthenticatedClient,
+    param_values_per_row: List[Dict[str, Any]],
+    policy: Policy,
+    privacy_request: PrivacyRequest,
+    secrets: Dict[str, Any],
+) -> int:
+    rows_updated = 0
+    # each update_params dict correspond to a record that needs to be updated
+    for row_param_values in param_values_per_row:
+        masked_object_fields = row_param_values["masked_object_fields"]
+
+        masked_object_fields = maskEmail(
+            masked_object_fields,
+            "Email"
+        )
+
+        masked_object_fields = truncateFieldsTo40Characters(masked_object_fields)
+
+        update_body = dumps(masked_object_fields)
+        lead_id = row_param_values["lead_id"]
+        client.send(
+            SaaSRequestParams(
+                method=HTTPMethod.PATCH,
+                headers={"Content-Type": "application/json"},
+                path=f"/services/data/v54.0/sobjects/Lead/{lead_id}",
+                body=update_body,
+            )
+        )
+        rows_updated += 1
+
+
+@register("salesforce_campaign_members_update", [SaaSRequestType.UPDATE])
+def salesforce_campaign_members_update(
+    client: AuthenticatedClient,
+    param_values_per_row: List[Dict[str, Any]],
+    policy: Policy,
+    privacy_request: PrivacyRequest,
+    secrets: Dict[str, Any],
+) -> int:
+    rows_updated = 0
+    # each update_params dict correspond to a record that needs to be updated
+    for row_param_values in param_values_per_row:
+        masked_object_fields = row_param_values["masked_object_fields"]
+
+        masked_object_fields = maskEmail(
+            masked_object_fields,
+            "Email"
+        )
+
+        masked_object_fields = truncateFieldsTo40Characters(masked_object_fields)
+
+        update_body = dumps(masked_object_fields)
+        campaign_member_id = row_param_values["campaign_member_id"]
+        client.send(
+            SaaSRequestParams(
+                method=HTTPMethod.PATCH,
+                headers={"Content-Type": "application/json"},
+                path=f"/services/data/v54.0/sobjects/CampaignMember/{campaign_member_id}",
+                body=update_body,
+            )
+        )
+        rows_updated += 1
