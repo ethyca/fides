633ec2647ff66032c898eab5d128afc6de45184d
Adrian Galvan <adrian@ethyca.com>
2024-11-01 15:21:01 -0700

    Adding retries to new database task sessions

--- src/fides/api/tasks/__init__.py
+++ src/fides/api/tasks/__init__.py
@@ -2,7 +2,15 @@ from typing import Any, ContextManager, Dict, List, Optional
 
 from celery import Celery, Task
 from loguru import logger
+from sqlalchemy.exc import OperationalError
 from sqlalchemy.orm import Session
+from tenacity import (
+    RetryCallState,
+    retry,
+    retry_if_exception_type,
+    stop_after_attempt,
+    wait_exponential,
+)
 
 from fides.api.db.session import get_db_engine, get_db_session
 from fides.api.util.logger import setup as setup_logging
@@ -11,6 +19,7 @@ from fides.config import CONFIG, FidesConfig
 MESSAGING_QUEUE_NAME = "fidesops.messaging"
 PRIVACY_PREFERENCES_QUEUE_NAME = "fides.privacy_preferences"  # This queue is used in Fidesplus for saving privacy preferences and notices served
 
+NEW_SESSION_RETRIES = 5
 
 autodiscover_task_locations: List[str] = [
     "fides.api.tasks",
@@ -20,10 +29,27 @@ autodiscover_task_locations: List[str] = [
 ]
 
 
+def log_retry_attempt(retry_state: RetryCallState) -> None:
+    """Log database connection retry attempts."""
+
+    logger.warning(
+        "Database connection attempt {} failed. Retrying in {} seconds...",
+        retry_state.attempt_number,
+        retry_state.next_action.sleep,  # type: ignore[union-attr]
+    )
+
+
 class DatabaseTask(Task):  # pylint: disable=W0223
     _task_engine = None
     _sessionmaker = None
 
+    @retry(
+        stop=stop_after_attempt(NEW_SESSION_RETRIES),
+        wait=wait_exponential(multiplier=1, min=1),
+        retry=retry_if_exception_type(OperationalError),
+        reraise=True,
+        before_sleep=log_retry_attempt,
+    )
     def get_new_session(self) -> ContextManager[Session]:
         """
         Creates a new Session to be used for each task invocation.
