ba4d5be73cdceffa3e731b78b8c760b5454d3f62
Thomas Van Dort <tom@ethyca.com>
2024-11-07 15:27:20 -0500

    Support BlueConic objectives.

---
+++ clients/fides-js/src/integrations/blueconic.ts
@@ -0,0 +1,55 @@
+declare global {
+  interface Window {
+    blueConicClient?: {
+      profile: {
+        setConsentedObjectives: (objectives: string[]) => void;
+        setRefusedObjectives: (objectives: string[]) => void;
+        updateProfile: () => void;
+      };
+      event: {
+        subscribe: any;
+      };
+    };
+  }
+}
+
+// https://support.blueconic.com/hc/en-us/articles/202605221-JavaScript-front-end-API
+const blueConicLoaded = () =>
+  typeof window.blueConicClient !== "undefined" &&
+  typeof window.blueConicClient.event !== "undefined" &&
+  typeof window.blueConicClient.event.subscribe !== "undefined";
+
+const configureObjectives = () => {
+  if (!blueConicLoaded() || !window.blueConicClient?.profile) {
+    return;
+  }
+
+  const profile = window.blueConicClient?.profile;
+  const { consent } = window.Fides;
+  if (consent.targeted_advertising_gpp_us_state) {
+    profile.setConsentedObjectives([
+      "iab_purpose_1",
+      "iab_purpose_2",
+      "iab_purpose_3",
+      "iab_purpose_4",
+    ]);
+    profile.setRefusedObjectives([]);
+  } else {
+    profile.setConsentedObjectives(["iab_purpose_1"]);
+    profile.setRefusedObjectives([
+      "iab_purpose_2",
+      "iab_purpose_3",
+      "iab_purpose_4",
+    ]);
+  }
+
+  profile.updateProfile();
+};
+
+export const blueconic = () => {
+  window.addEventListener("FidesInitialized", configureObjectives);
+  window.addEventListener("FidesUpdated", configureObjectives);
+  window.addEventListener("onBlueConicLoaded", configureObjectives);
+
+  configureObjectives();
+};
