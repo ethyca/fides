48ad9889f2e68166cfc01aa7caa3a2ac6542136d
Jason Gill <jason.gill@ethyca.com>
2024-11-11 16:22:12 -0700

    clean up ant exports now that it's getting bigger

--- clients/fidesui/src/index.ts
+++ clients/fidesui/src/index.ts
@@ -6,25 +6,29 @@ export * from "@chakra-ui/utils";
 
 // Unmodified component exported directly from Ant Design
 export type { ThemeConfig as AntThemeConfig } from "antd/es";
-export type { SwitchProps as AntSwitchProps } from "antd/lib";
-export type { ButtonProps as AntButtonProps } from "antd/lib";
-export type { InputProps as AntInputProps } from "antd/lib";
-export type { GetProps } from "antd/lib";
-export type { SelectProps as AntSelectProps } from "antd/lib";
-export { Layout as AntLayout } from "antd/lib";
-export { Space as AntSpace } from "antd/lib";
-export { Flex as AntFlex } from "antd/lib";
-export { Col as AntCol, Row as AntRow } from "antd/lib";
-export { Typography as AntTypography } from "antd/lib";
-export { Card as AntCard } from "antd/lib";
-export { Button as AntButton } from "antd/lib";
-export { Switch as AntSwitch } from "antd/lib";
-export { Select as AntSelect } from "antd/lib";
-export { Tooltip as AntTooltip } from "antd/lib";
-export { Alert as AntAlert } from "antd/lib";
-export { Tag as AntTag } from "antd/lib";
-export { Input as AntInput } from "antd/lib";
-export { Divider as AntDivider } from "antd/lib";
+export type {
+  ButtonProps as AntButtonProps,
+  InputProps as AntInputProps,
+  SelectProps as AntSelectProps,
+  SwitchProps as AntSwitchProps,
+  GetProps,
+} from "antd/lib";
+export {
+  Alert as AntAlert,
+  Button as AntButton,
+  Card as AntCard,
+  Col as AntCol,
+  Divider as AntDivider,
+  Flex as AntFlex,
+  Input as AntInput,
+  Layout as AntLayout,
+  Row as AntRow,
+  Space as AntSpace,
+  Switch as AntSwitch,
+  Tag as AntTag,
+  Tooltip as AntTooltip,
+  Typography as AntTypography,
+} from "antd/lib";
 
 /**
  * Custom Re-exports
