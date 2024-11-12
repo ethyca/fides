e663c8cf28117f1da5bb0875215a04e8cd5895d4
Jason Gill <jason.gill@ethyca.com>
2024-11-11 18:16:33 -0700

    fix typing issues

--- clients/fidesui/src/hoc/CustomSelect.tsx
+++ clients/fidesui/src/hoc/CustomSelect.tsx
@@ -1,21 +1,18 @@
 import { ChevronDown } from "@carbon/icons-react";
+import type { SelectProps } from "antd/lib";
 import { Select } from "antd/lib";
-import { SelectProps } from "antd/lib/select";
+import { BaseOptionType, DefaultOptionType } from "antd/lib/select";
 import React from "react";
 
 /**
  * Higher-order component that adds a custom arrow icon to the Select component.
  */
-export const withCustomArrowIcon = <P extends SelectProps>(
-  WrappedComponent: React.ComponentType<P>
-) => {
-  return function SpecifyCustomIcon(props: P) {
-    const customProps = {
-      ...props,
-      suffixIcon: <ChevronDown />,
-    };
-
-    return <WrappedComponent {...customProps} />;
+const withCustomArrowIcon = (WrappedComponent: typeof Select) => {
+  return function SpecifyCustomIcon<
+    ValueType = any,
+    OptionType extends DefaultOptionType | BaseOptionType = DefaultOptionType,
+  >(props: SelectProps<ValueType, OptionType>) {
+    return <WrappedComponent suffixIcon={<ChevronDown />} {...props} />;
   };
 };
 
