924742e4cdbf36c3a4c98239d42342a5f495691a
Jeremy Pople <jeremy@ethyca.com>
2024-11-15 01:20:15 -0600

    rewrite getResourceName

--- clients/admin-ui/src/features/data-discovery-and-detection/utils/getResourceName.ts
+++ clients/admin-ui/src/features/data-discovery-and-detection/utils/getResourceName.ts
@@ -1,22 +1,47 @@
 import { DiscoveryMonitorItem } from "~/features/data-discovery-and-detection/types/DiscoveryMonitorItem";
 
-const TOP_LEVEL_FIELD_URN_PARTS = 5;
+const MAX_NON_NESTED_URN_LENGTH = 5;
+const URN_SEPARATOR = ".";
+/**
+ * Helper method for deriving a resource's display name from its URN
+ */
+const getResourceName = ({
+  name,
+  urn,
+  monitor_config_id,
+  database_name,
+  schema_name,
+  table_name,
+  top_level_field_name,
+}: DiscoveryMonitorItem) => {
+  const splitUrn = urn.split(URN_SEPARATOR);
 
-const getResourceName = (resource: DiscoveryMonitorItem) => {
-  const URN_SEPARATOR = ".";
-  const splitUrn = resource.urn.split(URN_SEPARATOR);
   if (
-    !resource.parent_table_urn ||
-    splitUrn.length === TOP_LEVEL_FIELD_URN_PARTS
+    !table_name ||
+    !top_level_field_name ||
+    splitUrn.length < MAX_NON_NESTED_URN_LENGTH
   ) {
     // use name as-is if it's not a subfield
-    return resource.name;
+    return name;
   }
-  // TODO HJ-162: better handle case where field name contains "."
+  // URN format is "monitor.project?.dataset.table.field.[any number of subfields]"
+  // we want to show all subfield names separated by "."
+  const partsToRemove = [
+    monitor_config_id,
+    database_name,
+    schema_name,
+    table_name,
+    top_level_field_name,
+  ];
 
-  // URN format is "monitor.project.dataset.field.[any number of subfields]"
-  // for a subfield, we want to show all subfield names separated by "."
-  return splitUrn.slice(TOP_LEVEL_FIELD_URN_PARTS).join(URN_SEPARATOR);
+  partsToRemove.forEach((part) => {
+    const index = splitUrn.indexOf(part!);
+    if (index > -1) {
+      splitUrn.splice(index, 1);
+    }
+  });
+
+  return splitUrn.join(URN_SEPARATOR);
 };
 
 export default getResourceName;
