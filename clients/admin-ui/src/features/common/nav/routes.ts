93bf28e6e1f2537d7bdfb038d16ca97c5b9191d6
Jason Gill <jason.gill@ethyca.com>
2024-11-19 16:40:35 -0700

    initialize Form POC

--- clients/admin-ui/src/features/common/nav/routes.ts
+++ clients/admin-ui/src/features/common/nav/routes.ts
@@ -71,6 +71,6 @@ export const MESSAGING_EDIT_ROUTE = "/messaging/[id]";
 // OpenID Authentication group
 export const OPENID_AUTHENTICATION_ROUTE = "/settings/openid-authentication";
 
-export const ANT_POC_ROUTE = "/ant-poc";
-
+export const ANT_POC_ROUTE = "/poc/ant-components";
+export const FORMS_POC_ROUTE = "/poc/forms";
 export const FIDES_JS_DOCS = "/fides-js-docs";
