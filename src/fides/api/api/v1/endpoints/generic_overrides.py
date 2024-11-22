9feeedc24cdb53e69f0e2446e607a77fdaecf356
eastandwestwind <eastandwestwind@gmail.com>
2024-11-22 20:01:48 +0100

    updates tests

--- src/fides/api/api/v1/endpoints/generic_overrides.py
+++ src/fides/api/api/v1/endpoints/generic_overrides.py
@@ -128,17 +128,17 @@ async def create_data_use(
         disabled_resource_with_name = (
             db.query(DataUseDbModel)
             .filter(
-                DataUseDbModel.name == data_use.name,  # type: ignore[union-attr]
-                DataUseDbModel.active is False,
+                DataUseDbModel.active.is_(False),
+                DataUseDbModel.name == data_use.name,
             )
             .first()
         )
-        if disabled_resource_with_name:
-            data_use.active = True
-            return disabled_resource_with_name.update(db, data=data_use)  # type: ignore[union-attr]
         data_use.fides_key = get_key_from_data(
             {"key": data_use.fides_key, "name": data_use.name}, DataUse.__name__
         )
+        if disabled_resource_with_name:
+            data_use.active = True
+            return disabled_resource_with_name.update(db, data=data_use.model_dump(mode="json"))  # type: ignore[union-attr]
         try:
             return DataUseDbModel.create(db=db, data=data_use.model_dump(mode="json"))  # type: ignore[union-attr]
         except KeyOrNameAlreadyExists:
@@ -168,18 +168,18 @@ async def create_data_category(
         disabled_resource_with_name = (
             db.query(DataCategoryDbModel)
             .filter(
-                DataCategoryDbModel.name == data_category.name,  # type: ignore[union-attr]
-                DataCategoryDbModel.active is False,
+                DataCategoryDbModel.active.is_(False),
+                DataCategoryDbModel.name == data_category.name,
             )
             .first()
         )
-        if disabled_resource_with_name:
-            data_category.active = True
-            return disabled_resource_with_name.update(db, data=data_category)  # type: ignore[union-attr]
         data_category.fides_key = get_key_from_data(
             {"key": data_category.fides_key, "name": data_category.name},
             DataCategory.__name__,
         )
+        if disabled_resource_with_name:
+            data_category.active = True
+            return disabled_resource_with_name.update(db, data=data_category.model_dump(mode="json"))  # type: ignore[union-attr]
         try:
             return DataCategoryDbModel.create(db=db, data=data_category.model_dump(mode="json"))  # type: ignore[union-attr]
         except KeyOrNameAlreadyExists:
@@ -209,18 +209,18 @@ async def create_data_subject(
         disabled_resource_with_name = (
             db.query(DataSubjectDbModel)
             .filter(
-                DataSubjectDbModel.name == data_subject.name,  # type: ignore[union-attr]
-                DataSubjectDbModel.active is False,
+                DataSubjectDbModel.active.is_(False),
+                DataSubjectDbModel.name == data_subject.name,
             )
             .first()
         )
-        if disabled_resource_with_name:
-            data_subject.active = True
-            return disabled_resource_with_name.update(db, data=data_subject)  # type: ignore[union-attr]
         data_subject.fides_key = get_key_from_data(
             {"key": data_subject.fides_key, "name": data_subject.name},
             DataSubject.__name__,
         )
+        if disabled_resource_with_name:
+            data_subject.active = True
+            return disabled_resource_with_name.update(db, data=data_subject.model_dump(mode="json"))  # type: ignore[union-attr]
         try:
             return DataSubjectDbModel.create(db=db, data=data_subject.model_dump(mode="json"))  # type: ignore[union-attr]
         except KeyOrNameAlreadyExists:
--- tests/ops/api/v1/endpoints/test_taxonomy_overrides.py
+++ tests/ops/api/v1/endpoints/test_taxonomy_overrides.py
@@ -6,7 +6,7 @@ from starlette.testclient import TestClient
 
 from fides.api.models.client import ClientDetail
 from fides.api.models.sql_models import DataUse
-from fides.common.api.scope_registry import DATA_USE, STORAGE_READ, DATA_USE_CREATE
+from fides.common.api.scope_registry import DATA_USE, DATA_USE_CREATE, STORAGE_READ
 from fides.common.api.v1.urn_registry import V1_URL_PREFIX
 
 
@@ -23,16 +23,15 @@ class TestCreateDataUse:
             "is_default": False,
         }
 
-
     @pytest.fixture(scope="function")
     def disabled_data_use(self, db: Session):
         payload = {
-                "name": "test data use",
-                "fides_key": "test_data_use",
-                "active": False,
-                "is_default": False,
-                "description": "Disabled data use",
-            }
+            "name": "test data use",
+            "fides_key": "test_data_use",
+            "active": False,
+            "is_default": False,
+            "description": "Disabled data use",
+        }
         dataUse = DataUse.create(db=db, data=payload)
         yield dataUse
         dataUse.delete(db)
@@ -51,32 +50,32 @@ class TestCreateDataUse:
         dataUse.delete(db)
 
     def test_create_data_use_not_authenticated(
-            self,
-            api_client: TestClient,
-            payload,
-            url,
+        self,
+        api_client: TestClient,
+        payload,
+        url,
     ):
         response = api_client.post(url, headers={}, json=payload)
         assert 401 == response.status_code
 
     def test_create_data_use_incorrect_scope(
-            self,
-            api_client: TestClient,
-            payload,
-            url,
-            generate_auth_header,
+        self,
+        api_client: TestClient,
+        payload,
+        url,
+        generate_auth_header,
     ):
         auth_header = generate_auth_header([STORAGE_READ])
         response = api_client.post(url, headers=auth_header, json=payload)
         assert 403 == response.status_code
 
     def test_create_data_use_with_fides_key(
-            self,
-            db: Session,
-            api_client: TestClient,
-            payload,
-            url,
-            generate_auth_header,
+        self,
+        db: Session,
+        api_client: TestClient,
+        payload,
+        url,
+        generate_auth_header,
     ):
         auth_header = generate_auth_header([DATA_USE_CREATE])
         payload["fides_key"] = "test_data_use"
@@ -90,12 +89,12 @@ class TestCreateDataUse:
         data_use.delete(db)
 
     def test_create_data_use_with_no_fides_key(
-            self,
-            db: Session,
-            api_client: TestClient,
-            payload,
-            url,
-            generate_auth_header,
+        self,
+        db: Session,
+        api_client: TestClient,
+        payload,
+        url,
+        generate_auth_header,
     ):
         auth_header = generate_auth_header([DATA_USE_CREATE])
         response = api_client.post(url, headers=auth_header, json=payload)
@@ -108,13 +107,13 @@ class TestCreateDataUse:
         data_use.delete(db)
 
     def test_create_data_use_with_conflicting_key(
-            self,
-            db: Session,
-            api_client: TestClient,
-            payload,
-            url,
-            enabled_data_use,
-            generate_auth_header,
+        self,
+        db: Session,
+        api_client: TestClient,
+        payload,
+        url,
+        enabled_data_use,
+        generate_auth_header,
     ):
         auth_header = generate_auth_header([DATA_USE_CREATE])
         response = api_client.post(url, headers=auth_header, json=payload)
@@ -122,13 +121,13 @@ class TestCreateDataUse:
         assert 422 == response.status_code
 
     def test_create_data_use_with_disabled_matching_name(
-            self,
-            db: Session,
-            api_client: TestClient,
-            payload,
-            url,
-            disabled_data_use,
-            generate_auth_header,
+        self,
+        db: Session,
+        api_client: TestClient,
+        payload,
+        url,
+        disabled_data_use,
+        generate_auth_header,
     ):
         auth_header = generate_auth_header([DATA_USE_CREATE])
         response = api_client.post(url, headers=auth_header, json=payload)
