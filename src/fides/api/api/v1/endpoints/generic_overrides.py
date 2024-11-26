804db8df47f2d6fc4041b7f44ff29923af16328c
eastandwestwind <eastandwestwind@gmail.com>
2024-11-26 13:19:03 +0100

    adds support for taxonomy creation specifying a parent key

--- src/fides/api/api/v1/endpoints/generic_overrides.py
+++ src/fides/api/api/v1/endpoints/generic_overrides.py
@@ -124,29 +124,36 @@ async def create_data_use(
     """
     Create a data use. Updates existing data use if data use with name already exists and is disabled.
     """
-    if data_use.fides_key is None:
-        disabled_resource_with_name = (
-            db.query(DataUseDbModel)
-            .filter(
-                DataUseDbModel.active.is_(False),
-                DataUseDbModel.name == data_use.name,
+    try:
+        if data_use.fides_key is None:
+            disabled_resource_with_name = (
+                db.query(DataUseDbModel)
+                .filter(
+                    DataUseDbModel.active.is_(False),
+                    DataUseDbModel.name == data_use.name,
+                )
+                .first()
             )
-            .first()
-        )
-        data_use.fides_key = get_key_from_data(
-            {"key": data_use.fides_key, "name": data_use.name}, DataUse.__name__
-        )
-        if disabled_resource_with_name:
-            data_use.active = True
-            return disabled_resource_with_name.update(db, data=data_use.model_dump(mode="json"))  # type: ignore[union-attr]
-        try:
+            data_use.fides_key = get_key_from_data(
+                {"key": data_use.fides_key, "name": data_use.name}, DataUse.__name__
+            )
+            if data_use.parent_key:
+                data_use.fides_key = f"{data_use.parent_key}.{data_use.fides_key}"
+            if disabled_resource_with_name:
+                data_use.active = True
+                return disabled_resource_with_name.update(db, data=data_use.model_dump(mode="json"))  # type: ignore[union-attr]
             return DataUseDbModel.create(db=db, data=data_use.model_dump(mode="json"))  # type: ignore[union-attr]
-        except KeyOrNameAlreadyExists:
-            raise HTTPException(
-                status_code=HTTP_422_UNPROCESSABLE_ENTITY,
-                detail=f"Data use with key {data_use.fides_key} or name {data_use.name} already exists.",
-            )
-    return DataUseDbModel.create(db=db, data=data_use.model_dump(mode="json"))
+        return DataUseDbModel.create(db=db, data=data_use.model_dump(mode="json"))
+    except KeyOrNameAlreadyExists:
+        raise HTTPException(
+            status_code=HTTP_422_UNPROCESSABLE_ENTITY,
+            detail=f"Data use with key {data_use.fides_key} or name {data_use.name} already exists.",
+        )
+    except Exception as e:
+        raise HTTPException(
+            status_code=HTTP_422_UNPROCESSABLE_ENTITY,
+            detail=f"Error creating data use: {e}",
+        )
 
 
 @data_category_router.post(
@@ -164,30 +171,42 @@ async def create_data_category(
     Create a data category
     """
 
-    if data_category.fides_key is None:
-        disabled_resource_with_name = (
-            db.query(DataCategoryDbModel)
-            .filter(
-                DataCategoryDbModel.active.is_(False),
-                DataCategoryDbModel.name == data_category.name,
+    try:
+        if data_category.fides_key is None:
+            disabled_resource_with_name = (
+                db.query(DataCategoryDbModel)
+                .filter(
+                    DataCategoryDbModel.active.is_(False),
+                    DataCategoryDbModel.name == data_category.name,
+                )
+                .first()
             )
-            .first()
-        )
-        data_category.fides_key = get_key_from_data(
-            {"key": data_category.fides_key, "name": data_category.name},
-            DataCategory.__name__,
-        )
-        if disabled_resource_with_name:
-            data_category.active = True
-            return disabled_resource_with_name.update(db, data=data_category.model_dump(mode="json"))  # type: ignore[union-attr]
-        try:
+            data_category.fides_key = get_key_from_data(
+                {"key": data_category.fides_key, "name": data_category.name},
+                DataCategory.__name__,
+            )
+            if data_category.parent_key:
+                data_category.fides_key = (
+                    f"{data_category.parent_key}.{data_category.fides_key}"
+                )
+            if disabled_resource_with_name:
+                data_category.active = True
+                return disabled_resource_with_name.update(db, data=data_category.model_dump(mode="json"))  # type: ignore[union-attr]
             return DataCategoryDbModel.create(db=db, data=data_category.model_dump(mode="json"))  # type: ignore[union-attr]
-        except KeyOrNameAlreadyExists:
-            raise HTTPException(
-                status_code=HTTP_422_UNPROCESSABLE_ENTITY,
-                detail=f"Data category with key {data_category.fides_key} or name {data_category.name} already exists.",
-            )
-    return DataCategoryDbModel.create(db=db, data=data_category.model_dump(mode="json"))
+
+        return DataCategoryDbModel.create(
+            db=db, data=data_category.model_dump(mode="json")
+        )
+    except KeyOrNameAlreadyExists:
+        raise HTTPException(
+            status_code=HTTP_422_UNPROCESSABLE_ENTITY,
+            detail=f"Data category with key {data_category.fides_key} or name {data_category.name} already exists.",
+        )
+    except Exception as e:
+        raise HTTPException(
+            status_code=HTTP_422_UNPROCESSABLE_ENTITY,
+            detail=f"Error creating data category: {e}",
+        )
 
 
 @data_subject_router.post(
@@ -205,30 +224,37 @@ async def create_data_subject(
     Create a data subject
     """
 
-    if data_subject.fides_key is None:
-        disabled_resource_with_name = (
-            db.query(DataSubjectDbModel)
-            .filter(
-                DataSubjectDbModel.active.is_(False),
-                DataSubjectDbModel.name == data_subject.name,
+    try:
+        if data_subject.fides_key is None:
+            disabled_resource_with_name = (
+                db.query(DataSubjectDbModel)
+                .filter(
+                    DataSubjectDbModel.active.is_(False),
+                    DataSubjectDbModel.name == data_subject.name,
+                )
+                .first()
             )
-            .first()
-        )
-        data_subject.fides_key = get_key_from_data(
-            {"key": data_subject.fides_key, "name": data_subject.name},
-            DataSubject.__name__,
-        )
-        if disabled_resource_with_name:
-            data_subject.active = True
-            return disabled_resource_with_name.update(db, data=data_subject.model_dump(mode="json"))  # type: ignore[union-attr]
-        try:
+            data_subject.fides_key = get_key_from_data(
+                {"key": data_subject.fides_key, "name": data_subject.name},
+                DataSubject.__name__,
+            )
+            if disabled_resource_with_name:
+                data_subject.active = True
+                return disabled_resource_with_name.update(db, data=data_subject.model_dump(mode="json"))  # type: ignore[union-attr]
             return DataSubjectDbModel.create(db=db, data=data_subject.model_dump(mode="json"))  # type: ignore[union-attr]
-        except KeyOrNameAlreadyExists:
-            raise HTTPException(
-                status_code=HTTP_422_UNPROCESSABLE_ENTITY,
-                detail=f"Data subject with key {data_subject.fides_key} or name {data_subject.name} already exists.",
-            )
-    return DataSubjectDbModel.create(db=db, data=data_subject.model_dump(mode="json"))
+        return DataSubjectDbModel.create(
+            db=db, data=data_subject.model_dump(mode="json")
+        )
+    except KeyOrNameAlreadyExists:
+        raise HTTPException(
+            status_code=HTTP_422_UNPROCESSABLE_ENTITY,
+            detail=f"Data subject with key {data_subject.fides_key} or name {data_subject.name} already exists.",
+        )
+    except Exception as e:
+        raise HTTPException(
+            status_code=HTTP_422_UNPROCESSABLE_ENTITY,
+            detail=f"Error creating data subject: {e}",
+        )
 
 
 GENERIC_OVERRIDES_ROUTER = APIRouter()
