efa9fd34f97860d40a486bc2910d03d64a554163
Eliana Rosselli <eliana@ethyca.com>
2024-12-17 14:18:35 -0300

    Add DBCache model and table

---
+++ src/fides/api/models/db_cache.py
@@ -0,0 +1,47 @@
+from typing import Dict, Optional
+from enum import Enum
+
+from sqlalchemy import Column, String
+from sqlalchemy.dialects.postgresql import BYTEA
+from sqlalchemy.orm import Session
+
+
+from fides.api.db.base_class import Base
+
+
+class DBCacheNamespace(Enum):
+    """Namespaces for the DBCache"""
+
+    LIST_PRIVACY_EXPERIENCE = "list-privacy-experience"
+
+
+class DBCache(Base):
+    """
+    Cache table for storing arbitrary data, useful when in-memory caches aren't enough.
+    For example if cache contents need to be persisted across server restarts,
+    or if the cache needs to be shared across different Fides instances.
+    """
+
+    namespace = Column(
+        String, nullable=False, index=True
+    )  # Add a namespace since the same cache key could technically be used for different contexts
+    cache_key = Column(String, nullable=False, index=True)
+    cache_value = Column(BYTEA, nullable=False)
+
+    @classmethod
+    def get_cache_value(
+        cls,
+        db: Session,
+        namespace: DBCacheNamespace,
+        cache_key: str,
+    ) -> Optional[Dict]:
+        """
+        Retrieves the cache value for the given cache_key
+        """
+        cache_entry = (
+            db.query(cls)
+            .filter(cls.namespace == namespace, cls.cache_key == cache_key)
+            .first()
+        )
+
+        return cache_entry.cache_value if cache_entry else None
