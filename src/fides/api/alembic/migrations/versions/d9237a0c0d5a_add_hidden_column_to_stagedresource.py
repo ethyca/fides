df8e884673beb7eb196d284e65f448ebc29eacf3
kirk <kirk@ethyca.com>
2024-12-18 09:33:12 -0500

    change downrev on migration after main merge

--- src/fides/api/alembic/migrations/versions/d9237a0c0d5a_add_hidden_column_to_stagedresource.py
+++ src/fides/api/alembic/migrations/versions/d9237a0c0d5a_add_hidden_column_to_stagedresource.py
@@ -1,7 +1,7 @@
 """add `data_uses` columns to stagedresource
 
 Revision ID: d9237a0c0d5a
-Revises: c90d46f6d3f2
+Revises: e5ec30dfcd87
 Create Date: 2024-11-21 13:18:24.085858
 
 """
@@ -11,7 +11,7 @@ from alembic import op
 
 # revision identifiers, used by Alembic.
 revision = "d9237a0c0d5a"
-down_revision = "c90d46f6d3f2"
+down_revision = "e5ec30dfcd87"
 branch_labels = None
 depends_on = None
 
