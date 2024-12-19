63564b7819835639209bb766f3acb2c7f2d10b95
kirk <kirk@ethyca.com>
2024-12-19 16:09:54 -0500

    downrev

--- src/fides/api/alembic/migrations/versions/d9237a0c0d5a_add_data_uses_column_to_stagedresource.py
+++ src/fides/api/alembic/migrations/versions/d9237a0c0d5a_add_data_uses_column_to_stagedresource.py
@@ -1,7 +1,7 @@
 """add `data_uses` column to stagedresource
 
 Revision ID: d9237a0c0d5a
-Revises: 10c6b7709be3
+Revises: ae65da77c468
 Create Date: 2024-11-21 13:18:24.085858
 
 """
@@ -11,7 +11,7 @@ from alembic import op
 
 # revision identifiers, used by Alembic.
 revision = "d9237a0c0d5a"
-down_revision = "10c6b7709be3"
+down_revision = "ae65da77c468"
 branch_labels = None
 depends_on = None
 
