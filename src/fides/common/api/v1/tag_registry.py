0d0d762a20feab867ca182ae72b2628521d8a00d
Thomas Van Dort <tom@ethyca.com>
2025-01-10 16:10:03 -0500

    Add tag registry and global meta.

---
+++ src/fides/common/api/v1/tag_registry.py
@@ -0,0 +1,12 @@
+PUBLIC_INTERNET = "Public Internet"
+
+tags_metadata = [
+    {
+        "name": PUBLIC_INTERNET,
+        "description": "The following endpoints should be allow-listed from the public Internet to the Fides webserver.",
+        "externalDocs": {
+            "description": "Security Best Practices",
+            "url": "https://ethyca.com/docs/dev-docs/configuration/security-practices#public-internet-----fides-webserver",
+        },
+    }
+]
