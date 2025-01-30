3f14582544232b0bbb5d173eb2ec36c98986dfa8
Adrian Galvan <adriang430@gmail.com>
2025-01-29 22:46:09 -0800

    Timeout troubleshooting

--- tests/ctl/cli/test_cli.py
+++ tests/ctl/cli/test_cli.py
@@ -145,9 +145,6 @@ def test_parse(test_config_path: str, test_cli_runner: CliRunner) -> None:
 
 
 class TestDB:
-    @pytest.mark.skip(
-        "This test is timing out only in CI: Safe-Tests (3.10.13, ctl-not-external)"
-    )
     @pytest.mark.integration
     def test_reset_db(self, test_config_path: str, test_cli_runner: CliRunner) -> None:
         result = test_cli_runner.invoke(
