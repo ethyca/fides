8d8db9d59d654f5893f8271905029e0103b1a4e4
Adrian Galvan <adriang430@gmail.com>
2025-01-29 23:16:04 -0800

    Skipping test_reset_db Enabling test_filter_data_categories_excluded

--- tests/ctl/api/test_admin.py
+++ tests/ctl/api/test_admin.py
@@ -6,7 +6,6 @@ from fides.api.util.endpoint_utils import API_PREFIX
 from fides.config import FidesConfig
 
 
-@pytest.mark.skip("Troubleshooting")
 def test_db_reset_dev_mode_enabled(
     test_config: FidesConfig,
     test_client: TestClient,
@@ -22,7 +21,6 @@ def test_db_reset_dev_mode_enabled(
     }
 
 
-@pytest.mark.skip("Troubleshooting")
 def test_db_reset_dev_mode_disabled(
     test_config: FidesConfig,
     test_config_dev_mode_disabled: FidesConfig,  # temporarily switches off config.dev_mode
