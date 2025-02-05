a48253c374a6bc9a97a557d3830c52294ec3aa0a
Jeremy Pople <jeremy@ethyca.com>
2025-02-05 10:13:15 -0600

    categories of consent cell

---
+++ clients/admin-ui/src/features/data-discovery-and-detection/action-center/tables/cells/DiscoveredSystemDataUseCell.tsx
@@ -0,0 +1,30 @@
+import useTaxonomies from "~/features/common/hooks/useTaxonomies";
+import { BadgeCellExpandable } from "~/features/common/table/v2/cells";
+import { MonitorSystemAggregate } from "~/features/data-discovery-and-detection/action-center/types";
+import isConsentCategory from "~/features/data-discovery-and-detection/action-center/utils/isConsentCategory";
+
+const DiscoveredSystemDataUseCell = ({
+  system,
+}: {
+  system: MonitorSystemAggregate;
+}) => {
+  const { getDataUseDisplayName } = useTaxonomies();
+  const consentCategories = system.data_uses.filter((use) =>
+    isConsentCategory(use),
+  );
+  const cellValues = consentCategories.map((use) => ({
+    label: getDataUseDisplayName(use),
+    key: use,
+  }));
+
+  return (
+    <BadgeCellExpandable
+      values={cellValues}
+      bgColor="white"
+      borderWidth="1px"
+      borderColor="gray.200"
+    />
+  );
+};
+
+export default DiscoveredSystemDataUseCell;
