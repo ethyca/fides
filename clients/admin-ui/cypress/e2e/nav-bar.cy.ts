dcb61add68d91256cef638e85f0383ad9a7f16a2
Lucano Vera <lucanovera@live.com.ar>
2025-02-11 13:55:09 -0300

    Update tests

--- clients/admin-ui/cypress/e2e/nav-bar.cy.ts
+++ clients/admin-ui/cypress/e2e/nav-bar.cy.ts
@@ -8,50 +8,68 @@ describe("Nav Bar", () => {
   it("renders all navigation groups with links inside", () => {
     cy.visit("/");
 
-    cy.get("[id^='accordion-button']").should("have.length", 4);
-    cy.getByTestId("Overview-nav-group").within(() => {
-      cy.getByTestId("Home-nav-link");
-    });
-    cy.getByTestId("Data inventory-nav-group").within(() => {
-      cy.getByTestId("System inventory-nav-link");
-      cy.getByTestId("Add systems-nav-link");
-      cy.getByTestId("Manage datasets-nav-link");
-    });
-    cy.getByTestId("Privacy requests-nav-group").within(() => {
-      cy.getByTestId("Request manager-nav-link");
-      cy.getByTestId("Connection manager-nav-link");
-    });
-    cy.getByTestId("Settings-nav-group").within(() => {
-      cy.getByTestId("Users-nav-link");
-      cy.getByTestId("Organization-nav-link");
-      cy.getByTestId("Taxonomy-nav-link");
-      cy.getByTestId("About Fides-nav-link");
-    });
+    cy.get(".ant-menu-submenu-title").should("have.length", 4);
+    cy.getByTestId("Overview-nav-group")
+      .click()
+      .parents(".ant-menu-submenu")
+      .within(() => {
+        cy.getByTestId("Home-nav-link");
+      });
+    cy.getByTestId("Data inventory-nav-group")
+      .click()
+      .parents(".ant-menu-submenu")
+      .within(() => {
+        cy.getByTestId("System inventory-nav-link");
+        cy.getByTestId("Add systems-nav-link");
+        cy.getByTestId("Manage datasets-nav-link");
+      });
+    cy.getByTestId("Privacy requests-nav-group")
+      .click()
+      .parents(".ant-menu-submenu")
+      .within(() => {
+        cy.getByTestId("Request manager-nav-link");
+        cy.getByTestId("Connection manager-nav-link");
+      });
+    cy.getByTestId("Settings-nav-group")
+      .click()
+      .parents(".ant-menu-submenu")
+      .within(() => {
+        cy.getByTestId("Users-nav-link");
+        cy.getByTestId("Organization-nav-link");
+        cy.getByTestId("Taxonomy-nav-link");
+        cy.getByTestId("About Fides-nav-link");
+      });
   });
 
   it("renders the Consent and Detection & Discovery navs with Plus", () => {
     stubPlus(true);
     cy.visit("/");
 
-    cy.get("[id^='accordion-button']").should("have.length", 6);
-    cy.getByTestId("Detection & Discovery-nav-group").within(() => {
-      cy.getByTestId("Activity-nav-link").should("exist");
-      cy.getByTestId("Data detection-nav-link").should("exist");
-      cy.getByTestId("Data discovery-nav-link").should("exist");
-    });
+    cy.get(".ant-menu-submenu-title").should("have.length", 6);
+    cy.getByTestId("Detection & Discovery-nav-group")
+      .parents(".ant-menu-submenu")
+      .within(() => {
+        cy.getByTestId("Activity-nav-link").should("exist");
+        cy.getByTestId("Data detection-nav-link").should("exist");
+        cy.getByTestId("Data discovery-nav-link").should("exist");
+      });
   });
 
   it("styles the active navigation link based on the current route", () => {
-    const ACTIVE_COLOR = "rgb(206, 202, 194)";
+    const ACTIVE_COLOR = "rgb(43, 46, 53)";
     // Start on the Home page
     cy.visit("/");
 
     // The nav should reflect the active page.
+    cy.getByTestId("Overview-nav-group").click();
+
     cy.getByTestId("Home-nav-link")
-      .should("have.css", "background-color")
+      .should("have.css", "color")
       .should("eql", ACTIVE_COLOR);
+
+    cy.getByTestId("Data inventory-nav-group").click();
     cy.getByTestId("System inventory-nav-link")
-      .should("have.css", "background-color")
+      .should("have.css", "color")
       .should("not.eql", ACTIVE_COLOR);
 
     // Navigate by clicking a nav link.
@@ -59,22 +77,22 @@ describe("Nav Bar", () => {
 
     // The nav should update which page is active.
     cy.getByTestId("Home-nav-link")
-      .should("have.css", "background-color")
+      .should("have.css", "color")
       .should("not.eql", ACTIVE_COLOR);
     cy.getByTestId("System inventory-nav-link")
-      .should("have.css", "background-color")
+      .should("have.css", "color")
       .should("eql", ACTIVE_COLOR);
   });
 
   it("can collapse nav groups and persist across page views", () => {
     cy.visit("/");
+    cy.getByTestId("Privacy requests-nav-group").click();
     cy.getByTestId("Request manager-nav-link").should("be.visible");
-    cy.getByTestId("Privacy requests-nav-group").within(() => {
-      cy.get("button").click();
-    });
+    cy.getByTestId("Privacy requests-nav-group").click();
     cy.getByTestId("Request manager-nav-link").should("not.be.visible");
 
     // Move to another page
+    cy.getByTestId("Data inventory-nav-group").click();
     cy.getByTestId("System inventory-nav-link").click();
     cy.getByTestId("Request manager-nav-link").should("not.be.visible");
   });
