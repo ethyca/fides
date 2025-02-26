fb43875591f5b4a6bb7f79515b9e9e2ecf4e00eb
Thomas Van Dort <tom@ethyca.com>
2025-02-26 12:51:05 -0500

    Set default for dbname.

--- src/fides/api/schemas/connection_configuration/connection_secrets_mysql.py
+++ src/fides/api/schemas/connection_configuration/connection_secrets_mysql.py
@@ -32,6 +32,7 @@ class MySQLSchema(ConnectionConfigSecretsSchema):
         json_schema_extra={"sensitive": True},
     )
     dbname: Optional[str] = Field(
+        default=None,
         title="Database",
         description="The name of the specific database within the database server that you want to connect to.",
     )
