87d7d8a67f8325013b60539333f24436d8c8d565
kirk <kirk@ethyca.com>
2025-02-27 08:40:05 -0500

    migration merged ahead of this one

--- src/fides/api/alembic/migrations/versions/c9c72b3d550b_data_migration_converting_existing_.py
+++ src/fides/api/alembic/migrations/versions/c9c72b3d550b_data_migration_converting_existing_.py
@@ -1,7 +1,7 @@
 """data migration converting existing 'Cookies' resources to 'Asset' resources with type 'Cookie'
 
 Revision ID: c9c72b3d550b
-Revises: c961528edfc6
+Revises: 82883b0df5e4
 Create Date: 2025-02-18 18:33:56.039924
 
 """
@@ -18,7 +18,7 @@ from fides.api.models.asset import Asset
 
 # revision identifiers, used by Alembic.
 revision = "c9c72b3d550b"
-down_revision = "c961528edfc6"
+down_revision = "82883b0df5e4"
 branch_labels = None
 depends_on = None
 
