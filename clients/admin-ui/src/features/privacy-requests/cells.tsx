b6bc781eb5c5b0ba90cd0ad50d6cd04e0d232f75
Kelsey Thomas <101993653+Kelsey-Ethyca@users.noreply.github.com>
2025-03-06 07:02:03 +0100

    fix Days Left badge color

--- clients/admin-ui/src/features/privacy-requests/cells.tsx
+++ clients/admin-ui/src/features/privacy-requests/cells.tsx
@@ -97,7 +97,7 @@ export const RequestDaysLeftCell = ({
   } else if (percentage >= 75) {
     colorScheme = "success";
   } else if (percentage >= 25) {
-    colorScheme = "warn";
+    colorScheme = "warning";
   }
 
   return (
