9131a4028ac6aa41440bec12872b3535abdaca45
Adrian Galvan <adrian@ethyca.com>
2025-03-06 16:12:17 -0800

    Adding support for optional identities (#5836)

--- clients/admin-ui/src/features/privacy-requests/events-and-logs/EventDetail.tsx
+++ clients/admin-ui/src/features/privacy-requests/events-and-logs/EventDetail.tsx
@@ -1,12 +1,28 @@
 import ClipboardButton from "common/ClipboardButton";
 import { AntTag as Tag, Box, Divider, Flex, Text } from "fidesui";
+import { ExecutionLogStatus } from "privacy-requests/types";
 import React from "react";
 
-type EventErrorProps = {
+type EventDetailProps = {
   errorMessage: string;
+  status?: ExecutionLogStatus;
 };
 
-const EventError = ({ errorMessage }: EventErrorProps) => (
+const getTagColorForStatus = (status?: ExecutionLogStatus) => {
+  switch (status) {
+    case ExecutionLogStatus.SKIPPED:
+      return "warning";
+    case ExecutionLogStatus.ERROR:
+      return "error";
+    default:
+      return "error"; // fallback to error
+  }
+};
+
+const EventDetail = ({
+  errorMessage,
+  status = ExecutionLogStatus.ERROR,
+}: EventDetailProps) => (
   <Box height="100%" id="outer">
     <Flex alignItems="center" paddingBottom="8px">
       <Text
@@ -18,7 +34,7 @@ const EventError = ({ errorMessage }: EventErrorProps) => (
       >
         Status
       </Text>
-      <Tag color="error">Error</Tag>
+      <Tag color={getTagColorForStatus(status)}>{status}</Tag>
       <Box padding="0px" marginBottom="3px">
         <ClipboardButton copyText={errorMessage} />
       </Box>
@@ -30,4 +46,4 @@ const EventError = ({ errorMessage }: EventErrorProps) => (
   </Box>
 );
 
-export default EventError;
+export default EventDetail;
