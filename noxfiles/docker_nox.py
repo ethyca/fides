57be88bb10f146ad2421da88a05f90da95826017
Adrian Galvan <adrian@ethyca.com>
2025-03-12 19:47:52 -0700

    Testing publishing Docker images

--- noxfiles/docker_nox.py
+++ noxfiles/docker_nox.py
@@ -63,6 +63,8 @@ def generate_buildx_command(
         "--platform",
         DOCKER_PLATFORMS,
         dockerfile_path,
+        "--cache-from=type=gha",
+        "--cache-to=type=gha,mode=max",
     )
 
     for tag in image_tags:
