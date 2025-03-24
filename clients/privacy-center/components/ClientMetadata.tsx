0ca3bdcece4d40eddb62d8c67c4b613ff502c8ab
Lucano Vera <lucanovera@live.com.ar>
2025-03-24 12:37:38 -0300

    Remove description

--- clients/privacy-center/components/ClientMetadata.tsx
+++ clients/privacy-center/components/ClientMetadata.tsx
@@ -1,15 +1,12 @@
 const ClientMetadata = ({
   title = "Privacy Center",
-  description = "Privacy Center",
   icon,
 }: {
   title?: string;
-  description: string;
   icon?: string | null;
 }) => (
   <>
     <title>{title}</title>
-    <meta name="description" content={description} />
     <link rel="icon" href={icon || "/favicon.ico"} />
   </>
 );
