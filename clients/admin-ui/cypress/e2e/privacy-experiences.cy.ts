ffa812e4734fe4da486f27b7489d0d7b4962257e
Lucano Vera <lucanovera@live.com.ar>
2025-03-26 17:39:28 -0300

    Add tests for tcf

--- clients/admin-ui/cypress/e2e/privacy-experiences.cy.ts
+++ clients/admin-ui/cypress/e2e/privacy-experiences.cy.ts
@@ -21,14 +21,6 @@ import {
 const EXPERIENCE_ID = "pri_0338d055-f91b-4a17-ad4e-600c61551199";
 const DISABLED_EXPERIENCE_ID = "pri_8fd9d334-e625-4365-ba25-9c368f0b1231";
 
-// Helper function to get required fields for a component type
-const getRequiredFields = (component: ComponentType): string[] => {
-  const formFields = getTranslationFormFields(component);
-  return Object.entries(formFields)
-    .filter(([_, config]) => config.included && config.required)
-    .map(([field]) => field);
-};
-
 describe("Privacy experiences", () => {
   beforeEach(() => {
     cy.login();
@@ -443,6 +435,40 @@ describe("Privacy experiences", () => {
           cy.url().should("match", /privacy-experience$/);
           cy.getByTestId("toast-success-msg").should("exist");
         });
+      });
+
+      it("Can add translations to a TCF experience after filling required fields", () => {
+        // Intercept the TCF experience response
+        cy.intercept("GET", "/api/v1/experience-config/*", {
+          fixture: "privacy-experiences/tcf-experience.json",
+        }).as("getTCFExperience");
+
+        // Visit the TCF experience page
+        cy.visit(
+          `${PRIVACY_EXPERIENCE_ROUTE}/pri_f5eb2be4-95e3-45cc-9c82-9a4bd0d64182`,
+        );
+        cy.wait("@getTCFExperience");
+
+        // Add new translation
+        cy.getByTestId("add-language").click();
+        cy.getByTestId("select-language").antSelect("French (Canada)");
+
+        // Fill out all required fields with 'Test'
+        const typeOptions = { delay: 50 };
+        cy.getByTestId("privacy-experience-detail-page")
+          .find("input[required], textarea[required]")
+          .each(($input) => {
+            cy.wrap($input).type("Test", typeOptions);
+          });
+
+        // Verify save button is enabled
+        cy.getByTestId("save-btn").should("not.be.disabled");
+
+        // Save the translation
+        cy.getByTestId("save-btn").click();
+
+        // Verify the translation was added
+        cy.getByTestId("language-row-fr-CA").should("exist");
       });
     });
   });
