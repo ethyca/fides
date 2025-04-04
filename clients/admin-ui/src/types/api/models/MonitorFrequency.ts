3b377cb6f42dd042da3d991e1e36496698faeb73
Adam Sachs <adam@ethyca.com>
2025-04-04 13:21:02 -0400

    update monitor frequency ts enum

--- clients/admin-ui/src/types/api/models/MonitorFrequency.ts
+++ clients/admin-ui/src/types/api/models/MonitorFrequency.ts
@@ -9,5 +9,7 @@ export enum MonitorFrequency {
   DAILY = "Daily",
   WEEKLY = "Weekly",
   MONTHLY = "Monthly",
+  QUARTERLY = "Quarterly",
+  YEARLY = "Yearly",
   NOT_SCHEDULED = "Not scheduled",
 }
