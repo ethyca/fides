5c2f4ebd6608f525a83ce4630cef2d8a14a5c272
Adrian Galvan <adriang430@gmail.com>
2025-04-06 00:27:23 -0700

    Removing unused function

--- src/fides/api/api/v1/endpoints/privacy_request_endpoints.py
+++ src/fides/api/api/v1/endpoints/privacy_request_endpoints.py
@@ -1857,35 +1857,6 @@ def resume_privacy_request_from_requires_input(
 
     return privacy_request  # type: ignore[return-value]
 
-def _process_privacy_request_restart(
-    privacy_request: PrivacyRequest,
-    failed_step: Optional[CurrentStep],
-    db: Session,
-) -> PrivacyRequestResponse:
-    """If failed_step is provided, restart the DSR within that step. Otherwise,
-    restart the privacy request from the beginning."""
-    if failed_step:
-        logger.info(
-            "Restarting failed privacy request '{}' from '{}'",
-            privacy_request.id,
-            failed_step,
-        )
-    else:
-        logger.info(
-            "Restarting failed privacy request '{}' from the beginning",
-            privacy_request.id,
-        )
-
-    privacy_request.status = PrivacyRequestStatus.in_processing
-    privacy_request.save(db=db)
-    queue_privacy_request(
-        privacy_request_id=privacy_request.id,
-        from_step=failed_step.value if failed_step else None,
-    )
-
-    return privacy_request  # type: ignore[return-value]
-
-
 @router.get(
     REQUEST_TASKS,
     dependencies=[Security(verify_oauth_client, scopes=[PRIVACY_REQUEST_READ])],
