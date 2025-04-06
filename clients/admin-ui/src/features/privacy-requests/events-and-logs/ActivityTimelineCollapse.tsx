e8dbb0d4bfd4679f544cc2d3560e822fb1eb8056
Lucano Vera <lucanovera@live.com.ar>
2025-04-06 19:17:26 -0300

    WIP

--- clients/admin-ui/src/features/privacy-requests/events-and-logs/ActivityTimelineCollapse.tsx
+++ clients/admin-ui/src/features/privacy-requests/events-and-logs/ActivityTimelineCollapse.tsx
@@ -1,3 +1,4 @@
+import classNames from "classnames";
 import {
   AntCollapse as Collapse,
   AntCollapseProps as CollapseProps,
@@ -6,6 +7,7 @@ import {
 import { map } from "lodash";
 
 import { formatDate } from "~/features/common/utils";
+import { ExecutionLogStatus } from "~/types/api";
 
 import { ExecutionLog } from "../types";
 import styles from "./ActivityTimelineCollapse.module.scss";
@@ -16,25 +18,41 @@ interface ActivityTimelineProps {
 }
 
 const ActivityTimelineCollapse = ({ results }: ActivityTimelineProps) => {
-  const items: CollapseProps["items"] = map(results, (values, key) => ({
-    key,
-    label: (
-      <div className={styles.headerInner}>
-        <span className={styles.author}>Fides:</span>
-        <span className={styles.title}>{key}</span>
-        <span className={styles.timestamp}>
-          {formatDate(values[0].updated_at)}
-        </span>
-        <Tag color="sandstone">Request update</Tag>
-      </div>
-    ),
-    children: <EventLog eventLogs={values} openErrorPanel={() => {}} />,
-    className: styles.item,
-    classNames: {
-      header: styles.header,
-      body: "test2",
-    },
-  }));
+  const items: CollapseProps["items"] = map(results, (values, key) => {
+    const isError = values.some(
+      (value) => value.status === ExecutionLogStatus.ERROR,
+    );
+
+    return {
+      key,
+      label: (
+        <div className={styles.headerInner}>
+          <span className={styles.author}>Fides:</span>
+          <span
+            className={classNames(styles.title, {
+              [styles["title--error"]]: isError,
+            })}
+          >
+            {key}
+            {isError && " failed"}
+          </span>
+          <span className={styles.timestamp}>
+            {formatDate(values[0].updated_at)}
+          </span>
+          <Tag color="sandstone">Request update</Tag>
+          {isError && <span className={styles.viewLogs}>View Log</span>}
+        </div>
+      ),
+      children: <EventLog eventLogs={values} openErrorPanel={() => {}} />,
+      className: classNames(styles.item, {
+        [styles["item--error"]]: isError,
+      }),
+      classNames: {
+        header: styles.header,
+        body: "test2",
+      },
+    };
+  });
   return (
     <Collapse
       items={items}
