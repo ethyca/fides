40497b45b7680f8534ea18ef499f36687ca8cff0
Jason Gill <jason.gill@ethyca.com>
2025-04-07 10:43:40 -0600

    auto-generate related TS Types

---
+++ clients/admin-ui/src/types/api/models/TCFConfigurationResponse.ts
@@ -0,0 +1,17 @@
+/* istanbul ignore file */
+/* tslint:disable */
+/* eslint-disable */
+
+/**
+ * Schema for TCF Configuration responses
+ */
+export type TCFConfigurationResponse = {
+  /**
+   * The unique identifier of the TCF Configuration
+   */
+  id: string;
+  /**
+   * The unique name of the TCF Configuration
+   */
+  name: string;
+};
