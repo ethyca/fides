49b43bdde5d273fd9ab44d783e1ddde4e995a45b
bruno <bruno.gutierrez96@gmail.com>
2025-04-07 15:49:19 -0400

    running static checks

--- clients/fides-js/src/services/api.ts
+++ clients/fides-js/src/services/api.ts
@@ -60,9 +60,7 @@ export const fetchExperience = async <T = PrivacyExperience>({
     }
   }
 
-  const headers = [
-    ["Accept-Encoding", "gzip, deflate"],
-  ];
+  const headers = [["Accept-Encoding", "gzip, deflate"]];
   if (userLanguageString) {
     headers.push(["Accept-Language", userLanguageString]);
   }
