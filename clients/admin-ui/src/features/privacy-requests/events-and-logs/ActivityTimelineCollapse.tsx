d163e91166adda60c06daa314cf712ba081dcd89
Lucano Vera <lucanovera@live.com.ar>
2025-04-07 14:17:02 -0300

    fix ts

--- clients/admin-ui/src/features/privacy-requests/events-and-logs/ActivityTimelineCollapse.tsx
+++ clients/admin-ui/src/features/privacy-requests/events-and-logs/ActivityTimelineCollapse.tsx
@@ -14,7 +14,7 @@ import styles from "./ActivityTimelineCollapse.module.scss";
 import EventLog from "./EventLog";
 
 interface ActivityTimelineProps {
-  results: PrivacyRequestResults;
+  results?: PrivacyRequestResults;
 }
 
 const ActivityTimelineCollapse = ({ results }: ActivityTimelineProps) => {
