c8a4b1a8fde51f98b09abdfea0dc6b7615326bbf
Victoria Cruces <victoria@ethyca.com>
2025-04-09 11:08:14 -0300

    HA-524 - Unit tests for upload_to_gcs fuction

---
+++ tests/ops/tasks/test_storage.py
@@ -0,0 +1,104 @@
+from unittest.mock import MagicMock, patch
+
+import pytest
+
+from fides.api.schemas.storage.storage import ResponseFormat
+from fides.api.tasks.storage import upload_to_gcs
+from fides.config import CONFIG
+
+
+class TestUploadToGCS:
+    @patch("fides.api.tasks.storage.write_to_in_memory_buffer")
+    @patch("fides.api.tasks.storage.get_gcs_client")
+    def test_upload_to_gcs_success(
+        self, mock_get_gcs_client, mock_write_to_in_memory_buffer
+    ):
+        mock_storage_client = MagicMock()
+        mock_bucket = MagicMock()
+        mock_blob = MagicMock()
+        mock_in_memory_file = MagicMock()
+
+        mock_get_gcs_client.return_value = mock_storage_client
+        mock_storage_client.bucket.return_value = mock_bucket
+        mock_bucket.blob.return_value = mock_blob
+        mock_write_to_in_memory_buffer.return_value = mock_in_memory_file
+        mock_blob.generate_signed_url.return_value = "http://example.com/signed-url"
+
+        privacy_request = MagicMock(id="test-request-id")
+
+        result = upload_to_gcs(
+            storage_secrets={"private_key_id": "test-private-key-id"},
+            data={"key": "value"},
+            bucket_name="test-bucket",
+            file_key="test-file",
+            resp_format=ResponseFormat.json.value,
+            privacy_request=privacy_request,
+            auth_method="test-auth-method",
+        )
+
+        mock_get_gcs_client.assert_called_once_with(
+            "test-auth-method", {"private_key_id": "test-private-key-id"}
+        )
+        mock_storage_client.bucket.assert_called_once_with("test-bucket")
+        mock_bucket.blob.assert_called_once_with("test-file")
+        mock_write_to_in_memory_buffer.assert_called_once_with(
+            ResponseFormat.json.value, {"key": "value"}, privacy_request
+        )
+        mock_blob.upload_from_string.assert_called_once_with(
+            mock_in_memory_file.getvalue(), content_type="application/json"
+        )
+        mock_blob.generate_signed_url.assert_called_once_with(
+            version="v4",
+            expiration=CONFIG.security.subject_request_download_link_ttl_seconds,
+            method="GET",
+        )
+        assert result == "http://example.com/signed-url"
+
+    @patch("fides.api.tasks.storage.logger")
+    @patch("fides.api.tasks.storage.write_to_in_memory_buffer")
+    @patch("fides.api.tasks.storage.get_gcs_client")
+    def test_upload_to_gcs_exception(
+        self, mock_get_gcs_client, mock_write_to_in_memory_buffer, mock_logger
+    ):
+        mock_storage_client = MagicMock()
+        mock_bucket = MagicMock()
+        mock_blob = MagicMock()
+        mock_in_memory_file = MagicMock()
+
+        mock_get_gcs_client.return_value = mock_storage_client
+        mock_storage_client.bucket.return_value = mock_bucket
+        mock_bucket.blob.return_value = mock_blob
+        mock_write_to_in_memory_buffer.return_value = mock_in_memory_file
+        error = Exception("Upload failed")
+        mock_blob.upload_from_string.side_effect = error
+
+        privacy_request = MagicMock(id="test-request-id")
+
+        with pytest.raises(Exception) as excinfo:
+            upload_to_gcs(
+                storage_secrets={"private_key_id": "test-private-key-id"},
+                data={"key": "value"},
+                bucket_name="test-bucket",
+                file_key="test-file",
+                resp_format=ResponseFormat.csv.value,
+                privacy_request=privacy_request,
+                auth_method="test-auth-method",
+            )
+
+        mock_get_gcs_client.assert_called_once_with(
+            "test-auth-method", {"private_key_id": "test-private-key-id"}
+        )
+        mock_storage_client.bucket.assert_called_once_with("test-bucket")
+        mock_bucket.blob.assert_called_once_with("test-file")
+        mock_write_to_in_memory_buffer.assert_called_once_with(
+            ResponseFormat.csv.value, {"key": "value"}, privacy_request
+        )
+        mock_blob.upload_from_string.assert_called_once_with(
+            mock_in_memory_file.getvalue(), content_type="application/zip"
+        )
+        mock_blob.generate_signed_url.assert_not_called()
+        mock_logger.error.assert_called_once_with(
+            "Encountered error while uploading and generating link for Google Cloud Storage object: {}",
+            error,
+        )
+        assert "Upload failed" in str(excinfo.value)
