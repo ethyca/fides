6c99b2ff4a501cb9fef7fcd9d31785dad469475c
Thomas Van Dort <tom@ethyca.com>
2025-04-09 01:51:54 -0400

    Postgresql monitor.

--- clients/admin-ui/src/features/integrations/add-integration/allIntegrationTypes.tsx
+++ clients/admin-ui/src/features/integrations/add-integration/allIntegrationTypes.tsx
@@ -8,6 +8,7 @@ import GOOGLE_CLOUD_SQL_MYSQL_TYPE_INFO from "~/features/integrations/integratio
 import GOOGLE_CLOUD_SQL_POSTGRES_TYPE_INFO from "~/features/integrations/integration-type-info/googleCloudSQLPostgresInfo";
 import MICROSOFT_SQL_SERVER_TYPE_INFO from "~/features/integrations/integration-type-info/microsoftSQLServerInfo";
 import MYSQL_TYPE_INFO from "~/features/integrations/integration-type-info/mySQLInfo";
+import POSTGRES_TYPE_INFO from "~/features/integrations/integration-type-info/postgreSQLInfo";
 import RDS_MYSQL_TYPE_INFO from "~/features/integrations/integration-type-info/rdsMySQLInfo";
 import RDS_POSTGRES_TYPE_INFO from "~/features/integrations/integration-type-info/rdsPostgresInfo";
 import S3_TYPE_INFO from "~/features/integrations/integration-type-info/s3Info";
@@ -42,7 +43,11 @@ const INTEGRATION_TYPE_MAP: { [K in ConnectionType]?: IntegrationTypeInfo } = {
   [ConnectionType.SCYLLA]: SCYLLA_TYPE_INFO,
   [ConnectionType.SNOWFLAKE]: SNOWFLAKE_TYPE_INFO,
   [ConnectionType.MYSQL]: MYSQL_TYPE_INFO,
+<<<<<<< Updated upstream
   [ConnectionType.WEBSITE]: WEBSITE_INTEGRATION_TYPE_INFO,
+=======
+  [ConnectionType.POSTGRES]: POSTGRES_TYPE_INFO,
+>>>>>>> Stashed changes
 };
 
 export const INTEGRATION_TYPE_LIST: IntegrationTypeInfo[] =
