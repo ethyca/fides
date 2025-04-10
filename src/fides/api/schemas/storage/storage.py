e4ce3415e4d3fb6d53e685e388016d705c55391a
Thomas Van Dort <tom@ethyca.com>
2025-04-10 00:29:56 -0400

    Don't import things we don't need.

--- src/fides/api/schemas/storage/storage.py
+++ src/fides/api/schemas/storage/storage.py
@@ -1,5 +1,5 @@
 from enum import Enum
-from typing import Any, Dict, List, Optional, Union,
+from typing import Any, Dict, List, Optional, Union
 
 from fideslang.validation import FidesKey
 from pydantic import (
