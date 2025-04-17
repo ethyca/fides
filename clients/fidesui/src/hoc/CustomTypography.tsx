039406e741dff435b1fc89197200812681d51130
Jason Gill <jason.gill@ethyca.com>
2025-04-17 15:06:47 -0600

    custom paragraph

--- clients/fidesui/src/hoc/CustomTypography.tsx
+++ clients/fidesui/src/hoc/CustomTypography.tsx
@@ -1,9 +1,26 @@
 import type { TypographyProps } from "antd/lib";
 import { Typography } from "antd/lib";
+import classNames from "classnames";
+
+import styles from "./CustomTypography.module.scss";
 
 export interface CustomTypographyProps extends TypographyProps {
   children?: React.ReactNode;
 }
+
+// Custom wrapper for Paragraph to add paragraph role
+const CustomParagraph = (
+  props: React.ComponentProps<typeof Typography.Paragraph>,
+) => {
+  const { className, ...rest } = props;
+  return (
+    <Typography.Paragraph
+      role="paragraph"
+      className={classNames(styles.paragraph, className)}
+      {...rest}
+    />
+  );
+};
 
 export const withCustomProps = (WrappedComponent: typeof Typography) => {
   const WrappedTypography = ({ ...props }: CustomTypographyProps) => {
@@ -13,7 +30,7 @@ export const withCustomProps = (WrappedComponent: typeof Typography) => {
   // Preserve all Typography subcomponents
   WrappedTypography.Title = Typography.Title;
   WrappedTypography.Text = Typography.Text;
-  WrappedTypography.Paragraph = Typography.Paragraph;
+  WrappedTypography.Paragraph = CustomParagraph;
   WrappedTypography.Link = Typography.Link;
 
   return WrappedTypography;
