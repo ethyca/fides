f7c52b1803c48e287b6c7d28d7121367c9c9a1cd
Andrés Torres Marroquín <andres.torres.marroquin@gmail.com>
2025-05-05 12:37:49 -0600

    QA

--- clients/admin-ui/src/features/integrations/integration-type-info/oktaInfo.tsx
+++ clients/admin-ui/src/features/integrations/integration-type-info/oktaInfo.tsx
@@ -10,6 +10,15 @@ export const OKTA_INTEGRATION_PLACEHOLDER = {
   created_at: "",
 };
 
+export const OKTA_DESCRIPTION = (
+  <>
+    SSO providers manage user authentication and can help identify systems
+    within your infrastructure. Adding an SSO provider as a data source allows
+    you to detect connected systems, monitor access patterns, and enhance your
+    data map for better visibility and control.
+  </>
+);
+
 const OktaIntegrationOverview = () => (
   <>
     <InfoHeading text="Overview" />
@@ -26,9 +35,10 @@ const OKTA_TAGS = ["Discovery", "Inventory"];
 
 const OKTA_INTEGRATION_TYPE_INFO = {
   placeholder: OKTA_INTEGRATION_PLACEHOLDER,
-  category: ConnectionCategory.DATABASE,
+  category: ConnectionCategory.IDENTITY_PROVIDER,
   tags: OKTA_TAGS,
   overview: <OktaIntegrationOverview />,
+  description: OKTA_DESCRIPTION,
 };
 
 export default OKTA_INTEGRATION_TYPE_INFO;
