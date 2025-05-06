66cb1cd1227f77688995986c7f58bb968be17d16
Lucano Vera <lucanovera@live.com.ar>
2025-05-06 17:09:34 -0300

    update connection response type with new property

--- clients/admin-ui/src/types/api/models/ConnectionConfigurationResponse.ts
+++ clients/admin-ui/src/types/api/models/ConnectionConfigurationResponse.ts
@@ -27,4 +27,5 @@ export type ConnectionConfigurationResponse = {
   last_test_succeeded?: boolean | null;
   authorized?: boolean | null;
   enabled_actions?: Array<ActionType> | null;
+  system_id?: string | null;
 };
