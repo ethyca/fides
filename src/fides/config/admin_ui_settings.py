d5b67b3adf993cc5bb2fae36a6f8be71aa8ae124
Thomas Van Dort <tom@ethyca.com>
2025-05-06 14:41:50 -0400

    Unused import.

--- src/fides/config/admin_ui_settings.py
+++ src/fides/config/admin_ui_settings.py
@@ -1,7 +1,7 @@
 from enum import Enum
 from typing import Optional
 
-from pydantic import Field, SerializeAsAny, field_validator
+from pydantic import Field, SerializeAsAny
 from pydantic_settings import SettingsConfigDict
 
 from fides.api.custom_types import AnyHttpUrlStringRemovesSlash
