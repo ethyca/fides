703ffdc4de1fdbf2201fe89601c009bc7c33d971
wadesdev <william.ades94@gmail.com>
2025-05-13 14:52:17 -0400

    Rebasing after committing CLI results

--- src/fides/api/service/connectors/query_configs/mysql_query_config.py
+++ src/fides/api/service/connectors/query_configs/mysql_query_config.py
@@ -1,6 +1,4 @@
-from typing import Any, Dict, List, Optional
-
-from sqlalchemy.sql.elements import TextClause
+from typing import List
 
 from fides.api.service.connectors.query_configs.query_config import SQLQueryConfig
 
@@ -9,29 +7,6 @@ class MySQLQueryConfig(SQLQueryConfig):
     """
     Generates SQL valid for MySQL
     """
-
-    def generate_raw_query(
-        self, field_list: List[str], filters: Dict[str, List[Any]]
-    ) -> Optional[TextClause]:
-        formatted_field_list = [f"`{field}`" for field in field_list]
-        raw_query = super().generate_raw_query(formatted_field_list, filters)
-        return raw_query  # type: ignore
-
-    def format_clause_for_query(
-        self, string_path: str, operator: str, operand: str
-    ) -> str:
-        """
-        Formats a clause for a MySQL query with backticks in case the string_path or operand is a
-        reserved word.
-        """
-        if operator == "IN":
-            return f"{string_path} IN ({operand})"
-        if string_path == operand:
-            string_path = f"`{string_path}`"
-            operand = f"`{operand}`"
-        else:
-            string_path = f"`{string_path}`"
-        return f"{string_path} {operator} :{operand}"
 
     def get_formatted_query_string(
         self,
