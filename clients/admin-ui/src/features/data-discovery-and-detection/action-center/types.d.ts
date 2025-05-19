b22b87e82bd9a2aa3ebe3b21b75ea215ed784422
Jeremy Pople <jeremy@ethyca.com>
2025-05-19 13:29:10 -0500

    update properties used for promotion 'view' links

--- clients/admin-ui/src/features/data-discovery-and-detection/action-center/types.d.ts
+++ clients/admin-ui/src/features/data-discovery-and-detection/action-center/types.d.ts
@@ -14,17 +14,3 @@ export interface MonitorAggregatedResults {
 
 export interface MonitorSummaryPaginatedResponse
   extends PaginatedResponse<MonitorAggregatedResults> {}
-
-export interface MonitorSystemAggregate {
-  id: string;
-  name: string;
-  system_key: string | null; // null when the system is not a known system
-  vendor_id: string;
-  total_updates: 0;
-  data_uses: string[];
-  locations: string[];
-  domains: string[];
-}
-
-export interface MonitorSystemAggregatePaginatedResponse
-  extends PaginatedResponse<MonitorSystemAggregate> {}
--- clients/admin-ui/src/types/api/models/UserResponseExtended.ts
+++ clients/admin-ui/src/types/api/models/UserResponseExtended.ts
@@ -3,9 +3,9 @@
 /* eslint-disable */
 
 /**
- * Response after requesting a User
+ * Extended UserResponse schema to allow specifying if password login is enabled.
  */
-export type UserResponse = {
+export type UserResponseExtended = {
   id: string;
   username: string;
   created_at: string;
@@ -14,4 +14,8 @@ export type UserResponse = {
   last_name?: string | null;
   disabled?: boolean | null;
   disabled_reason?: string | null;
+  /**
+   * Whether password login is enabled for the user.
+   */
+  password_login_enabled?: boolean | null;
 };
