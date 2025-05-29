5e3c602fdc3191a4baae81514238cb7767b3e5ca
Lucano Vera <lucanovera@live.com.ar>
2025-05-29 19:08:17 -0300

    update descrpitions

--- clients/admin-ui/src/features/integrations/integration-type-info/manualInfo.tsx
+++ clients/admin-ui/src/features/integrations/integration-type-info/manualInfo.tsx
@@ -15,10 +15,15 @@ const ManualOverview = () => (
   <>
     <InfoHeading text="Overview" />
     <InfoText>
-      Manual integrations allow you to manually configure and manage data
-      connections that don&apos;t have automated connectors. This option
-      provides flexibility for custom data sources or systems that require
-      manual configuration and monitoring.
+      A Manual Integration provides a simple way for data to be manually
+      uploaded for access and erasure requests.
+    </InfoText>
+    <InfoText>
+      If you have manual integrations defined, privacy request execution will
+      exit early and remain in a state of <em>Requires input</em>. Once data has
+      been manually uploaded for all the manual integrations, then the privacy
+      request can be resumed. Data uploaded for manual integrations is passed on
+      directly to the data subject alongside the data package.
     </InfoText>
   </>
 );
