60cc5626bdc2090a0ebd3d483e7f97a216fbb24c
Lucano Vera <lucanovera@live.com.ar>
2025-06-02 12:43:36 -0300

    Minimal implementation Alpha feature flags

---
+++ clients/admin-ui/src/pages/settings/about-alpha.tsx
@@ -0,0 +1,8 @@
+import { NextPage } from "next";
+
+import AboutPage from "./about";
+
+const AboutAlpha: NextPage = () => {
+  return <AboutPage showAlphaFeatures />;
+};
+export default AboutAlpha;
