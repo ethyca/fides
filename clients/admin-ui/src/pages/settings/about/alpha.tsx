f37da0b4851cf7dc7f60c53528685a9d55d782a4
Lucano Vera <lucanovera@live.com.ar>
2025-06-03 15:37:06 -0300

    rename alpha page

--- clients/admin-ui/src/pages/settings/about/alpha.tsx
+++ clients/admin-ui/src/pages/settings/about/alpha.tsx
@@ -1,6 +1,6 @@
 import { NextPage } from "next";
 
-import AboutPage from "./about";
+import AboutPage from "./index";
 
 // This page is a copy of the about page, but it will also show the alpha feature flags
 const AboutAlpha: NextPage = () => {
