08219112dfcd50c4f68f361f94ca277e2fdfa100
Adrian Galvan <adrian@ethyca.com>
2025-06-17 13:52:24 -0700

    Adding missing import

--- src/fides/api/service/processors/post_processor_strategy/__init__.py
+++ src/fides/api/service/processors/post_processor_strategy/__init__.py
@@ -1,4 +1,5 @@
 from fides.api.service.processors.post_processor_strategy import (
+    post_processor_strategy_extract_for_execution_log,
     post_processor_strategy_filter,
     post_processor_strategy_unwrap,
 )
