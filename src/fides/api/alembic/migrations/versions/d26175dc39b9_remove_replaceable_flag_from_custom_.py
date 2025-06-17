faa3da308123195934a3d66a659baefe42742dd8
Facundo Lopez Janza <falopezjanza@gmail.com>
2025-06-17 14:51:35 -0300

    update revision

---
+++ src/fides/api/alembic/migrations/versions/d26175dc39b9_remove_replaceable_flag_from_custom_.py
@@ -0,0 +1,35 @@
+"""remove_replaceable_flag_from_custom_connector_template
+
+Revision ID: d26175dc39b9
+Revises: ba414a58ba90
+Create Date: 2025-06-17 17:51:02.800113
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'd26175dc39b9'
+down_revision = 'ba414a58ba90'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    # remove the CustomConnectorTemplate.replaceable column as it's no longer needed
+    op.drop_column("custom_connector_template", "replaceable")
+
+
+def downgrade():
+    # re-add the CustomConnectorTemplate.replaceable column
+    op.add_column(
+        "custom_connector_template",
+        sa.Column(
+            "replaceable",
+            sa.Boolean(),
+            unique=False,
+            nullable=False,
+            server_default=sa.text("false"),
+        ),
+    )
