ff04f7a539409ded72ca3c90fe4a27ed8f81af4c
Jade Wibbels <jade@ethyca.com>
2025-06-19 14:41:17 -0600

    missed type

--- src/fides/api/alembic/migrations/versions/aadfe83c5644_add_manual_task_to_connectiontype_enum.py
+++ src/fides/api/alembic/migrations/versions/aadfe83c5644_add_manual_task_to_connectiontype_enum.py
@@ -20,7 +20,7 @@ def upgrade():
     # Add manual_task to ConnectionType enum
     op.execute("alter type connectiontype rename to connectiontype_old")
     op.execute(
-        "create type connectiontype as enum('postgres', 'mongodb', 'mysql', 'https', 'snowflake', 'redshift', 'mssql', 'manual', 'mariadb', 'bigquery', 'saas', 'email', 'manual_webhook', 'manual_task')"
+        "create type connectiontype as enum('attentive_email', 'bigquery', 'datahub', 'dynamodb', 'fides', 'generic_consent_email', 'generic_erasure_email', 'dynamic_erasure_email', 'google_cloud_sql_mysql', 'google_cloud_sql_postgres', 'https', 'manual', 'manual_webhook', 'manual_task', 'mariadb', 'mongodb', 'mssql', 'mysql', 'okta', 'postgres', 'rds_mysql', 'rds_postgres', 'redshift', 's3', 'saas', 'scylla', 'snowflake', 'sovrn', 'timescale', 'website')"
     )
     op.execute(
         (
@@ -35,7 +35,7 @@ def downgrade():
     # Remove manual_task from ConnectionType enum
     op.execute("alter type connectiontype rename to connectiontype_old")
     op.execute(
-        "create type connectiontype as enum('postgres', 'mongodb', 'mysql', 'https', 'snowflake', 'redshift', 'mssql', 'mariadb', 'bigquery', 'saas', 'manual', 'email', 'manual_webhook')"
+        "create type connectiontype as enum('attentive_email', 'bigquery', 'datahub', 'dynamodb', 'fides', 'generic_consent_email', 'generic_erasure_email', 'dynamic_erasure_email', 'google_cloud_sql_mysql', 'google_cloud_sql_postgres', 'https', 'manual', 'manual_webhook', 'mariadb', 'mongodb', 'mssql', 'mysql', 'okta', 'postgres', 'rds_mysql', 'rds_postgres', 'redshift', 's3', 'saas', 'scylla', 'snowflake', 'sovrn', 'timescale', 'website')"
     )
     op.execute(
         (
