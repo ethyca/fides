5f9c691514c62b179ed28e81a832f98a49737ad2
Jason Gill <jason.gill@ethyca.com>
2025-06-19 11:57:49 -0600

    Update VersionLogger.tsx

--- clients/privacy-center/components/VersionLogger.tsx
+++ clients/privacy-center/components/VersionLogger.tsx
@@ -8,6 +8,13 @@ interface VersionLoggerProps {
 
 const VersionLogger = ({ version }: VersionLoggerProps) => {
   useEffect(() => {
+    /**
+     * The version is set in the next.config.js file and replaced
+     * by the using versioneer during the docker build process.
+     *
+     * If the version is not set, it means we are running locally
+     * so we don't want to log the version.
+     */
     if (version !== "__RELEASE_VERSION__") {
       // eslint-disable-next-line no-console
       console.info(`Privacy Center version: ${version}`);
