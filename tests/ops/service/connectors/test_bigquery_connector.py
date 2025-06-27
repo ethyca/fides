3334e9459797b5bd5c62ae9a0a24a961d5b02926
Adrian Galvan <adriang430@gmail.com>
2025-06-26 21:24:20 -0700

    Adding pytest mark for BigQuery tests

--- tests/ops/service/connectors/test_bigquery_connector.py
+++ tests/ops/service/connectors/test_bigquery_connector.py
@@ -236,7 +236,8 @@ class TestBigQueryConnector:
         assert len(results) == 1
         assert results[0]["email"] == "customer-1@example.com"
 
-
+@pytest.mark.integration_external
+@pytest.mark.integration_bigquery
 class TestBigQueryConnectorTimeBasedPartitioning:
     """
     Tests to verify the BigQuery connector with the new TimeBasedPartitioning schema
