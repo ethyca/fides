f9a303d2d014042de7b41100c76c15322a6b1a42
Jason Gill <jason.gill@ethyca.com>
2025-06-30 17:23:50 -0600

    remove test text

--- clients/admin-ui/src/features/data-discovery-and-detection/action-center/hooks/useDiscoveredSystemAggregateColumns.tsx
+++ clients/admin-ui/src/features/data-discovery-and-detection/action-center/hooks/useDiscoveredSystemAggregateColumns.tsx
@@ -74,7 +74,7 @@ export const useDiscoveredSystemAggregateColumns = ({
     header: () => {
       return (
         <Space>
-          <div>System test</div>
+          <div>System</div>
           <DiscoveryStatusIcon consentStatus={consentStatus} />
         </Space>
       );
