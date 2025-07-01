5b9ea63875e173207df28650c6c30dbae722c269
Jason Gill <jason.gill@ethyca.com>
2025-07-01 15:34:48 -0600

    Add tests for discovery consent status results

--- clients/admin-ui/src/features/data-discovery-and-detection/action-center/DiscoveryStatusIcon.tsx
+++ clients/admin-ui/src/features/data-discovery-and-detection/action-center/DiscoveryStatusIcon.tsx
@@ -11,7 +11,7 @@ export const DiscoveryStatusIcon = ({
 }: DiscoveryStatusIconProps) => {
   return consentStatus?.status === ConsentStatus.ALERT ? (
     <Tooltip title={consentStatus?.message}>
-      <div className="mb-px">
+      <div className="mb-px" data-testid="discovery-status-icon-alert">
         {/* div wrapper helps keep tooltip accessible and icon size consistent */}
         {/* mb-px is to assist with vertical centering since the visual weight of the triangle is bottom-heavy */}
         <Icons.WarningAltFilled style={{ color: "var(--fidesui-error)" }} />
