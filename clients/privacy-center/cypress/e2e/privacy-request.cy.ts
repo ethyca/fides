118e65bee744bc9bb749d5a5bebdd7d7eb7a31b3
Thomas Van Dort <tom@ethyca.com>
2025-07-02 15:21:20 -0400

    Remove test that is no longer relevant.

--- clients/privacy-center/cypress/e2e/privacy-request.cy.ts
+++ clients/privacy-center/cypress/e2e/privacy-request.cy.ts
@@ -1,43 +1,5 @@
 import { API_URL } from "../support/constants";
 
-describe("Privacy request", () => {
-  describe("when requesting data access", () => {
-    beforeEach(() => {
-      cy.intercept("POST", `${API_URL}/privacy-request`, {
-        fixture: "privacy-request/unverified",
-      }).as("postPrivacyRequest");
-      cy.intercept(
-        "POST",
-        `${API_URL}/privacy-request/privacy-request-id/verify`,
-        { body: {} },
-      ).as("postPrivacyRequestVerify");
-    });
-
-    it("requires valid inputs", () => {
-      cy.visit("/");
-      cy.loadConfigFixture("config/config_all.json").then(() => {
-        cy.getByTestId("card").contains("Access your data").click();
-
-        cy.getByTestId("privacy-request-form").within(() => {
-          // This block uses `.root()` to keep queries within the form. This is necessary because of
-          // `.blur()` which triggers input validation.
-
-          // test email being typed, continue becoming disabled due to invalid email
-          cy.root().get("input#email").type("invalid email").blur();
-          cy.root().should("contain", "Email is invalid");
-          cy.root().get("button").contains("Continue").should("be.disabled");
-          cy.root().get("input#email").clear().blur();
-
-          // test valid email, continue becoming enabled due to valid email
-          cy.root().get("input#email").type("valid@example.com").blur();
-          cy.root().get("button").contains("Continue").should("be.enabled");
-          cy.root().get("input#email").clear().blur();
-        });
-      });
-    });
-  });
-});
-
 describe("Privacy Request with custom fields with query params", () => {
   beforeEach(() => {
     cy.intercept("POST", `${API_URL}/privacy-request`, {
