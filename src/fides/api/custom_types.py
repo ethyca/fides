3c056e019b00bb33eb5fe2f69e33928a8ce97ef8
wadesdev <william.ades94@gmail.com>
2025-07-15 14:17:07 -0400

    Add margin-left attribute to tinker with spacing

--- src/fides/api/custom_types.py
+++ src/fides/api/custom_types.py
@@ -68,9 +68,7 @@ def validate_html_str(val: str) -> str:
         "u",
     }
 
-    ALLOWED_ATTRIBUTES = {
-        "a": {"href", "hreflang", "target"},
-    }
+    ALLOWED_ATTRIBUTES = {"a": {"href", "hreflang", "target"}, "ul": {"margin-left"}}
 
     if val:
         return clean(val, tags=ALLOWED_HTML_TAGS, attributes=ALLOWED_ATTRIBUTES)
