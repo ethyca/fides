10822dd34cf718da1ebc109d5faa435805043e62
Jason Gill <jason.gill@ethyca.com>
2025-07-18 16:29:23 -0600

    Remove all layout styles from StatusIndicators

--- clients/admin-ui/src/features/data-discovery-and-detection/action-center/tables/cells/DiscoveredSystemAggregateStatusCell.tsx
+++ clients/admin-ui/src/features/data-discovery-and-detection/action-center/tables/cells/DiscoveredSystemAggregateStatusCell.tsx
@@ -1,5 +1,5 @@
 import {
-  AntFlex as Flex,
+  AntSpace as Space,
   AntTooltip as Tooltip,
   AntTypography as Typography,
 } from "fidesui";
@@ -23,34 +23,32 @@ export const DiscoveredSystemStatusCell = ({
 }: DiscoveredSystemStatusCellProps) => {
   const url = rowClickUrl?.(system);
   return (
-    <Flex align="center">
+    <Space className="max-w-[250px] flex-nowrap">
       {!system.system_key && (
         <Tooltip title="New system">
           {/* icon has to be wrapped in a span for the tooltip to work */}
           <span>{STATUS_INDICATOR_MAP.Change}</span>
         </Tooltip>
       )}
-      <Flex align="center" gap={4} className="max-w-[250px] flex-nowrap">
-        {url ? (
-          <NextLink href={url} passHref legacyBehavior>
-            {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
-            <Link
-              size="sm"
-              strong
-              ellipsis
-              onClick={(e) => e.stopPropagation()}
-              data-testid="system-name-link"
-            >
-              {system.name || "Uncategorized assets"}
-            </Link>
-          </NextLink>
-        ) : (
-          <Text size="sm" strong ellipsis data-testid="system-name-text">
+      {url ? (
+        <NextLink href={url} passHref legacyBehavior>
+          {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
+          <Link
+            size="sm"
+            strong
+            ellipsis
+            onClick={(e) => e.stopPropagation()}
+            data-testid="system-name-link"
+          >
             {system.name || "Uncategorized assets"}
-          </Text>
-        )}
-        <DiscoveryStatusIcon consentStatus={system.consent_status} />
-      </Flex>
-    </Flex>
+          </Link>
+        </NextLink>
+      ) : (
+        <Text size="sm" strong ellipsis data-testid="system-name-text">
+          {system.name || "Uncategorized assets"}
+        </Text>
+      )}
+      <DiscoveryStatusIcon consentStatus={system.consent_status} />
+    </Space>
   );
 };
--- clients/admin-ui/src/features/data-discovery-and-detection/statusIndicators.tsx
+++ clients/admin-ui/src/features/data-discovery-and-detection/statusIndicators.tsx
@@ -7,57 +7,31 @@ import { ResourceChangeType } from "~/features/data-discovery-and-detection/type
 // FIXME: [ENG-893] remove all layout styles (margins) from these components and apply those to the wrapper. Layout styles should never be hard coded on a component!
 
 export const AdditionIndicator = () => (
-  <RightUpArrowIcon
-    color="green.400"
-    boxSize={2}
-    mr={2}
-    data-testid="add-icon"
-  />
+  <RightUpArrowIcon color="green.400" boxSize={2} data-testid="add-icon" />
 );
 
 export const RemovalIndicator = () => (
-  <RightDownArrowIcon
-    color="red.400"
-    boxSize={2}
-    mr={2}
-    data-testid="remove-icon"
-  />
+  <RightDownArrowIcon color="red.400" boxSize={2} data-testid="remove-icon" />
 );
 
 export const ClassificationIndicator = () => (
-  <TagIcon color="orange.400" boxSize={3} mr={1} data-testid="classify-icon" />
+  <TagIcon color="orange.400" boxSize={3} data-testid="classify-icon" />
 );
 
 export const ChangeIndicator = () => (
-  <CircleIcon
-    color="blue.400"
-    boxSize={2.5}
-    mb="2px"
-    mr="6px"
-    data-testid="change-icon"
-  />
+  <CircleIcon color="blue.400" boxSize={2.5} data-testid="change-icon" />
 );
 
 export const MonitoredIndicator = () => (
-  <CircleIcon
-    color="green.400"
-    boxSize={2}
-    mr={2}
-    data-testid="monitored-icon"
-  />
+  <CircleIcon color="green.400" boxSize={2} data-testid="monitored-icon" />
 );
 
 export const MutedIndicator = () => (
-  <CircleIcon color="red.400" boxSize={2} mr={2} data-testid="muted-icon" />
+  <CircleIcon color="red.400" boxSize={2} data-testid="muted-icon" />
 );
 
 export const InProgressIndicator = () => (
-  <CircleIcon
-    color="orange.400"
-    boxSize={2}
-    mr={2}
-    data-testid="in-progress-icon"
-  />
+  <CircleIcon color="orange.400" boxSize={2} data-testid="in-progress-icon" />
 );
 
 export const STATUS_INDICATOR_MAP: Record<
