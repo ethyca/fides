ab8c0fe2c90e966e0d6a2039b9f7f1a85d3f916b
Adrian Galvan <adrian@ethyca.com>
2025-07-21 16:03:45 -0700

    Adding QA scenario support

--- qa/utils/__init__.py
+++ qa/utils/__init__.py
@@ -1,5 +1,5 @@
 from .qa_test_scenario import QATestScenario, Argument
-from .fides_api import FidesAPI
+from .fides_api import FidesAPI, generate_dataset, generate_system, generate_connection
 from .rich_helpers import RichFormatter
 
-__all__ = ["FidesAPI", "QATestScenario", "Argument", "RichFormatter"]
+__all__ = ['FidesAPI', 'QATestScenario', "Argument", "RichFormatter", 'generate_dataset', 'generate_system', 'generate_connection']
