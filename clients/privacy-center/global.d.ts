9b42e770b75443a3726ac86f1e0276aa880ce8b4
Jason Gill <jason.gill@ethyca.com>
2025-07-21 13:42:37 -0600

    fix Privacy Center + FidesJS debug logging

--- clients/privacy-center/global.d.ts
+++ clients/privacy-center/global.d.ts
@@ -1,1 +1,7 @@
-declare module globalThis {}
+declare module globalThis {
+  /** Wrapper for console.log that only logs if debug mode is enabled. */
+  let fidesDebugger: (...args: unknown[]) => void;
+
+  /** Wrapper for console.error that only logs if debug mode is enabled. */
+  let fidesError: (...args: unknown[]) => void;
+}
