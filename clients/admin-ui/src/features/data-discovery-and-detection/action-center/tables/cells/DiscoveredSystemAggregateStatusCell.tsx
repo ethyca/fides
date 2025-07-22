8549254f78918caf7263177fe1c849ab4777d939
Jason Gill <jason.gill@ethyca.com>
2025-07-22 09:36:18 -0600

    link cell as component

--- clients/admin-ui/src/features/data-discovery-and-detection/action-center/tables/cells/DiscoveredSystemAggregateStatusCell.tsx
+++ clients/admin-ui/src/features/data-discovery-and-detection/action-center/tables/cells/DiscoveredSystemAggregateStatusCell.tsx
@@ -1,16 +1,10 @@
-import {
-  AntSpace as Space,
-  AntTooltip as Tooltip,
-  AntTypography as Typography,
-} from "fidesui";
-import NextLink from "next/link";
+import { AntSpace as Space, AntTooltip as Tooltip } from "fidesui";
 
+import { LinkCell } from "~/features/common/table/cells/LinkCell";
 import { STATUS_INDICATOR_MAP } from "~/features/data-discovery-and-detection/statusIndicators";
 import { SystemStagedResourcesAggregateRecord } from "~/types/api";
 
 import { DiscoveryStatusIcon } from "../../DiscoveryStatusIcon";
-
-const { Link, Text } = Typography;
 
 interface DiscoveredSystemStatusCellProps {
   system: SystemStagedResourcesAggregateRecord;
@@ -30,24 +24,12 @@ export const DiscoveredSystemStatusCell = ({
           <span>{STATUS_INDICATOR_MAP.Change}</span>
         </Tooltip>
       )}
-      {url ? (
-        <NextLink href={url} passHref legacyBehavior>
-          {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
-          <Link
-            size="sm"
-            strong
-            ellipsis
-            onClick={(e) => e.stopPropagation()}
-            data-testid="system-name-link"
-          >
-            {system.name || "Uncategorized assets"}
-          </Link>
-        </NextLink>
-      ) : (
-        <Text size="sm" strong ellipsis data-testid="system-name-text">
-          {system.name || "Uncategorized assets"}
-        </Text>
-      )}
+      <LinkCell
+        href={url}
+        data-testid={url ? "system-name-link" : "system-name-text"}
+      >
+        {system.name || "Uncategorized assets"}
+      </LinkCell>
       <DiscoveryStatusIcon consentStatus={system.consent_status} />
     </Space>
   );
