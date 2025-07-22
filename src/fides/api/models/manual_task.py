a72137148cab90f6126bb3335d40728bfd046002
Jade Wibbels <jade@ethyca.com>
2025-07-22 10:38:03 -0600

    redefine id

--- src/fides/api/models/manual_task.py
+++ src/fides/api/models/manual_task.py
@@ -1004,6 +1004,11 @@ class ManualTaskConditionalDependency(Base):
         """Overriding base class method to set the table name."""
         return "manual_task_conditional_dependency"
 
+    # redefined here because there's a minor, unintended discrepancy between
+    # this `id` field and that of the `Base` class, which explicitly sets `index=True`.
+    # In order for the `id` to be referenced by parent_id, we need to redefine it here.
+    id = Column(String(255), primary_key=True, default=FidesBase.generate_uuid)
+
     # Foreign key relationships
     manual_task_id = Column(
         String, ForeignKey("manual_task.id", ondelete="CASCADE"), nullable=False
