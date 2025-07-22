c10faa6917ee7bb4dfa03ee114799e574ee98b49
Jason Gill <jason.gill@ethyca.com>
2025-07-22 09:53:02 -0600

    Remove all layout styles (margins) from StatusIndicators (#6360)

--- clients/admin-ui/src/features/data-discovery-and-detection/action-center/tables/cells/DiscoveredSystemAggregateStatusCell.tsx
+++ clients/admin-ui/src/features/data-discovery-and-detection/action-center/tables/cells/DiscoveredSystemAggregateStatusCell.tsx
@@ -1,16 +1,10 @@
-import {
-  AntFlex as Flex,
-  AntTooltip as Tooltip,
-  AntTypography as Typography,
-} from "fidesui";
-import NextLink from "next/link";
+import { AntSpace as Space, AntTooltip as Tooltip } from "fidesui";
 
+import { LinkCell } from "~/features/common/table/cells/LinkCell";
 import { STATUS_INDICATOR_MAP } from "~/features/data-discovery-and-detection/statusIndicators";
 import { SystemStagedResourcesAggregateRecord } from "~/types/api";
 
 import { DiscoveryStatusIcon } from "../../DiscoveryStatusIcon";
-
-const { Link, Text } = Typography;
 
 interface DiscoveredSystemStatusCellProps {
   system: SystemStagedResourcesAggregateRecord;
@@ -23,34 +17,20 @@ export const DiscoveredSystemStatusCell = ({
 }: DiscoveredSystemStatusCellProps) => {
   const url = rowClickUrl?.(system);
   return (
-    <Flex align="center">
+    <Space className="max-w-[250px] flex-nowrap">
       {!system.system_key && (
         <Tooltip title="New system">
           {/* icon has to be wrapped in a span for the tooltip to work */}
           <span>{STATUS_INDICATOR_MAP.Change}</span>
         </Tooltip>
       )}
-      <Flex align="center" gap={4} className="max-w-[250px] flex-nowrap">
-        {url ? (
-          <NextLink href={url} passHref legacyBehavior>
-            {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
-            <Link
-              size="sm"
-              strong
-              ellipsis
-              onClick={(e) => e.stopPropagation()}
-              data-testid="system-name-link"
-            >
-              {system.name || "Uncategorized assets"}
-            </Link>
-          </NextLink>
-        ) : (
-          <Text size="sm" strong ellipsis data-testid="system-name-text">
-            {system.name || "Uncategorized assets"}
-          </Text>
-        )}
-        <DiscoveryStatusIcon consentStatus={system.consent_status} />
-      </Flex>
-    </Flex>
+      <LinkCell
+        href={url}
+        data-testid={url ? "system-name-link" : "system-name-text"}
+      >
+        {system.name || "Uncategorized assets"}
+      </LinkCell>
+      <DiscoveryStatusIcon consentStatus={system.consent_status} />
+    </Space>
   );
 };
