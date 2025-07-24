e99037303668be4057d5692bc53a5b6ee6b510e4
Jason Gill <jason.gill@ethyca.com>
2025-07-24 16:43:53 -0600

    retries and error handling

--- clients/fides-js/src/components/InfoBox.tsx
+++ clients/fides-js/src/components/InfoBox.tsx
@@ -2,12 +2,21 @@ import { ComponentChild, ComponentChildren } from "preact";
 
 const InfoBox = ({
   title,
+  isError,
   children,
 }: {
   title?: ComponentChild;
+  isError?: boolean;
   children: ComponentChildren;
 }) => (
-  <div className="fides-info-box">
+  <div
+    className="fides-info-box"
+    style={{
+      backgroundColor: isError
+        ? "var(--fides-overlay-background-error-color)"
+        : undefined,
+    }}
+  >
     {title ? <p className="fides-gpc-header">{title}</p> : null}
     {children}
   </div>
