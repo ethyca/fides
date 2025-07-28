48ba667653edbba6742351d5dcd2bc21459d4aac
Jason Gill <jason.gill@ethyca.com>
2025-07-28 16:00:38 -0600

    Update index.ts

--- clients/fides-js/src/lib/hooks/index.ts
+++ clients/fides-js/src/lib/hooks/index.ts
@@ -3,8 +3,8 @@ export { default as useElementById } from "./useElementById";
 export { default as useHasMounted } from "./useHasMounted";
 export { default as useNoticesServed } from "./useNoticesServed";
 export {
+  FetchState,
   type RetryConfig,
   useRetryableFetch,
-  FetchState,
 } from "./useRetryableFetch";
 export { default as useUUID4 } from "./useUUID4";
