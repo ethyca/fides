cd83ba6cda31bfadd2fd03c8895d225f7b4b9062
Vagoasdf <bruno.gutierrez96@gmail.com>
2025-07-28 11:57:19 -0400

    Running static Checks

--- tests/ops/integration_tests/test_integration_erasure_order.py
+++ tests/ops/integration_tests/test_integration_erasure_order.py
@@ -283,7 +283,7 @@ async def test_saas_erasure_order_request_task_resume_from_error(
         keys=["refunds_to_orders_id"],
     )
 
-    def side_effect(node, policy, privacy_request, request_task, rows,input_data):
+    def side_effect(node, policy, privacy_request, request_task, rows, input_data):
         if node.address.collection == "refunds_to_orders":
             raise Exception("Error executing refunds_to_orders task")
         request_task.rows_masked = 1
@@ -319,7 +319,9 @@ async def test_saas_erasure_order_request_task_resume_from_error(
 
     # "fix" the refunds_to_orders collection and resume the erasure
     def side_effect(node, policy, privacy_request, request_task, rows, input_data):
-        logger.info(f"side_effect fixed called with node: {node}, policy: {policy}, privacy_request: {privacy_request}, request_task: {request_task}, rows: {rows}")
+        logger.info(
+            f"side_effect fixed called with node: {node}, policy: {policy}, privacy_request: {privacy_request}, request_task: {request_task}, rows: {rows}"
+        )
         request_task.rows_masked = 1
         if request_task.id:
             session = Session.object_session(request_task)
