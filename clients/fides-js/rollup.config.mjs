7179851eff22c6ff83c543dd9ded28a156f26415
Jason Gill <jason.gill@ethyca.com>
2025-07-30 11:19:49 -0600

    fix fides debugger errors in privacy center

--- clients/fides-js/rollup.config.mjs
+++ clients/fides-js/rollup.config.mjs
@@ -41,7 +41,7 @@ const preactAliases = {
   ],
 };
 
-const fidesScriptPlugins = () => [
+const fidesScriptPlugins = (stripDebugger = false) => [
   alias(preactAliases),
   nodeResolve(),
   commonjs(),
@@ -51,7 +51,7 @@ const fidesScriptPlugins = () => [
   }),
   esbuild(),
   !IS_DEV && !IS_TEST && jsxRemoveAttributes(), // removes `data-testid`
-  !IS_DEV &&
+  (!IS_DEV || stripDebugger) &&
     strip({
       include: ["**/*.ts", "**/*.tsx"],
       functions: ["fidesDebugger"],
@@ -181,7 +181,7 @@ SCRIPTS.forEach(({ name, gzipErrorSizeKb, gzipWarnSizeKb, isExtension }) => {
   };
   const mjs = {
     input: `src/${name}.ts`,
-    plugins: fidesScriptPlugins(),
+    plugins: fidesScriptPlugins(true),
     output: [
       {
         // Compatible with ES module imports. Apps in this repo may be able to share the code.
