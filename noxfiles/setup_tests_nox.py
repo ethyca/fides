73fda422186685d0b81721ae2d0e11f7d341391b
JadeWibbels <jade.wibbels@gmail.com>
2025-08-06 14:15:03 -0600

    Apply suggestion from @JadeCara

--- noxfiles/setup_tests_nox.py
+++ noxfiles/setup_tests_nox.py
@@ -88,7 +88,7 @@ def pytest_ctl(session: Session, mark: str, coverage_arg: str) -> None:
             coverage_arg,
             "-m",
             "external",
-            "tests/ctl tests/qa",
+            "tests/ctl",
             "--tb=no",
         )
         session.run(*run_command, external=True)
