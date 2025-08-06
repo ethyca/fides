d020a90dcef15026ff610d744fa27fabbcfe6b80
Thabo Fletcher <thabofletcher@gmail.com>
2025-08-06 10:56:17 -0700

    include location configuration in redux store

--- clients/privacy-center/types/config.ts
+++ clients/privacy-center/types/config.ts
@@ -75,6 +75,11 @@ export type LegacyConfig = {
   consent?: LegacyConsentConfig | ConsentConfig;
 };
 
+export type LocationCollectionConfig = {
+  collection: "required" | "optional";
+  ip_geolocation_hint: boolean;
+};
+
 export type Config = {
   title: string;
   description: string;
@@ -90,6 +95,7 @@ export type Config = {
   consent?: ConsentConfig;
   privacy_policy_url?: string;
   privacy_policy_url_text?: string;
+  location?: LocationCollectionConfig;
 };
 
 export type LegacyConsentConfig = {
