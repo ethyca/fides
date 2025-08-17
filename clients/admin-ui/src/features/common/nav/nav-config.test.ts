65ec6dfdb79c34e5968116e1bdaeee8a8e2140bc
Adrian Galvan <adriang430@gmail.com>
2025-08-17 01:01:36 -0700

    Fixing test

--- clients/admin-ui/src/features/common/nav/nav-config.test.ts
+++ clients/admin-ui/src/features/common/nav/nav-config.test.ts
@@ -250,6 +250,10 @@ describe("configureNavGroups", () => {
           },
           { title: "Taxonomy", path: routes.TAXONOMY_ROUTE },
           { title: "Email templates", path: routes.EMAIL_TEMPLATES_ROUTE },
+          {
+            title: "Messaging providers",
+            path: routes.MESSAGING_PROVIDERS_ROUTE,
+          },
           { title: "About Fides", path: routes.ABOUT_ROUTE },
         ],
       });
