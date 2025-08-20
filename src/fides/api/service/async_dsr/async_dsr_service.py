f5a2680fb3827e5b7c7165e6f05c05c6827be612
Vagoasdf <bruno.gutierrez96@gmail.com>
2025-08-20 15:00:52 -0400

    Setting up service usage

--- src/fides/api/service/async_dsr/async_dsr_service.py
+++ src/fides/api/service/async_dsr/async_dsr_service.py
@@ -1,19 +1,34 @@
+from typing import List
+
 from loguru import logger
+from requests import Response
 from sqlalchemy.orm import Session
 
-from fides.api.common_exceptions import PrivacyRequestError
+from fides.api.graph.execution import ExecutionNode
 from fides.api.models.connectionconfig import ConnectionConfig
 from fides.api.models.datasetconfig import DatasetConfig
 from fides.api.models.privacy_request import PrivacyRequest, RequestTask
+from fides.api.schemas.policy import ActionType
 from fides.api.schemas.privacy_request import PrivacyRequestStatus
+from fides.api.service.async_dsr.async_dsr_strategy import AsyncDSRStrategy
+from fides.api.service.connectors.query_configs.saas_query_config import SaaSQueryConfig
+from fides.api.service.connectors.saas.authenticated_client import AuthenticatedClient
+from fides.api.service.connectors.saas_connector import SaaSConnector
+from fides.api.task.execute_request_tasks import (
+    _build_upstream_access_data,
+    create_graph_task,
+)
+from fides.api.task.graph_task import GraphTask
 from fides.api.task.task_resources import TaskResources
+from fides.api.util.collection_util import NodeInput, Row
+from fides.service.privacy_request.privacy_request_service import PrivacyRequestError
 
 
 def requeue_polling_request(
     db: Session,
     async_task: RequestTask,
 ) -> None:
-    """Re-queue a Privacy request that polls async tasks for a given privacy request"""
+    """Re-queue a Privacy request that polling async tasks for a given privacy request"""
     # Check that the privacy request is approved or in processing
     privacy_request: PrivacyRequest = async_task.privacy_request
 
@@ -41,16 +56,30 @@ def requeue_polling_request(
         async_task,
         db,
     ) as resources:
-        # graph_task: GraphTask = create_graph_task(db, async_task, resources)
-        # Currently, upstream tasks and "input keys" (which are built by data dependencies)
-        # are the same, but they may not be the same in the future.
-        # upstream_tasks = async_task.upstream_tasks_objects(db)
-        # upstream_access_data: List[List[Row]] = _build_upstream_access_data(
-        #    graph_task.execution_node.input_keys, upstream_tasks
-        # )
-        # TODO: Implement the polling strategy
-        logger.info(f"found resources: {resources}")
-        return None
+        graph_task: GraphTask = create_graph_task(db, async_task, resources)
+
+        saas_connector: SaaSConnector = graph_task.connector  # type: ignore
+        query_config: SaaSQueryConfig = saas_connector.query_config(
+            graph_task.execution_node
+        )  # type: ignore
+        if async_task.action_type == ActionType.access:
+            upstream_tasks = async_task.upstream_tasks_objects(db)
+            upstream_access_data: List[List[Row]] = _build_upstream_access_data(
+                graph_task.execution_node.input_keys, upstream_tasks
+            )
+            input_data: NodeInput = graph_task.pre_process_input_data(
+                *upstream_access_data, group_dependent_fields=True
+            )
+            execute_read_polling_requests(
+                db,
+                async_task,
+                query_config,
+                saas_connector,
+                graph_task.execution_node,
+                input_data,
+            )
+        elif async_task.action_type == ActionType.erasure:
+            execute_erasure_polling_requests(db, async_task, query_config)
 
 
 def get_connection_config_from_task(
@@ -71,5 +100,34 @@ def get_connection_config_from_task(
         raise PrivacyRequestError(
             f"ConnectionConfig with id {dataset_config.connection_config_id} not found."
         )
-
     return connection_config
+
+
+def execute_read_polling_requests(
+    db: Session,
+    async_task: RequestTask,
+    query_config: SaaSQueryConfig,
+    connector: SaaSConnector,
+    node: ExecutionNode,
+    input_data: NodeInput,
+) -> None:
+    """Execute the read polling requests for a given privacy request"""
+    read_requests = query_config.get_read_requests_by_identity()
+    for read_request in read_requests:
+        if read_request.async_config:
+            # strategy = AsyncDSRStrategy.get_strategy(
+            #     read_request.async_config.strategy,
+            #     read_request.async_config.configuration,
+            # )
+            # client: AuthenticatedClient = connector.create_client()
+            pass
+            # TODO: Proper call of the strategy
+
+
+def execute_erasure_polling_requests(
+    db: Session,
+    async_task: RequestTask,
+    query_config: SaaSQueryConfig,
+) -> None:
+    """Execute the erasure polling requests for a given privacy request"""
+    pass
