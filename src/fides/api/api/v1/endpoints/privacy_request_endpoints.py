5a7c044002df466bf7c39ccbfbddf78b45512c48
Tom Van Dort <tom@ethyca.com>
2025-08-27 15:00:44 -0400

    ENG-1273: Fix finalization (#6493)

--- src/fides/api/api/v1/endpoints/privacy_request_endpoints.py
+++ src/fides/api/api/v1/endpoints/privacy_request_endpoints.py
@@ -85,7 +85,7 @@ from fides.api.oauth.utils import (
 from fides.api.schemas.api import ResponseWithMessage
 from fides.api.schemas.dataset import CollectionAddressResponse, DryRunDatasetResponse
 from fides.api.schemas.external_https import PrivacyRequestResumeFormat
-from fides.api.schemas.policy import ActionType
+from fides.api.schemas.policy import ActionType, CurrentStep
 from fides.api.schemas.privacy_request import (
     BulkPostPrivacyRequests,
     BulkReviewResponse,
@@ -1900,6 +1900,7 @@ def finalize_privacy_request(
 
     queue_privacy_request(
         privacy_request_id=privacy_request_id,
+        from_step=CurrentStep.finalize_erasure.value,
     )
 
     return privacy_request  # type: ignore[return-value]
