2a7284acf98c7c2533baddfc4895aa7b8dec3402
Lucano Vera <lucanovera@live.com.ar>
2025-08-27 10:48:15 -0300

    update model

--- clients/admin-ui/src/types/api/models/Operator.ts
+++ clients/admin-ui/src/types/api/models/Operator.ts
@@ -13,6 +13,11 @@ export enum Operator {
   NOT_EXISTS = "not_exists",
   LIST_CONTAINS = "list_contains",
   NOT_IN_LIST = "not_in_list",
+  LIST_INTERSECTS = "list_intersects",
+  LIST_SUBSET = "list_subset",
+  LIST_SUPERSET = "list_superset",
+  LIST_DISJOINT = "list_disjoint",
   STARTS_WITH = "starts_with",
+  ENDS_WITH = "ends_with",
   CONTAINS = "contains",
 }
