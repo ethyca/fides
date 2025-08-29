94320e626cd1d05c306874d1acd6f2647258c9f6
3nder <speaker_ender@protonmail.com>
2025-08-29 12:35:29 -0400

    fix: modal and form styling [ENG-1295] (#6521)

--- clients/privacy-center/components/modals/privacy-request-modal/RequestSubmitted.tsx
+++ clients/privacy-center/components/modals/privacy-request-modal/RequestSubmitted.tsx
@@ -2,20 +2,18 @@ import { AntFlex as Flex, AntImage as Image, AntText as Text } from "fidesui";
 import React from "react";
 
 const RequestSubmitted = () => (
-  <>
-    <Flex>
-      <Image
-        src="/green-check.svg"
-        alt="green-checkmark"
-        width="48px"
-        height="48px"
-      />
-    </Flex>
-    <Text size="sm">
+  <Flex gap="middle" align="center">
+    <Image
+      src="/green-check.svg"
+      alt="green-checkmark"
+      width="48px"
+      height="48px"
+    />
+    <Text>
       Thanks for your request. A member of our team will review and be in
       contact with you shortly.
     </Text>
-  </>
+  </Flex>
 );
 
 export default RequestSubmitted;
