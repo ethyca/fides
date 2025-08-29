391b622592b462241a974e17cc14ffa43bd75d0a
speaker-ender <speaker_ender@protonmail.com>
2025-08-29 10:21:53 -0400

    fix: modal and form styling

--- clients/privacy-center/components/modals/verification-request/VerificationForm.tsx
+++ clients/privacy-center/components/modals/verification-request/VerificationForm.tsx
@@ -1,5 +1,6 @@
 import {
   AntButton as Button,
+  AntFlex as Flex,
   AntForm as Form,
   AntInput as Input,
   AntText as Text,
@@ -52,17 +53,22 @@ const VerificationForm = ({
   useEffect(() => resetForm(), [isOpen, resetForm]);
 
   return (
-    <>
-      <Text>Enter verification code</Text>
-      <Form onFinish={handleSubmit} data-testid="verification-form">
-        <Text>
-          A verification code has been sent. Return to this window and enter the
-          code below.
-        </Text>
+    <Flex gap="small" vertical>
+      <Text type="secondary">
+        A verification code has been sent. Return to this window and enter the
+        code below.
+      </Text>
+      <Form
+        onFinish={handleSubmit}
+        data-testid="verification-form"
+        layout="vertical"
+      >
         <Form.Item
           required
-          status={touched.code && Boolean(errors.code) ? "error" : undefined}
-          help={errors.code}
+          validateStatus={touched.code && !!errors.code ? "error" : undefined}
+          label="Verification Code"
+          help={touched.code && errors.code}
+          hasFeedback={touched.code && !!errors.code}
         >
           <Input
             id="code"
@@ -72,7 +78,7 @@ const VerificationForm = ({
             value={values.code}
           />
         </Form.Item>
-        <Form.Item>
+        <Flex vertical gap="middle">
           <Button
             htmlType="submit"
             type="primary"
@@ -81,19 +87,21 @@ const VerificationForm = ({
           >
             Submit code
           </Button>
-          <Text size="sm" type="secondary">
-            Didn&apos;t receive a code?
-          </Text>{" "}
-          <Button
-            size="small"
-            variant="text"
-            onClick={resetVerificationProcess}
-          >
-            Click here to try again
-          </Button>
-        </Form.Item>
+          <Flex align="center" gap="small" vertical>
+            <Text size="sm" type="secondary">
+              {`Didn't receive a code?`}
+            </Text>
+            <Button
+              size="small"
+              variant="outlined"
+              onClick={resetVerificationProcess}
+            >
+              Click here to try again
+            </Button>
+          </Flex>
+        </Flex>
       </Form>
-    </>
+    </Flex>
   );
 };
 
