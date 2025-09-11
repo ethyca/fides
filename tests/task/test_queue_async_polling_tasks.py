c254e0b8a122a20bcbd03e5eb263ecafa5f2c867
Vagoasdf <bruno.gutierrez96@gmail.com>
2025-09-11 15:30:11 -0300

    running static checks

--- tests/task/test_queue_async_polling_tasks.py
+++ tests/task/test_queue_async_polling_tasks.py
@@ -1,16 +1,20 @@
 from unittest import mock
 
-from fides.api.tasks import DSR_QUEUE_NAME
 import pytest
 
-
 from fides.api.service.privacy_request.request_service import poll_async_tasks_status
+from fides.api.tasks import DSR_QUEUE_NAME
 
 
 class TestPollAsyncTasksStatus:
 
     @mock.patch("fides.api.service.async_dsr.async_dsr_service.execute_polling_task")
-    def test_polling_task_is_requeued(self, mock_execute_polling_task, polling_request_task, in_processing_privacy_request):
+    def test_polling_task_is_requeued(
+        self,
+        mock_execute_polling_task,
+        polling_request_task,
+        in_processing_privacy_request,
+    ):
         """Test that a task awaiting processing and marked as a polling task is requeued."""
         poll_async_tasks_status.apply().get()
         mock_execute_polling_task.apply_async.assert_called_once_with(
@@ -43,7 +47,6 @@ class TestPollAsyncTasksStatus:
         """Test that no tasks are requeued when none meet the criteria."""
         poll_async_tasks_status.apply().get()
         mock_execute_polling_task.assert_not_called()
-
 
     @mock.patch("fides.api.service.async_dsr.async_dsr_service.execute_polling_task")
     def test_only_matching_task_is_requeued(
