ca634c696234e45ab0b0b05b73c364192ab5300d
John Ewart <john@johnewart.net>
2025-09-22 12:50:58 -0700

    Fix pylint error

--- src/fides/api/models/connection_oauth_credentials.py
+++ src/fides/api/models/connection_oauth_credentials.py
@@ -10,7 +10,9 @@ from sqlalchemy_utils.types.encrypted.encrypted_type import (
 
 from fides.api.db.base_class import Base
 from fides.api.models.connectionconfig import ConnectionConfig
-from fides.api.models.sql_models import System  # type: ignore[attr-defined]
+
+
+from fides.api.models.sql_models import System  # type: ignore[attr-defined] pylint: disable=unused-import
 from fides.config import CONFIG
 
 
