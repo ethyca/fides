47ec1ea8a50f10f51a000814577798461377aa67
Lucano Vera <lucanovera@live.com.ar>
2025-09-23 17:06:58 -0300

    cleanup code

--- clients/admin-ui/src/features/privacy-requests/PrivacyRequestDetailsManualTaskTab.tsx
+++ clients/admin-ui/src/features/privacy-requests/PrivacyRequestDetailsManualTaskTab.tsx
@@ -68,11 +68,11 @@ const PrivacyRequestDetailsManualTaskTab = ({
   // Check if there are any manual task connections
   // or if there are any tasks results. Some users may not have access to the connections,
   // so we use task results as backup.
-  const hasManualTaskConnections =
-    manualTaskConnections.length > 0 || tasks.length > 0;
+  const hasManualTaskConnections = manualTaskConnections.length > 0;
+  const hasTasks = tasks.length > 0;
 
   const hasAnyManualFeatures =
-    hasManualTaskConnections || hasLegacyManualIntegrations;
+    hasManualTaskConnections || hasLegacyManualIntegrations || hasTasks;
 
   const handleViewTasksClick = () => {
     router.push({
