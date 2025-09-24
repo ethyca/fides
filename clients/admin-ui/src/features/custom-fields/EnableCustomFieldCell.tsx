81a620117102aff1c1fba34563d60c0e40e7ab5d
jpople <jeremy@ethyca.com>
2025-09-24 10:17:05 -0500

    Custom fields migration/update (#6634)

---
+++ clients/admin-ui/src/features/custom-fields/EnableCustomFieldCell.tsx
@@ -0,0 +1,34 @@
+import { EnableCell } from "~/features/common/table/v2/cells";
+import { useUpdateCustomFieldDefinitionMutation } from "~/features/plus/plus.slice";
+import { CustomFieldDefinitionWithId } from "~/types/api";
+
+const EnableCustomFieldCell = ({
+  field,
+  isDisabled,
+}: {
+  field: CustomFieldDefinitionWithId;
+  isDisabled: boolean;
+}) => {
+  const [updateCustomFieldDefinitionTrigger, { isLoading }] =
+    useUpdateCustomFieldDefinitionMutation();
+
+  const onToggle = async (newValue: boolean) =>
+    updateCustomFieldDefinitionTrigger({
+      ...field,
+      active: newValue,
+    });
+
+  return (
+    <EnableCell
+      enabled={field.active ?? false}
+      onToggle={onToggle}
+      title="Disable custom field"
+      message="Are you sure you want to disable this custom field?"
+      isDisabled={isDisabled}
+      aria-label={field.active ? "Disable custom field" : "Enable custom field"}
+      loading={isLoading}
+    />
+  );
+};
+
+export default EnableCustomFieldCell;
