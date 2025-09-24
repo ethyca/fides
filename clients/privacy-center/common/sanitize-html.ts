2e108b8427552e723b56a0d3d7ca02737e5a04b1
Thomas Van Dort <tom@ethyca.com>
2025-09-24 10:08:44 -0400

    ENG-1506: Allow HTML in Privacy Center

---
+++ clients/privacy-center/common/sanitize-html.ts
@@ -0,0 +1,34 @@
+import sanitize from "sanitize-html";
+
+export default function sanitizeHTML(input: string) {
+  return sanitize(input, {
+    allowedTags: [
+      "a",
+      "blockquote",
+      "br",
+      "div",
+      "em",
+      "h1",
+      "h2",
+      "h3",
+      "h4",
+      "h5",
+      "h6",
+      "i",
+      "li",
+      "ol",
+      "p",
+      "pre",
+      "q",
+      "rt",
+      "ruby",
+      "s",
+      "span",
+      "strong",
+      "u",
+    ],
+    allowedAttributes: {
+      a: ["href", "hreflang", "target"],
+    },
+  });
+}
