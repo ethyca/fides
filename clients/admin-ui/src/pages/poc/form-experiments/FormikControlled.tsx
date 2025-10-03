0afebd25d7ed7add951b62ff397207f4965d698e
Jason Gill <jason.gill@ethyca.com>
2025-10-03 17:50:58 -0600

    fix spelling error

--- clients/admin-ui/src/pages/poc/form-experiments/FormikControlled.tsx
+++ clients/admin-ui/src/pages/poc/form-experiments/FormikControlled.tsx
@@ -29,7 +29,7 @@ export const FormikControlledPOC = () => (
         <Col span={12}>
           <Title level={2}>{`<Formik> + Controlled`}</Title>
           <div className="mb-4">
-            <Tag color="green">funcitonal</Tag>
+            <Tag color="green">functional</Tag>
             <Tag color="red">high effort</Tag>
           </div>
           <Form>
