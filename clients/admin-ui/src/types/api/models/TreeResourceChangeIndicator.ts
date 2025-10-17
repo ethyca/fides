b2b976edf64586fd3b1813d4aa539e0851b7b454
Jason Gill <jason.gill@ethyca.com>
2025-10-17 10:16:41 -0600

    update types

---
+++ clients/admin-ui/src/types/api/models/TreeResourceChangeIndicator.ts
@@ -0,0 +1,9 @@
+/* istanbul ignore file */
+/* tslint:disable */
+/* eslint-disable */
+
+export enum TreeResourceChangeIndicator {
+  ADDITION = "addition",
+  REMOVAL = "removal",
+  CHANGE = "change",
+}
