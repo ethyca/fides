b85383e682242007565a884e2dd4eea9c89337aa
Jason Gill <jason.gill@ethyca.com>
2025-10-17 18:16:05 -0600

    redirect faster

--- clients/admin-ui/src/pages/data-discovery/action-center/data-explorer/index.tsx
+++ clients/admin-ui/src/pages/data-discovery/action-center/data-explorer/index.tsx
@@ -1,16 +1,15 @@
 import { NextPage } from "next";
 import { useRouter } from "next/router";
-import { useEffect } from "react";
+import { useLayoutEffect } from "react";
 
 import { ACTION_CENTER_ROUTE } from "~/features/common/nav/routes";
 
 const DataExplorerRedirect: NextPage = () => {
   const router = useRouter();
 
-  useEffect(() => {
+  useLayoutEffect(() => {
     router.replace(ACTION_CENTER_ROUTE);
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+  }, [router]);
 
   return null;
 };
