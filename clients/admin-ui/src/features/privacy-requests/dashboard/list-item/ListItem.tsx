6ef13a9e630010d6998649d34524811272fa801e
Lucano Vera <lucanovera@live.com.ar>
2025-10-27 16:39:48 -0300

    Show custom fields in the new privacy request screen (#6849)
    Co-authored-by: Jason Gill <jason.gill@ethyca.com>
    Co-authored-by: greptile-apps[bot] <165735046+greptile-apps[bot]@users.noreply.github.com>

--- clients/admin-ui/src/features/privacy-requests/dashboard/list-item/ListItem.tsx
+++ clients/admin-ui/src/features/privacy-requests/dashboard/list-item/ListItem.tsx
@@ -1,14 +1,26 @@
-import { AntFlex as Flex, AntList as List, AntTag as Tag } from "fidesui";
+import {
+  AntFlex as Flex,
+  AntList as List,
+  AntTag as Tag,
+  formatIsoLocation,
+  isoStringToEntry,
+} from "fidesui";
+import { isArray, toString } from "lodash";
 import React from "react";
 
 import { PrivacyRequestEntity } from "~/features/privacy-requests/types";
 
 import { RequestTableActions } from "../../RequestTableActions";
 import {
+  getCustomFields,
+  getOtherIdentities,
+  getPrimaryIdentity,
+} from "../utils";
+import {
   DaysLeft,
-  EmailIdentity,
   Header,
-  NonEmailIdentities,
+  LabeledTag,
+  LabeledText,
   PolicyActionTypes,
   ReceivedOn,
   ViewButton,
@@ -19,34 +31,80 @@ interface ListItemProps {
   checkbox?: React.ReactNode;
 }
 
-export const ListItem = ({ item, checkbox }: ListItemProps) => (
-  <List.Item>
-    <div className="pr-4">{checkbox}</div>
-    <div className="grow">
-      <Header privacyRequest={item} />
-      <Flex vertical gap={16} wrap className="pt-1">
-        <Flex gap={8} wrap>
-          <EmailIdentity value={item.identity.email?.value} />
-          <PolicyActionTypes rules={item.policy.rules} />
-          <Tag>{item.source}</Tag>
-        </Flex>
+export const ListItem = ({ item, checkbox }: ListItemProps) => {
+  const primaryIdentity = getPrimaryIdentity(item.identity);
+  const otherIdentities = getOtherIdentities(item.identity, primaryIdentity);
+  const customFields = getCustomFields(item.custom_privacy_request_fields);
 
-        <Flex wrap gap={16}>
-          <NonEmailIdentities identities={item.identity} />
+  const hasExtraDetails: boolean =
+    otherIdentities.length > 0 || customFields.length > 0 || !!item.location;
+
+  const locationIsoEntry = item.location
+    ? (() => {
+        try {
+          return isoStringToEntry(item.location);
+        } catch {
+          return undefined;
+        }
+      })()
+    : undefined;
+
+  return (
+    <List.Item>
+      <div className="pr-4">{checkbox}</div>
+      <div className="grow">
+        <Header privacyRequest={item} />
+        <Flex vertical gap="small" wrap className="pt-1">
+          <Flex gap="small" wrap>
+            {primaryIdentity && (
+              <LabeledText label={primaryIdentity.label}>
+                {primaryIdentity.value}
+              </LabeledText>
+            )}
+            <PolicyActionTypes rules={item.policy.rules} />
+            <Tag>{item.source}</Tag>
+          </Flex>
+
+          {hasExtraDetails && (
+            <Flex wrap gap="middle">
+              {item.location && (
+                <LabeledTag key="location" label="Location">
+                  {locationIsoEntry
+                    ? formatIsoLocation({
+                        isoEntry: locationIsoEntry,
+                        showFlag: true,
+                      })
+                    : item.location}
+                </LabeledTag>
+              )}
+              {otherIdentities.map((identity) => (
+                <LabeledTag key={identity.key} label={identity.label}>
+                  {identity.value}
+                </LabeledTag>
+              ))}
+              {customFields.map((field) => (
+                <LabeledTag key={field.key} label={field.label}>
+                  {isArray(field.value)
+                    ? field.value.join(" - ")
+                    : toString(field.value)}
+                </LabeledTag>
+              ))}
+            </Flex>
+          )}
         </Flex>
+      </div>
+      <Flex gap="middle" wrap className="pr-2">
+        <ReceivedOn createdAt={item.created_at} />
+        <DaysLeft
+          daysLeft={item.days_left}
+          status={item.status}
+          timeframe={item.policy.execution_timeframe}
+        />
       </Flex>
-    </div>
-    <Flex gap={16} wrap className="pr-2">
-      <ReceivedOn createdAt={item.created_at} />
-      <DaysLeft
-        daysLeft={item.days_left}
-        status={item.status}
-        timeframe={item.policy.execution_timeframe}
-      />
-    </Flex>
-    <Flex className="min-w-[125px]" align="center" justify="end" gap={8}>
-      <ViewButton key="view" id={item.id} />
-      <RequestTableActions key="other-actions" subjectRequest={item} />
-    </Flex>
-  </List.Item>
-);
+      <Flex className="min-w-[125px]" align="center" justify="end" gap="small">
+        <ViewButton key="view" id={item.id} />
+        <RequestTableActions key="other-actions" subjectRequest={item} />
+      </Flex>
+    </List.Item>
+  );
+};
