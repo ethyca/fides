a6eee1f9ef12de38dc6bbcb9e4a3dcb9f4d2e52e
erosselli <67162025+erosselli@users.noreply.github.com>
2025-10-28 15:05:56 -0300

    Consent v3 Privacy Notice Sandbox (#6841)

---
+++ clients/admin-ui/src/features/poc/privacy-notices-sandbox/components/index.ts
@@ -0,0 +1,6 @@
+export { default as ConsentMechanismToggle } from "./ConsentMechanismToggle";
+export { default as DbStatePreview } from "./DbStatePreview";
+export { default as DisableChildrenTree } from "./DisableChildrenTree";
+export { default as GetApiPreview } from "./GetApiPreview";
+export { default as PostApiPreview } from "./PostApiPreview";
+export { default as PreviewCard } from "./PreviewCard";
