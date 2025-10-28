29b7eef456d93f1793be8bdaf4db5b3b61e32fbb
Jason Gill <jason.gill@ethyca.com>
2025-10-28 15:25:50 -0600

    fix tailwind config to pick up styles from fidesui

--- clients/admin-ui/tailwind.config.js
+++ clients/admin-ui/tailwind.config.js
@@ -1,6 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,ts,jsx,tsx,mdx}"],
+  content: [
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
+    "../fidesui/src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "../fidesui/src/hoc/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
   theme: {
     extend: {
       colors: {
