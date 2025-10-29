25f7c58eedf7a4e633a14d4611789823f71898d2
Adrian Galvan <adrian@ethyca.com>
2025-10-29 11:25:40 -0700

    Identity definition model (#6852)

--- src/fides/api/db/base.py
+++ src/fides/api/db/base.py
@@ -33,6 +33,7 @@ from fides.api.models.fides_user_permissions import FidesUserPermissions
 from fides.api.models.fides_user_respondent_email_verification import (
     FidesUserRespondentEmailVerification,
 )
+from fides.api.models.identity_definition import IdentityDefinition
 from fides.api.models.identity_salt import IdentitySalt
 from fides.api.models.location_regulation_selections import LocationRegulationSelections
 from fides.api.models.manual_task import (
