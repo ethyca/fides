f0a079499977f59eeec5d5f0d5dc9dddd46343ae
Lucano Vera <lucanovera@live.com.ar>
2025-10-29 12:31:11 -0300

    left align pagination

--- clients/admin-ui/src/features/privacy-requests/dashboard/PrivacyRequestsDashboard.tsx
+++ clients/admin-ui/src/features/privacy-requests/dashboard/PrivacyRequestsDashboard.tsx
@@ -159,7 +159,7 @@ export const PrivacyRequestsDashboard = () => {
               `${range[0]}-${range[1]} of ${total} items`
             }
             total={totalRows ?? 0}
-            align="end"
+            align="start"
           />
         </Flex>
       )}
