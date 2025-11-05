5cc2d483f26872ac8ebdb099c20c2c8d3e4c624f
DrJfrost <26847284+DrJfrost@users.noreply.github.com>
2025-11-05 00:32:35 -0500

    organize tabs to columns action center

--- clients/admin-ui/src/features/data-discovery-and-detection/action-center/tables/DiscoveredSystemAggregateTable.tsx
+++ clients/admin-ui/src/features/data-discovery-and-detection/action-center/tables/DiscoveredSystemAggregateTable.tsx
@@ -66,13 +66,13 @@ export const DiscoveredSystemAggregateTable = ({
         items={
           isOktaApp
             ? oktaFilterTabs.map((tab) => ({
-              key: tab.value,
-              label: `${tab.label}`,
-            }))
+                key: tab.value,
+                label: `${tab.label}`,
+              }))
             : filterTabs.map((tab) => ({
-              key: tab.hash,
-              label: tab.label,
-            }))
+                key: tab.hash,
+                label: tab.label,
+              }))
         }
         selectedKeys={[isOktaApp ? oktaActiveTab : activeTab]}
         onClick={async (menuInfo) => {
@@ -111,10 +111,10 @@ export const DiscoveredSystemAggregateTable = ({
                 },
                 !activeParams.diff_status.includes(DiffStatus.MUTED)
                   ? {
-                    key: "ignore",
-                    label: "Ignore",
-                    onClick: handleBulkIgnore,
-                  }
+                      key: "ignore",
+                      label: "Ignore",
+                      onClick: handleBulkIgnore,
+                    }
                   : null,
               ],
             }}
