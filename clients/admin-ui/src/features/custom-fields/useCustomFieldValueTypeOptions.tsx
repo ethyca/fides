01debb87622f710355297d3bd937088a77515704
Jeremy Pople <jeremy@ethyca.com>
2025-11-20 15:44:08 -0600

    add autocomplete behavior to form

--- clients/admin-ui/src/features/custom-fields/useCustomFieldValueTypeOptions.tsx
+++ clients/admin-ui/src/features/custom-fields/useCustomFieldValueTypeOptions.tsx
@@ -7,36 +7,27 @@ import {
 } from "~/features/taxonomy/constants";
 import { useGetCustomTaxonomiesQuery } from "~/features/taxonomy/taxonomy.slice";
 
-const useCustomFieldValueTypeOptions = (
-  includeCustomOption: boolean = true,
-) => {
+const useCustomFieldValueTypeOptions = () => {
   const { plus: isPlusEnabled } = useFeatures();
   const { data: customTaxonomies, isLoading: isCustomTaxonomiesLoading } =
     useGetCustomTaxonomiesQuery(undefined as void, {
       skip: !isPlusEnabled,
     });
-  const valueTypeOptions: AntSelectProps["options"] = [];
 
-  if (includeCustomOption) {
-    valueTypeOptions.push({ label: "Custom", value: "custom" });
-  }
-
-  valueTypeOptions.push(
-    ...[
-      {
-        label: CoreTaxonomiesEnum.DATA_CATEGORIES,
-        value: TaxonomyTypeEnum.DATA_CATEGORY,
-      },
-      {
-        label: CoreTaxonomiesEnum.DATA_USES,
-        value: TaxonomyTypeEnum.DATA_USE,
-      },
-      {
-        label: CoreTaxonomiesEnum.DATA_SUBJECTS,
-        value: TaxonomyTypeEnum.DATA_SUBJECT,
-      },
-    ],
-  );
+  const valueTypeOptions: AntSelectProps["options"] = [
+    {
+      label: CoreTaxonomiesEnum.DATA_CATEGORIES,
+      value: TaxonomyTypeEnum.DATA_CATEGORY,
+    },
+    {
+      label: CoreTaxonomiesEnum.DATA_USES,
+      value: TaxonomyTypeEnum.DATA_USE,
+    },
+    {
+      label: CoreTaxonomiesEnum.DATA_SUBJECTS,
+      value: TaxonomyTypeEnum.DATA_SUBJECT,
+    },
+  ];
 
   if (isPlusEnabled) {
     valueTypeOptions.push({
