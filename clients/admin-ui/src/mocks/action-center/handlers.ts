bf3de587efee38421304c6f126ddb4d83e393497
Lucano Vera <lucanovera@live.com.ar>
2025-11-21 18:09:53 -0300

    add mock monitor results

--- clients/admin-ui/src/mocks/action-center/handlers.ts
+++ clients/admin-ui/src/mocks/action-center/handlers.ts
@@ -1,7 +1,7 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import { rest } from "msw";
 
-import { mockOktaMonitor } from "./data";
+import { mockOktaApp, mockOktaMonitor } from "./data";
 
 /**
  * MSW handlers for discovery monitor aggregate results
@@ -19,6 +19,21 @@ export const discoveryMonitorHandlers = () => {
           ctx.json({
             items: [mockOktaMonitor],
             total: 1,
+            page: 1,
+            size: 25,
+            pages: 1,
+          }),
+        ),
+    ),
+    // GET - return mock Okta app assets for system aggregate results
+    rest.get(
+      `${apiBase}/plus/discovery-monitor/system-aggregate-results`,
+      (_req, res, ctx) =>
+        res(
+          ctx.status(200),
+          ctx.json({
+            items: [mockOktaApp],
+            total: 10,
             page: 1,
             size: 25,
             pages: 1,
