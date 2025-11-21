045a58ac85887cab8d32649b51b3f195ce1142eb
Jade Wibbels <jade@ethyca.com>
2025-11-21 16:34:22 -0700

    added enums for the other standard fields

--- src/fides/api/task/conditional_dependencies/privacy_request/schemas.py
+++ src/fides/api/task/conditional_dependencies/privacy_request/schemas.py
@@ -17,6 +17,41 @@ class PrivacyRequestConvenienceFields(Enum):
     has_storage_destination = "privacy_request.policy.has_storage_destination"
 
 
+class PrivacyRequestFields(Enum):
+    """Fields for privacy request."""
+
+    created_at = "privacy_request.created_at"
+    due_date = "privacy_request.due_date"
+    identity_verified_at = "privacy_request.identity_verified_at"
+    location = "privacy_request.location"
+    origin = "privacy_request.origin"
+    requested_at = "privacy_request.requested_at"
+    source = "privacy_request.source"
+    submitted_by = "privacy_request.submitted_by"
+
+
+class PolicyFields(Enum):
+    """Fields for policy."""
+
+    id = "privacy_request.policy.id"
+    name = "privacy_request.policy.name"
+    key = "privacy_request.policy.key"
+    description = "privacy_request.policy.description"
+    execution_timeframe = "privacy_request.policy.execution_timeframe"
+    rules = "privacy_request.policy.rules"
+
+
+class IdentityFields(Enum):
+    """Fields for identity."""
+
+    email = "privacy_request.identity.email"
+    phone_number = "privacy_request.identity.phone_number"
+    external_id = "privacy_request.identity.external_id"
+    fides_user_device_id = "privacy_request.identity.fides_user_device_id"
+    ljt_readerID = "privacy_request.identity.ljt_readerID"
+    ga_client_id = "privacy_request.identity.ga_client_id"
+
+
 class ConditionalDependencyFieldType(Enum):
     """Type of the field for conditional dependencies."""
 
@@ -31,9 +66,9 @@ class ConditionalDependencyFieldType(Enum):
 class ConditionalDependencyFieldInfo(BaseModel):
     """Information about a field available for conditional dependencies."""
 
-    field_path: PrivacyRequestConvenienceFields = Field(
+    field_path: str = Field(
         ...,
-        description="Convenience field from PrivacyRequestConvenienceFields",
+        description="Path to the field in the PrivacyRequest",
     )
     field_type: ConditionalDependencyFieldType = Field(
         ..., description="Type of the field from ConditionalDependencyFieldType"
