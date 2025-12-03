6f3a58493484b1322189a8d78c924010f1c92df4
speaker-ender <speaker_ender@protonmail.com>
2025-12-03 15:13:31 -0500

    chore: add comment explaining type usage
    chore: formatting

--- clients/admin-ui/src/features/data-discovery-and-detection/action-center/action-center.slice.ts
+++ clients/admin-ui/src/features/data-discovery-and-detection/action-center/action-center.slice.ts
@@ -386,6 +386,7 @@ const actionCenterApi = baseApi.injectEndpoints({
         };
       },
     }),
+    /* stagedResourceUrn is required by the API. This is made optional as a convenience but should not be expected to return a successful response unless a valid value is passed */
     getStagedResourceDetails: build.query<
       MonitorResource,
       { stagedResourceUrn?: string }
