651c54b205b8ed3bc8bb09c08910d9ed1ea5f825
Jeremy Pople <jeremy@ethyca.com>
2025-12-08 16:53:48 -0600

    cleanup on PrivacyNoticeRowType

--- clients/admin-ui/src/features/privacy-notices/table/PrivacyNoticeRowType.ts
+++ clients/admin-ui/src/features/privacy-notices/table/PrivacyNoticeRowType.ts
@@ -1,7 +1,6 @@
-// we have to alias this because Ant Table automatically sets the "expandable"
-
 import { LimitedPrivacyNoticeResponseSchema } from "~/types/api/models/LimitedPrivacyNoticeResponseSchema";
 
+// we have to alias this because Ant Table automatically sets the "expandable"
 // prop on table rows if the data type has a "children" property
 export interface PrivacyNoticeRowType
   extends Omit<LimitedPrivacyNoticeResponseSchema, "children"> {
