f3971adb0f92beb620b83c9dd4e85ef16fb52499
John Ewart <john@johnewart.net>
2025-12-11 13:40:45 -0800

    Fix pytest_nox method

--- noxfiles/setup_tests_nox.py
+++ noxfiles/setup_tests_nox.py
@@ -101,11 +101,10 @@ def pytest_lib(session: Session, pytest_config: PytestConfig) -> None:
     session.run(*run_command, external=True)
 
 
-def pytest_nox(session: Session, additional_args: list[str]) -> None:
+def pytest_nox(session: Session, pytest_config: PytestConfig) -> None:
     """Runs any tests of nox commands themselves."""
-    # the nox tests don't run with coverage, override the provided arg
-    coverage_arg = "--no-cov"
-    run_command = ("pytest", *additional_args, "noxfiles/")
+    # the nox tests don't run with coverage or xdist so just add the reporting config here
+    run_command = ("pytest", *pytest_config.report_config, "noxfiles/")
     session.run(*run_command, external=True)
 
 
