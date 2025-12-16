7bf39cb4530cccae48de47667eb001898080a43a
Jeremy Pople <jeremy@ethyca.com>
2025-12-16 16:58:12 -0600

    remove redundant clipboard button

--- clients/admin-ui/src/features/common/errors/ErrorPage.tsx
+++ clients/admin-ui/src/features/common/errors/ErrorPage.tsx
@@ -8,10 +8,10 @@ import {
   Icons,
 } from "fidesui";
 import { ReactNode } from "react";
+import ClipboardButton from "~/features/common/ClipboardButton";
 
 import ErrorImage from "~/features/common/errors/ErrorImage";
 import { getErrorMessage } from "~/features/common/helpers";
-import useClipboardButton from "~/features/common/hooks/useClipboard";
 
 type ActionProps = AntButtonProps & { label: ReactNode };
 
@@ -26,7 +26,6 @@ const ErrorPage = ({
   // TODO: examine this error handling more carefully to extract the right message
   const errorMessage = getErrorMessage(error);
   const dataString = JSON.stringify(data);
-  const { tooltipText, handleCopy } = useClipboardButton(dataString);
 
   return (
     <AntFlex vertical align="center" justify="center" className="h-screen">
@@ -40,15 +39,7 @@ const ErrorPage = ({
         </AntTypography.Paragraph>
         <AntFlex gap="small" align="center">
           <AntTypography.Text type="secondary">{dataString}</AntTypography.Text>
-          <AntTooltip title={tooltipText} placement="top">
-            <AntButton
-              icon={<Icons.Copy />}
-              aria-label="Copy error data"
-              type="text"
-              size="small"
-              onClick={handleCopy}
-            />
-          </AntTooltip>
+          <ClipboardButton copyText={dataString} />
         </AntFlex>
         {actions.length > 0 && (
           <AntFlex gap="middle" justify="center">
