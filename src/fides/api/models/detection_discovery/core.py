d3a366432df7abfa277332138432947f1459a5f6
Victoria Cruces <victoria@ethyca.com>
2026-01-07 13:43:14 -0300

    Add is_leaf

--- src/fides/api/models/detection_discovery/core.py
+++ src/fides/api/models/detection_discovery/core.py
@@ -591,6 +591,8 @@ class StagedResource(Base):
     )
     parent = Column(String, nullable=True)
 
+    is_leaf = Column(Boolean, nullable=False, default=False)
+
     # diff-related fields
     diff_status = Column(String, nullable=True, index=True)
 
@@ -691,6 +693,11 @@ class StagedResource(Base):
             "idx_stagedresource_user_categories_gin",
             "user_assigned_data_categories",
             postgresql_using="gin",
+        ),
+        Index(
+            "ix_stagedresource_monitor_config_is_leaf",
+            "monitor_config_id",
+            "is_leaf",
         ),
     )
 
