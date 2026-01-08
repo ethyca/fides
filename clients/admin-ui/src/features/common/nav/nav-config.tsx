1b222e99a9dc54b741dafc6ce465242871419599
Jeremy Pople <jeremy@ethyca.com>
2026-01-07 21:32:00 -0600

    domain verification table

--- clients/admin-ui/src/features/common/nav/nav-config.tsx
+++ clients/admin-ui/src/features/common/nav/nav-config.tsx
@@ -277,7 +277,7 @@ export const NAV_CONFIG: NavConfigGroup[] = [
         title: "Domain verification",
         path: routes.DOMAIN_RECORDS_ROUTE,
         requiresPlus: true,
-        requiresFidesCloud: true,
+        requiresFidesCloud: false,
         scopes: [ScopeRegistryEnum.FIDES_CLOUD_CONFIG_READ],
       },
       {
