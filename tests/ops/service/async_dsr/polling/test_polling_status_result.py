7e62c91ee2df8ee93d367e5860167a75640bafbb
Vagoasdf <bruno.gutierrez96@gmail.com>
2026-01-13 16:40:29 -0300

    Updating - creating tests

---
+++ tests/ops/service/async_dsr/polling/test_polling_status_result.py
@@ -0,0 +1,28 @@
+import pytest
+
+from fides.api.schemas.saas.async_polling_configuration import PollingStatusResult
+
+@pytest.mark.async_dsr
+class TestPollingStatusResult:
+    """
+    Base tests for PollingStatusResult model
+    To be expanded if there are additional logics for the PollingStatusResult model
+    """
+
+    def test_init_default(self):
+        status_result = PollingStatusResult(is_complete=True)
+        assert status_result.skip_result_request == False
+        pass
+
+    def test_init_skip_result(self):
+        status_result = PollingStatusResult(is_complete=True, skip_result_request=True)
+        assert status_result.skip_result_request == True
+        pass
+
+    def test_dict_output(self):
+        status_result = PollingStatusResult(is_complete=True, skip_result_request=True)
+        assert status_result.dict() == {
+            "is_complete": True,
+            "skip_result_request": True,
+        }
+        pass
