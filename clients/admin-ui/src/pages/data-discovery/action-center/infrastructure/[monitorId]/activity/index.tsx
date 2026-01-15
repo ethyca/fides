f0472ae9c91dfcbb179e0a0e8392f46bfed11bfc
Lucano Vera <lucanovera@live.com.ar>
2026-01-15 13:26:22 -0300

    add feature flag check

--- clients/admin-ui/src/pages/data-discovery/action-center/infrastructure/[monitorId]/activity/index.tsx
+++ clients/admin-ui/src/pages/data-discovery/action-center/infrastructure/[monitorId]/activity/index.tsx
@@ -1,18 +1,31 @@
+import { Result } from "fidesui";
 import { NextPage } from "next";
 import { useParams } from "next/navigation";
 
+import { useFeatures } from "~/features/common/features";
 import ActionCenterLayout from "~/features/data-discovery-and-detection/action-center/ActionCenterLayout";
 import { InProgressMonitorTasksList } from "~/features/data-discovery-and-detection/action-center/components/InProgressMonitorTasksList";
 
-import { MONITOR_INFRASTRUCTURE_ACTION_CENTER_CONFIG } from "..";
+import {
+  HELIOS_ACCESS_ERROR,
+  MONITOR_INFRASTRUCTURE_ACTION_CENTER_CONFIG,
+} from "..";
 
 const InfrastructureMonitorActivity: NextPage = () => {
+  const {
+    flags: { heliosV2 },
+  } = useFeatures();
   const params = useParams<{ monitorId: string }>();
 
   const monitorId = params?.monitorId
     ? decodeURIComponent(params.monitorId)
     : undefined;
   const loading = !monitorId;
+  const error = !heliosV2 && HELIOS_ACCESS_ERROR;
+
+  if (error) {
+    return <Result status="error" title={error} />;
+  }
 
   return (
     <ActionCenterLayout
--- clients/admin-ui/src/pages/data-discovery/action-center/infrastructure/[monitorId]/index.tsx
+++ clients/admin-ui/src/pages/data-discovery/action-center/infrastructure/[monitorId]/index.tsx
@@ -1,6 +1,8 @@
+import { Result } from "fidesui";
 import { NextPage } from "next";
 import { useParams } from "next/navigation";
 
+import { useFeatures } from "~/features/common/features";
 import {
   ACTION_CENTER_INFRASTRUCTURE_MONITOR_ACTIVITY_ROUTE,
   ACTION_CENTER_INFRASTRUCTURE_MONITOR_ROUTE,
@@ -16,13 +18,24 @@ export const MONITOR_INFRASTRUCTURE_ACTION_CENTER_CONFIG = {
     ACTION_CENTER_INFRASTRUCTURE_MONITOR_ROUTE,
 } as const;
 
+export const HELIOS_ACCESS_ERROR =
+  "Attempting to access monitor results without the required feature flag enabled";
+
 const InfrastructureMonitorResultSystems: NextPage = () => {
+  const {
+    flags: { heliosV2 },
+  } = useFeatures();
   const params = useParams<{ monitorId: string }>();
 
   const monitorId = params?.monitorId
     ? decodeURIComponent(params.monitorId)
     : undefined;
   const loading = !monitorId;
+  const error = !heliosV2 && HELIOS_ACCESS_ERROR;
+
+  if (error) {
+    return <Result status="error" title={error} />;
+  }
 
   return (
     <ActionCenterLayout
