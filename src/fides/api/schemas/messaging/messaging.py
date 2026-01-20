e849a9b1f211e8a5e12bda319eca1374d7de3283
jack-gale-ethyca <jack.gale@ethyca.com>
2026-01-20 12:19:07 -0500

    Revert changes to messaging.py

--- src/fides/api/schemas/messaging/messaging.py
+++ src/fides/api/schemas/messaging/messaging.py
@@ -88,6 +88,7 @@ class MessagingActionType(StrEnum):
     PRIVACY_REQUEST_RECEIPT = "privacy_request_receipt"
     PRIVACY_REQUEST_COMPLETE_ACCESS = "privacy_request_complete_access"
     PRIVACY_REQUEST_COMPLETE_DELETION = "privacy_request_complete_deletion"
+    PRIVACY_REQUEST_COMPLETE_CONSENT = "privacy_request_complete_consent"
     PRIVACY_REQUEST_REVIEW_DENY = "privacy_request_review_deny"
     PRIVACY_REQUEST_REVIEW_APPROVE = "privacy_request_review_approve"
     USER_INVITE = "user_invite"
@@ -103,6 +104,7 @@ CONFIGURABLE_MESSAGING_ACTION_TYPES: Tuple[str, ...] = (
     MessagingActionType.PRIVACY_REQUEST_RECEIPT.value,
     MessagingActionType.PRIVACY_REQUEST_COMPLETE_ACCESS.value,
     MessagingActionType.PRIVACY_REQUEST_COMPLETE_DELETION.value,
+    MessagingActionType.PRIVACY_REQUEST_COMPLETE_CONSENT.value,
     MessagingActionType.PRIVACY_REQUEST_REVIEW_DENY.value,
     MessagingActionType.PRIVACY_REQUEST_REVIEW_APPROVE.value,
     MessagingActionType.MANUAL_TASK_DIGEST.value,
@@ -120,7 +122,6 @@ class ErrorNotificationBodyParams(BaseModel):
     """Body params required for privacy request error notifications."""
 
     unsent_errors: int
-    is_test: Optional[bool] = False
 
 
 class SubjectIdentityVerificationBodyParams(BaseModel):
@@ -262,7 +263,6 @@ class FidesopsMessage(
 
     @model_validator(mode="after")
     def validate_body_params_match_action_type(self) -> "FidesopsMessage":
-
         valid_body_params_for_action_type = {
             MessagingActionType.CONSENT_REQUEST: None,  # Don't validate this one
             MessagingActionType.CONSENT_REQUEST_EMAIL_FULFILLMENT: ConsentEmailFulfillmentBodyParams,
@@ -542,7 +542,6 @@ class MessagingTemplateDefault(BaseModel):
 
 
 class MessagingTemplateWithPropertiesSummary(MessagingTemplateWithPropertiesBase):
-
     model_config = ConfigDict(from_attributes=True, use_enum_values=True)
 
 
@@ -560,7 +559,6 @@ class MessagingTemplateWithPropertiesDetail(MessagingTemplateWithPropertiesBase)
 
 
 class MessagingTemplateWithPropertiesBodyParams(BaseModel):
-
     content: Dict[str, Any] = Field(
         examples=[
             {
@@ -574,7 +572,6 @@ class MessagingTemplateWithPropertiesBodyParams(BaseModel):
 
 
 class MessagingTemplateWithPropertiesPatchBodyParams(BaseModel):
-
     content: Optional[Dict[str, Any]] = Field(
         None,
         examples=[
