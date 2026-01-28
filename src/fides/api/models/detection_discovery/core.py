25a902c793417539ed6fe01bba29f58148b6a3e3
Victoria Cruces <victoria@ethyca.com>
2026-01-28 17:05:09 -0300

    is_leaf null by default

--- src/fides/api/models/detection_discovery/core.py
+++ src/fides/api/models/detection_discovery/core.py
@@ -582,7 +582,7 @@ class StagedResource(Base):
     )
     parent = Column(String, nullable=True)
 
-    is_leaf = Column(Boolean, nullable=True, default=False)
+    is_leaf = Column(Boolean, nullable=True, default=None)
 
     # diff-related fields
     diff_status = Column(String, nullable=True, index=True)
@@ -689,6 +689,7 @@ class StagedResource(Base):
             "ix_stagedresource_monitor_config_is_leaf",
             "monitor_config_id",
             "is_leaf",
+            postgresql_where=text("is_leaf IS NOT NULL"),
         ),
     )
 
