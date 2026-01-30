c4b77c3c72dd865a68d0f99fdbfdcb6e255693db
3nder <speaker_ender@protonmail.com>
2026-01-30 06:21:25 -0500

    feat: website monitor config stewards [ENG-2468] (#7275)

--- clients/admin-ui/src/features/integrations/configure-monitor/ConfigureMonitorForm.tsx
+++ clients/admin-ui/src/features/integrations/configure-monitor/ConfigureMonitorForm.tsx
@@ -263,7 +263,7 @@ const ConfigureMonitorForm = ({
         <Select
           mode="multiple"
           aria-label="Select stewards"
-          data-testid="controlled-select"
+          data-testid="controlled-select-stewards"
           options={dataStewardOptions}
         />
       </Form.Item>
