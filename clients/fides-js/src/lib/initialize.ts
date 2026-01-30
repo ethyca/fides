5b826a56f34173b33c7febb00e978adc77231912
Jason Gill <103820+gilluminate@users.noreply.github.com>
2026-01-30 13:16:25 -0700

    save automated consent cookie prior to updating experience

--- clients/fides-js/src/lib/initialize.ts
+++ clients/fides-js/src/lib/initialize.ts
@@ -319,6 +319,18 @@ export const initialize = async ({
           },
         );
         fidesDebugger("Applied automated consent to experience defaults");
+
+        // save the cookie to the browser prior to updating the experience
+        Object.assign(fides.cookie.fides_meta, {
+          consentMethod: automatedMethod,
+        });
+        Object.assign(fides.cookie.consent, automatedNoticeConsent);
+        await saveFidesCookie(fides.cookie, options);
+
+        fidesDebugger(
+          "Saved automated consent to browser cookie",
+          fides.cookie,
+        );
       }
 
       /**
@@ -375,20 +387,6 @@ export const initialize = async ({
       );
       // eslint-disable-next-line no-param-reassign
       fides.cookie = updatedCookie;
-
-      // If automated consent was applied, save the cookie to the browser
-      if (automatedApplied) {
-        // Set the consentMethod on fides_meta before saving
-        Object.assign(fides.cookie.fides_meta, {
-          consentMethod: automatedMethod,
-        });
-
-        await saveFidesCookie(fides.cookie, options);
-        fidesDebugger(
-          "Saved automated consent to browser cookie",
-          fides.cookie,
-        );
-      }
 
       // Initialize the i18n singleton before we render the overlay
       const i18n = setupI18n();
