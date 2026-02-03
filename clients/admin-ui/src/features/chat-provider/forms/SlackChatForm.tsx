cecf26f43b93d8a17752437e90c90863efcdd00f
Adrian Galvan <galvana@uci.edu>
2026-02-03 15:00:11 -0800

    Formatting fixes

--- clients/admin-ui/src/features/chat-provider/forms/SlackChatForm.tsx
+++ clients/admin-ui/src/features/chat-provider/forms/SlackChatForm.tsx
@@ -5,10 +5,7 @@ import { useEffect } from "react";
 import { isErrorResult } from "~/features/common/helpers";
 import { useAPIHelper } from "~/features/common/hooks";
 import { CHAT_PROVIDERS_ROUTE } from "~/features/common/nav/routes";
-import {
-  ChatConfigCreate,
-  ChatConfigUpdate,
-} from "~/types/api";
+import { ChatConfigCreate, ChatConfigUpdate } from "~/types/api";
 
 import {
   useCreateChatConfigMutation,
