c0475f43889ee6c922d501202538b0c1e4a4aee1
John Ewart <john@johnewart.net>
2026-02-05 15:55:48 -0800

    Fix package name

--- scripts/print_version.py
+++ scripts/print_version.py
@@ -1,7 +1,7 @@
 try:
     from importlib.metadata import version
     import json
-    print(json.dumps({'version': version('ethyca_fides')}))
+    print(json.dumps({'version': version('ethyca-fides')}))
 except Exception:
     import json
     print(json.dumps({'version': 'unknown'}))
