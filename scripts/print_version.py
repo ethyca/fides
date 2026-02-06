72444e8a1e707d180c895b579e0e01f76bef87ae
John Ewart <john@johnewart.net>
2026-02-05 16:00:30 -0800

    Formatting

--- scripts/print_version.py
+++ scripts/print_version.py
@@ -1,6 +1,6 @@
 try:
-    from importlib.metadata import version
     import json
+    from importlib.metadata import version
     print(json.dumps({'version': version('ethyca-fides')}))
 except Exception:
     import json
