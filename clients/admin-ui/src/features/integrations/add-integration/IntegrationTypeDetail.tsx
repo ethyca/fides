ab491ecd408853fa601a966bae68b25aa1fc9596
Facundo Lopez Janza <56484504+Linker44@users.noreply.github.com>
2026-02-11 15:17:19 -0300

    Custom integration UI updates (#7321)
    Co-authored-by: Adrian Galvan <galvana@uci.edu>
    Co-authored-by: Lucano Vera <lucanovera@live.com.ar>

--- clients/admin-ui/src/features/integrations/add-integration/IntegrationTypeDetail.tsx
+++ clients/admin-ui/src/features/integrations/add-integration/IntegrationTypeDetail.tsx
@@ -1,5 +1,10 @@
+import { Button } from "fidesui";
+
 import { IntegrationTypeInfo } from "~/features/integrations/add-integration/allIntegrationTypes";
+import { useRemoveCustomIntegration } from "~/features/integrations/hooks/useRemoveCustomIntegration";
 import IntegrationBox from "~/features/integrations/IntegrationBox";
+import { SaasConnectionTypes } from "~/features/integrations/types/SaasConnectionTypes";
+import useIntegrationOption from "~/features/integrations/useIntegrationOption";
 
 const IntegrationTypeDetail = ({
   integrationType,
@@ -7,15 +12,42 @@ const IntegrationTypeDetail = ({
 }: {
   integrationType?: IntegrationTypeInfo;
   onConfigure: () => void;
-}) => (
-  <>
-    <IntegrationBox
-      integration={integrationType?.placeholder}
-      integrationTypeInfo={integrationType}
-      onConfigureClick={onConfigure}
-    />
-    {integrationType?.overview}
-  </>
-);
+}) => {
+  const connectionOption = useIntegrationOption(
+    integrationType?.placeholder.connection_type,
+    integrationType?.placeholder.saas_config?.type as SaasConnectionTypes,
+  );
+
+  const { handleRemove: handleRemoveCustomIntegration, modalContext } =
+    useRemoveCustomIntegration(connectionOption);
+
+  const showRemoveButton =
+    !!connectionOption?.custom &&
+    !!connectionOption?.default_connector_available;
+
+  return (
+    <>
+      {modalContext}
+      <IntegrationBox
+        integration={integrationType?.placeholder}
+        integrationTypeInfo={integrationType}
+        onConfigureClick={onConfigure}
+        otherButtons={
+          showRemoveButton ? (
+            <Button
+              type="link"
+              danger
+              data-testid="remove-custom-integration-btn"
+              onClick={handleRemoveCustomIntegration}
+            >
+              Remove
+            </Button>
+          ) : undefined
+        }
+      />
+      {integrationType?.overview}
+    </>
+  );
+};
 
 export default IntegrationTypeDetail;
