7439952c123c6eb1dff936782314d8ed8e6821a7
Jade Wibbels <jade@ethyca.com>
2026-02-12 16:44:41 -0700

    fix downrev

--- src/fides/api/alembic/migrations/versions/xx_2026_02_11_1840_c0dc13ad2a05_add_property_ids_to_datasetconfig_and_.py
+++ src/fides/api/alembic/migrations/versions/xx_2026_02_11_1840_c0dc13ad2a05_add_property_ids_to_datasetconfig_and_.py
@@ -1,7 +1,7 @@
 """add property_ids to datasetconfig and manual_task_config
 
 Revision ID: c0dc13ad2a05
-Revises: f85bd4c08401
+Revises: d304f57aea6d
 Create Date: 2026-02-11 18:40:41.477080
 
 """
@@ -11,7 +11,7 @@ from sqlalchemy.dialects import postgresql
 
 # revision identifiers, used by Alembic.
 revision = 'c0dc13ad2a05'
-down_revision = 'f85bd4c08401'
+down_revision = 'd304f57aea6d'
 branch_labels = None
 depends_on = None
 
