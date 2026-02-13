696f63b9d968123c31b584bea65e79013c141134
Lucano Vera <lucanovera@live.com.ar>
2026-02-13 16:08:13 -0300

    remove msw mocks

--- clients/admin-ui/src/mocks/handlers.ts
+++ clients/admin-ui/src/mocks/handlers.ts
@@ -1,11 +1,6 @@
 import { taxonomyHandlers } from "~/features/taxonomy/taxonomy.mocks";
 
 import { discoveryMonitorHandlers } from "./action-center/handlers";
-import { privacyAssessmentHandlers } from "./privacy-assessments/handlers";
 
 // eslint-disable-next-line import/prefer-default-export
-export const handlers = [
-  ...taxonomyHandlers(),
-  ...discoveryMonitorHandlers(),
-  ...privacyAssessmentHandlers(),
-];
+export const handlers = [...taxonomyHandlers(), ...discoveryMonitorHandlers()];
