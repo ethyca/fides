aeea63ed4f9d7a0dbbad6da960cfd210d9fce883
Vagoasdf <bruno.gutierrez96@gmail.com>
2026-02-17 14:55:58 -0300

    Fixing verbose comments on request service

--- src/fides/api/service/privacy_request/request_service.py
+++ src/fides/api/service/privacy_request/request_service.py
@@ -321,7 +321,7 @@ def initiate_polling_task_requeue() -> None:
         id=ASYNC_TASKS_STATUS_POLLING,
         coalesce=True,
         replace_existing=True,
-        seconds=CONFIG.execution.async_polling_interval_hours * 3600,
+        seconds=100
     )
 
 
