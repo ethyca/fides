994c22ed5321c86df779a5b4fe05390515e79e33
eastandwestwind <eastandwestwind@gmail.com>
2026-02-18 17:28:32 +0100

    bump num expected connections for test

--- tests/ops/api/v1/endpoints/test_connection_template_endpoints.py
+++ tests/ops/api/v1/endpoints/test_connection_template_endpoints.py
@@ -390,7 +390,7 @@ class TestGetConnections:
         resp = api_client.get(url + "system_type=database", headers=auth_header)
         assert resp.status_code == 200
         data = resp.json()["items"]
-        assert len(data) == 21  # Includes test_datastore
+        assert len(data) == 22  # Includes test_datastore
 
     def test_search_system_type_and_connection_type(
         self,
