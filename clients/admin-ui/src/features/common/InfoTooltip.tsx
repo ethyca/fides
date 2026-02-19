7c606f143cae5dd4ead608f1237ed922c08dd7b9
Jason Gill <103820+gilluminate@users.noreply.github.com>
2026-02-19 16:51:28 -0700

    clean up and polish

--- clients/admin-ui/src/features/common/InfoTooltip.tsx
+++ clients/admin-ui/src/features/common/InfoTooltip.tsx
@@ -14,6 +14,8 @@ export const InfoTooltip = ({ label, ...props }: InfoTooltipProps) =>
     >
       <span
         style={{ color: "var(--fidesui-neutral-200)", display: "inline-block" }}
+        // eslint-disable-next-line jsx-a11y/no-noninteractive-tabindex -- popover makes it interactive
+        tabIndex={0}
       >
         {/* ViewBox is set to match the size of the icon (default 16px) with 1px padding all around */}
         <Icons.InformationFilled viewBox="0 -1 34 34" />
