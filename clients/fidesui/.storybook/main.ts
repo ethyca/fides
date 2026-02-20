0fd1d94fc152d08c4020e5f2c1e690118ef8b2cc
speaker-ender <speaker_ender@protonmail.com>
2026-02-20 15:43:02 -0500

    chore: fix storybook
    chore: remove code
    
    chore: update changelog
    
    chore: remove extra imports
    
    fix: imports
    
    chore: another import fix
    
    chore: actual fix

--- clients/fidesui/.storybook/main.ts
+++ clients/fidesui/.storybook/main.ts
@@ -1,13 +1,5 @@
 import type { StorybookConfig } from "@storybook/react-vite";
-import { dirname, join } from "path";
 
-/**
- * This function is used to resolve the absolute path of a package.
- * It is needed in projects that use Yarn PnP or are set up within a monorepo.
- */
-function getAbsolutePath(value: string): any {
-  return dirname(require.resolve(join(value, "package.json")));
-}
 const config: StorybookConfig = {
   refs: {
     "@chakra-ui/react": {
@@ -17,7 +9,7 @@ const config: StorybookConfig = {
   stories: ["../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
   addons: [],
   framework: {
-    name: getAbsolutePath("@storybook/react-vite"),
+    name: "@storybook/react-vite",
     options: {
       legacyRootApi: true,
     },
