53f34fb473e1fb1f49dcde315e828af52d30e69e
Jeremy Pople <jeremy@ethyca.com>
2026-02-24 12:27:26 -0600

    further code cleanup

--- clients/admin-ui/src/mocks/handlers.ts
+++ clients/admin-ui/src/mocks/handlers.ts
@@ -2,12 +2,10 @@ import { taxonomyHandlers } from "~/features/taxonomy/taxonomy.mocks";
 
 import { discoveryMonitorHandlers } from "./action-center/handlers";
 import { policyHandlers } from "./policy/handlers";
-import { systemLinksHandlers } from "./system-links/handlers";
 
 // eslint-disable-next-line import/prefer-default-export
 export const handlers = [
   ...taxonomyHandlers(),
   ...discoveryMonitorHandlers(),
   ...policyHandlers(),
-  ...systemLinksHandlers(),
 ];
