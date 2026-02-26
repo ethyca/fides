187a1565cad9eba603f3320867be55c6ba522e25
Jeremy Pople <jeremy@ethyca.com>
2026-02-26 17:27:49 -0600

    update remaining pickers

--- clients/admin-ui/src/features/system/privacy-declarations/PrivacyDeclarationForm.tsx
+++ clients/admin-ui/src/features/system/privacy-declarations/PrivacyDeclarationForm.tsx
@@ -21,6 +21,7 @@ import * as Yup from "yup";
 
 import { LegacyResourceTypes } from "~/features/common/custom-fields/types";
 import { ControlledSelect } from "~/features/common/form/ControlledSelect";
+import DatasetSelectOption from "~/features/dataset/DatasetSelectOption";
 import { CustomTextInput } from "~/features/common/form/inputs";
 import { FormGuard } from "~/features/common/hooks/useIsAnyFormDirty";
 import {
@@ -144,6 +145,7 @@ export const PrivacyDeclarationFormComponents = ({
           tooltip="Referenced Dataset fides keys used by the system."
           mode="multiple"
           layout="stacked"
+          optionRender={DatasetSelectOption}
         />
       ) : null}
       {includeCustomFields ? (
