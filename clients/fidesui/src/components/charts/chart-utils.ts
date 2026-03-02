78676935605db28bc88db33dfe0be5923dc40298
Karolis <karolis@ethyca.com>
2026-03-02 12:11:32 -0500

    lint

--- clients/fidesui/src/components/charts/chart-utils.ts
+++ clients/fidesui/src/components/charts/chart-utils.ts
@@ -3,9 +3,16 @@ import { useEffect, useState } from "react";
 export const useChartAnimation = (animationDuration: number): boolean => {
   const [animationActive, setAnimationActive] = useState(true);
   useEffect(() => {
-    if (animationDuration <= 0) return;
-    const timer = setTimeout(() => setAnimationActive(false), animationDuration);
-    return () => clearTimeout(timer);
+    if (animationDuration <= 0) {
+      return undefined;
+    }
+    const timer = setTimeout(
+      () => setAnimationActive(false),
+      animationDuration,
+    );
+    return () => {
+      clearTimeout(timer);
+    };
   }, [animationDuration]);
   return animationActive;
 };
