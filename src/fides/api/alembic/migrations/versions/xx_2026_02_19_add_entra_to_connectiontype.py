89e688018086f01033ab40bc4789222d76d1906e
Daniel Sill <daniel.sill@ethyca.com>
2026-03-03 13:50:58 -0500

    Fix alembic migration

--- src/fides/api/alembic/migrations/versions/xx_2026_02_19_add_entra_to_connectiontype.py
+++ src/fides/api/alembic/migrations/versions/xx_2026_02_19_add_entra_to_connectiontype.py
@@ -10,7 +10,7 @@ from alembic import op
 
 # revision identifiers, used by Alembic.
 revision = "b5c6d7e8f9a0"
-down_revision = "cc9a5690f9e7"
+down_revision = "074796d61d8a"
 branch_labels = None
 depends_on = None
 
