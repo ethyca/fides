4d0b5d0a5013bf985c67a875fd5a6d357d89864e
Karolis <karolis@ethyca.com>
2026-03-16 14:45:47 -0400

    adjust for data

--- clients/admin-ui/src/home/HomeDashboard.tsx
+++ clients/admin-ui/src/home/HomeDashboard.tsx
@@ -1,9 +1,7 @@
 import { Flex } from "fidesui";
 
-const HomeDashboard = () => (
+export const HomeDashboard = () => (
   <Flex vertical gap={24} className="px-10 py-6">
     <div />
   </Flex>
 );
-
-export default HomeDashboard;
