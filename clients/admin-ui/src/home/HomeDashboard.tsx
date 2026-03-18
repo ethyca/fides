26fa05cdf449e2599f6a1ec98a76a64d7ad1c351
Karolis <karolis@ethyca.com>
2026-03-18 00:38:46 -0400

    fix merge issues

--- clients/admin-ui/src/home/HomeDashboard.tsx
+++ clients/admin-ui/src/home/HomeDashboard.tsx
@@ -7,10 +7,7 @@ import { SystemCoverageCard } from "./SystemCoverageCard";
 
 export const HomeDashboard = () => (
   <Flex vertical gap={24} className="px-10 py-6">
-    <Row
-      gutter={24}
-      className="h-[420px] items-stretch lg:h-[500px] xl:h-[650px]"
-    >
+    <Row gutter={24} className="h-[400px] items-stretch lg:h-[500px]">
       <Col xs={24} md={8} lg={8} xxl={8} className="h-full">
         <PostureCard />
       </Col>
