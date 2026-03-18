6168158c6b9ecef48d9847b73ad6613070df76bb
speaker-ender <speaker_ender@protonmail.com>
2026-03-18 14:49:09 -0400

    feat: aggregate statistics
    chore: more types
    
    chore: even more types
    
    chore: all the types
    
    wip: something that works
    
    chore: adding query hook
    
    chore: wip
    
    chore: more types
    
    chore: more type updates
    
    wip: more card stuff
    
    chore: update types
    
    wip: some labels and such
    
    chore: fix comments
    
    core: layout improvements and refactoring
    
    chore: linting and formatting
    
    chore: more responsiveness
    
    chore: further refactors
    
    wip: stuf
    
    chore: comment out
    
    refactor: compact variant
    
    chore: mostly there
    
    fix: formatting
    
    fix: donut chart fit feature
    
    fix: formatting
    
    chore: adding changelog

---
+++ clients/admin-ui/src/features/data-discovery-and-detection/action-center/MonitorStatCard.tsx
@@ -0,0 +1,256 @@
+import classNames from "classnames";
+import {
+  Card,
+  Descriptions,
+  Divider,
+  DonutChart,
+  Flex,
+  Paragraph,
+  TagList,
+  Text,
+  Title,
+} from "fidesui";
+import { PropsWithChildren } from "react";
+
+import { useRelativeTime } from "~/features/common/hooks/useRelativeTime";
+import { nFormatter } from "~/features/common/utils";
+
+type NumericStat = {
+  label: string;
+  count: number;
+};
+
+type PercentStat = {
+  label: string;
+  value: number;
+};
+
+export interface MonitorStatCardProps {
+  title: string;
+  subtitle: string;
+  primaryStat: {
+    label: string;
+    percent?: number;
+    numerator?: number;
+    denominator?: number;
+  };
+  numericStats?: {
+    data: NumericStat[];
+    label: string;
+  };
+  percentageStats?: {
+    data: PercentStat[];
+    label: string;
+  };
+  lastUpdated?: string;
+  compact?: boolean;
+}
+
+const getProgressColor = (percent: number) => {
+  switch (true) {
+    case percent > 80:
+      return "colorSuccess";
+    case percent > 60:
+      return "colorWarning";
+    default:
+      return "colorErrorTextHover";
+  }
+};
+
+export const MonitorStatCard = ({
+  title,
+  subtitle,
+  primaryStat,
+  numericStats,
+  percentageStats,
+  lastUpdated,
+  compact,
+}: PropsWithChildren<MonitorStatCardProps>) => {
+  const relativeTime = useRelativeTime(
+    lastUpdated ? new Date(lastUpdated) : new Date(),
+  );
+
+  return (
+    <Card
+      size="small"
+      rootClassName={classNames(
+        compact ? "w-full" : "h-full overflow-hidden w-full",
+      )}
+      classNames={{
+        body: "h-full overflow-hidden",
+      }}
+      title={
+        !compact && (
+          <Flex justify="space-between" align="baseline">
+            <span>{title}</span>
+            <Text type="secondary" className="text-xs !font-normal">
+              {subtitle}
+            </Text>
+          </Flex>
+        )
+      }
+    >
+      <div
+        className={classNames(
+          ...[
+            `grid overflow-hidden w-full gap-2 xl:gap-4`,
+            compact
+              ? "grid-cols-[1fr,min-content,1fr,min-content,1fr]"
+              : "grid-cols-1 xl:grid-cols-[auto,1fr,1fr]",
+          ],
+        )}
+      >
+        <Flex
+          vertical={!compact}
+          className={classNames([
+            "gap-0 xl:gap-1",
+            ...(compact ? [] : ["content-center justify-center text-center"]),
+          ])}
+        >
+          <div
+            className={classNames(
+              ...[compact ? "h-full" : "min-h-[80px] w-full"],
+            )}
+          >
+            <DonutChart
+              centerLabel={
+                <Title
+                  level={compact ? 3 : 5}
+                  rootClassName={compact ? "!text-xs" : ""}
+                >{`${nFormatter(primaryStat.percent)}%`}</Title>
+              }
+              variant={compact ? "thin" : "default"}
+              fit={compact ? "fill" : "contain"}
+              segments={[
+                {
+                  color: getProgressColor(primaryStat.percent ?? 0),
+                  value: primaryStat.percent ?? 0,
+                },
+                {
+                  color: "colorPrimaryBg",
+                  value: 100 - (primaryStat.percent ?? 0),
+                },
+              ]}
+            />
+          </div>
+          <Flex
+            vertical
+            justify={compact ? "center" : undefined}
+            className="w-full"
+          >
+            <Text
+              type="secondary"
+              strong={compact}
+              className={compact ? "whitespace-nowrap text-xs" : undefined}
+            >
+              {primaryStat.label}
+            </Text>
+            <Text strong>
+              {nFormatter(primaryStat.numerator)} of{" "}
+              {nFormatter(primaryStat.denominator)}
+            </Text>
+          </Flex>
+        </Flex>
+        <div
+          className={classNames(
+            ...[
+              "grid grid-cols-1 grid-rows-[1fr,min-content,1fr,min-content] col-span-2 gap-2",
+              ...(compact ? ["contents"] : []),
+            ],
+          )}
+        >
+          {numericStats && (
+            <>
+              {compact && (
+                <Divider
+                  size="small"
+                  rootClassName="!m-0 !my-0 h-full"
+                  vertical={compact}
+                />
+              )}
+              <div>
+                <Text type="secondary" strong className="text-xs">
+                  {numericStats.label}
+                </Text>
+                <Paragraph
+                  type="secondary"
+                  className="!my-0 !text-xs"
+                  ellipsis={{
+                    rows: 1,
+                    tooltip: {
+                      color: "white",
+                      title: (
+                        <Descriptions
+                          size="small"
+                          column={1}
+                          layout="horizontal"
+                        >
+                          {numericStats.data?.map(({ label, count }) => (
+                            <Descriptions.Item label={label} key={label}>
+                              {nFormatter(count)}
+                            </Descriptions.Item>
+                          ))}
+                        </Descriptions>
+                      ),
+                    },
+                  }}
+                >
+                  {numericStats.data.length > 0
+                    ? numericStats.data
+                        ?.map(
+                          ({ label, count }) => `${nFormatter(count)} ${label}`,
+                        )
+                        .join(", ")
+                    : "None"}
+                </Paragraph>
+              </div>
+            </>
+          )}
+          {percentageStats && (
+            <>
+              <Divider
+                size="small"
+                rootClassName="!m-0 !my-0 h-full"
+                vertical={compact}
+              />
+              <div>
+                <Text type="secondary" strong className="text-xs">
+                  {percentageStats.label}
+                </Text>
+                {percentageStats.data.length > 0 ? (
+                  <TagList
+                    maxTags={undefined}
+                    className="flex flex-nowrap gap-1"
+                    tags={percentageStats.data?.map(({ label, value }) => ({
+                      label: <span>{`${nFormatter(value)}% ${label}`}</span>,
+                      value: `${value}%`,
+                    }))}
+                  />
+                ) : (
+                  <div className="text-xs">None</div>
+                )}
+              </div>
+            </>
+          )}
+          <Paragraph
+            type="secondary"
+            className={classNames(
+              ...[
+                "text-xs w-full text-right self-end !my-0",
+                ...(compact ? ["hidden"] : []),
+              ],
+            )}
+            ellipsis={{ rows: 1 }}
+          >
+            <>
+              Updated: {relativeTime}
+              {
+                // <Button icon={<Icons.Renew />} size="small" type="text" />
+              }
+            </>
+          </Paragraph>
+        </div>
+      </div>
+    </Card>
+  );
+};
---
+++ clients/admin-ui/src/types/api/models/SystemOverview.ts
@@ -0,0 +1,13 @@
+/* istanbul ignore file */
+/* tslint:disable */
+/* eslint-disable */
+
+/**
+ * High-level system counts for infrastructure monitors.
+ */
+export type SystemOverview = {
+  total?: number;
+  approved?: number;
+  needs_attention?: number;
+  coverage_percentage?: number;
+};
