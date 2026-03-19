75d0f9facba0aa9bba01b5032180562e73827f13
Thomas Van Dort <tom@ethyca.com>
2026-03-19 12:23:10 -0400

    MIGRATION

--- src/fides/api/alembic/migrations/versions/xx_2026_03_13_1658_38071fffda39_add_name_to_client.py
+++ src/fides/api/alembic/migrations/versions/xx_2026_03_13_1658_38071fffda39_add_name_to_client.py
@@ -1,7 +1,7 @@
 """add name to client
 
 Revision ID: 38071fffda39
-Revises: b5c6d7e8f9a0
+Revises: 94273d7e8319
 Create Date: 2026-03-13 16:58:07.889894
 
 """
@@ -11,7 +11,7 @@ import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
 revision = '38071fffda39'
-down_revision = 'b5c6d7e8f9a0'
+down_revision = '94273d7e8319'
 branch_labels = None
 depends_on = None
 
