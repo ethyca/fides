04119646a90e7ea9232b1c3b7807d63b8057855c
Karolis <karolis@ethyca.com>
2026-03-26 14:00:40 -0400

    update fonts

--- clients/admin-ui/cypress/support/component.ts
+++ clients/admin-ui/cypress/support/component.ts
@@ -15,10 +15,7 @@
 
 // Import commands.js using ES2015 syntax:
 import "./commands";
-import "@fontsource/inter/400.css";
-import "@fontsource/inter/500.css";
-import "@fontsource/inter/600.css";
-import "@fontsource/inter/700.css";
+import "fidesui/src/ant-theme/global.scss";
 
 import { EnhancedStore } from "@reduxjs/toolkit";
 // Alternatively you can use CommonJS syntax:
--- clients/admin-ui/src/pages/_app.tsx
+++ clients/admin-ui/src/pages/_app.tsx
@@ -1,7 +1,3 @@
-import "@fontsource/inter/400.css";
-import "@fontsource/inter/500.css";
-import "@fontsource/inter/600.css";
-import "@fontsource/inter/700.css";
 import "fidesui/src/tailwind.css";
 import "fidesui/src/ant-theme/global.scss";
 
