f08f9991593baf1b8ec07645fd615ae184376377
Jeremy Pople <jeremy@ethyca.com>
2026-03-30 16:01:41 -0500

    revert text change

--- clients/admin-ui/src/features/common/table/cells/LinkCell.tsx
+++ clients/admin-ui/src/features/common/table/cells/LinkCell.tsx
@@ -27,7 +27,7 @@ export const LinkCell = ({
               variant="primary"
               {...props}
             >
-              <Text unStyled size="sm" ellipsis={{ tooltip: children }}>
+              <Text unStyled ellipsis={{ tooltip: children }}>
                 {children}
               </Text>
             </LinkText>
