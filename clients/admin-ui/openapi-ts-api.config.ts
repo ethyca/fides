4bac34db6ef01f64b5280ca0499ac9b1d9d42e52
Lucano Vera <lucanovera@live.com.ar>
2026-04-08 17:48:27 -0300

    lint fix

--- clients/admin-ui/openapi-ts-api.config.ts
+++ clients/admin-ui/openapi-ts-api.config.ts
@@ -1,4 +1,5 @@
-// eslint-disable-next-line import/no-extraneous-dependencies
+/* eslint-disable import/no-extraneous-dependencies */
+/* eslint-disable no-param-reassign */
 import { defineConfig } from "@hey-api/openapi-ts";
 
 export default defineConfig({
