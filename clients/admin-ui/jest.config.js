415e09dc9bd3c0223bd271b71d8d3f4fbec531e9
Jason Gill <103820+gilluminate@users.noreply.github.com>
2026-04-13 15:00:38 -0600

    fix jest warnings

--- clients/admin-ui/jest.config.js
+++ clients/admin-ui/jest.config.js
@@ -41,7 +41,14 @@ module.exports = {
   transform: {
     // Use babel-jest to transpile tests with the next/babel preset
     // https://jestjs.io/docs/configuration#transform-objectstring-pathtotransformer--pathtotransformer-object
-    "^.+\\.(js|jsx|ts|tsx)$": ["babel-jest", { presets: ["next/babel"] }],
+    "^.+\\.(js|jsx|ts|tsx)$": [
+      "babel-jest",
+      {
+        presets: [
+          ["next/babel", { "preset-react": { runtime: "automatic" } }],
+        ],
+      },
+    ],
   },
   transformIgnorePatterns: [
     "/node_modules/(?!(react-hotkeys-hook)/)",
