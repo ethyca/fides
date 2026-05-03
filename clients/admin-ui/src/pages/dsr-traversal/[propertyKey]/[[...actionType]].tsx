04955997c3c7ea10a377564a1dc45e12062fe997
mfbrown <michael.brown@ethyca.com>
2026-05-03 11:34:50 -0400

    feat(traversal-visualizer): add /dsr-traversal Next.js routes

---
+++ clients/admin-ui/src/pages/dsr-traversal/[propertyKey]/[[...actionType]].tsx
@@ -0,0 +1,23 @@
+import { NextPage } from "next";
+import { useRouter } from "next/router";
+
+import FixedLayout from "~/features/common/FixedLayout";
+import TraversalVisualizerPage from "~/features/dsr-traversal-visualizer/TraversalVisualizerPage";
+
+const DsrTraversalPropertyPage: NextPage = () => {
+  const router = useRouter();
+  const propertyKey =
+    typeof router.query.propertyKey === "string" ? router.query.propertyKey : null;
+  const actionTypeParam = router.query.actionType;
+  const actionType =
+    Array.isArray(actionTypeParam) && actionTypeParam[0] === "erasure"
+      ? "erasure"
+      : "access";
+  return (
+    <FixedLayout title="DSR Traversal" fullHeight>
+      <TraversalVisualizerPage propertyKey={propertyKey} actionType={actionType} />
+    </FixedLayout>
+  );
+};
+
+export default DsrTraversalPropertyPage;
