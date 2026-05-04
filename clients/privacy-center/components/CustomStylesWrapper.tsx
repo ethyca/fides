8ab77e1bc08f81eae895027ff0cbaabf2d392509
Jeremy Pople <jeremy@ethyca.com>
2026-05-04 11:16:55 -0500

    Handle missing styles slice in CustomStylesWrapper for external store

--- clients/privacy-center/components/CustomStylesWrapper.tsx
+++ clients/privacy-center/components/CustomStylesWrapper.tsx
@@ -5,7 +5,9 @@ import { ReactNode } from "react";
 import { useAppSelector } from "~/app/hooks";
 
 const CustomStylesWrapper = ({ children }: { children: ReactNode }) => {
-  const { styles } = useAppSelector((state) => state.styles);
+  const styles = useAppSelector(
+    (state) => (state as any).styles?.styles as string | undefined,
+  );
 
   return (
     <>
