0d90e1d205355a5d1ce89b7ca5413f4eb4bf22fd
Thabo Fletcher <thabofletcher@gmail.com>
2026-05-05 14:04:04 -0700

    Fix migration down_revision to chain from main head (3a91e5d4f7b2)

--- src/fides/api/alembic/migrations/versions/xx_2026_04_23_1930_e3f4a5b6c7d8_add_att_exempt_to_privacy_notice.py
+++ src/fides/api/alembic/migrations/versions/xx_2026_04_23_1930_e3f4a5b6c7d8_add_att_exempt_to_privacy_notice.py
@@ -7,7 +7,7 @@ when the user denies ATT. When True, the notice is exempt from ATT and remains
 user-toggleable regardless of the ATT decision.
 
 Revision ID: e3f4a5b6c7d8
-Revises: d71c7d274c04
+Revises: 3a91e5d4f7b2
 Create Date: 2026-04-23 19:30:00.000000
 
 """
@@ -17,7 +17,7 @@ from alembic import op
 
 # revision identifiers, used by Alembic.
 revision = "e3f4a5b6c7d8"
-down_revision = "d71c7d274c04"
+down_revision = "3a91e5d4f7b2"
 branch_labels = None
 depends_on = None
 
