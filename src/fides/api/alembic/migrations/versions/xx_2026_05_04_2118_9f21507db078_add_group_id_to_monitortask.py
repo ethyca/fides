c03c18de585f2f4b088a2dcf3e2c3b1f7a2f6dc1
Victoria Cruces <victoria@ethyca.com>
2026-05-06 17:21:14 -0300

    Update migration

--- src/fides/api/alembic/migrations/versions/xx_2026_05_04_2118_9f21507db078_add_group_id_to_monitortask.py
+++ src/fides/api/alembic/migrations/versions/xx_2026_05_04_2118_9f21507db078_add_group_id_to_monitortask.py
@@ -1,7 +1,7 @@
 """Add group_id to MonitorTask
 
 Revision ID: 9f21507db078
-Revises: d76443a8a2e3
+Revises: e3f4a5b6c7d8
 Create Date: 2026-05-04 21:18:44.962348
 
 """
@@ -11,7 +11,7 @@ import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
 revision = '9f21507db078'
-down_revision = 'd76443a8a2e3'
+down_revision = 'e3f4a5b6c7d8'
 branch_labels = None
 depends_on = None
 
