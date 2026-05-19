947a69e23aa8f1b8f5c2f23c49b982a4710c5c1c
Mike Garifullin <mike@ethyca.com>
2026-05-19 14:59:05 +0100

    fix(alembic): rebase property_id-nullable migration onto current main head
    Both 1e00f8e12f44 and 9f21507db078 had down_revision=9b449105864d,
    producing two heads and breaking alembic upgrade in CI.

--- src/fides/api/alembic/migrations/versions/xx_2026_05_18_1500_1e00f8e12f44_make_attachment_user_provided_property_id_nullable.py
+++ src/fides/api/alembic/migrations/versions/xx_2026_05_18_1500_1e00f8e12f44_make_attachment_user_provided_property_id_nullable.py
@@ -1,7 +1,7 @@
 """make attachment_user_provided property_id nullable
 
 Revision ID: 1e00f8e12f44
-Revises: 9b449105864d
+Revises: 9f21507db078
 Create Date: 2026-05-18 15:00:00.000000
 
 """
@@ -11,7 +11,7 @@ from alembic import op
 
 # revision identifiers, used by Alembic.
 revision = "1e00f8e12f44"
-down_revision = "9b449105864d"
+down_revision = "9f21507db078"
 branch_labels = None
 depends_on = None
 
